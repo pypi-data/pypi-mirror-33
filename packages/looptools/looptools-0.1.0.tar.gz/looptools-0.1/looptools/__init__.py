143bb2c2f90765c337fe55bb19d4949a8630f152
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:11:58 +0000

    Add package looptools/0.1/looptools-0.1.0.tar.gz

---
+++ packages/looptools/looptools-0.1.0.tar.gz/looptools-0.1/looptools/__init__.py
@@ -0,0 +1,6 @@
+from log import LogOutput
+from timer import Timer
+from counter import Counter
+
+__all__ = ["Timer", "Counter", "LogOutput"]
+__name__ = "Loop Tools"
---
+++ packages/looptools/looptools-0.1.0.tar.gz/looptools-0.1/setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='looptools',
+    version='0.1',
+    packages=['looptools'],
+    url='https://github.com/mrstephenneal/looptools',
+    license='MIT Licence',
+    author='Stephen Neal',
+    author_email='stephen@stephenneal.net',
+    description='Helper utility functions for logging output, timing processes and counting iterations.'
+)
