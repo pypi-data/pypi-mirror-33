152e2734e2a2b96b5e8f754d60e4f4eb27b8e28c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:07:39 +0000

    Add package ie_mbdbl2017_A_yahoo_ks_datareader/0.7/ie_mbdbl2017_A_yahoo_ks_datareader-0.7.tar.gz

---
+++ packages/ie_mbdbl2017_A_yahoo_ks_datareader/ie_mbdbl2017_A_yahoo_ks_datareader-0.7.tar.gz/ie_mbdbl2017_A_yahoo_ks_datareader-0.7/setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(name='ie_mbdbl2017_A_yahoo_ks_datareader',
+      version='0.7',  # Development release
+      description='Python package to retrieve the Price/Sales for technology stocks starting 06/03/2018 till 06/06/2018 as part of Risk & Fraud course IE GMBD 2017',
+	  long_description='Python package to retrieve the Price/Sales for technology stocks starting 06/03/2018 as part of Risk & Fraud course IE GMBD 2017',
+      url='https://github.com/michela23/ie_mbdbl2017_A_yahoo_ks_datareader',
+      author='IE GMBD 2017 Group A',
+      author_email='michel.aa@student.ie.edu',
+      license='MIT',
+          packages=['ie_mbdbl2017_A_yahoo_ks_datareader'],
+      zip_safe=False)
