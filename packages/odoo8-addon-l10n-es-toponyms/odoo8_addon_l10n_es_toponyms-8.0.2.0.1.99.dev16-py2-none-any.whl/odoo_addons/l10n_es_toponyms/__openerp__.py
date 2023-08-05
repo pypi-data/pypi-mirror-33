ed57c0dca8e7081ce37a6b4330b3cbd28de1b07c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:24:37 +0000

    Add package odoo8-addon-l10n-es-toponyms/8.0.2.0.1.99.dev16/odoo8_addon_l10n_es_toponyms-8.0.2.0.1.99.dev16-py2-none-any.whl

---
+++ packages/odoo8-addon-l10n-es-toponyms/odoo8_addon_l10n_es_toponyms-8.0.2.0.1.99.dev16-py2-none-any.whl/odoo_addons/l10n_es_toponyms/__openerp__.py
@@ -0,0 +1,29 @@
+# -*- coding: utf-8 -*-
+# © 2009 Jordi Esteve <jesteve@zikzakmedia.com>
+# © 2013-2016 Pedro M. Baeza <pedro.baeza@serviciosbaeza.com>
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
+
+{
+    "name": "Topónimos españoles",
+    "version": "8.0.2.0.1",
+    "author": "Spanish Localization Team, "
+              "ZikZakMedia, "
+              "Serv. Tecnol. Avanzados - Pedro M. Baeza, "
+              "Odoo Community Association (OCA)",
+    "website": "https://github.com/OCA/l10n-spain",
+    "category": "Localisation/Europe",
+    "depends": [
+        "base_location_geonames_import",
+    ],
+    'contributors': [
+        'Jordi Esteve <jesteve@zikzakmedia.com>',
+        'Pedro M. Baeza <pedro.baeza@serviciosbaeza.com>',
+    ],
+    "license": "AGPL-3",
+    "data": [
+        "wizard/l10n_es_toponyms_wizard.xml",
+    ],
+    'images': ['images/l10n_es_toponyms_config.png'],
+    "demo": [],
+    "installable": True
+}
