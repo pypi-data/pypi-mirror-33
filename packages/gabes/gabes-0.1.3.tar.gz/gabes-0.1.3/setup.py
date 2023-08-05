12ccff65b0b221e58b26998ec9258705500e0293
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:31:21 +0000

    Add package gabes/0.1.3/gabes-0.1.3.tar.gz

---
+++ packages/gabes/gabes-0.1.3.tar.gz/gabes-0.1.3/setup.py
@@ -0,0 +1,50 @@
+#!/usr/bin/env python
+
+import os
+import sys
+
+try:
+    from setuptools import setup
+except ImportError:
+    from distutils.core import setup
+
+
+if sys.argv[-1] == 'publish':
+    os.system('python setup.py sdist upload')
+    sys.exit()
+
+readme = open('README.rst').read()
+
+with open('requirements.txt') as fp:
+    install_requires = fp.read()
+
+setup(
+    name='gabes',
+    version='0.1.3',
+    description='A pythonic library to use Garbled Circuits',
+    long_description=readme + '\n\n',
+    author='Ignacio Navarro',
+    author_email='nachonavarroasv@gmail.com',
+    url='https://github.com/nachonavarro/gabes',
+    packages=[
+        'gabes',
+    ],
+    package_dir={'gabes': 'gabes'},
+    include_package_data=True,
+    install_requires=install_requires,
+    license='MIT',
+    zip_safe=False,
+    entry_points = {
+        'console_scripts': [
+            'gabes = gabes.__main__:main'
+        ]
+    },
+    keywords='gabes',
+    classifiers=[
+        'Development Status :: 2 - Pre-Alpha',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Natural Language :: English',
+        'Programming Language :: Python :: 3.6',
+    ],
+)
