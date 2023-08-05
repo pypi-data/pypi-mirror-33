e7ca83ef1bc33ff181dfa54c4525d0fc69f93735
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:04:08 +0000

    Add package osc-controller/0.1/osc_controller-0.1-py3-none-any.whl

---
+++ packages/osc-controller/osc_controller-0.1-py3-none-any.whl/osccontroller/helpers.py
@@ -0,0 +1,22 @@
+from collections import Iterable
+
+from pythonosc import osc_bundle_builder
+from pythonosc.osc_message_builder import OscMessageBuilder
+
+
+def osc_message(address, args=()):
+    builder = OscMessageBuilder(address)
+    if not isinstance(args, Iterable) or isinstance(args, (str, bytes)):
+        args = [args]
+    for arg in args:
+        builder.add_arg(arg)
+    return builder.build()
+
+
+def osc_bundle(contents):
+    builder = osc_bundle_builder.OscBundleBuilder(osc_bundle_builder.IMMEDIATELY)
+    if not isinstance(contents, Iterable):
+        contents = [contents]
+    for content in contents:
+        builder.add_content(content)
+    return builder.build()
