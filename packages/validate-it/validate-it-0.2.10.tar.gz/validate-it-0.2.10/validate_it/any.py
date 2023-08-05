bdf2c36aae70f521450dff63a5b8d764cd11b790
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:25:05 +0000

    Add package validate-it/0.2.10/validate-it-0.2.10.tar.gz

---
+++ packages/validate-it/validate-it-0.2.10.tar.gz/validate-it-0.2.10/validate_it/any.py
@@ -0,0 +1,15 @@
+import attr
+
+from validate_it.strict import StrictType
+
+
+@attr.s(slots=True)
+class AnyType(StrictType):
+    """
+    Поле, для которого любое значение считается правильным.
+    """
+
+    _base_type = object
+
+
+__all__ = ["AnyType"]
