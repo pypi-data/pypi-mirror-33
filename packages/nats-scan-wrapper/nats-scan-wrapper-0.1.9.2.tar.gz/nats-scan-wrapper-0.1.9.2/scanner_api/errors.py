2ea706b22a076f6ba47b5d9be3e2d69e3beed341
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:05:50 +0000

    Add package nats-scan-wrapper/0.1.9.2/nats-scan-wrapper-0.1.9.2.tar.gz

---
+++ packages/nats-scan-wrapper/nats-scan-wrapper-0.1.9.2.tar.gz/nats-scan-wrapper-0.1.9.2/scanner_api/errors.py
@@ -0,0 +1,35 @@
+class ScanWrapperError(Exception):
+    pass
+
+
+class ModuleNameError(ScanWrapperError):
+    def __str__(self):
+        return "ScanWrapper: Not set module name in kwargs"
+
+
+class WorkerError(ScanWrapperError):
+    def __str__(self):
+        return "ScanWrapper: Error in worker function"
+
+
+class NotEnoughArgsError(ScanWrapperError):
+    def __str__(self):
+        return "ScanWrapper: Take me more arguments"
+
+
+class BadPackageDataError(ScanWrapperError):
+    def __str__(self):
+        return "ScanWrapper: Data does not match the scanner"
+
+
+class ExpiredTimeError(ScanWrapperError):
+    def __str__(self):
+        return "ScanWrapper: Max Task time is expired"
+
+# class (ScanWrapperError):
+#     def __str__(self):
+#         return ""
+
+# class (ScanWrapperError):
+#     def __str__(self):
+#         return ""
