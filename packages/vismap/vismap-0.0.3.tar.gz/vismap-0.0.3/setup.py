9df50c54e1206002dec744f555caa9abd3061988
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 23:09:43 +0000

    Add package vismap/0.0.3/vismap-0.0.3.tar.gz

---
+++ packages/vismap/vismap-0.0.3.tar.gz/vismap-0.0.3/setup.py
@@ -0,0 +1,38 @@
+import setuptools
+
+install_requires = [
+    'numpy',
+    'vispy',
+    'mercantile',
+    'requests',
+    'requests_cache',
+    'pillow',
+]
+
+with open("README.md", "r") as f:
+    long_description = f.read()
+
+entry_points={
+    'console_scripts': [
+        'vismap-example = vismap.example:main',
+    ]
+}
+
+setuptools.setup(
+    name="vismap",
+    version="0.0.3",
+    author="Cody Piersall",
+    author_email="cody.piersall@gmail.com",
+    description="Tile maps rendered with Vispy",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/codypiersall/vismap",
+    packages=['vismap'],
+    classifiers=(
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ),
+    entry_points=entry_points,
+    install_requires=install_requires,
+)
