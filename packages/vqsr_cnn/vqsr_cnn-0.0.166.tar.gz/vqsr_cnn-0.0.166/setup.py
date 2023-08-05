e16da3637ae1445025d298a185344f2c2be153bc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:29:19 +0000

    Add package vqsr_cnn/0.0.166/vqsr_cnn-0.0.166.tar.gz

---
+++ packages/vqsr_cnn/vqsr_cnn-0.0.166.tar.gz/vqsr_cnn-0.0.166/setup.py
@@ -0,0 +1,20 @@
+from distutils.core import setup
+
+setup(name='vqsr_cnn',
+      version='0.0.166',
+      description='Variant quality score recalibration with Convolutional Neural Networks',
+      author='Sam Friedman',
+      author_email='sam@broadinstitute.org',
+      packages=['vqsr_cnn'],
+      url='https://broadinstitute.org/',
+      install_requires=[
+          "keras >= 2.0",
+          "numpy >= 1.13.1",
+          "scipy >= 0.19.1",
+          "pysam >= 0.13",
+          "scikit-learn >= 0.19.1",
+          "matplotlib >= 2.1.2",
+          "pyvcf >= 0.6.8",
+          "biopython >= 1.70"
+      ]
+      )
