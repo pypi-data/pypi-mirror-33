1b424ccf731532c888c409c4355fd4e98d4b3c06
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:04:45 +0000

    Add package datafeed/0.14.87/datafeed-0.14.87.tar.gz

---
+++ packages/datafeed/datafeed-0.14.87.tar.gz/datafeed-0.14.87/setup.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python
+
+from setuptools import setup, find_packages
+
+setup(name='datafeed',
+      version='0.14.87',
+      description='This package has shared components.',
+      author='Simple Trading',
+      packages=find_packages())
