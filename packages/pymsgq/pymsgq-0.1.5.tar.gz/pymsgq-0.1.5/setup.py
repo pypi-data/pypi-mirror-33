68dbb96c4786371f11c43c4aa2363bbfa89cd9c8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:29:29 +0000

    Add package pymsgq/0.1.5/pymsgq-0.1.5.tar.gz

---
+++ packages/pymsgq/pymsgq-0.1.5.tar.gz/pymsgq-0.1.5/setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+setup(  
+      name='pymsgq',
+      version='0.1.5',
+      description="A simple System V msgq interface for linux IPC (msgq)",
+      keywords='system V msgq linux IPC msgsnd msgrcv msgget',  
+      author='jj4jj',
+      author_email='resc@vip.qq.com',
+      license='MIT License',
+      py_modules=['pymsgq'],
+      url='https://github.com/jj4jj/pymsgq',
+)
+
