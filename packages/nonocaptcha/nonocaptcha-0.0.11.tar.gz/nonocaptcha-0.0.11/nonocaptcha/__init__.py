0135ef32ed9f860ad5eb703f387e2747e9652988
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:31:48 +0000

    Add package nonocaptcha/0.0.11/nonocaptcha-0.0.11.tar.gz

---
+++ packages/nonocaptcha/nonocaptcha-0.0.11.tar.gz/nonocaptcha-0.0.11/nonocaptcha/__init__.py
@@ -0,0 +1,35 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+import os.path, sys
+
+version_info = (0, 0, 11)
+__version__ = "{}.{}.{}".format(*version_info)
+
+
+authors = (("Michael Mooney", "mikeyy@mikeyy.com"),)
+
+authors_email = ", ".join("{}".format(email) for _, email in authors)
+
+__license__ = "GPL-3.0"
+__author__ = ", ".join(
+    "{} <{}>".format(name, email) for name, email in authors
+)
+
+package_info = "An asynchronized Python library to automate solving ReCAPTCHA v2 by audio, using Microsoft Azure's Speech-to-Text API. "
+
+__package_dir__ = os.path.dirname(os.path.abspath(__file__))
+
+__maintainer__ = __author__
+
+__all__ = (
+    "__author__",
+    "__author__",
+    "__license__",
+    "__maintainer__",
+    "__package_dir__",
+    "__version__",
+    "version_info",
+)
+
+sys.path.append(os.getcwd())
