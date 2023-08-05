c3c74f40250352de134c83be3ae4291364d1f55e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 23:09:59 +0000

    Add package ie_mbdbl2017_D_yahoo_ks_datareader/0.2/ie_mbdbl2017_D_yahoo_ks_datareader-0.2.tar.gz

---
+++ packages/ie_mbdbl2017_D_yahoo_ks_datareader/ie_mbdbl2017_D_yahoo_ks_datareader-0.2.tar.gz/ie_mbdbl2017_D_yahoo_ks_datareader-0.2/ie_mbdbl2017_D_yahoo_ks_datareader/__init__.py
@@ -0,0 +1,16 @@
+def  datareader_doc():
+    """ function to return the string 'My first Python package that anyone call install it! How cool!!!' """
+    return("Group D first package that anyone can install it! How cool!!!")
+
+def get_data(URL, ticker, start_date, end_date):
+    import pandas as pd
+    df = pd.read_json(URL + ticker +"/" + start_date + "/" + end_date + "/")  
+    return(df)
+    
+#import pandas as pd
+#url = "http://dlichti.pythonanywhere.com/IEGMBD/"
+#ticker = '2885.TW'
+#start_date = '2018-01-01'
+#end_date = '2018-06-06'
+#
+#print(get_data(url, ticker, start_date, end_date))
---
+++ packages/ie_mbdbl2017_D_yahoo_ks_datareader/ie_mbdbl2017_D_yahoo_ks_datareader-0.2.tar.gz/ie_mbdbl2017_D_yahoo_ks_datareader-0.2/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='ie_mbdbl2017_D_yahoo_ks_datareader',
+	version='0.2',  # Development release
+	description='Python Packaging Starting Kit - Simple Structure to creater your own package - for training purposes!',
+	url='https://github.com/manoelgadi/ie_mbdbl2017_D_yahoo_ks_datareader',
+	author='Derrick Lichti',
+	author_email='dlichti@student.ie.edu',
+	license='MIT',
+	packages=['ie_mbdbl2017_D_yahoo_ks_datareader'],
+	zip_safe=False)
