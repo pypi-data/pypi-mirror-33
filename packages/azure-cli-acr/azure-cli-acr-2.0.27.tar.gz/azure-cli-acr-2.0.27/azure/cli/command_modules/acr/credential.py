d0bbf04761aaed6000c7f0391a91d511093ea51f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:31:18 +0000

    Add package azure-cli-acr/2.0.27/azure-cli-acr-2.0.27.tar.gz

---
+++ packages/azure-cli-acr/azure-cli-acr-2.0.27.tar.gz/azure-cli-acr-2.0.27/azure/cli/command_modules/acr/credential.py
@@ -0,0 +1,34 @@
+# --------------------------------------------------------------------------------------------
+# Copyright (c) Microsoft Corporation. All rights reserved.
+# Licensed under the MIT License. See License.txt in the project root for license information.
+# --------------------------------------------------------------------------------------------
+
+from knack.util import CLIError
+
+from ._utils import get_registry_by_name
+
+
+# pylint: disable=inconsistent-return-statements
+def acr_credential_show(cmd, client, registry_name, resource_group_name=None):
+    registry, resource_group_name = get_registry_by_name(cmd.cli_ctx, registry_name, resource_group_name)
+
+    if registry.admin_user_enabled:  # pylint: disable=no-member
+        return client.list_credentials(resource_group_name, registry_name)
+
+    admin_not_enabled_error(registry_name)
+
+
+# pylint: disable=inconsistent-return-statements
+def acr_credential_renew(cmd, client, registry_name, password_name, resource_group_name=None):
+    registry, resource_group_name = get_registry_by_name(cmd.cli_ctx, registry_name, resource_group_name)
+
+    if registry.admin_user_enabled:  # pylint: disable=no-member
+        return client.regenerate_credential(
+            resource_group_name, registry_name, password_name)
+
+    admin_not_enabled_error(registry_name)
+
+
+def admin_not_enabled_error(registry_name):
+    raise CLIError("Run 'az acr update -n {} --admin-enabled true' to enable admin first.".format(
+        registry_name))
