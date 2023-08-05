e96fbade9b8ece132373a2710ca0f7be1b8b66bf
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:08:16 +0000

    Add package eventcore/0.0.3/eventcore-0.0.3.tar.gz

---
+++ packages/eventcore/eventcore-0.0.3.tar.gz/eventcore-0.0.3/setup.py
@@ -0,0 +1,16 @@
+import setuptools
+
+
+setuptools.setup(
+    name='eventcore',
+    version='0.0.3',
+    description='Produce and consume events with any queue.',
+    author='Maikel van den Boogerd',
+    author_email='maikelboogerd@gmail.com',
+    url='https://github.com/maikelboogerd/python-eventcore',
+    keywords=['event', 'queue', 'producer', 'consumer'],
+    packages=['eventcore'],
+    install_requires=['transaction', 'boto3'],
+    license='MIT',
+    zip_safe=False,
+)
