57f7e553d8d46fd09e4aaa0037c7407997384020
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 23:09:42 +0000

    Add package slotomania/0.0.2/slotomania-0.0.2-py3-none-any.whl

---
+++ packages/slotomania/slotomania-0.0.2-py3-none-any.whl/slotomania-0.0.2.data/scripts/sloto.py
@@ -0,0 +1,40 @@
+#!python
+import importlib
+import argparse
+
+from marshmallow import Schema
+from slotomania.contrib.marshmallow_converter import schemas_to_slots
+
+parser = argparse.ArgumentParser(
+    description='Convert marshallow schemas to Python sloto classes '
+    'and typescript interfaces.'
+)
+parser.add_argument(
+    'schema_modules',
+    type=str,
+    nargs='*',
+    help='Dotted path to the module containing marshmallow schemas.'
+)
+parser.add_argument(
+    '-o', '--outputfile', type=str, nargs='?', help='Outputfile'
+)
+
+
+def main():
+    args = parser.parse_args()
+    for module in args.schema_modules:
+        importlib.import_module(module)
+
+    schemas = [s() for s in Schema.__subclasses__()]
+    assert len(schemas), "No schemas"
+
+    output = schemas_to_slots([schema for schema in schemas])
+    if not args.outputfile:
+        print(output)
+    else:
+        with open(args.outputfile, 'w') as f:
+            print(output, file=f)
+
+
+if __name__ == '__main__':
+    main()
