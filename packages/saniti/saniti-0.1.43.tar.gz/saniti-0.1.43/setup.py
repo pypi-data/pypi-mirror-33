0f52b6d1b629724de54d9b85012b965694587a05
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:06:15 +0000

    Add package saniti/0.1.43/saniti-0.1.43.tar.gz

---
+++ packages/saniti/saniti-0.1.43.tar.gz/saniti-0.1.43/setup.py
@@ -0,0 +1,23 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="saniti",
+    version="0.1.43",
+    author="JBMountford",
+    author_email="jbm112358@gmail.com",
+    description="Sanitise text while keeping your sanity",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/ChamRoshi/Saniti",
+    install_requires=['gensim','nltk'],
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ]
+
+)
