a0827231f292a424314cfc6ee3c0d29be42c0731
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:07:37 +0000

    Add package nbrmd/0.2.0/nbrmd-0.2.0.tar.gz

---
+++ packages/nbrmd/nbrmd-0.2.0.tar.gz/nbrmd-0.2.0/setup.py
@@ -0,0 +1,28 @@
+from setuptools import setup, find_packages
+from nbrmd.nbrmd import readme
+
+setup(
+    name='nbrmd',
+    version='0.2.0',
+    author='Marc Wouts',
+    author_email='marc.wouts@gmail.com',
+    description='Jupyter from/to R markdown notebooks',
+    long_description=readme(),
+    long_description_content_type='text/markdown',
+    url='https://github.com/mwouts/nbrmd',
+    packages=find_packages(),
+    entry_points={'console_scripts': ['nbrmd = nbrmd.cli:main']},
+    tests_require=['pytest'],
+    license='MIT',
+    classifiers=('Development Status :: 2 - Pre-Alpha',
+                 'Environment :: Console',
+                 'Framework :: Jupyter',
+                 'Intended Audience :: Science/Research',
+                 'Programming Language :: Python',
+                 'Topic :: Text Processing :: Markup',
+                 'Programming Language :: Python',
+                 'Programming Language :: Python :: 2.7',
+                 'Programming Language :: Python :: 3',
+                 'Programming Language :: Python :: 3.4',
+                 'Programming Language :: Python :: 3.6')
+)
