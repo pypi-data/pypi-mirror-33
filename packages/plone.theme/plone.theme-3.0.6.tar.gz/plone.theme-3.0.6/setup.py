053b957add9df147e3c5d0e3d88e82a1b32a8db5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:30:29 +0000

    Add package plone.theme/3.0.6/plone.theme-3.0.6.tar.gz

---
+++ packages/plone.theme/plone.theme-3.0.6.tar.gz/plone.theme-3.0.6/setup.py
@@ -0,0 +1,59 @@
+import os
+from setuptools import setup, find_packages
+
+
+def read(*rnames):
+    return open(os.path.join(os.path.dirname(__file__), *rnames)).read()
+
+version = '3.0.6'
+
+long_description = (
+    read('README.rst')
+    + '\n' +
+    read('plone', 'theme', 'README.rst')
+    + '\n' +
+    read('CHANGES.rst')
+    + '\n'
+    )
+
+
+setup(name='plone.theme',
+      version=version,
+      description="Tools for managing themes in CMF and Plone sites",
+      long_description=long_description,
+      classifiers=[
+          "Environment :: Web Environment",
+          "Framework :: Plone",
+          "Framework :: Plone :: 5.0",
+          "Framework :: Plone :: 5.1",
+          "Framework :: Plone :: 5.2",
+          "Framework :: Zope2",
+          "License :: OSI Approved :: GNU General Public License v2 (GPLv2)",
+          "Operating System :: OS Independent",
+          "Programming Language :: Python",
+          "Programming Language :: Python :: 2.7",
+      ],
+      keywords='plone theme manage',
+      author='Plone Foundation',
+      author_email='plone-developers@lists.sourceforge.net',
+      url='https://pypi.python.org/pypi/plone.theme',
+      license='GPL version 2',
+      packages=find_packages(exclude=['ez_setup']),
+      namespace_packages=['plone'],
+      include_package_data=True,
+      zip_safe=False,
+      extras_require=dict(
+          test=[
+              'plone.app.testing',
+          ]
+      ),
+      install_requires=[
+          'setuptools',
+          'zope.component',
+          'zope.interface',
+          'zope.publisher',
+          'zope.traversing',
+          'Products.CMFCore',
+          'Zope2',
+      ],
+      )
