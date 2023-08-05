9b57ec71bb953b9ff3812e9c74caea1ade73bfef
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:06:52 +0000

    Add package rainbowmindmachine/3/rainbowmindmachine-3.tar.gz

---
+++ packages/rainbowmindmachine/rainbowmindmachine-3.tar.gz/rainbowmindmachine-3/rainbowmindmachine/SocialSheep.py
@@ -0,0 +1,264 @@
+from .TwitterSheep import TwitterSheep
+import twitter
+import logging
+import traceback
+import time
+
+"""
+Social Sheep
+
+This is the base class for social sheep.
+Social sheep basically add the ability to
+interact with the twitterverse via a pool
+of tweets (matching a hashtag, search term,
+user, etc.)
+
+NOTE: SocialSheep could even be used
+as a "flock" for a single account.
+
+
+
+The Social Sheep class also requires 
+two other classes:
+
+    Toilet - a small pool of tweets
+
+We define all of these classes in one file,
+mainly to challenge ourselves to keep it short.
+"""
+
+
+##########################
+# SocialSheep class
+
+class SocialSheep(TwitterSheep):
+    """
+    SocialSheep receive parameters from their keys.
+    Let Sheep take care of the constructor.
+    """
+
+
+    #############################
+    # Toilet class
+
+    class Toilet(object):
+        """
+        The Toilet class creates a small pool of tweets.
+
+        This class is only defined within the scope of SocialSheep.
+        No other classes can use this Toilet.
+        SocialSheep are *not* social about their Toilets.
+        """
+
+        #############################
+        # BurdHurd class
+
+        class BurdHurd(list):
+            """
+            The simplest class evar.
+
+            Sometimes you can hear the Toilet 
+            whispering to the BurdHurd: 
+                
+                "no one will ever know about you except me."
+            """
+            pass
+
+        ####################################
+        # Ok, on with the Toilet class
+
+        def __init__(self, api):
+            """
+            The Toilet uses the SocialSheep's api and params.
+            """
+            self.api = api
+    
+        def flush(self, **kwargs):
+            """
+            Flush the toilet (get fresh tweets).
+            
+            Currently only one parameter:
+
+                search_term     String to search for
+            """
+            if 'search_term' in kwargs:
+                search_term = kwargs['search_term']
+            else:
+                err = "ERROR: called SocialSheep flush() without search term.\n"
+                err += "Specify a search_term kwarg: flush(search_term = '...')"
+                raise Exception(err)
+
+            self.bowl = list(self.api.GetSearch(term=search_term,count=10))
+
+            # self.bowl is a list of Status objects
+            # this is really useful:
+            # http://python-twitter.readthedocs.io/en/latest/_modules/twitter/models.html#Status
+            # 
+            # a BurdHurd is just a list of usernames
+            # pulled straight out of the toilet.
+            self.hurd = self.BurdHurd()
+            for s in self.bowl:
+                self.hurd.append(s.user)
+    
+        def favorite(self):
+            """Favorite every tweet in the toilet."""
+            for s in self.bowl:
+                try:
+                    self.api.CreateFavorite(status=s)
+                except twitter.error.TwitterError:
+                    # already faved
+                    pass
+    
+        def retweet(self):
+            """Retweet every tweet in the toilet"""
+            for s in self.bowl:
+                self.api.PostRetweet(status_id=s.id)
+
+        def follow(self):
+            """
+            Follow all users in a BurdHurd
+            """
+            # self.hurd is a list of strings (usernames)
+            for u in self.hurd:
+                try:
+                    self.api.CreateFriendship(screen_name=u.screen_name)
+                    logger = logging.getLogger('rainbowmindmachine')
+                    logger.info("Started following @%s"%(u.screen_name))
+                except twitter.error.TwitterError:
+                    # following ourselves,
+                    # or cannot find user
+                    pass
+
+
+
+    #############################################
+    # Ok, on with the SocialSheep class
+
+    def __init__(self, json_file, **kwargs):
+        """
+        SocialSheep constructor calls the Sheep constructor,
+        which sets the Sheep's parameter values.
+
+        It then calls a Plumber to install a Toilet.
+        """
+        # Call super constructor
+        super().__init__(json_file, **kwargs)
+
+        # Now call a plumber to have a toilet installed
+        self._call_plumber()
+
+
+    def _call_plumber(self):
+        """
+        Call a plumber to install a Toilet
+        """
+        # Toilets take parameters for their search.
+        # These come from the SocialSheep's parameters.
+        self.toilet = self.Toilet(self.api)
+
+
+    def favorite(self, **kwargs):
+        """
+        Favorite all the tweets in the toilet
+
+        kwargs:
+            sleep: Time to sleep between flushes
+            follow: boolean, should we follow all the tweeters in the toilet
+
+        The call to flush passes along all of the parameters. 
+        The flush function takes the following parameter:
+
+        kwargs:
+            search_term: String to search for
+
+        This function never ends, so it never returns.
+        """
+        logger = logging.getLogger('rainbowmindmachine')
+
+        if('follow' not in kwargs.keys()):
+            kwargs['follow'] = False
+
+        if('sleep' not in kwargs.keys()):
+            err = "ERROR: called SocialSheep favorite() without search term.\n"
+            err += "Specify a search_term kwarg: flush(search_term = '...')"
+            raise Exception(err)
+
+        while True:
+
+            try:
+                self.toilet.flush(**kwargs)
+            except Exception:
+                err = "ERROR: SocialSheep encountered exception flush()ing tweets in the Toilet"
+                logger.info(err)
+                logger.info(traceback.format_exc())
+
+            try:
+                self.toilet.favorite()
+            except Exception:
+                err = "ERROR: SocialSheep encountered exception favorite()ing tweets in the Toilet"
+                logger.info(err)
+                logger.info(traceback.format_exc())
+
+            if kwargs['follow']:
+                try:
+                    self.toilet.follow()
+                except Exception:
+                    err = "ERROR: SocialSheep encountered exception follow()ing tweets in the Toilet"
+                    logger.info(err)
+                    logger.info(traceback.format_exc())
+
+            time.sleep( kwargs['sleep'] )
+
+
+    def follow(self, **kwargs):
+        """
+        Follow all the tweeters in the toilet
+
+        kwargs:
+            sleep: Time to sleep between flushes
+
+        Call to flush passes along all of the parameters. 
+        The flush function takes the following parameter:
+
+        kwargs:
+            search_term: String to search for
+
+        This function never ends, so it never returns.
+        """
+        logger = logging.getLogger('rainbowmindmachine')
+
+        if('sleep' not in kwargs.keys()):
+            err = "ERROR: called SocialSheep favorite() without search term.\n"
+            err += "Specify a search_term kwarg: flush(search_term = '...')"
+            raise Exception(err)
+
+        while True:
+
+            try:
+                self.toilet.flush(**kwargs)
+            except Exception:
+                err = "ERROR: SocialSheep encountered exception flush()ing toilet"
+                logger.info(err)
+                logger.info(traceback.format_exc())
+
+            try:
+                self.toilet.favorite()
+            except Exception:
+                err = "ERROR: SocialSheep encountered exception favorite()ing toilet"
+                logger.info(err)
+                logger.info(traceback.format_exc())
+
+            time.sleep( kwargs['sleep'] )
+
+    def retweet(self):
+        """
+        Retweet all the tweets in the toilet
+        """
+        self.toilet.retweet()
+
+    def follow(self):
+        """
+        Follow all the users in the toilet
+        """
+        self.toilet.follow()
+
---
+++ packages/rainbowmindmachine/rainbowmindmachine-3.tar.gz/rainbowmindmachine-3/rainbowmindmachine/TwitterSheep.py
@@ -0,0 +1,523 @@
+import boringmindmachine as bmm
+import logging
+import urllib
+import oauth2 as oauth
+import twitter
+import time
+import simplejson as json
+import datetime
+import traceback
+import base64
+import os
+
+class TwitterSheep(bmm.BoringSheep):
+    """
+    rainbow mind machine Sheep class.
+
+    Sheep are created by the Shepherd.
+    Sheep are initialized with a JSON key file plus parameters from the Shepherd.
+    Sheep are expected to take care of their own API instance.
+
+    Input bot key (JSON file) is stored as self.params and contains everything the sheep needs
+    """
+    def __init__(self, bot_key, **kwargs):
+        """
+        bot_key - parameters that come from the keys (and the Keymaker, and the key-making process)
+            - consumer_token
+            - consumer_token_secret
+            - oauth_token
+            - oauth_token_secret
+            - user_id
+            - screen_name
+        
+        kwargs - extra parameter args passed 
+                 into the Sheep (from the Shepherd)
+        
+        A Sheep object manages information for a single Twitter bot account.
+        The information (oauth keys, bot name, bot account, etc) are contained
+        in the JSON file passed in by the Shepherd.
+        
+        The JSON file contains information compiled by the Keymaker.
+        If there is other information the Shepherd needs to pass to the 
+        Sheep that is not in the JSON file, it can use keyword args.
+        """
+
+        # This is where we should initialize the Twitter API instance 
+        # using params found in the json file.
+
+        self.params = bot_key
+ 
+        # combine the user-provided parameters 
+        # (in kwargs) with the json-provided parameters
+        for keys in kwargs:
+            self.params[key] = kwargs[key]
+
+        # Initialize your API instance
+        self.api = twitter.Api( consumer_key        = self.params['consumer_token'],
+                                consumer_secret     = self.params['consumer_token_secret'],
+                                access_token_key    = self.params['oauth_token'],
+                                access_token_secret = self.params['oauth_token_secret'])
+
+        # Get an OAuth token to do bot stuff
+        self.token = oauth.Token(
+                key = self.params['oauth_token'], 
+                secret = self.params['oauth_token_secret']
+        )
+
+        # Add an OAuth application to consume the API
+        self.consumer = oauth.Consumer(
+                key = self.params['consumer_token'], 
+                secret = self.params['consumer_token_secret']
+        )
+
+        # Create an OAuth client
+        self.client = oauth.Client(
+                self.consumer,
+                self.token
+        )
+
+        # Set names
+        self.name = bot_key['screen_name']
+        self.flock_name = bot_key['flock_name']
+
+        logger = logging.getLogger('rainbowmindmachine')
+        msg = "Set up Twitter API for bot {screen_name}"
+        msg = msg.format(screen_name=self.params['screen_name'])
+        logger.info(msg)
+
+
+    #####################################
+    # rainbow mind machine Sheep:
+    # non-Twitter actions
+
+    def dummy(self, **kwargs):
+        """Debug: do nothing."""
+        pass
+
+    def echo(self, **kwargs):
+        """Just say hi"""
+        print("Hello world! This is {name}".format(name=self.name))
+
+
+    #################################
+    # rainbow mind machine Sheep:
+    # Twitter actions
+    # 
+    # change_url
+    # change_bio
+    # change_color
+    # change_image
+    # tweet
+    # follow_user
+    # unfollow_user
+
+    def change_url(self, **kwargs):
+        """Update twiter profile URL.
+
+        kwargs:
+            url:    The new url (string) to set as the profile URL
+
+        Does not return anything.
+        """
+        logger = logging.getLogger('rainbowmindmachine')
+
+        if( 'url' not in kwargs.keys()):
+            err = "ERROR: rmm Sheep: change_url() action called without 'url' kwarg specified."
+            logger.error(err)
+            raise Exception(err)
+
+        # Set the API endpoint 
+        api_url = "https://api.twitter.com/1.1/account/update_profile.json"
+
+        bot_url = kwargs['url']
+
+        resp, content = self.client.request(
+                api_url,
+                method = "POST",
+                body = urllib.parse.urlencode({'url':bot_url}),
+                headers = None
+        )
+
+        logger.info("Completed change_url() method. Set url to: %s"%(bot_url))
+
+
+    def change_bio(self,**kwargs):
+        """Update twitter profile bio.
+
+        kwargs:
+            bio: The bio string
+
+        Does not return anything.
+        """
+        logger = logging.getLogger('rainbowmindmachine')
+
+        if( 'bio' not in kwargs.keys()):
+            err = "change_bio() action called without 'bio' key specified in the parameters dict."
+            logger.error(err)
+            raise Exception(err)
+
+        # Set the API endpoint 
+        url = "https://api.twitter.com/1.1/account/update_profile.json"
+
+        bot_bio = kwargs['bio']
+        resp, content = self.client.request(
+                url,
+                method = "POST",
+                body=urllib.urlencode({'description': bot_bio}),
+                headers=None
+        )
+
+        logger.info("Completed change_bio() method.")
+        logger.info(content)
+
+
+    def change_colors(self,**kwargs):
+        """
+        Update twitter profile colors.
+
+        kwargs:
+            background: RGB code for background color (no #)
+            links: RGB code for links color (no #)
+        
+        Example:
+            kwargs = {
+                'background':'3D3D3D', 
+                'link':'AAF'
+            }
+
+        Does not return anything.
+        """
+        logger = logging.getLogger('rainbowmindmachine')
+
+        if( 'background' not in kwargs.keys()
+                and 'links' not in kwargs.keys()):
+            err = "ERROR: rmm Sheep: change_colors() action called "
+            err += "with neither 'background' nor 'links' kwargs specified."
+            logger.error(err)
+            raise Exception(err)
+
+        # json sent to the Twitter API
+        payload = {}
+
+        if 'background' in kwargs.keys():
+            background_rgbcode = kwargs['background']
+            payload['profile_background_color'] = background_rgbcode
+
+        if 'links' in kwargs.keys():
+            links_rgbcode = kwargs['links']
+            payload['profile_link_color'] = links_rgbcode
+
+        # Set the API endpoint 
+        url = "https://api.twitter.com/1.1/account/update_profile_colors.json"
+
+        resp, content = self.client.request(
+                url,
+                method = "POST",
+                body=urllib.urlencode(payload),
+                headers=None
+        )
+
+        logger.info("Completed change_colors() method.")
+        logger.info(content)
+
+
+    def change_image(self,**kwargs):
+        """Update twitter profile bio.
+
+        Setting 'image' keyword argument takes the highest
+        priority and is the image used if present.
+
+        If that is not available, change_image() will look
+        for an 'image' keyword argument in the bot key.
+
+        kwargs:
+            image: The path to the image to use as the Twitter avatar
+
+        This method does not return anything.
+        """
+        logger = logging.getLogger('rainbowmindmachine')
+
+        if( 'image' not in kwargs.keys() and 'image' not in self.params):
+            err = "change_image() action called without 'image' key specified in the bot key or the parameters dict."
+            logger.error(err)
+            raise Exception(err)
+
+        img_file = ''
+        if( 'image' in kwargs.keys() ):
+            img_file = kwargs['image']
+            if os.path.isfile(img_file) is False:
+                err = "change_image() action called with an 'image' key that is not a file!"
+                raise Exception(err)
+        elif( 'image' in self.params ):
+            img_file = self.params['image']
+            if os.path.isfile(img_file) is False:
+                err = "change_image() action called with an 'image' key that is not a file!"
+                raise Exception(err)
+
+        # json sent to the Twitter API
+        payload = {}
+
+        b64 = base64.encodestring(open(img_file,"rb").read())
+
+        # Set the API endpoint 
+        api_url = "https://api.twitter.com/1.1/account/update_profile_image.json"
+
+        resp, content = self.client.request(
+                api_url,
+                method = "POST",
+                body=urllib.parse.urlencode({'image': b64}),
+                headers=None
+        )
+
+        logger.info("Completed change_image() method.")
+        logger.info(content)
+
+
+    def follow_user(self, **kwargs):
+        """
+        Follow a twitter user.
+
+        kwargs:
+            username: The username of the user to follow
+            notify: Whether to notify the followed user (boolean)
+
+        This method does not return anything.
+        """
+        logger = logging.getLogger('rainbowmindmachine')
+
+        if( 'username' not in kwargs.keys()):
+            err = "change_image() action called without 'image' key specified in the parameters dict."
+            logger.error(err)
+            raise Exception(err)
+
+        if( 'notify' not in kwargs.keys()):
+            kwargs['notify'] = False
+
+        # json sent to the Twitter API
+        payload = {}
+
+        # Set the API endpoint 
+        url = "https://api.twitter.com/1.1/friendships/create.json"
+
+        resp, content = self.client.request(
+                api_url,
+                method = "POST",
+                body=urllib.urlencode({'image': b64}),
+                headers=None
+        )
+
+        logger.info("Completed follow_user() method.")
+        logger.info(content)
+
+
+    def unfollow_user(self, notify=True, **kwargs):
+        """
+        Unfollow a twitter user.
+
+        kwargs:
+            username: The username of the user to follow
+            notify: Whether to notify the followed user (boolean)
+
+        This method does not return anything.
+        """
+        logger = logging.getLogger('rainbowmindmachine')
+
+        if 'username' not in kwargs.keys():
+            err = "unfollow_user() action called without a 'username' key specified in the params dict."
+            logger.error(err)
+            raise Exception(err)
+
+        if( 'notify' not in kwargs.keys()):
+            kwargs['notify'] = False
+
+        # json sent to the Twitter API
+        payload = {}
+
+        # Set the API endpoint 
+        url = "https://api.twitter.com/1.1/friendships/destroy.json"
+
+        payload['user_id'] = kwargs['username']
+
+        resp, content = self.client.request(
+                url,
+                method = "POST",
+                body=urllib.urlencode(payload),
+                headers=None
+        )
+
+        logger.info("Completed unfollow_user() method.")
+        logger.info(content)
+
+
+    def tweet(self, **kwargs):
+        """
+        Send out a tweet. This uses the function:
+
+            populate_tweet_queue()
+
+        Run an infinity loop in which the bot decides when to tweet.
+
+        Default Sheep have the following scheduling kwargs:
+        
+        kwargs:
+            inner_sleep: Inner loop sleep time (1 s)
+            outer_sleep: Outer loop sleep time (10 s)
+            publish: Actually publish (False)
+
+        Additional kwargs:
+
+            media: A URL, a local file, or a file-like object (something with a read() method)
+                   or a list of any of the above
+
+        This function never ends, so it never returns.
+        """
+
+        # Process kwargs
+        defaults = {}
+        defaults['inner_sleep'] = 1.0
+        defaults['outer_sleep'] = 10.0
+        defaults['publish'] = False
+
+        # populate missing params with default values
+        for dk in defaults.keys():
+            if dk not in kwargs.keys():
+                kwargs[dk] = defaults[dk]
+
+        # --------------------------
+        # The Real McCoy
+        #
+        # call populate_tweet_queue() to populate the list of tweets to send out
+        # 
+        # apply some rube goldberg logic to figure out when to tweet each item
+
+        logger = logging.getLogger('rainbowmindmachine')
+
+        while True:
+
+            try:
+                
+                # Outer loop
+                tweet_queue = self.populate_tweet_queue()
+                nelements = len(tweet_queue)
+
+                assert nelements>0
+
+                for ii in range(nelements):
+
+                    twit = tweet_queue.pop(0)
+
+                    # Fire off the tweet
+                    if kwargs['publish']:
+
+                        if(media is None):
+                            self._tweet( twit )
+                        else:
+                            self._tweet( 
+                                    twit, 
+                                    media = media 
+                            )
+
+                    else:
+                        self._print( twit )
+
+
+                    time.sleep( kwargs['inner_sleep'] )
+
+                time.sleep( kwargs['outer_sleep'] )
+
+                msg = "Completed a cycle."
+                logger.info(msg)
+
+            except Exception:
+
+                # oops!
+
+                msg1 = "Sheep encountered an exception. More info:"
+                msg2 = traceback.format_exc()
+                msg3 = "Sheep is continuing..."
+
+                logger.info(msg1)
+                logger.info(msg2)
+                logger.info(msg3)
+
+                time.sleep( kwargs['outer_sleep'] )
+
+            except AssertionError:
+
+                err = "Error: tweet queue was empty. Check your populate_tweet_queue() method definition."
+                logger.error(err)
+                raise Exception(err)
+
+
+    def _tweet(self,twit,media=None):
+        """
+        Private method.
+        Publish a twit.
+        """
+        # call twitter api to tweet the twit
+
+        logger = logging.getLogger('rainbowmindmachine')
+
+        try:
+            # tweet:
+            if(media is not None):
+                stats = self.api.PostUpdates(twit,media=media)
+            else:
+                stats = self.api.PostUpdates(twit)
+
+            # everything else:
+            msg = "@%s tweeted: \"%s\""%(self.name, twit)
+            logger.info(msg)
+
+        except twitter.TwitterError as e:
+            
+            if e.message[0]['code'] == 185:
+                msg = "Twitter error: Daily message limit reached"
+                logger.info(msg)
+
+            elif e.message[0]['code'] == 187:
+                msg = "Twitter error: Duplicate error"
+                logger.info(msg)
+            
+            else:
+                msg = "Twitter error: %s"%(e.message)
+                logger.info(msg)
+
+
+    def populate_tweet_queue(self):
+        """
+        Populate a tweet queue.
+
+        This method should be extended by new Sheep classes that have their own
+        creative means of generating tweets.
+
+        The default Sheep object will generate a tweet queue filled with
+        5 "Hello World" messages.
+
+        Returns a list of tweets.
+        """
+        maxlen = 5
+        tweet_queue = []
+
+        # (technically, a list is a queue)
+        
+        for j in range(maxlen):
+            tweet = "Hello world! That's number %d of 5."%(j+1)
+            tweet_queue.append(tweet)
+
+        logger = logging.getLogger('rainbowmindmachine')
+        msg = "Finished populating a new tweet queue with %d tweets."%(len(tweet_queue))
+        logger.info(msg)
+
+        return tweet_queue
+
+
+    def _print(self,twit):
+        """
+        Private method.
+        Print a twit.
+        """
+        logger = logging.getLogger('rainbowmindmachine')
+        msg = "@%s printed: \"%s\""%(self.name, twit)
+        logger.info(msg)
+
