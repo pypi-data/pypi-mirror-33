1f8e167428228c90aab92bdf3b5088bef08379c0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:05:30 +0000

    Add package listsum/0.1/listsum-0.1.tar.gz

---
+++ packages/listsum/listsum-0.1.tar.gz/listsum-0.1/setup.py
@@ -0,0 +1,13 @@
+from distutils.core import setup
+setup(
+  name = 'listsum',
+  packages = ['listsum'], # this must be the same as the name above
+  version = '0.1',
+  description = 'suma de listas',
+  author = 'jose gabriel guzman',
+  author_email = 'joseguzmanlopez404@gmail.com',
+  url = 'https://github.com/joseGabriel06/listsum', # use the URL to the github repo
+  download_url = 'https://github.com/joseGabriel06/listsum/releases',
+  keywords = ['list', 'sum'],
+  classifiers = [],
+)
