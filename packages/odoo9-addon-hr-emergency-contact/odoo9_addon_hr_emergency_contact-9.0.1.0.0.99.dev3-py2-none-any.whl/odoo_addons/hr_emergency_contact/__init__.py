fc40826725985757191fe9bf8d80474293b5656d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:07:47 +0000

    Add package odoo9-addon-hr-emergency-contact/9.0.1.0.0.99.dev3/odoo9_addon_hr_emergency_contact-9.0.1.0.0.99.dev3-py2-none-any.whl

---
+++ packages/odoo9-addon-hr-emergency-contact/odoo9_addon_hr_emergency_contact-9.0.1.0.0.99.dev3-py2-none-any.whl/odoo_addons/hr_emergency_contact/__init__.py
@@ -0,0 +1,6 @@
+# -*- coding: utf-8 -*-
+# Copytright 2011 Michael Telahun Makonnen <mmakonnen@gmail.com>
+# Copyright 2016 OpenSynergy Indonesia
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
+
+from . import models
