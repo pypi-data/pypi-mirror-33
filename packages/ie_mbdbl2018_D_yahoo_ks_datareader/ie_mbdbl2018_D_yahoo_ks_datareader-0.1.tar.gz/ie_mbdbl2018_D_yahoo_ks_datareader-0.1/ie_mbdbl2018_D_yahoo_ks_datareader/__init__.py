2ca03b0c97d30aeca1b6838cc50debd5fccf91ff
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 23:10:00 +0000

    Add package ie_mbdbl2018_D_yahoo_ks_datareader/0.1/ie_mbdbl2018_D_yahoo_ks_datareader-0.1.tar.gz

---
+++ packages/ie_mbdbl2018_D_yahoo_ks_datareader/ie_mbdbl2018_D_yahoo_ks_datareader-0.1.tar.gz/ie_mbdbl2018_D_yahoo_ks_datareader-0.1/ie_mbdbl2018_D_yahoo_ks_datareader/__init__.py
@@ -0,0 +1,17 @@
+def  datareader_doc():
+    """ function to return the string 'My first Python package that anyone call install it! How cool!!!' """
+    return("Group D first package that anyone can install it! How cool!!!")
+
+def KsDataReader(ticker, start_date, end_date):
+    import pandas as pd
+    URL = "http://dlichti.pythonanywhere.com/IEGMBD/"
+    df = pd.read_json(URL + ticker +"/" + start_date + "/" + end_date + "/")  
+    return(df)
+    
+#import pandas as pd
+#url = "http://dlichti.pythonanywhere.com/IEGMBD/"
+#ticker = '2885.TW'
+#start_date = '2018-01-01'
+#end_date = '2018-06-06'
+#
+#print(get_data(url, ticker, start_date, end_date))
