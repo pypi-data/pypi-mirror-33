9cbccc0df3d5aa6d5b73c59a9dbdeac4c3a92fac
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:25:42 +0000

    Add package espoem_facts/0.0.3/espoem_facts-0.0.3.tar.gz

---
+++ packages/espoem_facts/espoem_facts-0.0.3.tar.gz/espoem_facts-0.0.3/setup.py
@@ -0,0 +1,18 @@
+from setuptools import setup
+
+setup(
+    name='espoem_facts',
+    version='0.0.3',
+    packages=["espoem_facts",],
+    url='http://github.com/emre/espoem_facts',
+    license='MIT',
+    author='emre yilmaz',
+    author_email='mail@emreyilmaz.me',
+    description='an @espoem fact bot on steem blockchain',
+    entry_points={
+        'console_scripts': [
+            'espoem_facts = espoem_facts.main:main',
+        ],
+    },
+    install_requires=["steem"]
+)
