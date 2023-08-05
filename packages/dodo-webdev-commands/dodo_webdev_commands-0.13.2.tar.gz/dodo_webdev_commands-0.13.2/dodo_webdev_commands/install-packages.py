106c0c1467f90f3602caa6031627bc49de487896
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:05:34 +0000

    Add package dodo-webdev-commands/0.13.2/dodo_webdev_commands-0.13.2.tar.gz

---
+++ packages/dodo-webdev-commands/dodo_webdev_commands-0.13.2.tar.gz/dodo_webdev_commands-0.13.2/dodo_webdev_commands/install-packages.py
@@ -0,0 +1,26 @@
+from argparse import ArgumentParser
+from dodo_commands.framework import Dodo
+import os
+
+
+def _args():
+    parser = ArgumentParser()
+    args = Dodo.parse_args(parser)
+    args.node_modules_dir = Dodo.get_config('/SERVER/node_modules_dir')
+    return args
+
+
+if Dodo.is_main(__name__, safe=True):
+    args = _args()
+
+    pip = os.path.join(Dodo.get_config('/SERVER/venv_dir'), 'bin', 'pip')
+    requirements_filename = os.path.join(
+        Dodo.get_config('/SERVER/src_dir'),
+        'website/requirements.txt'
+    )
+
+    Dodo.runcmd([pip, 'install', '-r', requirements_filename])
+    Dodo.runcmd(
+        ['yarn', 'install'],
+        cwd=os.path.abspath(os.path.join(args.node_modules_dir, '..'))
+    )
