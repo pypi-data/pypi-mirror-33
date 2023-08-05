40dc62a3bdd718a3a0689cfbba06dbd90308e742
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:23:51 +0000

    Add package pydnsfeature/0.0.1/pydnsfeature-0.0.1.tar.gz

---
+++ packages/pydnsfeature/pydnsfeature-0.0.1.tar.gz/pydnsfeature-0.0.1/setup.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python
+from __future__ import print_function
+from setuptools import setup, find_packages
+import sys
+
+setup(
+    name='pydnsfeature',
+    version="0.0.1",
+    author="huyifeng",
+    author_email="solopointer@qq.com",
+    description="Get the dns feature for machine learning",
+    long_description=open("README.md").read(),
+    license="MIT",
+    keywords = ["dns", "feature", "machine learning"],
+    url="https://github.com/solopointer/pydnsfeature",
+    packages=['pydnsfeature'],
+    include_package_data = True,
+    install_requires=['pytld', 'dns'],
+    classifiers=[
+        "Environment :: Web Environment",
+        "Intended Audience :: Developers",
+        "Operating System :: OS Independent",
+        "Topic :: Text Processing :: Indexing",
+        "Topic :: Utilities",
+        "Topic :: Internet",
+        "Topic :: Software Development :: Libraries :: Python Modules",
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 2",
+        "Programming Language :: Python :: 2.6",
+        "Programming Language :: Python :: 2.7",
+    ],
+)
+
