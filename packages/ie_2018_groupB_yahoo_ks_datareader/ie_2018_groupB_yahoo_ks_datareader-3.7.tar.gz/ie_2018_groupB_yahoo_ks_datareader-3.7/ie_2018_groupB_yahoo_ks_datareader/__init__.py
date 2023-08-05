8d3f1043b5d708a8998b6135218e33c839806943
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:30:53 +0000

    Add package ie_2018_groupB_yahoo_ks_datareader/3.7/ie_2018_groupB_yahoo_ks_datareader-3.7.tar.gz

---
+++ packages/ie_2018_groupB_yahoo_ks_datareader/ie_2018_groupB_yahoo_ks_datareader-3.7.tar.gz/ie_2018_groupB_yahoo_ks_datareader-3.7/ie_2018_groupB_yahoo_ks_datareader/__init__.py
@@ -0,0 +1,4 @@
+#def  datareader_doc():
+#    """ function to return the string 'My first Python package that anyone call install it! How cool!!!' """
+#    return("Group B first package that anyone can install it! How cool!!!")
+#
