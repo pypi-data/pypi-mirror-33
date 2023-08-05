c44ca26a6a7acdbe45121b170dc53a64ac242b0a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:15:06 +0000

    Add package twsecli/0.6.0/twsecli-0.6.0.tar.gz

---
+++ packages/twsecli/twsecli-0.6.0.tar.gz/twsecli-0.6.0/setup.py
@@ -0,0 +1,24 @@
+from setuptools import setup
+
+setup(
+  name='twsecli',
+  packages=['twsecli'],
+  version='0.6.0',
+  description='TWSE unofficial command-line interface',
+  long_description=open('README.md', encoding='utf-8').read(),
+  long_description_content_type='text/markdown',
+  author='Hans Liu',
+  author_email='hansliu.tw@gmail.com',
+  url='https://github.com/hansliu/twsecli',
+  keywords=['twse'],
+  python_requires='>=3',
+  classifiers=[
+    'License :: OSI Approved :: MIT License',
+    'Programming Language :: Python :: 3',
+  ],
+  entry_points={
+    'console_scripts': [
+      'twsecli=twsecli:cli',
+    ],
+  }
+)
