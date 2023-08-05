b76a4a124f178c45c6061f065a8e0d31177db1fc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:09:19 +0000

    Add package pnvdb/0.2.7/pnvdb-0.2.7-py2.py3-none-any.whl

---
+++ packages/pnvdb/pnvdb-0.2.7-py2.py3-none-any.whl/pnvdb/models/pnvdb_exceptions.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+"""Provide the error handling class """
+
+
+class NvdbError(Exception):
+    """Base class for exceptions in this module."""
+    pass
+
+
+class ApiError(NvdbError):
+    def __init__(self, message):
+        self.message = message
+
+
+def read_api_error(resp):
+    try:
+        return resp.json()[0]['message']
+    except:
+        return 'API seems dead : {}'.format(resp)
