52565c2703490804bab3ebda6102d5c068b135da
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 23:07:46 +0000

    Add package pybel/0.11.6/PyBEL-0.11.6-py2.py3-none-any.whl

---
+++ packages/pybel/PyBEL-0.11.6-py2.py3-none-any.whl/pybel/struct/mutation/__init__.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+
+"""This module contains functions that mutate or make transformations on a network."""
+
+from . import collapse, deletion, expansion, induction, inference, metadata, transfer
+from .collapse import *
+from .deletion import *
+from .expansion import *
+from .induction import *
+from .inference import *
+from .metadata import *
+from .transfer import *
+
+__all__ = (
+        collapse.__all__ +
+        deletion.__all__ +
+        expansion.__all__ +
+        induction.__all__ +
+        inference.__all__ +
+        metadata.__all__ +
+        transfer.__all__
+)
