8a75703d18d62aff5427e675d14d897f68aa8d74
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:06:26 +0000

    Add package mcore-organization-chart/1.0.0/mcore_organization_chart-1.0.0-py2.py3-none-any.whl

---
+++ packages/mcore-organization-chart/mcore_organization_chart-1.0.0-py2.py3-none-any.whl/mcore_organization_chart/__init__.py
@@ -0,0 +1,9 @@
+# -*- coding: utf-8 -*-
+
+from mcore_organization_chart.core import OrganizationTreeBuilder, OrganizationTreeNode
+
+__author__ = """Acciaioli Valverde"""
+__email__ = 'acci.valverde@gmail.com'
+__version__ = '1.0.0'
+
+
