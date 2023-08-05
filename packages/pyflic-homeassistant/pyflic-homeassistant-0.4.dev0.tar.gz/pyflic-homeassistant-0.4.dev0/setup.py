33dd04b7853bb3093f01a4df86f59eb6a181286b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:25:44 +0000

    Add package pyflic-homeassistant/0.4.dev0/pyflic-homeassistant-0.4.dev0.tar.gz

---
+++ packages/pyflic-homeassistant/pyflic-homeassistant-0.4.dev0.tar.gz/pyflic-homeassistant-0.4.dev0/setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+
+setup(name='pyflic-homeassistant',
+      version='0.4.dev0',
+      description='Python library to connect to and interact with Flic buttons.',
+      long_description='This package is created from https://github.com/soldag/pyflic/archive/0.4.zip#pyflic==0.4 No updates are expected.',
+      author='Sören Oldag',
+      author_email='soeren_oldag@freenet.de',
+      url='https://github.com/soldag/pyflic',
+      packages=['pyflic']
+     )
