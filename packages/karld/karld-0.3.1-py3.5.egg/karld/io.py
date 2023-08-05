e865e8aca292a17781aebec1e093d04bbe8c276e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:30:04 +0000

    Add package karld/0.3.1/karld-0.3.1-py3.5.egg

---
+++ packages/karld/karld-0.3.1-py3.5.egg/karld/io.py
@@ -0,0 +1,22 @@
+from karld.loadump import dump_dicts_to_json_file
+
+from karld.loadump import ensure_dir
+from karld.loadump import ensure_file_path_dir
+
+from karld.loadump import i_get_csv_data
+from karld.loadump import is_file_csv
+
+from karld.loadump import i_get_json_data
+from karld.loadump import is_file_json
+
+from karld.loadump import raw_line_reader
+
+from karld.loadump import split_csv_file
+from karld.loadump import split_file
+
+from karld.loadump import split_file_output
+from karld.loadump import split_file_output_csv
+from karld.loadump import split_file_output_json
+
+from karld.loadump import write_as_csv
+from karld.loadump import write_as_json
---
+++ packages/karld/karld-0.3.1-py3.5.egg/karld/tap.py
@@ -0,0 +1,3 @@
+from stream_tap import Bucket
+from stream_tap import stream_tap
+
