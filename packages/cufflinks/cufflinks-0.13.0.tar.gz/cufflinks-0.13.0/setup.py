480a5d9be70737b3823db03bf067869e3b957f7d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:25:58 +0000

    Add package cufflinks/0.13.0/cufflinks-0.13.0.tar.gz

---
+++ packages/cufflinks/cufflinks-0.13.0.tar.gz/cufflinks-0.13.0/setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(name='cufflinks',
+      version='0.13.0',
+      description='Productivity Tools for Plotly + Pandas',
+      author='Jorge Santos',
+      author_email='santos.jorge@gmail.com',
+      license='MIT',
+      keywords = ['pandas', 'plotly', 'plotting'],
+      url = 'https://github.com/santosjorge/cufflinks',
+      packages=['cufflinks'],
+      install_requires = ['pandas','plotly>=2.0.0','colorlover>=0.2'],
+	  zip_safe=False)
