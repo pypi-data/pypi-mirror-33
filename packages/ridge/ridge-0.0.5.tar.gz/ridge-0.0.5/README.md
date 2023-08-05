0b184598ab100aad7033cd86ecce5dfd3781abee
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 23:08:04 +0000

    Add package ridge/0.0.5/ridge-0.0.5.tar.gz

---
+++ packages/ridge/ridge-0.0.5.tar.gz/ridge-0.0.5/README.md
@@ -0,0 +1,24 @@
+# ridge
+Python Machine Learning Library specialized in L2R and Recommendation.
+Named after my favorite racing game `R4`.
+
+## Requirements
+- Python 3.6.0 ~
+
+## Directories
+| name | descripiton |
+|:----:|:------------|
+| ridge | Model implementation |
+| ridge.racer | Cython optimization |
+| docs | Documents (usage of this package & memo related to implementation) |
+| tests | Model test suites |
+
+## Models
+### Matrix Factorization
+#### Performance
+- MovieLens 100k
+  - with Cython 17m20s (1000epochs)
+  - with Python 28m38s (1000epochs)
+
+### Factorization Machine
+WIP
---
+++ packages/ridge/ridge-0.0.5.tar.gz/ridge-0.0.5/setup.py
@@ -0,0 +1,33 @@
+# -*- coding: utf-8 -*-
+import numpy as np
+from setuptools import setup, find_packages, Extension
+from Cython.Distutils import build_ext
+
+
+path2src = 'ridge/racer/src/'
+ext_modules = [
+    Extension('ridge.racer.gradient_steps', [path2src + 'gradient_steps.pyx']),
+    Extension('ridge.racer.link_functions', [path2src + 'link_functions.pyx']),
+    Extension('ridge.racer.loss_calculators', [path2src + 'loss_calculators.pyx']),
+    Extension('ridge.racer.predictors', [path2src + 'predictors.pyx']),
+]
+
+setup(
+    name='ridge',
+    version='0.0.5',
+    description='Python Machine Learning Library specialized in L2R and Recommendation.',
+    author='Moriaki Saigusa',
+    author_email='moriaki3193@gmail.com',
+    url='https://github.com/moriaki3193/ridge',
+    license=license,
+    packages=find_packages(exclude=('tests', 'docs')),
+    install_requires=[
+        'numpy',
+        'scipy',
+        'tqdm',
+    ],
+    cmdclass={'build_ext': build_ext},
+    ext_modules=ext_modules,
+    include_dirs=[np.get_include()],
+    zip_safe=False,
+)
