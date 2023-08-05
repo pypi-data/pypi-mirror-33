59ecd2299dfdff12221ab9c28f18df79bff64f00
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:29:17 +0000

    Add package django-keyerror/3.7.0/django-keyerror-3.7.0.tar.gz

---
+++ packages/django-keyerror/django-keyerror-3.7.0.tar.gz/django-keyerror-3.7.0/README.rst
@@ -0,0 +1,7 @@
+KeyError Django client
+~~~~~~~~~~~~~~~~~~~~~~
+
+This is the Django client for the `KeyError <https://keyerror.com/>`_ error
+service.
+
+`Installation instructions <https://keyerror.com/docs/django>`_.
---
+++ packages/django-keyerror/django-keyerror-3.7.0.tar.gz/django-keyerror-3.7.0/setup.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python
+
+from setuptools import setup, find_packages
+
+setup(
+    name='django-keyerror',
+    version='3.7.0',
+    description='KeyError.com Django client',
+
+    url='https://keyerror.com/docs/django',
+    author="Chris Lamb",
+    author_email='chris@keyerror.com',
+
+    packages=find_packages(),
+
+    install_requires=(
+        'Django>=1.9',
+    ),
+)
