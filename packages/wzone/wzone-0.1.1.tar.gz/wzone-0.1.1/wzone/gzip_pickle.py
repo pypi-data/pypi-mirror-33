2f71dc642014b8ff75599e93aa7a2c48c5d1021d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:29:27 +0000

    Add package wzone/0.1.1/wzone-0.1.1.tar.gz

---
+++ packages/wzone/wzone-0.1.1.tar.gz/wzone-0.1.1/wzone/gzip_pickle.py
@@ -0,0 +1,26 @@
+
+### from https://gist.github.com/thearn/5424244 ###
+
+import cPickle as pickle
+import gzip
+
+def save(object, filename, protocol = 0):
+        """Saves a compressed object to disk
+        """
+        file = gzip.GzipFile(filename, 'wb')
+        file.write(pickle.dumps(object, protocol))
+        file.close()
+
+def load(filename):
+        """Loads a compressed object from disk
+        """
+        file = gzip.GzipFile(filename, 'rb')
+        buffer = ""
+        while True:
+                data = file.read()
+                if data == "":
+                        break
+                buffer += data
+        object = pickle.loads(buffer)
+        file.close()
+        return object
