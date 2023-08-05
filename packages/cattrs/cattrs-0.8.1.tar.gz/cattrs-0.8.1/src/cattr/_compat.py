18fd1aa762089484fda97eeb697c134bc5224fa1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:24:04 +0000

    Add package cattrs/0.8.1/cattrs-0.8.1.tar.gz

---
+++ packages/cattrs/cattrs-0.8.1.tar.gz/cattrs-0.8.1/src/cattr/_compat.py
@@ -0,0 +1,17 @@
+import sys
+
+version_info = sys.version_info[0:3]
+is_py2 = version_info[0] == 2
+is_py3 = version_info[0] == 3
+
+if is_py2:
+    from functools32 import lru_cache
+    from singledispatch import singledispatch
+
+    unicode = unicode  # noqa
+    bytes = str
+else:
+    from functools import lru_cache, singledispatch  # noqa
+
+    unicode = str
+    bytes = bytes
