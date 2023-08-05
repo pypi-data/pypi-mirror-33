731844098cce10eb666a60f9ceaaacfe6613c436
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:31:07 +0000

    Add package apply-pr/1.5.1/apply_pr-1.5.1.tar.gz

---
+++ packages/apply-pr/apply_pr-1.5.1.tar.gz/apply_pr-1.5.1/setup.py
@@ -0,0 +1,29 @@
+from setuptools import setup, find_packages
+
+
+setup(
+    name='apply_pr',
+    version='1.5.1',
+    packages=find_packages(),
+    url='https://github.com/gisce/apply_pr',
+    license='MIT',
+    author='GISCE-TI, S.L.',
+    author_email='devel@gisce.net',
+    description='Apply Pull Requests from GitHub',
+    entry_points='''
+        [console_scripts]
+        apply_pr=apply_pr.cli:apply_pr
+        check_pr=apply_pr.cli:check_pr
+        status_pr=apply_pr.cli:status_pr
+        check_prs_status=apply_pr.cli:check_prs_status
+        create_changelog=apply_pr.cli:create_changelog
+    ''',
+    install_requires=[
+        'fabric<2.0',
+        'osconf',
+        'python-slugify',
+        'requests',
+        'click',
+        'tqdm'
+    ]
+)
