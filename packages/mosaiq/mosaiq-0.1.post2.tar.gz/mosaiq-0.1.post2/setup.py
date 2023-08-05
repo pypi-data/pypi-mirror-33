ef65baf10ea5b1ca0ff2df5337027f02daa69b29
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:14:21 +0000

    Add package mosaiq/0.1.post2/mosaiq-0.1.post2.tar.gz

---
+++ packages/mosaiq/mosaiq-0.1.post2.tar.gz/mosaiq-0.1.post2/setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='mosaiq',
+    version='0.1-2',
+    install_requires=['pandas', 'matplotlib'],
+    license=['MIT'],
+    url=['https://github.com/jdonaldson/mosaiq-python'],
+    author=["Justin Donaldson"],
+    packages=["mosaiq"],
+    author_email=['jdonaldson@gmail.com'],
+)
