9f56c9dd31aa8ddeaafd249f9dc82fff42e8390d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:07:38 +0000

    Add package croniter/0.3.24/croniter-0.3.24.tar.gz

---
+++ packages/croniter/croniter-0.3.24.tar.gz/croniter-0.3.24/setup.py
@@ -0,0 +1,51 @@
+# -*- coding: utf-8 -*-
+import os
+from setuptools import setup, find_packages
+
+
+def read(*rnames):
+    return open(
+        os.path.join('.', *rnames)
+    ).read()
+
+install_requires = [
+    a.strip()
+    for a in read('requirements/base.txt').splitlines()
+    if a.strip() and not a.startswith(('#', '-'))
+]
+
+long_description = "\n\n".join(
+    [
+        read('README.rst'),
+        read('docs', 'CHANGES.rst'),
+    ]
+)
+
+setup(
+    name='croniter',
+    version='0.3.24',
+    py_modules=['croniter', ],
+    description=(
+        'croniter provides iteration for datetime '
+        'object with cron like format'
+    ),
+    long_description=long_description,
+    author="Matsumoto Taichi, kiorky",
+    author_email='taichino@gmail.com, kiorky@cryptelium.net',
+    url='http://github.com/kiorky/croniter',
+    keywords='datetime, iterator, cron',
+    install_requires=install_requires,
+    license="MIT License",
+    classifiers=[
+        "Development Status :: 4 - Beta",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: POSIX",
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 2",
+        "Programming Language :: Python :: 3",
+        "Topic :: Software Development :: Libraries :: Python Modules"],
+    packages=find_packages('src'),
+    package_dir={'': 'src'},
+    include_package_data=True,
+)
