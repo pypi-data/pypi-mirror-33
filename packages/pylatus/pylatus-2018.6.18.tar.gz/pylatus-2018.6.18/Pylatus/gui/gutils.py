c244a8d2a3a39d7434381c825900ec833c73132e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:24:09 +0000

    Add package pylatus/2018.6.18/pylatus-2018.6.18.tar.gz

---
+++ packages/pylatus/pylatus-2018.6.18.tar.gz/pylatus-2018.6.18/Pylatus/gui/gutils.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+from PyQt5 import QtCore, QtWidgets
+from ..controller.config import Config
+from ..controller.utils import hash_pass
+
+
+class GUtils:
+    @staticmethod
+    def askPass(parent):
+        if not Config.RootHash:
+            return True
+        pas, ok = QtWidgets.QInputDialog.getText(parent, 'Password required',
+                                                 'This operation requires a special permission:',
+                                                 QtWidgets.QLineEdit.Password)
+        return Config.RootHash == hash_pass(pas) if ok else False
+
+    @staticmethod
+    def delay(msec):
+        dieTime = QtCore.QTime.currentTime().addMSecs(msec)
+        while QtCore.QTime.currentTime() < dieTime:
+            QtCore.QCoreApplication.processEvents()
