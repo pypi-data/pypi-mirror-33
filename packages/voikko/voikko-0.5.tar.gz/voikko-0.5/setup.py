9bc60926043d597f296b9c994228716d81be4509
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:25:28 +0000

    Add package voikko/0.5/voikko-0.5.tar.gz

---
+++ packages/voikko/voikko-0.5.tar.gz/voikko-0.5/setup.py
@@ -0,0 +1,25 @@
+from setuptools import setup
+
+with open("README.md") as f:
+	long_description = f.read()
+
+setup(
+	name='voikko',
+	version='0.5',
+	description='Python 3 version of libvoikko and the word inflecting library',
+	long_description=long_description,
+	long_description_content_type='text/markdown',
+	url='http://github.com/fergusq/voikko',
+	author='Harri Pitkänen and Iikka Hauhio',
+	author_email='iikka.hauhio@gmail.com',
+	license='GPL',
+	classifiers=[
+		'Programming Language :: Python :: 3'
+	],
+	packages=['voikko'],
+	package_data={
+		'voikko': ['sanat.txt', 'subst.aff', 'verb.aff'],
+	},
+	python_requires='>=3',
+	install_requires=[],
+)
