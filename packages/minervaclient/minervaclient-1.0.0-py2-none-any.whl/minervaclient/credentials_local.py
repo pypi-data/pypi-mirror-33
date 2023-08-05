69acafe2f78ed206a27f109417ef8397666c4429
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:09:26 +0000

    Add package minervaclient/1.0.0/minervaclient-1.0.0-py2-none-any.whl

---
+++ packages/minervaclient/minervaclient-1.0.0-py2-none-any.whl/minervaclient/credentials_local.py
@@ -0,0 +1,6 @@
+### Copy this file to credentials_local.py and edit the settings
+### ONLY EDIT THIS FILE WITH A REAL TEXT EDITOR (No TextEdit or Notepad, ever!)
+
+id = 'McGill Id Number'
+pin = 'McGill Password'
+always_dry_run = False
