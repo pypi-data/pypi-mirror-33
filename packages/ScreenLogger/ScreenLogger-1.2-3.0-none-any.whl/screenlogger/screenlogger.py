253ab92c000a623f718dd4c21fa9b9db6d177bba
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:24:15 +0000

    Add package ScreenLogger/1.2/ScreenLogger-1.2-3.0-none-any.whl

---
+++ packages/ScreenLogger/ScreenLogger-1.2-3.0-none-any.whl/screenlogger/screenlogger.py
@@ -0,0 +1,39 @@
+#!/usr/bin/python3
+
+import logging
+import time
+import os
+
+
+class Msg:
+
+
+    def __init__(self, filename="error.log", 
+            mode=None):
+        self.mode = mode
+        path = (os.path.dirname(
+            os.path.abspath(__file__)) 
+            + "/" + filename)
+        LOGFORMAT = ("%(asctime)s %(levelname)s - "
+                    + "%(message)s - %(pathname)s")
+        logging.basicConfig(filename = path, 
+                            level = logging.ERROR, 
+                            format = LOGFORMAT)
+        self.logger = logging.getLogger()
+
+
+    def message(self, msg):
+        if self.mode != "quiet":
+            print (msg)
+        self.logger.info(msg)
+        time.sleep(0.05)
+
+
+    def error_message(self, msg):
+        if self.mode != "quiet":
+            print (msg)
+        self.logger.error(msg)
+        time.sleep(0.05)
+
+
+# EOF
