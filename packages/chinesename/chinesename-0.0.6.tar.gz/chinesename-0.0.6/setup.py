78fb89f88bf779f761c957919a91f4731c25dcfd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:30:33 +0000

    Add package chinesename/0.0.6/chinesename-0.0.6.tar.gz

---
+++ packages/chinesename/chinesename-0.0.6.tar.gz/chinesename-0.0.6/setup.py
@@ -0,0 +1,21 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="chinesename",
+    version="0.0.6",
+    author="Peng Shiyu",
+    author_email="pengshiyuyx@gmail.com",
+    description="get a chinesename by random",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/mouday/chinesename",
+    packages=setuptools.find_packages(),
+    classifiers=(
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ),
+)
