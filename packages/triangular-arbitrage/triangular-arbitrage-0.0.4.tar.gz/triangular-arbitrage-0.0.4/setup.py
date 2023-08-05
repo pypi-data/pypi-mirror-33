0b82fb481de4ea2ffe2775eec0e47a0b0629f82c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:31:09 +0000

    Add package triangular-arbitrage/0.0.4/triangular-arbitrage-0.0.4.tar.gz

---
+++ packages/triangular-arbitrage/triangular-arbitrage-0.0.4.tar.gz/triangular-arbitrage-0.0.4/setup.py
@@ -0,0 +1,24 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="triangular-arbitrage",
+
+    version='0.0.4',
+
+    description='block chain triangular arbitrage',
+
+    long_description='block chain triangular arbitrage',
+
+    url='http://github.com/mervynlh/triangular_arbitrage',
+
+    author='mervynlh',
+
+    author_email='mervyn_lh@163.com',
+
+    license='MIT',
+
+    keywords='block chain triangular arbitrage',
+
+    packages=find_packages(),
+
+)
