1943e2205d4c88ea6a95507b1228bfc02c4a3f53
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:08:01 +0000

    Add package catchbot/0.7.0/catchbot-0.7.0.tar.gz

---
+++ packages/catchbot/catchbot-0.7.0.tar.gz/catchbot-0.7.0/README.rst
@@ -0,0 +1,4 @@
+catch-hook-telegram-bot
+=======================
+
+Telegram bot to catch webhooks and send prettified data to telegram clients
---
+++ packages/catchbot/catchbot-0.7.0.tar.gz/catchbot-0.7.0/src/catchbot/tasks.py
@@ -0,0 +1,13 @@
+from catchbot.router_bot import RouterBot
+
+from .celery_app import app
+
+try:
+    bot = RouterBot.from_env()
+except:
+    print('Failed to create bot')
+
+
+@app.task
+def send_message_to_bot(chat_id, message):
+    bot.send_message(chat_id, message)
