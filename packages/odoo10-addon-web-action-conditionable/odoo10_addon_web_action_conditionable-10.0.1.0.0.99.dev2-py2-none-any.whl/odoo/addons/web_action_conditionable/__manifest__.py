2cc07388d5b4e9a37d52283b6d59bc7470fdf37c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:07:28 +0000

    Add package odoo10-addon-web-action-conditionable/10.0.1.0.0.99.dev2/odoo10_addon_web_action_conditionable-10.0.1.0.0.99.dev2-py2-none-any.whl

---
+++ packages/odoo10-addon-web-action-conditionable/odoo10_addon_web_action_conditionable-10.0.1.0.0.99.dev2-py2-none-any.whl/odoo/addons/web_action_conditionable/__manifest__.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+{
+    "name": 'web_action_conditionable',
+    "version": "10.0.1.0.0",
+    "depends": [
+        'base',
+        'web',
+    ],
+    'data': ['views/view.xml'],
+    "author": "Cristian Salamea,Odoo Community Association (OCA)",
+    "license": "AGPL-3",
+    'installable': True,
+}
