76f5b88125eaf2b9e588106be443086671ccdfe7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 23:07:02 +0000

    Add package sqlalchemy-filters/0.8.0/sqlalchemy-filters-0.8.0.tar.gz

---
+++ packages/sqlalchemy-filters/sqlalchemy-filters-0.8.0.tar.gz/sqlalchemy-filters-0.8.0/setup.py
@@ -0,0 +1,52 @@
+# -*- coding: utf-8 -*-
+
+import os
+from codecs import open
+from setuptools import setup, find_packages
+
+
+here = os.path.abspath(os.path.dirname(__file__))
+
+with open(os.path.join(here, 'README.rst'), 'r', 'utf-8') as handle:
+    readme = handle.read()
+
+setup(
+    name='sqlalchemy-filters',
+    version='0.8.0',
+    description='A library to filter SQLAlchemy queries.',
+    long_description=readme,
+    author='Student.com',
+    author_email='wearehiring@student.com',
+    url='https://github.com/Overseas-Student-Living/sqlalchemy-filters',
+    packages=find_packages(exclude=['test', 'test.*']),
+    install_requires=[
+        'sqlalchemy>=1.0.16',
+        'six>=1.10.0',
+    ],
+    extras_require={
+        'dev': [
+            'pytest==3.0.5',
+            'flake8==3.2.1',
+            'coverage==4.3.1',
+            'sqlalchemy-utils==0.32.12',
+        ],
+        'mysql': [
+            'mysql-connector-python-rf==2.1.3',
+        ]
+    },
+    zip_safe=True,
+    license='Apache License, Version 2.0',
+    classifiers=[
+        "Programming Language :: Python",
+        "Operating System :: POSIX",
+        "Operating System :: MacOS :: MacOS X",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.3",
+        "Programming Language :: Python :: 3.4",
+        "Programming Language :: Python :: 3.5",
+        "Programming Language :: Python :: 3.6",
+        "Topic :: Internet",
+        "Topic :: Software Development :: Libraries :: Python Modules",
+        "Intended Audience :: Developers",
+    ]
+)
