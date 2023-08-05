557c3238b901328e0867c3e3d8bbe16554f77ec6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:30:35 +0000

    Add package nombot/2.2.1/nombot-2.2.1.tar.gz

---
+++ packages/nombot/nombot-2.2.1.tar.gz/nombot-2.2.1/setup.py
@@ -0,0 +1,54 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+"""The setup script."""
+
+from setuptools import setup, find_packages
+
+
+def parse_requirements(filename):
+    """ load requirements from a pip requirements file """
+    lineiter = (line.strip() for line in open(filename))
+    return [line for line in lineiter if line and not line.startswith("#")]
+
+with open('README.rst') as readme_file:
+    readme = readme_file.read()
+
+with open('HISTORY.rst') as history_file:
+    history = history_file.read()
+
+requirements = parse_requirements('requirements/prod.txt')
+test_requirements = parse_requirements('requirements/test.txt')
+
+setup_requirements = ['pytest-runner', ]
+
+setup(
+    author="Bobby",
+    author_email='karma0@gmail.com',
+    classifiers=[
+        'Development Status :: 2 - Pre-Alpha',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: GNU General Public License v3 (GPLv3)',
+        'Natural Language :: English',
+        'Programming Language :: Python :: 3.6',
+    ],
+    description="A cryptocurrency trading bot",
+    entry_points={
+        'console_scripts': [
+            'nombot=nombot.cli:main',
+        ],
+    },
+    install_requires=requirements,
+    license="GNU General Public License v3",
+    long_description=readme + '\n\n' + history,
+    include_package_data=True,
+    keywords='nombot cryptocurrency bot',
+    name='nombot',
+    packages=find_packages(include=['nombot']),
+    setup_requires=setup_requirements,
+    test_suite='tests',
+    tests_require=test_requirements,
+    url='https://github.com/karma0/nombot',
+    version='2.2.1',
+    zip_safe=False,
+)
