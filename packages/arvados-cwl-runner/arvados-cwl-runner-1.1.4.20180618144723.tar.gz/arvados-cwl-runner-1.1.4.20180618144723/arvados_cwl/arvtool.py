44fe9c2ebe9e31ef271f3501a7a3bae618132b21
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:25:49 +0000

    Add package arvados-cwl-runner/1.1.4.20180618144723/arvados-cwl-runner-1.1.4.20180618144723.tar.gz

---
+++ packages/arvados-cwl-runner/arvados-cwl-runner-1.1.4.20180618144723.tar.gz/arvados-cwl-runner-1.1.4.20180618144723/arvados_cwl/arvtool.py
@@ -0,0 +1,59 @@
+# Copyright (C) The Arvados Authors. All rights reserved.
+#
+# SPDX-License-Identifier: Apache-2.0
+
+from cwltool.command_line_tool import CommandLineTool
+from .arvjob import ArvadosJob
+from .arvcontainer import ArvadosContainer
+from .pathmapper import ArvPathMapper
+
+class ArvadosCommandTool(CommandLineTool):
+    """Wrap cwltool CommandLineTool to override selected methods."""
+
+    def __init__(self, arvrunner, toolpath_object, **kwargs):
+        super(ArvadosCommandTool, self).__init__(toolpath_object, **kwargs)
+        self.arvrunner = arvrunner
+        self.work_api = kwargs["work_api"]
+
+    def makeJobRunner(self, **kwargs):
+        if self.work_api == "containers":
+            return ArvadosContainer(self.arvrunner)
+        elif self.work_api == "jobs":
+            return ArvadosJob(self.arvrunner)
+
+    def makePathMapper(self, reffiles, stagedir, **kwargs):
+        # type: (List[Any], unicode, **Any) -> PathMapper
+        if self.work_api == "containers":
+            return ArvPathMapper(self.arvrunner, reffiles+kwargs.get("extra_reffiles", []), kwargs["basedir"],
+                                 "/keep/%s",
+                                 "/keep/%s/%s",
+                                 **kwargs)
+        elif self.work_api == "jobs":
+            return ArvPathMapper(self.arvrunner, reffiles, kwargs["basedir"],
+                                 "$(task.keep)/%s",
+                                 "$(task.keep)/%s/%s",
+                                 **kwargs)
+
+    def job(self, joborder, output_callback, **kwargs):
+
+        # Workaround for #13365
+        builderargs = kwargs.copy()
+        builderargs["toplevel"] = True
+        builderargs["tmp_outdir_prefix"] = ""
+        builder = self._init_job(joborder, **builderargs)
+        joborder = builder.job
+
+        if self.work_api == "containers":
+            dockerReq, is_req = self.get_requirement("DockerRequirement")
+            if dockerReq and dockerReq.get("dockerOutputDirectory"):
+                kwargs["outdir"] = dockerReq.get("dockerOutputDirectory")
+                kwargs["docker_outdir"] = dockerReq.get("dockerOutputDirectory")
+            else:
+                kwargs["outdir"] = "/var/spool/cwl"
+                kwargs["docker_outdir"] = "/var/spool/cwl"
+        elif self.work_api == "jobs":
+            kwargs["outdir"] = "$(task.outdir)"
+            kwargs["docker_outdir"] = "$(task.outdir)"
+            kwargs["tmpdir"] = "$(task.tmpdir)"
+            kwargs["docker_tmpdir"] = "$(task.tmpdir)"
+        return super(ArvadosCommandTool, self).job(joborder, output_callback, **kwargs)
