552b5c742e976e9f3e57076264bb50f067a7e3bc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:06:09 +0000

    Add package liteflow.providers.mongo/0.3.1/liteflow.providers.mongo-0.3.1.tar.gz

---
+++ packages/liteflow.providers.mongo/liteflow.providers.mongo-0.3.1.tar.gz/liteflow.providers.mongo-0.3.1/setup.py
@@ -0,0 +1,30 @@
+from setuptools import setup
+
+with open("readme.md", "r") as fh:
+    long_description = fh.read()
+
+setup(
+    name='liteflow.providers.mongo',
+    version='0.3.1',
+    description='MongoDB persistence provider for LiteFlow',
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    author='Daniel Gerlag',
+    author_email='daniel@gerlag.ca',
+    license='MIT',
+    namespace_packages=['liteflow'],
+    packages=['liteflow.providers.mongo'],
+    zip_safe=False,
+    install_requires=[
+        'liteflow.core>=0.3',
+        'pymongo>=3.6.1',
+        'python-interface>=1.4.0'
+    ],
+    url="https://github.com/danielgerlag/liteflow",
+    python_requires='>=3.6',
+    classifiers=(
+        "Programming Language :: Python :: 3.6",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    )
+)
