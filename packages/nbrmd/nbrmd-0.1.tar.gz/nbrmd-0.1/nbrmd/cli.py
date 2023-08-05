639f5638a268819e87f941db003607cc344ee092
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:06:26 +0000

    Add package nbrmd/0.1/nbrmd-0.1.tar.gz

---
+++ packages/nbrmd/nbrmd-0.1.tar.gz/nbrmd-0.1/nbrmd/cli.py
@@ -0,0 +1,37 @@
+import os
+from nbrmd import readf, writef
+import argparse
+
+
+def convert(nb_files):
+    """
+    Export R markdown notebooks, or Jupyter notebooks, to the opposite format
+    :param nb_files: list of notebooks
+    :return:
+    """
+    for nb_file in nb_files:
+        file, ext = os.path.splitext(nb_file)
+        if ext == '.ipynb':
+            nb_dest = file + '.Rmd'
+            print("Jupyter notebook {} being converted to R markdown {}".format(nb_file, nb_dest))
+        elif ext == '.Rmd':
+            nb_dest = file + '.ipynb'
+            print("R markdown {} being converted to Jupyter notebook {}".format(nb_file, nb_dest))
+        else:
+            raise TypeError(
+                "File '{}' is neither a Jupyter (.ipynb) nor a R markdown (.Rmd) notebook".format(nb_file))
+        nb = readf(nb_file)
+        writef(nb, nb_dest)
+
+
+def cli(args=None):
+    parser = argparse.ArgumentParser(description='''Jupyter notebook to R markdown converter''')
+    parser.add_argument('notebooks',
+                        help='Name of one or multiple .ipynb or .Rmd notebook(s) to be converted to the alternate form',
+                        nargs='+')
+    return parser.parse_args(args)
+
+
+def main():
+    args = cli()
+    convert(args.notebooks)
---
+++ packages/nbrmd/nbrmd-0.1.tar.gz/nbrmd-0.1/setup.py
@@ -0,0 +1,24 @@
+from setuptools import setup, find_packages
+from nbrmd.nbrmd import readme
+
+setup(
+    name='nbrmd',
+    version='0.1',
+    author='Marc Wouts',
+    author_email='marc.wouts@gmail.com',
+    description='Jupyter from/to R markdown notebooks',
+    long_description=readme(),
+    long_description_content_type='text/markdown',
+    url='https://github.com/mwouts/nbrmd',
+    packages=find_packages(),
+    scripts=['bin/nbrmd'],
+    setup_requires=['pytest-runner'],
+    tests_require=['pytest'],
+    license='MIT',
+    classifiers=('Development Status :: 2 - Pre-Alpha',
+                 'Environment :: Console',
+                 'Framework :: Jupyter',
+                 'Intended Audience :: Science/Research',
+                 'Programming Language :: Python',
+                 'Topic :: Text Processing :: Markup')
+)
