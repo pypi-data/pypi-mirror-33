2e773e31e119e71a9ae91a0cd3bcb8ddbb393800
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:30:46 +0000

    Add package paunch-dir/0.0.4/paunch-dir-0.0.4.zip

---
+++ packages/paunch-dir/paunch-dir-0.0.4.zip/paunch-dir-0.0.4/paunchdir/__init__.py
@@ -0,0 +1,9 @@
+# -*- coding: utf-8 -*-
+
+"""Top-level package for paunch-dir."""
+
+__author__ = """Hong-She Liang"""
+__email__ = 'starofrainnight@gmail.com'
+__version__ = '0.0.4'
+
+from .paunchdir import PaunchDir  # noqa
