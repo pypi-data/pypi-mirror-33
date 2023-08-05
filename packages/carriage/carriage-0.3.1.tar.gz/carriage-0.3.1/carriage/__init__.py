22022da38436b174ec7a0a2d03639857db016602
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:09:43 +0000

    Add package carriage/0.3.1/carriage-0.3.1.tar.gz

---
+++ packages/carriage/carriage-0.3.1.tar.gz/carriage-0.3.1/carriage/__init__.py
@@ -0,0 +1,10 @@
+
+
+from .array import Array
+from .map import Map
+from .optional import Nothing, NothingError, Optional, Some
+from .row import Row
+from .stream import Stream
+
+__all__ = ['Row', 'Map', 'Stream', 'Array',
+           'Optional', 'Some', 'Nothing', 'NothingError']
---
+++ packages/carriage/carriage-0.3.1.tar.gz/carriage-0.3.1/carriage/repr.py
@@ -0,0 +1,14 @@
+import reprlib
+
+short_repr = reprlib.Repr()
+short_repr.maxlist = 10
+short_repr.maxset = 10
+short_repr.maxdict = 10
+short_repr.maxtuple = 10
+short_repr.maxset = 10
+short_repr.maxfrozenset = 10
+short_repr.maxdeque = 10
+short_repr.maxarray = 10
+short_repr.maxlong = 20
+short_repr.maxstring = 20
+short_repr.maxother = 30
