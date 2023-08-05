d8faf8acbb1bf233634d244145a0b8aa266581d5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:28:39 +0000

    Add package pyCore/17.4.6/pycore-17.4.6.tar.gz

---
+++ packages/pyCore/pycore-17.4.6.tar.gz/pycore-17.4.6/setup.py
@@ -0,0 +1,16 @@
+from distutils.core import setup
+from setuptools import find_packages
+
+setup(
+  name = 'pycore',
+  packages = find_packages(),
+  version = '17.04.06',
+  description = 'CoreCluster API Library',
+  author = 'Maciej & Marta Nabozny',
+  author_email = 'mn@mnabozny.pl',
+  url = 'http://cloudover.org/pycore/',
+  download_url = 'https://github.com/cloudOver/PyCloud/archive/master.zip',
+  keywords = ['corecluster', 'overcluster', 'cloudover', 'cloud'],
+  classifiers = [],
+  install_requires = ['requests', 'pytest', 'pytest-cov']
+)
