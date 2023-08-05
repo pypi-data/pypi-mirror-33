152c63a8e450c3b09ba2ff47f20c614f9cca3f9b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:30:38 +0000

    Add package marsha/0.0.3/marsha-0.0.3.tar.gz

---
+++ packages/marsha/marsha-0.0.3.tar.gz/marsha-0.0.3/marsha/formats/iso_date_string.py
@@ -0,0 +1,18 @@
+from dateutil import parser
+from datetime import datetime
+from .base_format import BaseFormat
+
+
+# TODO: Break this out into a module named marsha_dateutil
+class ISODateString(BaseFormat):
+    python_type = datetime
+    formatted_type = str
+
+    def __init__(self, **kwargs):
+        self.kwargs = kwargs
+
+    def load(self, data: formatted_type) -> python_type:
+        return parser.parse(data, **self.kwargs).date()
+
+    def dump(self, data: python_type) -> formatted_type:
+        return data.isoformat()
