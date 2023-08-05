010554e6db96a1c028ff901ae8e9c528680dbfef
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:31:19 +0000

    Add package django-requests-cache/0.1.6/django-requests-cache-0.1.6.tar.gz

---
+++ packages/django-requests-cache/django-requests-cache-0.1.6.tar.gz/django-requests-cache-0.1.6/cache/middleware.py
@@ -0,0 +1,57 @@
+from django.http import HttpResponse
+import redis
+import pickle
+from . import settings
+from redis.exceptions import ConnectionError
+
+
+class CacheMiddleware:
+    def __init__(self, get_response):
+        self.get_response = get_response
+        if settings.ENABLE_REDIS_CACHE:
+            self.cache = redis.Redis(
+                host=settings.REDIS_HOST,
+                port=settings.REDIS_PORT,
+                db=settings.REDIS_DB,
+                socket_timeout=settings.REDIS_SOCKET_TIMEOUT,
+                socket_connect_timeout=settings.REDIS_SOCKET_CONNECT_TIMEOUT,
+                socket_keepalive=settings.REDIS_SOCKET_KEEPALIVE,
+                socket_keepalive_options=settings.REDIS_SOCKET_KEEPALIVE_OPTIONS,
+                connection_pool=settings.REDIS_CONNECTION_POOL,
+                unix_socket_path=settings.REDIS_UNIX_SOCKET_PATH,
+                encoding=settings.REDIS_ENCODING,
+                encoding_errors=settings.REDIS_ENCODING_ERRORS,
+                charset=settings.REDIS_CHARSET,
+                errors=settings.REDIS_ERRORS,
+                decode_responses=settings.REDIS_DECODE_RESPONSES,
+                retry_on_timeout=settings.REDIS_RETRY_ON_TIMEOUT,
+                ssl=settings.REDIS_SSL,
+                ssl_keyfile=settings.REDIS_SSL_KEYFILE,
+                ssl_certfile=settings.REDIS_SSL_CERTFILE,
+                ssl_cert_reqs=settings.REDIS_SSL_CERT_REQS,
+                ssl_ca_certs=settings.REDIS_SSL_CA_CERTS,
+                max_connections=settings.REDIS_MAX_CONNECTIONS
+            )
+            self.cache.info()
+        else:
+            self.cache = None
+
+    def __call__(self, request):
+        if self.cache:
+            response = self.cache.get(request.get_full_path())
+            if not response:
+                response = self.get_response(request)
+                if str(response.status_code) in settings.REDIS_STATUS_CODE_ALLOWED:
+                    data = {
+                        'content': response.content,
+                        'content_type': response._headers.get('content-type')[1]
+                    }
+                    self.cache.set(request.get_full_path(), pickle.dumps(data))
+                    self.cache.expire(request.get_full_path(), settings.REDIS_EXPIRE)
+            else:
+                data = pickle.loads(response)
+                response = HttpResponse(data.get('content'),
+                                        content_type=data.get('content_type'))
+        else:
+            response = self.get_response(request)
+        return response
---
+++ packages/django-requests-cache/django-requests-cache-0.1.6.tar.gz/django-requests-cache-0.1.6/setup.py
@@ -0,0 +1,33 @@
+from setuptools import setup, find_packages
+from codecs import open
+from os import path
+
+here = path.abspath(path.dirname(__file__))
+with open(path.join(here, 'README.rst'), encoding='utf-8') as f:
+    long_description = f.read()
+
+setup(
+    name='django-requests-cache',
+    version='0.1.6',
+    description='Django Cache.',
+    long_description=long_description,
+    url='https://github.com/davidsanfal/django-requests-cache',
+    author='davidsanfal',
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: Developers',
+        'Topic :: Software Development :: Documentation',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 3.6',
+    ],
+    keywords='django cache, redis, cache',
+    packages=find_packages(exclude=['contrib', 'docs', 'tests', 'examples']),
+    install_requires=[
+        'Django==2.0',
+        'redis==2.10.6'
+    ],
+    extras_require={
+        'dev': ['check-manifest'],
+        'test': ['coverage'],
+    },
+)
