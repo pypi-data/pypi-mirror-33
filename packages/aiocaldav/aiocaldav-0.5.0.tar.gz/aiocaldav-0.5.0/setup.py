537e2147960e4de0b52d24bbfad5625784565c8e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:31:27 +0000

    Add package aiocaldav/0.5.0/aiocaldav-0.5.0.tar.gz

---
+++ packages/aiocaldav/aiocaldav-0.5.0.tar.gz/aiocaldav-0.5.0/setup.py
@@ -0,0 +1,34 @@
+#!/usr/bin/python2
+# -*- encoding: utf-8 -*-
+from setuptools import setup, find_packages
+
+version = '0.5.0'
+
+if __name__ == '__main__':
+    setup(
+        name='aiocaldav',
+        version=version,
+        description="asynchronous CalDAV (RFC4791) client library",
+        classifiers=["Development Status :: 4 - Beta",
+                     "Intended Audience :: Developers",
+                     "License :: OSI Approved :: GNU General "
+                     "Public License (GPL)",
+                     "License :: OSI Approved :: Apache Software License",
+                     "Operating System :: OS Independent",
+                     "Programming Language :: Python",
+                     "Programming Language :: Python :: 3.6",
+                     "Topic :: Office/Business :: Scheduling",
+                     "Topic :: Software Development :: Libraries "
+                     ":: Python Modules"],
+        keywords='',
+        author='Cyril Robert',
+        author_email='cyril@hippie.io',
+        maintainer='Thomas Chiroux',
+        maintainer_email='thomas@chroux.com',
+        url='https://github.com/ThomasChiroux/aiocaldav',
+        license='GPL',
+        packages=find_packages(exclude=['tests']),
+        include_package_data=True,
+        zip_safe=False,
+        install_requires=['vobject', 'lxml', 'aiohttp', 'pytz'],
+    )
