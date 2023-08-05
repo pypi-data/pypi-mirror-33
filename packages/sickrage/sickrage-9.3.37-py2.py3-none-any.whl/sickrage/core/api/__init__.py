9fc4f60a058ae24fd87cd84280d126e042b8d5d6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:09:55 +0000

    Add package sickrage/9.3.37/sickrage-9.3.37-py2.py3-none-any.whl

---
+++ packages/sickrage/sickrage-9.3.37-py2.py3-none-any.whl/sickrage/core/api/__init__.py
@@ -0,0 +1,65 @@
+from __future__ import unicode_literals
+
+import json
+import os
+import time
+from urlparse import urljoin
+
+from oauthlib.oauth2 import MissingTokenError, InvalidClientIdError, TokenExpiredError
+from requests_oauthlib import OAuth2Session
+
+import sickrage
+from sickrage.core.api.exceptions import unauthorized, error
+
+
+class API(object):
+    def __init__(self):
+        self.api_url = 'https://api.sickrage.ca/api/v1/'
+        self.token_file = os.path.join(sickrage.app.data_dir, 'sr_token.json')
+        self._session = None
+        self._token = None
+
+    @property
+    def session(self):
+        if self._session is None:
+            self._session = OAuth2Session(token=self.token)
+        return self._session
+
+    @property
+    def token(self):
+        if os.path.exists(self.token_file):
+            with open(self.token_file) as infile:
+                self._token = json.load(infile)
+        return self._token
+
+    @token.setter
+    def token(self, value):
+        with open(self.token_file, 'w') as outfile:
+            json.dump(value, outfile)
+
+    def _request(self, method, url, **kwargs):
+        try:
+            resp = self.session.request(method, urljoin(self.api_url, url), timeout=30,
+                                        hooks={'response': self.throttle_hook}, **kwargs)
+
+            if resp.status_code == 401:
+                raise unauthorized(resp.json()['message'])
+            elif resp.status_code >= 400:
+                raise error(resp.json()['message'])
+
+            return resp.json()
+        except TokenExpiredError as e:
+            self.token = sickrage.app.oidc_client.refresh_token(self.token['refresh_token'])
+            return self._request(method, url, **kwargs)
+        except (InvalidClientIdError, MissingTokenError) as e:
+            sickrage.app.log.warning("SiCKRAGE username or password is incorrect, please try again")
+
+    @staticmethod
+    def throttle_hook(response, **kwargs):
+        ratelimited = "X-RateLimit-Remaining" in response.headers
+
+        if ratelimited:
+            remaining = int(response.headers["X-RateLimit-Remaining"])
+            if remaining == 1:
+                sickrage.app.log.debug("Throttling SiCKRAGE API Calls... Sleeping for 60 secs...\n")
+                time.sleep(60)
