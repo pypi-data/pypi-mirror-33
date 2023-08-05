cbbe44935b8d1e2e8e9aa01f950ab812ff4a3c2a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:24:45 +0000

    Add package django-codenerix-invoicing/2.0.3/django_codenerix_invoicing-2.0.3-py2.py3-none-any.whl

---
+++ packages/django-codenerix-invoicing/django_codenerix_invoicing-2.0.3-py2.py3-none-any.whl/codenerix_invoicing/static/vendings/vending_app.js
@@ -0,0 +1,5 @@
+var module=codenerix_builder(['codenerixInvoicingVendingControllers'],
+    {
+        'list0': [undefined, get_static('vendings/vending_list.html'), 'codenerixVendingCtrl'],
+    }
+);
