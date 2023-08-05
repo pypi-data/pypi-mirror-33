0f62bf7992dbe332e685b1afe93d9c69fbee7d74
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:07:31 +0000

    Add package type2nc/0.3.1/type2nc-0.3.1.tar.gz

---
+++ packages/type2nc/type2nc-0.3.1.tar.gz/type2nc-0.3.1/NEWS.md
@@ -0,0 +1,11 @@
+# Release notes for 0.3.1
+
+  - assign all values from the command line
+  - setup-script for building stand alone windows binaries
+
+# Release notes for 0.3.0
+
+  - First public release
+  - converts font outline to NC code for TNC controls
+  - support for 21 character ranges
+  - output of demo file
---
+++ packages/type2nc/type2nc-0.3.1.tar.gz/type2nc-0.3.1/setup.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="type2nc",
+    version="0.3.1",
+    author="drunsinn",
+    author_email="dr.unsinn@googlemail.com",
+    description="convert truetype fonts to klartext nc-code",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/drunsinn/type2nc",
+    packages=setuptools.find_packages(exclude=['temp', 'tests', 'tests.*', '*.dll']),
+    package_data={'': ['demo_pgm_template.H', 'pgm_foot_template.H', 'pgm_head_template.H']},
+    include_package_data=True,
+    classifiers=(
+        "Programming Language :: Python :: 3",
+        "Development Status :: 4 - Beta",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ),
+    install_requires=('numpy>=1.6.2', 'scipy>=0.19.0', 'freetype>=1.1', 'argparse>=1.4.0'),
+)
