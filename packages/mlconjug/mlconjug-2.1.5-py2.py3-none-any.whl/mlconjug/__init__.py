a955d7e6a820da53a32d24807b93c8521b515371
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:05:56 +0000

    Add package mlconjug/2.1.5/mlconjug-2.1.5-py2.py3-none-any.whl

---
+++ packages/mlconjug/mlconjug-2.1.5-py2.py3-none-any.whl/mlconjug/__init__.py
@@ -0,0 +1,78 @@
+# -*- coding: utf-8 -*-
+
+"""
+MLConjug.
+
+A Python library to conjugate verbs of in French, English, Spanish, Italian, Portuguese and Romanian (mores soon) using Machine Learning techniques.
+Any verb in one of the supported language can be conjugated as the module contains a Machine Learning model of how the verbs behave.
+Even completely new or made-up verbs can be successfully conjugated in this manner.
+The supplied pre-trained models are composed of:
+
+- a binary feature extractor,
+- a feature selector using Linear Support Vector Classification,
+- a classifier using Stochastic Gradient Descent.
+
+MLConjug uses scikit-learn to implement the Machine Learning algorithms.
+Users of the library can use any compatible classifiers from scikit-learn to modify and retrain the model.
+
+Usage example:
+    $ mlconjug manger
+
+    $ mlconjug bring -l en
+
+    $ mlconjug gallofar --language es
+"""
+
+__author__ = """SekouD"""
+__email__ = 'sekoud.python@gmail.com'
+__version__ = '2.1.5'
+
+from .mlconjug import EndingCountVectorizer, DataSet, Model, LinearSVC, SGDClassifier, SelectFromModel, Conjugator, Pipeline
+from .PyVerbiste import Verbiste
+import pkg_resources
+import platform
+import locale
+import gettext
+
+
+# Sets up the automatic translation of annotated strings displayed to the user.
+RESOURCE_PACKAGE = __name__
+TRANSLATIONS_PATH = pkg_resources.resource_filename(RESOURCE_PACKAGE, 'locale')
+
+SUPPORETD_LANGUAGES = ('es', 'fr', 'it', 'pt', 'ro')
+
+if 'Windows' in platform.system():
+    import ctypes
+    windll = ctypes.windll.kernel32
+    user_locale = locale.windows_locale[ windll.GetUserDefaultUILanguage() ][:2]
+    pass
+else:
+    user_locale = locale.getdefaultlocale()[:2]
+    pass
+
+if user_locale in SUPPORETD_LANGUAGES:
+    MLCONJUG_TRANSLATIONS = gettext.translation(domain='mlconjug',
+                                                localedir=pkg_resources.resource_filename(RESOURCE_PACKAGE, 'locale'),
+                                                languages=[user_locale], fallback=True, codeset='UTF-8')
+    print('blah')
+    pass
+else:
+    MLCONJUG_TRANSLATIONS = gettext.NullTranslations()
+    # _ = lambda string: string
+    pass
+
+MLCONJUG_TRANSLATIONS.install()
+
+
+# Enables the translation of docstrings when using the help() builtin function.
+import inspect
+def getdoc(object):
+    try:
+        doc = object.__doc__
+    except AttributeError:
+        return None
+    if not isinstance(doc, str):
+        return None
+    return inspect.cleandoc(_(doc))
+
+inspect.getdoc = getdoc
---
+++ packages/mlconjug/mlconjug-2.1.5-py2.py3-none-any.whl/mlconjug/cli.py
@@ -0,0 +1,24 @@
+# -*- coding: utf-8 -*-
+
+"""Console script for mlconjug."""
+
+import click
+import mlconjug
+from pprint import pprint
+
+
+@click.command()
+@click.argument('verb')
+@click.option('-l', '--language',
+              default='fr',
+              help=_("The language for the conjugation model. The values can be fr, en, es, it, pt or ro. The default value is fr."),
+              type=click.STRING)
+def main(verb, language):
+    """Console script for mlconjug."""
+    conjugator = mlconjug.Conjugator(language)
+    result = conjugator.conjugate(verb)
+    pprint(result.conjug_info)
+
+
+if __name__ == "__main__":
+    main()
