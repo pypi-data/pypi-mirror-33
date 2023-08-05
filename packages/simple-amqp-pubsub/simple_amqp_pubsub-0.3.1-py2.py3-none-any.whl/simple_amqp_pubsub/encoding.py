374bb1f96499d8e77d8f1b6ff5bceeb5d65d5421
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:09:15 +0000

    Add package simple-amqp-pubsub/0.3.1/simple_amqp_pubsub-0.3.1-py2.py3-none-any.whl

---
+++ packages/simple-amqp-pubsub/simple_amqp_pubsub-0.3.1-py2.py3-none-any.whl/simple_amqp_pubsub/encoding.py
@@ -0,0 +1,37 @@
+import msgpack
+
+from simple_amqp import AmqpMsg
+
+from .data import Event
+
+CONTENT_TYPE_MSGPACK = 'application/msgpack'
+
+
+def encode_event(event: Event) -> AmqpMsg:
+    payload = msgpack.packb({
+        'source': event.source,
+        'topic': event.topic,
+        'payload': event.payload,
+    })
+    headers = {}
+    if event.retry_count > 0:
+        headers['retry_count'] = event.retry_count
+
+    return AmqpMsg(
+        payload=payload,
+        content_type=CONTENT_TYPE_MSGPACK,
+        headers=headers,
+    )
+
+
+def decode_event(msg: AmqpMsg, pipe_name: str) -> Event:
+    payload = msgpack.unpackb(msg.payload, encoding='utf8')
+    retry_count = msg.headers.get('retry_count', 0)
+
+    return Event(
+        source=payload['source'],
+        topic=payload['topic'],
+        payload=payload['payload'],
+        pipe=pipe_name,
+        retry_count=retry_count,
+    )
