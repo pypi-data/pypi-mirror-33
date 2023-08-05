ff3b0fb0c2f299e7792820a845003395c3c676d9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 23:07:06 +0000

    Add package roboemotion/1.0.1/roboemotion-1.0.1.tar.gz

---
+++ packages/roboemotion/roboemotion-1.0.1.tar.gz/roboemotion-1.0.1/setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup
+
+setup(name='roboemotion',
+	version='1.0.1',
+	description='emotion expressing for robotic arms',
+	url='https://github.com/chengjovy/RoboEmotion',
+	author='Jovy Cheng',
+	author_email='chengjovy83@gmail.com',
+	license='MIT',
+	packages=['roboemotion'],
+	install_requires=[
+		'serial',
+	],
+	zip_safe=False)
