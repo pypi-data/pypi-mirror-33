e175ac9815f34d14d1c9b9d4a4b0c0935ba873fe
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 23:09:50 +0000

    Add package aquachain.py/0.0.5/aquachain.py-0.0.5.tar.gz

---
+++ packages/aquachain.py/aquachain.py-0.0.5.tar.gz/aquachain.py-0.0.5/setup.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python3
+from setuptools import setup
+version = '0.0.5'
+setup(
+    install_requires = ['base58', 'mnemonic', 'web3', 'ethereum'],
+    name='aquachain.py',
+    url='https://github.com/aquachain/aquachain.py',
+    packages = ['aquachain'],
+    version=version,
+    description='Aquachain for Python 3.6.5',
+    long_description='Aquachain for Python 3.6.5 -- See https://github.com/aquachain/aquachain.py or https://aquachain.github.io for more information.',
+    author='Aquachain Authors',
+    author_email='aquachain@riseup.net',
+    license = 'GPL',
+    test_suite='aquachain.tests',
+    classifiers = [
+        'Development Status :: 2 - Pre-Alpha',
+        'License :: Free for non-commercial use',
+        'License :: OSI Approved :: GNU General Public License (GPL)',
+	],
+)
