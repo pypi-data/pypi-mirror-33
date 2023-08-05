948b673c98fab99bfd359f305fa13d95aad2351a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:30:44 +0000

    Add package guider/3.9.2/guider-3.9.2.tar.gz

---
+++ packages/guider/guider-3.9.2.tar.gz/guider-3.9.2/setup.py
@@ -0,0 +1,39 @@
+import site, os
+from distutils.core import setup
+
+setup(
+    name = 'guider',
+    version = '3.9.2',
+    license = 'GPL2',
+    description = 'A system-wide analyzer of performance',
+    author = 'Peace Lee',
+    author_email = 'iipeace5@gmail.com',
+    url = 'https://github.com/iipeace/guider',
+    download_url = 'https://github.com/iipeace/guider/archive/master.zip',
+    keywords = ['guider', 'linux', 'analyzer', 'performance', 'profile', 'trace', 'kernel'],
+    packages = ['guider'],
+    data_files=[('/usr/sbin', ['guider/guider'])],
+    classifiers = [],
+)
+
+try:
+    os.mkdir('/usr/share/guider')
+except:
+    pass
+
+for path in site.getsitepackages():
+    try:
+        os.symlink('%s/guider/guider.pyc' % path, '/usr/share/guider/guider.pyc')
+    except:
+        pass
+    try:
+        os.symlink('%s/usr/sbin/guider' % path, '/usr/sbin/guider')
+    except:
+        pass
+
+'''
+upload command
+    # python setup.py sdist upload -r pypitest
+    # python setup.py sdist upload -r https://upload.pypi.org/legacy/
+'''
+
