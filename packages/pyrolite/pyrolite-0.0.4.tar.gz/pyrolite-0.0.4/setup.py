b802ddb71a06acb11b569e595c5c11354a154038
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:29:17 +0000

    Add package pyrolite/0.0.4/pyrolite-0.0.4.tar.gz

---
+++ packages/pyrolite/pyrolite-0.0.4.tar.gz/pyrolite-0.0.4/setup.py
@@ -0,0 +1,48 @@
+from setuptools import setup, find_packages
+import versioneer
+
+tests_require = ['pytest',
+                 'pytest-runner',
+                 'pytest-cov',
+                 'coverage',
+                 'coveralls']
+
+setup(name='pyrolite',
+      description="Tools for geochemical data analysis.",
+      long_description=open('README.md').read(),
+      version=versioneer.get_version(),
+      url='https://github.com/morgan.j.williams/pyrolite',
+      author='Morgan Williams',
+      author_email='morgan.williams@csiro.au',
+      classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: Science/Research',
+        'Natural Language :: English',
+        'Programming Language :: Python :: 3 :: Only',
+        'Topic :: Scientific/Engineering',
+        'Topic :: Software Development :: Libraries :: Python Modules',
+        ],
+      packages=find_packages(exclude=['test*']),
+      install_requires=['pathlib',
+                        'numpy',
+                        'scipy',
+                        'scikit-learn',
+                        'pandas',
+                        'matplotlib',
+                        'periodictable',
+                        'xlrd',
+                        'mpmath',
+                        'python-ternary'
+                        ],
+
+      extras_require={'dev': ['versioneer',
+                              'nbstripout',
+                              'nbdime',
+                              'twine'] + tests_require},
+
+      tests_require=tests_require,
+      test_suite="test",
+      package_data={'pyrolite': ['data/*']},
+      license='CSIRO Modifed MIT/BSD',
+      cmdclass=versioneer.get_cmdclass()
+)
