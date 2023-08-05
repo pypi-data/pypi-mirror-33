8ac79eed2fcb56a71a96cc76384816bbead54022
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:08:16 +0000

    Add package SMSGateway/0.3.0.dev0/SMSGateway-0.3.0.dev0.tar.gz

---
+++ packages/SMSGateway/SMSGateway-0.3.0.dev0.tar.gz/SMSGateway-0.3.0.dev0/smsgateway/sink_sms.py
@@ -0,0 +1,38 @@
+import datetime, os
+from smsgateway.config import *
+
+def send(type, text, _from, group=None):
+    lines = [
+        type,
+        f"From: {_from}"
+    ]
+    if group:
+        lines += [f"Group: {group}"]
+    lines += [
+        "",
+        text
+    ]
+    send_to(CONTROL_PHONES[0], '\n'.join(lines))
+
+def send_from_me(type, text, to):
+    lines = [
+        type,
+        f"To: {to}",
+        "",
+        text
+    ]
+    send_to(CONTROL_PHONES[0], '\n'.join(lines))
+
+def send_notif(text):
+    send_to(CONTROL_PHONES[0], text)
+
+def send_to(to, text):
+    print("Sending SMS:\n%s" % text)
+    header = '\n'.join([f"To: {to}", "Alphabet: UCS-2", "", ""])
+    d = datetime.datetime.now().strftime("%y-%m-%d_%H-%M-%S.%f")[:-3]
+    p = os.path.join(SMS_DIR, d)
+    print("Writing sms to %s" % p)
+    with open(p, 'wb') as f:
+        f.write(header.encode("UTF-8"))
+        f.write(text.encode("utf-16-be"))
+        f.flush()
