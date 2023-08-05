a791622418215427d1cf8351217479c8667f8d75
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:29:38 +0000

    Add package gym-super-mario-bros/1.1.0/gym_super_mario_bros-1.1.0.tar.gz

---
+++ packages/gym-super-mario-bros/gym_super_mario_bros-1.1.0.tar.gz/gym_super_mario_bros-1.1.0/setup.py
@@ -0,0 +1,51 @@
+from setuptools import setup, find_packages
+
+
+def README() -> str:
+    """Return the contents of the README file for this project."""
+    with open('README.md') as README_file:
+        return README_file.read()
+
+
+setup(
+    name='gym_super_mario_bros',
+    setup_requires=[
+        'very-good-setuptools-git-version'
+    ],
+    version_format='{tag}',
+    description='Super Mario Bros. for OpenAI Gym',
+    long_description=README(),
+    long_description_content_type='text/markdown',
+    keywords='OpenAI-Gym NES Super-Mario-Bros Reinforcement-Learning-Environment',
+    classifiers=[
+        'License :: OSI Approved :: MIT License',
+        'Development Status :: 2 - Pre-Alpha',
+        'Intended Audience :: Developers',
+        'Intended Audience :: Science/Research',
+        'Operating System :: MacOS :: MacOS X',
+        'Operating System :: POSIX :: Linux',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+        'Topic :: Games/Entertainment :: Side-Scrolling/Arcade Games',
+        'Topic :: Scientific/Engineering :: Artificial Intelligence',
+    ],
+    url='https://github.com/Kautenja/gym-super-mario-bros',
+    author='Christian Kauten',
+    author_email='kautencreations@gmail.com',
+    license='MIT',
+    packages=find_packages() ,
+    package_data = {
+        'gym_super_mario_bros': ['lua/*.lua', 'roms/*.nes']
+    },
+    zip_safe=False,
+    install_requires=[
+        'gym>=0.10.5',
+        'Pillow>=5.0.0',
+        'numpy>=1.14.2'
+    ],
+    entry_points={
+        'console_scripts': [
+            'gym_super_mario_bros = gym_super_mario_bros._cli:main',
+        ],
+    },
+)
