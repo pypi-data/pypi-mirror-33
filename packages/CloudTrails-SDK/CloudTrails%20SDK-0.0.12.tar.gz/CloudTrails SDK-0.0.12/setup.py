abc6cc8859fb3ad7940db17371f126e262ec434c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:31:16 +0000

    Add package CloudTrails-SDK/0.0.12/CloudTrails%20SDK-0.0.12.tar.gz

---
+++ packages/CloudTrails-SDK/CloudTrails%20SDK-0.0.12.tar.gz/CloudTrails SDK-0.0.12/setup.py
@@ -0,0 +1,27 @@
+import os
+
+import setuptools
+
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+
+VERSION = os.environ.get("TAG_VERSION")
+VERSION = VERSION[2:] if VERSION else "0.0.12"
+
+setuptools.setup(
+    name="CloudTrails SDK",
+    version=VERSION,
+    author="Yaisel Hurtado, Raydel Miranda",
+    author_email="hurta2yaisel@gmail.com, raydel.miranda.gomez@gmail.com",
+    description="SDK for CloudTrails DevOps management",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://bitbucket.org/elasbit/fc-cloudtrails-sdk-py",
+    packages=setuptools.find_packages(),
+    classifiers=(
+        "Programming Language :: Python :: 2",
+        "Operating System :: OS Independent",
+    ),
+)
