e635b1d9591bdb429a8773f6f7bb47085285aa30
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:23:44 +0000

    Add package py3x/0.0.1/py3x-0.0.1.tar.gz

---
+++ packages/py3x/py3x-0.0.1.tar.gz/py3x-0.0.1/setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup
+setup(
+    name='py3x',
+    version='0.0.1',
+    description='python3 extension',
+    url='https://github.com/uedak/py3x',
+    author='uedak',
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 3.6',
+    ],
+    packages=['py3x'],
+)
