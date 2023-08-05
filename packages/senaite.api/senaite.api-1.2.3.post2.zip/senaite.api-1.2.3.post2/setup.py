7b4e566ff35c768f9b72a5e904debcfda3d8b225
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:08:02 +0000

    Add package senaite.api/1.2.3.post2/senaite.api-1.2.3.post2.zip

---
+++ packages/senaite.api/senaite.api-1.2.3.post2.zip/senaite.api-1.2.3.post2/setup.py
@@ -0,0 +1,68 @@
+# -*- coding: utf-8 -*-
+
+from setuptools import setup, find_packages
+
+version = "1.2.3-r2"
+
+with open("docs/About.rst", "r") as fh:
+    long_description = fh.read()
+
+with open("src/senaite/api/docs/API.rst", "r") as fh:
+    long_description += "\n\n"
+    long_description += fh.read()
+
+with open("src/senaite/api/docs/API_analysis.rst", "r") as fh:
+    long_description += "\n\n"
+    long_description += fh.read()
+
+with open("docs/Changelog.rst", "r") as fh:
+    long_description += "\n\n"
+    long_description += "Changelog\n"
+    long_description += "=========\n"
+    long_description += fh.read()
+
+setup(
+    name="senaite.api",
+    version=version,
+    description="SENAITE API",
+    long_description=long_description,
+    # Get more strings from
+    # http://pypi.python.org/pypi?:action=list_classifiers
+    classifiers=[
+        "Programming Language :: Python",
+        "Framework :: Plone",
+        "Framework :: Zope2",
+    ],
+    keywords="",
+    author="SENAITE Foundation",
+    author_email="hello@senaite.com",
+    url="https://github.com/senaite/senaite.api",
+    license="GPLv2",
+    packages=find_packages("src", exclude=["ez_setup"]),
+    package_dir={"": "src"},
+    namespace_packages=["senaite"],
+    include_package_data=True,
+    zip_safe=False,
+    install_requires=[
+        "setuptools",
+        "plone.api",
+        "senaite.core",
+    ],
+    extras_require={
+        "test": [
+            "Products.PloneTestCase",
+            "Products.SecureMailHost",
+            "plone.app.robotframework",
+            "plone.app.testing",
+            "robotframework-debuglibrary",
+            "robotframework-selenium2library",
+            "robotsuite",
+            "unittest2",
+        ]
+    },
+    entry_points="""
+      # -*- Entry points: -*-
+      [z3c.autoinclude.plugin]
+      target = plone
+      """,
+)
