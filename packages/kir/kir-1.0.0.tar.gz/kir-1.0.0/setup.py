b5d8116f50c4515f6f8dabf50ed17f4ae72c80fa
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:07:08 +0000

    Add package kir/1.0.0/kir-1.0.0.tar.gz

---
+++ packages/kir/kir-1.0.0.tar.gz/kir-1.0.0/setup.py
@@ -0,0 +1,17 @@
+from setuptools import setup, find_packages
+
+readme = "KIR"
+
+setup(
+    name='kir',
+    version='1.0.0',
+    description='Kir tu kosse khare Taki',
+    long_description=readme,
+    author='Johnny Sins',
+    author_email='jhnnysins@gmail.com',
+    url='https://github.com/jhnnysins/kir.git',
+    license='MIT',
+    packages=find_packages(),
+    install_requires=[
+    ]
+)
