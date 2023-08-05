220bd91e7586a4b66fac41f83ce4d8ca77024a13
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:15:21 +0000

    Add package downloader-py3/1.0.2/downloader-py3-1.0.2.tar.gz

---
+++ packages/downloader-py3/downloader-py3-1.0.2.tar.gz/downloader-py3-1.0.2/setup.py
@@ -0,0 +1,31 @@
+from distutils.core import setup
+
+from setuptools import find_packages
+
+setup(
+    name='downloader-py3',
+    version='1.0.2',
+    keywords='downloader dwonloader-py3',
+    author='Bohan Zhang',
+    author_email='zhangbohan.dell@gmail.com',
+    url='https://github.com/ice1995/downloader-py3',
+    project_urls={'Source': 'https://github.com/gtzampanakis/downloader'},
+    py_modules=['downloader', 'memoize'],
+    packages=find_packages(exclude=['contrib', 'docs', 'tests*', 'test*']),
+    license='MIT',
+    platforms='Any',
+    python_requires='>=3',
+    requires=[
+        'lxml (> 3.2.1)',
+    ],
+    description='Download URLs using a compressed disk cache and a random throttling interval.',
+    long_description=open('README').read(),
+    classifiers=['Programming Language :: Python',
+                 'License :: OSI Approved :: MIT License',
+                 'Operating System :: OS Independent',
+                 'Development Status :: 4 - Beta',
+                 'Intended Audience :: Developers',
+                 'Topic :: Internet :: WWW/HTTP',
+                 'Topic :: Internet :: WWW/HTTP :: Indexing/Search',
+                 ],
+)
