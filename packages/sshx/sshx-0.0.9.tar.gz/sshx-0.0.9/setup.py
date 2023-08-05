883590e18736119562ae56cf4d20a4b96b6a6bad
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:25:01 +0000

    Add package sshx/0.0.9/sshx-0.0.9.tar.gz

---
+++ packages/sshx/sshx-0.0.9.tar.gz/sshx-0.0.9/setup.py
@@ -0,0 +1,58 @@
+from __future__ import unicode_literals
+
+import os
+import sys
+import platform
+import unittest
+import setuptools
+
+# test requires
+tests_require = [
+    'mock',
+    'coverage',
+]
+
+
+# install requires
+_PYHOOK_URL = 'https://github.com/WqyJh/python-wheels/raw/master/pyHook/pyHook-1.5.1-{python}-{python}m-{platform}.whl'
+pyhook_url = _PYHOOK_URL.format(
+    python='cp%s%s' % platform.python_version_tuple()[0:2],
+    platform=sys.platform
+)
+
+install_requires = [
+    'paramiko',
+    'itsdangerous',
+    'pexpect;platform_system != "Windows"',
+    'colorama;platform_system == "Windows"',
+    'pywin32;platform_system == "Windows"',
+    'pyuserinput;platform_system == "Windows"',
+]
+
+dependency_links = []
+
+if os.name == 'nt':
+    dependency_links.append(pyhook_url)
+
+# entry points
+entry_points = {
+    'console_scripts': [
+        'sshx = sshx.sshx:main'
+    ]
+}
+
+
+# test suite
+def load_test_suite():
+    test_loader = unittest.TestLoader()
+    test_suite = test_loader.discover('sshx/tests')
+    return test_suite
+
+
+setuptools.setup(
+    tests_require=tests_require,
+    install_requires=install_requires,
+    dependency_links=dependency_links,
+    test_suite="setup.load_test_suite",
+    entry_points=entry_points
+)
