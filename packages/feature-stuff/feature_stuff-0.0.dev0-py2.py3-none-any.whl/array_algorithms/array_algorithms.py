a145457450e7dab851a7bc76fbe2440820358c48
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:04:46 +0000

    Add package feature-stuff/0.0.dev0/feature_stuff-0.0.dev0-py2.py3-none-any.whl

---
+++ packages/feature-stuff/feature_stuff-0.0.dev0-py2.py3-none-any.whl/array_algorithms/array_algorithms.py
@@ -0,0 +1,11 @@
+
+'''
+
+Input:  takes an list of lists of variable length
+Output: returns a set of it by removing duplicate rows
+
+'''
+
+def removeDuplicateRows(ndarr):
+
+    return [list(y) for y in set( [tuple(sorted(x)) for x in ndarr])]
