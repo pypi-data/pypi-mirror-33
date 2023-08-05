1d4ccfe10c2efed10cc651ef8b9f841d2f9bd24b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 23:08:49 +0000

    Add package revelation/0.3.0/revelation-0.3.0-py2.py3-none-any.whl

---
+++ packages/revelation/revelation-0.3.0-py2.py3-none-any.whl/revelation/__init__.py
@@ -0,0 +1,9 @@
+# -*- coding: utf-8 -*-
+"""Revelation root module with package info"""
+
+from .app import Revelation
+
+__author__ = "Humberto Rocha"
+__email__ = "humrochagf@gmail.com"
+__version__ = "0.3.0"
+__all__ = ["Revelation"]
---
+++ packages/revelation/revelation-0.3.0-py2.py3-none-any.whl/tests/test_app.py
@@ -0,0 +1,36 @@
+# -*- coding: utf-8 -*-
+
+import os
+import tempfile
+import unittest
+
+from revelation import Revelation
+
+
+class RevelationTestCase(unittest.TestCase):
+    def setUp(self):
+        self.media = tempfile.mkdtemp()
+        _, self.slide = tempfile.mkstemp(".md")
+
+        with open(self.slide, "w") as file:
+            file = file.write("# Pag1\n---\n# Pag2")
+
+        self.app = Revelation(self.slide, media=self.media)
+
+    def test_parse_shared_data_empty(self):
+        shared_data_config = self.app.parse_shared_data(None)
+
+        self.assertDictEqual(shared_data_config, {})
+
+    def test_parse_shared_data(self):
+        shared_data_config = self.app.parse_shared_data(self.media)
+
+        self.assertDictEqual(
+            shared_data_config,
+            {"/{}".format(os.path.basename(self.media)): self.media},
+        )
+
+    def test_load_slides(self):
+        slides = self.app.load_slides(self.slide, "---")
+
+        self.assertListEqual(slides, ["# Pag1\n", "\n# Pag2"])
