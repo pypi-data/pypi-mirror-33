07cd90d4ba0e879362b5f68f080cba078b6b2fd7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:04:37 +0000

    Add package nanohttp/0.28.0/nanohttp-0.28.0.tar.gz

---
+++ packages/nanohttp/nanohttp-0.28.0.tar.gz/nanohttp-0.28.0/nanohttp/__init__.py
@@ -0,0 +1,15 @@
+
+
+from .exceptions import HttpStatus, HttpBadRequest, HttpUnauthorized, HttpForbidden, HttpNotFound, \
+    HttpMethodNotAllowed, HttpConflict, HttpGone, HttpRedirect, HttpMovedPermanently, HttpFound, \
+    HttpInternalServerError, HttpNotModified, HttpBadGatewayError
+from .controllers import Controller, RestController, Static, RegexRouteController
+from .decorators import action, html, json, xml, binary, text, ifmatch, etag
+from .helpers import quickstart, LazyAttribute
+from .cli import main
+from .contexts import context, ContextIsNotInitializedError
+from .configuration import settings, configure
+from .application import Application
+from .validation import validate
+
+__version__ = '0.28.0'
---
+++ packages/nanohttp/nanohttp-0.28.0.tar.gz/nanohttp-0.28.0/nanohttp/exceptions.py
@@ -0,0 +1,95 @@
+import ujson
+import traceback
+
+from .contexts import context
+from .configuration import settings
+
+
+class HttpStatus(Exception):
+    status = None
+
+    def __init__(self, status=None):
+        if status is not None:
+            self.status = status
+
+        super().__init__(self.status)
+
+    def render(self):
+        stack_trace = traceback.format_exc()
+        if context.response_content_type == 'application/json':
+            return ujson.encode(
+                dict(
+                    stackTrace=stack_trace) if settings.debug else dict()
+            )
+        else:
+            context.response_encoding = 'utf-8'
+            context.response_content_type = 'text/plain'
+            return stack_trace if settings.debug else ''
+
+
+class HttpBadRequest(HttpStatus):
+    status = '400 Bad Request'
+
+
+class HttpUnauthorized(HttpStatus):
+    status = '401 Unauthorized'
+
+
+class HttpForbidden(HttpStatus):
+    status = '403 Forbidden'
+
+
+class HttpNotFound(HttpStatus):
+    status = '404 Not Found'
+
+
+class HttpMethodNotAllowed(HttpStatus):
+    status = '405 Method Not Allowed'
+
+
+class HttpConflict(HttpStatus):
+    status = '409 Conflict'
+
+
+class HttpGone(HttpStatus):
+    status = '410 Gone'
+
+
+class HttpPreconditionFailed(HttpStatus):
+    status = '412 Precondition Failed'
+
+
+class HttpRedirect(HttpStatus):
+    """
+    This is an abstract class for all redirects.
+    """
+
+    def __init__(self, location, *args, **kw):
+        context.response_headers.add_header('Location', location)
+        super().__init__(*args, **kw)
+
+
+class HttpMovedPermanently(HttpRedirect):
+    status = '301 Moved Permanently'
+
+
+class HttpFound(HttpRedirect):
+    status = '302 Found'
+
+
+class HttpNotModified(HttpStatus):
+    status = '304 Not Modified'
+
+
+class HttpInternalServerError(HttpStatus):
+    status = '500 Internal Server Error'
+
+    @property
+    def info(self):
+        if settings.debug:
+            return traceback.format_exc()
+        return 'Server got itself in trouble'
+
+
+class HttpBadGatewayError(HttpStatus):
+    status = '502 Bad Gateway'
