da74bcfa75c13d260fc66657f2bda925da7c14b4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:09:58 +0000

    Add package ie_mbdbl2017_C_yahoo_ks_datareader/0.1/ie_mbdbl2017_C_yahoo_ks_datareader-0.1.tar.gz

---
+++ packages/ie_mbdbl2017_C_yahoo_ks_datareader/ie_mbdbl2017_C_yahoo_ks_datareader-0.1.tar.gz/ie_mbdbl2017_C_yahoo_ks_datareader-0.1/ie_mbdbl2017_C_yahoo_ks_datareader/__init__.py
@@ -0,0 +1,3 @@
+def  datareader_doc():
+    """ function to return the string 'Group C first package that anyone can install it! How cool!!!' """
+    return("Group C first package that anyone can install it! How cool!!!")
---
+++ packages/ie_mbdbl2017_C_yahoo_ks_datareader/ie_mbdbl2017_C_yahoo_ks_datareader-0.1.tar.gz/ie_mbdbl2017_C_yahoo_ks_datareader-0.1/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='ie_mbdbl2017_C_yahoo_ks_datareader',
+      version='0.1',  # Development release
+      description='Python Packaging Starting Kit - Simple Structure to creater your own package - for training purposes!',
+      url='https://github.com/dancual/pythonpackagesample',
+      author='Daniel Serrano',
+      author_email='dserrano1983@gmail.com',
+      license='MIT',
+          packages=['ie_mbdbl2017_C_yahoo_ks_datareader'],
+      zip_safe=False)
