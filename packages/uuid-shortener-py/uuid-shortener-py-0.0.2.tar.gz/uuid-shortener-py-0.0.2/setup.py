d68dd9f61e3ab8e6f2b3dc3339f97c31472d4e93
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:28:45 +0000

    Add package uuid-shortener-py/0.0.2/uuid-shortener-py-0.0.2.tar.gz

---
+++ packages/uuid-shortener-py/uuid-shortener-py-0.0.2.tar.gz/uuid-shortener-py-0.0.2/setup.py
@@ -0,0 +1,31 @@
+from setuptools import setup, find_packages
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+REQUIRES = ['pybaseconv==0.*']
+
+setup(
+    name="uuid-shortener-py",
+    version="0.0.2",
+    author="Samira El Aabidi",
+    url='https://github.com/El-Sam/uuid_shortener',
+    author_email="sam.elaabidi@gmail.com",
+    keywords=["Python", "UUID", "UUID Shortener", "Hex base", "Short URL"],
+    description="Shorten a uuid into a URL friendly format.",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    install_requires=REQUIRES,
+    packages=find_packages(),
+    classifiers=(
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+        'Development Status :: 5 - Production/Stable',
+        'Intended Audience :: Developers',
+        'Intended Audience :: Information Technology',
+        'License :: OSI Approved :: MIT License',
+        'Natural Language :: English',
+        'Operating System :: OS Independent',
+        'Topic :: Software Development :: Libraries :: Python Modules'
+    ),
+)
