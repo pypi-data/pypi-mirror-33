7467cb2e9fd5e9dcc1dcfe75f3bfb1a43824af4c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:29:29 +0000

    Add package database-sanitizer/0.4.0/database-sanitizer-0.4.0.tar.gz

---
+++ packages/database-sanitizer/database-sanitizer-0.4.0.tar.gz/database-sanitizer-0.4.0/database_sanitizer/__main__.py
@@ -0,0 +1,65 @@
+# -*- coding: utf-8 -*-
+
+from __future__ import unicode_literals
+
+import argparse
+import os
+import sys
+
+from .config import Configuration
+from .dump import run
+
+
+def main(args=None):
+    if args is None:
+        args = sys.argv[1:]
+
+    parser = argparse.ArgumentParser(
+        description="Sanitizes contents of databases.",
+    )
+    parser.add_argument(
+        "--config",
+        "-c",
+        type=str,
+        dest="config",
+        help="Path to the sanitizer configuration file.",
+    )
+    parser.add_argument(
+        "--output",
+        "-o",
+        type=str,
+        dest="output",
+        help=(
+            "Path to the file where the sanitized database will be written into. "
+            "If omitted, standard output will be used instead."
+        ),
+    )
+    parser.add_argument(
+        "url",
+        help="Database URL to which to connect into and sanitize contents.",
+    )
+
+    args = parser.parse_args(args=args)
+    output = sys.stdout
+    config = None
+
+    if args.config:
+        conf_dir = os.path.realpath(os.path.dirname(args.config))
+        sys.path.insert(0, conf_dir)
+        config = Configuration.from_file(args.config)
+    if args.output:
+        output = open(args.output, "w")
+
+    try:
+        run(
+            url=args.url,
+            output=output,
+            config=config,
+        )
+    finally:
+        if args.output:
+            output.close()
+
+
+if __name__ == "__main__":
+    main()
