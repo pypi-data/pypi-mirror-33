867042a1e758dde71dc626a31bf43828e5ce1c6f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:06:25 +0000

    Add package pp-l3-distributions/0.1/pp_l3_distributions-0.1.tar.gz

---
+++ packages/pp-l3-distributions/pp_l3_distributions-0.1.tar.gz/pp_l3_distributions-0.1/setup.py
@@ -0,0 +1,7 @@
+from setuptools import setup
+
+setup(name='pp_l3_distributions',
+      version='0.1',
+      description='Gaussian distributions',
+      packages=['pp_l3_distributions'],
+      zip_safe=False)
