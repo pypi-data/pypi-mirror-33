a13062284aa2fb0612f2ae5dac6156006a631693
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:25:45 +0000

    Add package pylgnetcast-homeassistant/0.2.0.dev0/pylgnetcast-homeassistant-0.2.0.dev0.tar.gz

---
+++ packages/pylgnetcast-homeassistant/pylgnetcast-homeassistant-0.2.0.dev0.tar.gz/pylgnetcast-homeassistant-0.2.0.dev0/setup.py
@@ -0,0 +1,12 @@
+
+from setuptools import setup
+
+setup(name='pylgnetcast-homeassistant',
+            version='0.2.0.dev0',
+            description='Client for the LG Smart TV running NetCast 3 or 4.',
+            long_description='This package is created from https://github.com/wokar/pylgnetcast/archive/v0.2.0.zip#pylgnetcast==0.2.0 No updates are expected.',
+            url='https://github.com/wokar/pylgnetcast',
+            license='MIT',
+            packages=['pylgnetcast'],
+            install_requires=[],
+            zip_safe=False)
