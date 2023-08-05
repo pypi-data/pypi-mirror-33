cd8aee70ff2c925693780e4a94322028c2ba49e6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:29:22 +0000

    Add package odk-ppp/1.1.0/odk_ppp-1.1.0.tar.gz

---
+++ packages/odk-ppp/odk_ppp-1.1.0.tar.gz/odk_ppp-1.1.0/setup.py
@@ -0,0 +1,39 @@
+"""Setup for Pypi"""
+from setuptools import setup, find_packages
+
+
+packages = find_packages(exclude=['test'])
+packages.append('ppp.test')
+
+setup(
+    name='odk_ppp',  # ppp was already taken
+    version='1.1.0',
+    author='Joe Flack',
+    author_email='jflack@jhu.edu',
+    url='http://www.pma2020.org',
+    packages=packages,
+    package_dir={
+        'ppp': 'ppp',
+        'ppp.test': 'test'
+    },
+    package_data={
+        'ppp': [
+            'templates/*.html',
+            'templates/content/*.html',
+            'templates/content/group/*.html',
+            'templates/content/prompt/*.html',
+            'templates/content/prompt/inputs/*.html',
+            'templates/content/repeat/*.html',
+            'templates/content/table/*.html'
+        ],
+        'ppp.test': ['files/*.xlsx']
+    },
+
+    license='LICENSE.txt',
+    description='Converts ODK XlsForm Excel files into readable formats.',
+    long_description=open('README.md').read(),
+    install_requires=[
+        'xlrd>=0.9.3',
+        'Jinja2>=2.9.6'
+    ]
+)
