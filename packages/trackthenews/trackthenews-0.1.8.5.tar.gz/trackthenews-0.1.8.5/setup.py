060e1c3fa2457b6862c4e6b4460b70181ee15e86
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:24:04 +0000

    Add package trackthenews/0.1.8.5/trackthenews-0.1.8.5.tar.gz

---
+++ packages/trackthenews/trackthenews-0.1.8.5.tar.gz/trackthenews-0.1.8.5/setup.py
@@ -0,0 +1,35 @@
+from setuptools import setup, find_packages
+
+with open('requirements.txt') as f:
+    reqs = f.read().split()
+
+with open('README.rst') as f:
+    readme = f.read()
+
+with open('LICENSE') as f:
+    license = f.read()
+
+setup(
+    name='trackthenews',
+    version='0.1.8.5',
+    description='Monitor RSS feeds for keywords and act on matching results. A special project of the Freedom of the Press Foundation.',
+    long_description=readme,
+    install_requires=reqs,
+    author='Parker Higgins',
+    author_email='parker@freedom.press',
+    url='https://github.com/freedomofpress/trackthenews',
+    entry_points={
+        'console_scripts': ['trackthenews=trackthenews:main']
+    },
+    package_data={
+        'trackthenews': ['fonts/*']
+    },
+    include_package_data=True,
+    license=license,
+    classifiers=[
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.6'],
+    packages=find_packages(exclude=('ttnconfig',))
+)
