f659b5dd9863e2b315674fc4b05c74d58c5bb0e7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:13:16 +0000

    Add package NtDocutils/1.0.0/NtDocutils-1.0.0-py3-none-any.whl

---
+++ packages/NtDocutils/NtDocutils-1.0.0-py3-none-any.whl/ntdocutils/__init__.py
@@ -0,0 +1,14 @@
+# NtDocutils https://nt.web.ve/en/projects/ntdocutils
+# Copyright (c) 2017 Miguel Angel Rivera Notararigo
+
+"""NtDocutils."""
+
+__version__ = "1.0.0"
+__docformat__ = "reStructuredText"
+
+COPYRIGHT = """NtDocutils v{} https://nt.web.ve/en/projects/ntdocutils
+Copyright (c) 2017 Miguel Angel Rivera Notararigo
+Licensed under The MIT License""".format(__version__)
+
+DESCRIPTION = """NtDocutils is a theme manager for Docutils."""
+
---
+++ packages/NtDocutils/NtDocutils-1.0.0-py3-none-any.whl/ntdocutils/exceptions.py
@@ -0,0 +1,33 @@
+# NtDocutils https://nt.web.ve/en/projects/ntdocutils
+# Copyright (c) 2017 Miguel Angel Rivera Notararigo
+
+"""
+NtDocutils exceptions.
+
+Provides:
+
+* ``OfflineUnsupported``: theme doesn't support offline mode.
+"""
+
+
+class OfflineUnsupported(Exception):
+    """
+    Creates an exception to raise when theme doesn't support offline mode.
+    """
+
+    def __init__(self, theme):
+        """
+        ``OfflineUnsupported("<theme>")``
+
+        ``theme`` (string)
+          Theme name.
+
+        Example
+        =======
+
+        .. code:: python
+
+            raise OfflineUnsupported("mdl")
+        """
+        message = "{theme} theme doesn't support offline mode"
+        Exception.__init__(self, message.format(theme=theme))
