444ef93e5efcd0cf02ed637831421560e8f20419
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:24:44 +0000

    Add package QGDT-cpu/0.3.0/QGDT_cpu-0.3.0-py2.py3-none-any.whl

---
+++ packages/QGDT-cpu/QGDT_cpu-0.3.0-py2.py3-none-any.whl/QGDT/templates_x/__init__.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+"""
+努力不需要理由，如果需要，就是为了不需要的理由。
+"""
+
+__title__ = 'QGDT-cpu'
+__author__ = 'Ex_treme'
+__license__ = 'MIT'
+__copyright__ = 'Copyright 2018, Ex_treme'
+
+import os
+import sys
+
+sys.path.append(os.path.dirname(os.getcwd()))
+
+from QGDT.templates_x import templates_x1,templates_x2,templates_x3
