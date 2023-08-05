99e5ede7fca3452ae0d2034f2f56b74b9ffcb2ee
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:06:16 +0000

    Add package food-trucks-boston/0.21/food_trucks_boston-0.21.tar.gz

---
+++ packages/food-trucks-boston/food_trucks_boston-0.21.tar.gz/food_trucks_boston-0.21/setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup
+
+setup(name='food_trucks_boston',
+      version='0.21',
+      description='Know where the food trucks are in Boston, from the command line.',
+      url='https://github.com/DennisMelamed/food_trucks_boston.git',
+      author='dennis.melamed',
+      author_email='dennis@dennismelamed.me',
+      packages=['food_trucks_boston'],
+      scripts=['scripts/test.py'],
+      install_requires=[
+          'jsonpath_ng', 'click', 'esridump', 'fuzzywuzzy', 'numpy', 'prettytable',
+          ],
+      zip_safe=False)
