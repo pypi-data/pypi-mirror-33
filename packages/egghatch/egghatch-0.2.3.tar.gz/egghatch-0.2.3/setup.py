8b54b175c5a4970d3b24ea431ba0b1a6d84339bf
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:14:44 +0000

    Add package egghatch/0.2.3/egghatch-0.2.3.tar.gz

---
+++ packages/egghatch/egghatch-0.2.3.tar.gz/egghatch-0.2.3/setup.py
@@ -0,0 +1,51 @@
+# Copyright (C) 2017-2018 Cuckoo Foundation.
+# This file is part of Cuckoo Sandbox - https://cuckoosandbox.org/.
+# See the file 'docs/LICENSE' for copying permission.
+
+import setuptools
+
+setuptools.setup(
+    name="egghatch",
+    version="0.2.3",
+    author="Jurriaan Bremer",
+    author_email="jbr@cuckoo.sh",
+    packages=[
+        "egghatch",
+    ],
+    classifiers=[
+        "Development Status :: 4 - Beta",
+        "Environment :: Console",
+        "Environment :: Web Environment",
+        "Framework :: Pytest",
+        "Intended Audience :: Information Technology",
+        "Intended Audience :: Science/Research",
+        "Natural Language :: English",
+        "License :: OSI Approved :: GNU General Public License v3 (GPLv3)",
+        "Operating System :: POSIX :: Linux",
+        "Programming Language :: Python :: 2.7",
+        "Topic :: Security",
+    ],
+    url="https://cuckoosandbox.org/",
+    license="GPLv3",
+    description="Cuckoo Sandbox Shellcode Identification & Formatting",
+    long_description=open("README.rst", "rb").read(),
+    include_package_data=True,
+    entry_points={
+        "console_scripts": [
+            "egghatch = egghatch.main:main",
+        ],
+    },
+    install_requires=[
+    ],
+    extras_require={
+        ":sys_platform == 'win32'": [
+            "capstone-windows==3.0.4",
+        ],
+        ":sys_platform == 'darwin'": [
+            "capstone==3.0.5rc2",
+        ],
+        ":sys_platform == 'linux2'": [
+            "capstone==3.0.5rc2",
+        ],
+    },
+)
