a788792eef480cc4f68e23061b532883e9020fd0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 23:06:36 +0000

    Add package clashroyale/3.4.5/clashroyale-3.4.5.tar.gz

---
+++ packages/clashroyale/clashroyale-3.4.5.tar.gz/clashroyale-3.4.5/setup.py
@@ -0,0 +1,28 @@
+from setuptools import setup
+
+version = '3.4.5'
+
+with open('README.rst') as f:
+    long_description = f.read()
+
+setup(
+    name='clashroyale',
+    packages=['clashroyale'],
+    version=version,
+    description='An (a)sync wrapper for royaleapi.com',
+    long_description=long_description,
+    long_description_content_type='text/x-rst',
+    license='MIT',
+    url='https://github.com/cgrok/clashroyale',
+    keywords=['clashroyale', 'wrapper', 'cr', 'royaleapi'],
+    install_requires=['aiohttp==3.3.2', 'python-box==3.1.1', 'requests==2.18.4'],
+    classifiers=[
+        'Development Status :: 5 - Production/Stable',
+        'Intended Audience :: Developers',
+        'Topic :: Games/Entertainment :: Real Time Strategy',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+    ],
+    python_requires='>=3.5'
+)
