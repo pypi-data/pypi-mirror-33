52fcacc6d1fb22affebff41abdfd219443d98ec8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:09:21 +0000

    Add package tdl/4.5.1/tdl-4.5.1.tar.gz

---
+++ packages/tdl/tdl-4.5.1.tar.gz/tdl-4.5.1/tcod/cffi.h
@@ -0,0 +1,16 @@
+
+/* This header is the entry point for the cffi parser.
+   Anything included here will be accessible from tcod.libtcod.lib */
+
+#ifndef TDL_NO_SDL2_EXPORTS
+#include <SDL.h>
+#endif
+
+#include <libtcod.h>
+#include <libtcod_int.h>
+#include <wrappers.h>
+
+#include "noise.h"
+#include "path.h"
+#include "random.h"
+#include "tdl.h"
