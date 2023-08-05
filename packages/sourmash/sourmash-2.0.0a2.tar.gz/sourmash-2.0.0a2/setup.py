16a1975ec0699f81e94258f53d9e76bac638a8de
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:29:48 +0000

    Add package sourmash/2.0.0a2/sourmash-2.0.0a2.tar.gz

---
+++ packages/sourmash/sourmash-2.0.0a2.tar.gz/sourmash-2.0.0a2/setup.py
@@ -0,0 +1,83 @@
+from __future__ import print_function
+import sys
+from setuptools import setup
+from setuptools import Extension
+import os
+
+# retrieve VERSION from sourmash_lib/VERSION.
+thisdir = os.path.dirname(__file__)
+version_file = open(os.path.join(thisdir, 'sourmash_lib', 'VERSION'))
+VERSION = version_file.read().strip()
+
+EXTRA_COMPILE_ARGS = ['-std=c++11', '-pedantic']
+EXTRA_LINK_ARGS=[]
+
+CLASSIFIERS = [
+    "Environment :: Console",
+    "Environment :: MacOS X",
+    "Intended Audience :: Science/Research",
+    "License :: OSI Approved :: BSD License",
+    "Natural Language :: English",
+    "Operating System :: POSIX :: Linux",
+    "Operating System :: MacOS :: MacOS X",
+    "Programming Language :: C++",
+    "Programming Language :: Python :: 2.7",
+    "Programming Language :: Python :: 3.5",
+    "Programming Language :: Python :: 3.6",
+    "Topic :: Scientific/Engineering :: Bio-Informatics",
+]
+
+CLASSIFIERS.append("Development Status :: 5 - Production/Stable")
+
+if sys.platform == 'darwin':              # Mac OS X?
+    # force 64bit only builds
+    EXTRA_COMPILE_ARGS.extend(['-arch', 'x86_64', '-mmacosx-version-min=10.7',
+                               '-stdlib=libc++'])
+
+else:                                     # ...likely Linux
+   if os.environ.get('SOURMASH_COVERAGE'):
+      print('Turning on coverage analysis.')
+      EXTRA_COMPILE_ARGS.extend(['-g', '--coverage', '-lgcov'])
+      EXTRA_LINK_ARGS.extend(['--coverage', '-lgcov'])
+   else:
+      EXTRA_COMPILE_ARGS.append('-O3')
+
+SETUP_METADATA = \
+               {
+    "name": "sourmash",
+    "version": VERSION,
+    "description": "tools for comparing DNA sequences with MinHash sketches",
+    "url": "https://github.com/dib-lab/sourmash",
+    "author": "C. Titus Brown",
+    "author_email": "titus@idyll.org",
+    "license": "BSD 3-clause",
+    "packages": ["sourmash_lib"],
+    "entry_points": {'console_scripts': [
+        'sourmash = sourmash_lib.__main__:main'
+        ]
+    },
+    "ext_modules": [Extension("sourmash_lib._minhash",
+                               sources=["sourmash_lib/_minhash.pyx",
+                                        "third-party/smhasher/MurmurHash3.cc"],
+                               depends=["sourmash_lib/kmer_min_hash.hh"],
+                               include_dirs=["./sourmash_lib",
+                                             "./third-party/smhasher/"],
+                               language="c++",
+                               extra_compile_args=EXTRA_COMPILE_ARGS,
+                               extra_link_args=EXTRA_LINK_ARGS)],
+    "install_requires": ["screed>=0.9", "ijson", "khmer>2.0<3.0"],
+    "setup_requires": ['Cython>=0.25.2', "setuptools>=18.0"],
+    "extras_require": {
+        'test' : ['pytest', 'pytest-cov', 'numpy', 'matplotlib', 'scipy'],
+        'demo' : ['jupyter', 'jupyter_client', 'ipython'],
+        'doc' : ['sphinx'],
+        },
+    "include_package_data": True,
+    "package_data": {
+        "sourmash_lib": ['*.pxd']
+    },
+    "classifiers": CLASSIFIERS
+    }
+
+setup(**SETUP_METADATA)
+
