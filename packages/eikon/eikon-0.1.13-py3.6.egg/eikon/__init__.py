ea9091bcdb33ab94a283ed6168e780cbfc93ea22
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:15:32 +0000

    Add package eikon/0.1.13/eikon-0.1.13-py3.6.egg

---
+++ packages/eikon/eikon-0.1.13-py3.6.egg/eikon/__init__.py
@@ -0,0 +1,26 @@
+# coding: utf-8
+__version__ = '0.1.13'
+
+"""
+    eikon is a Python library to access Thomson Reuters Data with Python.
+    It's usage requires:
+        - An Thomson Reuters Eikon login
+        - The Eikon Scripting Proxy
+
+"""
+
+from .Profile import set_app_key, get_app_key,\
+    set_timeout, get_timeout, \
+    set_port_number, get_port_number,\
+    set_log_level, set_log_path,\
+    set_app_id, get_app_id
+from .symbology import get_symbology
+from .json_requests import send_json_request
+from .news_request import get_news_headlines, get_news_story
+from .time_series import get_timeseries
+from .data_grid import get_data, TR_Field
+from .eikonError import EikonError
+
+
+
+
