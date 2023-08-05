e1bf2e28eb1c822dc7eea6a8d739c124c8fa1ebc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:31:05 +0000

    Add package molten/0.0.3/molten-0.0.3.tar.gz

---
+++ packages/molten/molten-0.0.3.tar.gz/molten-0.0.3/setup.py
@@ -0,0 +1,28 @@
+# -*- coding: utf-8 -*-
+from distutils.core import setup
+
+packages = \
+['molten', 'molten.http', 'molten.testing', 'molten.validation']
+
+package_data = \
+{'': ['*'], 'molten': ['contrib/*']}
+
+install_requires = \
+['typing-extensions>=3.6,<4.0']
+
+setup_kwargs = {
+    'name': 'molten',
+    'version': '0.0.3',
+    'description': 'An experimental web framework for Python 3.6+.',
+    'long_description': None,
+    'author': 'Bogdan Popa',
+    'author_email': 'bogdan@cleartype.io',
+    'url': None,
+    'packages': packages,
+    'package_data': package_data,
+    'install_requires': install_requires,
+    'python_requires': '>=3.6,<4.0',
+}
+
+
+setup(**setup_kwargs)
