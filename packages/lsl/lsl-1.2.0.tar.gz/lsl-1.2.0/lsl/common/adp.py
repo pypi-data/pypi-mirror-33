809bc676e4206e068bac9e445ac8470f422f0412
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:06:23 +0000

    Add package lsl/1.2.0/lsl-1.2.0.tar.gz

---
+++ packages/lsl/lsl-1.2.0.tar.gz/lsl-1.2.0/lsl/common/adp.py
@@ -0,0 +1,102 @@
+# -*- coding: utf-8 -*-
+
+"""
+Module that contains common values found in the ADP ICD.  The values 
+are:
+  * f_S - Sampling rate in samples per second
+  * f_C - Width of each correlator frequency channel
+  * T - Slot duration in seconds
+  * T_2 - Sub-slot duration
+
+Also included are two functions to convert between frequencies and ADP tuning 
+words.
+"""
+
+__version__ = '0.1'
+__revision__ = '$Rev: 2138 $'
+__all__ = ['fS', 'fC', 'T', 'T2', 'N_MAX', 'freq2word', 'word2freq', 'delaytoDPD', 'DPDtodelay', 
+		 'gaintoDPG', 'DPGtogain', '__version__', '__revision__', '__all__']
+
+fS = 196.0e6	# Hz
+fC = 25e3		# Hz
+T = 1.0		# seconds
+T2 = 0.010	# seconds
+N_MAX = 8192	# bytes
+
+
+def freq2word(freq):
+	"""
+	Given a frequency in Hz, convert it to the closest DP tuning word.
+	"""
+	
+	return int(round(freq*2**32 / fS))
+
+
+def word2freq(word):
+	"""
+	Given a DP tuning word, convert it to a frequncy in Hz.
+	"""
+	
+	return word*fS / 2**32
+
+
+def delaytoDPD(delay):
+	"""Given a delay in ns, convert it to a course and fine portion and into the 
+	final format expected by ADP (big endian 16.12 unsigned integer)."""
+	
+	# Convert the delay to a combination of FIFO delays (~5.1 ns) and 
+	# FIR delays (~0.3 ns)
+	sample = int(round(delay * fS * 16 / 1e9))
+	course = sample // 16
+	fine   = sample % 16
+	
+	# Combine into one value
+	combined = (course << 4) | fine
+	
+	# Convert to big-endian
+	combined = ((combined & 0xFF) << 8) | ((combined >> 8) & 0xFF)
+	
+	return combined
+
+
+def DPDtodelay(combined):
+	"""Given a delay value in the final format expect by ADP, return the delay in ns."""
+	
+	# Convert to little-endian
+	combined = ((combined & 0xFF) << 8) | ((combined >> 8) & 0xFF)
+	
+	# Split
+	fine = combined & 15
+	course = (combined >> 4) & 4095
+	
+	# Convert to time
+	delay = (course + fine/16.0) / fS
+	delay *= 1e9
+	
+	return delay
+
+
+def gaintoDPG(gain):
+	"""Given a gain (between 0 and 1), convert it to a gain in the final form 
+	expected by ADP (big endian 16.1 signed integer)."""
+	
+	# Convert
+	combined = int(32767*gain)
+	
+	# Convert to big-endian
+	combined = ((combined & 0xFF) << 8) | ((combined >> 8) & 0xFF)
+	
+	return combined
+
+
+def DPGtogain(combined):
+	"""Given a gain value in the final format expected by ADP, return the gain
+	as a decimal value (0 to 1)."""
+	
+	# Convert to little-endian
+	combined = ((combined & 0xFF) << 8) | ((combined >> 8) & 0xFF)
+	
+	# Convert back
+	gain = combined / 32767.0
+	
+	return gain
---
+++ packages/lsl/lsl-1.2.0.tar.gz/lsl-1.2.0/lsl/reader/gofast.c
@@ -0,0 +1,147 @@
+#include "Python.h"
+#include <math.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <complex.h>
+
+#define PY_ARRAY_UNIQUE_SYMBOL gofast_ARRAY_API
+#include "numpy/arrayobject.h"
+
+#include "readers.h"
+
+
+/*
+  Exceptions for the Go Fast! Readers
+*/
+
+PyObject *syncError;
+PyObject *eofError;
+
+
+/*
+  Validate a collection of Mark 5C sync words.  Return 1 is all are
+  valid.
+*/
+
+int validSync5C(unsigned int syncWord) {
+	int valid = 0;
+	
+	if( syncWord == 0x5CDEC0DE ) {
+		valid = 1;
+	}
+	
+	return valid;
+}
+
+
+/* 
+  Look-up Tables
+*/
+short int tbw4LUT[256][2];
+float tbnLUT[256];
+float drxLUT[256][2];
+float tbfLUT[256][2];
+
+static void initLWALUTs(void) {
+	// Look-up table inialization function from the VDIFIO library
+	
+	int i,j;
+	short int t;
+	
+	//TBW - 4 bit
+	for(i=0; i<256; i++) {
+		for(j=0; j<2; j++) {
+			t = (i >> 4*(1-j)) & 15;
+			tbw4LUT[i][j] = t;
+			tbw4LUT[i][j] -= ((t&8)<<1);
+		}
+	}
+	
+	// TBN
+	for(i=0; i<256; i++) {
+		tbnLUT[i] = i;
+		tbnLUT[i] -= ((i&128)<<1);
+	}
+	
+	// DRX & TBF
+	for(i=0; i<256; i++) {
+		for(j=0; j<2; j++) {
+			t = (i >> 4*(1-j)) & 15;
+			drxLUT[i][j] = t;
+			drxLUT[i][j] -= ((t&8)<<1);
+			
+			tbfLUT[i][j] = t;
+			tbfLUT[i][j] -= ((t&8)<<1);
+		}
+	}
+}
+
+
+/*
+  Module Setup - Function Definitions and Documentation
+*/
+
+static PyMethodDef GoFastMethods[] = {
+	{"readTBW",    (PyCFunction) readTBW,    METH_VARARGS,               readTBW_doc   }, 
+	{"readTBN",    (PyCFunction) readTBN,    METH_VARARGS,               readTBN_doc   }, 
+	{"readDRX",    (PyCFunction) readDRX,    METH_VARARGS,               readDRX_doc   }, 
+	{"readDRSpec", (PyCFunction) readDRSpec, METH_VARARGS,               readDRSpec_doc},
+	{"readVDIF",   (PyCFunction) readVDIF,   METH_VARARGS|METH_KEYWORDS, readVDIF_doc  }, 
+	{"readTBF",    (PyCFunction) readTBF,    METH_VARARGS,               readTBF_doc   }, 
+	{"readCOR",    (PyCFunction) readCOR,    METH_VARARGS,               readCOR_doc   }, 
+	{NULL,         NULL,                     0,                          NULL          }
+};
+
+PyDoc_STRVAR(GoFast_doc, "Go Fast! (TM) - TBW, TBN, DRX, DR Spectrometer, and VDIF readers written in C");
+
+
+/*
+  Module Setup - Initialization
+*/
+
+PyMODINIT_FUNC init_gofast(void) {
+	PyObject *m, *dict1, *dict2;
+	
+	// Initialize the look-up tables
+	initLWALUTs();
+	initVDIFLUTs();
+	
+	// Module definitions and functions
+	m = Py_InitModule3("_gofast", GoFastMethods, GoFast_doc);
+	import_array();
+
+	// Exceptions
+	
+	//   1.  syncError -> similar to lsl.reader.errors.syncError
+	dict1 = (PyObject *) PyDict_New();
+	if(dict1 == NULL) {
+		PyErr_Format(PyExc_MemoryError, "Cannot create exception dictionary");
+		Py_XDECREF(dict1);
+		Py_XDECREF(m);
+	}
+	PyDict_SetItemString(dict1, "__doc__", \
+		PyString_FromString("Exception raised when a reader encounters an error with one or more of the four sync. words."));
+	syncError = PyErr_NewException("_gofast.syncError", PyExc_IOError, dict1);
+	Py_INCREF(syncError);
+	PyModule_AddObject(m, "syncError", syncError);
+	
+	//    2. eofError -> similar to lsl.reader.errors.eofError
+	dict2 = (PyObject *) PyDict_New();
+	if(dict2 == NULL) {
+		PyErr_Format(PyExc_MemoryError, "Cannot create exception dictionary");
+		Py_XDECREF(dict1);
+		Py_XDECREF(syncError);
+		Py_XDECREF(dict2);
+		Py_XDECREF(m);
+	}
+	PyDict_SetItemString(dict2, "__doc__", \
+		PyString_FromString("Exception raised when a reader encounters the end-of-file while reading."));
+	eofError = PyErr_NewException("_gofast.eofError", PyExc_IOError, dict2);
+	Py_INCREF(eofError);
+	PyModule_AddObject(m, "eofError", eofError);
+	
+	// Version and revision information
+	PyModule_AddObject(m, "__version__", PyString_FromString("0.8"));
+	PyModule_AddObject(m, "__revision__", PyString_FromString("$Rev: 2132 $"));
+	
+}
