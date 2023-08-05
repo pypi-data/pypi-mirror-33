8ad2b8f6f45f2581ecefcbf12f9bce0a8fd77622
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:09:06 +0000

    Add package spylunking/1.0.12/spylunking-1.0.12-py2.py3-none-any.whl

---
+++ packages/spylunking/spylunking-1.0.12-py2.py3-none-any.whl/spylunking-1.0.12.data/scripts/get_splunk_token.py
@@ -0,0 +1,21 @@
+#!python
+
+"""
+Get a Splunk User Token
+"""
+
+import spylunking.get_token
+
+
+def run_main():
+    """run_main"""
+
+    token = spylunking.get_token.get_token(
+        url="https://localhost:8089")
+
+    print(token)
+# end of run_main
+
+
+if __name__ == '__main__':
+    run_main()
