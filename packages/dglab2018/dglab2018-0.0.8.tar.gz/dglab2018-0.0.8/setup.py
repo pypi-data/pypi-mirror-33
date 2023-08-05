a6a789253c52e0ee894682eb0e899f3f64cd86df
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 23:09:13 +0000

    Add package dglab2018/0.0.8/dglab2018-0.0.8.tar.gz

---
+++ packages/dglab2018/dglab2018-0.0.8.tar.gz/dglab2018-0.0.8/setup.py
@@ -0,0 +1,16 @@
+import setuptools
+
+setuptools.setup(
+    name="dglab2018",
+    version="0.0.8",
+    author="Dominik Gotojuch",
+    author_email="dominik@gotojuch.com",
+    description="Data processing and analysis scripts supporting the PhD project by one Dominik Gotojuch",
+    url="https://github.com/robotgentleman/dglab2018",
+    packages=setuptools.find_packages(),
+    classifiers=(
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ),
+)
