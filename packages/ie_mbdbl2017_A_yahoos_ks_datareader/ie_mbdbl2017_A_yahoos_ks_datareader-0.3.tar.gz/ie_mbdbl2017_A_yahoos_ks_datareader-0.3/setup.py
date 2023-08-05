bad01afd0fd9c89e140e416453cd3ee8cd346428
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:25:44 +0000

    Add package ie_mbdbl2017_A_yahoos_ks_datareader/0.3/ie_mbdbl2017_A_yahoos_ks_datareader-0.3.tar.gz

---
+++ packages/ie_mbdbl2017_A_yahoos_ks_datareader/ie_mbdbl2017_A_yahoos_ks_datareader-0.3.tar.gz/ie_mbdbl2017_A_yahoos_ks_datareader-0.3/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='ie_mbdbl2017_A_yahoos_ks_datareader',
+      version='0.3',  # Development release
+      description='Python Packaging Starting Kit - Simple Structure to create your own package - for training purposes!',
+      url='https://github.com/manoelgadi/ie_mbdbl2017_A_yahoos_ks_datareader',
+      author='GroupA',
+      author_email='michelaa@student.ie.edu',
+      license='MIT',
+          packages=['ie_mbdbl2017_A_yahoos_ks_datareader'],
+      zip_safe=False)
