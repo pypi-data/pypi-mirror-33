05f7c78d14bc18c1957ccb3414e00f701949a8c2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:08:05 +0000

    Add package fabkit/0.1.0.dev2/fabkit-0.1.0.dev2.tar.gz

---
+++ packages/fabkit/fabkit-0.1.0.dev2.tar.gz/fabkit-0.1.0.dev2/setup.py
@@ -0,0 +1,34 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='fabkit',
+    version='0.1.0.dev2',
+    packages=find_packages(),
+    license='MIT',
+    install_requires=[
+        "fabric3>=1.7.0",
+    ],
+    description='Group of tools extending fabric v1.x tasks',
+    url="https://github.com/karolhor/fabkit",
+    python_requires='>=3.6',
+    zip_safe=False,
+    author="Karol Horowski",
+    author_email="karol.hor@gmail.com",
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Environment :: Console',
+        'Intended Audience :: Developers',
+        'Intended Audience :: System Administrators',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: MacOS',
+        'Operating System :: POSIX',
+        'Operating System :: Unix',
+        'Programming Language :: Python :: 3 :: Only',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Topic :: Software Development',
+        'Topic :: Software Development :: Build Tools',
+        'Topic :: Software Development :: Libraries',
+        'Topic :: System :: Systems Administration',
+    ]
+)
