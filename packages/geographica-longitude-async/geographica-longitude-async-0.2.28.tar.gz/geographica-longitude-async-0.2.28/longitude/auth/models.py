513daead2d5166baa8907a86eb2269d470abbf65
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:18:42 +0000

    Add package geographica-longitude-async/0.2.28/geographica-longitude-async-0.2.28.tar.gz

---
+++ packages/geographica-longitude-async/geographica-longitude-async-0.2.28.tar.gz/geographica-longitude-async-0.2.28/longitude/auth/models.py
@@ -0,0 +1,79 @@
+from longitude.models.sql import SQLCRUDModel
+from longitude import config
+
+
+class BaseUserModel(SQLCRUDModel):
+    table_name = 'longitude_auth_users'
+
+    _select_columns = [
+        'id',
+        'name',
+        'username',
+        'email',
+        'r.id AS _role_id',
+        'r.name AS _role_name',
+        'r.is_superadmin AS _role_is_superadmin'
+    ]
+
+    simple_filters = (
+        'username',
+        'password'
+    )
+
+    _simple_joins = {
+        'r': '''
+            LEFT JOIN longitude_permission_roles r
+                ON r.id=_t.role_id
+        ''',
+    }
+
+    @property
+    def select_columns(self):
+        return tuple(
+            x
+            for x
+            in self._select_columns
+            if (
+                config.LONGITUDE_PERMISSION_PLUGIN_ENABLED or
+                not x.startswith('r.')
+            )
+        )
+
+    @property
+    def simple_joins(self):
+        joins = dict(self._simple_joins.items())
+
+        if not config.LONGITUDE_PERMISSION_PLUGIN_ENABLED:
+            joins.pop('r')
+
+        return joins
+
+
+class UserTokenModel(SQLCRUDModel):
+    table_name = 'longitude_auth_users_refresh_tokens'
+
+    simple_filters = (
+        'auth_user_id',
+    )
+
+    async def upsert(self, values, pk=('auth_user_id',), returning_columns=None):
+        return await super().upsert(values, pk=pk, returning_columns=returning_columns)
+
+# The base user model may be overwritten if LONGITUDE_AUTH_PLUGIN_USER_MODEL
+# option is set.
+if config.LONGITUDE_AUTH_PLUGIN_USER_MODEL and \
+    config.LONGITUDE_AUTH_PLUGIN_USER_MODEL != 'longitude.auth.models.UserModel':
+
+    from importlib import import_module
+
+    _cls, _module = (
+        ''.join(reversed(x))
+        for x
+        in ''.join(reversed(config.LONGITUDE_AUTH_PLUGIN_USER_MODEL)).split('.', 1)
+    )
+
+    UserModel = getattr(import_module(_module), _cls)
+
+else:
+    class UserModel(BaseUserModel):
+        pass
