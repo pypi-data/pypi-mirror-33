ac80de1842cd6f7ae0a0ec28154b51ef20922afc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:29:53 +0000

    Add package vxaat/0.5.6/vxaat-0.5.6.tar.gz

---
+++ packages/vxaat/vxaat-0.5.6.tar.gz/vxaat-0.5.6/setup.py
@@ -0,0 +1,27 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="vxaat",
+    version="0.5.6",
+    url='http://github.com/praekelt/vumi-aat',
+    license='BSD',
+    description="An AAT USSD transport for Vumi.",
+    long_description=open('README.rst', 'r').read(),
+    author='Praekelt Foundation',
+    author_email='dev@praekeltfoundation.org',
+    packages=find_packages(),
+    include_package_data=True,
+    install_requires=[
+        'vumi',
+        'Twisted>=13.1.0',
+    ],
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: BSD License',
+        'Operating System :: POSIX',
+        'Programming Language :: Python',
+        'Topic :: Software Development :: Libraries :: Python Modules',
+        'Topic :: System :: Networking',
+    ],
+)
