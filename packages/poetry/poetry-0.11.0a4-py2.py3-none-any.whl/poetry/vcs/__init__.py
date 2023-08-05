9a3fa85fc176ff72bf9f10228d2ed9b72cb73cc4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:31:26 +0000

    Add package poetry/0.11.0a4/poetry-0.11.0a4-py2.py3-none-any.whl

---
+++ packages/poetry/poetry-0.11.0a4-py2.py3-none-any.whl/poetry/vcs/__init__.py
@@ -0,0 +1,11 @@
+from poetry.utils._compat import Path
+
+from .git import Git
+
+
+def get_vcs(directory):  # type: (Path) -> Git
+    directory = directory.resolve()
+
+    for p in [directory] + list(directory.parents):
+        if (p / ".git").is_dir():
+            return Git(p)
