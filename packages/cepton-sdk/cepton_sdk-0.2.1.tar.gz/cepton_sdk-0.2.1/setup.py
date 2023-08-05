289e5381d1edf784410ad6932d8f4421f6542b3f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:08:11 +0000

    Add package cepton-sdk/0.2.1/cepton_sdk-0.2.1.tar.gz

---
+++ packages/cepton-sdk/cepton_sdk-0.2.1.tar.gz/cepton_sdk-0.2.1/setup.py
@@ -0,0 +1,29 @@
+import platform
+import sys
+
+import setuptools
+
+if __name__ == "__main__":
+    setuptools.setup(
+        name="cepton_sdk",
+        version="0.2.1",
+        description="Cepton Python SDK",
+        long_description=open("README.md").read(),
+        url="https://github.com/ceptontech/cepton_sdk_redist",
+        author="Cepton Technologies",
+        classifiers=[
+            "Development Status :: 4 - Beta",
+            "Programming Language :: Python :: 3",
+        ],
+        keywords="cepton sdk",
+        python_requires=">=3.3",
+        packages=setuptools.find_packages(),
+        include_package_data=True,
+        install_requires=[
+            "numpy",
+            "pyqt5",
+            "pyserial",
+            "transforms3d",
+            "uuid",
+        ]
+    )
