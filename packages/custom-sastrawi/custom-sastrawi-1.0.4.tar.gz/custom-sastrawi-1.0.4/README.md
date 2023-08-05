f1f4d8c2dd9a468e3d664790dc3a425780150233
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:06:19 +0000

    Add package custom-sastrawi/1.0.4/custom-sastrawi-1.0.4.tar.gz

---
+++ packages/custom-sastrawi/custom-sastrawi-1.0.4.tar.gz/custom-sastrawi-1.0.4/README.md
@@ -0,0 +1,2 @@
+# custom-sastrawi
+An indonesian text-mining library derived from PySastrawi (https://github.com/har07/PySastrawi) with certain modification
---
+++ packages/custom-sastrawi/custom-sastrawi-1.0.4.tar.gz/custom-sastrawi-1.0.4/setup.py
@@ -0,0 +1,20 @@
+import setuptools
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+setuptools.setup(
+	name = 'custom-sastrawi',
+	version = '1.0.4',
+	description = 'An indonesian text-mining library derived from PySastrawi (https://github.com/har07/PySastrawi) with certain modification',
+	long_description=long_description,
+	long_description_content_type="text/markdown",
+	author = 'Yunaz Gilang',
+	author_email = 'yunaz.gilang@gmail.com',
+	url = 'https://github.com/YunazGilang/custom-sastrawi', # use the URL to the github repo
+	keywords = ['sastrawi','custom','indonesian','text-mining'], # arbitrary keywords
+	packages=setuptools.find_packages(),
+	classifiers=(
+		"Programming Language :: Python :: 3",
+		"License :: OSI Approved :: MIT License",
+		"Operating System :: OS Independent",
+	),
+	)
