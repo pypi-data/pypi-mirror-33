35187cdca5e2f689e48c7c6b714712be5714da80
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:09:14 +0000

    Add package spotify-local/0.3.0/spotify-local-0.3.0.tar.gz

---
+++ packages/spotify-local/spotify-local-0.3.0.tar.gz/spotify-local-0.3.0/src/spotify_local/__init__.py
@@ -0,0 +1,12 @@
+import sys
+
+try:
+    assert sys.version_info.major == 3
+    assert sys.version_info.minor > 2
+except AssertionError:
+    raise RuntimeError("Spotify-Local requires Python 3.6+!")
+
+from .core import SpotifyLocal
+
+__version__ = "0.3.0"
+
---
+++ packages/spotify-local/spotify-local-0.3.0.tar.gz/spotify-local-0.3.0/src/spotify_local/config.py
@@ -0,0 +1,4 @@
+from typing import Dict
+
+DEFAULT_ORIGIN = {"Origin": "https://open.spotify.com"}
+DEFAULT_PORT = 4381
