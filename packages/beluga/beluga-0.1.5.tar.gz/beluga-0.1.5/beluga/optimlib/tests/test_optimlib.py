a4e32544fa91119e603db0947fe29a93e0002d54
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:13:34 +0000

    Add package beluga/0.1.5/beluga-0.1.5.tar.gz

---
+++ packages/beluga/beluga-0.1.5.tar.gz/beluga-0.1.5/beluga/optimlib/tests/test_optimlib.py
@@ -0,0 +1,27 @@
+import pytest
+from beluga.optimlib import init_workspace
+
+
+def test_init_workspace():
+    from beluga.problem import OCP
+    problem = OCP('test_problem')
+
+    # Throw an error with no independent variable defined.
+    with pytest.raises(KeyError):
+        init_workspace(problem)
+
+    problem.independent('t', 's')
+    problem.state('x', 'v', 'm')
+    problem.state('v', 'g + u', 'm/s')
+    problem.control('u', 'N')
+    problem.constant('g', 9.80665, 'm/s^2')
+    problem.path_cost('1', 's')
+    problem.constraints().initial('x-x_0', 'm')
+    problem.constraints().initial('v-v_0', 'm/s')
+    problem.constraints().terminal('x-x_f', 'm')
+    problem.scale(m='x', s='x/v', N=1)
+
+    ws = init_workspace(problem)
+
+    assert isinstance(ws, dict)
+    assert ws['problem_name'] == 'test_problem'
