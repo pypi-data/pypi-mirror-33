16c75c9420989c29d9ca3510e7ea4e3006f88cae
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:06:43 +0000

    Add package zi_dct0/0.0.7/zi_dct0-0.0.7.tar.gz

---
+++ packages/zi_dct0/zi_dct0-0.0.7.tar.gz/zi_dct0-0.0.7/setup.py
@@ -0,0 +1,20 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="zi_dct0",
+    version="0.0.7",
+    author="Kien Nguyen",
+    author_email="kiennt23@gmail.com",
+    description="ZI DCT0 implementation in python",
+    long_description=long_description,
+    url="https://github.com/kiennt23/zi_dct0.git",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: GNU General Public License v3 (GPLv3)",
+        "Operating System :: OS Independent",
+    ],
+)
