b2facefa91512f7a834f2aae7298487e468a57ab
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 23:06:47 +0000

    Add package simple_model/1.3.0.post4/simple_model-1.3.0.post4.tar.gz

---
+++ packages/simple_model/simple_model-1.3.0.post4.tar.gz/simple_model-1.3.0.post4/setup.py
@@ -0,0 +1,47 @@
+# -*- coding: utf-8 -*-
+
+import os
+import sys
+
+try:
+    from setuptools import setup, find_packages
+except ImportError:
+    from distutils.core import setup, find_packages
+
+from simple_model import __version__
+
+
+def read(fname):
+    return open(os.path.join(os.path.dirname(__file__), fname), 'r').read()
+
+
+if 'upload' in sys.argv or 'register' in sys.argv:
+    if not __version__ or __version__ == '<VERSION>':
+        raise RuntimeError("Package version not set!")
+    elif '.rc' in __version__:
+        raise RuntimeError("Not deploying release candidate to PyPi!")
+
+setup(
+    name="simple_model",
+    version=__version__,
+    author="Aljosha Friemann",
+    author_email="a.friemann@automate.wtf",
+    description="very simple model framework",
+    long_description=open('README.rst').read(),
+    #license=open('LICENSE.txt').read(),
+    url="https://github.com/afriemann/simple_model",
+    download_url="https://github.com/afriemann/simple_model/archive/{}.tar.gz".format(__version__),
+    packages=find_packages(exclude=('test*', 'assets')),
+    install_requires=[],
+    platforms=['linux'],
+    keywords=['model', 'serialization', 'validation', 'dataclass'],
+    classifiers=[
+        "Programming Language :: Python :: 2.7",
+        "Programming Language :: Python :: 3",
+        "Operating System :: POSIX :: Linux",
+        "Topic :: Software Development :: Libraries",
+        "License :: OSI Approved :: BSD License",
+    ],
+)
+
+# vim: tabstop=8 expandtab shiftwidth=4 softtabstop=4 fenc=utf-8
