999445e38e1afd9aa40834e9d9032578a5fe78ca
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:29:46 +0000

    Add package geneticocolor/0.0.9/geneticocolor-0.0.9.tar.gz

---
+++ packages/geneticocolor/geneticocolor-0.0.9.tar.gz/geneticocolor-0.0.9/README.md
@@ -0,0 +1,20 @@
+# Genetic Opposite Color (geneticocolor)
+
+Geneticocolor is a python package for color sets generation.
+
+## Install
+Using shell just write the following line:
+```
+$ pip install geneticocolor
+```
+## How to use
+Once installed, you can import and use the library in any python file.
+```
+import geneticocolor.color_generator as generator
+import matplotlib.pyplot as plt
+
+colors = generator.generate(x, y, point_classes)
+plt.scatter(x, y, colors)
+
+```
+## More about the implementantion
---
+++ packages/geneticocolor/geneticocolor-0.0.9.tar.gz/geneticocolor-0.0.9/setup.py
@@ -0,0 +1,22 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="geneticocolor",
+    version="0.0.9",
+    author="Beatriz Hernandez Perez",
+    author_email="beatriz.hpz@gmail.com",
+    description="Package for genetic generation of opposite colors for 2D point graphs",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/geneticocolor",
+    packages=setuptools.find_packages(),
+    classifiers=(
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: GNU Affero General Public License v3",
+        "Operating System :: OS Independent",
+        "Topic :: Scientific/Engineering :: Visualization",
+    ),
+)
