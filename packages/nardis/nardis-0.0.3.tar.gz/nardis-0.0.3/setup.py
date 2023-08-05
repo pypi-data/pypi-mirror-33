a66f52c2659bf6e3f1fd2cf3c6ba000fe6bfbe5e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:07:35 +0000

    Add package nardis/0.0.3/nardis-0.0.3.tar.gz

---
+++ packages/nardis/nardis-0.0.3.tar.gz/nardis-0.0.3/setup.py
@@ -0,0 +1,41 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+from setuptools import setup, find_packages
+import io
+import os
+
+here = os.path.abspath(os.path.dirname(__file__))
+
+with io.open(os.path.join(here, 'README.md'), encoding='utf-8') as f:
+    long_description = '\n' + f.read()
+
+
+setup(
+    name="nardis",
+    version="0.0.3",
+    description="A web framework based on ASGI",
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    author="Yoong Kang Lim",
+    author_email="yoongkang.lim@gmail.com",
+    python_requires=">=3.6",
+    url="https://github.com/yoongkang/nardis",
+    packages=find_packages("."),
+    install_requires=[
+        "typing_extensions",
+        "uvicorn",
+    ],
+    include_package_data=True,
+    license='MIT',
+    classifiers=[
+        # Trove classifiers
+        # Full list: https://pypi.python.org/pypi?%3Aaction=list_classifiers
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: Implementation :: CPython',
+        'Programming Language :: Python :: Implementation :: PyPy'
+    ],
+)
