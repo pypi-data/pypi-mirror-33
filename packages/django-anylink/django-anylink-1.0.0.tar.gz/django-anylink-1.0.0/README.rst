e9ae60b971a0dab6fb37b5cc35268d28aca300b9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:25:25 +0000

    Add package django-anylink/1.0.0/django-anylink-1.0.0.tar.gz

---
+++ packages/django-anylink/django-anylink-1.0.0.tar.gz/django-anylink-1.0.0/README.rst
@@ -0,0 +1,50 @@
+==========================
+Generic linking for Django
+==========================
+
+.. image:: https://badge.fury.io/py/django-anylink.png
+    :target: http://badge.fury.io/py/django-anylink
+    :alt: Latest PyPI version
+
+.. image:: https://travis-ci.org/moccu/django-anylink.png
+   :target: https://travis-ci.org/moccu/django-anylink
+   :alt: Latest Travis CI build status
+
+.. image:: https://coveralls.io/repos/moccu/django-anylink/badge.svg
+  :target: https://coveralls.io/github/moccu/django-anylink
+  :alt: Coverage of master build
+
+.. image:: https://readthedocs.org/projects/django-anylink/badge/?version=latest
+    :target: https://readthedocs.org/projects/django-anylink/?badge=latest
+    :alt: Latest read the docs build
+
+
+`django-anylink` is a generic linking module for Django. Using this module, you
+can create links for many usecases.
+
+
+Installation & Documentation
+----------------------------
+
+You can find all documentation in the "docs/source" folder and online at
+All documentation is in the "docs/source" directory and online at
+`Read the Docs <https://readthedocs.org/projects/django-anylink/>`_.
+
+Supported versions
+------------------
+The latest release of `django-anylink` works with Python 2.7 and Python 3.4-3.6,
+and supports Django 1.11.
+
+
+License
+-------
+
+*django-anylink* is licenced under the BSD License (see LICENSE).
+
+
+Resources
+---------
+
+* `Documentation <https://readthedocs.org/projects/django-anylink/>`_
+* `Bug Tracker <https://github.com/moccu/django-anylink/issues/>`_
+* `Code <https://github.com/moccu/django-anylink>`_
---
+++ packages/django-anylink/django-anylink-1.0.0.tar.gz/django-anylink-1.0.0/setup.py
@@ -0,0 +1,108 @@
+from __future__ import unicode_literals
+import codecs
+import os
+import sys
+from setuptools import setup, find_packages
+from setuptools.command.test import test as test_command
+
+
+version = '1.0.0'
+
+
+if sys.argv[-1] == 'publish':
+    os.system('python setup.py sdist upload')
+    os.system('python setup.py bdist_wheel upload')
+    print('You probably want to also tag the version now:')
+    print('  git tag -a %s -m "version %s"' % (version, version))
+    print('  git push --tags')
+    sys.exit()
+
+
+test_requires = [
+    'py>=1.4.26',
+    'pyflakes==1.1.0',
+    'pytest>=2.8.0',
+    'pytest-cache>=1.0',
+    'pytest-cov>=2.1.0',
+    'pytest-flakes>=2.0.0,<2.1',
+    'pytest-pep8>=1.0.6,<1.1',
+    'pytest-django==2.9.1',
+    'coverage>=4.0',
+    'mock>=1.3.0',
+    'pep8>=1.6.2',
+    'tox',
+]
+
+
+install_requires = [
+    'Django',
+]
+
+
+docs_requires = [
+    'sphinx',
+    'sphinx_rtd_theme'
+]
+
+
+def read(*parts):
+    filename = os.path.join(os.path.dirname(__file__), *parts)
+    with codecs.open(filename, encoding='utf-8') as fp:
+        return fp.read()
+
+
+class PyTest(test_command):
+
+    def finalize_options(self):
+        test_command.finalize_options(self)
+        self.test_args = []
+        self.test_suite = True
+
+    def run_tests(self):
+        # import here, cause outside the eggs aren't loaded
+        import pytest
+        errno = pytest.main(self.test_args)
+        sys.exit(errno)
+
+
+setup(
+    name='django-anylink',
+    version=version,
+    description='Generic links for Django models.',
+    long_description=read('README.rst') + '\n\n' + read('CHANGES.rst'),
+    author='Moccu GmbH & Co. KG',
+    author_email='info@moccu.com',
+    url='https://github.com/moccu/django-anylink/',
+    extras_require={
+        'docs': docs_requires,
+        'tests': test_requires,
+    },
+    tests_require=test_requires,
+    install_requires=install_requires,
+    cmdclass={'test': PyTest},
+    packages=find_packages(exclude=[
+        'testing',
+        'testing.pytests',
+        'testing.testproject',
+        'examples',
+    ]),
+    include_package_data=True,
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Environment :: Web Environment',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: BSD License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: Implementation :: PyPy',
+        'Programming Language :: Python :: Implementation :: CPython',
+        'Framework :: Django',
+    ],
+    zip_safe=False,
+)
