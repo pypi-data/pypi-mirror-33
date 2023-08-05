245122caa2e5033bf3197b067d0296c26eccc2d1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:31:38 +0000

    Add package livedataframe/0.0.21/livedataframe-0.0.21.tar.gz

---
+++ packages/livedataframe/livedataframe-0.0.21.tar.gz/livedataframe-0.0.21/setup.py
@@ -0,0 +1,18 @@
+import setuptools
+
+
+setuptools.setup(
+    name="livedataframe",
+    version="0.0.21",
+    author="Chris Dimoff",
+    author_email="chris@livedataframe.com",
+    description="Client for the LiveDataFrame analysis tool",
+    install_requires = ['requests', 'pandas', 'websocket-client', 'matplotlib', 'numpy', 'pyarrow'],
+    url="http://www.livedataframe.com",
+    packages=setuptools.find_packages(),
+    classifiers=(
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ),
+)
