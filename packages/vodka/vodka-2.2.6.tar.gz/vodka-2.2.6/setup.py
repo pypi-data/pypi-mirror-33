423d6a178dce1db331dcfe0bff62377f23e40941
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 23:06:55 +0000

    Add package vodka/2.2.6/vodka-2.2.6.tar.gz

---
+++ packages/vodka/vodka-2.2.6.tar.gz/vodka-2.2.6/setup.py
@@ -0,0 +1,33 @@
+from setuptools import find_packages, setup
+
+version = open('facsimile/VERSION').read().strip()
+requirements = open('facsimile/requirements.txt').read().split("\n")
+test_requirements = open('facsimile/requirements-test.txt').read().split("\n")
+
+
+setup(
+    name='vodka',
+    version=version,
+    author='Twentieth Century',
+    author_email='code@20c.com',
+    description='vodka is a plugin based real-time web service daemon',
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+        'License :: OSI Approved :: Apache Software License'
+    ],
+    packages = find_packages(),
+    install_requires=requirements,
+    test_requires=test_requirements,
+    url='https://github.com/20c/vodka',
+    download_url='https://github.com/20c/vodka/%s' % version,
+    entry_points={
+        'console_scripts': [
+            'bartender=vodka.bartender:bartender'
+        ]
+    },
+    include_package_data=True,
+    zip_safe=True
+)
