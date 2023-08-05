0ed177095107d99d510fd000c8892a69c6f8763f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:24:31 +0000

    Add package Podd/0.1.4/Podd-0.1.4.tar.gz

---
+++ packages/Podd/Podd-0.1.4.tar.gz/Podd-0.1.4/setup.py
@@ -0,0 +1,38 @@
+import setuptools
+
+with open('README.md') as file:
+    long_description = file.read()
+
+requirements = [
+    'feedparser',
+    'jinja2',
+    'mutagen'
+
+]
+
+setuptools.setup(
+    name='Podd',
+    version='0.1.4',
+    author='Alexander Potts',
+    author_email='alexander.potts@gmail.com',
+    description='A Podcast downloader',
+    long_description=long_description,
+    url='https://github.com/jakkso/Podd',
+    packages=setuptools.find_packages(),
+    classifiers=(
+        "Development Status :: 4 - Beta",
+        "Programming Language :: Python :: 3.6",
+        "License :: OSI Approved :: GNU General Public License v2 (GPLv2)",
+        "Operating System :: MacOS",
+        "Operating System :: POSIX :: Linux",
+    ),
+    install_requires=requirements,
+    entry_points={
+        'console_scripts': [
+            'podd = podd.__main__:podd'
+        ]
+    },
+    package_data={'podd': ['templates/*.txt', 'templates/*.html']},
+    include_package_data=True,
+)
+
