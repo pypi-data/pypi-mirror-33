98fbd9a01ae80651e5e1a4b64348c35dc9ec0726
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 23:06:55 +0000

    Add package cmdline-provenance/0.1.2/cmdline_provenance-0.1.2.tar.gz

---
+++ packages/cmdline-provenance/cmdline_provenance-0.1.2.tar.gz/cmdline_provenance-0.1.2/setup.py
@@ -0,0 +1,22 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name='cmdline_provenance',
+    version='0.1.2',
+    description='A Python package for keeping track of your data processing steps',
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    author='Damien Irving',
+    author_email='irving.damien@gmail.com',
+    url='https://github.com/DamienIrving/cmdline_provenance',
+    packages=['cmdline_provenance'],
+    zip_safe=False,
+    install_requires=['datetime', 'gitpython'],
+    classifiers=(
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',)
+)
