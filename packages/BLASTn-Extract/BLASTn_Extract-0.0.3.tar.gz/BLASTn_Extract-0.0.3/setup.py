5cd54869bc17da23d36c81fcec224feb32bf901b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:30:00 +0000

    Add package BLASTn-Extract/0.0.3/BLASTn_Extract-0.0.3.tar.gz

---
+++ packages/BLASTn-Extract/BLASTn_Extract-0.0.3.tar.gz/BLASTn_Extract-0.0.3/setup.py
@@ -0,0 +1,17 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="BLASTn_Extract",
+    version="0.0.3",
+    author="Forest Dussault",
+    author_email="forest.dussault@canada.ca",
+    description="Internal BFSSI package for querying BLASTn results and filtering contig FASTA files",
+    long_description=long_description,
+    url="https://github.com/bfssi-forest-dussault/BLASTn_Extract",
+    packages=setuptools.find_packages(),
+    scripts=['BLASTn_Extract/blastn_extract.py'],
+    install_requires=['click']  # list all dependencies here
+)
