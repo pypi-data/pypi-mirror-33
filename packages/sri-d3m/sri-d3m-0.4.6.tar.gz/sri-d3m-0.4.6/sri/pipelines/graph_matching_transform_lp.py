bc39926c76b9dbdbf0f02996d9f65b1de737f2db
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:06:48 +0000

    Add package sri-d3m/0.4.6/sri-d3m-0.4.6.tar.gz

---
+++ packages/sri-d3m/sri-d3m-0.4.6.tar.gz/sri-d3m-0.4.6/sri/pipelines/graph_matching_transform_lp.py
@@ -0,0 +1,54 @@
+from d3m.metadata import pipeline as meta_pipeline
+
+from sri.pipelines.base import BasePipeline
+from sri.graph.graph_matching import GraphMatchingParser
+from sri.graph.transform import GraphTransformer
+from sri.psl.link_prediction import LinkPrediction
+
+DATASETS = {
+    '49_facebook'
+}
+
+class GraphMatchingTransformLPPipeline(BasePipeline):
+    def __init__(self):
+        super().__init__(DATASETS)
+
+    def _gen_pipeline(self):
+        pipeline = meta_pipeline.Pipeline(context = meta_pipeline.PipelineContext.TESTING)
+        pipeline.add_input(name = 'inputs')
+
+        step_0 = meta_pipeline.PrimitiveStep(primitive_description = GraphMatchingParser.metadata.query())
+        step_0.add_argument(
+                name = 'inputs',
+                argument_type = meta_pipeline.ArgumentType.CONTAINER,
+                data_reference = 'inputs.0'
+        )
+        step_0.add_output('produce')
+        pipeline.add_step(step_0)
+
+        step_1 = meta_pipeline.PrimitiveStep(primitive_description = GraphTransformer.metadata.query())
+        step_1.add_argument(
+                name = 'inputs',
+                argument_type = meta_pipeline.ArgumentType.CONTAINER,
+                data_reference = 'steps.0.produce'
+        )
+        step_1.add_output('produce')
+        pipeline.add_step(step_1)
+
+        step_2 = meta_pipeline.PrimitiveStep(primitive_description = LinkPrediction.metadata.query())
+        step_2.add_argument(
+                name = 'inputs',
+                argument_type = meta_pipeline.ArgumentType.CONTAINER,
+                data_reference = 'steps.1.produce'
+        )
+        step_2.add_output('produce')
+        pipeline.add_step(step_2)
+
+        # Adding output step to the pipeline
+        pipeline.add_output(name = 'Links', data_reference = 'steps.2.produce')
+
+        return pipeline
+
+    def assert_result(self, tester, results, dataset):
+        tester.assertEquals(len(results), 1)
+        tester.assertEquals(len(results[0]), 1)
---
+++ packages/sri-d3m/sri-d3m-0.4.6.tar.gz/sri-d3m-0.4.6/sri/pipelines/graph_node_splitter.py
@@ -0,0 +1,40 @@
+from d3m.metadata import pipeline as meta_pipeline
+
+import sri.pipelines.datasets as datasets
+from sri.pipelines.base import BasePipeline
+from sri.graph.node_splitter import GraphNodeSplitter, GraphNodeSplitterHyperparams
+
+# All graph datasets pass, some are left out for speed.
+SKIP_DATASETS = {
+    '6_70_com_amazon',
+    '6_86_com_DBLP',
+}
+DATASETS = set(datasets.GRAPH_DATASETS) - SKIP_DATASETS
+
+class GraphNodeSplitterPipeline(BasePipeline):
+    def __init__(self):
+        super().__init__(DATASETS)
+
+    def _gen_pipeline(self):
+        pipeline = meta_pipeline.Pipeline(context = meta_pipeline.PipelineContext.TESTING)
+        pipeline.add_input(name = 'inputs')
+
+        step_0 = meta_pipeline.PrimitiveStep(primitive_description = GraphNodeSplitter.metadata.query())
+        step_0.add_argument(
+                name = 'inputs',
+                argument_type = meta_pipeline.ArgumentType.CONTAINER,
+                data_reference = 'inputs.0'
+        )
+        step_0.add_output('produce')
+        pipeline.add_step(step_0)
+
+        # Adding output step to the pipeline
+        pipeline.add_output(name = 'Splits', data_reference = 'steps.0.produce')
+
+        return pipeline
+
+    def assert_result(self, tester, results, dataset):
+        num_fold = GraphNodeSplitterHyperparams.defaults()['num_fold']
+
+        tester.assertEquals(len(results), 1)
+        # TODO(eriq)
