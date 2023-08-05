5ed231b02f66e40b917024c21f87ac15e7f0e168
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:06:12 +0000

    Add package nexus3-cli/0.3.0/nexus3-cli-0.3.0.tar.gz

---
+++ packages/nexus3-cli/nexus3-cli-0.3.0.tar.gz/nexus3-cli-0.3.0/src/nexuscli/exception.py
@@ -0,0 +1,22 @@
+class NexusClientAPIError(Exception):
+    pass
+
+
+class NexusClientConfigurationNotFound(Exception):
+    pass
+
+
+class NexusClientInvalidCredentials(Exception):
+    pass
+
+
+class NexusClientInvalidRepositoryPath(Exception):
+    pass
+
+
+class NexusClientInvalidRepository(Exception):
+    pass
+
+
+class NexusClientDownloadError(Exception):
+    pass
