4152d09477d4e3c485dbf455173aed6b76fc700c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:24:28 +0000

    Add package oolearning/0.2.45/oolearning-0.2.45.tar.gz

---
+++ packages/oolearning/oolearning-0.2.45.tar.gz/oolearning-0.2.45/oolearning/model_wrappers/AdaBoost.py
@@ -0,0 +1,95 @@
+from typing import Union
+
+import numpy as np
+import pandas as pd
+from sklearn.ensemble import AdaBoostClassifier as SkAdaBoostClassifier
+from sklearn.ensemble import AdaBoostRegressor as SkAdaBoostRegressor
+from sklearn.tree import DecisionTreeClassifier, DecisionTreeRegressor
+
+from oolearning.model_wrappers.HyperParamsBase import HyperParamsBase
+from oolearning.model_wrappers.ModelWrapperBase import ModelWrapperBase
+from oolearning.model_wrappers.SklearnPredictMixin import SklearnPredictClassifierMixin, \
+    SklearnPredictRegressorMixin
+
+
+class AdaBoostClassifierHP(HyperParamsBase):
+    # noinspection SpellCheckingInspection
+    def __init__(self,
+                 max_depth: Union[int, None]=None,
+                 n_estimators: int=50,
+                 learning_rate: float=1.0,
+                 algorithm: str='SAMME.R'):
+        """
+        for more info, see
+            http://scikit-learn.org/stable/modules/generated/sklearn.ensemble.AdaBoostClassifier.html#sklearn.ensemble.AdaBoostClassifier
+        """
+        super().__init__()
+
+        self._params_dict = dict(max_depth=max_depth,
+                                 n_estimators=n_estimators,
+                                 learning_rate=learning_rate,
+                                 algorithm=algorithm)
+
+
+class AdaBoostClassifier(SklearnPredictClassifierMixin, ModelWrapperBase):
+    def __init__(self, seed: int=42):
+        super().__init__()
+        self._seed = seed
+
+    @property
+    def feature_importance(self):
+        raise NotImplementedError()
+
+    # noinspection PyMethodOverriding
+    def _train(self, data_x: pd.DataFrame, data_y: np.ndarray, hyper_params: AdaBoostClassifierHP) -> object:
+        assert hyper_params is not None
+        assert isinstance(hyper_params, AdaBoostClassifierHP)
+        param_dict = hyper_params.params_dict
+        tree = SkAdaBoostClassifier(base_estimator=DecisionTreeClassifier(max_depth=param_dict['max_depth']),
+                                    n_estimators=param_dict['n_estimators'],
+                                    learning_rate=param_dict['learning_rate'],
+                                    algorithm=param_dict['algorithm'],
+                                    random_state=self._seed)
+        tree.fit(data_x, data_y)
+        return tree
+
+
+class AdaBoostRegressorHP(HyperParamsBase):
+    def __init__(self,
+                 max_depth: Union[int, None]=None,
+                 n_estimators: int=50,
+                 learning_rate: float=1.0,
+                 loss: str='linear'):
+        """
+        for more info, see
+            http://scikit-learn.org/stable/modules/generated/sklearn.ensemble.AdaBoostRegressor.html
+        """
+        super().__init__()
+
+        self._params_dict = dict(max_depth=max_depth,
+                                 n_estimators=n_estimators,
+                                 learning_rate=learning_rate,
+                                 loss=loss)
+
+
+class AdaBoostRegressor(SklearnPredictRegressorMixin, ModelWrapperBase):
+    def __init__(self, seed: int=42):
+        super().__init__()
+        self._seed = seed
+
+    @property
+    def feature_importance(self):
+        raise NotImplementedError()
+
+    # noinspection PyMethodOverriding
+    def _train(self, data_x: pd.DataFrame, data_y: np.ndarray, hyper_params: AdaBoostRegressorHP) -> object:
+        assert hyper_params is not None
+        assert isinstance(hyper_params, AdaBoostRegressorHP)
+        param_dict = hyper_params.params_dict
+        tree = SkAdaBoostRegressor(base_estimator=DecisionTreeRegressor(max_depth=param_dict['max_depth']),
+                                   n_estimators=param_dict['n_estimators'],
+                                   learning_rate=param_dict['learning_rate'],
+                                   loss=param_dict['loss'],
+                                   random_state=self._seed)
+        tree.fit(data_x, data_y)
+        return tree
