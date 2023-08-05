cf3ebecb2bbe2727bedc56b20183388877fe59aa
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:07:43 +0000

    Add package odoo8-addon-website-mail-snippet-fixed/8.0.1.0.1.99.dev3/odoo8_addon_website_mail_snippet_fixed-8.0.1.0.1.99.dev3-py2-none-any.whl

---
+++ packages/odoo8-addon-website-mail-snippet-fixed/odoo8_addon_website_mail_snippet_fixed-8.0.1.0.1.99.dev3-py2-none-any.whl/odoo_addons/website_mail_snippet_fixed/__openerp__.py
@@ -0,0 +1,33 @@
+# -*- coding: utf-8 -*-
+# © 2016 Antiun Ingeniería S.L. - Jairo Llopis
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
+{
+    "name": "Fixed-Width Layout Snippets for Writing Emails",
+    "summary": "560px width extra building blocks",
+    "version": "8.0.1.0.1",
+    "category": "Marketing",
+    "website": "https://www.tecnativa.com",
+    "author": "Antiun Ingeniería S.L., "
+              "Tecnativa, "
+              "Odoo Community Association (OCA)",
+    "license": "AGPL-3",
+    "application": False,
+    "installable": True,
+    "images": [
+        "images/snippets.png",
+    ],
+    "depends": [
+        "website_mail_snippet_vertical_resize_base",
+    ],
+    "data": [
+        "views/assets.xml",
+        "views/templates.xml",
+        "views/snippet_1_col.xml",
+        "views/snippet_2_cols.xml",
+        "views/snippet_3_cols.xml",
+        "views/snippet_event_date.xml",
+        "views/snippet_hr.xml",
+        "views/snippet_img_text.xml",
+        "views/snippet_text_img.xml",
+    ],
+}
