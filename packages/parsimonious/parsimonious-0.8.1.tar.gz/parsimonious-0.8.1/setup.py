fc7f983591eed1d8132d8f165c8c0043a35037a1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:09:35 +0000

    Add package parsimonious/0.8.1/parsimonious-0.8.1.tar.gz

---
+++ packages/parsimonious/parsimonious-0.8.1.tar.gz/parsimonious-0.8.1/setup.py
@@ -0,0 +1,46 @@
+from sys import version_info
+
+# Prevent spurious errors during `python setup.py test` in 2.6, a la
+# http://www.eby-sarna.com/pipermail/peak/2010-May/003357.html:
+try:
+    import multiprocessing
+except ImportError:
+    pass
+
+from io import open
+from setuptools import setup, find_packages
+
+long_description=open('README.rst', 'r', encoding='utf8').read()
+
+setup(
+    name='parsimonious',
+    version='0.8.1',
+    description='(Soon to be) the fastest pure-Python PEG parser I could muster',
+    long_description=long_description,
+    author='Erik Rose',
+    author_email='erikrose@grinchcentral.com',
+    license='MIT',
+    packages=find_packages(exclude=['ez_setup']),
+    tests_require=['nose'],
+    test_suite='nose.collector',
+    url='https://github.com/erikrose/parsimonious',
+    include_package_data=True,
+    install_requires=['six>=1.9.0'],
+    classifiers=[
+        'Intended Audience :: Developers',
+        'Natural Language :: English',
+        'Development Status :: 3 - Alpha',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+        'Topic :: Scientific/Engineering :: Information Analysis',
+        'Topic :: Software Development :: Libraries',
+        'Topic :: Text Processing :: General'],
+    keywords=['parse', 'parser', 'parsing', 'peg', 'packrat', 'grammar', 'language'],
+)
