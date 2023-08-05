358b2359f7a8e86a65f907e7c6f23e29a605c007
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:30:14 +0000

    Add package spada/1.21.16/spada-1.21.16-py3-none-any.whl

---
+++ packages/spada/spada-1.21.16-py3-none-any.whl/spada/methods/method.py
@@ -0,0 +1,26 @@
+import pickle
+import logging
+
+class Method:
+	def __init__(self, name, genes, transcripts):
+		self.logger = logging.getLogger(name)
+
+		if genes:
+			if isinstance(genes, bool):
+				self._genes = pickle.load(open("genes.pkl", "rb"))
+			elif isinstance(genes, str):
+				self._genes = pickle.load(open(genes, "rb"))
+			else:
+				self._genes = genes
+
+			self._genes.createLogger()
+
+		if transcripts:
+			if isinstance(transcripts, bool):
+				self._txs = pickle.load(open("transcripts.pkl", "rb"))
+			elif isinstance(transcripts, str):
+				self._txs = pickle.load(open(transcripts, "rb"))
+			else:
+				self._txs = transcripts
+
+			self._txs.createLogger()
