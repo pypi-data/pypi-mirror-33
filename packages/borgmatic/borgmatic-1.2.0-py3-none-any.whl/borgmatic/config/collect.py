6ef074e8967f779c61c36156fe1f262215c14c0f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:06:24 +0000

    Add package borgmatic/1.2.0/borgmatic-1.2.0-py3-none-any.whl

---
+++ packages/borgmatic/borgmatic-1.2.0-py3-none-any.whl/borgmatic/config/collect.py
@@ -0,0 +1,36 @@
+import os
+
+
+DEFAULT_CONFIG_PATHS = [
+    '/etc/borgmatic/config.yaml',
+    '/etc/borgmatic.d',
+    os.path.expanduser('~/.config/borgmatic/config.yaml'),
+]
+
+
+def collect_config_filenames(config_paths):
+    '''
+    Given a sequence of config paths, both filenames and directories, resolve that to just an
+    iterable of files. Accomplish this by listing any given directories looking for contained config
+    files. This is non-recursive, so any directories within the given directories are ignored.
+
+    Return paths even if they don't exist on disk, so the user can find out about missing
+    configuration paths. However, skip a default config path if it's missing, so the user doesn't
+    have to create a default config path unless they need it.
+    '''
+    real_default_config_paths = set(map(os.path.realpath, DEFAULT_CONFIG_PATHS))
+
+    for path in config_paths:
+        exists = os.path.exists(path)
+
+        if os.path.realpath(path) in real_default_config_paths and not exists:
+            continue
+
+        if not os.path.isdir(path) or not exists:
+            yield path
+            continue
+
+        for filename in os.listdir(path):
+            full_filename = os.path.join(path, filename)
+            if not os.path.isdir(full_filename):
+                yield full_filename
