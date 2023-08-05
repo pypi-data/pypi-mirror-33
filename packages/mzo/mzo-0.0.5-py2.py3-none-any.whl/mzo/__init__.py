e5efaff8cf150b5a7b6a88dfeb20c7c2d0691433
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:04:03 +0000

    Add package mzo/0.0.5/mzo-0.0.5-py2.py3-none-any.whl

---
+++ packages/mzo/mzo-0.0.5-py2.py3-none-any.whl/mzo/__init__.py
@@ -0,0 +1,22 @@
+
+OAUTH_REDIRECT_URI = 'http://localhost:40004/oauth'
+
+
+class ContextObject:
+
+    def __init__(self, *, http, app_dir, client_id, client_secret, account_id, access_token):
+        self.http = http
+        self.app_dir = app_dir
+        self.client_id = client_id
+        self.client_secret = client_secret
+        self.account_id = account_id
+        self.access_token = access_token
+
+
+from mzo.utils import group, command
+from mzo import types, arguments, options
+from mzo.accounts import accounts
+from mzo.balance import balance
+from mzo.login import login
+from mzo.logout import logout
+from mzo.pay import pay
