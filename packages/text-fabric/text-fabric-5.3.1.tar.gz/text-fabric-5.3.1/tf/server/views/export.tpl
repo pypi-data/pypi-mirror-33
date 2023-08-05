2e8ef7fa1b81eb4dc194626e03789ff07c8808cc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:28:01 +0000

    Add package text-fabric/5.3.1/text-fabric-5.3.1.tar.gz

---
+++ packages/text-fabric/text-fabric-5.3.1.tar.gz/text-fabric-5.3.1/tf/server/views/export.tpl
@@ -0,0 +1,145 @@
+<html> 
+    <head>
+        <meta charset="utf-8"/>
+        <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
+        <title>{{fileName}}</title>
+        <meta name="application-name" content="Text-Fabric Search Box"/>
+<style type="text/css">
+body {
+    padding: 2rem;
+}
+a:link, a:visited {
+    text-decoration: None;
+}
+div.hline {
+    font-size: large;
+    font-style: italic;
+    text-align: center;
+}
+pre.tuples {
+	font-size: medium;
+	font-family: monospace;
+	width: calc(100% - 1rem);
+	margin: 0.3rem 0.3rem;
+    background-color: #eeffdd;
+}
+pre.template {
+	font-size: medium;
+	font-family: monospace;
+	width: calc(100% - 1rem);
+	margin: 0.3rem 0.3rem;
+    background-color: #ffeedd;
+}
+div.description {
+	font-size: medium;
+	font-family: sans-serif;
+}
+div.description a:link, div.description a:visited {
+    color: #0000ee;
+    text-decoration: underline;
+}
+div.dtheadrow {
+    font-weight: bold;
+    background-color: #dddddd;
+    margin-top: 0.5rem;
+    margin-bottom: 0.5rem;
+}
+div.dtheadrow > span {
+    margin: 0.2rem;
+    padding: 0 0.5rem;
+    border-right: 0.2rem solid #ffffff;
+}
+details.dtrow {
+    border: 0.1rem solid #bbbbbb;
+    margin: 0.2rem 0;
+    padding: 0.2rem;
+}
+
+details.dtrow[open] {
+    page-break-before: always;
+    break-before: always;
+}
+details.dtrow[open] {
+    page-break-after: always;
+    break-after: always;
+}
+details.dtrow[open]+details.dtrow[open] {
+    page-break-before: auto;
+    break-before: auto;
+}
+
+details.dtrow > summary {
+    background-color: #f0f0f0;
+    margin: 0.2rem 0;
+    padding: 0.2rem;
+}
+details.dtrow > summary > span {
+    padding: 0 0.5rem;
+    border-right: 0.2rem solid #bbbbbb;
+}
+div.pretty {
+    width: 100%;
+    margin: 0.2rem 0 0.5rem 0;
+}
+a.rwh {
+    font-size: x-small;
+    color: #333333;
+}
+.sq {
+    font-size: small;
+    color: #66aa55;
+}
+div.colofon {
+    display: flex;
+    flex-flow: row nowrap;
+    justify-content: space-between;
+    align-items: flex-start;
+}
+img.hdlogo {
+    flex: 0 0 auto;
+    max-width: 5rem;
+    height: 4rem;
+}
+div.hdlinks {
+    flex: 0 1 auto;
+    display: flex;
+    flex-flow: row wrap;
+    justify-content: space-around;
+    align-items: baseline;
+}
+div.hdlinks > a {
+    flex: 1 1 4rem;
+    background-color: #eeeeff;
+    border-radius: 0.4rem;
+    border: 0.1rem solid #cccccc;
+    padding: 0.2rem 0.2rem;
+    margin: 0.2rem 0.2rem;
+    text-align: center;
+    text-decoration: none;
+}
+</style>
+        {{!css}}
+    </head>
+    <body>
+        <div class="header">
+            <div class="hline">{{fileName}}</div>
+        </div>
+        <div class="description">
+            {{!description}}
+        </div>
+        <hr/>
+        <div>
+            <pre class="template">{{tuples}}</pre>
+        </div>
+        <div>
+            <pre class="template">{{searchTemplate}}</pre>
+        </div>
+        <div class="table">
+            {{!table}}
+        </div>
+        <hr/>
+        <div class="colofon">
+            {{!colofon}}
+        </div>
+    </body>
+</html>
---
+++ packages/text-fabric/text-fabric-5.3.1.tar.gz/text-fabric-5.3.1/tf/server/web.py
@@ -0,0 +1,184 @@
+import os
+
+from markdown import markdown
+
+import bottle
+from bottle import (post, get, route, template, request, static_file, run)
+
+from tf.server.data import makeTfConnection
+from tf.server.common import (
+    getParam, getDebug, getConfig, getAppDir, getValues,
+    pageLinks,
+    shapeMessages, shapeOptions, shapeCondense,
+)
+from tf.apphelpers import RESULT
+
+
+BATCH = 20
+
+myDir = os.path.dirname(os.path.abspath(__file__))
+appDir = None
+bottle.TEMPLATE_PATH = [f'{myDir}/views']
+
+dataSource = None
+config = None
+
+
+def getStuff():
+  global TF
+  global appDir
+
+  config = getConfig(dataSource)
+  if config is None:
+    return None
+
+  TF = makeTfConnection(config.host, config.port)
+  appDir = getAppDir(myDir, dataSource)
+  return config
+
+
+def getInt(x, default=1):
+  if len(x) > 15:
+    return default
+  if not x.isdecimal():
+    return default
+  return int(x)
+
+
+@route('/server/static/<filepath:path>')
+def serveStatic(filepath):
+  return static_file(filepath, root=f'{myDir}/static')
+
+
+@route('/data/static/<filepath:path>')
+def serveData(filepath):
+  return static_file(filepath, root=f'{appDir}/static')
+
+
+@route('/local/<filepath:path>')
+def serveLocal(filepath):
+  return static_file(filepath, root=f'{config.localDir}')
+
+
+@post('/<anything:re:.*>')
+@get('/<anything:re:.*>')
+def serveSearch(anything):
+  searchTemplate = request.forms.searchTemplate.replace('\r', '')
+  tuples = request.forms.tuples.replace('\r', '')
+  fileName = request.forms.fileName.strip()
+  description = request.forms.description.replace('\r', '')
+  withNodes = request.forms.withNodes
+  condensed = request.forms.condensed
+  export = request.forms.export
+  expandAll = request.forms.expandAll
+  linked = getInt(request.forms.linked, default=1)
+  condensedAtt = ' checked ' if condensed else ''
+  withNodesAtt = ' checked ' if withNodes else ''
+
+  options = config.options
+  values = getValues(options, request.forms)
+
+  openedStr = request.forms.opened
+  position = getInt(request.forms.position, default=1)
+  batch = getInt(request.forms.batch, default=BATCH)
+  pages = ''
+
+  opened = {int(n) for n in openedStr.split(',')} if openedStr else set()
+
+  api = TF.connect()
+  header = api.header()
+  css = api.css()
+
+  (defaultCondenseType, condenseTypes) = api.condenseTypes()
+  condenseType = request.forms.condensetp or defaultCondenseType
+  condenseOpts = shapeCondense(condenseTypes, condenseType)
+
+  resultKind = condenseType if condensed else RESULT
+  resultPl = 's' if batch != 1 else ''
+  resultItems = f'{batch} {resultKind}{resultPl}'
+
+  if searchTemplate or tuples:
+    (table, tupleMessages, queryMessages, start, total) = api.search(
+        searchTemplate,
+        tuples,
+        condensed,
+        condenseType,
+        batch,
+        position=position,
+        opened=opened,
+        withNodes=withNodes,
+        linked=linked,
+        **values,
+    )
+    if table is not None:
+      pages = pageLinks(total, position)
+
+    if tupleMessages:
+      tupleMessages = shapeMessages(tupleMessages)
+    if queryMessages:
+      queryMessages = shapeMessages(queryMessages)
+  else:
+    table = f'no {resultKind}s'
+    searchTemplate = ''
+    tupleMessages = ''
+    queryMessages = ''
+
+  if not fileName:
+    fileName = f'{dataSource}-{resultItems} around {position}'
+
+  return (
+      template(
+          'index',
+          dataSource=dataSource,
+          css=css,
+          header=header,
+          options=shapeOptions(options, values),
+          tupleMessages=tupleMessages,
+          queryMessages=queryMessages,
+          table=table,
+          searchTemplate=searchTemplate,
+          tuples=tuples,
+          description=description,
+          condensedAtt=condensedAtt,
+          condenseOpts=condenseOpts,
+          withNodesAtt=withNodesAtt,
+          expandAll=expandAll,
+          linked=linked,
+          batch=batch,
+          position=position,
+          opened=openedStr,
+          pages=pages,
+          test=condensed and condenseType,
+          fileName=fileName,
+      )
+      if not export else
+      template(
+          'export',
+          dataSource=dataSource,
+          css=css,
+          searchTemplate=searchTemplate,
+          description=markdown(description),
+          tuples=tuples,
+          table=table,
+          condensed=condensed,
+          condenseType=condenseType,
+          batch=batch,
+          position=position,
+          colofon=header,
+          fileName=fileName,
+      )
+  )
+
+
+if __name__ == "__main__":
+  dataSource = getParam(interactive=True)
+  if dataSource is not None:
+    debug = getDebug()
+    config = getStuff()
+    if config is not None:
+      run(
+          debug=debug,
+          reloader=debug,
+          host=config.host,
+          port=config.webport,
+      )
