2c72ee7c2f49a0d7eb2666b02b22b5085fef2849
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:24:27 +0000

    Add package tapioca-senado/0.2.0/tapioca_senado-0.2.0-py3-none-any.whl

---
+++ packages/tapioca-senado/tapioca_senado-0.2.0-py3-none-any.whl/tapioca_senado/resource_mapping.py
@@ -0,0 +1,11 @@
+# coding: utf-8
+
+RESOURCE_MAPPING = {
+
+    # Senadores
+    'senadores': {
+        'resource': 'senador/lista/atual',
+        'docs': 'http://legis.senado.gov.br/dadosabertos/docs/ui/index.html#!/ListaSenadorService/resource_ListaSenadorService_listaSenadoresXml_GET',
+        'methods': ['GET']
+    },
+}
