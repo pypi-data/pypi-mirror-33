28c886561c37f175572ae549838da80f35574ba6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:04:11 +0000

    Add package odoo8-addon-membership-prorrate-variable-period/8.0.1.0.0.99.dev14/odoo8_addon_membership_prorrate_variable_period-8.0.1.0.0.99.dev14-py2-none-any.whl

---
+++ packages/odoo8-addon-membership-prorrate-variable-period/odoo8_addon_membership_prorrate_variable_period-8.0.1.0.0.99.dev14-py2-none-any.whl/odoo_addons/membership_prorrate_variable_period/__openerp__.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+# (c) 2015 Antiun Ingeniería S.L. - Pedro M. Baeza
+# License AGPL-3 - See http://www.gnu.org/licenses/agpl-3.0.html
+
+{
+    'name': 'Prorrate membership fee for variable periods',
+    'version': '8.0.1.0.0',
+    'license': 'AGPL-3',
+    'category': 'Association',
+    'author': 'Antiun Ingeniería S.L., '
+              'Tecnativa, '
+              'Odoo Community Association (OCA)',
+    'website': 'http://www.antiun.com',
+    'depends': [
+        'membership_prorrate',
+        'membership_variable_period',
+    ],
+    'data': [
+    ],
+    "installable": True,
+    "auto_install": True,
+}
