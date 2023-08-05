b3d98501e69c81e8df52a44e49e451411f79d7ed
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:07:47 +0000

    Add package c/0.0.2/c-0.0.2.tar.gz

---
+++ packages/c/c-0.0.2.tar.gz/c-0.0.2/c/screen_on_week.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python3
+
+import matplotlib.pyplot as plt
+import pandas as pd
+
+from .reader import Analyzer, read_battery_history
+from .common import smoothed_line
+
+
+def plot(df, canvas):
+    analyzer = Analyzer(df)
+    start = df['date'].min()
+    end = df['date'].max()
+    index = pd.date_range(start, end, freq='W')
+
+    lol = []
+    for date in index:
+        val = analyzer.screen_on_percent_by_week(date=date)
+        lol.append(val / 100 * 24)
+
+    canvas.plot(index, lol)
+    canvas.scatter(index, lol, alpha=0.2)
+    canvas.axhline(y=4, linewidth=1, color='r', alpha=0.5)
+    canvas.set_xlabel('Week')
+    canvas.set_ylabel('Screen on time per day (hr)')
+    canvas.set_title('Screen on by week')
+
+
+if __name__ == '__main__':
+    fig, ax = plt.subplots()
+    df = read_battery_history()
+    plot(df=df, canvas=ax)
+    plt.show()
---
+++ packages/c/c-0.0.2.tar.gz/c-0.0.2/c/voltages.py
@@ -0,0 +1,42 @@
+#!/usr/bin/env python3
+
+import matplotlib.pyplot as plt
+import numpy as np
+
+from .reader import get_last_7_days, read_battery_history
+
+
+def plot(df, canvas):
+    df = df[df.voltage > 0]
+
+    canvas.scatter(
+        df.percent,
+        df.Voltage,
+        s=3,
+        c='#cccccc',
+        alpha=0.07)
+    canvas.set_xlabel('Battery percent')
+    canvas.set_ylabel('Voltage (V)')
+    canvas.set_title('Battery voltage by capacity')
+
+    mdf = get_last_7_days(df)
+    canvas.scatter(
+        mdf.percent,
+        mdf.Voltage,
+        s=3,
+        c='green',
+        alpha=0.2)
+
+    # Polynomial trend line
+    z = np.polyfit(df.percent, df.Voltage, deg=3)
+    p = np.poly1d(z)
+    points = sorted(df.percent)
+    canvas.plot(points, p(points), c='#3bba9c')
+
+    canvas.axhline(y=3.92, linewidth=1, color='r', alpha=0.5)
+
+
+if __name__ == '__main__':
+    fig, ax = plt.subplots()
+    plot(df=read_battery_history(), canvas=ax)
+    plt.show()
