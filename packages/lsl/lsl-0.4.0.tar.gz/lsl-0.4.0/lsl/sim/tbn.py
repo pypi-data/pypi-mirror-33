15a2a0c5f2c8b70acd9cedac29fed7b48eafa6e9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:06:01 +0000

    Add package lsl/0.4.0/lsl-0.4.0.tar.gz

---
+++ packages/lsl/lsl-0.4.0.tar.gz/lsl-0.4.0/lsl/sim/tbn.py
@@ -0,0 +1,210 @@
+# -*- coding: utf-8 -*-
+
+"""
+Python module for creating creating, validating, and writing simulated 
+TBN frames to a file.
+"""
+
+import numpy
+
+from lsl.reader import tbn
+from errors import *
+
+__version__ = '0.1'
+__revision__ = '$ Revision: 6 $'
+__all__ = ['SimFrame', 'frame2frame', '__version__', '__revision__', '__all__']
+
+
+def frame2frame(tbnFrame):
+	"""
+	Convert a :class:`lsl.reader.tbn.Frame` object to a raw DP TBN frame.
+	"""
+
+	# The raw frame
+	rawFrame = numpy.zeros(tbn.FrameSize, dtype=numpy.uint8)
+
+	# Part 1: The header
+	## Sync. words (0xDEC0DE5C)
+	rawFrame[0] = 0xDE  # 222
+	rawFrame[1] = 0xC0  # 192
+	rawFrame[2] = 0xDE  # 222
+	rawFrame[3] = 0x5C  #  92
+	## Frame count
+	rawFrame[5] = (tbnFrame.header.frameCount>>16) & 255
+	rawFrame[6] = (tbnFrame.header.frameCount>>8) & 255
+	rawFrame[7] = tbnFrame.header.frameCount & 255
+	## Seconds count
+	rawFrame[8] = (tbnFrame.header.secondsCount>>24) & 255
+	rawFrame[9] = (tbnFrame.header.secondsCount>>16) & 255
+	rawFrame[10] = (tbnFrame.header.secondsCount>>8) & 255
+	rawFrame[11] = tbnFrame.header.secondsCount & 255
+	## TBN ID
+	rawFrame[12] = (tbnFrame.header.tbnID>>8) & 255
+	rawFrame[13] = tbnFrame.header.tbnID & 255
+	## NB: Next two bytes are unsigned
+	
+	# Part 2: The data
+	## Time tag
+	rawFrame[16] = (tbnFrame.data.timeTag>>56) & 255
+	rawFrame[17] = (tbnFrame.data.timeTag>>48) & 255
+	rawFrame[18] = (tbnFrame.data.timeTag>>40) & 255
+	rawFrame[19] = (tbnFrame.data.timeTag>>32) & 255
+	rawFrame[20] = (tbnFrame.data.timeTag>>24) & 255
+	rawFrame[21] = (tbnFrame.data.timeTag>>16) & 255
+	rawFrame[22] = (tbnFrame.data.timeTag>>8) & 255
+	rawFrame[23] = tbnFrame.data.timeTag & 255
+	## Data
+	i = tbnFrame.data.iq.real
+	q = tbnFrame.data.iq.imag
+	### Round, clip, and convert to unsigned integers
+	i = i.round()
+	i = i.clip(-128, 127)
+	i = i.astype(numpy.uint8)
+	q = q.round()
+	q = q.clip(-128, 127)
+	q = q.astype(numpy.uint8)
+	
+	rawFrame[24::2] = i
+	rawFrame[25::2] = q
+	
+	return rawFrame
+
+
+class SimFrame(tbn.Frame):
+	"""
+	tbn.SimFrame extends the :class:`lsl.reader.tbn.Frame` object to yield a method 
+	for easily creating DP ICD-compliant raw TBN frames.  Frames created with
+	this method can be written to a file via the methods writeRawFrame() function.
+	"""
+
+	def __init__(self, stand=None, pol=None, frameCount=None, obsTime=None, iq=None):
+		"""
+		Given a list of parameters, build a tbn.SimFrame object.  The parameters
+		needed are:
+		  * stand id (>0 & <259)
+		  * polarization (0 for x, or 1 for y)
+		  * which frame number to create
+		  * observation time in samples at fS since the epoch
+		  * 1-D numpy array representing the frame I/Q (complex) data
+		  
+		Not all of these parameters are needed at initialization of the object and
+		the values can be added later.
+
+		.. versionchanged:: 0.3.4
+			obsTime now in samples at fS, not seconds
+		"""
+		
+		self.stand = stand
+		self.pol = pol
+		self.frameCount = frameCount
+		self.obsTime = obsTime
+		self.iq = iq
+		
+		self.header = tbn.FrameHeader()
+		self.data = tbn.FrameData()
+		
+	def __update(self):
+		"""
+		Private function to use the object's parameter values to build up 
+		a tbn.Frame-like object.
+		"""
+		
+		self.header.frameCount = self.frameCount
+		self.header.secondsCount = int(self.obsTime)
+		self.header.tbnID = 2*(self.stand-1) + self.pol + 1
+		
+		self.data.timeTag = self.obsTime
+		self.data.iq = self.iq
+	
+	def loadFrame(self, tbnFrame):
+		"""
+		Populate the a tbn.SimFrame object with a pre-made frame.
+		"""
+		
+		self.header = tbnFrame.header
+		self.data = tbnFrame.data
+		
+		# Back-fill the class' fields to make sure the object is consistent
+		## Header
+		self.stand = self.header.parseID()[0]
+		self.pol = self.header.parseID()[1]
+		self.frameCount = self.header.frameCount
+		## Data
+		self.obsTime = self.data.timeTag
+		self.iq = self.data.iq
+	
+	def isValid(self, raiseErrors=False):
+		"""
+		Check if simulated TBN frame is valid or not.  Valid frames return 
+		True and invalid frames False.  If the 'raiseErrors' keyword is set, 
+		isValid raises an error when a problem is encountered.
+		"""
+
+		# Make sure we have the latest values
+		self.__update()
+
+		stand, pol = self.parseID()
+		# Is the stand number reasonable?
+		if stand == 0 or stand > 258:
+			if raiseErrors:
+				raise invalidStand()
+			return False
+
+		# Is the polarization reasonable?
+		if pol not in [0, 1]:
+			if raiseErrors:
+				raise invalidPol()
+			return False
+
+		# Is there data loaded into frame.data.iq?
+		if self.data.iq is None:
+			if raiseErrors:
+				raise invalidDataSize()
+			return False
+
+		# Does the data length make sense?
+		if self.data.iq.shape[0] != 512:
+			if raiseErrors:
+				raise invalidDataSize()
+			return False
+
+		# Does the data type make sense?
+		if self.data.iq.dtype.kind != 'c':
+			if raiseErrors:
+				raise invalidDataType()
+			return False
+
+		# If we made it this far, it's valid
+		return True
+
+	def createRawFrame(self):
+		"""
+		Re-express a simulated TBN frame as a numpy array of unsigned 8-bit 
+		integers.  Returns a numpy array if the frame  is valid.  If the frame 
+		is not ICD-compliant, a errors.baseSimError-type error is raised.
+		"""
+
+		# Make sure we have the latest values
+		self.__update()
+
+		self.isValid(raiseErrors=True)
+		return frame2frame(self)
+
+	def writeRawFrame(self, fh):
+		"""
+		Write a simulated TBN frame to a filehandle if the frame is valid.
+		If the frame is not ICD-compliant, a errors.baseSimError-type error 
+		is raised.
+		"""
+
+		# Make sure we have the latest values
+		self.__update()
+
+		rawFrame = self.createRawFrame()
+		rawFrame.tofile(fh)
+
+	def __str__(self):
+		if self.stand is None:
+			return "Empty TBN SimFrame object"
+		else:
+			return "TBN SimFrame for stand %i, pol. %i @ time %i" % (self.stand, self.pol, self.obsTime)
---
+++ packages/lsl/lsl-0.4.0.tar.gz/lsl-0.4.0/scripts/plotUVCoverage.py
@@ -0,0 +1,129 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+"""Randomly select 20 antennae from LWA-1 and plot the uv-plane coverage for 
+a zenith snapshot and the expected beam."""
+
+import os
+import sys
+import math
+import numpy
+
+from lsl.common import stations
+from lsl.correlator import uvUtils
+
+import matplotlib.pyplot as plt
+from matplotlib.ticker import NullFormatter
+
+allStands = numpy.arange(1,257)
+
+
+def shift(data, xy=[0,0]):
+	data1 = numpy.concatenate([data[xy[0]:], data[:xy[0]]], axis=0)
+	data2 = numpy.concatenate([data1[:,xy[1]:], data1[:,:xy[1]]], axis=1)
+
+	return data2
+
+
+def randomSelection(N, readyStands, allStands, Fraction=0.75, IncludeOutlier=False):
+	Nready = int(Fraction*N)
+	Nall = N - Nready
+	
+	standList = []
+	for i in range(Nready):
+		randInt = numpy.random.randint(0, readyStands.shape[0])
+		while readyStands[randInt] in standList or readyStands[randInt] in excludeStands:
+			randInt = numpy.random.randint(0, readyStands.shape[0])
+		standList.append( readyStands[randInt] )
+	
+	for i in range(Nall):
+		randInt = numpy.random.randint(0, allStands.shape[0])
+		while allStands[randInt] in standList or allStands[randInt] in excludeStands or allStands[randInt] in readyStands:
+			randInt = numpy.random.randint(0, allStands.shape[0])
+		standList.append( allStands[randInt] )
+
+	if IncludeOutlier:
+		standList[0] = -1
+
+	return numpy.array(standList)
+
+
+def main(args):
+	# Set the LWA Station (and use only the 256 "official" stands)
+	station = stations.lwa1
+	antennas = []
+	for ant in station.getAntennas()[0::2]:
+		if ant.stand.id < 257:
+			antennas.append(ant)
+
+	HA = 0.0
+	dec = station.lat*180.0/math.pi
+	freq = 50.0e6
+
+	uvw = uvUtils.computeUVW(antennas, HA=HA, dec=dec, freq=freq)
+	uvw = numpy.squeeze(uvw[:,:,0])
+	uvw = uvw * 2.9979245800e8 / freq
+
+	# Coursely grid the uv data to come up with a rough beam
+	grid = numpy.zeros((1*240,1*240))
+	for i in range(uvw.shape[0]):
+		u = round((uvw[i,0]+120)*1)
+		v = round((uvw[i,1]+120)*1)
+		grid[u,v] = grid[u,v]+1
+
+	# Plot
+	fig = plt.figure(figsize=(8,8))
+	ax1 = plt.axes([0.30, 0.30, 0.60, 0.60])
+	ax2 = plt.axes([0.30, 0.05, 0.60, 0.15])
+	ax3 = plt.axes([0.05, 0.30, 0.15, 0.60])
+	#ax4 = plt.axes([0.05, 0.05, 0.10, 0.15])
+	ax4 = plt.axes([0.08, 0.08, 0.15, 0.15])
+	ax5 = plt.axes([0.32, 0.32, 0.15, 0.15])
+
+	beam = numpy.fft.fft2(grid)
+	beam = shift(beam, xy=(grid.shape[0]/2,grid.shape[1]/2))
+	beam = beam.real - beam.real.min()
+	beam = numpy.where( beam > 0, beam, 1e-6 )
+	beam = numpy.log10(beam.real)*10.0
+	ax5.imshow(beam[40:200,40:200], interpolation="nearest", vmin=numpy.median(beam), vmax=beam.max())
+	ax5.xaxis.set_major_formatter( NullFormatter() )
+	ax5.yaxis.set_major_formatter( NullFormatter() )
+
+	c = ax1.scatter(uvw[:,0], uvw[:,1], c=uvw[:,2], s=10.0, alpha=0.75)
+	d = ax1.scatter(-uvw[:,0], -uvw[:,1], c=-uvw[:,2], s=10.0, alpha=0.75)
+	ax1.set_xlabel('u [m]')
+	ax1.set_ylabel('v [m]')
+	ax1.set_title('UV Coverage for HA=%+.3f$^h$, $\delta$=%+.3f$^\circ$ at %s' % (HA, dec, station.name))
+
+	ax2.scatter(uvw[:,0], uvw[:,2], c=uvw[:,2], s=10.0)
+	ax2.scatter(-uvw[:,0], -uvw[:,2], c=-uvw[:,2], s=10.0)
+	ax2.xaxis.set_major_formatter( NullFormatter() )
+	ax2.set_ylabel('w')
+
+	ax3.scatter(uvw[:,2], uvw[:,1], c=uvw[:,2], s=10.0)
+	ax3.scatter(-uvw[:,2], -uvw[:,1], c=-uvw[:,2], s=10.0)
+	ax3.yaxis.set_major_formatter( NullFormatter() )
+	ax3.set_xlabel('w')
+
+	#cb = plt.colorbar(c, cax=ax4, orientation='vertical')
+
+	rad = numpy.zeros(uvw.shape[0])
+	for i in range(rad.shape[0]):
+		rad[i] = math.sqrt( uvw[i,0]**2.0 + uvw[i,1]**2.0 + uvw[i,2]**2.0 ) * freq / 2.9979245800e8
+	ax4.hist(rad, 20)
+	ax4.set_xlabel('uv Radius [$\lambda$]')
+	ax4.set_ylabel('Baselines')
+	ax4.set_xticks([0, 4, 8, 12, 16, 20])
+	ax4.set_yticks([0, 500, 1000, 1500, 2000, 2500, 3000])
+
+	ax1.set_xlim([-120, 120])
+	ax1.set_ylim([-120, 120])
+	ax2.set_xlim( ax1.get_xlim() )
+	ax3.set_ylim( ax1.get_ylim() )
+
+	plt.show()
+	fig.savefig('uv-plane.png')
+
+
+if __name__ == "__main__":
+	main(sys.argv[1:])
