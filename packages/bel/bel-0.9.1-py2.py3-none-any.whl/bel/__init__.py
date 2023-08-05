75015796e94085c6ec07db2dd3a2b2c04fab34d7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 23:09:38 +0000

    Add package bel/0.9.1/bel-0.9.1-py2.py3-none-any.whl

---
+++ packages/bel/bel-0.9.1-py2.py3-none-any.whl/bel/__init__.py
@@ -0,0 +1,9 @@
+########################
+# BEL FUNCTION IMPORTS #
+########################
+
+from bel.lang.belobj import BEL
+import bel.lang.bel_specification as bel_specification
+
+import logging
+logging.getLogger(__name__).addHandler(logging.NullHandler())
