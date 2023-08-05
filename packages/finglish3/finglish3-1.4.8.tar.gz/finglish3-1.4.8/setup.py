75f6c12fd664b68b47fc5a4e59b6d92e7bc820df
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:04:50 +0000

    Add package finglish3/1.4.8/finglish3-1.4.8.tar.gz

---
+++ packages/finglish3/finglish3-1.4.8.tar.gz/finglish3-1.4.8/setup.py
@@ -0,0 +1,51 @@
+#!/usr/bin/env python3
+
+try:
+    from setuptools.core import setup
+except ImportError:
+    from distutils.core import setup
+
+try:  # for pip >= 10
+    from pip._internal.req import parse_requirements
+    from pip._internal.download import PipSession
+except ImportError:  # for pip <= 9.0.3
+    from pip.req import parse_requirements
+    from pip.download import PipSession
+import os
+
+def get_file_path(name):
+    return os.path.abspath(os.path.join(
+        os.path.dirname(__file__),
+        name))
+
+with open(get_file_path('finglish/version.py')) as f:
+    exec(f.read())
+
+# read requirements from requirements.txt
+requirements = parse_requirements(get_file_path('requirements.txt'),
+                                  session=PipSession())
+requirements = [str(r.req) for r in requirements]
+
+setup(
+    name = 'finglish3',
+    py_modules = ['version'],
+    packages = ['finglish'],
+    install_requires = requirements,
+    include_package_data=True,
+    version = __version__,
+    description = 'Finglish-to-Persian converter.',
+    author = 'Mostafa Razavi',
+    license = 'MIT',
+    author_email = 'mostafa@sepent.com',
+    url = 'https://github.com/elektito/finglish',
+    download_url = 'https://github.com/elektito/finglish/tarball/' + __version__,
+    keywords = ['persian', 'finglish'],
+    classifiers = [
+        'Programming Language :: Python :: 3'
+    ],
+    entry_points = {
+        'console_scripts': [
+            'f2p=finglish.f2p:main',
+        ],
+    },
+)
