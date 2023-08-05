82a282d40826daa1f6a28e468a758c535aea732d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:05:33 +0000

    Add package jokyjokes/0.0.1/jokyjokes-0.0.1.tar.gz

---
+++ packages/jokyjokes/jokyjokes-0.0.1.tar.gz/jokyjokes-0.0.1/setup.py
@@ -0,0 +1,11 @@
+import setuptools
+
+setuptools.setup(
+    name="jokyjokes",
+    version="0.0.1",
+    author="Demir Antay",
+    author_email="demir99antay@gmail.com",
+    description="A package that provides you hundreds of terrbile jokes",
+    url="https://github.com/demirantay/jokes",
+    packages=setuptools.find_packages(),
+)
