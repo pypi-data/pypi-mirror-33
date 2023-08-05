1230d6ec61fbfbd1a3a9c2223b94762a285b2ad2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:09:17 +0000

    Add package hal-x/0.5/hal-x-0.5.tar.gz

---
+++ packages/hal-x/hal-x-0.5.tar.gz/hal-x-0.5/README.md
@@ -0,0 +1,17 @@
+# Hierarchical Agglomerative Learning (HAL)
+Package for performing clustering for high-dimensional data. 
+
+# Installing (once)
+1. Activate an [Anaconda](https://conda.io/docs/user-guide/tasks/manage-environments.html) Python 3 environment
+```
+conda config --add channels conda-forge #if not already in your channels. Needed for fftw.
+conda install cython numpy fftw
+pip install hal
+
+```
+# Updating
+Again from your Anaconda Python 3 environment:
+```
+pip install hal --upgrade
+```
+
---
+++ packages/hal-x/hal-x-0.5.tar.gz/hal-x-0.5/setup.py
@@ -0,0 +1,23 @@
+from setuptools import setup
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setup(
+      name='hal-x',
+      version='0.5',
+      description='Clustering via hierarchical agglomerative learning',
+      author='Alexandre Day',
+      author_email='alexandre.day1@gmail.com',
+      license='MIT',
+      packages=['hal'],
+      install_requires =['fitsne'],
+      zip_safe=False,
+      long_description_content_type="text/markdown",
+      url="https://alexandreday.github.io/",
+      classifiers=(
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    )
+)
