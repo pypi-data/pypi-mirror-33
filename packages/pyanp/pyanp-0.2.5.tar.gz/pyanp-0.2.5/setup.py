f7066e0082fa73b3e39365918432661d43681743
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:25:21 +0000

    Add package pyanp/0.2.5/pyanp-0.2.5.tar.gz

---
+++ packages/pyanp/pyanp-0.2.5.tar.gz/pyanp-0.2.5/setup.py
@@ -0,0 +1,22 @@
+'''
+Created October 24, 2017
+
+@author: Bill Adams
+'''
+
+from setuptools import setup
+
+setup(name='pyanp',
+      version='0.2.5',
+      description='Math library for ANP/AHP calculations',
+      url='https://github.com/wjladams/pyanp',
+      author='Bill Adams',
+      author_email='wjadams@decisionlens.com',
+      license='MIT',
+      package_dir={'pyanp':'pyanp'},
+      python_requires=">=3",
+      install_requires=['numpy'],
+      packages=['pyanp'],
+      #py_modules=['fxmodel'],
+      #package_data={'bamath': ['data/*.csv', 'data/*.pickle', 'data/*.json', '*.css']},
+      zip_safe=False)
