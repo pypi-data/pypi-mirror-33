38a3930d815afae282c087a82fb9b3e1cff905cb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:25:08 +0000

    Add package dictsorted/0.0.2/dictsorted-0.0.2.tar.gz

---
+++ packages/dictsorted/dictsorted-0.0.2.tar.gz/dictsorted-0.0.2/setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+
+setup(
+name='dictsorted',
+version='0.0.2',
+py_modules=['dictsorted'],
+author='lucyking',
+author_email='joehisaishi1943@gmail.com',
+url='https://github.com/lucyking/dictsorted',
+description='return sorted(dict.key/value)',
+)
+
