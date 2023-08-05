17c4321bafdbcfc304d66105e9743f8c8dddd53d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 23:10:00 +0000

    Add package html_librarian/0.0.1/html_librarian-0.0.1.tar.gz

---
+++ packages/html_librarian/html_librarian-0.0.1.tar.gz/html_librarian-0.0.1/setup.py
@@ -0,0 +1,20 @@
+from setuptools import setup, find_packages
+
+
+with open('readme.md') as f:
+    readme = f.read()
+
+with open('LICENSE') as f:
+    license = f.read()
+
+setup(
+    name='html_librarian',
+    version='0.0.1',
+    description='Never ever scrape the html off of a site multiple times ever again.',
+    long_description=readme,
+    author='Alex Sieusahai',
+    author_email='alexsieu14@gmail.com',
+    url='https://github.com/alexsieusahai/Librarian',
+    license=license,
+    packages=find_packages(exclude=('tests'))
+)
