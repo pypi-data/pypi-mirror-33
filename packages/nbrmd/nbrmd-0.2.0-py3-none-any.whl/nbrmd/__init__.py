6effa9b4c777d3efd936335f4a1fe395717dd9a0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:07:37 +0000

    Add package nbrmd/0.2.0/nbrmd-0.2.0-py3-none-any.whl

---
+++ packages/nbrmd/nbrmd-0.2.0-py3-none-any.whl/nbrmd/__init__.py
@@ -0,0 +1,12 @@
+"""R markdown notebook format for Jupyter
+
+Use this module to read or write Jupyter notebooks as Rmd documents (methods 'read', 'reads', 'write', 'writes')
+
+Use the 'pre_save_hook' method (see its documentation) to automatically dump your Jupyter notebooks as a Rmd file, in addition
+to the ipynb file.
+
+Use the 'nbrmd' conversion script to convert Jupyter notebooks from/to R markdown notebooks.
+"""
+
+from .nbrmd import read, reads, readf, write, writes, writef
+from .hooks import update_rmd, update_ipynb, update_rmd_and_ipynb
