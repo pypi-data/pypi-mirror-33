12d69620f793ab6df54f13b6b740e0296730f8b1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:23:50 +0000

    Add package cibuildwheel/0.9.0/cibuildwheel-0.9.0.tar.gz

---
+++ packages/cibuildwheel/cibuildwheel-0.9.0.tar.gz/cibuildwheel-0.9.0/cibuildwheel/util.py
@@ -0,0 +1,50 @@
+from fnmatch import fnmatch
+
+
+def prepare_command(command, python, pip, project):
+    '''
+    Preprocesses a command by expanding variables like {python} or {pip}.
+
+    For example, used for the before_build option, where the user would
+    like to run a command like `python setup.py test`. If the command should run on
+    Python 3, the user could write `{python} setup.py test`. This command would expand
+    it out to python2 or python3 as appropriate.
+    '''
+    return command.format(python=python, pip=pip, project=project)
+
+
+def get_build_verbosity_extra_flags(level):
+    if level > 0:
+        return ['-' + level * 'v']
+    elif level < 0:
+        return ['-' + -level * 'q']
+    else:
+        return []
+
+
+class BuildSkipper(object):
+    def __init__(self, skip_config):
+        self.patterns = skip_config.split()
+
+    def __call__(self, build_id):
+        return any(fnmatch(build_id, pattern) for pattern in self.patterns)
+
+    def __repr__(self):
+        return 'BuildSkipper(%r)' % ' '.join(self.patterns)
+
+
+# Taken from https://stackoverflow.com/a/107717
+class Unbuffered(object):
+    def __init__(self, stream):
+        self.stream = stream
+
+    def write(self, data):
+        self.stream.write(data)
+        self.stream.flush()
+
+    def writelines(self, datas):
+        self.stream.writelines(datas)
+        self.stream.flush()
+
+    def __getattr__(self, attr):
+        return getattr(self.stream, attr)
