489925964c96f5dbf47af64a18c916cd0be1990e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:06:22 +0000

    Add package primes_times_table/0.0.1/primes_times_table-0.0.1.tar.gz

---
+++ packages/primes_times_table/primes_times_table-0.0.1.tar.gz/primes_times_table-0.0.1/setup.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python
+
+from setuptools import setup, find_packages
+
+setup(
+    name='primes_times_table',
+    version='0.0.1',
+    author='Jacob Powers',
+    author_email='powersjcb@gmail.com',
+    url='https://github.com/powersjcb/primes_times_table',
+    packages=find_packages(),
+    entry_points={
+        'console_scripts': [
+            'genprimes = primes_times_table:main'
+        ],
+    },
+)
