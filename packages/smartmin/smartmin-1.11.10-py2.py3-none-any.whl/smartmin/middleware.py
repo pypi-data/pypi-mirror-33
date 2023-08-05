0dbbf865920fade226985cba0b66814962558cc3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:25:52 +0000

    Add package smartmin/1.11.10/smartmin-1.11.10-py2.py3-none-any.whl

---
+++ packages/smartmin/smartmin-1.11.10-py2.py3-none-any.whl/smartmin/middleware.py
@@ -0,0 +1,77 @@
+from __future__ import unicode_literals
+
+import os
+import sys
+
+from django.conf import settings
+from django.http import HttpResponseRedirect, HttpResponse
+from django.utils import timezone
+from six.moves import StringIO
+
+try:
+    from django.utils.deprecation import MiddlewareMixin
+except ImportError:
+    class MiddlewareMixin(object):
+        pass
+
+
+class AjaxRedirect(MiddlewareMixin):
+    def process_response(self, request, response):
+        if request.is_ajax():
+            if type(response) == HttpResponseRedirect:
+                # This is our own AJAX friend redirect to allow
+                # the calling Javascript the opportunity to deal
+                # with redirect responses in its own way
+                response = HttpResponse(response["Location"])
+                response.status_code = 302
+        return response
+
+
+class ProfileMiddleware(MiddlewareMixin):
+    def __init__(self):
+        pass
+
+    def process_view(self, request, view, *args, **kwargs):
+        import hotshot, hotshot.stats  # noqa
+
+        for item in request.META['QUERY_STRING'].split('&'):
+            if item.split('=')[0] == 'profile':  # profile in query string
+
+                # catch the output, must happen before stats object is created
+                # see https://bugs.launchpad.net/webpy/+bug/133080 for the details
+                std_old, std_new = sys.stdout, StringIO.StringIO()
+                sys.stdout = std_new
+
+                # now let's do some profiling
+                tmpfile = '/tmp/%s' % request.COOKIES['sessionid']
+                prof = hotshot.Profile(tmpfile)
+
+                # make a call to the actual view function with the given arguments
+                prof.runcall(view, request, *args[0], **args[1])
+                prof.close()
+
+                # and then statistical reporting
+                stats = hotshot.stats.load(tmpfile)
+                stats.strip_dirs()
+                stats.sort_stats('time')
+
+                # do the output
+                stats.print_stats(1.0)
+
+                # restore default output
+                sys.stdout = std_old
+
+                # delete file
+                os.remove(tmpfile)
+
+                return HttpResponse('<pre>%s</pre>' % std_new.getvalue())
+
+        return None
+
+
+class TimezoneMiddleware(object):
+    def process_request(self, request):
+        user_tz = getattr(settings, 'USER_TIME_ZONE', None)
+
+        if user_tz:
+            timezone.activate(user_tz)
