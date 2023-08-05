834cb46fb55562a9b96d20b660c84119dfd49093
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:30:52 +0000

    Add package silph.models/0.8.15/silph.models-0.8.15.tar.gz

---
+++ packages/silph.models/silph.models-0.8.15.tar.gz/silph.models-0.8.15/silph/models/__init__.py
@@ -0,0 +1,44 @@
+
+from .communities import (
+    Community,
+    CommunityAdmin,
+    DiscordMeta,
+    TelegramMeta,
+)
+
+from .events import (
+    Event,
+    EventCommunities,
+    EventStaff,
+    EventCheckin,
+)
+
+from .meetings import (
+    Meeting,
+    TravelersMet,
+)
+
+from .nests import (
+    Nest,
+    NestVerification,
+    NestAdvice,
+    NestMigration,
+)
+
+from .news import (
+    RedditSubmission,
+)
+
+from .pokemon import (
+    Pokemon,
+    PokemonType,
+)
+
+from .users import (
+    Avatar,
+    TravelersCard,
+    User,
+    UserLogin,
+    UnlockedAvatar,
+    UserBadge,
+)
