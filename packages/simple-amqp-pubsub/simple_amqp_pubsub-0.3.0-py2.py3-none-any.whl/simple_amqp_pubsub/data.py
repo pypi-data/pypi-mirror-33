2e30f8f5de86d3bd8edf20c4994a5ccc598dd92a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:08:30 +0000

    Add package simple-amqp-pubsub/0.3.0/simple_amqp_pubsub-0.3.0-py2.py3-none-any.whl

---
+++ packages/simple-amqp-pubsub/simple_amqp_pubsub-0.3.0-py2.py3-none-any.whl/simple_amqp_pubsub/data.py
@@ -0,0 +1,34 @@
+from typing import Any, List
+
+from dataclasses import dataclass, field, replace
+
+
+class Data:
+    def replace(self, **kwargs):
+        return replace(self, **kwargs)
+
+
+@dataclass(frozen=True)
+class Event(Data):
+    source: str
+    topic: str
+    payload: Any
+    pipe: str = ''
+    retry_count: int = 0
+
+
+@dataclass(frozen=True)
+class Source(Data):
+    name: str
+    durable: bool = False
+
+
+@dataclass(frozen=True)
+class Pipe(Data):
+    name: str
+    retries_enabled: bool = True
+    retries: List[str] = field(
+        default_factory=lambda: ['1m', '15m', '1h'],
+    )
+    exclusive: bool = False
+    durable: bool = False
