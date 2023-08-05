b87841723862b38272336902f2eb2799590b7a95
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:25:08 +0000

    Add package validate-it/0.2.11/validate-it-0.2.11.tar.gz

---
+++ packages/validate-it/validate-it-0.2.11.tar.gz/validate-it-0.2.11/validate_it/base.py
@@ -0,0 +1,53 @@
+from typing import Tuple, Any
+
+import attr
+
+
+@attr.s(slots=True)
+class Validator:
+    _singletons = {}
+    _base_type = object
+    _field_name = attr.ib(default="", validator=[attr.validators.instance_of(str)])
+    _alias = attr.ib(default="", validator=[attr.validators.instance_of(str)])
+    _rename = attr.ib(default="", validator=[attr.validators.instance_of(str)])
+    _description = attr.ib(default="", validator=[attr.validators.instance_of(str)])
+
+    def __set_name__(self, owner, name):
+        self._field_name = name
+
+    # CONFLICT: _field_name representation for two different fields represent as first defined with same signature
+    # def __new__(cls, *args, **kwargs):
+    #     _name = cls.get_singleton_name(*args, **kwargs)
+    #
+    #     if _name not in cls._singletons:
+    #         _instance = object.__new__(cls)
+    #         _instance.__init__(*args, **kwargs)
+    #         cls._singletons[_name] = _instance
+    #     return cls._singletons[_name]
+
+    @property
+    def extended_type(self):
+        return self.__class__.__name__
+
+    @classmethod
+    def get_singleton_name(cls, *args, **kwargs):
+        return cls.__name__ + str(kwargs)
+
+    def representation(self, **kwargs):
+        _data = {"base_type": self._base_type.__name__, "extended_type": self.extended_type}
+
+        _data.update(**kwargs)
+
+        if self._field_name:
+            _data["name"] = self._field_name
+
+        if self._description:
+            _data["description"] = self._description
+
+        return _data
+
+    def validate_it(self, value, convert=False, strip_unknown=False) -> Tuple[Any, Any]:
+        raise NotImplementedError
+
+
+__all__ = ["Validator"]
---
+++ packages/validate-it/validate-it-0.2.11.tar.gz/validate-it-0.2.11/validate_it/union.py
@@ -0,0 +1,51 @@
+from typing import Tuple
+
+import attr
+
+from validate_it.base import Validator
+from validate_it.utils import list_of, not_empty
+
+
+@attr.s(slots=True)
+class UnionType(Validator):
+    """
+    Схема для которой можно задать несколько допустимых типов.
+
+    Например:
+
+    .. code-block:: python
+
+        class MySchema(UnionSchema):
+            alternatives=[
+                FirstSchema(),
+                SecondSchema()
+            ]
+
+    """
+
+    _alternatives = attr.ib(
+        default=attr.Factory(list), validator=[attr.validators.instance_of(list), list_of(Validator), not_empty]
+    )
+
+    def representation(self, **kwargs):
+        _data = super().representation(**kwargs)
+
+        _data["one of"] = [x.representation(**kwargs) for x in self._alternatives]
+
+        return _data
+
+    def validate_it(self, value, convert=False, strip_unknown=False) -> Tuple[dict, dict]:
+        _errors = {}
+
+        for _alternative in self._alternatives:
+            _error, _value = _alternative.validate_it(value, convert, strip_unknown)
+
+            if not _error:
+                return _error, _value
+            else:
+                _errors[_alternative.__class__.__name__] = _error
+
+        return _errors, value
+
+
+__all__ = ["UnionType"]
