8e02a5b3e90cddbca23b800bf640accfbb84ecfe
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:29:20 +0000

    Add package voximplant-client/0.0.1/voximplant_client-0.0.1.tar.gz

---
+++ packages/voximplant-client/voximplant_client-0.0.1.tar.gz/voximplant_client-0.0.1/setup.py
@@ -0,0 +1,23 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='voximplant_client',
+    version='0.0.1',
+    description="Voximplant.com API client.",
+    keywords=[],
+    url="https://github.com/gdml/voximplant-client/",
+    author="Fedor Borshev",
+    author_email="f@f213.in",
+    license="MIT",
+    packages=find_packages(),
+    install_requires=[
+        'Click',
+        'requests',
+    ],
+    entry_points="""
+        [console_scripts]
+        voximplant = voximplant_client.cli:main
+    """,
+    include_package_data=True,
+    zip_safe=False,
+)
