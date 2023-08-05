a2f0a8e88cb0a4885aaac9732d7f626ee429794a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:07:59 +0000

    Add package efficient-apriori/0.4.3/efficient_apriori-0.4.3-py3-none-any.whl

---
+++ packages/efficient-apriori/efficient_apriori-0.4.3-py3-none-any.whl/efficient_apriori/__init__.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Implementation of the Apriori algorithm.
+"""
+
+__version__ = '0.4.3'
+
+from efficient_apriori.apriori import apriori
+
+def run_tests():
+    """
+    Run all tests.
+    """
+    import pytest
+    import os
+    base, _ = os.path.split(__file__)
+    pytest.main(args=[base, '--doctest-modules'])
