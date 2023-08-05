801f9bf1ed8a31353961849c67593e5a9603494e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 23:06:39 +0000

    Add package attributedict/0.1.2/attributedict-0.1.2.tar.gz

---
+++ packages/attributedict/attributedict-0.1.2.tar.gz/attributedict-0.1.2/setup.py
@@ -0,0 +1,63 @@
+
+import setuptools
+
+# DISABLED/BUG: this line fails when `pip install attributedict` but works `pip install .`
+# from attributedict import __version__
+
+setuptools.setup(
+    name = 'attributedict',
+    version = '0.1.2',
+    description = (
+        'A dictionary object with attributes support.'
+    ),
+    long_description = open('README.md').read(),
+    long_description_content_type = 'text/markdown',
+    keywords = [
+        'object',
+        'dict',
+        'dictionary',
+        'collection',
+        'attributes',
+        'attribute',
+        'attr',
+        'properties',
+        'property',
+        'props',
+        'openstruct',
+        'struct',
+        'hashmap',
+    ],
+    author = 'Jonas Grimfelt',
+    author_email = 'grimen@gmail.com',
+    url = 'https://github.com/grimen/python-attributedict',
+    project_urls = {
+        'repository': 'https://github.com/grimen/python-attributedict',
+        'bugs': 'https://github.com/grimen/python-attributedict/issues',
+    },
+    package_data = {
+        '': [
+            'MIT-LICENSE',
+            'README.md',
+        ],
+    },
+    py_modules = ['attributedict'],
+    license = 'MIT',
+    classifiers = [
+        'Topic :: Software Development :: Libraries',
+        'Development Status :: 5 - Production/Stable',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: POSIX',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: Implementation :: CPython',
+        'Programming Language :: Python :: Implementation :: PyPy',
+    ],
+    zip_safe = True,
+)
