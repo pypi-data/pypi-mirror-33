f3c53366ad04dab76f8f94b67233854cb9a4ad6d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 23:06:29 +0000

    Add package Flask-Vises/2.0.0/Flask-Vises-2.0.0.tar.gz

---
+++ packages/Flask-Vises/Flask-Vises-2.0.0.tar.gz/Flask-Vises-2.0.0/flask_vises/database/models.py
@@ -0,0 +1,61 @@
+#!/usr/bin/env python
+# coding=utf-8
+
+
+from uuid import uuid4
+from sqlalchemy import Column, DateTime, String, func
+from sqlalchemy.dialects import postgresql
+
+
+class ModelMixinAbstract(object):
+    __abstract__ = True
+
+    id = Column(
+        postgresql.UUID(as_uuid=True),
+        primary_key=True,
+        default=uuid4,
+    )
+
+    created_time = Column(
+        DateTime(timezone=True),
+        nullable=False,
+        default=func.now(),
+    )
+
+    def __repr__(self):
+        """don't forget overload!"""
+        return '{}'.format(self.id)
+
+
+class ObjectModelMixin(ModelMixinAbstract):
+    """model mixin - Object"""
+    __abstract__ = True
+
+    # last update time
+    updated_time = Column(
+        DateTime(timezone=True),
+        nullable=False,
+        default=func.now(),
+        onupdate=func.now(),
+    )
+
+
+class RecordModelMixin(ModelMixinAbstract):
+    """model mixin - Record/Logging"""
+    __abstract__ = True
+
+
+class GeneralOptionMixin(ObjectModelMixin):
+    group = Column(
+        String(20),
+        server_default='',
+        nullable=False,
+    )
+    key = Column(
+        String(50),
+        server_default='',
+        nullable=False,
+    )
+    value = Column(
+        postgresql.JSONB,
+    )
