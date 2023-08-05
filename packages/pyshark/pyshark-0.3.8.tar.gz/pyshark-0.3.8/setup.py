23a4d8ccd7e6e11ce5b5123c0af8da29801c9cb9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 23:07:01 +0000

    Add package pyshark/0.3.8/pyshark-0.3.8.tar.gz

---
+++ packages/pyshark/pyshark-0.3.8.tar.gz/pyshark-0.3.8/setup.py
@@ -0,0 +1,26 @@
+import os
+from setuptools import setup, find_packages
+
+with open(os.path.join(os.path.dirname(__file__), 'README.txt')) as f:
+    long_description = f.read()
+
+setup(
+    name="pyshark",
+    version="0.3.8",
+    packages=find_packages(),
+    package_data={'': ['*.ini', '*.pcapng']},
+    # Temporarily using trollius 1.0.4 until issue https://github.com/haypo/trollius/issues/4 is resolved.
+    install_requires=['lxml', 'py', 'trollius==1.0.4', 'logbook'],
+    tests_require=['mock', 'pytest'],
+    url="https://github.com/KimiNewt/pyshark",
+    long_description=long_description,
+    author="KimiNewt",
+    description="Python wrapper for tshark, allowing python packet parsing using wireshark dissectors",
+    keywords="wireshark capture packets parsing packet",
+    use_2to3=True,
+    classifiers=[
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 3',
+    ],
+)
