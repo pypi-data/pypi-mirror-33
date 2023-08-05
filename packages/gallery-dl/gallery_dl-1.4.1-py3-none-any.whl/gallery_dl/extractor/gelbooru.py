5fdb28aaf31dd881f4a9a66b51499613fffcc61d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:28:42 +0000

    Add package gallery-dl/1.4.1/gallery_dl-1.4.1-py3-none-any.whl

---
+++ packages/gallery-dl/gallery_dl-1.4.1-py3-none-any.whl/gallery_dl/extractor/gelbooru.py
@@ -0,0 +1,202 @@
+# -*- coding: utf-8 -*-
+
+# Copyright 2014-2018 Mike Fährmann
+#
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License version 2 as
+# published by the Free Software Foundation.
+
+"""Extract images from https://gelbooru.com/"""
+
+from .common import SharedConfigExtractor, Message
+from .. import text, util, exception
+import xml.etree.ElementTree as ET
+
+
+class GelbooruExtractor(SharedConfigExtractor):
+    """Base class for gelbooru extractors"""
+    basecategory = "booru"
+    category = "gelbooru"
+    filename_fmt = "{category}_{id}_{md5}.{extension}"
+    api_url = "https://gelbooru.com/index.php?page=dapi&s=post&q=index"
+
+    def __init__(self):
+        SharedConfigExtractor.__init__(self)
+        self.start_post = 0
+        self.use_api = self.config("api", True)
+        if self.use_api:
+            self.get_post_data = self.get_post_data_api
+
+    def items(self):
+        data = self.get_metadata()
+
+        yield Message.Version, 1
+        yield Message.Directory, data
+
+        for post in util.advance(self.get_posts(), self.start_post):
+            if isinstance(post, str):
+                post = self.get_post_data(post)
+            for key in ("id", "width", "height", "score", "change"):
+                post[key] = text.parse_int(post[key])
+            url = post["file_url"]
+            post.update(data)
+            yield Message.Url, url, text.nameext_from_url(url, post)
+
+    def skip(self, num):
+        self.start_post += num
+        return num
+
+    def get_metadata(self):
+        """Return general metadata"""
+        return {}
+
+    def get_posts(self):
+        """Return an iterable containing all relevant post objects"""
+
+    def get_post_data(self, post_id):
+        """Extract metadata of a single post"""
+        page = self.request("https://gelbooru.com/index.php?page=post&s=view"
+                            "&id=" + post_id).text
+        data = text.extract_all(page, (
+            (None        , '<meta name="keywords"', ''),
+            ("tags"      , ' imageboard, ', '"'),
+            ("id"        , '<li>Id: ', '<'),
+            ("created_at", '<li>Posted: ', '<'),
+            ("width"     , '<li>Size: ', 'x'),
+            ("height"    , '', '<'),
+            ("source"    , '<li>Source: <a href="', '"'),
+            ("rating"    , '<li>Rating: ', '<'),
+            (None        , '<li>Score: ', ''),
+            ("score"     , '>', '<'),
+            ("file_url"  , '<li><a href="http', '"'),
+            ("change"    , ' id="lupdated" value="', '"'),
+        ))[0]
+        data["file_url"] = "http" + data["file_url"].replace("m//", "m/", 1)
+        data["md5"] = data["file_url"].rpartition("/")[2].partition(".")[0]
+        data["rating"] = (data["rating"] or "?")[0].lower()
+        data["tags"] = " ".join(
+            [tag.replace(" ", "_") for tag in data["tags"].split(", ")])
+        return data
+
+    def get_post_data_api(self, post_id):
+        """Request metadata of a single post from Gelbooru's API"""
+        return ET.fromstring(
+            self.request(self.api_url + "&id=" + post_id).text)[0].attrib
+
+
+class GelbooruTagExtractor(GelbooruExtractor):
+    """Extractor for images from gelbooru.com based on search-tags"""
+    subcategory = "tag"
+    directory_fmt = ["{category}", "{search_tags}"]
+    archive_fmt = "t_{search_tags}_{id}"
+    pattern = [r"(?:https?://)?(?:www\.)?gelbooru\.com/(?:index\.php)?"
+               r"\?page=post&s=list&tags=([^&]+)"]
+    test = [
+        ("https://gelbooru.com/index.php?page=post&s=list&tags=bonocho", {
+            "count": 5,
+        }),
+        ("https://gelbooru.com/index.php?page=post&s=list&tags=bonocho", {
+            "options": (("api", False),),
+            "count": 5,
+        }),
+    ]
+
+    def __init__(self, match):
+        GelbooruExtractor.__init__(self)
+        self.tags = text.unquote(match.group(1).replace("+", " "))
+        self.per_page = 100 if self.use_api else 42
+        self.start_page = 0
+
+    def skip(self, num):
+        pages, posts = divmod(num, self.per_page)
+        self.start_page += pages
+        self.start_post += posts
+        return num
+
+    def get_metadata(self):
+        return {"search_tags": self.tags}
+
+    def get_posts(self):
+        if self.use_api:
+            return self._get_posts_api()
+        return self._get_posts_manual()
+
+    def _get_posts_api(self):
+        params = {
+            # 'pid' is page-id; first page has index 0
+            "tags": self.tags, "limit": self.per_page, "pid": self.start_page}
+        while True:
+            root = ET.fromstring(
+                self.request(self.api_url, params=params).text)
+            for item in root:
+                yield item.attrib
+            if len(root) < self.per_page:
+                return
+            params["pid"] += 1
+
+    def _get_posts_manual(self):
+        url = "https://gelbooru.com/index.php?page=post&s=list"
+        # 'pid' is post-id; values for 'pid' must be multiples of 42
+        params = {"tags": self.tags, "pid": self.start_page * self.per_page}
+
+        while True:
+            page = self.request(url, params=params).text
+            ids = list(text.extract_iter(page, '<a id="p', '"'))
+            yield from ids
+            if len(ids) < self.per_page:
+                return
+            params["pid"] += self.per_page
+
+
+class GelbooruPoolExtractor(GelbooruExtractor):
+    """Extractor for image-pools from gelbooru.com"""
+    subcategory = "pool"
+    directory_fmt = ["{category}", "pool", "{pool}"]
+    archive_fmt = "p_{pool}_{id}"
+    pattern = [r"(?:https?://)?(?:www\.)?gelbooru\.com/(?:index\.php)?"
+               r"\?page=pool&s=show&id=(\d+)"]
+    test = [("https://gelbooru.com/index.php?page=pool&s=show&id=761", {
+        "count": 6,
+    })]
+
+    def __init__(self, match):
+        GelbooruExtractor.__init__(self)
+        self.pool_id = match.group(1)
+        self.posts = None
+
+    def get_metadata(self):
+        page = self.request("https://gelbooru.com/index.php?page=pool&s=show"
+                            "&id=" + self.pool_id).text
+        name, pos = text.extract(page, "<h3>Now Viewing: ", "</h3>")
+        self.posts = list(text.extract_iter(page, 'id="p', '"', pos))
+
+        if not name:
+            raise exception.NotFoundError("pool")
+
+        return {
+            "pool": text.parse_int(self.pool_id),
+            "pool_name": text.unescape(name),
+            "count": len(self.posts),
+        }
+
+    def get_posts(self):
+        return self.posts
+
+
+class GelbooruPostExtractor(GelbooruExtractor):
+    """Extractor for single images from gelbooru.com"""
+    subcategory = "post"
+    archive_fmt = "{id}"
+    pattern = [r"(?:https?://)?(?:www\.)?gelbooru\.com/(?:index\.php)?"
+               r"\?page=post&s=view&id=(\d+)"]
+    test = [("https://gelbooru.com/index.php?page=post&s=view&id=313638", {
+        "content": "5e255713cbf0a8e0801dc423563c34d896bb9229",
+        "count": 1,
+    })]
+
+    def __init__(self, match):
+        GelbooruExtractor.__init__(self)
+        self.post_id = match.group(1)
+
+    def get_posts(self):
+        return (self.post_id,)
---
+++ packages/gallery-dl/gallery_dl-1.4.1-py3-none-any.whl/gallery_dl/extractor/imagefap.py
@@ -0,0 +1,192 @@
+# -*- coding: utf-8 -*-
+
+# Copyright 2016-2018 Mike Fährmann
+#
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License version 2 as
+# published by the Free Software Foundation.
+
+"""Extract images from http://imagefap.com/"""
+
+from .common import Extractor, Message
+from .. import text
+import json
+
+
+class ImagefapExtractor(Extractor):
+    """Base class for imagefap extractors"""
+    category = "imagefap"
+    directory_fmt = ["{category}", "{gallery_id} {title}"]
+    filename_fmt = "{category}_{gallery_id}_{name}.{extension}"
+    archive_fmt = "{gallery_id}_{image_id}"
+
+
+class ImagefapGalleryExtractor(ImagefapExtractor):
+    """Extractor for image galleries from imagefap.com"""
+    subcategory = "gallery"
+    pattern = [(r"(?:https?://)?(?:www\.)?imagefap\.com/"
+                r"(?:gallery\.php\?gid=|gallery/|pictures/)(\d+)")]
+    test = [
+        ("http://www.imagefap.com/pictures/7102714", {
+            "url": "ffb1d58924fc9d6a0650006a96395a04454b85cf",
+            "keyword": "b65c310d75269cb6dcc32c3fc1bdcf39bea45342",
+            "content": "694a0a57385980a6f90fbc296cadcd6c11ba2dab",
+        }),
+        ("http://www.imagefap.com/gallery/5486966", {
+            "url": "eace9b33be99f87f3382c87bd915cf495a865d6e",
+            "keyword": "b84da0543c2d1f848bf5e4c2950dd4f4543a1e0c",
+        }),
+        ("http://www.imagefap.com/gallery.php?gid=7102714", None),
+    ]
+
+    def __init__(self, match):
+        ImagefapExtractor.__init__(self)
+        self.gid = match.group(1)
+        self.image_id = ""
+
+    def items(self):
+        url = "http://www.imagefap.com/pictures/" + self.gid + "/"
+        page = self.request(url).text
+        data = self.get_job_metadata(page)
+        yield Message.Version, 1
+        yield Message.Directory, data
+        for url, image in self.get_images():
+            data.update(image)
+            yield Message.Url, url, data
+
+    def get_job_metadata(self, page):
+        """Collect metadata for extractor-job"""
+        data, pos = text.extract_all(page, (
+            ("section" , '<meta name="description" content="', '"'),
+            ("title"   , '<title>Porn pics of ', ' (Page 1)</title>'),
+            ("uploader", '>Uploaded by ', '</font>'),
+            ("count"   , ' 1 of ', ' pics"'),
+        ), values={"gallery_id": self.gid})
+        self.image_id = text.extract(page, 'id="img_ed_', '"', pos)[0]
+        data["title"] = text.unescape(data["title"])
+        return data
+
+    def get_images(self):
+        """Collect image-urls and -metadata"""
+        num = 0
+        url = "http://www.imagefap.com/photo/" + self.image_id + "/"
+        params = {"gid": self.gid, "idx": 0, "partial": "true"}
+        while True:
+            pos = 0
+            page = self.request(url, params=params).text
+            for _ in range(24):
+                imgurl, pos = text.extract(page, '<a href="', '"', pos)
+                if not imgurl:
+                    return
+                num += 1
+                _, imgid, name = imgurl.rsplit("/", 2)
+                data = {"image_id": imgid, "num": num}
+                yield imgurl, text.nameext_from_url(name, data)
+            params["idx"] += 24
+
+
+class ImagefapImageExtractor(ImagefapExtractor):
+    """Extractor for single images from imagefap.com"""
+    subcategory = "image"
+    pattern = [r"(?:https?://)?(?:www\.)?imagefap\.com/photo/(\d+)"]
+    test = [("http://www.imagefap.com/photo/1369341772/", {
+        "url": "24cc4312e4a5084f39f1e35af5ba92e5f7c1ad3c",
+        "keyword": "26ae84575067b8231878ec1a2d1e14a0fbcea865",
+    })]
+
+    def __init__(self, match):
+        ImagefapExtractor.__init__(self)
+        self.image_id = match.group(1)
+
+    def items(self):
+        info = self.load_json()
+        data = self.get_job_metadata(info)
+        yield Message.Version, 1
+        yield Message.Directory, data
+        yield Message.Url, info["contentUrl"], data
+
+    def get_job_metadata(self, info):
+        """Collect metadata for extractor-job"""
+        parts = info["contentUrl"].rsplit("/", 3)
+        return text.nameext_from_url(parts[3], {
+            "title": text.unescape(info["name"]),
+            "section": info["section"],
+            "uploader": info["author"],
+            "date": info["datePublished"],
+            "width": info["width"],
+            "height": info["height"],
+            "gallery_id": parts[1],
+            "image_id": parts[2],
+        })
+
+    def load_json(self):
+        """Load the JSON dictionary associated with the image"""
+        url = "http://www.imagefap.com/photo/" + self.image_id + "/"
+        page = self.request(url).text
+        section  , pos = text.extract(
+            page, '<meta name="description" content="', '"'
+        )
+        json_data, pos = text.extract(
+            page, '<script type="application/ld+json">', '</script>', pos
+        )
+        json_dict = json.loads(json_data)
+        json_dict["section"] = section
+        return json_dict
+
+
+class ImagefapUserExtractor(ImagefapExtractor):
+    """Extractor for all galleries from a user at imagefap.com"""
+    subcategory = "user"
+    categorytransfer = True
+    pattern = [(r"(?:https?://)?(?:www\.)?imagefap\.com/"
+                r"profile(?:\.php\?user=|/)([^/]+)"),
+               (r"(?:https?://)?(?:www\.)?imagefap\.com/"
+                r"usergallery\.php\?userid=(\d+)")]
+    test = [("http://www.imagefap.com/profile/Mr%20Bad%20Example/galleries", {
+        "url": "145e98a8648c7695c150800ff8fd578ab26c28c1",
+    })]
+
+    def __init__(self, match):
+        ImagefapExtractor.__init__(self)
+        try:
+            self.user_id = int(match.group(1))
+            self.user = None
+        except ValueError:
+            self.user_id = None
+            self.user = match.group(1)
+
+    def items(self):
+        yield Message.Version, 1
+        for gid, name in self.get_gallery_data():
+            url = "http://www.imagefap.com/gallery/" + gid
+            data = {"gallery_id": text.parse_int(gid), "title": name}
+            yield Message.Queue, url, data
+
+    def get_gallery_data(self):
+        """Yield all gallery_ids of a specific user"""
+        folders = self.get_gallery_folders()
+        url = "http://www.imagefap.com/ajax_usergallery_folder.php"
+        params = {"userid": self.user_id}
+        for folder_id in folders:
+            params["id"] = folder_id
+            page = self.request(url, params=params).text
+
+            pos = 0
+            while True:
+                gid, pos = text.extract(page, '<a  href="/gallery/', '"', pos)
+                if not gid:
+                    break
+                name, pos = text.extract(page, "<b>", "<", pos)
+                yield gid, name
+
+    def get_gallery_folders(self):
+        """Create a list of all folder_ids of a specific user"""
+        if self.user:
+            url = "http://www.imagefap.com/profile/" + self.user + "/galleries"
+        else:
+            url = ("http://www.imagefap.com/usergallery.php?userid=" +
+                   str(self.user_id))
+        page = self.request(url).text
+        self.user_id, pos = text.extract(page, '?userid=', '"')
+        folders, pos = text.extract(page, ' id="tgl_all" value="', '"', pos)
+        return folders.split("|")[:-1]
