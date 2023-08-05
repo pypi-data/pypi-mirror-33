e6421d73be4301c3295c9abeefd14245228c9cf1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:29:44 +0000

    Add package pyfvs/0.0.13/pyfvs-0.0.13-cp27-cp27m-win_amd64.whl

---
+++ packages/pyfvs/pyfvs-0.0.13-cp27-cp27m-win_amd64.whl/pyfvs/_version.py
@@ -0,0 +1,6 @@
+# This file is automatically updated during packaging.
+# The version is extracted from the current Git tag using setup.py version.
+
+__version__ = '0.0.13'
+__git_describe__ = ''
+__git_commit__ = ''
