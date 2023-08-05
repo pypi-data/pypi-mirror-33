b52b01c5f0d7ff9228c0055cac882d5b34cdcbbc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 23:07:07 +0000

    Add package nester-k/1.2.0/nester_k-1.2.0.tar.gz

---
+++ packages/nester-k/nester_k-1.2.0.tar.gz/nester_k-1.2.0/setup.py
@@ -0,0 +1,9 @@
+from distutils.core import setup
+
+setup(
+	   name          =     'nester_k',
+	   version       =     '1.2.0',
+	   py_modules    =     ['nester'],
+	   author        =     'll',
+	   author_email  =     'll@ll.ll'
+)
