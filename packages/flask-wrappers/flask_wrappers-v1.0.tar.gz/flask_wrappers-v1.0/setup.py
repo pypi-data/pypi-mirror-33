2b0607c0df9f97c4d7ecd1c228ed6f7da728b0af
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 23:08:28 +0000

    Add package flask-wrappers/v1.0/flask_wrappers-v1.0.tar.gz

---
+++ packages/flask-wrappers/flask_wrappers-v1.0.tar.gz/flask_wrappers-v1.0/setup.py
@@ -0,0 +1,14 @@
+from distutils.core import setup
+
+setup(
+    name = 'flask_wrappers',
+    packages = ['flask_wrappers'],
+    version = 'v1.0',  # Ideally should be same as your GitHub release tag varsion
+    description = 'Decorators to facilitate flask enpoints implementation',
+    author = 'metadeta96',
+    author_email = 'metadeta96@gmail.com',
+    url = 'https://github.com/metadeta96/flask-wrappers',
+    download_url = 'https://github.com/metadeta96/flask-wrappers',
+    keywords = ['flask', 'decorator', 'route', 'wrapper'],
+    classifiers = [],
+)
