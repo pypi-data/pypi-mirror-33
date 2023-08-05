3cddd34c470c4cbee3258a263a19a62b43dcf4a2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:30:18 +0000

    Add package spada/1.21.18/spada-1.21.18-py3-none-any.whl

---
+++ packages/spada/spada-1.21.18-py3-none-any.whl/spada/methods/get_switches.py
@@ -0,0 +1,14 @@
+from spada.methods import method
+
+class GetSwitches(method.Method):
+	def __init__(self, gn_network, tx_network):
+		method.Method.__init__(self, __name__, gn_network, tx_network)
+		self._genes.flushSwitches()
+
+	def run(self, switchesFile):
+
+		self._genes.readSwitches(switchesFile, self._txs)
+		self._genes.saveNetwork("genes.pkl")
+
+if __name__ == '__main__':
+	pass
