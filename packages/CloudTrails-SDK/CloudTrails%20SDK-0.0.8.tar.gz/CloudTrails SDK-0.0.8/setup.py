3ae526db2f046688500515b233e77a0fa8707a60
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:23:58 +0000

    Add package CloudTrails-SDK/0.0.8/CloudTrails%20SDK-0.0.8.tar.gz

---
+++ packages/CloudTrails-SDK/CloudTrails%20SDK-0.0.8.tar.gz/CloudTrails SDK-0.0.8/setup.py
@@ -0,0 +1,21 @@
+import setuptools
+
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="CloudTrails SDK",
+    version="0.0.8",
+    author="Yaisel Hurtado, Raydel Miranda",
+    author_email="hurta2yaisel@gmail.com, raydel.miranda.gomez@gmail.com",
+    description="SDK for CloudTrails DevOps management",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://bitbucket.org/elasbit/fc-cloudtrails-sdk-py/src/develop/",
+    packages=setuptools.find_packages(),
+    classifiers=(
+        "Programming Language :: Python :: 2",
+        "Operating System :: OS Independent",
+    ),
+)
