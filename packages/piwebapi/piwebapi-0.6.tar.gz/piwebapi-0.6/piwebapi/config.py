a711dbcbd5e03eb63cae5196ebe2ce9a302a0eef
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:31:11 +0000

    Add package piwebapi/0.6/piwebapi-0.6.tar.gz

---
+++ packages/piwebapi/piwebapi-0.6.tar.gz/piwebapi-0.6/piwebapi/config.py
@@ -0,0 +1,9 @@
+# module level persistence
+global_persistence = 1000000
+# disable cache
+enable_cache = True
+# webapi server address
+webapi_server = "http://10.10.143.28/piwebapi/"
+#webapi_server = "https://www.abelheinsbroek.nl/piwebapi/"
+#auth_config = ('admin', 'spic-pi-loto-diz')
+auth_config = ('','')
