355c26672307a8daf91397611ca6512cacd8b565
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:08:15 +0000

    Add package pyabeles/0.1.dev0/pyabeles-0.1.dev0.tar.gz

---
+++ packages/pyabeles/pyabeles-0.1.dev0.tar.gz/pyabeles-0.1.dev0/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(
+    name='pyabeles',
+    version='0.1dev',
+    author='Miro Furtado',
+    author_email="furtadomiro@gmail.com",
+    license='Apache License 2.0',
+)
