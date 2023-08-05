259172ae5b57b0e913da81bfe1a127b2550ea9c3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:29:41 +0000

    Add package geomdl/4.0b4/geomdl-4.0b4-py2.py3-none-any.whl

---
+++ packages/geomdl/geomdl-4.0b4-py2.py3-none-any.whl/geomdl/shapes/__init__.py
@@ -0,0 +1,12 @@
+""" NURBS-Python Shapes Component
+
+.. moduleauthor:: Onur Rauf Bingol <orbingol@gmail.com>
+
+"""
+
+__author__ = "Onur Rauf Bingol"
+__version__ = "1.0.0"
+__license__ = "MIT"
+
+
+from geomdl import NURBS
