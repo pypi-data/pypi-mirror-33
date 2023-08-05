5495471481dac87d380898c44a848bd41ed8d6d6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:15:41 +0000

    Add package psyshort/0.1.3/psyshort-0.1.3.tar.gz

---
+++ packages/psyshort/psyshort-0.1.3.tar.gz/psyshort-0.1.3/setup.py
@@ -0,0 +1,19 @@
+import setuptools
+
+with open("README.md", "rb") as f:
+    long_description = f.read().decode("utf-8")
+    
+setuptools.setup(
+    name="psyshort",
+    version="0.1.3",
+    author="Quit3Simpl3",
+    description="Making psycopg2 a little simpler to use.",
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    url="https://github.com/Quit3Simpl3/psyshort",
+    packages=setuptools.find_packages(),
+    classifiers=(
+        "Programming Language :: Python :: 3",
+        )
+    )
+
