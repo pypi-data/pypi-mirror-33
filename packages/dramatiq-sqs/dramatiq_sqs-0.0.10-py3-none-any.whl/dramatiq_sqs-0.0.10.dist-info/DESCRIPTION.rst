511c0778a77cfed38082d3bd668ecab831e2f76b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:07:01 +0000

    Add package dramatiq-sqs/0.0.10/dramatiq_sqs-0.0.10-py3-none-any.whl

---
+++ packages/dramatiq-sqs/dramatiq_sqs-0.0.10-py3-none-any.whl/dramatiq_sqs-0.0.10.dist-info/DESCRIPTION.rst
@@ -0,0 +1,3 @@
+Visit https://github.com/Bogdanp/dramatiq_sqs for more information.
+
+
---
+++ packages/dramatiq-sqs/dramatiq_sqs-0.0.10-py3-none-any.whl/dramatiq_sqs/__init__.py
@@ -0,0 +1,8 @@
+from .broker import SQSBroker
+
+__version__ = "0.0.10"
+
+__all__ = [
+    "SQSBroker",
+    "__version__",
+]
