8d954acc4ed210ffa1836f22eca08420589d453b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:31:03 +0000

    Add package flask-google-actions/1.0.0/flask-google-actions-1.0.0.tar.gz

---
+++ packages/flask-google-actions/flask-google-actions-1.0.0.tar.gz/flask-google-actions-1.0.0/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='flask-google-actions',
+      version='1.0.0',
+      description='Client library for Actions on Google using python and flask',
+      url='https://github.com/caycewilliams/flask-actions-on-google-python',
+      author='Cayce Williams',
+      author_email='caycewilliams77@gmail.com',
+      license='MIT',
+      packages=['flaskactions'],
+      zip_safe=False)
