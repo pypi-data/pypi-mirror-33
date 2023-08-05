de6f1d6d06c2c4a30c8b28504fbc2d01bb93635f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 23:08:28 +0000

    Add package TFFactory/0.1.5001/TFFactory-0.1.5001.tar.gz

---
+++ packages/TFFactory/TFFactory-0.1.5001.tar.gz/TFFactory-0.1.5001/TFFactory/DataLoader.py
@@ -0,0 +1,31 @@
+import numpy as np
+
+class DataLoader(object):
+    def __init__(self):
+        self.FileCache = {}
+        return
+
+    def sampleFile(self, filePath, nRows, caching = True):
+        data = []
+        if caching:
+            if filePath in self.FileCache:
+                for i in np.random.randint(0, len(self.FileCache[filePath]), nRows):
+                    data.append(self.FileCache[filePath][i])
+                return data
+            else:
+                self.FileCache[filePath] = []
+        with open(filePath, 'r') as fin:
+            for i, line in enumerate(fin):
+                row = line.strip()
+                if caching:
+                    self.FileCache[filePath].append(row)
+                if i < nRows:
+                    data.append(row)
+                else:
+                    r = np.random.randint(0, i+1)
+                    if r < nRows:
+                        data[r] = row
+        self.FileCache[filePath] = data
+        return data
+
+
