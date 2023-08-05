f1ae70ee1dcb513312f312f37b462f30db873648
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:27:57 +0000

    Add package kfw/0.0.1a1/kfw-0.0.1a1.tar.gz

---
+++ packages/kfw/kfw-0.0.1a1.tar.gz/kfw-0.0.1a1/README.md
@@ -0,0 +1,4 @@
+# keras framework wrapper 
+
+This is a simple keras framework wrapper package. You can use
+[Github-flavored Markdown](https://gitlab.com/hebinhao1993/kfw)
---
+++ packages/kfw/kfw-0.0.1a1.tar.gz/kfw-0.0.1a1/setup.py
@@ -0,0 +1,21 @@
+import setuptools
+
+with open("README.md", "r") as f:
+    long_description = f.read()
+
+setuptools.setup(
+    name="kfw",
+    version="0.0.1a1",
+    author="hebinhao",
+    author_email="hebinhao1993@outlook.com",
+    description="keras framework wrapper",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://gitlab.com/hebinhao1993/kfw",
+    packages=setuptools.find_packages(),
+    classifiers=(
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: GNU General Public License v3 (GPLv3)",
+        "Operating System :: OS Independent",
+    ),
+)
