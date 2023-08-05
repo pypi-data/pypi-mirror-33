4f0e3ef3b1afc95ce6b7d87cb9c2154c55e4b3d7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:04:50 +0000

    Add package doodle-dashboard-display-console/0.0.10/doodle-dashboard-display-console-0.0.10.tar.gz

---
+++ packages/doodle-dashboard-display-console/doodle-dashboard-display-console-0.0.10.tar.gz/doodle-dashboard-display-console-0.0.10/sketchingdev/console.py
@@ -0,0 +1,25 @@
+import click
+from doodledashboarddisplay import Display
+from doodledashboarddisplay.display import CanWriteText, CanDrawImage
+
+
+class ConsoleDisplay(Display, CanWriteText, CanDrawImage):
+
+    @staticmethod
+    def get_id():
+        return "console"
+
+    def clear(self):
+        click.clear()
+
+    def write_text(self, text):
+        click.echo(text)
+
+    def draw_image(self, image_path):
+        click.echo("One day I'll draw an ASCII version of %s" % image_path)
+
+    def _get_size(self):
+        return click.get_terminal_size()
+
+    def __str__(self):
+        return "Console display"
