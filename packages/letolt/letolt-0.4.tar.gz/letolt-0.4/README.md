b03274adbe262458dc7b1fb244449103c22b2b7d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:24:34 +0000

    Add package letolt/0.4/letolt-0.4.tar.gz

---
+++ packages/letolt/letolt-0.4.tar.gz/letolt-0.4/README.md
@@ -0,0 +1,9 @@
+# `Letolt`
+Csinaltam egy [Android alkalmazast](https://github.com/MatyiFKBT/ErettsegiDroid), valamint egy Python alapu [Windows programot](https://github.com/MatyiFKBT/erettsegi), melyek az oktatas.hu -rol valo letoltest teszik egyszerubbe a felhasznalo szamara, ez a `letolt` fuggveny tartalmazza a program magjat.
+## Telepites
+`pip install letolt`
+## Hasznalat, pelda
+```
+import letolt
+letolt(15,'okt','emelt','inf')
+```
---
+++ packages/letolt/letolt-0.4.tar.gz/letolt-0.4/setup.py
@@ -0,0 +1,27 @@
+try:
+    from setuptools import setup
+except ImportError:
+    from distutils.core import setup
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+   
+setup(
+  name = 'letolt',
+  packages = ['letolt'],
+  version = '0.4',
+  description = 'Erettsegi letolto fuggveny 4 input alapjan.',
+  long_description=long_description,
+  long_description_content_type="text/markdown",
+  author = 'Matyi',
+  author_email = 'mmatyi1999@gmail.com',
+  url = 'https://github.com/MatyiFKBT/letolt/',
+  download_url = 'https://github.com/MatyiFKBT/letolt/archive/0.1.tar.gz',
+  keywords = ['testing', 'erettsegi', 'letolt'],
+  #packages=setuptools.find_packages(),
+  classifiers=(
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ),
+)
