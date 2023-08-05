f5b17f9f5184f89fbaf000a10cdfcde78b70a303
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:09:58 +0000

    Add package nardis/0.0.1/nardis-0.0.1-py3-none-any.whl

---
+++ packages/nardis/nardis-0.0.1-py3-none-any.whl/nardis/asgi.py
@@ -0,0 +1,42 @@
+import re
+from nardis.http import Request, Response
+from nardis.utils import encode_string
+
+
+class HTTPHandler:
+    def __init__(self, scope, matchers):
+        self.scope = scope
+        self.request = Request(scope, b'')
+        self.matchers = matchers
+
+    async def wait_request(self, receive, send):
+        while True:
+            msg = await receive()
+            if msg['type'] == 'http.disconnect':
+                break
+            self.request.append_body(msg['body'])
+            if not msg.get('more_body', False):
+                self.request.mark_complete()
+                break
+
+    async def __call__(self, receive, send):
+        await self.wait_request(receive, send)
+        resp = Response(send)
+        for matcher in self.matchers:
+            if matcher.match(self.request):
+                await matcher.dispatch(self.request, resp)
+                break
+        else:
+            resp.status(404)
+            await resp.send("not found")
+
+
+CONSUMERS = {
+    'http': HTTPHandler,
+}
+
+
+def main(urls: list):
+    def application(scope: dict):
+        return CONSUMERS[scope['type']](scope, urls)
+    return application
