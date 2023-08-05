f6d53b4968300621970d0e9aa582f51414dfc92a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:31:03 +0000

    Add package ie_2018_groupB_yahoo_ks_datareader/4.2/ie_2018_groupB_yahoo_ks_datareader-4.2.tar.gz

---
+++ packages/ie_2018_groupB_yahoo_ks_datareader/ie_2018_groupB_yahoo_ks_datareader-4.2.tar.gz/ie_2018_groupB_yahoo_ks_datareader-4.2/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='ie_2018_groupB_yahoo_ks_datareader',
+      version='4.2',  # Development release
+      description='Python Packaging Starting Kit - Simple Structure to creater your own package - for training purposes!',
+      url='https://github.com/RimaNakhala/ie_2018_groupB_yahoo_ks_datareader',
+      author='rimanakhala',
+      author_email='rima.nakhala@student.ie.edu',
+      license='MIT',
+          packages=['ie_2018_groupB_yahoo_ks_datareader'],
+      zip_safe=False)
