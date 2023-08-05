a81d2c6de5a5b021653e1463a4f28999ef6078b5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:13:31 +0000

    Add package simple_model/1.3.0.post3/simple_model-1.3.0.post3-py3-none-any.whl

---
+++ packages/simple_model/simple_model-1.3.0.post3-py3-none-any.whl/simple_model/__init__.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+"""
+.. module:: simple_model
+   :platform: Unix
+   :synopsis: Simple models for easy data (de-)serialization and validation.
+
+.. moduleauthor:: Aljosha Friemann a.friemann@automate.wtf
+"""
+
+__version__ = '1.3.0.post3'
+
+from simple_model.helpers import list_type, one_of  # noqa: F401
+from simple_model.v1 import Model, Attribute  # noqa: F401
+
+# vim: tabstop=8 expandtab shiftwidth=4 softtabstop=4 fenc=utf-8
