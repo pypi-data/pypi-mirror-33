da89c4b7c5fb9cda237f02f987a1fecbdd06208d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 23:09:35 +0000

    Add package openapi3/0.0.1/openapi3-0.0.1.tar.gz

---
+++ packages/openapi3/openapi3-0.0.1.tar.gz/openapi3-0.0.1/openapi3/__main__.py
@@ -0,0 +1,26 @@
+import sys
+import yaml
+
+from .openapi import OpenAPI
+
+def main():
+    specfile = sys.argv[1]
+
+    with open(specfile) as f:
+        spec = yaml.safe_load(f.read())
+
+    o = OpenAPI(spec, validate=True)
+
+    errors = o.errors()
+
+    if errors:
+        # print errors
+        for e in errors:
+            print('{}: {}'.format('.'.join(e.path), e.message[:300]))
+        print()
+        print("{} errors".format(len(errors)))
+    else:
+        print("OK")
+
+if __name__ == '__main__':
+    main()
---
+++ packages/openapi3/openapi3-0.0.1.tar.gz/openapi3-0.0.1/openapi3/servers.py
@@ -0,0 +1,36 @@
+from .object_base import ObjectBase
+
+class Server(ObjectBase):
+    """
+    The Server object, as described `here`_
+
+    .. _here: https://github.com/OAI/OpenAPI-Specification/blob/master/versions/3.0.1.md#serverObject
+    """
+    __slots__ = ['url','description','variables']
+    required_fields=['url']
+
+    def _parse_data(self):
+        """
+        Implementation of :any:`ObjectBase._parse_data`
+        """
+        self.url = self._get('url', str)
+        self.description = self._get('description', str)
+        self.variables = self._get('variables', ['ServerVariable'], is_map=True)
+
+
+class ServerVariable(ObjectBase):
+    """
+    A ServerVariable object as defined `here`_.
+
+    .. _here: https://github.com/OAI/OpenAPI-Specification/blob/master/versions/3.0.1.md#server-variable-object
+    """
+    __slots__ = ['enum','default','description']
+    required_fields = ['default']
+
+    def _parse_data(self):
+        """
+        Implementation of :any:`ObjectBase._parse_data`
+        """
+        self.enum = self._get('enum', [str], is_list=True)
+        self.default = self._get('default', str)
+        self.description = self._get('description', str)
