51df0997a6537a22644432ecde3fe03bd9c7e669
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:29:19 +0000

    Add package geomdl/4.0b5/geomdl-4.0b5-py2.py3-none-any.whl

---
+++ packages/geomdl/geomdl-4.0b5-py2.py3-none-any.whl/geomdl/__init__.py
@@ -0,0 +1,21 @@
+""" NURBS & B-Spline 2D/3D curve and surface evaluation library in pure Python
+
+.. moduleauthor:: Onur Rauf Bingol <orbingol@gmail.com>
+
+"""
+
+__author__ = "Onur Rauf Bingol"
+__version__ = "4.0b5"
+__license__ = "MIT"
+
+
+import os
+import sys
+import abc
+import math
+from array import array
+import random
+import copy
+import warnings
+import struct
+import pickle
