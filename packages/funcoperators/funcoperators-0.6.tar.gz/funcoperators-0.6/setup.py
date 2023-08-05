b463a89ba8d5f18ce2d7b46352ed79ab2dd9905a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 23:09:25 +0000

    Add package funcoperators/0.6/funcoperators-0.6.tar.gz

---
+++ packages/funcoperators/funcoperators-0.6.tar.gz/funcoperators-0.6/setup.py
@@ -0,0 +1,16 @@
+from distutils.core import setup
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setup(
+    name='funcoperators',
+    version='0.6',
+    description='Allow infix function notation like (1,2) /dot/ (3,4) for dot((1,2), (3,4) or 1 /frac/ 3 for frac(1,3) and other useful operators to functions.',
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    author='Robert Vanden Eynde',
+    author_email='robertvandeneynde@hotmail.com',
+    packages=['funcoperators'], # 'mymath.adv'
+    url='https://github.com/robertvandeneynde/python',
+)
