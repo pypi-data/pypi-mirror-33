9bef783251cfaf8cbc340647700a1c42be374600
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 23:09:32 +0000

    Add package TBWW/0.2.10/TBWW-0.2.10.tar.gz

---
+++ packages/TBWW/TBWW-0.2.10.tar.gz/TBWW-0.2.10/setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(
+    name="TBWW",
+    version="0.2.10",
+    description="Telegram Bot Wrapper Wraper",
+    license="GNU GPL 3.0",
+    install_requires=["python-telegram-bot"],
+    py_modules = ["tbww"]
+)
