9b9e56d460fbfd3a0361e2dd879d48b2c4c816c5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:08:27 +0000

    Add package arxiv-submission-events/0.2.4/arxiv-submission-events-0.2.4.tar.gz

---
+++ packages/arxiv-submission-events/arxiv-submission-events-0.2.4.tar.gz/arxiv-submission-events-0.2.4/setup.py
@@ -0,0 +1,10 @@
+"""Install arXiv submission event core package."""
+
+from setuptools import setup, find_packages
+
+setup(
+    name='arxiv-submission-events',
+    version='0.2.4',
+    packages=find_packages(exclude=['test*']),
+    zip_safe=False
+)
