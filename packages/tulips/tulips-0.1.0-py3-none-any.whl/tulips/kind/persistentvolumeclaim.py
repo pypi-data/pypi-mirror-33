1fe7a52f6fe697a12dfd07129023ae8534661988
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 23:06:38 +0000

    Add package tulips/0.1.0/tulips-0.1.0-py3-none-any.whl

---
+++ packages/tulips/tulips-0.1.0-py3-none-any.whl/tulips/kind/persistentvolumeclaim.py
@@ -0,0 +1,26 @@
+import typing as t
+
+import yaml
+from kubernetes import client as k8s
+
+from . import Kind
+
+
+class PersistentVolumeClaim(Kind):
+    def __init__(self, client: k8s.ApiClient, namespace: str) -> None:
+        self.client = client
+        self.namespace: str = namespace
+
+    def delete(self, name: t.AnyStr, body: k8s.V1DeleteOptions):
+        return k8s.CoreV1Api(
+            self.client
+        ).delete_namespaced_persistent_volume_claim(
+            body=body, namespace=self.namespace, name=name
+        )
+
+    def create(self, body: yaml.YAMLObject):
+        return k8s.CoreV1Api(
+            self.client
+        ).create_namespaced_persistent_volume_claim(
+            body=body, namespace=self.namespace
+        )
