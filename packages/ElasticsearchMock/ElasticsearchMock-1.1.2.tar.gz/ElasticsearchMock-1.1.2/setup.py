72fc221b0ddd3a1032f3fa8ded62ebb77fb77f90
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:31:16 +0000

    Add package ElasticsearchMock/1.1.2/ElasticsearchMock-1.1.2.tar.gz

---
+++ packages/ElasticsearchMock/ElasticsearchMock-1.1.2.tar.gz/ElasticsearchMock-1.1.2/setup.py
@@ -0,0 +1,30 @@
+# -*- coding: utf-8 -*-
+
+from setuptools import setup, find_packages
+
+__version__ = '1.1.2'
+
+setup(
+    name='ElasticsearchMock',
+    version=__version__,
+    url='https://github.com/jmlw/elasticmock',
+    license='MIT',
+    author='Josh Wood',
+    author_email='josh.m.l.wood@gmail.com',
+    description='Python Elasticsearch Mock for test purposes',
+    packages=find_packages(exclude=['tests']),
+    zip_safe=False,
+    include_package_data=True,
+    platforms='any',
+    install_requires=[
+        'elasticsearch>=5.0.0,<6.0.0',
+        'mock<=1.0.1'
+    ],
+    classifiers=[
+        'Environment :: Web Environment',
+        'Intended Audience :: Developers',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Topic :: Software Development :: Libraries :: Python Modules'
+    ]
+)
