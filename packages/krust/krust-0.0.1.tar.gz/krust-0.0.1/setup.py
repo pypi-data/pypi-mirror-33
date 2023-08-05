31643ff4956ed318caba6501786fcf11eaa70df9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:31:27 +0000

    Add package krust/0.0.1/krust-0.0.1.tar.gz

---
+++ packages/krust/krust-0.0.1.tar.gz/krust-0.0.1/setup.py
@@ -0,0 +1,23 @@
+# -*- coding:utf-8 -*-
+
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+
+setuptools.setup(
+    name="krust",
+    version="0.0.1",
+    author="claydodo and his little friends (xiao huo ban)",
+    author_email="claydodo@foxmail.com",
+    description="Shell utils",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/claydodo/krust",
+    packages=setuptools.find_packages(),
+    classifiers=(
+        "Programming Language :: Python :: 2.7 ",
+        "Operating System :: OS Independent",
+    ),
+)
