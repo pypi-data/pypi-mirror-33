4c14d8ae8736a7baf9e35789f1d7a806ee52e4dd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:04:42 +0000

    Add package pytest-djangoapp/0.3.1/pytest_djangoapp-0.3.1-py2.py3-none-any.whl

---
+++ packages/pytest-djangoapp/pytest_djangoapp-0.3.1-py2.py3-none-any.whl/pytest_djangoapp/tests/test_fixtures_request.py
@@ -0,0 +1,22 @@
+from django.urls import reverse
+
+
+def test_request_factory(request_factory):
+    assert request_factory()
+
+
+def test_request_get(request_get, user_create):
+    assert request_get(user=user_create())
+
+
+def test_request_post(request_post, user_create):
+    assert request_post(data={'a': 'b'}, user=user_create())
+
+
+def test_request_client(request_client):
+    client = request_client()
+    assert client
+
+    response = client.get(reverse('index'))
+
+    assert response.status_code == 200
