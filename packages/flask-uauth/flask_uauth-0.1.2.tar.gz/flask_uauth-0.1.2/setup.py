04ca6efca000c0425a9e3a7020994e1828dbac9c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:05:51 +0000

    Add package flask-uauth/0.1.2/flask_uauth-0.1.2.tar.gz

---
+++ packages/flask-uauth/flask_uauth-0.1.2.tar.gz/flask_uauth-0.1.2/setup.py
@@ -0,0 +1,30 @@
+from setuptools import setup, find_packages
+
+
+def get_long_description():
+    with open("README.md", "r") as f:
+        return f.read()
+
+
+setup(
+    name="flask_uauth",
+    version="0.1.2",
+    author="Panagiotis Matigakis",
+    author_email="pmatigakis@gmail.com",
+    description="Simple authentication for Flask REST apis",
+    long_description=get_long_description(),
+    long_description_content_type="text/markdown",
+    url="https://github.com/pmatigakis/flask-uauth",
+    packages=find_packages(exclude=["tests"]),
+    install_requires=[
+        "Flask>=0.11.1"
+    ],
+    tests_require=["nose"],
+    test_suite="nose.collector",
+    zip_safe=True,
+    classifiers=(
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    )
+)
