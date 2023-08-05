32406301ddee0e3865867387d9b2d7217ad20618
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 23:06:20 +0000

    Add package odoo9-addon-auth-session-timeout/9.0.1.0.0.99.dev11/odoo9_addon_auth_session_timeout-9.0.1.0.0.99.dev11-py2-none-any.whl

---
+++ packages/odoo9-addon-auth-session-timeout/odoo9_addon_auth_session_timeout-9.0.1.0.0.99.dev11-py2-none-any.whl/odoo_addons/auth_session_timeout/models/ir_config_parameter.py
@@ -0,0 +1,45 @@
+# -*- coding: utf-8 -*-
+# (c) 2015 ACSONE SA/NV, Dhinesh D
+
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
+
+from openerp import models, api, tools, SUPERUSER_ID
+
+DELAY_KEY = 'inactive_session_time_out_delay'
+IGNORED_PATH_KEY = 'inactive_session_time_out_ignored_url'
+
+
+class IrConfigParameter(models.Model):
+    _inherit = 'ir.config_parameter'
+
+    @tools.ormcache('db')
+    def get_session_parameters(self, db):
+        param_model = self.pool['ir.config_parameter']
+        cr = self.pool.cursor()
+        delay = False
+        urls = []
+        try:
+            delay = int(param_model.get_param(
+                cr, SUPERUSER_ID, DELAY_KEY, 7200))
+            urls = param_model.get_param(
+                cr, SUPERUSER_ID, IGNORED_PATH_KEY, '').split(',')
+        finally:
+            cr.close()
+        return delay, urls
+
+    def _auth_timeout_get_parameter_delay(self):
+        delay, urls = self.get_session_parameters(self.pool.db_name)
+        return delay
+
+    def _auth_timeout_get_parameter_ignoredurls(self):
+        delay, urls = self.get_session_parameters(self.pool.db_name)
+        return urls
+
+    @api.multi
+    def write(self, vals, context=None):
+        res = super(IrConfigParameter, self).write(vals)
+        if self.key == DELAY_KEY:
+            self.get_session_parameters.clear_cache(self)
+        elif self.key == IGNORED_PATH_KEY:
+            self.get_session_parameters.clear_cache(self)
+        return res
