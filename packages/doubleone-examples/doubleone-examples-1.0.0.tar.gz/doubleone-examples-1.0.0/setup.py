ead1bff19087a2400283a9bbff2fca72c1e7b15c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:24:16 +0000

    Add package doubleone-examples/1.0.0/doubleone-examples-1.0.0.tar.gz

---
+++ packages/doubleone-examples/doubleone-examples-1.0.0.tar.gz/doubleone-examples-1.0.0/setup.py
@@ -0,0 +1,20 @@
+"""from distutils.core import setup"""
+import setuptools 
+
+setuptools.setup(
+    name="doubleone-examples",
+    version="1.0.0",
+    author="Example Author",
+    description="A small example package",
+    
+)
+"""
+setup(
+name = 'doubleone-examples',
+version = '1.1.0',
+py_modules = ['nester'],
+author = '11',
+author_email = 'doubleone@outlook.com',
+url = 'http//www.11.com',
+description = 'A simple printer of nested list'
+)"""
