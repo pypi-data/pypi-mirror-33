0c02ef743f035301ccae82926526aa894c2e7647
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:30:59 +0000

    Add package filetracker/2.1.2/filetracker-2.1.2.tar.gz

---
+++ packages/filetracker/filetracker-2.1.2.tar.gz/filetracker-2.1.2/setup.py
@@ -0,0 +1,41 @@
+from setuptools import setup, find_packages
+setup(
+    name = 'filetracker',
+    version = '2.1.2',
+    author = 'SIO2 Project Team',
+    author_email = 'sio2@sio2project.mimuw.edu.pl',
+    description = 'Filetracker caching file storage',
+    url = 'https://github.com/sio2project/filetracker',
+    license = 'GPL',
+
+    packages = find_packages(),
+
+    install_requires = [
+        'bsddb3',
+        'flup6',
+        'gunicorn==19.8.1',
+        'gevent==1.3.1',
+        'progressbar2',
+        'requests',
+        'six',
+    ],
+
+    setup_requires = [
+        'pytest-runner',
+    ],
+
+    tests_require = [
+        'pytest',
+    ],
+
+    entry_points = {
+        'console_scripts': [
+            'filetracker = filetracker.client.shell:main',
+            'filetracker-server = filetracker.servers.run:main',
+            'filetracker-cache-cleaner = filetracker.scripts.cachecleaner:main',
+            'filetracker-migrate = filetracker.scripts.migrate:main',
+            'filetracker-recover = filetracker.scripts.recover:main',
+        ],
+    }
+)
+
