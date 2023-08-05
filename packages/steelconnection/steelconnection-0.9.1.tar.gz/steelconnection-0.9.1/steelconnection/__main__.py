8af6a7e10c0e2fb04c3acf25b08bda13ebdf9341
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 23:06:29 +0000

    Add package steelconnection/0.9.1/steelconnection-0.9.1.tar.gz

---
+++ packages/steelconnection/steelconnection-0.9.1.tar.gz/steelconnection-0.9.1/steelconnection/__main__.py
@@ -0,0 +1,10 @@
+from __future__ import absolute_import
+from __future__ import division
+from __future__ import print_function
+
+from .__version__ import __url__, __version__
+
+
+if __name__ == '__main__':
+    print('SteelConnection version:', __version__)
+    print('Documentation available at', __url__)
