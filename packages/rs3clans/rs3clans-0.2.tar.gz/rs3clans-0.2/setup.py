4bb1d46f54a259be6b247e8682ba43560b1aabc1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:29:29 +0000

    Add package rs3clans/0.2/rs3clans-0.2.tar.gz

---
+++ packages/rs3clans/rs3clans-0.2.tar.gz/rs3clans-0.2/setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup
+
+setup(
+    name="rs3clans",
+    description='A Python 3 module wrapper for RuneScape 3 Clan\'s API',
+    long_description_content_type='Markdown',
+    version='0.2',
+    author='John Victor',
+    author_email='johnvictorfs@gmail.com',
+    license='MIT',
+    packages=['rs3clans'],
+    zip_safe=False,
+    classifiers = (
+        "Programming Language :: Python :: 3"
+    )
+)
