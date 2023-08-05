1b0bbdf8ba73657ed6e4accd8f9b2a6ff8e1632e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:04:52 +0000

    Add package tinytext/2.0.0/tinytext-2.0.0.tar.gz

---
+++ packages/tinytext/tinytext-2.0.0.tar.gz/tinytext-2.0.0/README.md
@@ -0,0 +1,32 @@
+tinytext
+========
+
+[![PyPI](https://img.shields.io/pypi/v/tinytext.svg)](https://pypi.org/project/tinytext)
+[![Python versions](https://img.shields.io/pypi/pyversions/tinytext.svg)](https://pypi.org/project/tinytext)
+[![Build Status](https://travis-ci.org/hugovk/tinytext.svg?branch=master)](https://travis-ci.org/hugovk/tinytext)
+[![codecov](https://codecov.io/gh/hugovk/tinytext/branch/master/graph/badge.svg)](https://codecov.io/gh/hugovk/tinytext)
+
+Convert your text ᶦᶰᵗᵒ ᵗᶦᶰᶦᵉʳ ᵗᵉˣᵗ
+
+Helpful for all your tiny cute needs.
+
+Especially helpful for cute bot needs.
+
+A Python port of [Rachel White's tiny-text for Node](https://github.com/rachelnicole/tiny-text).
+
+How to use
+----------
+
+Install the package into your lovely codebase.
+
+```bash
+pip install tinytext
+```
+
+```python
+from tinytext import tinytext
+
+print(tinytext("hello friend"))
+```
+
+And that's it!
---
+++ packages/tinytext/tinytext-2.0.0.tar.gz/tinytext-2.0.0/setup.py
@@ -0,0 +1,39 @@
+# encoding: utf-8
+from setuptools import setup
+
+with open("README.md") as f:
+    long_description = f.read()
+
+setup(
+    name="tinytext",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    version="2.0.0",
+    packages=["tinytext"],
+    author="hugovk",
+    description="A helpful text converter to change any normal text into "
+                "cuter tinier text",
+    url="https://github.com/hugovk/tinytext",
+    keywords=["botally", "tiny type", "tiny type", "tiny text", "cute text",
+              "generator"],
+    python_requires=">=3.4",
+    classifiers=[
+        "Development Status :: 3 - Alpha",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: MIT License",
+        "Natural Language :: English",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.4",
+        "Programming Language :: Python :: 3.5",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3 :: Only",
+        "Programming Language :: Python :: Implementation :: CPython",
+        "Programming Language :: Python :: Implementation :: PyPy",
+        "Topic :: Artistic Software",
+        "Topic :: Text Processing",
+    ],
+    test_suite="tests",
+)
+
+# End of file
