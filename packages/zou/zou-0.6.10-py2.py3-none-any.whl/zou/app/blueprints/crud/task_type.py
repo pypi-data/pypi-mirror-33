947cf2ed2ad99c50f85a296dd5d229484fa87697
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:31:24 +0000

    Add package zou/0.6.10/zou-0.6.10-py2.py3-none-any.whl

---
+++ packages/zou/zou-0.6.10-py2.py3-none-any.whl/zou/app/blueprints/crud/task_type.py
@@ -0,0 +1,39 @@
+from zou.app.models.task_type import TaskType
+from zou.app.services.exception import ArgumentsException
+
+from .base import BaseModelResource, BaseModelsResource
+
+
+class TaskTypesResource(BaseModelsResource):
+
+    def __init__(self):
+        BaseModelsResource.__init__(self, TaskType)
+
+    def check_read_permissions(self):
+        return True
+
+    def update_data(self, data):
+        name = data.get("name", None)
+        task_type = TaskType.get_by(name=name)
+        if task_type is not None:
+            raise ArgumentsException("A task with similar name already exists")
+        return data
+
+
+class TaskTypeResource(BaseModelResource):
+
+    def __init__(self):
+        BaseModelResource.__init__(self, TaskType)
+
+    def check_read_permissions(self, instance):
+        return True
+
+    def update_data(self, data):
+        name = data.get("name", None)
+        if name is not None:
+            task_type = TaskType.get_by(name=name)
+            if task_type is not None:
+                raise ArgumentsException(
+                    "A task with similar name already exists"
+                )
+        return data
