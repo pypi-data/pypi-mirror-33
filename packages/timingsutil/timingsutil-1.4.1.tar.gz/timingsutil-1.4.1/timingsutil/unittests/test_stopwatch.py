750114a2ddb2351cd28aad8ba6263e3249831958
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:08:16 +0000

    Add package timingsutil/1.4.1/timingsutil-1.4.1.tar.gz

---
+++ packages/timingsutil/timingsutil-1.4.1.tar.gz/timingsutil-1.4.1/timingsutil/unittests/test_stopwatch.py
@@ -0,0 +1,31 @@
+# encoding: utf-8
+
+import time
+import unittest
+import datetime
+from timingsutil.timers import Timeout, Throttle, Stopwatch
+import logging_helper
+logging = logging_helper.setup_logging()
+
+
+class TestConfiguration(unittest.TestCase):
+
+    def setUp(self):
+        pass
+
+    def tearDown(self):
+        pass
+
+    def test_stopwatch(self):
+
+        stopwatch = Stopwatch()
+
+        for _ in range(3):
+            time.sleep(1)
+            self.assertEqual(round(stopwatch.lap()), 1)
+
+        self.assertEqual(round(stopwatch.stop()), 3)
+
+
+if __name__ == u'__main__':
+    unittest.main()
