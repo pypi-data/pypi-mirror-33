9226f670857e857a62fdc6b39f8b164c4232673b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 23:06:58 +0000

    Add package auto-pack/1.2.5/auto_pack-1.2.5.tar.gz

---
+++ packages/auto-pack/auto_pack-1.2.5.tar.gz/auto_pack-1.2.5/auto_pack/__init__.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+__version__ = '1.2.5'
+
+from .producer import FlavorApkProducer
+import checker
+
+
+def fromFile(filePath):
+    return FlavorApkProducer().fromFile(filePath)
+
+
+def reportApkFilesInfo(destFiles):
+    checker.reportApkFilesInfo(destFiles)
