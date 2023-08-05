e263b72316a00803cf6724eea6636b2868f9c1e7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:05:32 +0000

    Add package sqlitebiter/0.17.0/sqlitebiter-0.17.0-py2.py3-none-any.whl

---
+++ packages/sqlitebiter/sqlitebiter-0.17.0-py2.py3-none-any.whl/sqlitebiter/_const.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+# encoding: utf-8
+
+"""
+.. codeauthor:: Tsuyoshi Hombashi <tsuyoshi.hombashi@gmail.com>
+"""
+
+from __future__ import absolute_import, unicode_literals
+
+
+PROGRAM_NAME = "sqlitebiter"
+IPYNB_FORMAT_NAME_LIST = ["ipynb"]
