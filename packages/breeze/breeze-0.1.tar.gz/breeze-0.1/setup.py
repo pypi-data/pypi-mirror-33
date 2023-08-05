4ccb9ad2d60e4f36b5505d8e2e98056e0fd6bb13
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:07:46 +0000

    Add package breeze/0.1/breeze-0.1.tar.gz

---
+++ packages/breeze/breeze-0.1.tar.gz/breeze-0.1/setup.py
@@ -0,0 +1,13 @@
+import setuptools
+
+setuptools.setup(
+  name='breeze',
+  version='0.1',
+  author='Nikhil Kothari',
+  description='A Python DSL',
+  license='Apache 2.0',
+  keywords='',
+  url='https://github.com/nikhilk',
+  packages=[],
+  install_requires=[],
+  classifiers=[])
