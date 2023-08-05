ecfa76be065f8bf8beaccde2a5af215c9b2a9d3b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:30:38 +0000

    Add package assemblyai/0.2.5/assemblyai-0.2.5-py2.py3-none-any.whl

---
+++ packages/assemblyai/assemblyai-0.2.5-py2.py3-none-any.whl/assemblyai/exceptions.py
@@ -0,0 +1,34 @@
+"""Exceptions for the assemblyai library."""
+
+
+def handle_errors(response, status_code):
+    if status_code >= 400 and status_code < 500:
+        msg = response.json()
+        raise ClientError(msg)
+    if status_code >= 500:
+        msg = 'Server error, developers have been alerted.'
+        raise ServerError(msg)
+    pass
+
+
+def handle_warnings(response, object, log):
+    """Handle  warnings and exceptions."""
+    warning = None
+    handle_errors(response, response.status_code)
+    if response:
+        response = response.json()[object]
+    if 'warning' in response:
+        warning = response['warning']
+        log.warning('Warning: %s' % warning)
+    if response['status'] == 'error':
+        msg = response['error']
+        raise ServerError(msg)
+    return warning
+
+
+class ClientError(Exception):
+    pass
+
+
+class ServerError(Exception):
+    pass
