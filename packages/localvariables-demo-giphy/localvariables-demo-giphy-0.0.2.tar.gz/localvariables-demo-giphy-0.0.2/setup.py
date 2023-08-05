52ccff39a099f7dec18e9688237f499af02605fb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:30:33 +0000

    Add package localvariables-demo-giphy/0.0.2/localvariables-demo-giphy-0.0.2.tar.gz

---
+++ packages/localvariables-demo-giphy/localvariables-demo-giphy-0.0.2.tar.gz/localvariables-demo-giphy-0.0.2/setup.py
@@ -0,0 +1,20 @@
+import setuptools
+import pypandoc
+
+long_description = pypandoc.convert_file('README.md', 'rst')
+
+setuptools.setup(
+    name="localvariables-demo-giphy",
+    version="0.0.2",
+    author="Michael Barney",
+    author_email="mbarneyjr5309@gmail.com",
+    description="A demo for localvariables",
+    long_description=long_description,
+    packages=setuptools.find_packages(),
+    install_requires=['giphy_client'],
+    entry_points={
+        "console_scripts": [
+            'giphy-search=localvariables.demo:main'
+        ]
+    }
+)
