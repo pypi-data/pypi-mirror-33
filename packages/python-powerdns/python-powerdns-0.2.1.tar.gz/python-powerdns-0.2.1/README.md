fcd364168893e8a2c95982af8710ebb51a1d3386
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:29:12 +0000

    Add package python-powerdns/0.2.1/python-powerdns-0.2.1.tar.gz

---
+++ packages/python-powerdns/python-powerdns-0.2.1.tar.gz/python-powerdns-0.2.1/README.md
@@ -0,0 +1,150 @@
+[![PythonSupport][1]][1l][![License][2]][2l]
+
+# python-powerdns -- PowerDNS web api python client and interface
+
+**Contact:** Denis 'jawa' Pompilio <dpompilio@vente-privee.com>
+
+**Sources:** https://github.com/vente-privee/python-powerdns
+
+## About
+
+This package provides intuitive and easy to use python client and interface
+for the PowerDNS web API.
+
+## Installation
+
+```bash
+python setup.py install
+```
+or
+```bash
+pip install python-powerdns
+```
+
+## Helpers
+
+**pdns-zone-creator**
+```bash
+usage: pdns-create-zone [-h] -A API -K APIKEY -z ZONE -o ORIGIN -c ZONE -d DNS
+                        [-t TIMERS]
+
+PowerDNS zone creator
+
+optional arguments:
+  -h, --help            show this help message and exit
+  -A API, --api API     PowerDNS api (eg. https://api.domain.tld/api/v1
+  -K APIKEY, --key APIKEY
+                        PowerDNS api key
+  -z ZONE, --zone ZONE  Zone name (canonical)
+  -o ORIGIN, --origin ORIGIN
+                        Zone origin (for SOA)
+  -c ZONE, --contact ZONE
+                        Zone contact (for SOA)
+  -d DNS, --dns DNS     Zone nameservers comma separated
+  -t TIMERS, --timers TIMERS
+                        Zone timers (eg. '28800 7200 604800 86400')
+```
+```bash
+./bin/pdns-create-zone -A "https://api.domain.tld/api/v1" -K "xxxxxxxxx" \
+                       -z "myzone.domain.tld." \
+                       -o "ns01.domain.tld." -c "admin.domain.tld." \
+                       -d "nsd01.domain.tld.,nsd02.domain.tld."
+powerdns.interface INFO: listing available PowerDNS servers
+powerdns.interface INFO: getting available servers from API
+powerdns.client INFO: request: GET https://api.domain.tld/api/v1/servers
+powerdns.client INFO: request response code: 200
+powerdns.interface INFO: 1 server(s) listed
+powerdns.interface INFO: creation of zone: myzone.domain.tld.
+powerdns.client INFO: request: POST https://api.domain.tld/api/v1/servers/localhost/zones
+powerdns.client INFO: request response code: 201
+powerdns.interface INFO: zone myzone.domain.tld. successfully created
+```
+
+## Exemples
+
+Basic initialization:
+```python
+import powerdns
+
+PDNS_API = "https://my.pdns.api.domain.tld/api/v1"
+PDNS_KEY = "mysupersecretbase64key"
+
+api_client = powerdns.PDNSApiClient(api_endpoint=PDNS_API, api_key=PDNS_KEY)
+api = powerdns.PDNSEndpoint(api_client)
+```
+
+Creation and deletion of zones:
+```python
+from datetime import date
+
+# Creating new zone on first PowerDNS server
+serial = date.today().strftime("%Y%m%d00")
+soa = "ns0.domain.tld. admin.domain.tld. %s 28800 7200 604800 86400" % serial
+soa_r = powerdns.RRSet(name='test.python-powerdns.domain.tld.',
+                       rtype="SOA",
+                       records=[(soa, False)],
+                       ttl=86400)
+zone = api.servers[0].create_zone(name="test.python-powerdns.domain.tld.",
+                                  kind="Native",
+                                  rrsets=[soa_r],
+                                  nameservers=["ns1.domain.tld.",
+                                               "ns2.domain.tld."])
+
+# Getting new zone info
+print(zone)
+print(zone.details)
+
+# Deleting newly created zone
+api.servers[0].delete_zone(zone.name)
+```
+
+Creation and deletion of DNS records:
+```python
+zone = api.server[0].get_zone("test.python-powerdns.domain.tld.")
+
+zone.create_records([
+    powerdns.RRSet('a', 'A', [('1.1.1.1', False)]),
+    powerdns.RRSet('b', 'A', ['1.1.1.2', '1.1.1.3']),
+    powerdns.RRSet('c', 'A', [('1.1.1.4', False)]),
+    powerdns.RRSet('d', 'CNAME', ['a'])
+])
+
+zone.delete_record([
+    powerdns.RRSet('a', 'A', [('1.1.1.1', False)]),
+    powerdns.RRSet('d', 'CNAME', ['a'])
+])
+```
+
+Backup and restoration of zones:
+```python
+# Backup every zone of every PowerDNS server
+for server in api.servers:
+    backup_dir = "backups/%s" % server.id
+    for zone in server.zones:
+        zone.backup(backup_dir)
+
+# Restore a single zone on first PowerDNS server
+zone_file = "backups/pdns-server-01/my.domain.tld.json"
+api.servers[0].restore_zone(zone_file)
+```
+
+## License
+
+MIT LICENSE *(see LICENSE file)*
+
+## Miscellaneous
+
+```
+    ╚⊙ ⊙╝
+  ╚═(███)═╝
+ ╚═(███)═╝
+╚═(███)═╝
+ ╚═(███)═╝
+  ╚═(███)═╝
+   ╚═(███)═╝
+```
+
+[1]: https://img.shields.io/badge/python-2.7,3.4+-blue.svg
+[1l]: https://github.com/vente-privee/python-powerdns
+[2]: https://img.shields.io/badge/license-MIT-blue.svg
+[2l]: https://github.com/vente-privee/python-powerdns
---
+++ packages/python-powerdns/python-powerdns-0.2.1.tar.gz/python-powerdns-0.2.1/powerdns/client.py
@@ -0,0 +1,169 @@
+# -*- coding: utf-8 -*-
+#
+#  PowerDNS web api python client and interface (python-powerdns)
+#
+#  Copyright (C) 2018 Denis Pompilio (jawa) <dpompilio@vente-privee.com>
+#
+#  This file is part of python-powerdns
+#
+#  This program is free software; you can redistribute it and/or
+#  modify it under the terms of the MIT License.
+#
+#  This program is distributed in the hope that it will be useful,
+#  but WITHOUT ANY WARRANTY; without even the implied warranty of
+#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#  MIT License for more details.
+#
+#  You should have received a copy of the MIT License along with this
+#  program; if not, see <https://opensource.org/licenses/MIT>.
+
+"""
+powerdns.client - PowerDNS API client
+"""
+
+import json
+import logging
+from functools import partial
+import requests
+from .exceptions import PDNSError
+
+
+LOG = logging.getLogger(__name__)
+
+
+class PDNSApiClient(object):
+    """Powerdns API client
+
+    It implements common HTTP methods GET, POST, PUT, PATCH and DELETE
+
+    This client is using :mod:`requests` package. Please see
+    http://docs.python-requests.org/ for more information.
+
+    :param str api_endpoint: Powerdns API endpoint
+    :param str api_key: API key
+    :param bool verify: Control SSL certificate validation
+    :param int timeout: Request timeout in seconds
+
+    .. method:: get(self, path, data=None, **kwargs)
+
+        Partial method invoking :meth:`~PDNSApiClient.request` with
+        http method *GET*.
+
+    .. method:: post(self, path, data=None, **kwargs)
+
+        Partial method invoking :meth:`~PDNSApiClient.request` with
+        http method *POST*.
+
+    .. method:: put(self, path, data=None, **kwargs)
+
+        Partial method invoking :meth:`~PDNSApiClient.request` with
+        http method *PUT*.
+
+    .. method:: patch(self, path, data=None, **kwargs)
+
+        Partial method invoking :meth:`~PDNSApiClient.request` with
+        http method *PATCH*.
+
+    .. method:: delete(self, path, data=None, **kwargs)
+
+        Partial method invoking :meth:`~PDNSApiClient.request` with
+        http method *DELETE*.
+    """
+    def __init__(self, api_endpoint, api_key, verify=True, timeout=None):
+        """Initialization"""
+        self._api_endpoint = api_endpoint
+        self._api_key = api_key
+        self._verify = verify
+        self._timeout = timeout
+
+        if not verify:
+            LOG.debug("removing insecure https connection warnings")
+            import urllib3
+            urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
+
+        self.request_headers = {
+            'Content-Type': 'application/json',
+            'Accept': 'application/json'
+        }
+
+        # Directly expose common HTTP methods
+        self.get = partial(self.request, method='GET')
+        self.post = partial(self.request, method='POST')
+        self.put = partial(self.request, method='PUT')
+        self.patch = partial(self.request, method='PATCH')
+        self.delete = partial(self.request, method='DELETE')
+
+    def request(self, path, method, data=None, **kwargs):
+        """Handle requests to API
+
+        :param str path: API endpoint's path to request
+        :param str method: HTTP method to use
+        :param dict data: Data to send (optional)
+        :return: Parsed json response as :class:`dict`
+
+        Additional named argument may be passed and are directly transmitted
+        to :meth:`request` method of :class:`requests.Session` object.
+
+        :raise PDNSError: If request's response is an error.
+        """
+        if self._api_key:
+            self.request_headers['X-API-Key'] = self._api_key
+
+        LOG.debug("request: original path is %s", path)
+        if not path.startswith('http://') and not path.startswith('https://'):
+            if path.startswith('/'):
+                path = path.lstrip('/')
+            url = "%s/%s" % (self._api_endpoint, path)
+        else:
+            url = path
+
+        if data is None:
+            data = {}
+        data = json.dumps(data)
+
+        LOG.info("request: %s %s", method, url)
+        LOG.debug("headers: %s", self.request_headers)
+        LOG.debug("data: %s", data)
+        response = requests.request(method, url,
+                                    data=data,
+                                    headers=self.request_headers,
+                                    timeout=self._timeout,
+                                    verify=self._verify,
+                                    **kwargs)
+
+        LOG.info("request response code: %d", response.status_code)
+        LOG.debug("response: %s", response.text)
+
+        # Try to handle basic return
+        if response.status_code in [200, 201]:
+            return response.json()
+        elif response.status_code == 204:
+            return ""
+        elif response.status_code == 404:
+            error_message = 'Not found'
+        else:
+            try:
+                error_message = self._get_error(response=response.json())
+            except Exception:
+                error_message = response.text
+
+        LOG.error("raising error code %d", response.status_code)
+        LOG.debug("error response: %s", error_message)
+        raise PDNSError(url=response.url,
+                        status_code=response.status_code,
+                        message=error_message)
+
+    @staticmethod
+    def _get_error(response):
+        """Get error message from API response
+
+        :param dict response: API response
+        :return: Error message as :func:`str`
+        """
+        if 'error' in response:
+            err = response.get('error')
+        elif 'errors' in response:
+            err = response.get('errors')
+        else:
+            err = 'No error message found'
+        return err
