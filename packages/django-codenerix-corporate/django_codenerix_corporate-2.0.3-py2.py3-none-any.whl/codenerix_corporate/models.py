88b4806f5826fbe2b16f1378956750f64ede58c8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:08:22 +0000

    Add package django-codenerix-corporate/2.0.3/django_codenerix_corporate-2.0.3-py2.py3-none-any.whl

---
+++ packages/django-codenerix-corporate/django_codenerix_corporate-2.0.3-py2.py3-none-any.whl/codenerix_corporate/models.py
@@ -0,0 +1,73 @@
+# -*- coding: utf-8 -*-
+#
+# django-codenerix-corporate
+#
+# Copyright 2017 Centrologic Computational Logistic Center S.L.
+#
+# Project URL : http://www.codenerix.com
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+from django.db import models, transaction
+from django.utils.translation import ugettext_lazy as _
+
+from codenerix.models import CodenerixModel
+from codenerix.exceptions import CodenerixException
+from codenerix.lib.helpers import upload_path
+from codenerix.fields import ImageAngularField
+from codenerix_geodata.models import GeoAddress
+
+
+class CorporateImage(CodenerixModel, GeoAddress):
+
+    nid = models.CharField(_("NID"), max_length=20, blank=True)
+    business_name = models.CharField(_("Business name"), max_length=254, blank=True, null=True)
+    digital_sign = ImageAngularField(_("Digital sign"), upload_to=upload_path, max_length=200, blank=True, null=True)
+    name_file_sign = models.CharField(_("Name file digital sign"), max_length=254, blank=True, null=True)
+    company_seal = ImageAngularField(_("Company seal"), upload_to=upload_path, max_length=200, blank=True, null=True)
+    name_file_seal = models.CharField(_("Name file company seal"), max_length=254, blank=True, null=True)
+    company_logo = ImageAngularField(_("Company logo"), upload_to=upload_path, max_length=200, blank=True, null=True)
+    name_file_logo = models.CharField(_("Name file company logo"), max_length=254, blank=True, null=True)
+    public = models.BooleanField(_("Public"), default=False)
+    email = models.EmailField(_("Email"), max_length=60, blank=True, null=True)
+
+    def __str__(self):
+        return u"{}".format(self.business_name)
+
+    def __unicode__(self):
+        return self.__str__()
+
+    def __fields__(self, info):
+        fields = super(CorporateImage, self).__fields__(info)
+        fields.insert(0, ('nid', _('NID')))
+        fields.insert(0, ('business_name', _('Business name')))
+        fields.append(('email', _('Email')))
+        fields.append(('public', _('Public')))
+        fields.append(('updated', _('Last Update')))
+        return fields
+
+    def save(self, *args, **kwards):
+        """
+        Siempre debe haber un registro marcado como público
+        There should always be a record marked as public
+        """
+        try:
+            with transaction.atomic():
+                if self.public:
+                    CorporateImage.objects.exclude(pk=self.pk).update(public=False)
+                elif not CorporateImage.objects.exclude(pk=self.pk).filter(public=True).exists():
+                    self.public = True
+
+                return super(CorporateImage, self).save(*args, **kwards)
+        except CodenerixException:
+            return super(CorporateImage, self).save(*args, **kwards)
---
+++ packages/django-codenerix-corporate/django_codenerix_corporate-2.0.3-py2.py3-none-any.whl/django_codenerix_corporate-2.0.3.dist-info/DESCRIPTION.rst
@@ -0,0 +1,61 @@
+==========================
+django-codenerix-corporate
+==========================
+
+Codenerix Corporate is a module that enables `CODENERIX <http://www.codenerix.com/>`_ to manage corporate/enterprise data on several platforms in a general manner. Model for managing business's information
+
+.. image:: http://www.codenerix.com/wp-content/uploads/2018/05/codenerix.png
+    :target: http://www.codenerix.com
+    :alt: Try our demo with Codenerix Cloud
+
+*********
+Changelog
+*********
+
+2018-01-17 - Codenerix Corporate v1.x is incompatible with v2.x, `what has changed and how to migrate to v2.x? <https://github.com/codenerix/django-codenerix-corporate/wiki/Codenerix-Corporate-version-1.x-is-icompatible-with-2.x>`_.
+
+****
+Demo
+****
+
+You can have a look to our `demo online <http://demo.codenerix.com>`_.
+
+You can find some working examples in GITHUB at `django-codenerix-examples <https://github.com/codenerix/django-codenerix-examples>`_ project.
+
+**********
+Quickstart
+**********
+
+1. Install this package::
+
+    For python 2: sudo pip2 install django-codenerix-corporate
+    For python 3: sudo pip3 install django-codenerix-corporate
+
+2. Add "codenerix_corporate" to your INSTALLED_APPS setting like this::
+
+    INSTALLED_APPS = [
+        ...
+        'codenerix_corporate',
+    ]
+
+3. Add the param in setting:
+	# list of languages available for the translation of the content of the models
+	LANGUAGES_DATABASES = ['ES', 'EN']
+
+4. Since Codenerix Corporate is a library, you only need to import its parts into your project and use them.
+
+*************
+Documentation
+*************
+
+Coming soon... do you help us? `Codenerix <http://www.codenerix.com/>`_
+
+You can chat with us `here <https://goo.gl/NgpzBh>`_.
+
+*******
+Credits
+*******
+
+This project has been possible thanks to `Centrologic <http://www.centrologic.com/>`_.
+
+
