c132918c7b1bffa01d1246ca27eef6a8e3443213
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:05:33 +0000

    Add package lichanghongpip/1.0.0/lichanghongpip-1.0.0.tar.gz

---
+++ packages/lichanghongpip/lichanghongpip-1.0.0.tar.gz/lichanghongpip-1.0.0/setup.py
@@ -0,0 +1,70 @@
+import codecs
+import os
+import sys
+
+
+try:
+
+    from setuptools import setup
+
+except:
+
+    from distutils.core import setup
+
+def read(fname):
+    return codecs.open(os.path.join(os.path.dirname(__file__), fname)).read()
+
+ 
+
+NAME = "lichanghongpip"
+
+
+PACKAGES = ['sourceforprint',]
+
+
+DESCRIPTION = "this is a test package for print hello world"
+
+
+LONG_DESCRIPTION = "import index function print hello world"
+
+
+KEYWORDS = "print hello world"
+ 
+
+AUTHOR = "wenyali"
+
+
+AUTHOR_EMAIL = "2917073217@qq.com"
+
+
+URL = "https://github.com/lichanghong/wenyali.git"
+
+
+
+VERSION = "1.0.0"
+
+
+
+LICENSE = "MIT"
+
+
+
+setup(
+
+    name =NAME,version = VERSION,description = DESCRIPTION,long_description =LONG_DESCRIPTION,
+
+    classifiers =[
+
+        'License :: OSI Approved :: MIT License',
+
+        'Programming Language :: Python',
+
+        'Intended Audience :: Developers',
+
+        'Operating System :: OS Independent',
+
+    ],
+
+    keywords =KEYWORDS,author = AUTHOR,author_email = AUTHOR_EMAIL,url = URL, packages = PACKAGES,include_package_data=True,zip_safe=True,
+
+)
