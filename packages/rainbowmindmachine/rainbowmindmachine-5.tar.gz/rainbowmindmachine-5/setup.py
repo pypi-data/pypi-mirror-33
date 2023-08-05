6924aa9e3cea6ab2c6f4cf876a35af35a0471dd4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 23:06:37 +0000

    Add package rainbowmindmachine/5/rainbowmindmachine-5.tar.gz

---
+++ packages/rainbowmindmachine/rainbowmindmachine-5.tar.gz/rainbowmindmachine-5/setup.py
@@ -0,0 +1,19 @@
+from setuptools import setup
+
+version = "5"
+
+setup(  name='rainbowmindmachine',
+        version=version,
+        description='The extensible framework for running Twitter bot flocks.',
+        url='https://pages.charlesreid1.com/rainbow-mind-machine',
+        author='charlesreid1',
+        author_email='rainbowmindmachine@charlesreid1.com',
+        test_suite='nose.collector',
+        tests_require=['nose'],
+        license='MIT',
+        packages=['rainbowmindmachine'],
+        install_requires=['boringmindmachine==5',
+            'TextBlob>=0.15'
+        ],
+        zip_safe=False)
+
