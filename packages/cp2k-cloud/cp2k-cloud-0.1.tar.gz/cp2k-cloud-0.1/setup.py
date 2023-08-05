55ee52976047eac38e8f6398af7a1d1afb38eb3b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:06:52 +0000

    Add package cp2k-cloud/0.1/cp2k-cloud-0.1.tar.gz

---
+++ packages/cp2k-cloud/cp2k-cloud-0.1.tar.gz/cp2k-cloud-0.1/setup.py
@@ -0,0 +1,9 @@
+from distutils.core import setup
+setup(
+  name = 'cp2k-cloud',
+  packages = ['cp2k-cloud'],
+  version = '0.1',
+  author = 'Ole Schuett',
+  author_email = 'ole@cp2k.org',
+  url = 'https://www.cp2k.org',
+)
