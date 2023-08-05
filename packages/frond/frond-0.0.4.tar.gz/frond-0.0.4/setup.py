a57a7f3d433a91a4ce0910865296c2414aeef693
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:31:25 +0000

    Add package frond/0.0.4/frond-0.0.4.tar.gz

---
+++ packages/frond/frond-0.0.4.tar.gz/frond-0.0.4/setup.py
@@ -0,0 +1,31 @@
+import os
+from setuptools import setup, find_packages
+
+here = os.path.abspath(os.path.dirname(__file__))
+
+with open(os.path.join(here, 'README.md')) as f:
+    README = f.read()
+
+
+setup(
+    name='frond',
+    version='0.0.4',
+    description='Asynchronous Fan Out/In for Pyramid.',
+    long_description=README,
+    long_description_content_type="text/markdown",
+    classifiers=[
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 3 :: Only",
+        "Framework :: Pyramid",
+        "Topic :: Internet :: WWW/HTTP",
+        "License :: OSI Approved :: MIT License",
+    ],
+    keywords="",
+    author='Landreville',
+    author_email='landreville@heyneat.ca',
+    url='https://gitlab.com/landreville/frond',
+    packages=find_packages(),
+    include_package_data=True,
+    zip_safe=False,
+    install_requires=[]
+)
