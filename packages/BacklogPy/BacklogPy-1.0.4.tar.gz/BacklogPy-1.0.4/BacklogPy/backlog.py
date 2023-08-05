7ed54ad9519777dab79a47561abebd2dcb4fa95c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 23:06:52 +0000

    Add package BacklogPy/1.0.4/BacklogPy-1.0.4.tar.gz

---
+++ packages/BacklogPy/BacklogPy-1.0.4.tar.gz/BacklogPy-1.0.4/BacklogPy/backlog.py
@@ -0,0 +1,15 @@
+# coding: utf-8
+
+"""
+    This file was created by Backlog APIGenerator
+"""
+
+
+from __future__ import unicode_literals, absolute_import
+
+from BacklogPy.api import *
+
+
+class Backlog(Groups, Issues, Notifications, Priorities, Projects,
+              Resolutions, Space, Stars, Statuses, Users, Watchings, Wikis):
+    pass
