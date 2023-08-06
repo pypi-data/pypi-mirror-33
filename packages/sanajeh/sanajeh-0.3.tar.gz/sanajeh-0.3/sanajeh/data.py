919753a737ea4cd7979a3f054ce809f1dab3b398
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:59:52 +0000

    Add package sanajeh/0.3/sanajeh-0.3.tar.gz

---
+++ packages/sanajeh/sanajeh-0.3.tar.gz/sanajeh-0.3/sanajeh/data.py
@@ -0,0 +1,9 @@
+from pathlib import Path
+
+
+ROOT_DIR = Path(__file__).parent
+
+ANAT_DIR = ROOT_DIR / 'data' / 'anat'
+data_t1 = ANAT_DIR / 'T1.mgz'
+data_aparc = ANAT_DIR / 'aparc.a2009s+aseg.mgz'
+data_elec = ROOT_DIR / 'data' / 'electrodes.tsv'
