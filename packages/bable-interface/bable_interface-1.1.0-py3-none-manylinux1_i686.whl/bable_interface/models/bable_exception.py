f91b852beddadbf45925c210e993ef1da3a43019
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:18:12 +0000

    Add package bable-interface/1.1.0/bable_interface-1.1.0-py3-none-manylinux1_i686.whl

---
+++ packages/bable-interface/bable_interface-1.1.0-py3-none-manylinux1_i686.whl/bable_interface/models/bable_exception.py
@@ -0,0 +1,34 @@
+from bable_interface.BaBLE.Payload import Payload
+
+
+class BaBLEException(Exception):
+
+    def __init__(self, packet, message=None, **kwargs):
+        super(BaBLEException, self).__init__()
+
+        if packet.payload_type == Payload.BaBLEError:
+            self.message = packet.get('message')
+        else:
+            self.message = message if message is not None else "Response with error status received"
+
+        self.packet = packet
+        self.kwargs = kwargs
+
+    def __str__(self):
+        return self.__repr__()
+
+    def __repr__(self):
+        result = "{} (packet={}, ".format(self.message, self.packet)
+
+        for key, value in self.kwargs.items():
+            result += "{}={}, ".format(key, value)
+
+        return result[:-2] + ')'
+
+    def __getattribute__(self, name):
+        try:
+            result = super(BaBLEException, self).__getattribute__(name)
+        except AttributeError:
+            result = self.kwargs[name]
+
+        return result
