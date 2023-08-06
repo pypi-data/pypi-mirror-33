f3a4d6bd1f05f58dc94c4be367de8fa7874e7136
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 05:06:17 +0000

    Add package pydogs/0.1.1/pydogs-0.1.1.tar.gz

---
+++ packages/pydogs/pydogs-0.1.1.tar.gz/pydogs-0.1.1/setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='pydogs',
+    description='A Delaunay based approach of hyperparmeter optimization',
+    version='0.1.1',
+    url='https://github.com/deltadogs/pyDOGS4',
+    author='Shahrouz Ryan Alimo',
+    maintainer='Andrew Hess',
+    maintainer_email='ahess3815@gmail.com',
+    packages=['pydogs']
+)
