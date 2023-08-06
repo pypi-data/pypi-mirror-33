ef9ca5a228b54f9a7025beb667880c141776e162
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:56:12 +0000

    Add package log-factory/0.1.1/log_factory-0.1.1.tar.gz

---
+++ packages/log-factory/log_factory-0.1.1.tar.gz/log_factory-0.1.1/setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='log_factory',
+    version='0.1.1',
+    packages=['log_factory', ],
+    long_description=open('README.md').read(),
+    url='https://github.com/colinp85/log_factory',
+    author='cp',
+    license='MIT',
+    python_requires='>=2.6'
+)
