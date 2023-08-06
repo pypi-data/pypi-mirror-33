5e98cc0e98188ab092e4005f210f406f171c9f3f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:47:57 +0000

    Add package resolwe/10.0.1/resolwe-10.0.1-py3-none-any.whl

---
+++ packages/resolwe/resolwe-10.0.1-py3-none-any.whl/resolwe/flow/executors/docker/__init__.py
@@ -0,0 +1,10 @@
+""".. Ignore pydocstyle D400.
+
+====================
+Docker Flow Executor
+====================
+
+.. automodule:: resolwe.flow.executors.docker.prepare
+.. automodule:: resolwe.flow.executors.docker.run
+
+"""
