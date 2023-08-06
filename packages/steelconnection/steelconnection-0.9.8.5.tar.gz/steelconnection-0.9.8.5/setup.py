303815161cc55763dde63226f4cb39335cd30fab
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 07:13:37 +0000

    Add package steelconnection/0.9.8.5/steelconnection-0.9.8.5.tar.gz

---
+++ packages/steelconnection/steelconnection-0.9.8.5.tar.gz/steelconnection-0.9.8.5/setup.py
@@ -0,0 +1,25 @@
+from setuptools import setup
+
+with open('README.md', 'rt') as f:
+    long_description = f.read()
+
+setup(
+    name='steelconnection',
+    install_requires=['requests>=2.12.1'],
+    version='0.9.8.5',
+    author='Greg Mueller',
+    author_email='steelconnection@grelleum.com',
+    description='Simplify access to the Riverbed SteelConnect REST API.',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    url='https://github.com/grelleum/SteelConnection',
+    keywords=['SteelConnect', 'REST', 'API', 'Riverbed', 'Grelleum'],
+    packages=['steelconnection'],
+    license='MIT',
+    classifiers=[
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent'
+    ],
+)
