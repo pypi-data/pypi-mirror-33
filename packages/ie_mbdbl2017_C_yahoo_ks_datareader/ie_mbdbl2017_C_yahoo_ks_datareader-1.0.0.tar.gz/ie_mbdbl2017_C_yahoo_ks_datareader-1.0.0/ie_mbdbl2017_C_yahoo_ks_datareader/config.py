201ec9feda005296ab5a40d12641b72e0cca2b02
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:52:17 +0000

    Add package ie_mbdbl2017_C_yahoo_ks_datareader/1.0.0/ie_mbdbl2017_C_yahoo_ks_datareader-1.0.0.tar.gz

---
+++ packages/ie_mbdbl2017_C_yahoo_ks_datareader/ie_mbdbl2017_C_yahoo_ks_datareader-1.0.0.tar.gz/ie_mbdbl2017_C_yahoo_ks_datareader-1.0.0/ie_mbdbl2017_C_yahoo_ks_datareader/config.py
@@ -0,0 +1,5 @@
+#Data for the email
+
+EMAIL_ADDRESS = "riskandfraudclass@gmail.com"
+PASSWORD = "bigdata18"
+MAIL = "alvarofq@student.ie.edu"
---
+++ packages/ie_mbdbl2017_C_yahoo_ks_datareader/ie_mbdbl2017_C_yahoo_ks_datareader-1.0.0.tar.gz/ie_mbdbl2017_C_yahoo_ks_datareader-1.0.0/ie_mbdbl2017_C_yahoo_ks_datareader/send_email.py
@@ -0,0 +1,45 @@
+ 
+
+def send_email(subject, msg, send_plt):
+    
+    #Functions for email
+    import smtplib
+    from email.mime.multipart import MIMEMultipart
+    from email.mime.text import MIMEText
+    from email.mime.base import MIMEBase
+    from email import encoders
+    import config
+    
+    try:
+        fromaddr = config.EMAIL_ADDRESS
+        toaddr = config.MAIL
+         
+        msg = MIMEMultipart()
+         
+        msg['From'] = fromaddr
+        msg['To'] = toaddr
+        msg['Subject'] = subject
+         
+        body =  msg
+         
+        msg.attach(MIMEText(body, 'plain'))
+         
+        filename = send_plt
+        attachment = open(send_plt, "rb")
+         
+        part = MIMEBase('application', 'octet-stream')
+        part.set_payload((attachment).read())
+        encoders.encode_base64(part)
+        part.add_header('Content-Disposition', "attachment; filename= %s" % filename)
+         
+        msg.attach(part)
+         
+        server = smtplib.SMTP('smtp.gmail.com:587')
+        server.ehlo()
+        server.starttls()
+        server.login(fromaddr, config.PASSWORD)
+        text = msg.as_string()
+        server.sendmail(fromaddr, toaddr, text)
+        server.quit()
+    except:
+        print("Email failed to send")
