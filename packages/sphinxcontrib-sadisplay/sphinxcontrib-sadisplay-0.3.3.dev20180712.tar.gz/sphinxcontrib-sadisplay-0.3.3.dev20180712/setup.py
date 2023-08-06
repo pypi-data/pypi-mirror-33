1f98be8570621b411d0c57d8cc4426d0bc1d5e22
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:43:56 +0000

    Add package sphinxcontrib-sadisplay/0.3.3.dev20180712/sphinxcontrib-sadisplay-0.3.3.dev20180712.tar.gz

---
+++ packages/sphinxcontrib-sadisplay/sphinxcontrib-sadisplay-0.3.3.dev20180712.tar.gz/sphinxcontrib-sadisplay-0.3.3.dev20180712/setup.py
@@ -0,0 +1,44 @@
+# -*- coding: utf-8 -*-
+import os
+
+try:
+    from setuptools import setup, find_packages
+except ImportError:
+    from distutils.core import setup, find_packages
+
+here = os.path.dirname(os.path.abspath(__file__))
+
+requires = [
+    'Sphinx>=0.6',
+    'sadisplay',
+]
+
+setup(
+    name='sphinxcontrib-sadisplay',
+    version='0.3.3',
+    url='https://github.com/sphinx-contrib/sadisplay',
+    download_url='http://pypi.python.org/pypi/sphinxcontrib-sadisplay',
+    license='BSD',
+    author='Evgeniy Tatarkin',
+    author_email='tatarkin.evg@gmail.com',
+    description='Sphinx "sadisplay" extension',
+    long_description=open(os.path.join(here, 'README.rst')).read(),
+    zip_safe=False,
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Environment :: Console',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: BSD License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 3',
+        'Topic :: Documentation',
+        'Topic :: Utilities',
+    ],
+    platforms='any',
+    # packages=['sphinxcontib',],
+    packages=find_packages(),
+    include_package_data=True,
+    install_requires=requires,
+    namespace_packages=['sphinxcontrib'], )
