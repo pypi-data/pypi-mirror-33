b9f2c3f0c1f98af99e6a9e54d4fcd3fff5254f22
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 07:12:42 +0000

    Add package aiohttp_traversal/0.10.0/aiohttp_traversal-0.10.0.tar.gz

---
+++ packages/aiohttp_traversal/aiohttp_traversal-0.10.0.tar.gz/aiohttp_traversal-0.10.0/aiohttp_traversal/ext/views.py
@@ -0,0 +1,77 @@
+import json
+import asyncio
+
+from aiohttp.web import Response, StreamResponse
+from aiohttp.web import HTTPMethodNotAllowed
+
+from aiohttp_traversal.abc import AbstractView
+
+
+class View(AbstractView):
+    def __init__(self, request, resource, tail):
+        self.request = request
+        self.resource = resource
+        self.tail = tail
+
+    @asyncio.coroutine
+    def __call__(self):
+        raise NotImplementedError()
+
+
+class MethodsView(View):
+    methods = frozenset()  # {'get', 'post', 'put', 'patch', 'delete', 'option'}
+
+    @asyncio.coroutine
+    def __call__(self):
+        method = self.request.method.lower()
+
+        if method in self.methods:
+            return (yield from getattr(self, method)())
+        else:
+            raise HTTPMethodNotAllowed(method, self.methods)
+
+    @asyncio.coroutine
+    def get(self):
+        raise NotImplementedError
+
+    @asyncio.coroutine
+    def post(self):
+        raise NotImplementedError
+
+    @asyncio.coroutine
+    def put(self):
+        raise NotImplementedError
+
+    @asyncio.coroutine
+    def patch(self):
+        raise NotImplementedError
+
+    @asyncio.coroutine
+    def delete(self):
+        raise NotImplementedError
+
+    @asyncio.coroutine
+    def option(self):
+        raise NotImplementedError
+
+
+class RESTView(MethodsView):
+    def serialize(self, data):
+        """ Serialize data to JSON.
+
+        You can owerride this method if you data cant be serialized
+        standart json.dumps routine.
+        """
+        return json.dumps(data).encode('utf8')
+
+    @asyncio.coroutine
+    def __call__(self):
+        data = yield from super().__call__()
+
+        if isinstance(data, StreamResponse):
+            return data
+        else:
+            return Response(
+                body=self.serialize(data),
+                headers={'Content-Type': 'application/json; charset=utf-8'},
+            )
---
+++ packages/aiohttp_traversal/aiohttp_traversal-0.10.0.tar.gz/aiohttp_traversal-0.10.0/setup.py
@@ -0,0 +1,27 @@
+from setuptools import setup
+
+
+setup(
+    name='aiohttp_traversal',
+    version='0.10.0',
+    description='Traversal based router for aiohttp.web',
+    classifiers=[
+        "License :: OSI Approved :: BSD License",
+        "Operating System :: POSIX",
+        "Programming Language :: Python :: 3.4",
+        "Programming Language :: Python :: 3.5",
+        "Topic :: Internet :: WWW/HTTP",
+    ],
+    author='Alexander Zelenyak',
+    author_email='zzz.sochi@gmail.com',
+    license='BSD',
+    url='https://github.com/zzzsochi/aiohttp_traversal',
+    keywords=['asyncio', 'aiohttp', 'traversal', 'pyramid'],
+    packages=['aiohttp_traversal', 'aiohttp_traversal.ext'],
+    install_requires=[
+        'aiohttp >=2.0',
+        'resolver_deco',
+        'zope.dottedname',
+    ],
+    tests_require=['pytest']
+),
