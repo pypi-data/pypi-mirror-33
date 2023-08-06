e4df02060ffdbb3484b442773872cecb7a187076
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:44:12 +0000

    Add package smooch/3.12.0/smooch-3.12.0-py2.py3-none-any.whl

---
+++ packages/smooch/smooch-3.12.0-py2.py3-none-any.whl/smooch/apis/__init__.py
@@ -0,0 +1,11 @@
+from __future__ import absolute_import
+
+# import apis into api package
+from .app_api import AppApi
+from .app_user_api import AppUserApi
+from .attachments_api import AttachmentsApi
+from .conversation_api import ConversationApi
+from .integration_api import IntegrationApi
+from .menu_api import MenuApi
+from .service_account_api import ServiceAccountApi
+from .webhook_api import WebhookApi
