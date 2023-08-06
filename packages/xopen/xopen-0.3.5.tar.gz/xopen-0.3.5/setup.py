6816a4bdbbed96dc3e6e0dec4ff313b2bd579759
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:16:05 +0000

    Add package xopen/0.3.5/xopen-0.3.5.tar.gz

---
+++ packages/xopen/xopen-0.3.5.tar.gz/xopen-0.3.5/setup.py
@@ -0,0 +1,38 @@
+import sys
+from setuptools import setup
+
+if sys.version_info < (2, 7):
+	sys.stdout.write("At least Python 2.7 is required.\n")
+	sys.exit(1)
+
+with open('README.rst') as f:
+	long_description = f.read()
+
+if sys.version_info < (3, ):
+	requires = ['bz2file']
+else:
+	requires = []
+
+setup(
+	name='xopen',
+	version='0.3.5',
+	author='Marcel Martin',
+	author_email='mail@marcelm.net',
+	url='https://github.com/marcelm/xopen/',
+	description='Open compressed files transparently',
+	long_description=long_description,
+	license='MIT',
+	py_modules=['xopen'],
+	install_requires=requires,
+	classifiers=[
+		"Development Status :: 4 - Beta",
+		"License :: OSI Approved :: MIT License",
+		"Programming Language :: Python :: 2.7",
+		"Programming Language :: Python :: 3",
+		"Programming Language :: Python :: 3.3",
+		"Programming Language :: Python :: 3.4",
+		"Programming Language :: Python :: 3.5",
+		"Programming Language :: Python :: 3.6",
+		"Programming Language :: Python :: 3.7",
+	]
+)
