4c8119034e75be3d9888ee32aa0d48425eec83f3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 05:06:37 +0000

    Add package mri-pop-vis/0.1.0/mri_pop_vis-0.1.0.tar.gz

---
+++ packages/mri-pop-vis/mri_pop_vis-0.1.0.tar.gz/mri_pop_vis-0.1.0/mri_pop_vis/__init__.py
@@ -0,0 +1,7 @@
+# -*- coding: utf-8 -*-
+
+"""Top-level package for PythonHackathon_Mos."""
+
+__author__ = """Ayam Greental"""
+__email__ = 'ayam.greental@gmail.com'
+__version__ = '0.1.0'
---
+++ packages/mri-pop-vis/mri_pop_vis-0.1.0.tar.gz/mri_pop_vis-0.1.0/setup.py
@@ -0,0 +1,44 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+"""The setup script."""
+
+from setuptools import setup, find_packages
+
+with open('README.rst') as readme_file:
+    readme = readme_file.read()
+
+with open('HISTORY.rst') as history_file:
+    history = history_file.read()
+
+requirements = [ ]
+
+setup_requirements = ['pytest-runner', ]
+
+test_requirements = ['pytest', ]
+
+setup(
+    author="Ayam Greental, Hila Gast, Omri Tomer, Gadi Drori, Shira Halabi",
+    author_email='ayam.greental@gmail.com',
+    classifiers=[
+        'Development Status :: 2 - Pre-Alpha',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Natural Language :: English',
+        'Programming Language :: Python :: 3.6',
+    ],
+    description="Python Boilerplate contains all the boilerplate you need to create a Python package.",
+    install_requires=requirements,
+    license="MIT license",
+    long_description=readme + '\n\n' + history,
+    include_package_data=True,
+    keywords='mri_pop_vis',
+    name='mri_pop_vis',
+    packages=find_packages(include=['mri_pop_vis']),
+    setup_requires=setup_requirements,
+    test_suite='tests',
+    tests_require=test_requirements,
+    url='https://github.com/amygreen/Pyhack',
+    version='0.1.0',
+    zip_safe=False,
+)
