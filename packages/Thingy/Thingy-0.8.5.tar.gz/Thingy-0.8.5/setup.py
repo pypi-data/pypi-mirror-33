93fb7c4c158210039a516cbd08d1017a042dc100
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:35:38 +0000

    Add package Thingy/0.8.5/Thingy-0.8.5.tar.gz

---
+++ packages/Thingy/Thingy-0.8.5.tar.gz/Thingy-0.8.5/setup.py
@@ -0,0 +1,29 @@
+from setuptools import setup
+
+
+def get_description():
+    with open("README.rst") as file:
+        return file.read()
+
+
+setup(
+    name="Thingy",
+    version="0.8.5",
+    url="https://github.com/numberly/thingy",
+    license="MIT",
+    author="Guillaume Gelin",
+    author_email="ramnes@1000mercis.com",
+    description="Dictionary as an object, that can have different views",
+    long_description=get_description(),
+    py_modules=["thingy"],
+    include_package_data=True,
+    zip_safe=False,
+    platforms="any",
+    install_requires=["six>=1.10.0"],
+    classifiers=[
+        "Intended Audience :: Developers",
+        "Operating System :: OS Independent",
+        "Programming Language :: Python",
+        "Topic :: Software Development :: Libraries :: Python Modules"
+    ]
+)
