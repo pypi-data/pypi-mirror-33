3c99081ee5c9a1188aa58ddd5076e5f52e5fd33e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:57:35 +0000

    Add package radical.pilot/0.50.0/radical.pilot-0.50.0.tar.gz

---
+++ packages/radical.pilot/radical.pilot-0.50.0.tar.gz/radical.pilot-0.50.0/src/radical/pilot/agent/lm/mpirun_ccmrun.py
@@ -0,0 +1,118 @@
+
+__copyright__ = "Copyright 2016, http://radical.rutgers.edu"
+__license__   = "MIT"
+
+
+import radical.utils as ru
+
+from .base import LaunchMethod
+
+
+# ==============================================================================
+#
+# ccmrun: Cluster Compatibility Mode job launcher for Cray systems
+#
+class MPIRunCCMRun(LaunchMethod):
+
+    # --------------------------------------------------------------------------
+    #
+    def __init__(self, cfg, session):
+
+        LaunchMethod.__init__(self, cfg, session)
+
+
+    # --------------------------------------------------------------------------
+    #
+    def _configure(self):
+
+        self.launch_command = self._find_executable([
+            'mpirun',            # General case
+            'mpirun_rsh',        # Gordon @ SDSC
+            'mpirun-mpich-mp',   # Mac OSX MacPorts
+            'mpirun-openmpi-mp'  # Mac OSX MacPorts
+        ])
+
+        self.ccmrun_command = self._find_executable([
+            'ccmrun',            # General case
+        ])
+
+        if not self.ccmrun_command:
+            raise RuntimeError("ccmrun not found!")
+
+        # alas, the way to transplant env variables to the target node differs
+        # per mpi(run) version...
+        out, err, ret = ru.sh_callout('%s -v' % self.launch_command)
+
+        if ret != 0:
+            out, err, ret = ru.sh_callout('%s -info' % self.launch_command)
+
+        self.launch_version = ''
+        for line in out.splitlines():
+            if 'HYDRA build details:' in line:
+                self.launch_version += 'hydra-'
+            if 'version:' in line.lower():
+                self.launch_version += line.split(':')[1].strip().lower()
+                break
+
+        if not self.launch_version:
+            self.launch_version = 'unknown'
+
+
+    # --------------------------------------------------------------------------
+    #
+    def construct_command(self, cu, launch_script_hop):
+
+        opaque_slots = cu['opaque_slots']
+        cud          = cu['description']
+        task_exec    = cud['executable']
+        task_cores   = cud['cores']
+        task_env     = cud.get('environment') or dict()
+        task_args    = cud.get('arguments')   or list()
+        task_argstr  = self._create_arg_string(task_args)
+
+        if not 'task_slots' in opaque_slots:
+            raise RuntimeError('insufficient information to launch via %s: %s' \
+                    % (self.name, opaque_slots))
+
+        task_slots = opaque_slots['task_slots']
+
+        # Extract all the hosts from the slots
+        # TODO: is there any use in using $HOME/.crayccm/ccm_nodelist.$JOBID?
+        hosts_string = ",".join([slot.split(':')[0] for slot in task_slots])
+
+        # Construct the executable and arguments
+        if task_argstr:
+            task_command = "%s %s" % (task_exec, task_argstr)
+        else:
+            task_command = task_exec
+
+
+        env_string = ''
+        env_list   = self.EXPORT_ENV_VARIABLES + task_env.keys()
+        if env_list:
+            if 'hydra' in self.launch_version:
+                env_string = '-envlist "%s"' % ','.join(env_list)
+
+            elif 'openmpi' in self.launch_version:
+                env_string = ''
+                for var in task_env:
+                    env_string += '-x "%s" ' % var
+
+            else:
+                # this is a crude version of env transplanting where we prep the
+                # shell command line.  We likely won't survive any complicated vars
+                # (multiline, quotes, etc)
+                env_string = ' '
+                for var in env_string:
+                    env_string += '%s="$%s" ' % (var, var)
+
+
+        command = "%s %s -np %d -host %s %s %s" % \
+                  (self.ccmrun_command, self.launch_command,
+                  task_cores, hosts_string, env_string, task_command)
+
+        return command, None
+
+
+# ------------------------------------------------------------------------------
+
---
+++ packages/radical.pilot/radical.pilot-0.50.0.tar.gz/radical.pilot-0.50.0/src/radical/pilot/agent/lm/mpirun_dplace.py
@@ -0,0 +1,114 @@
+
+__copyright__ = "Copyright 2016, http://radical.rutgers.edu"
+__license__   = "MIT"
+
+
+import radical.utils as ru
+
+from .base import LaunchMethod
+
+
+# ==============================================================================
+#
+# dplace: job launcher for SGI systems (e.g. on Blacklight)
+#
+class MPIRunDPlace(LaunchMethod):
+
+    # --------------------------------------------------------------------------
+    #
+    def __init__(self, cfg, session):
+
+        LaunchMethod.__init__(self, cfg, session)
+
+
+    # --------------------------------------------------------------------------
+    #
+    def _configure(self):
+
+        self.launch_command = self._find_executable([
+            'mpirun',            # General case
+            'mpirun_rsh',        # Gordon @ SDSC
+            'mpirun-mpich-mp',   # Mac OSX MacPorts
+            'mpirun-openmpi-mp'  # Mac OSX MacPorts
+        ])
+
+        self.dplace_command = self._find_executable([
+            'dplace',            # General case
+        ])
+
+        if not self.dplace_command:
+            raise RuntimeError("mpirun not found!")
+
+        # alas, the way to transplant env variables to the target node differs
+        # per mpi(run) version...
+        out, err, ret = ru.sh_callout('%s -v' % self.launch_command)
+
+        if ret != 0:
+            out, err, ret = ru.sh_callout('%s -info' % self.launch_command)
+
+        self.launch_version = ''
+        for line in out.splitlines():
+            if 'HYDRA build details:' in line:
+                self.launch_version += 'hydra-'
+            if 'version:' in line.lower():
+                self.launch_version += line.split(':')[1].strip().lower()
+                break
+
+        if not self.launch_version:
+            self.launch_version = 'unknown'
+
+
+    # --------------------------------------------------------------------------
+    #
+    def construct_command(self, cu, launch_script_hop):
+
+        opaque_slots = cu['opaque_slots']
+        cud          = cu['description']
+        task_exec    = cud['executable']
+        task_cores   = cud['cores']
+        task_env     = cud.get('environment') or dict()
+        task_args    = cud.get('arguments')   or list()
+        task_argstr  = self._create_arg_string(task_args)
+
+        if not 'task_offsets' in opaque_slots:
+            raise RuntimeError('insufficient information to launch via %s: %s' \
+                    % (self.name, opaque_slots))
+
+        dplace_offset = opaque_slots['task_offsets']
+
+        if task_argstr:
+            task_command = "%s %s" % (task_exec, task_argstr)
+        else:
+            task_command = task_exec
+
+
+        env_string = ''
+        env_list   = self.EXPORT_ENV_VARIABLES + task_env.keys()
+        if env_list:
+            if 'hydra' in self.launch_version:
+                env_string = '-envlist "%s"' % ','.join(env_list)
+
+            elif 'openmpi' in self.launch_version:
+                env_string = ''
+                for var in task_env:
+                    env_string += '-x "%s" ' % var
+
+            else:
+                # this is a crude version of env transplanting where we prep the
+                # shell command line.  We likely won't survive any complicated vars
+                # (multiline, quotes, etc)
+                env_string = ' '
+                for var in env_string:
+                    env_string += '%s="$%s" ' % (var, var)
+
+
+        command = "%s -np %d %s -c %d-%d %s %s" % \
+                  (self.dplace_command, task_cores, self.launch_command,
+                   dplace_offset, dplace_offset+task_cores-1, 
+                   env_string, task_command)
+
+        return command, None
+
+
+# ------------------------------------------------------------------------------
+
