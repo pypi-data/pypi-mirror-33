ad0788de15b000ea2eb1b2cffdc251446581897c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 05:07:20 +0000

    Add package thug/0.9.26/thug-0.9.26.tar.gz

---
+++ packages/thug/thug-0.9.26.tar.gz/thug-0.9.26/thug/DOM/thug.js
@@ -0,0 +1,17 @@
+/*
+ * Window object 
+ * Javascript objects visibility
+ */
+
+//this.eval     = window.eval;
+this.unescape = window.unescape;
+window = this;
+
+Object.defineProperty(window, "location", {
+	set: function(value){
+		window.setLocation(value);
+	},
+	get: function(){
+		return window.getLocation();
+	}
+});
