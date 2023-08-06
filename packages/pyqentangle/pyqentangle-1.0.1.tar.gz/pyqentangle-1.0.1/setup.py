06f1c54decad5afb56c634cfe7eaf855d8245540
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:49:12 +0000

    Add package pyqentangle/1.0.1/pyqentangle-1.0.1.tar.gz

---
+++ packages/pyqentangle/pyqentangle-1.0.1.tar.gz/pyqentangle-1.0.1/setup.py
@@ -0,0 +1,35 @@
+from setuptools import setup
+from Cython.Build import cythonize
+import numpy as np
+
+def readme():
+    with open('README.md') as f:
+        return f.read()
+
+setup(name='pyqentangle',
+      version="1.0.1",
+      description="Quantum Entanglement for Python",
+      long_description="Schmidt decomposition for discrete and continuous bi-partite quantum systems",
+      classifiers=[
+          "Topic :: Scientific/Engineering :: Physics",
+          "Topic :: Scientific/Engineering :: Mathematics",
+          "License :: OSI Approved :: MIT License",
+          "Programming Language :: Python :: 2.7",
+          "Programming Language :: Python :: 3.5",
+          "Programming Language :: Python :: 3.6",
+      ],
+      keywords="quantum physics Schmidt decompostion entanglement",
+      url="https://github.com/stephenhky/pyqentangle",
+      author="Kwan-Yuet Ho",
+      author_email="stephenhky@yahoo.com.hk",
+      license='MIT',
+      packages=['pyqentangle'],
+      include_dirs=[np.get_include()],
+      setup_requires=['Cython', 'numpy', ],
+      install_requires=['numpy',],
+      tests_require=['unittest2', 'numpy', 'scipy',],
+      ext_modules=cythonize(['pyqentangle/interpolate_nocheck.pyx',
+                             'pyqentangle/bipartite_reddenmat_nocheck.pyx']),
+      test_suite="test",
+      include_package_data=True,
+      zip_safe=False)
