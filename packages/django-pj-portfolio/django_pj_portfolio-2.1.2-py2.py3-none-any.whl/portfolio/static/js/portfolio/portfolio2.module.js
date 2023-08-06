1e407a7c5da3a745bf586437792f15736e99cc92
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:43:52 +0000

    Add package django-pj-portfolio/2.1.2/django_pj_portfolio-2.1.2-py2.py3-none-any.whl

---
+++ packages/django-pj-portfolio/django_pj_portfolio-2.1.2-py2.py3-none-any.whl/portfolio/static/js/portfolio/portfolio2.module.js
@@ -0,0 +1,20 @@
+(function () { 
+    'use strict';
+
+    angular
+        .module('portfolio', [
+            'portfolio.account.summary',
+            'portfolio.positions',
+            'portfolio.securities',
+            'loadingSpinner',
+            'portfolio.account',
+            'portfolio.currency',
+            'portfolio.config',
+            'angular-toArrayFilter',
+        ]);
+
+    angular
+        .module('portfolio.config', []);
+
+})(); 
+
