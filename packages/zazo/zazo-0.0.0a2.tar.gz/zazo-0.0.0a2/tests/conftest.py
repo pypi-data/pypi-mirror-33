e4940e83825ba123e27657cf522528925f1e56d6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:46:19 +0000

    Add package zazo/0.0.0a2/zazo-0.0.0a2.tar.gz

---
+++ packages/zazo/zazo-0.0.0a2.tar.gz/zazo-0.0.0a2/tests/conftest.py
@@ -0,0 +1,39 @@
+import os
+
+import pytest
+
+from tests.lib.yaml.fixtures import YamlFixtureFile
+
+
+def pytest_collect_file(parent, path):
+    if path.ext == ".yaml" and ".ignore" not in str(path):
+        return YamlFixtureFile(path, parent)
+
+
+def pytest_collection_modifyitems(session, config, items):
+    # Mark Tests based on which directory they live in
+    for item in items:
+        if isinstance(item, YamlFixtureFile):
+            item.add_marker(pytest.mark.yaml)
+            item.add_marker(pytest.mark.integration)
+
+        if not hasattr(item, "module"):
+            continue
+
+        # Get path of the file
+        module_path = os.path.relpath(
+            item.module.__file__, os.path.commonprefix([__file__, item.module.__file__])
+        )
+        # Mark the type of test
+        module_root_dir = module_path.split(os.pathsep)[0]
+        if module_root_dir.startswith("integration"):
+            item.add_marker(pytest.mark.integration)
+        elif module_root_dir.startswith("unit"):
+            item.add_marker(pytest.mark.unit)
+        elif module_root_dir.startswith("lib"):
+            item.add_marker(pytest.mark.integration)
+        else:
+            msg = "Unknown test type (filename = {0}, root_dir = {1})".format(
+                module_path, module_root_dir
+            )
+            raise RuntimeError(msg)
---
+++ packages/zazo/zazo-0.0.0a2.tar.gz/zazo-0.0.0a2/tests/lib/yaml/fixtures.py
@@ -0,0 +1,135 @@
+"""Helpers for YAML-file based tests
+"""
+
+from __future__ import absolute_import
+
+import traceback
+
+import pytest
+import yaml
+from packaging.requirements import InvalidRequirement, Requirement
+
+from zazo.api import BackTrackingResolver, CannotSatisfy
+
+from .abcs import YAMLProvider
+from .exceptions import YAMLException
+from .expand import expand_shorthands
+from .translate import translate_specification
+from .verify import verify_installation_result
+
+
+class YamlFixtureItem(pytest.Item):
+
+    def __init__(self, name, parent, spec):
+        super(YamlFixtureItem, self).__init__(name, parent)
+        self.spec = spec
+
+        self._actions = {
+            "install": (self._do_install, self._check_install),
+            "uninstall": (self._do_uninstall, self._check_uninstall),
+        }
+
+    # Called when the tests fail
+    def repr_failure(self, excinfo):
+        error = excinfo.value
+        if isinstance(error, YAMLException):
+            return str(error)
+        elif isinstance(error, AssertionError):
+            msg = ": {}".format(error.args[0]) if error.args else ""
+            return "assertion failed" + msg
+        else:
+            trace = traceback.format_exception(excinfo.type, excinfo.value, excinfo.tb)
+            return "".join(trace) + "\nError occurred."
+
+    def reportinfo(self):
+        return self.fspath, 0, "yaml-test: %s" % self.name
+
+    def runtest(self):
+        # Convert things into something usable.
+        expanded_spec = expand_shorthands(self.name, self.spec)
+        test_spec = translate_specification(self.name, expanded_spec)
+
+        # Create a Provider, using information
+        provider = YAMLProvider(test_spec["candidates"], test_spec["dependencies"])
+
+        for action, expected in test_spec["subtests"]:
+            result = self._perform_action(action, provider)
+            self._check_action_result(action, result, expected)
+
+    ###### Helpers ######
+    def _perform_action(self, action, provider):
+        verb = action["action"]
+        func = getattr(self, "_do_" + verb, None)
+        if func is None:
+            raise Exception("Can not perform verb {!r}.".format(verb))
+
+        return func(provider, action)
+
+    def _check_action_result(self, action, result, expected):
+        verb = action["action"]
+        func = getattr(self, "_check_" + verb, None)
+        if func is None:
+            raise Exception("Can not check verb {!r}.".format(verb))
+
+        func(result, expected)
+
+    ###### Actions ######
+    def _do_uninstall(self, provider, names):
+        # _check_list(names, "uninstall items", str)
+        provider.uninstall_names(names)
+
+    def _check_uninstall(self, result, expected):
+        assert result is None
+        assert expected == "tada"
+
+    def _do_install_prepare_reqs(self, action):
+        """Create a list of requirements from given items.
+        """
+
+        def _make_req(string):
+            try:
+                return Requirement(string)
+            except Exception:
+                raise YAMLException(
+                    "Could not parse requirement {!r} from {!r}",
+                    string,
+                    action["items"],
+                )
+
+        return [_make_req(r) for r in action["items"]]
+
+    def _do_install(self, provider, items):
+        """Compose a list of requirements and then run the resolver to install them.
+        """
+        requirements = self._do_install_prepare_reqs(items)
+        result = self._run_resolver(requirements, provider)
+
+        return result
+
+    def _check_install(self, result, expected):
+        verify_installation_result(result, expected)
+
+    def _run_resolver(self, requirements, provider):
+        resolver = BackTrackingResolver(provider)
+        try:
+            to_install = resolver.resolve(requirements)
+        except CannotSatisfy as e:
+            return e
+        else:
+            return to_install
+
+
+class YamlFixtureFile(pytest.File):
+
+    def _compose_tests(self, data):
+        # _check_list(data, "test data")
+        for i, item in enumerate(data):
+            name = "{}[{}]".format(self.fspath.basename, i)
+            yield YamlFixtureItem(name, self, item)
+
+    def collect(self):
+        loader = yaml.CLoader if hasattr(yaml, "CLoader") else yaml.Loader
+        with self.fspath.open() as f:
+            data = yaml.load(f.read(), Loader=loader)
+        for test in self._compose_tests(data):
+            yield test
