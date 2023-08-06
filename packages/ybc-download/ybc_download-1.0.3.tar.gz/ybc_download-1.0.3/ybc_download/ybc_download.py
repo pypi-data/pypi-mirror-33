38fa6d08749ee9d09128be5f16e22e2dbdf4529b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:17:33 +0000

    Add package ybc-download/1.0.3/ybc_download-1.0.3.tar.gz

---
+++ packages/ybc-download/ybc_download-1.0.3.tar.gz/ybc_download-1.0.3/ybc_download/ybc_download.py
@@ -0,0 +1,30 @@
+import os
+import urllib.request as rq
+import zipfile
+
+def create_my_world():
+    url = 'https://www.yuanfudao.com/tutor-ybc-course-api/res.zip'
+    filename = 'res.zip'
+    rq.urlretrieve(url,filename)
+    zip_file = zipfile.ZipFile(filename)
+    for files in zip_file.namelist():
+        zip_file.extract(files,'./')
+    zip_file.close()
+    os.remove(filename)
+
+def create_my_world_l2():
+    url = 'https://www.yuanfudao.com/tutor-ybc-course-api/res2.zip'
+    filename = 'res2.zip'
+    rq.urlretrieve(url,filename)
+    zip_file = zipfile.ZipFile(filename)
+    for files in zip_file.namelist():
+        zip_file.extract(files,'./')
+    zip_file.close()
+    os.remove(filename)
+
+def main():
+    create_my_world()
+    create_my_world_l2()
+
+if __name__ == '__main__':
+    main()
