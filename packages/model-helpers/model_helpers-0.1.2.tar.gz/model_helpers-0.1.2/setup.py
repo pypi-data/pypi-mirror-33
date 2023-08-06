f1bd09d87d6a92785d6ad4f5f63fa748ac40e2c1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:21:28 +0000

    Add package model-helpers/0.1.2/model_helpers-0.1.2.tar.gz

---
+++ packages/model-helpers/model_helpers-0.1.2.tar.gz/model_helpers-0.1.2/setup.py
@@ -0,0 +1,18 @@
+from setuptools import setup
+
+setup(name='model_helpers',
+      packages=['model_helpers'],
+      version='0.1.2',
+      description='Tools for helping construct/train models',
+      url='https://github.com/bpben/model_helpers',
+      download_url='https://github.com/bpben/model_helpers/archive/0.1.tar.gz',
+      author='BB',
+      author_email='yksrotab@gmail.com',
+      keywords =  'models ML tuning training',
+      license='MIT',
+      install_requires=['scikit-learn', 'pandas'],
+      python_requires='>=2.6',
+      classifiers=['Development Status :: 3 - Alpha',
+      'Intended Audience :: Developers',
+      'License :: OSI Approved :: MIT License',
+      'Programming Language :: Python :: 2.7'])
