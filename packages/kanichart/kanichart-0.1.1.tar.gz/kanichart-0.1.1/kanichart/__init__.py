b01b8414259ca3843d8a8f129831d05c2598d115
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:35:27 +0000

    Add package kanichart/0.1.1/kanichart-0.1.1.tar.gz

---
+++ packages/kanichart/kanichart-0.1.1.tar.gz/kanichart-0.1.1/kanichart/__init__.py
@@ -0,0 +1,8 @@
+"""kanichart - Kan i chart. Easy plotting library."""
+
+__version__ = '0.1.1'
+__author__ = 'fx-kirin'
+__all__ = []
+
+from kanichart._chart import CandlestickCharts
+from kanichart._chart import LineCharts
