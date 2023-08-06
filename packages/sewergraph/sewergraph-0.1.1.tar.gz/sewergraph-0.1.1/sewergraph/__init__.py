b23db3a3ca94167a3185f1e0da71df30e27b2414
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:49:38 +0000

    Add package sewergraph/0.1.1/sewergraph-0.1.1.tar.gz

---
+++ packages/sewergraph/sewergraph-0.1.1.tar.gz/sewergraph-0.1.1/sewergraph/__init__.py
@@ -0,0 +1,13 @@
+################################################################################
+# Module: __init__.py
+# Description: Tool for graph calculations on drainage networks
+# License: MIT, see full license in LICENSE.txt
+# Web: https://github.com/aerispaha/trace
+################################################################################
+
+from .core import *
+from .helpers import *
+from .area_calcs import *
+
+
+__version__ = '0.1.1'
