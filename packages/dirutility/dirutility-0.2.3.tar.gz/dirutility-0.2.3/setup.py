869c7e53b4500ba7f3ea7622c648eb4a525e67a3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:35:56 +0000

    Add package dirutility/0.2.3/dirutility-0.2.3.tar.gz

---
+++ packages/dirutility/dirutility-0.2.3.tar.gz/dirutility-0.2.3/setup.py
@@ -0,0 +1,18 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='dirutility',
+    version='0.2.3',
+    packages=find_packages(),
+    install_requires=[
+        'tqdm',
+        'looptools',
+    ],
+    url='https://github.com/mrstephenneal/dirutility',
+    license='MIT License',
+    author='Stephen Neal',
+    author_email='stephen@stephenneal.net',
+    description='Further improvements to functionality and structure of DirPaths.  More parameters added to further '
+                'tune result set.',
+    long_description='Utility functions for quickly reading directory contents and refactoring folder structure'
+)
