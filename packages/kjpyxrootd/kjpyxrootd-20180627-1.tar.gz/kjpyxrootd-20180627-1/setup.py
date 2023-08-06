c76505fbe1315aa572489e8f4b44949ae9ec4a52
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:16:46 +0000

    Add package kjpyxrootd/20180627-1/kjpyxrootd-20180627-1.tar.gz

---
+++ packages/kjpyxrootd/kjpyxrootd-20180627-1.tar.gz/kjpyxrootd-20180627-1/setup.py
@@ -0,0 +1,36 @@
+from setuptools import setup, Extension
+from setuptools.command.install import install
+
+import subprocess
+import sys
+
+
+class CustomInstall(install):
+    def run(self): 
+        subprocess.call('./install.sh')
+
+
+def get_version():
+    version = subprocess.check_output(['./genversion.sh', '--print-only'])
+    if version.startswith('v'):
+        version = version[1:]
+    version = version.split('-')[0]
+    return version + '/1'
+
+version = get_version()
+
+with open('bindings/python/VERSION_INFO', 'w') as vi:
+    vi.write(version)
+
+
+setup( 
+    name             = 'kjpyxrootd',
+    version          = version,
+    author           = 'XRootD Developers',
+    author_email     = 'xrootd-dev@slac.stanford.edu',
+    url              = 'http://xrootd.org',
+    license          = 'LGPLv3+',
+    description      = "XRootD with Python bindings",
+    long_description = "XRootD with Python bindings",
+    cmdclass        = {'install': CustomInstall}
+)
