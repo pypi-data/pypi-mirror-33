83f58265019441485e5cae04ecac71cdc38dfe2f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 05:06:10 +0000

    Add package neze-bloch-sphere/0b1/neze-bloch-sphere-0b1.tar.gz

---
+++ packages/neze-bloch-sphere/neze-bloch-sphere-0b1.tar.gz/neze-bloch-sphere-0b1/setup.py
@@ -0,0 +1,19 @@
+import setuptools
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name='neze-bloch-sphere',
+    version='0b1',
+    author="Clement Durand",
+    author_email="durand.clement.13@gmail.com",
+    description="Bloch sphere simulation",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="",
+    packages=setuptools.find_packages(),
+    classifiers=(
+        "Programming Language :: Python :: 3",
+    ),
+    install_requires=['matplotlib','numpy'],
+)
