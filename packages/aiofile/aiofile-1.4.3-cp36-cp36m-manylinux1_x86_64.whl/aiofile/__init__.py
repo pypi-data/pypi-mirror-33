88b3d40b4899e4b37a489170ccb78e02ab4983d9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:09:42 +0000

    Add package aiofile/1.4.3/aiofile-1.4.3-cp36-cp36m-manylinux1_x86_64.whl

---
+++ packages/aiofile/aiofile-1.4.3-cp36-cp36m-manylinux1_x86_64.whl/aiofile/__init__.py
@@ -0,0 +1,16 @@
+from .utils import Reader, Writer, LineReader
+from .version import (
+    __author__, __version__, author_info, package_info,
+    package_license, project_home, team_email, version_info,
+)
+
+
+from .aio import AIOFile
+
+
+__all__ = (
+    'AIOFile',
+    'Reader',
+    'LineReader',
+    'Writer',
+)
