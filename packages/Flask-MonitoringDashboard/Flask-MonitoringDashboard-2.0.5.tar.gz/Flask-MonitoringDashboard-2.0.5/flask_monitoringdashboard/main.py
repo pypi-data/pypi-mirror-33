5ca931a96d61eb310500daaa97cd12f857cdbf7b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:20:50 +0000

    Add package Flask-MonitoringDashboard/2.0.5/Flask-MonitoringDashboard-2.0.5.tar.gz

---
+++ packages/Flask-MonitoringDashboard/Flask-MonitoringDashboard-2.0.5.tar.gz/Flask-MonitoringDashboard-2.0.5/flask_monitoringdashboard/main.py
@@ -0,0 +1,32 @@
+"""
+    This file can be executed for developing purposes.
+    It is not used when the flask_monitoring_dashboard is attached to an existing flask application.
+"""
+import random
+
+from flask import Flask
+
+
+def create_app():
+    import flask_monitoringdashboard as dashboard
+    import time
+
+    app = Flask(__name__)
+
+    dashboard.config.version = '3.1'
+    dashboard.bind(app)
+
+    def f():
+        time.sleep(2)
+        time.sleep(1)
+
+    @app.route('/endpoint')
+    def endpoint():
+        f()
+        return 'Ok'
+
+    return app
+
+
+if __name__ == '__main__':
+    create_app().run(debug=True, threaded=True)
