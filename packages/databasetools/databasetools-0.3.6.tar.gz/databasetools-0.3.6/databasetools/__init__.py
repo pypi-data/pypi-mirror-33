0be11bd6c17523cde71fe6b3f97c23c8d8559e84
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:14:49 +0000

    Add package databasetools/0.3.6/databasetools-0.3.6.tar.gz

---
+++ packages/databasetools/databasetools-0.3.6.tar.gz/databasetools-0.3.6/databasetools/__init__.py
@@ -0,0 +1,7 @@
+from databasetools.csv_tools import ExportCSV
+from databasetools.sqlite import GenerateSQLite, ExecuteSQLite, RunSQLite
+from databasetools.dict import ExportDict
+from databasetools.my_sql import MySQLTools
+
+__name__ = "Database Tools"
+__all__ = ["ExportCSV", "GenerateSQLite", "ExecuteSQLite", "RunSQLite", "ExportDict", "MySQLTools"]
---
+++ packages/databasetools/databasetools-0.3.6.tar.gz/databasetools-0.3.6/databasetools/dict/dict.py
@@ -0,0 +1,25 @@
+import numpy as np
+import os
+
+
+class ExportDict:
+    def __init__(self, name):
+        self.dict = dict
+        self.save_name = name + ".npy"
+
+    def __iter__(self):
+        return iter(self.read_dictionary)
+
+    def save(self, dict):
+        try:
+            np.save(self.save_name, dict)
+            print("\nnpy saved to " + os.getcwd())
+        except:
+            print("\nnpy not saved")
+
+    def load(self):
+        try:
+            self.read_dictionary = np.load(self.save_name).item()
+            return self.read_dictionary
+        except IOError:
+            print('\nError: Unable to load file ' + str(self.save_name))
