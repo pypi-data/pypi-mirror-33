ee091b9e2b5a4c4a1ad1ab18adf02daf27e4bc6b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:50:22 +0000

    Add package eosjs-python/0.1.41/eosjs_python-0.1.41.tar.gz

---
+++ packages/eosjs-python/eosjs_python-0.1.41.tar.gz/eosjs_python-0.1.41/eosjs_python/Exceptions.py
@@ -0,0 +1,11 @@
+class GenerateKeysException(Exception):
+    pass
+
+class CreateAccountException(Exception):
+    pass
+
+class PushContractTransactionException(Exception):
+    pass
+
+class GetTableException(Exception):
+	pass
