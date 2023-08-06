ef5b907356994d4e27434254bee466df7012944f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:19:05 +0000

    Add package createpyproject/0.0.6/createpyproject-0.0.6.tar.gz

---
+++ packages/createpyproject/createpyproject-0.0.6.tar.gz/createpyproject-0.0.6/setup.py
@@ -0,0 +1,41 @@
+import setuptools
+
+with open('README.md', 'r') as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name='createpyproject',
+    version='0.0.6',
+    author="Srijan Manandhar",
+    author_email="srijan.manandhar@gmail.com",
+    description='Test project to use python script for creating barebone python project folder structure',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    url='https://github.com/srijanss/createpy',
+    packages=setuptools.find_packages(),
+    classifiers=(
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ),
+    install_requires=[
+        'Click',
+        'jinja2',
+    ],
+    python_requires=">=2.7, >=3.0",
+    data_files=[
+        ('templates', [
+            'templates/example_README.md',
+            'templates/example_LICENSE.txt',
+            'templates/example_setup.py',
+            'templates/.gitignore',
+            'templates/.travis.yml',
+            'templates/Makefile',
+            'templates/licenses.json'
+            ])
+    ],
+    entry_points='''
+    [console_scripts]
+    createpyproject=createpy.cli:main
+    '''
+)
