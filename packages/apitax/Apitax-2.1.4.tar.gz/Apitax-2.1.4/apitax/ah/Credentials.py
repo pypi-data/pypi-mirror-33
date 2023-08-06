543a36bfaa614655da7d0ca7e8eabfd2e1e00dbd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:26:00 +0000

    Add package apitax/2.1.4/Apitax-2.1.4.tar.gz

---
+++ packages/apitax/Apitax-2.1.4.tar.gz/Apitax-2.1.4/apitax/ah/Credentials.py
@@ -0,0 +1,13 @@
+
+
+class Credentials:
+    def __init__(self, username='', password='', token=''):
+        self.username = username
+        self.password = password
+        self.token = token
+        
+    def getAuthObj(self):
+        return {'username': self.username, 'password': self.password, 'token': self.token}
+        	
+    def serialize(self):
+        return str(self.username)
