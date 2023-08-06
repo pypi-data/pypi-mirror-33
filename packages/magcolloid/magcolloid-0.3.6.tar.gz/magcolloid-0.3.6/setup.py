628d45ac3007bbddd6707caedbe00c104749dbd7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:48:24 +0000

    Add package magcolloid/0.3.6/magcolloid-0.3.6.tar.gz

---
+++ packages/magcolloid/magcolloid-0.3.6.tar.gz/magcolloid-0.3.6/setup.py
@@ -0,0 +1,26 @@
+import setuptools
+
+with open("Readme.md","r") as fh:
+    long_description = fh.read()
+    
+setuptools.setup(
+    name="magcolloid",
+    version="0.3.6",
+    author="Antonio Ortiz-Ambriz",
+    author_email="aortiza@gmail.com",
+    description="A set of routines to interface with lammps and setup simulations of magnetic colloidal particles",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/aortiza/magcolloid",
+    packages=setuptools.find_packages(),
+    install_requires = ['numpy','scipy','pandas','matplotlib','pint','jsonpickle'],
+    classifiers=(
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Development Status :: 3 - Alpha",
+        "Operating System :: Microsoft :: Windows",
+        "Operating System :: MacOS :: MacOS X",
+        "Operating System :: Unix",
+    ),
+    include_package_data=True
+)
