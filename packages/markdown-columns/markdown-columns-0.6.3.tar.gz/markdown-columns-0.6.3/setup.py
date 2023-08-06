0dc60e16225b2863618cfd1e3626138891f6d3a4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:48:25 +0000

    Add package markdown-columns/0.6.3/markdown-columns-0.6.3.tar.gz

---
+++ packages/markdown-columns/markdown-columns-0.6.3.tar.gz/markdown-columns-0.6.3/setup.py
@@ -0,0 +1,20 @@
+from setuptools import setup
+
+setup(
+    name='markdown-columns',
+    version='0.6.3',
+    packages=['md_columns'],
+    url='https://github.com/sander76/markdown-columns',
+    license='MIT',
+    author='Sander Teunissen',
+    classifiers=[
+        'Programming Language :: Python',
+        'Topic :: Software Development :: Libraries :: Python Modules',
+        'License :: OSI Approved :: BSD License',
+        'Operating System :: OS Independent',
+        'Development Status :: 5 - Production/Stable',
+        'Intended Audience :: Developers',
+        'Programming Language :: Python :: 3'],
+    author_email='',
+    description='css columns for Python Markdown'
+)
