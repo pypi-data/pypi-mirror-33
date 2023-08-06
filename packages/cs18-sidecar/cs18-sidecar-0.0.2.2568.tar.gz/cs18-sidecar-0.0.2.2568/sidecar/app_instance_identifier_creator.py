f1cb93607cdd3694255ff1d40922851ea4dc3d0d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:22:54 +0000

    Add package cs18-sidecar/0.0.2.2568/cs18-sidecar-0.0.2.2568.tar.gz

---
+++ packages/cs18-sidecar/cs18-sidecar-0.0.2.2568.tar.gz/cs18-sidecar-0.0.2.2568/sidecar/app_instance_identifier_creator.py
@@ -0,0 +1,18 @@
+from abc import abstractmethod, ABCMeta
+
+from sidecar.app_instance_identifier import AppInstanceIdentifier
+
+
+class IAppInstanceIdentifierCreator:
+    __metaclass__ = ABCMeta
+
+    def __init__(self) -> None:
+        pass
+
+    @abstractmethod
+    def create(self, app_name: str, ip_address: str, additional_data: {}) -> AppInstanceIdentifier:
+        raise NotImplementedError
+
+    @abstractmethod
+    def create_from_instance_id(self, app_name: str, instance_id: str) -> AppInstanceIdentifier:
+        raise NotImplementedError
