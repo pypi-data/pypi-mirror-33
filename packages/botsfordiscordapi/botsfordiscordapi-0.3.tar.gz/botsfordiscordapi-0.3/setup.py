218da0c35206c47ca954b2fefdbdb8a8a1b8c593
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:49:56 +0000

    Add package botsfordiscordapi/0.3/botsfordiscordapi-0.3.tar.gz

---
+++ packages/botsfordiscordapi/botsfordiscordapi-0.3.tar.gz/botsfordiscordapi-0.3/setup.py
@@ -0,0 +1,18 @@
+from setuptools import setup
+
+setup(
+	name="botsfordiscordapi",
+	version="0.3",
+	description="A bots for discord api handler made by Luke#6723",
+	long_description="For setup info check gitlab: https://gitlab.com/Luke-6723/botsforsdiscordapi",
+	url="https://gitlab.com/Luke-6723/botsfordiscordapi",
+	author="",
+	email="",
+	license="",
+	packages=[],
+	zip_safe=False,
+	scripts=["botsfordiscordapi.py"],
+	install_requires=[
+		"requests",
+	]
+)
