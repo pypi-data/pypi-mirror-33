378f92e9cc3b5bc43c9f0af3c71e81c0e5bfe830
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:37:36 +0000

    Add package teerace-tml/0.3/teerace-tml-0.3.tar.gz

---
+++ packages/teerace-tml/teerace-tml-0.3.tar.gz/teerace-tml-0.3/setup.py
@@ -0,0 +1,43 @@
+"""
+    setup for tml.
+
+    :copyright: 2010-2011 by the TML Team, see AUTHORS for more details.
+    :license: GNU GPL, see LICENSE for more details.
+"""
+
+import os
+from setuptools import setup, find_packages
+
+
+def read_file(filename):
+    """Read a file into a string"""
+    path = os.path.abspath(os.path.dirname(__file__))
+    filepath = os.path.join(path, filename)
+    try:
+        return open(filepath).read()
+    except IOError:
+        return ""
+
+
+# Use the docstring of the __init__ file to be the description
+DESC = " ".join(__import__("tml").__doc__.splitlines()).strip()
+
+maps = [os.sep.join(("tml/maps", name)) for name in os.listdir("tml/maps")]
+mapres = [os.sep.join(("tml/mapres", name)) for name in os.listdir("tml/mapres")]
+setup(
+    name="teerace-tml",
+    version=__import__("tml").get_version().replace(" ", "-"),
+    url="",
+    author="TML Team",
+    author_email="",
+    description=DESC,
+    long_description=read_file("README"),
+    packages=find_packages(),
+    include_package_data=True,
+    install_requires=read_file("requirements.txt"),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: GNU General Public License (GPL)",
+    ],
+    data_files=[("tml/maps", maps), ("tml/mapres", mapres)],
+)
---
+++ packages/teerace-tml/teerace-tml-0.3.tar.gz/teerace-tml-0.3/tml/__init__.py
@@ -0,0 +1,29 @@
+"""
+    tml
+
+    :copyright: 2010-2011 by the TML Team, see AUTHORS for more details.
+    :license: GNU GPL, see LICENSE for more details.
+"""
+__version_info__ = {
+    "major": 0,
+    "minor": 3,
+    "micro": 0,
+    "releaselevel": "final",
+    "serial": 1,
+}
+
+
+def get_version():
+    """
+    Return the formatted version information
+    """
+    vers = ["%(major)i.%(minor)i" % __version_info__]
+
+    if __version_info__["micro"]:
+        vers.append(".%(micro)i" % __version_info__)
+    if __version_info__["releaselevel"] != "final":
+        vers.append("%(releaselevel)s%(serial)i" % __version_info__)
+    return "".join(vers)
+
+
+__version__ = get_version()
