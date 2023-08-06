b591a5a22e12251a7e6d4230c29903486ad02c72
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:38:52 +0000

    Add package magician/0.0.1/magician-0.0.1.tar.gz

---
+++ packages/magician/magician-0.0.1.tar.gz/magician-0.0.1/hummingbird/downloader/base.py
@@ -0,0 +1,18 @@
+from typing import Callable
+from tornado.httpclient import HTTPRequest
+
+
+class BaseDownloader(object):
+    """
+    Interface for Downloader
+    """
+
+    def fetch(self, request: HTTPRequest, callback: Callable) -> bool:
+        """
+        Fetch page, and call callback.
+        Return True is request start, else return False.
+        :param request: HTTPRequest object
+        :param callback: func with HTTPResponse object as arguments
+        :return: boolean, start fetch current page or not.
+        """
+        pass
