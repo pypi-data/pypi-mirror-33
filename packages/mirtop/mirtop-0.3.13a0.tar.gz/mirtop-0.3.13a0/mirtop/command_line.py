580f68352e4571fc5d5fbea842edd4739a7b63a8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 07:12:44 +0000

    Add package mirtop/0.3.13a0/mirtop-0.3.13a0.tar.gz

---
+++ packages/mirtop/mirtop-0.3.13a0.tar.gz/mirtop-0.3.13a0/mirtop/command_line.py
@@ -0,0 +1,49 @@
+"""Main functions to simulates reads"""
+
+import sys
+
+from mirtop.libs.logger import initialize_logger
+from mirtop.libs.parse import parse_cl
+from mirtop.libs.simulator import simulate
+from mirtop.gff import reader
+from mirtop.gff.stats import stats
+from mirtop.gff.compare import compare
+from mirtop.gff.convert import convert_gff_counts
+from mirtop.exporter import isomirs
+import mirtop.libs.logger as mylog
+
+import time
+
+FILE_FORMAT = "gtf"
+
+def main(**kwargs):
+    kwargs = parse_cl(sys.argv[1:])
+    initialize_logger(kwargs['args'].out, kwargs['args'].debug, kwargs['args'].print_debug)
+    logger = mylog.getLogger(__name__)
+    start = time.time()
+
+    if "gff" in kwargs:
+        logger.info("Run annotation")
+        reader(kwargs["args"])
+    elif "stats" in kwargs:
+        logger.info("Run stats.")
+        stats(kwargs["args"])
+    elif "compare" in kwargs:
+        logger.info("Run compare.")
+        compare(kwargs["args"])
+    elif "simulator" in kwargs:
+        logger.info("Run simulation")
+        simulate(kwargs["args"])
+    elif "counts" in kwargs:
+        logger.info("Run convert of GFF to TSV containing expression")
+        convert_gff_counts(kwargs["args"])
+    elif "export" in kwargs:
+        logger.info("Run export of GFF into other format.")
+        isomirs.convert(kwargs["args"])
+    elif "check" in kwargs["args"]:
+        logger.info("Not yet ready: This will check GFF files.")
+    elif "query" in kwargs["args"]:
+        logger.info("Not yet ready: This will allow queries to GFF files.")
+    elif "convert" in kwargs["args"]:
+        logger.info("Not yet ready: This will output tabular format from GFF files.")
+    logger.info('It took %.3f minutes' % ((time.time()-start)/60))
---
+++ packages/mirtop/mirtop-0.3.13a0.tar.gz/mirtop-0.3.13a0/mirtop/gff/header.py
@@ -0,0 +1,31 @@
+import mirtop.libs.logger as mylog
+logger = mylog.getLogger(__name__)
+
+def create(samples, database, custom, filter = None):
+    header = ""
+    header += _get_gff_version()
+    header += _get_database(database)
+    header += _get_samples(samples)
+    header += custom
+    return header
+
+def _get_gff_version():
+    return "## GFF3 adapted for miRNA sequencing data. VERSION 0.0.1\n"
+
+def _get_samples(samples):
+    return "## COLDATA: %s" % ",".join(samples)
+
+def _get_database(database):
+    return "## source-ontology: %s\n" % database
+
+def _filter(filters):
+    if not filters:
+        return "## FILTER: PASS\n"
+    return "## FILTER: %s" % ";\n".join(filters)
+
+def read_samples(fn):
+    with open(fn) as inh:
+        for line in inh:
+            if line.startswith("## COLDATA"):
+                return line.strip().split(": ")[1].strip().split(",")
+    raise ValueError("%s doesn't contain COLDATA header." % fn)
