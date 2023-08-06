7173c4955a740d2952bedcd02ab4519cfd421431
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:44:00 +0000

    Add package enable/4.7.2/enable-4.7.2.tar.gz

---
+++ packages/enable/enable-4.7.2.tar.gz/enable-4.7.2/kiva/tests/points_in_polygon_test.py
@@ -0,0 +1,135 @@
+import unittest
+import warnings
+
+from numpy import array, allclose
+
+from kiva.api import points_in_polygon
+from kiva.agg import points_in_polygon as points_in_polygon_deprecated
+
+
+class TestPointsInPolygon(unittest.TestCase):
+
+    def test_deprecated_import(self):
+        polygon = array(((0.0, 0.0), (10.0, 0.0), (0.0, 10.0)))
+        points = array(((5.0, 5.0),))
+
+        with warnings.catch_warnings(record=True) as collector:
+            warnings.simplefilter("always")
+            points_in_polygon_deprecated(points, polygon)
+
+            assert len(collector) == 1
+            warn = collector[0]
+            assert issubclass(warn.category, DeprecationWarning)
+
+    def test_simple_points_in_polygon(self):
+
+        polygon = array(((0.0, 0.0), (10.0, 0.0), (10.0, 10.0), (0.0, 10.0)))
+        points = array(((-1.0, -1.0), (5.0, 5.0), (15.0, 15.0)))
+
+        result = points_in_polygon(points, polygon)
+        self.assertTrue(allclose(array([0, 1, 0]), result))
+
+        return
+
+    def test_transposed_points_in_polygon(self):
+
+        polygon = array(((0.0, 10.0, 10.0, 0.0), (0.0, 0.0, 10.0, 10.0)))
+        points = array(((-1.0, 5.0, 15.0), (-1.0, 5.0, 15.0)))
+
+        result = points_in_polygon(points, polygon)
+        self.assertTrue(allclose(array([0, 1, 0]), result))
+
+        return
+
+    def test_asymmetric_points_in_polygon(self):
+
+        polygon = array(((0.0, 0.0), (20.0, 0.0), (20.0, 10.0), (0.0, 10.0)))
+        points = array(((5.0, 5.0), (10.0, 5.0), (15.0, 5.0)))
+
+        result = points_in_polygon(points, polygon)
+        self.assertTrue(allclose(array([1, 1, 1]), result))
+
+        return
+
+    def test_rectangle(self):
+
+        vertices = array(((0, 0), (0, 10), (10, 10), (10, 0)))
+
+        # Try the lower left.
+        trial = array(((0, 0),))
+        oe_result = points_in_polygon(trial, vertices)
+        w_result = points_in_polygon(trial, vertices, True)
+        self.assertEqual(0, oe_result[0],
+                         "Lower left corner not in polygon. OEF")
+        self.assertEqual(1, w_result[0],
+                         "Lower left corner not in polygon. Winding")
+
+        # Try the center.
+        trial = array(((5, 5),))
+        oe_result = points_in_polygon(trial, vertices)
+        w_result = points_in_polygon(trial, vertices, True)
+        self.assertEqual(1, oe_result[0],
+                         "Center not in polygon. OEF")
+        self.assertEqual(1, w_result[0],
+                         "Center not in polygon. Winding")
+
+        # Try the center.
+        trial = array(((10, 10),))
+        oe_result = points_in_polygon(trial, vertices)
+        w_result = points_in_polygon(trial, vertices, True)
+        self.assertEqual(1, oe_result[0],
+                         "Top-right in polygon. OEF")
+        self.assertEqual(0, w_result[0],
+                         "Top-right in polygon. Winding")
+
+        return
+
+    def test_center_removed(self):
+        # Tests a polygon which resembles the following:
+        #
+        # 9------8
+        # |      |
+        # |  3---+---------2
+        # |  |   |         |
+        # |  4---+----5    |
+        # |      |    |    |
+        # |      7----6    |
+        # |                |
+        # 0----------------1
+        #
+        # Using the winding rule, the inner square containing the edge (3,4)
+        # is inside the polygon, while using the odd-even rule, it is outside.
+        # The inner square with containing the edge (5,6) is outside in both
+        # cases.
+
+        vertices = array(((0, 0),
+                          (10, 0),
+                          (10, 8),
+                          (2, 8),
+                          (2, 6),
+                          (8, 6),
+                          (8, 2),
+                          (5, 2),
+                          (5, 10),
+                          (0, 10)))
+
+        trial = array(((3, 7),))
+        oe_result = points_in_polygon(trial, vertices)
+        w_result = points_in_polygon(trial, vertices, True)
+        self.assertEqual(0, oe_result[0],
+                         "Interior polygon inside: odd-even")
+        self.assertEqual(1, w_result[0],
+                         "Interior polygon outside: winding")
+
+        trial = array(((6, 5),))
+        oe_result = points_in_polygon(trial, vertices)
+        w_result = points_in_polygon(trial, vertices, True)
+        self.assertEqual(0, oe_result[0],
+                         "Interior polygon inside: odd-even")
+        self.assertEqual(0, w_result[0],
+                         "Interior polygon inside: winding")
+
+
+if __name__ == "__main__":
+    unittest.main()
+
