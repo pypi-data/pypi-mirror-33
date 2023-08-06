270621d0f5b6198d526e415a48742b4b376f14e6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:11:27 +0000

    Add package kameris-formats/1.0.0/kameris-formats-1.0.0.tar.gz

---
+++ packages/kameris-formats/kameris-formats-1.0.0.tar.gz/kameris-formats-1.0.0/setup.py
@@ -0,0 +1,29 @@
+from setuptools import setup, find_packages
+
+
+setup(
+    name='kameris-formats',
+    version='1.0.0',
+    description=('Reader and writer implementations for the file formats used '
+                 'by kameris-backend.'),
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+        'Topic :: Scientific/Engineering'
+    ],
+    author='Stephen',
+    author_email='stephsolis@gmail.com',
+    url='https://github.com/stephensolis/kameris-formats/',
+    license='MIT',
+    packages=find_packages(),
+    install_requires=[
+        'numpy',
+        'scipy'
+    ]
+)
