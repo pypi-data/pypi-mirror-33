c00fd2308b109bf58204f8a0330c8662dd3b9ee9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:34:16 +0000

    Add package pyledgerqrl/0.5.1/pyledgerqrl-0.5.1.tar.gz

---
+++ packages/pyledgerqrl/pyledgerqrl-0.5.1.tar.gz/pyledgerqrl-0.5.1/setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(name='pyledgerqrl',
+      version='0.5.1',
+      description='python module for Ledger Nano S',
+      url='https://github.com/theQRL/ledger-qrl',
+      author='The QRL Team',
+      author_email='info@theqrl.org',
+      license='MIT',
+      packages=['pyledgerqrl'],
+      install_requires=['ledgerblue>=0.1.18'],
+      zip_safe=False)
