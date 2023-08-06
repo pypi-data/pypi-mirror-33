f4925504017ee6c9353566eb8eb33fb58d172d81
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:35:27 +0000

    Add package law/0.0.27/law-0.0.27.tar.gz

---
+++ packages/law/law-0.0.27.tar.gz/law-0.0.27/law/contrib/slack/parameter.py
@@ -0,0 +1,61 @@
+# -*- coding: utf-8 -*-
+
+"""
+Slack-related parameters.
+"""
+
+
+import collections
+
+from law.config import Config
+from law.parameter import NotifyParameter
+from law.contrib.slack.notification import notify_slack
+
+
+class NotifySlackParameter(NotifyParameter):
+
+    def __init__(self, *args, **kwargs):
+        super(NotifySlackParameter, self).__init__(*args, **kwargs)
+
+        if not self.description:
+            self.description = "when true, and the task's run method is decorated with " \
+                "law.decorator.notify, a slack notification is sent once the task finishes"
+
+    @staticmethod
+    def notify(success, title, content, token=None, channel=None, mention_user=None, **kwargs):
+        # test import
+        import slackclient  # noqa: F401
+
+        if not mention_user:
+            cfg = Config.instance()
+            mention_user = cfg.get_expanded("notifications", "slack_mention_user")
+
+        # title with slack markup
+        slack_title = "Notification from *{}*".format(content["Task"])
+        if mention_user:
+            slack_title += " (@{})".format(mention_user)
+        del content["Task"]
+
+        # markup for traceback
+        if "Traceback" in content:
+            content["Traceback"] = "```{}```".format(content["Traceback"])
+
+        # prepend the status text to the message content
+        parts = list(content.items())
+        status_text = "success :tada:" if success else "failure :exclamation:"
+        parts.insert(0, ("Status", status_text))
+        content = collections.OrderedDict(parts)
+
+        # attachment color and fallback
+        color = "#4bb543" if success else "#ff0033"
+        fallback = "*{}*\n\n".format(title)
+
+        # send the notification
+        return notify_slack(slack_title, content, attachment_color=color,
+            attachment_fallback=fallback, token=token, channel=channel, **kwargs)
+
+    def get_transport(self):
+        return {
+            "func": self.notify,
+            "raw": True,
+        }
