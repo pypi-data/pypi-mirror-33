2d91726fd97575662d28352f1a628120bdd97631
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:44:14 +0000

    Add package haps/1.0.5/haps-1.0.5-py3-none-any.whl

---
+++ packages/haps/haps-1.0.5-py3-none-any.whl/chaps/__init__.py
@@ -0,0 +1,8 @@
+from haps.container import (Container, Inject, inject, base, egg,
+                             INSTANCE_SCOPE, SINGLETON_SCOPE, scope, Egg)
+
+from haps import scopes
+
+
+__all__ = [Container, Inject, inject, base, egg, INSTANCE_SCOPE,
+           SINGLETON_SCOPE, scope, Egg, scopes]
