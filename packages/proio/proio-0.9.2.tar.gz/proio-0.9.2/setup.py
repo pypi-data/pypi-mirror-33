057c179e614a6be2d241d04384611ec0244a921c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:22:19 +0000

    Add package proio/0.9.2/proio-0.9.2.tar.gz

---
+++ packages/proio/proio-0.9.2.tar.gz/proio-0.9.2/setup.py
@@ -0,0 +1,18 @@
+from glob import glob
+from os.path import basename
+from os.path import splitext
+from setuptools import setup
+
+models = ['proio.model.' + splitext(basename(model))[0] for model in glob('proio/model/*')]
+models = list(filter(lambda model: model != 'proio.model.__init__', models))
+
+setup(name='proio',
+      version='0.9.2',
+      description='Library for reading and writing proio files and streams',
+      url='http://github.com/decibelcooper/proio',
+      author='David Blyth',
+      author_email='dblyth@anl.gov',
+      license='None',
+      packages=['proio', 'proio.proto', 'proio.model'] + models,
+      install_requires=['protobuf', 'lz4'],
+      zip_safe=True)
