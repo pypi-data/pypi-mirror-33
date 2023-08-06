d454a36bedd5e50cc536b743962803172a057a7f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:17:38 +0000

    Add package m-singleton/0.3/m-singleton-0.3.tar.gz

---
+++ packages/m-singleton/m-singleton-0.3.tar.gz/m-singleton-0.3/setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+setup(name='m-singleton',
+      version='0.3',
+      description='Setup normal class to SingletonClass',
+      url='https://github.com/mobiovn',
+      author='MOBIO',
+      author_email='contact@mobio.vn',
+      license='MIT',
+      packages=['mobio/libs/Singleton'],
+      install_requires=[])
