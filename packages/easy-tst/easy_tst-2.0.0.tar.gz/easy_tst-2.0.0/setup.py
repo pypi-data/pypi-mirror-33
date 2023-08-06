e93c4c728668d9144de09b8612d6e6b8db8dec59
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:26:01 +0000

    Add package easy-tst/2.0.0/easy_tst-2.0.0.tar.gz

---
+++ packages/easy-tst/easy_tst-2.0.0.tar.gz/easy_tst-2.0.0/setup.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python
+
+from setuptools import setup
+
+execfile('easy_tst/__version__.py')
+
+setup(name='easy_tst',
+      packages=['easy_tst'],
+      version=__version__,
+      description='tst automatically',
+      author='Bruno Siqueira, Joao Espindula',
+      author_email='bruno.siqueira@ccc.ufcg.edu.br',
+      url='https://github.com/brobsc/easy_tst',
+      download_url='https://github.com/brobsc/easy_tst/archive/2.0.0.tar.gz',
+      install_requires=[
+          'pyperclip',
+          'requests',
+      ],
+      scripts=['bin/easy_tst'],
+      )
