33112f847e11623857e894d2efc0512177c7f180
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:48:08 +0000

    Add package kaybee/0.1.18.dev0/kaybee-0.1.18.dev0.tar.gz

---
+++ packages/kaybee/kaybee-0.1.18.dev0.tar.gz/kaybee-0.1.18.dev0/kaybee/__init__.py
@@ -0,0 +1,101 @@
+import dectate
+import importscan
+from sphinx.application import Sphinx
+
+from kaybee import plugins
+from kaybee.app import kb
+from kaybee.plugins.events import EventAction, SphinxEvent
+from kaybee.plugins.settings.model import KaybeeSettings
+
+__version__ = '0.1.18.dev0'
+__title__ = "kaybee"
+
+
+def flush_everything(sphinx_app, sphinx_env):
+    e = sphinx_app.env
+    values = list(e.resources.values()) + list(e.genericpages.values())
+    return [value.docname for value in values]
+
+
+def setup(app: Sphinx):
+    """ Initialize Kaybee as a Sphinx extension """
+
+    # Scan for directives, first in the system, second in the docs project
+    importscan.scan(plugins)
+    dectate.commit(kb)
+
+    app.add_config_value('kaybee_settings', KaybeeSettings(), 'html')
+    bridge = 'kaybee.plugins.postrenderer.config.KaybeeBridge'
+    app.config.template_bridge = bridge
+
+    app.connect('env-updated', flush_everything)
+    app.connect(SphinxEvent.BI.value,
+                # pragma nocover
+                lambda sphinx_app: EventAction.call_builder_init(
+                    kb, sphinx_app)
+                )
+    app.connect(SphinxEvent.EPD.value,
+                # pragma nocover
+                lambda sphinx_app, sphinx_env,
+                       docname: EventAction.call_purge_doc(
+                    kb, sphinx_app, sphinx_env, docname)
+                )
+
+    app.connect(SphinxEvent.EBRD.value,
+                # pragma nocover
+                lambda sphinx_app, sphinx_env,
+                       docnames: EventAction.call_env_before_read_docs(
+                    kb, sphinx_app, sphinx_env, docnames)
+                )
+
+    app.connect(SphinxEvent.DREAD.value,
+                # pragma nocover
+                lambda sphinx_app,
+                       doctree: EventAction.call_env_doctree_read(
+                    kb, sphinx_app, doctree)
+                )
+
+    app.connect(SphinxEvent.DRES.value,
+                # pragma nocover
+                lambda sphinx_app, doctree,
+                       fromdocname: EventAction.call_doctree_resolved(
+                    kb, sphinx_app, doctree, fromdocname)
+                )
+
+    app.connect(SphinxEvent.EU.value,
+                # pragma nocover
+                lambda sphinx_app, sphinx_env: EventAction.call_env_updated(
+                    kb, sphinx_app, sphinx_env)
+                )
+
+    app.connect(SphinxEvent.HCP.value,
+                # pragma nocover
+                lambda sphinx_app: EventAction.call_html_collect_pages(
+                    kb, sphinx_app)
+                )
+
+    app.connect(SphinxEvent.ECC.value,
+                # pragma nocover
+                lambda sphinx_builder,
+                       sphinx_env: EventAction.call_env_check_consistency(
+                    kb, sphinx_builder, sphinx_env)
+                )
+
+    app.connect(SphinxEvent.MR.value,
+                # pragma nocover
+                lambda sphinx_app, sphinx_env, node,
+                       contnode: EventAction.call_missing_reference(
+                    kb, sphinx_app, sphinx_env, node, contnode)
+                )
+
+    app.connect(SphinxEvent.HPC.value,
+                # pragma nocover
+                lambda sphinx_app, pagename, templatename, context,
+                       doctree: EventAction.call_html_page_context(
+                    kb, sphinx_app, pagename, templatename, context, doctree)
+                )
+
+    return dict(
+        version=__version__,
+        parallel_read_safe=False
+    )
---
+++ packages/kaybee/kaybee-0.1.18.dev0.tar.gz/kaybee-0.1.18.dev0/kaybee/plugins/articles/base_article.py
@@ -0,0 +1,98 @@
+from datetime import datetime
+from typing import List
+
+from kaybee.plugins.articles.image_type import ImageModel
+from kaybee.plugins.resources.base_resource import (
+    BaseResource,
+    BaseResourceModel
+)
+
+
+class BaseArticleModel(BaseResourceModel):
+    style: str = None
+    css_class: str = None
+    in_nav: bool = False
+    nav_title: str = None
+    weight: int = 0
+    published: datetime = None
+    excerpt: str = None
+    auto_excerpt: int = 1
+    images: List[ImageModel] = []
+    is_series: bool = False
+    duration: str = None
+    primary_reference: str = None
+
+
+class BaseArticle(BaseResource):
+    props: BaseArticleModel
+    excerpt: str = None  # Stamped on later by the handler
+    toctree: List[str] = []  # Stamped on later by the handler
+
+    def section(self, resources):
+        """ Which section is this in, if any """
+
+        section = [p for p in self.parents(resources) if p.rtype == 'section']
+        if section:
+            return section[0]
+        return None
+
+    def in_navitem(self, resources, nav_href):
+        """ Given  href of nav item, determine if resource is in it """
+
+        # The navhref might end with '/index' so remove it if so
+        if nav_href.endswith('/index'):
+            nav_href = nav_href[:-6]
+
+        return self.docname.startswith(nav_href)
+
+    @property
+    def is_published(self):
+        """ Return true if this resource has published date in the past """
+
+        now = datetime.now()
+        published = self.props.published
+        if published:
+            return published < now
+        return False
+
+    def series(self, resources):
+        # Make sure the parent is a registered resource
+        parent = resources.get(self.parent)
+        if not parent:
+            return None
+
+        # The parent has to declare that it wants to have its children
+        # in a "series", which is false by default.
+        if not getattr(parent.props, 'is_series', False):
+            return None
+
+        results = []
+        for docname in parent.toctree:
+            resource = resources.get(docname)
+            if resource:
+                # We might have a non-resource page in the toctree,
+                # so skip it if true
+                excerpt = getattr(resource, 'excerpt', False)
+                results.append(
+                    dict(
+                        docname=docname,
+                        title=resource.title,
+                        excerpt=excerpt,
+                        current=self.docname == docname
+                    )
+                )
+        return results
+
+    def __json__(self, resources):
+        d = super().__json__(resources)
+        if self.excerpt:
+            d['excerpt'] = self.excerpt
+        section = getattr(self.section(resources), 'docname', '')
+        if section:
+            d['section'] = section
+        if self.toctree:
+            d['toctree'] = self.toctree
+        if self.series:
+            d['series'] = self.series(resources)
+
+        return d
