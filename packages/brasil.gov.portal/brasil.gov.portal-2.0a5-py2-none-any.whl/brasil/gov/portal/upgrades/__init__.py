40a72b65af45c19f73a3876f00bdcbd605e8a335
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:16:43 +0000

    Add package brasil.gov.portal/2.0a5/brasil.gov.portal-2.0a5-py2-none-any.whl

---
+++ packages/brasil.gov.portal/brasil.gov.portal-2.0a5-py2-none-any.whl/brasil/gov/portal/upgrades/__init__.py
@@ -0,0 +1,28 @@
+# -*- coding: utf-8 -*-
+from brasil.gov.portal.logger import logger
+from plone import api
+
+
+def upgrade_profile(setup, profile_id):
+    """Runs all non-installed upgrades for a profile."""
+    step = None
+    # get non-installed upgrades
+    for upgrades in setup.listUpgrades(profile_id):
+        for upgrade in upgrades:
+            if upgrade['done']:
+                continue
+
+            step = upgrade['step']
+            step.doStep(setup)
+            logger.info('Ran upgrade step %s for profile %s' % (
+                step.title,
+                profile_id,
+            ))
+
+    if step and step.dest is not None and step.checker is None:
+        setup.setLastVersionForProfile(profile_id, step.dest)
+
+
+def csscookresources(portal_setup=None):
+
+    api.portal.get_tool('portal_css').cookResources()
