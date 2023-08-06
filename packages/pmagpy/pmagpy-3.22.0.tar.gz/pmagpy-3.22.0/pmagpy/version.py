4afe9a115261a70bb705e492861dec4f18889cd8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:35:00 +0000

    Add package pmagpy/3.22.0/pmagpy-3.22.0.tar.gz

---
+++ packages/pmagpy/pmagpy-3.22.0.tar.gz/pmagpy-3.22.0/pmagpy/version.py
@@ -0,0 +1,8 @@
+"""
+Module contains current pmagpy version number.
+Version number is displayed by GUIs
+and used by setuptools to assign number to pmagpy/pmagpy-cli.
+"""
+
+"pmagpy-3.22.0"
+version = 'pmagpy-3.22.0'
