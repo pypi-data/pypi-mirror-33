65dab50df3231cefd0c10fbb5203ef3ab671d923
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:35:02 +0000

    Add package jnrbase/0.11.0/jnrbase-0.11.0-py3-none-any.whl

---
+++ packages/jnrbase/jnrbase-0.11.0-py3-none-any.whl/jnrbase/_version.py
@@ -0,0 +1,10 @@
+# This is jnrbase version 0.11.0 (2018-07-01)
+# pylint: disable=C0103, C0111, C0121, W0622
+
+dotted = '0.11.0'
+libtool = '11:20'
+hex = 0x000b00
+date = '2018-07-01'
+tuple = (0, 11, 0)
+dict = {'major': 0, 'minor': 11, 'micro': 0}
+web = 'jnrbase/0.11.0'
