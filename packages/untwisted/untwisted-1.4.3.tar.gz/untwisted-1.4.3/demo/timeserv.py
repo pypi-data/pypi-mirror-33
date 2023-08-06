c25321980e94677c332698d521f1d83124cf3254
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:22:04 +0000

    Add package untwisted/1.4.3/untwisted-1.4.3.tar.gz

---
+++ packages/untwisted/untwisted-1.4.3.tar.gz/untwisted-1.4.3/demo/timeserv.py
@@ -0,0 +1,31 @@
+# It imports basic objects.
+from builtins import object
+from untwisted.network import core, Spin, xmap
+from untwisted.iostd import Server, Stdin, ACCEPT, CLOSE
+from time import asctime
+
+class TimeServ(object):
+    def __init__(self, server):
+        xmap(server, ACCEPT, self.handle_accept)
+       
+
+    def handle_accept(self, server, con):
+        Stdin(con)
+
+        con.dump('%s\r\n' % asctime())
+        xmap(con, CLOSE, lambda con, err: lose(con))
+
+
+if __name__ == '__main__':
+    server = Spin()
+    server.bind(('', 1234))
+    server.listen(200)
+
+    Server(server)
+    TimeServ(server)
+    core.gear.mainloop()
+
+
+
+
+
