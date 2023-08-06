1ad38e490e3a9efdd73af261f2b02d736971091e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:52:18 +0000

    Add package BEER-curve/0.3/BEER_curve-0.3.tar.gz

---
+++ packages/BEER-curve/BEER_curve-0.3.tar.gz/BEER_curve-0.3/setup.py
@@ -0,0 +1,23 @@
+from setuptools import setup
+import BEER_curve
+
+def readme():
+    with open('README.md') as f:
+        return f.read()
+
+setup(name='BEER_curve',
+    version=BEER_curve.__version__,
+    description='A very small package to model the BEaming, Ellipsoidal variations, and Reflected/emitted light from low-mass companions',
+    author='Brian Jackson',
+    author_email='bjackson@boisestate.edu',
+    url='https://github.com/decaelus/BEER_curve',
+    download_url = 'https://github.com/decaelus/BEER_curve/archive/0.3.tar.gz',
+    long_description = 'A very small package to model the BEaming, Ellipsoidal variations, and Reflected/emitted light from low-mass companions',
+    classifiers=[
+        'Development Status :: 2 - Pre-Alpha',
+        'Topic :: Scientific/Engineering',
+        'Programming Language :: Python'
+    ],
+    license=['MIT'],
+    packages=['BEER_curve'],
+    install_requires=['PyAstronomy', 'statsmodels'])
