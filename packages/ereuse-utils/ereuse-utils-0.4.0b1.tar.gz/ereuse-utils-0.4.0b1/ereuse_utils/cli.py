1e6b9aa2cc77d7cf9992427d244e890d9d953984
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:16:46 +0000

    Add package ereuse-utils/0.4.0b1/ereuse-utils-0.4.0b1.tar.gz

---
+++ packages/ereuse-utils/ereuse-utils-0.4.0b1.tar.gz/ereuse-utils-0.4.0b1/ereuse_utils/cli.py
@@ -0,0 +1,22 @@
+import enum as _enum
+from typing import Type
+
+import click
+
+
+class Enum(click.Choice):
+    """
+    Enum support for click.
+
+    Use it as a collection: @click.option(..., type=cli.Enum(MyEnum)).
+    Then, this expects you to pass the *name* of a member of the enum.
+
+    From `this github issue <https://github.com/pallets/click/issues/
+    605#issuecomment-277539425>`_.
+    """
+    def __init__(self, enum: Type[_enum.Enum]):
+        self.__enum = enum
+        super().__init__(enum.__members__)
+
+    def convert(self, value, param, ctx):
+        return self.__enum[super().convert(value, param, ctx)]
