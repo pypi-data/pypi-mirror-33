7da9bb34901a94eaf4ea64ee957b98fa820beeb4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 07:14:02 +0000

    Add package kushabk/1.4/kushabk-1.4.tar.gz

---
+++ packages/kushabk/kushabk-1.4.tar.gz/kushabk-1.4/setup.py
@@ -0,0 +1,9 @@
+from distutils.core import setup
+
+setup(name='kushabk',
+      version='1.4',
+      description='This is a python package just made for testing the package and versions ',
+      author='KUSHA B K',
+      url='https://pypi.org/project/kushabk/',
+      author_email='kusha.b.k@gmail.com'
+     )
