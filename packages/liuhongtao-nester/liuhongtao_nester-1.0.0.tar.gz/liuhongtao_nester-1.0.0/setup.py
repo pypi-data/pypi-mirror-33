a63eacbde93e15e7711c955231e71fa16a36ad2f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:19:09 +0000

    Add package liuhongtao-nester/1.0.0/liuhongtao_nester-1.0.0.tar.gz

---
+++ packages/liuhongtao-nester/liuhongtao_nester-1.0.0.tar.gz/liuhongtao_nester-1.0.0/setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+
+setup(
+    name = 'liuhongtao_nester',
+    version = '1.0.0',
+    py_modules = ['liuhongtao_nester'],
+    author = 'liuhongtao',
+    author_email = 'liuhongtao19852013@gmail.com',
+    url = 'https://upload.pypi.org/legacy/',
+    description = 'a simple printer fo nested lists',
+    )
