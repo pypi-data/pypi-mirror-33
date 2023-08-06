ed98ea6f515bc506603608e4690c73d066a758a3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:34:55 +0000

    Add package geomdl/4.0b9/geomdl-4.0b9.tar.gz

---
+++ packages/geomdl/geomdl-4.0b9.tar.gz/geomdl-4.0b9/geomdl/visualization/__init__.py
@@ -0,0 +1,14 @@
+""" NURBS-Python Visualization Component
+
+.. moduleauthor:: Onur Rauf Bingol <orbingol@gmail.com>
+
+"""
+
+__author__ = "Onur Rauf Bingol"
+__version__ = "1.0.0"
+__license__ = "MIT"
+
+
+import numpy
+from geomdl import Abstract
+from geomdl import utilities
