8f7d3069e05056c97ead5c669f2b8afa07c54622
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:15:45 +0000

    Add package printRecursiveList/1.0.0/printRecursiveList-1.0.0.tar.gz

---
+++ packages/printRecursiveList/printRecursiveList-1.0.0.tar.gz/printRecursiveList-1.0.0/setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+
+setup(
+    name            =   'printRecursiveList',
+    version         =   '1.0.0',
+    py_modules      =   ['printRecursiveList'],
+    author          =   'grtPython',
+    authour_email   =   'thakur.pankaj@gmail.com',
+    url             =   'http://thakur.com',
+    description     =   'A Simple recursive printer of recursive lists'
+)
