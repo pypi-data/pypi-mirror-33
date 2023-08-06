a86479f0c0ccd03ff3a3f21a87fc147e45c674a0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:46:46 +0000

    Add package tinkt/0.0.3/tinkt-0.0.3-py2-none-any.whl

---
+++ packages/tinkt/tinkt-0.0.3-py2-none-any.whl/tests/test_new_norm.py
@@ -0,0 +1,6 @@
+#!/usr/bin/env python
+
+from konfluence import Konfluence
+from pylab import *
+from tinkt.klasses import *
+
---
+++ packages/tinkt/tinkt-0.0.3-py2-none-any.whl/tinkt/__init__.py
@@ -0,0 +1,9 @@
+# -*- coding:utf-8 -*-
+
+name = "tinkt"
+
+from . import cm
+from . import cmap_utils
+from . import klasses
+
+from .cmap_utils import get_cmap
