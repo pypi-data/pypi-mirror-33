822bc6b6bc1912861973c26ffc0076da5618300a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:58:48 +0000

    Add package accolade/0.1/accolade-0.1.tar.gz

---
+++ packages/accolade/accolade-0.1.tar.gz/accolade-0.1/setup.py
@@ -0,0 +1,4 @@
+from setuptools import setup, find_packages
+
+setup(name='accolade', version='0.1', packages = find_packages(), scripts=['sa.py']
+      , author= 'J.V.', description= 'Accolade CLI' )
