e1db55541e00b9c0d5becbdb9a14762e68ffafdc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 07:14:06 +0000

    Add package telebakacore/0.1.11/telebakacore-0.1.11-py3-none-any.whl

---
+++ packages/telebakacore/telebakacore-0.1.11-py3-none-any.whl/bots/urls.py
@@ -0,0 +1,14 @@
+from django.urls import path, include
+
+from bots.models import TelegramBot
+from bots.utils import get_plugins
+
+app_name = 'bots'
+
+urlpatterns = []
+
+plugins = get_plugins()
+for telegram_bot in TelegramBot.objects.all():
+    if telegram_bot.url_prefix:
+        urlpatterns.append(path(f'{telegram_bot.url_prefix}/',
+                                include(plugins[telegram_bot.plugin_name].urls, namespace=str(telegram_bot.pk))))
