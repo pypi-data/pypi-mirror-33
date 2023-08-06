e0413b5a3ad4c87384c9a257178d528bc8cea667
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 05:06:28 +0000

    Add package Warrior-pytho-2/0.0.1/Warrior-pytho-2-0.0.1.tar.gz

---
+++ packages/Warrior-pytho-2/Warrior-pytho-2-0.0.1.tar.gz/Warrior-pytho-2-0.0.1/setup.py
@@ -0,0 +1,23 @@
+from setuptools import setup, find_packages
+
+
+setup(
+    name='Warrior-pytho-2',
+    version='0.0.1',
+    packages=['warriorpy', 'warriorpy.towers.beginner', 'warriorpy.towers.intermediate','warriorpy.warriorpyAbilities.src', 'warriorpy.warriorpyCli.src', 'warriorpy.warriorpyCli.src.ui', 'warriorpy.warriorpyCli.templates' ,'warriorpy.warriorpyCore.src', 'warriorpy.warriorpyGeography.src', 'warriorpy.warriorpyUnits.src'],
+    packages_dir={'warriorpy': 'warriorpy'},
+    scripts=['bin/warriorpy.py'],
+    author="Divyansh Dwivedi",
+    author_email="justdvnsh2208@gmail.com",
+    description=("An exciting game of programming and Artificial Intelligence"),
+    long_description=open('README.md').read(),
+    install_requires=['jinja2'],
+    license="MIT",
+    url="https://github.com/justdvnsh/warriorpy",
+    keywords="python ruby javascript rubywarrior warriorjs warriorpy",
+    classifiers=[
+        "Programming Language :: Python",
+        "License :: OSI Approved :: MIT License",
+        "Development Status :: 3 - Alpha"
+    ]
+)
