2d8673b0b90c51f485567a8031036baeecf9aeeb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:07:07 +0000

    Add package wattpad-ebook/0.0.8/wattpad_ebook-0.0.8.tar.gz

---
+++ packages/wattpad-ebook/wattpad_ebook-0.0.8.tar.gz/wattpad_ebook-0.0.8/setup.py
@@ -0,0 +1,25 @@
+try:
+    from setuptools import setup
+except ImportError:
+    from distutils.core import setup
+
+requisites = []
+
+setup(
+    name='wattpad_ebook',
+    version='0.0.8',
+    description='Generate ebook from Wattpad '
+                'and send to Kindle',
+    author='Do Son Tung',
+    url='https://github.com/tung491/wattpad_ebook',
+    author_email='dosontung007@gmail.com',
+    packages=['wattpad_ebook'],
+    license='MIT',
+    install_requires=['requests_html'],
+
+    entry_points={
+        'console_scripts': [
+            'wattpad-ebook=wattpad_ebook.wattpad_ebook:cli'
+        ],
+    }
+)
