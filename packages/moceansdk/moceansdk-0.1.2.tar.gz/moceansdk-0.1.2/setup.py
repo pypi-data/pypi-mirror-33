8fcd278535ee3a71efea13af04e0c7b3e80a115a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 05:08:05 +0000

    Add package moceansdk/0.1.2/moceansdk-0.1.2.tar.gz

---
+++ packages/moceansdk/moceansdk-0.1.2.tar.gz/moceansdk-0.1.2/setup.py
@@ -0,0 +1,27 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="moceansdk",
+    version="0.1.2",
+    author="Micro Ocean Technologies Sdn Bhd",
+    author_email="support@moceanapi.com",
+    description="Mocean SDK for Python",
+    long_description="This is a Mocean SDK written in python, to use it you will need a mocean account. Signup for free at https://moceanapi.com",
+    long_description_content_type="text/markdown",
+    url="https://github.com/MoceanAPI/mocean-sdk-python",
+    packages=setuptools.find_packages(),
+    classifiers=(
+        "Programming Language :: Python",
+		"Programming Language :: Python :: 2",
+		"Programming Language :: Python :: 2.7",
+		"Programming Language :: Python :: 3",
+		"Programming Language :: Python :: 3.4",
+		"Programming Language :: Python :: 3.5",
+		"Programming Language :: Python :: 3.6",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ),
+)
