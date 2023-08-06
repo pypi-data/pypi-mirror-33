7ce6f51da385006c93f09cb047742689cdd14326
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:34:43 +0000

    Add package django-1.6-fine-uploader/0.2.0.1/django_1.6_fine_uploader-0.2.0.1-py2-none-any.whl

---
+++ packages/django-1.6-fine-uploader/django_1.6_fine_uploader-0.2.0.1-py2-none-any.whl/django_fine_uploader/urls.py
@@ -0,0 +1,9 @@
+# -*- coding: utf-8 -*-
+from django.conf.urls import url
+
+from . import views
+
+urlpatterns = [
+    url(r'^upload/$', views.FineUploaderView.as_view(), name='upload'),
+    url(r'^delete(/(?P<uuid>[0-9a-f-]+))?$', views.FineUploaderDeleteView.as_view(), name='delete'),
+]
---
+++ packages/django-1.6-fine-uploader/django_1.6_fine_uploader-0.2.0.1-py2-none-any.whl/tests/test_models.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+"""
+test_django-fine-uploader
+------------
+
+Tests for `django-fine-uploader` models module.
+"""
+
+from django.test import TestCase
+
+
+class TestDjango_fine_uploader(TestCase):
+
+    def setUp(self):
+        pass
+
+    def test_something(self):
+        pass
+
+    def tearDown(self):
+        pass
