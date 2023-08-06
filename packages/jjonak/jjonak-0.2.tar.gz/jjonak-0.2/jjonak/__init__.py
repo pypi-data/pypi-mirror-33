1f0871ebf9092730740a6bfb3c4a32af5f332842
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:36:20 +0000

    Add package jjonak/0.2/jjonak-0.2.tar.gz

---
+++ packages/jjonak/jjonak-0.2.tar.gz/jjonak-0.2/jjonak/__init__.py
@@ -0,0 +1,5 @@
+print("JJONAK是宇宙第一小可爱！！！٩(๑>◡<๑)۶")
+
+def speak(): 
+
+	print("JJONAK是宇宙第一小可爱！！！٩(๑>◡<๑)۶")
---
+++ packages/jjonak/jjonak-0.2.tar.gz/jjonak-0.2/setup.py
@@ -0,0 +1,7 @@
+from setuptools import setup
+
+setup(
+	name='jjonak',    
+	version='0.2',
+	packages=['jjonak'] 
+)
