72780f459093099973737eb028032c581d15466a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:11:06 +0000

    Add package approxposterior/0.1.post1/approxposterior-0.1.post1.tar.gz

---
+++ packages/approxposterior/approxposterior-0.1.post1.tar.gz/approxposterior-0.1.post1/approxposterior/__init__.py
@@ -0,0 +1,23 @@
+# -*- coding: utf-8 -*-
+"""
+@author: David P. Fleming, University of Washington, Seattle
+
+approxposteior package init file.
+
+"""
+
+from __future__ import (absolute_import, division, print_function,
+                        unicode_literals)
+
+__version__ = "0.1.post1"
+__author__ = "David Fleming (dflemin3@uw.edu)"
+__copyright__ = "Copyright 2018 David P. Fleming"
+
+from .bp import *
+from .utility import *
+from .likelihood import *
+from .gp_utils import *
+from .mcmc_utils import *
+from .plot_utils import *
+from .gmm_utils import *
+from .pool import *
