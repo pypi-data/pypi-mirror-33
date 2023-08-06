ce3afe636c819b4c30ff14b5ea4c6363094889e8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:44:11 +0000

    Add package urlexpander/0.0.10/urlexpander-0.0.10.tar.gz

---
+++ packages/urlexpander/urlexpander-0.0.10.tar.gz/urlexpander-0.0.10/urlexpander/__init__.py
@@ -0,0 +1,14 @@
+from urlexpander.core.api import (
+    strip_url,
+    get_domain,
+    is_short,
+    is_short_domain,
+    expand,
+    multithread_expand,
+    count_matrix
+)
+
+from urlexpander.core import utils, html_parser, tweet_parser
+from urlexpander.core.constants import Constants
+
+__version__ = '0.0.10'
---
+++ packages/urlexpander/urlexpander-0.0.10.tar.gz/urlexpander-0.0.10/urlexpander/core/utils.py
@@ -0,0 +1,11 @@
+def chunks(iterable, chunksize):
+    """Yield successive n-sized chunks from l.
+    
+    :input iterable: an iterable
+    :input chunksize: chunksize
+    """
+    for i in range(0, len(iterable), chunksize):
+        yield iterable[i:i + chunksize]
+        
+def get_constant(constant_name):
+    
