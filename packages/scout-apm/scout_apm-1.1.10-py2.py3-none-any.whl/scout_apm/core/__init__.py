ffa103fa7b4529581c3debf1a470a44c0520e26b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:09:59 +0000

    Add package scout-apm/1.1.10/scout_apm-1.1.10-py2.py3-none-any.whl

---
+++ packages/scout-apm/scout_apm-1.1.10-py2.py3-none-any.whl/scout_apm/core/__init__.py
@@ -0,0 +1,23 @@
+from __future__ import absolute_import
+
+# Python Modules
+import logging
+from os import getpid
+
+from scout_apm.core.context import AgentContext
+from scout_apm.core.core_agent_manager import CoreAgentManager
+from scout_apm.core.metadata import AppMetadata
+
+logger = logging.getLogger(__name__)
+
+
+def install():
+    if not AgentContext.instance().config.value('monitor'):
+        logger.info("APM Not Launching on PID: %s - Configuration 'monitor' is not true", getpid())
+        return False
+
+    logger.debug('APM Launching on PID: %s', getpid())
+    CoreAgentManager().launch()
+    AppMetadata.report()
+    AgentContext.socket().stop()
+    return True
