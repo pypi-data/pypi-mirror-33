babf7487071a5a3a32983b17d15dff39ca7e1756
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:49:24 +0000

    Add package cube-simulator/1.0/cube_simulator-1.0-py3-none-any.whl

---
+++ packages/cube-simulator/cube_simulator-1.0-py3-none-any.whl/simulator/constants.py
@@ -0,0 +1,24 @@
+__author__ = 'SHASHANK'
+
+BANK_HOLIDAYS = ['2018-01-26', '2018-03-20', '2018-04-02', '2018-08-15',
+                 '2018-08-22', '2018-10-02', '2018-12-25', '2018-04-01']
+
+
+BSE_HOLIDAYS = ['2018-01-26', '2018-02-13', '2018-03-02', '2018-03-29',
+                '2018-03-30', '2018-05-01', '2018-08-15', '2018-08-22',
+                '2018-09-13', '2018-09-20', '2018-10-02', '2018-10-18',
+                '2018-11-07', '2018-11-08', '2018-11-23', '2018-12-25',
+                '2018-02-19', '2018-04-30', '2018-08-17', '2018-11-21']
+
+
+CAPTURE_ERROR_CASE = False
+
+MF_ALLOTMENT_TAT = {
+    'Franklin India Corporate Bond Opportunities Fund': [0, 2],
+    'RELIANCE LIQUID FUND-TREASURY PLAN - PLAN GROWTH': [1, 0],
+    'Invesco India Medium Term Bond Fund': [0, 1],
+    'IDBI India Top 100 Equity Fund': [0, 1],
+    'Motilal Oswal MOSt Focused Midcap 30 Fund': [0, 1],
+    'Motilal Oswal MOSt Focused Multicap 35 Fund': [0, 1],
+    'Canara Robeco Emerging Equities Fund': [0, 1],
+}
