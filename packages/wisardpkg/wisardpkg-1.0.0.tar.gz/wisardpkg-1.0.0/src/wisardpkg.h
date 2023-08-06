e258ee5e19b61327aa8574496c04b97e52d2d276
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:22:00 +0000

    Add package wisardpkg/1.0.0/wisardpkg-1.0.0.tar.gz

---
+++ packages/wisardpkg/wisardpkg-1.0.0.tar.gz/wisardpkg-1.0.0/src/wisardpkg.h
@@ -0,0 +1,21 @@
+
+//common
+#include "common/utils.cc"
+#include "common/exceptions.cc"
+#include "common/bleaching.cc"
+
+//synthesizers
+#include "synthetic_data/synthesizers.cc"
+
+//binarization
+#include "binarization/kernelcanvas.cc"
+#include "binarization/averageentry.cc"
+
+//wisard model
+#include "models/wisard/ram.cc"
+#include "models/wisard/discriminator.cc"
+#include "models/wisard/wisard.cc"
+
+//cluswisard wisard_model
+#include "models/cluswisard/cluster.cc"
+#include "models/cluswisard/cluswisard.cc"
