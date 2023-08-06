c27cc7fd7fa7e7d215d71c2081fb74c7b13b9564
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:21:54 +0000

    Add package dronedirector/0.0.7/dronedirector-0.0.7.tar.gz

---
+++ packages/dronedirector/dronedirector-0.0.7.tar.gz/dronedirector-0.0.7/dronedirector/__init__.py
@@ -0,0 +1,5 @@
+# -*- coding: utf-8 -*-
+# Copyright 2018 Alex Marchenko
+# Distributed under the terms of the Apache License 2.0
+from .aerial import Drone, AerialObject, SinusoidalDrone
+from .distance import lat_to_rad, lon_to_rad, earth_radius
