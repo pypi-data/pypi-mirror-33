f57917162ffe554fe606dd6361a6d2ac4df48ecb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:47:53 +0000

    Add package dictknife/0.7.6/dictknife-0.7.6-py2.py3-none-any.whl

---
+++ packages/dictknife/dictknife-0.7.6-py2.py3-none-any.whl/dictknife/loading/json.py
@@ -0,0 +1,10 @@
+from collections import OrderedDict
+from ._lazyimport import m
+
+
+def load(fp, *, loader=None, errors=None):
+    return m.json.load(fp, object_pairs_hook=OrderedDict)
+
+
+def dump(d, fp, *, sort_keys=False):
+    return m.json.dump(d, fp, ensure_ascii=False, indent=2, default=str, sort_keys=sort_keys)
---
+++ packages/dictknife/dictknife-0.7.6-py2.py3-none-any.whl/dictknife/transform.py
@@ -0,0 +1,2 @@
+def identity(data, **kwargs):
+    return data
