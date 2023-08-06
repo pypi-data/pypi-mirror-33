253d4fcf12cf091c8ab55f978c5efd2ae3f262d0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:18:37 +0000

    Add package bces/1.0.3/bces-1.0.3.tar.gz

---
+++ packages/bces/bces-1.0.3.tar.gz/bces-1.0.3/setup.py
@@ -0,0 +1,21 @@
+from setuptools import setup, find_packages
+
+with open('README.md') as f:
+    readme = f.read()
+
+with open('LICENSE') as f:
+    license = f.read()
+
+setup(
+    name='bces',
+    version='1.0.3',
+    description='Python module for performing linear regression for data with measurement errors and intrinsic scatter',
+    long_description=readme,
+    author='Rodrigo Nemmen',
+    author_email='rodrigo.nemmen@iag.usp.br',
+    url='https://github.com/rsnemmen/BCES',
+    download_url = 'https://github.com/rsnemmen/BCES/archive/1.0.3.tar.gz',
+    license=license,
+    keywords = ['statistics', 'fitting', 'linear-regression'],
+    packages=find_packages(exclude=('tests', 'docs'))
+)
