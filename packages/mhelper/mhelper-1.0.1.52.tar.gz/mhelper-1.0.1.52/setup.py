e0d71540133fa01ee5efffd3dec60201351fecb5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:48:06 +0000

    Add package mhelper/1.0.1.52/mhelper-1.0.1.52.tar.gz

---
+++ packages/mhelper/mhelper-1.0.1.52.tar.gz/mhelper-1.0.1.52/setup.py
@@ -0,0 +1,19 @@
+from distutils.core import setup
+
+
+setup( name = "mhelper",
+       url = "https://bitbucket.org/mjr129/mhelper",
+       version = "1.0.1.52",
+       description = "Includes a collection of utility functions.",
+       author = "Martin Rusilowicz",
+       license = "https://www.gnu.org/licenses/agpl-3.0.html",
+       packages = ["mhelper",
+                   "mhelper_qt",
+                   "mhelper_qt.designer",
+                   "mdbg"],
+       python_requires = ">=3.6",
+       install_requires = ["PyQt5",
+                           "mistune",
+                           "py-flags",
+                           "jsonpickle", 'colorama']
+       )
