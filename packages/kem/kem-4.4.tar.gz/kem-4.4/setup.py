35960655175e5d988ac0aa6f7fdd08722e305f0d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:22:05 +0000

    Add package kem/4.4/kem-4.4.tar.gz

---
+++ packages/kem/kem-4.4.tar.gz/kem-4.4/setup.py
@@ -0,0 +1,23 @@
+from distutils.core import setup
+
+setup(
+    name = 'kem',
+    packages=['kem'],
+    package_dir={'kem':'kem'},
+    package_data={'kem':['management/commands/*']},
+    version = '4.4',
+    description = 'A django App for kem',
+    author = ['davidtnfsh', 'theshaneyu'],
+    author_email = 'davidtnfsh@gmail.com',
+    url = 'https://github.com/udicatnchu/kem',
+    download_url = 'https://github.com/udicatnchu/kem/archive/v4.4.tar.gz',
+    keywords = ['kem', 'gensim'],
+    classifiers = [],
+    license='GPL3.0',
+    install_requires=[
+        'simplejson',
+        'gensim',
+        'ngram'
+    ],
+    zip_safe=True,
+)
