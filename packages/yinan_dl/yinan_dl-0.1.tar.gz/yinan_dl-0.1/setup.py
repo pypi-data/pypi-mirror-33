c4fa1b50bac14e4bd9998eefef65dabda112ba80
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:50:04 +0000

    Add package yinan_dl/0.1/yinan_dl-0.1.tar.gz

---
+++ packages/yinan_dl/yinan_dl-0.1.tar.gz/yinan_dl-0.1/setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+setup(name='yinan_dl',
+      version='0.1',
+      description='clinical trial information retriver',
+      url='http://github.com/tongling/clinicaltrial',
+      author='Yinan Xu',
+      author_email='yinanxu@wezhuiyi.com',
+      license='MIT',
+      packages=['yinan_dl'],
+      zip_safe=False)
