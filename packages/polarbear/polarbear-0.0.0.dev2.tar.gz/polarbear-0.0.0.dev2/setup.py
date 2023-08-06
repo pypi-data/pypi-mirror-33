ee325f5507e4015e71c2ad52b9051093fca53a9b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:24:37 +0000

    Add package polarbear/0.0.0.dev2/polarbear-0.0.0.dev2.tar.gz

---
+++ packages/polarbear/polarbear-0.0.0.dev2.tar.gz/polarbear-0.0.0.dev2/setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(name='polarbear',
+      version='0.0.0.dev2',
+      description='logic for social science survey data',
+      packages=['polarbear'],
+      install_requires=[
+            'pandas',
+            'savReaderWriter',
+            'sqlalchemy'
+        ]
+      )
