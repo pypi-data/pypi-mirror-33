7a633dad9124fb391274eb3d234c714500bcfc6f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 07:12:09 +0000

    Add package vecto/0.1.6/vecto-0.1.6-py3-none-any.whl

---
+++ packages/vecto/vecto-0.1.6-py3-none-any.whl/vecto/benchmarks/fetch_benchmarks.py
@@ -0,0 +1,12 @@
+from git import Repo
+from git.exc import GitCommandError
+from os import path
+
+path_to_repo = 'https://github.com/vecto-ai/benchmarks.git'
+
+
+def fetch_benchmarks(path_to_local_dir=path.join('data', 'benchmarks')):
+    try:
+        Repo.clone_from('https://github.com/vecto-ai/benchmarks.git', path_to_local_dir)
+    except GitCommandError:
+        raise ValueError('Directory exists')
