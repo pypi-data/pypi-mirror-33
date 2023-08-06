cfb29e7079f7d4d45281fd2cede001f3998a8490
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:16:03 +0000

    Add package aio-openapi/0.2.4/aio-openapi-0.2.4.tar.gz

---
+++ packages/aio-openapi/aio-openapi-0.2.4.tar.gz/aio-openapi-0.2.4/tests/example/db.py
@@ -0,0 +1,18 @@
+import sqlalchemy as sa
+
+
+def meta(meta=None):
+    """Add task related tables
+    """
+    if meta is None:
+        meta = sa.MetaData()
+
+    sa.Table(
+        'tasks', meta,
+        sa.Column('id', sa.Integer, primary_key=True),
+        sa.Column('title', sa.String, nullable=False),
+        sa.Column('done', sa.DateTime),
+        sa.Column('severity', sa.Integer)
+    )
+
+    return meta
