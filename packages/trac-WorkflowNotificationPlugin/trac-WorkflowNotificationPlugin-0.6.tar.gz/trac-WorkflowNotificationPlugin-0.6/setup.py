c6b2a6285f4cccc0b153bf7f5e811832628a46dd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:35:04 +0000

    Add package trac-WorkflowNotificationPlugin/0.6/trac-WorkflowNotificationPlugin-0.6.tar.gz

---
+++ packages/trac-WorkflowNotificationPlugin/trac-WorkflowNotificationPlugin-0.6.tar.gz/trac-WorkflowNotificationPlugin-0.6/setup.py
@@ -0,0 +1,32 @@
+from setuptools import setup
+
+try:
+    long_description = open("README.txt").read()
+except:
+    long_description = ''
+try:
+    long_description += open("CHANGES.txt").read()
+except:
+    pass
+
+setup(name='trac-WorkflowNotificationPlugin',
+      version='0.6',
+      description="Configurable notifications for trac tickets tied to workflow actions",
+      long_description=long_description,
+      packages=['workflow_notification'],
+      package_data = {
+        'workflow_notification': [
+            'templates/*.html',
+            'templates/*.txt',
+        ],
+      },
+      author='Ethan Jucovy',
+      author_email='ejucovy@gmail.com',
+      url="https://trac-hacks.org/wiki/WorkflowNotificationPlugin",
+      license='BSD',
+      entry_points = {
+        'trac.plugins':
+            ['workflow_notification = workflow_notification']
+        },
+      zip_safe=False,
+      )
