93c7c2225a4ad54d9d67a2c5f55fde2a054dccba
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:17:59 +0000

    Add package chainer-mask-rcnn/0.5.12/chainer-mask-rcnn-0.5.12.tar.gz

---
+++ packages/chainer-mask-rcnn/chainer-mask-rcnn-0.5.12.tar.gz/chainer-mask-rcnn-0.5.12/chainer_mask_rcnn/utils/__init__.py
@@ -0,0 +1,16 @@
+# flake8: noqa
+
+from .commands import git_hash
+
+from .evaluations import eval_instseg_voc
+from .evaluations import eval_instseg_coco
+
+from .geometry import get_bbox_overlap
+from .geometry import get_mask_overlap
+from .geometry import instance_boxes2label
+from .geometry import label2instance_boxes
+from .geometry import mask_to_bbox
+
+from .visualizations import draw_instance_boxes
+from .visualizations import draw_instance_bboxes
+from .visualizations import visualize_instance_segmentation
