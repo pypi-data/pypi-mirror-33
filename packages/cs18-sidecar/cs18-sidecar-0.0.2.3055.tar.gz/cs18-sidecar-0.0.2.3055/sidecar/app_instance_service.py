337df0b8823d4277a78a11d717dc2f327671e9cf
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:49:28 +0000

    Add package cs18-sidecar/0.0.2.3055/cs18-sidecar-0.0.2.3055.tar.gz

---
+++ packages/cs18-sidecar/cs18-sidecar-0.0.2.3055.tar.gz/cs18-sidecar-0.0.2.3055/sidecar/app_instance_service.py
@@ -0,0 +1,28 @@
+from abc import ABCMeta, abstractmethod
+from logging import Logger
+from typing import List, Dict
+
+from sidecar.app_instance_identifier import AppInstanceIdentifier
+
+
+class StaleAppInstanceException(Exception):
+    pass
+
+
+class IAppInstanceService:
+    __metaclass__ = ABCMeta
+
+    def __init__(self, logger: Logger):
+        self._logger = logger
+
+    @abstractmethod
+    def update_status_if_not_stale(self, app_instance_identifier: AppInstanceIdentifier, status: str):
+        raise NotImplementedError
+
+    @abstractmethod
+    def get_all_app_instances(self) -> List[AppInstanceIdentifier]:
+        raise NotImplementedError
+
+    @abstractmethod
+    def get_all_app_instances_statuses(self) -> Dict[AppInstanceIdentifier, str]:
+        raise NotImplementedError
