1a17acbf01dd993e3af8f7ec4c4688787306aa52
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:35:37 +0000

    Add package xudo/1.1.0/xudo-1.1.0-py2.py3-none-any.whl

---
+++ packages/xudo/xudo-1.1.0-py2.py3-none-any.whl/xudo/commands/prune.py
@@ -0,0 +1,10 @@
+from .base import Base
+
+import os
+
+
+class Prune(Base):
+    """Prompts the user to prune their docker environment"""
+
+    def run(self):
+        os.system("docker system prune")
