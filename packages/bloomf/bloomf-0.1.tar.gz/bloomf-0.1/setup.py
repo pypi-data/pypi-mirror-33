eb3309cc023608493d13e98be846829122218387
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:09:24 +0000

    Add package bloomf/0.1/bloomf-0.1.tar.gz

---
+++ packages/bloomf/bloomf-0.1.tar.gz/bloomf-0.1/setup.py
@@ -0,0 +1,13 @@
+import setuptools
+
+setuptools.setup(
+    name='bloomf',
+    version='0.1',
+    description='Simple Bloom Filter implmentation in Python',
+    url='https://github.com/sambhav2612/karumanchi/tree/master/bloom-filter',
+    author='Sambhav Jain',
+    author_email='sambhavjain2612@gmail.com',
+    license='GPL-3.0',
+    packages=setuptools.find_packages(),
+    zip_safe=False
+)
