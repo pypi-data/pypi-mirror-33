24e221993d2b1ff4d194dec8c8b64c38178ac84e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:50:02 +0000

    Add package mdx_titlecase/1.2.0/mdx_titlecase-1.2.0.tar.gz

---
+++ packages/mdx_titlecase/mdx_titlecase-1.2.0.tar.gz/mdx_titlecase-1.2.0/CHANGES.rst
@@ -0,0 +1,37 @@
+ChangeLog
+=========
+
+
+`1.2.0 (2018-06-26) <http://github.com/kdeldycke/mdx_titlecase/compare/v1.1.0...v1.2.0>`_
+-----------------------------------------------------------------------------------------
+
+* Fix plugin initialization.
+* Add Python 3 support.
+* Add default ``isort`` config.
+* Add missing trove classifiers.
+* Activate tests on Python 3.3, 3.4, 3.5, PyPy and PyPy3.
+* Bump Markdown requirement to 2.6.
+
+
+`1.1.0 (2015-11-29) <http://github.com/kdeldycke/mdx_titlecase/compare/v1.0.1...v1.1.0>`_
+-----------------------------------------------------------------------------------------
+
+* Allow title casing of metadata.
+
+
+`1.0.1 (2015-11-23) <http://github.com/kdeldycke/mdx_titlecase/compare/v1.0.0...v1.0.1>`_
+-----------------------------------------------------------------------------------------
+
+* Switch from coveralls.io to codecov.io.
+
+
+`1.0.0 (2015-11-23) <http://github.com/kdeldycke/mdx_titlecase/compare/v0.0.0...v1.0.0>`_
+-----------------------------------------------------------------------------------------
+
+* First public release.
+
+
+`0.0.0 (2015-11-22) <http://github.com/kdeldycke/mdx_titlecase/commit/8c24bb>`_
+-------------------------------------------------------------------------------
+
+* First commit.
---
+++ packages/mdx_titlecase/mdx_titlecase-1.2.0.tar.gz/mdx_titlecase-1.2.0/setup.py
@@ -0,0 +1,88 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+#
+# Copyright (C) 2015-2016 Kevin Deldycke <kevin@deldycke.com>
+#
+# This program is Free Software; you can redistribute it and/or
+# modify it under the terms of the GNU General Public License
+# as published by the Free Software Foundation; either version 2
+# of the License, or (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program; if not, write to the Free Software
+# Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
+
+import codecs
+import os
+import re
+
+from setuptools import find_packages, setup
+
+MODULE_NAME = 'mdx_titlecase'
+
+
+def get_version():
+
+    with open(os.path.join(
+        os.path.dirname(__file__), MODULE_NAME, '__init__.py')
+    ) as init:
+
+        for line in init.readlines():
+            res = re.match(r'__version__ *= *[\'"]([0-9a-z\.]*)[\'"]$', line)
+            if res:
+                return res.group(1)
+
+
+def get_long_description():
+    readme = os.path.join(os.path.dirname(__file__), 'README.rst')
+    changes = os.path.join(os.path.dirname(__file__), 'CHANGES.rst')
+    return codecs.open(readme, encoding='utf-8').read() + '\n' + \
+        codecs.open(changes, encoding='utf-8').read()
+
+
+setup(
+    name='mdx_titlecase',
+    version=get_version(),
+    description="Proper title-casing for Python's Markdown.",
+    long_description=get_long_description(),
+
+    author='Kevin Deldycke',
+    author_email='kevin@deldycke.com',
+    url='https://github.com/kdeldycke/mdx_titlecase',
+    license='GPLv2+',
+
+    install_requires=[
+        'Markdown >= 2.6',
+        'titlecase',
+    ],
+
+    packages=find_packages(),
+
+    tests_require=[],
+    test_suite=MODULE_NAME + '.tests',
+
+    classifiers=[
+        # See: https://pypi.python.org/pypi?:action=list_classifiers
+        'Development Status :: 5 - Production/Stable',
+        'Intended Audience :: Developers',
+        'Intended Audience :: Information Technology',
+        'License :: OSI Approved :: '
+        'GNU General Public License v2 or later (GPLv2+)',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: Implementation :: PyPy',
+        'Topic :: Text Processing :: Filters',
+        'Topic :: Text Processing :: Markup',
+    ],
+)
