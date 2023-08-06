c4c20dc13b99ada21e4385ef7425f019ce1b11e7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:35:15 +0000

    Add package castle.cms/2.0.45/castle.cms-2.0.45.tar.gz

---
+++ packages/castle.cms/castle.cms-2.0.45.tar.gz/castle.cms-2.0.45/castle/cms/_scripts/importjson.py
@@ -0,0 +1,313 @@
+from AccessControl.SecurityManagement import newSecurityManager
+from castle.cms._scripts import mjson
+from castle.cms._scripts.importtypes import get_import_type
+from lxml.html import fromstring
+from lxml.html import tostring
+from OFS.interfaces import IFolder
+from plone import api
+from plone.app.blocks.layoutbehavior import ILayoutAware
+from plone.app.redirector.interfaces import IRedirectionStorage
+from plone.app.textfield.value import RichTextValue
+from Products.CMFPlone.interfaces.constrains import ISelectableConstrainTypes
+from zope.component import getUtility
+from zope.component.hooks import setSite
+
+import argparse
+import logging
+import os
+import transaction
+
+
+logger = logging.getLogger('castle.cms')
+
+# ToDo:
+#   - do NOT export images
+#       - only images that are referenced in content
+#   - lookup reference, if image only used once, add as lead image
+#   - other images, if not referenced elsewhere, include inline
+#       as data uris
+#   - links that go to files should be moved to s3 and be converted
+#       into file objects
+#   - if default page and only page inside folder, replace folder
+#
+
+
+parser = argparse.ArgumentParser(
+    description='...')
+parser.add_argument('--site-id', dest='site_id', default='Plone')
+parser.add_argument('--export-directory', dest='export_directory')
+parser.add_argument('--overwrite', dest='overwrite', default=False)
+args, _ = parser.parse_known_args()
+
+
+user = app.acl_users.getUser('admin')  # noqa
+newSecurityManager(None, user.__of__(app.acl_users))  # noqa
+site = app[args.site_id]  # noqa
+setSite(site)
+
+
+def traverse(path):
+    folder = site  # noqa
+    for part in path.strip('/').split('/'):
+        if part in folder.objectIds():
+            folder = folder[part]
+        else:
+            return
+
+
+def relpath(obj):
+    return '/'.join(obj.getPhysicalPath())[len('/'.join(site.getPhysicalPath())) + 1:]
+
+
+def recursive_create_path(path):
+    if path in ('/', ''):
+        folder = site
+    else:
+        path = path.lstrip('/')
+        folder = traverse(path)
+        if folder is not None and not IFolder.providedBy(folder):
+            api.content.delete(folder)
+            folder = None
+        if folder is None:
+            # Need to create folders up to where we want content
+            # we'll walk it up create folders as needed
+            folder = site
+            for part in path.split('/'):
+                try:
+                    ob = folder[part]
+                    if not IFolder.providedBy(ob):
+                        api.content.delete(ob)
+                        raise KeyError()
+                    else:
+                        folder = ob
+                except (KeyError, AttributeError):
+                    fpath = os.path.join(
+                        args.export_directory,
+                        relpath(folder),
+                        part,
+                        '__folder__')
+
+                    if os.path.exists(fpath):
+                        fi = open(fpath)
+                        data = mjson.loads(fi.read())
+                        fi.close()
+                        importtype = get_import_type(data, fpath[len(args.export_directory):], None)
+                        creation_data = importtype.get_data()
+                        creation_data['container'] = folder
+                        creation_data['id'] = part
+                        try:
+                            folder = api.content.create(**creation_data)
+                        except api.exc.InvalidParameterError:
+                            logger.error('Error creating content {}'.format(fpath), exc_info=True)
+                            return
+                        importtype.post_creation(folder)
+                        if data['state']:
+                            try:
+                                api.content.transition(folder, to_state=data['state'])
+                            except:
+                                # maybe workflows do not match up
+                                pass
+                        folder.reindexObject()
+                    else:
+                        folder = api.content.create(
+                            type='Folder',
+                            id=part,
+                            title=part.capitalize(),
+                            container=folder)
+                        bdata = ILayoutAware(folder)
+                        bdata.contentLayout = '++contentlayout++castle/folder-query.html'
+    return folder
+
+
+_importable_fields = (
+    'title',
+    'description',
+)
+
+
+_read_phase = {}
+
+
+def read_object(filepath):
+    # disable, we don't use
+    pass
+    # fi = open(filepath)
+    # data = mjson.loads(fi.read())
+    # fi.close()
+
+
+def fix_html_images(obj):
+    try:
+        html = obj.text.raw
+    except:
+        return
+    if not html:
+        return
+    changes = False
+    try:
+        dom = fromstring(html)
+    except:
+        return
+    for el in dom.cssselect('img'):
+        src = el.attrib.get('src', '')
+        if 'resolveuid' not in src:
+            continue
+        if '@@images' in src:
+            continue
+        parts = src.split('/')
+        uid = parts[1]
+        scale = '/'.join(parts[2:])
+        if scale:
+            scale = scale.replace('image_', '')
+        src = 'resolveuid/%s/@@images/image' % uid
+        if scale:
+            src += '/' + scale
+        attribs = {
+            'src': src,
+            'data-linktype': 'image',
+            'data-val': uid
+        }
+        if scale:
+            attribs['data-scale'] = scale
+        el.attrib.update(attribs)
+        changes = True
+    if changes:
+        obj.text = RichTextValue(
+            tostring(dom), mimeType=obj.text.mimeType, outputMimeType=obj.text.outputMimeType)
+
+
+def import_object(filepath, count):
+    fi = open(filepath)
+    data = mjson.loads(fi.read())
+    fi.close()
+    if filepath.endswith('__folder__'):
+        filepath = '/'.join(filepath.split('/')[:-1])
+
+    if data['portal_type'] in ('Topic', 'Collection', 'PressRoom'):
+        return
+
+    original_path = filepath[len(args.export_directory):]
+    importtype = get_import_type(data, original_path, _read_phase)
+    path = importtype.get_path()
+    folder = recursive_create_path('/'.join(path.split('/')[:-1]))
+    if folder is None:
+        print('Skipped {} because of creation error'.format(filepath))
+    _id = path.split('/')[-1]
+
+    create = True
+    if _id in folder.objectIds():
+        if args.overwrite:
+            if data['portal_type'] == 'Folder':
+                cnt = folder[_id]
+                if len(folder.objectIds()) == 1 or len(cnt.objectIds()) == 0:
+                    api.content.delete(folder[_id])
+            else:
+                api.content.delete(folder[_id])
+        else:
+            create = False
+
+    print('import path: %s' % path)
+    creation_data = importtype.get_data()
+    creation_data['container'] = folder
+
+    aspect = ISelectableConstrainTypes(folder, None)
+    if aspect:
+        if (aspect.getConstrainTypesMode() != 1 or
+                [creation_data['type']] != aspect.getImmediatelyAddableTypes()):
+            aspect.setConstrainTypesMode(1)
+            aspect.setImmediatelyAddableTypes([creation_data['type']])
+
+    if create:
+        try:
+            obj = api.content.create(**creation_data)
+        except api.exc.InvalidParameterError:
+            return logger.error('Error creating content {}'.format(filepath), exc_info=True)
+    else:
+        obj = folder[_id]
+        for key, value in creation_data.items():
+            if key not in ('id', 'type'):
+                setattr(obj, key, value)
+
+    if path != original_path:
+        storage = getUtility(IRedirectionStorage)
+        rpath = os.path.join('/'.join(site.getPhysicalPath()),
+                             original_path.strip('/'))
+        storage.add(rpath, "/".join(obj.getPhysicalPath()))
+
+    importtype.post_creation(obj)
+    if data['state']:
+        try:
+            api.content.transition(obj, to_state=data['state'])
+        except:
+            # maybe workflows do not match up
+            pass
+
+    fix_html_images(obj)
+    obj.reindexObject()
+
+    if count % 50 == 0:
+        print('%i processed, committing' % count)
+        transaction.commit()
+        app._p_jar.invalidateCache()  # noqa
+        transaction.begin()
+        app._p_jar.sync()  # noqa
+
+
+def import_pages(path, count=0):
+    for filename in os.listdir(path):
+        if filename in ('.DS_Store', '__folder__'):
+            continue
+        count += 1
+        filepath = os.path.join(path, filename)
+        if os.path.isdir(filepath):
+            count = import_pages(filepath, count)
+        else:
+            try:
+                import_object(filepath, count)
+            except:
+                logger.error('Error importing object', exc_info=True)
+    return count
+
+
+def import_folders(path, count=0):
+    for filename in sorted(os.listdir(path)):
+        count += 1
+        filepath = os.path.join(path, filename)
+
+        if filename == '__folder__':
+            try:
+                import_object(filepath, count)
+            except:
+                logger.error('Error importing object', exc_info=True)
+
+        if os.path.isdir(filepath):
+            count = import_folders(filepath, count)
+    return count
+
+
+def read_pass(path):
+    for filename in sorted(os.listdir(path)):
+        if filename in ('.DS_Store', '__folder__'):
+            continue
+        filepath = os.path.join(path, filename)
+        if os.path.isdir(filepath):
+            read_pass(filepath)
+        else:
+            read_object(filepath)
+
+
+if __name__ == '__main__':
+    print('------------------------------')
+    print('Start importing')
+    print('------------------------------')
+    if args.overwrite:
+        print('------------------------------')
+        print('Importing with overwrite enabled')
+        print('------------------------------')
+
+    print('doing read pass...')
+    # read_pass(args.export_directory)
+    print('creating objects now...')
+    # import_folders(args.export_directory)
+    import_pages(args.export_directory)
+    transaction.commit()
---
+++ packages/castle.cms/castle.cms-2.0.45.tar.gz/castle.cms-2.0.45/castle/cms/_scripts/importtypes.py
@@ -0,0 +1,360 @@
+from castle.cms.behaviors.location import ILocation
+from plone.app.blocks.layoutbehavior import ILayoutAware
+from plone.app.contenttypes.behaviors.richtext import IRichText
+from plone.app.dexterity.behaviors.metadata import IBasic
+from plone.app.dexterity.behaviors.metadata import ICategorization
+from plone.app.dexterity.behaviors.metadata import IPublication
+from plone.app.event.dx.behaviors import IEventAttendees
+from plone.app.event.dx.behaviors import IEventBasic
+from plone.app.event.dx.behaviors import IEventContact
+from plone.app.event.dx.behaviors import IEventLocation
+from plone.app.textfield.value import RichTextValue
+from plone.event.utils import pydt
+from plone.namedfile.file import NamedBlobFile
+from plone.namedfile.file import NamedBlobImage
+
+import base64
+import OFS
+import re
+
+
+FOLDER_DEFAULT_PAGE_LAYOUT = u"""
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN"
+                      "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en" lang="en"
+      data-layout="./@@page-site-layout">
+  <body>
+    <div data-panel="content">
+      <div class="mosaic-grid-row"
+           data-grid='{"type": "row"}'>
+        <div class="mosaic-grid-cell mosaic-width-full mosaic-position-leftmost"
+             data-grid='{"type": "cell", "info":{"xs": "true", "sm": "true", "lg": "true",
+                         "pos": {"x": 1, "width": 12}}}'>
+          <div class="movable removable mosaic-tile mosaic-IDublinCore-title-tile">
+          <div class="mosaic-tile-content">
+          <div data-tile="./@@plone.app.standardtiles.field?field=IDublinCore-title"></div>
+          </div>
+          </div>
+          <div class="movable removable mosaic-tile mosaic-IDublinCore-description-tile">
+          <div class="mosaic-tile-content">
+          <div data-tile="./@@plone.app.standardtiles.field?field=IDublinCore-description"></div>
+          </div>
+          </div>
+          <div class="movable removable mosaic-tile mosaic-text-tile">
+          <div class="mosaic-tile-content">
+            %s
+          </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </body>
+</html>
+"""
+
+_types = {}
+
+
+def register_import_type(name, klass):
+    _types[name] = klass
+
+
+def is_base64(s):
+    return (len(s) % 4 == 0) and re.match('^[A-Za-z0-9+/]+[=]{0,2}$', s)
+
+
+def decode_file_data(data):
+    if isinstance(data, OFS.Image.Image):
+        data = data.data
+        if isinstance(data, OFS.Image.Pdata):
+            return str(data)
+        return data
+    if is_base64(data):
+        return base64.b64decode(data)
+    return data
+
+
+def to_unicode(s):
+    if not isinstance(s, unicode):
+        s = s.decode('utf8')
+    return s
+
+
+def _generate_file_repo_object_id(path):
+    path = path.lower()
+    if path[0] == '/':
+        path = path[1:]
+    for _type in ('file', 'audio', 'video', 'videos', 'image', 'images'):
+        path = path.replace('%s-repository/' % _type, '').replace('/' + _type, '')
+    return path.replace('/', '-')
+
+
+def DateTime_to_datetime(val):
+    if hasattr(val, 'asdatetime'):
+        return pydt(val)
+    return val
+
+
+class BaseImportType(object):
+    layout = ''
+    fields_mapping = {}
+    data_converters = {}
+    lead_image_field_names = ('image', 'leadImage')
+    lead_image_caption_field_names = ('leadCaption', 'leadImage_caption')
+    behavior_data_mappers = ()
+
+    def __init__(self, data, path, read_phase):
+        self.data = data
+        self.field_data = data['data']
+        self.path = path
+        self.read_phase = read_phase
+        self.original_type = data['portal_type']
+        if data.get('is_default_page'):
+            # change to be folder instead of sub-item...
+            self.path = '/'.join(path.split('/')[:-1])
+            self.data['portal_type'] = 'Folder'
+            # disabled for now, can think about how to use this more properly...
+            # if data.get('has_sibling_pages'):
+            #     self.data['portal_type'] = 'Folder'
+
+    def get_path(self):
+        return self.path
+
+    def get_data(self):
+        data = {
+            '_plone.uuid': self.data['uid']
+        }
+
+        return dict(
+            id=self.field_data['id'],
+            type=self.data['portal_type'],
+            title=self.field_data['title'],
+            description=self.field_data['description'],
+            **data)
+
+    def post_creation(self, obj):
+        field_data = self.field_data
+        bdata = ILayoutAware(obj, None)
+        if bdata:
+            bdata.contentLayout = '++contentlayout++default/document.html'
+        bdata = IRichText(obj, None)
+        if bdata:
+            bdata.text = RichTextValue(field_data['text'], 'text/html', 'text/html')
+
+        bdata = IBasic(obj, None)
+        if bdata:
+            bdata.title = field_data['title']
+            bdata.description = field_data['description']
+        else:
+            obj.title = field_data['title']
+            obj.description = field_data['description']
+
+        bdata = ICategorization(obj, None)
+        if bdata:
+            bdata.subjects = field_data['subject']
+
+            bdata = IPublication(obj)
+            if field_data['effectiveDate']:
+                bdata.effective = pydt(field_data['effectiveDate'])
+
+        ldata = ILocation(obj, None)
+        if ldata:
+            if field_data.get('location'):
+                ldata.locations = [field_data['location']]
+
+            if field_data.get('newsLocation'):
+                if ldata.locations:
+                    ldata.locations.append(field_data['newsLocation'])
+                else:
+                    ldata.locations = [field_data['newsLocation']]
+
+        obj.modification_date = field_data['modification_date']
+        obj.creation_date = field_data['creation_date']
+
+        bdata = ILayoutAware(obj, None)
+        if bdata:
+            if self.data['portal_type'] == 'Folder' and 'text' in self.field_data:
+                bdata.content = FOLDER_DEFAULT_PAGE_LAYOUT % self.field_data['text']
+                # need to explicitly reset contentLayout value because this data
+                # could be overwritten
+                bdata.contentLayout = None
+            elif self.layout:
+                bdata.contentLayout = self.layout
+
+        inv_field_mapping = {v: k for k, v in self.fields_mapping.iteritems()}
+        for IBehavior, field_name in self.behavior_data_mappers:
+
+            original_field_name = inv_field_mapping.get(field_name, field_name)
+
+            if original_field_name not in self.field_data:
+                # data not here...
+                continue
+
+            behavior = IBehavior(obj, None)
+            if behavior is None:
+                # behavior not valid for obj type
+                continue
+
+            val = self.field_data[original_field_name]
+
+            if field_name in self.data_converters:
+                val = self.data_converters[field_name](val)
+
+            setattr(behavior, field_name, val)
+
+        # handle lead images
+        for field_name in self.lead_image_field_names:
+            if self.field_data.get(field_name):
+                im_obj = self.field_data.get(field_name)
+                if hasattr(im_obj, 'read'):
+                    im_data = im_obj.read()
+                else:
+                    im_data = im_obj
+
+                if not im_data:
+                    continue
+
+                filename = self.field_data.get('image_filename')
+                if not filename:
+                    if hasattr(im_obj, 'filename'):
+                        filename = im_obj.filename
+                    else:
+                        filename = self.field_data['id']
+                obj.image = NamedBlobImage(
+                    data=decode_file_data(im_data),
+                    filename=to_unicode(filename))
+                if not obj.image.contentType:
+                    obj.image.contentType = 'image/jpeg'
+                for caption_field_name in self.lead_image_caption_field_names:
+                    if caption_field_name in self.field_data:
+                        obj.imageCaption = self.field_data.get(caption_field_name)
+
+
+class DocumentType(BaseImportType):
+    layout = '++contentlayout++default/document.html'
+
+
+register_import_type('Document', DocumentType)
+
+
+class FolderType(BaseImportType):
+    layout = '++contentlayout++castle/folder.html'
+
+
+register_import_type('Folder', FolderType)
+
+
+class NewsItemType(BaseImportType):
+
+    layout = '++contentlayout++castle/newsitem.html'
+
+
+register_import_type('News Item', NewsItemType)
+
+
+class FileType(BaseImportType):
+    layout = None
+
+    def __init__(self, data, path, read_phase):
+        super(FileType, self).__init__(data, path, read_phase)
+        filename = self.field_data.get('file_filename')
+        _, ext = filename.lower().rsplit('.', 1)
+        _id = _generate_file_repo_object_id(path)
+        self.field_data['id'] = _id
+        if ext in ('wav', 'mp3'):
+            self.data['portal_type'] = 'Audio'
+            self.path = '/audio-repository/' + _id
+        elif ext in ['webm', 'ogv', 'avi', 'wmv', 'm4v',
+                     'mpg', 'mpeg', 'flv', 'mp4', 'mov']:
+            self.data['portal_type'] = 'Video'
+            self.path = '/video-repository/' + _id
+        else:
+            self.path = '/file-repository/' + _id
+
+    def get_data(self):
+        data = super(FileType, self).get_data()
+        data['file'] = NamedBlobFile(
+            data=decode_file_data(self.field_data['file'].read()),
+            filename=to_unicode(self.field_data.get('file_filename')))
+        return data
+
+
+register_import_type('File', FileType)
+
+
+class ImageType(BaseImportType):
+
+    def __init__(self, data, path, read_phase):
+        super(ImageType, self).__init__(data, path, read_phase)
+        _id = _generate_file_repo_object_id(path)
+        self.field_data['id'] = _id
+        self.path = '/image-repository/' + _id
+
+
+register_import_type('Image', ImageType)
+
+
+class LinkType(BaseImportType):
+
+    youtube = False
+
+    def __init__(self, data, path, read_phase):
+        super(LinkType, self).__init__(data, path, read_phase)
+        url = self.field_data['remoteUrl']
+        if 'youtube' in url:
+            self.youtube = True
+            self.data['portal_type'] = 'Video'
+            _id = _generate_file_repo_object_id(path)
+            self.path = '/video-repository/' + _id
+
+    def get_path(self):
+        if self.data['portal_type'] == 'Video':
+            return '/video-repository/' + self.field_data['id']
+        return super(LinkType, self).get_path()
+
+    def get_data(self):
+        data = super(LinkType, self).get_data()
+        if self.youtube:
+            data['youtube_url'] = self.field_data['remoteUrl']
+        else:
+            data['remoteUrl'] = self.field_data['remoteUrl']
+        return data
+
+
+register_import_type('Link', LinkType)
+
+
+class EventType(BaseImportType):
+    fields_mapping = {
+        'startDate': 'start',
+        'endDate': 'end',
+        'eventUrl': 'event_url',
+        'contactEmail': 'contact_email',
+        'contactName': 'contact_name',
+        'contactPhone': 'contact_phone'
+    }
+    data_converters = {
+        'start': DateTime_to_datetime,
+        'end': DateTime_to_datetime
+    }
+    behavior_data_mappers = (
+        (IEventBasic, 'start'),
+        (IEventBasic, 'end'),
+        (IEventBasic, 'whole_day'),
+        (IEventBasic, 'open_end'),
+        (IEventAttendees, 'attendees'),
+        (IEventLocation, 'location'),
+        (IEventContact, 'contact_name'),
+        (IEventContact, 'contact_email'),
+        (IEventContact, 'contact_phone'),
+        (IEventContact, 'event_url'),
+    )
+
+
+register_import_type('Event', EventType)
+
+
+def get_import_type(data, path, read_phase):
+    if data['portal_type'] in _types:
+        return _types[data['portal_type']](data, path, read_phase)
+    return BaseImportType(data, path, read_phase)
