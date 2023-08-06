2e7b509227dd598a7901e2f9dd17ce8eaacb49b6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:43:37 +0000

    Add package pluralsight/2.1.1/pluralsight-2.1.1.tar.gz

---
+++ packages/pluralsight/pluralsight-2.1.1.tar.gz/pluralsight-2.1.1/setup.py
@@ -0,0 +1,62 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+
+try:
+    from setuptools import setup
+except ImportError:
+    from distutils.core import setup
+
+
+with open('README.rst') as readme_file:
+    readme = readme_file.read()
+
+with open('HISTORY.rst') as history_file:
+    history = history_file.read()
+
+requirements = [
+    'six', 'requests', 'arrow'
+]
+
+test_requirements = [
+   'pytest',
+   'requests_staticmock'
+]
+
+setup(
+    name='pluralsight',
+    version='2.1.1',
+    description="Pluralsight client library for API management",
+    long_description=readme + '\n\n' + history,
+    author="Anthony Shaw",
+    author_email='anthonyshaw@apache.org',
+    url='https://github.com/tonybaloney/pluralsight',
+    packages=[
+        'pluralsight',
+        'pluralsight.licensing',
+        'pluralsight.reports',
+        'pluralsight.models'
+    ],
+    package_dir={'pluralsight':
+                 'pluralsight'},
+    include_package_data=True,
+    install_requires=requirements,
+    license="Apache License (2.0)",
+    zip_safe=False,
+    keywords='pluralsight',
+    classifiers=[
+        'Development Status :: 2 - Pre-Alpha',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: Apache Software License',
+        'Natural Language :: English',
+        "Programming Language :: Python :: 2",
+        'Programming Language :: Python :: 2.6',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+    ],
+    tests_require=test_requirements,
+    setup_requires=['pytest-runner']
+)
