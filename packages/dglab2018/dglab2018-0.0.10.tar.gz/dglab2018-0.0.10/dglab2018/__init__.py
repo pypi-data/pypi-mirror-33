566a40fa6e3dcbb34cfbe2b39f16d7c4d137a372
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:49:59 +0000

    Add package dglab2018/0.0.10/dglab2018-0.0.10.tar.gz

---
+++ packages/dglab2018/dglab2018-0.0.10.tar.gz/dglab2018-0.0.10/dglab2018/__init__.py
@@ -0,0 +1,15 @@
+import google.datalab.bigquery as bq
+
+def execute_query(queryStr, dataset):
+  query = bq.Query(queryStr.format(dataset))
+  outputOptions = bq.QueryOutput.table(use_cache=False)
+  return query.execute(output_options=outputOptions).result()
+
+def execute_query(client, queryStr, dataset, queryParams):
+  query = queryStr.format(dataset)
+  jobConfig = bq.QueryJobConfig()
+  jobConfig.query_parameters = queryParams
+  queryJob = client.query(
+      query,
+      job_config=jobConfig)  
+  return queryJob.result()
