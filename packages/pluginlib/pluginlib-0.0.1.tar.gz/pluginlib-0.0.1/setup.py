af3c1a9bb82b9dbdcd694c2ddb7b0ee0278341e8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 05:06:49 +0000

    Add package pluginlib/0.0.1/pluginlib-0.0.1.tar.gz

---
+++ packages/pluginlib/pluginlib-0.0.1.tar.gz/pluginlib-0.0.1/setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup
+
+setup(
+    name='pluginlib',
+    version='0.0.1',
+    description='A framework for creating and importing plugins',
+    long_description='Coming Soon...',
+    url='https://github.com/Rockhopper-Technologies/pluginlib',
+    license='MPLv2.0',
+    author='Avram Lubkin',
+    author_email='avylove@rockhopper.net',
+    packages=[],
+    classifiers=['Development Status :: 1 - Planning'],
+)
