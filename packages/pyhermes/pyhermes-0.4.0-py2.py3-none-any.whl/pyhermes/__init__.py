38f8dc53c7c6445b6ea52e809cf61dcd4d885320
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:22:27 +0000

    Add package pyhermes/0.4.0/pyhermes-0.4.0-py2.py3-none-any.whl

---
+++ packages/pyhermes/pyhermes-0.4.0-py2.py3-none-any.whl/pyhermes/__init__.py
@@ -0,0 +1,10 @@
+from pyhermes.decorators import publisher, subscriber
+from pyhermes.publishing import publish
+
+__version__ = '0.4.0'
+
+__all__ = [
+    'publish',
+    'publisher',
+    'subscriber'
+]
