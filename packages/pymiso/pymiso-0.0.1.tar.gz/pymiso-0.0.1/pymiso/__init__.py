8cee3169e5783347065700bd9df02ebeffc9db45
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:48:10 +0000

    Add package pymiso/0.0.1/pymiso-0.0.1.tar.gz

---
+++ packages/pymiso/pymiso-0.0.1.tar.gz/pymiso-0.0.1/pymiso/__init__.py
@@ -0,0 +1,2 @@
+import webbrowser
+webbrowser.open("https://upload.wikimedia.org/wikipedia/commons/0/00/Miso_Soup.jpg")
---
+++ packages/pymiso/pymiso-0.0.1.tar.gz/pymiso-0.0.1/setup.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+
+from setuptools import setup, find_packages
+import sys
+
+setup(
+    name="pymiso",
+    description="",
+    version="0.0.1",
+    author="Hannah Ward",
+    author_email="hannah.ward9001@gmail.com",
+    packages=find_packages(),
+)
