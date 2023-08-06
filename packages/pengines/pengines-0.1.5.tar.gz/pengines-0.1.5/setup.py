7817a2d3775ec1d4db69f1137cb9e92e12378112
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:48:13 +0000

    Add package pengines/0.1.5/pengines-0.1.5.tar.gz

---
+++ packages/pengines/pengines-0.1.5.tar.gz/pengines-0.1.5/setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup
+setup(
+    name = "pengines",
+    packages = ["pengines"],
+    version = "0.1.5",
+    description = "A simple python library for interacting with SWI-Prologs Pengines",
+    author = "Ian Andrich",
+    author_email = "iandrich87+github@gmail.com",
+    url = "https://github.com/ian-andrich/PythonPengines",
+    download_url = "https://github.com/ian-andrich/PythonPengines/0.1.4",
+
+    keywords = ["pengines", "pengine", "prolog", "swipl", "swi-prolog", "logic programming"],
+    classifiers = [],
+)
