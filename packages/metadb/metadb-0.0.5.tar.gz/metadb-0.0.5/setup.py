e91ac0c9f76b032bdc7be081602bf25debd72cc9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:17:08 +0000

    Add package metadb/0.0.5/metadb-0.0.5.tar.gz

---
+++ packages/metadb/metadb-0.0.5.tar.gz/metadb-0.0.5/setup.py
@@ -0,0 +1,20 @@
+# Copyright (c) 2018 WeFindX Foundation, CLG.
+# All Rights Reserved.
+
+from setuptools import find_packages, setup
+
+setup(
+    name='metadb',
+    version='0.0.5',
+    description='A pattern of types theory to mongodb.',
+    url='https://gitlab.com/wefindx/metadb',
+    author='Mindey',
+    author_email='mindey@qq.com',
+    license='ASK FOR PERMISSIONS',
+    packages = find_packages(exclude=['docs', 'tests*']),
+    install_requires=["pymongo", "typology", "metaform", "requests", "progress"],
+    extras_require = {
+        'test': ['coverage', 'pytest', 'pytest-cov'],
+    },
+    zip_safe=False
+)
