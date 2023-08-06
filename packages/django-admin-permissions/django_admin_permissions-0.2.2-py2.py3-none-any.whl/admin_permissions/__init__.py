94f153c363c04d8b26e13a4987b7994e9c7faf84
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:35:20 +0000

    Add package django-admin-permissions/0.2.2/django_admin_permissions-0.2.2-py2.py3-none-any.whl

---
+++ packages/django-admin-permissions/django_admin_permissions-0.2.2-py2.py3-none-any.whl/admin_permissions/__init__.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+
+from __future__ import unicode_literals
+
+
+__author__ = 'Dmitriy Sokolov'
+__version__ = '0.2.2'
+
+
+default_app_config = 'admin_permissions.apps.AdminPermissionsConfig'
+
+
+VERSION = __version__
