b23886fc178b4eb3b4c02a61dd078fc30f6f2f35
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:20:56 +0000

    Add package boggart/0.1.9/boggart-0.1.9.tar.gz

---
+++ packages/boggart/boggart-0.1.9.tar.gz/boggart-0.1.9/src/boggart/core/replacement.py
@@ -0,0 +1,42 @@
+__all__ = ['Replacement']
+
+from .location import FileLocationRange
+
+
+# FIXME: use attrs
+class Replacement(object):
+    """
+    Describes the replacement of a contiguous body of text in a single source
+    code file with a provided text.
+    """
+    def __init__(self,
+                 location: FileLocationRange,
+                 text: str
+                 ) -> None:
+        self.__location = location
+        self.__text = text
+
+    def __repr__(self) -> str:
+        return "Replacement({}, {})".format(repr(self.location),
+                                            self.text)
+
+    @property
+    def filename(self) -> str:
+        """
+        The name of the file in which the replacement should be made.
+        """
+        return self.__location.filename
+
+    @property
+    def location(self) -> FileLocationRange:
+        """
+        The contiguous range of text that should be replaced.
+        """
+        return self.__location
+
+    @property
+    def text(self) -> str:
+        """
+        The source text that should be used as a replacement.
+        """
+        return self.__text
