2257051785c240a2b6d118536462fa808333b74f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:09:16 +0000

    Add package dvh-analytics/0.3.27/dvh-analytics-0.3.27.tar.gz

---
+++ packages/dvh-analytics/dvh-analytics-0.3.27.tar.gz/dvh-analytics-0.3.27/dvh/get_settings.py
@@ -0,0 +1,15 @@
+from options import SETTINGS_PATHS
+import os
+
+
+def get_settings(settings_type):
+    """
+    :param settings_type: either 'sql' or 'import'
+    :return:
+    """
+    if os.path.isdir(SETTINGS_PATHS['docker'][settings_type]):
+        print SETTINGS_PATHS['docker'][settings_type]
+        return SETTINGS_PATHS['docker'][settings_type]
+    else:
+        script_dir = os.path.dirname(__file__)
+        return os.path.join(script_dir, SETTINGS_PATHS['default'][settings_type])
