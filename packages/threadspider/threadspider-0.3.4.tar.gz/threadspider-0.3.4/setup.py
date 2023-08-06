08b7d92320ab696b2f7cef3216706f304ea65f99
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:19:21 +0000

    Add package threadspider/0.3.4/threadspider-0.3.4.tar.gz

---
+++ packages/threadspider/threadspider-0.3.4.tar.gz/threadspider-0.3.4/setup.py
@@ -0,0 +1,12 @@
+#coding:utf-8
+__author__ = 'admin'
+# --------------------------------
+# Created by admin  on 2016/11/29.
+# ---------------------------------
+from setuptools import  setup,find_packages
+setup(
+    name="threadspider",
+    packages=["threadspider","threadspider.utils","threadspider.utils.tests"],
+    version='0.3.4',
+    install_requires=["setuptools","psutil","selenium","pybloom","pyquery"],
+)
