1a2e54238740ad58f419f77ed40c3400ff6fe2f9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:49:28 +0000

    Add package brume/1.3.2/brume-1.3.2.tar.gz

---
+++ packages/brume/brume-1.3.2.tar.gz/brume-1.3.2/brume/boto_client.py
@@ -0,0 +1,46 @@
+"""Boto clients."""
+
+import boto3
+from botocore.exceptions import ClientError
+
+
+def boto_client(service, region=None):
+    """
+    Instanciate boto client for specified service and region
+    """
+    return boto3.client(service, region_name=region)
+
+
+def cfn_client(region):
+    """
+    Instanciate cloudformation client for specified region
+    """
+    return boto_client('cloudformation', region)
+
+
+def s3_client(region):
+    """
+    Instanciate S3 client for specified region
+    """
+    return boto_client('s3', region)
+
+
+def bucket_exists(region, bucket):
+    """
+    Test if specified bucket exists in region
+    """
+    try:
+        s3_client(region).head_bucket(Bucket=bucket)
+        return True
+    except ClientError:
+        return False
+
+
+def stack(name):
+    resource = boto3.resource('cloudformation')
+    return resource.Stack(name)
+
+
+def stack_resource(stack_id, logical_id):
+    resource = boto3.resource('cloudformation')
+    return resource.StackResource(stack_id, logical_id)
