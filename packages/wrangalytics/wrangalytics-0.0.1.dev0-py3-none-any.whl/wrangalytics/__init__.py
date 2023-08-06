548d6967d018597145c4984b4f7462847bdac2a1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 07:12:01 +0000

    Add package wrangalytics/0.0.1.dev0/wrangalytics-0.0.1.dev0-py3-none-any.whl

---
+++ packages/wrangalytics/wrangalytics-0.0.1.dev0-py3-none-any.whl/wrangalytics/__init__.py
@@ -0,0 +1,10 @@
+name = "wrangalytics"
+
+### Import statements
+# import wrangalytics
+# import wrangalytics.config.api
+# import wrangalytics.converters.api
+# import wrangalytics.eda.viz.api
+# import wrangalytics.eda.stats.api
+
+print("Importing wrangalytics")
