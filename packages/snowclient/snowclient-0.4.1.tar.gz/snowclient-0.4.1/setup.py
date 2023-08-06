c5f74e924fa1e88504d6423a009f4d3c0563302b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:09:39 +0000

    Add package snowclient/0.4.1/snowclient-0.4.1.tar.gz

---
+++ packages/snowclient/snowclient-0.4.1.tar.gz/snowclient-0.4.1/setup.py
@@ -0,0 +1,26 @@
+try:
+    from setuptools import setup
+except ImportError:
+    from distutils.core import setup
+
+import os
+project_root = os.path.dirname(os.path.abspath(__file__))
+with open(os.path.join(project_root, 'snowclient','VERSION')) as version_file:
+    version = version_file.read().strip()
+
+config = {
+    'description': 'ServiceNow api client in python',
+    'long_description': 'Python REST client with some syntax sugar - see QueryBuilder and docs for details.',
+    'author': 'shai rosenfeld',
+    'url': 'https://github.com/shaiguitar/snowclient.py',
+    'download_url': 'https://github.com/shaiguitar/snowclient.py',
+    'author_email': 'shaiguitar@gmail.com',
+    'version': version,
+    'install_requires': ['nose', 'requests', 'responses', 'backoff'],
+    'package_data': {'': ['VERSION','LICENSE.txt']},
+    'packages': ['snowclient'],
+    'scripts': [],
+    'name': 'snowclient'
+}
+
+setup(**config)
