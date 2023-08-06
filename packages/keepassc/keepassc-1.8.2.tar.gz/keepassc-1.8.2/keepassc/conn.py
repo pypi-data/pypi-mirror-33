64d253583812f6174984b98ffe955bd2da7ccfd8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 05:06:19 +0000

    Add package keepassc/1.8.2/keepassc-1.8.2.tar.gz

---
+++ packages/keepassc/keepassc-1.8.2.tar.gz/keepassc-1.8.2/keepassc/conn.py
@@ -0,0 +1,79 @@
+"""This module implements some functions for a connection.
+
+Functions:
+    build_message(parts)
+    receive(conn)
+    sendmsg(sock, msg)
+"""
+
+import logging
+
+
+def build_message(parts):
+    """Join many parts to one message with a seperator
+
+    A message will look like
+
+    b'foo\xB2\xEA\xC0bar\xB2\xEA\xC0foobar'
+
+    so that it could easily splitted by .split
+
+    parts has to be a tuple of bytestrings
+
+    """
+
+    msg = b''
+    for i in parts[:-1]:
+        msg += i
+        msg += b'\xB2\xEA\xC0' # \xB2\xEA\xC0 = BREAK
+    msg += parts[-1]
+
+    return msg
+
+def receive(conn):
+    """Receive a message
+
+    conn has to be the socket which receive the message
+
+    A message has to end with the bytestring  b'\xDE\xAD\xE1\x1D'
+    
+    """
+
+    ip, port = conn.getpeername()
+    logging.info('Receiving a message from '+ip+':'+str(port))
+    data = b''
+    while True:
+        try:
+            received = conn.recv(16)
+            if not received:
+                logging.error("No data received")
+                break
+        except:
+            raise
+        if b'\xDE\xAD\xE1\x1D' in received:
+            data += received[:received.find(b'\xDE\xAD\xE1\x1D')]
+            break
+        else:
+            data += received
+            if data[-4:] == b'\xDE\xAD\xE1\x1D':
+                data = data[:-4]
+                break
+    return data
+
+def sendmsg(sock, msg):
+    """Send message
+
+    sock is the socket which sends the message
+
+    msg hast to be a bytestring
+
+    """
+
+    ip, port = sock.getpeername()
+    try:
+        logging.info('Send a message to '+ip+':'+str(port))
+        # \xDE\xAD\xE1\x1D = DEAD END
+        sock.sendall(msg + b'\xDE\xAD\xE1\x1D')
+    except:
+        raise
+
---
+++ packages/keepassc/keepassc-1.8.2.tar.gz/keepassc-1.8.2/setup.py
@@ -0,0 +1,25 @@
+from os import mkdir, stat
+from stat import ST_MODE
+
+from distutils.core import setup
+
+setup(name = "keepassc",
+      version = "1.8.2",
+      author = "Karsten-Kai König, Scott Hansen",
+      author_email = "grayfox@outerhaven.de",
+      url = "http://raymontag.github.com/keepassc",
+      download_url = "https://github.com/raymontag/keepassc/tarball/master",
+      description = "A password manager that is fully compatible to KeePass v.1.x and KeePassX",
+      packages = ['keepassc'],
+      scripts = ['bin/keepassc', 'bin/keepassc-server', 'bin/keepassc-agent'],
+      install_requires = ['kppy', 'pycryptodomex'],
+      classifiers = [
+          'Programming Language :: Python :: 3.3',
+          'Operating System :: POSIX',
+          'License :: OSI Approved :: GNU General Public License v3 or later (GPLv3+)',
+          'Development Status :: 5 - Production/Stable',
+          'Environment :: Console :: Curses'],
+      license = "ISC, MIT",
+      data_files = [('share/man/man1', ['keepassc.1', 'keepassc-server.1', 'keepassc-agent.1']),
+                    ('share/doc/keepassc', ['README.md', 'LICENSE.md', 'CHANGELOG'])],
+)
