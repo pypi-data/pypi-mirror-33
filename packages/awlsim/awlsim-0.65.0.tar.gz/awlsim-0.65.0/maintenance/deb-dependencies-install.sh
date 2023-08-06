7065bc3744020bc14ed59dab27137531d9fdedf6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:49:09 +0000

    Add package awlsim/0.65.0/awlsim-0.65.0.tar.gz

---
+++ packages/awlsim/awlsim-0.65.0.tar.gz/awlsim-0.65.0/maintenance/deb-dependencies-install.sh
@@ -0,0 +1,24 @@
+#!/bin/sh
+
+exec apt install \
+	python \
+	python-dev \
+	python3 \
+	python3-dev \
+	pypy \
+	pypy-dev \
+	cython \
+	cython3 \
+	build-essential \
+	python-cffi \
+	python3-cffi \
+	python-nose \
+	python3-nose \
+	python-serial \
+	python3-serial \
+	python-pyside \
+	python3-pyside \
+	python-pyqt5 \
+	python3-pyqt5 \
+	time \
+	git
