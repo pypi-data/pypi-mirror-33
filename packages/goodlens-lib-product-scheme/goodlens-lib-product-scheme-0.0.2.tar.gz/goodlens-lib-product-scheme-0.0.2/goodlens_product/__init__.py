039eec578370998dc5b2bd6c9eda7e1b8d9ae4ef
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:48:51 +0000

    Add package goodlens-lib-product-scheme/0.0.2/goodlens-lib-product-scheme-0.0.2.tar.gz

---
+++ packages/goodlens-lib-product-scheme/goodlens-lib-product-scheme-0.0.2.tar.gz/goodlens-lib-product-scheme-0.0.2/goodlens_product/__init__.py
@@ -0,0 +1,11 @@
+# coding: utf-8
+
+# flake8: noqa
+from __future__ import absolute_import
+# import models into model package
+from goodlens_product.create_product_common_response import CreateProductCommonResponse
+from goodlens_product.create_product_food_response import CreateProductFoodResponse
+from goodlens_product.food_origin_material import FoodOriginMaterial
+from goodlens_product.nutrient import Nutrient
+from goodlens_product.product_common import ProductCommon
+from goodlens_product.product_food import ProductFood
