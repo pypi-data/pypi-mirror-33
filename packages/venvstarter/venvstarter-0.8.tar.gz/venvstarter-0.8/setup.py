73f214a23166bfcbf86543c23033c46feafd777c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:36:05 +0000

    Add package venvstarter/0.8/venvstarter-0.8.tar.gz

---
+++ packages/venvstarter/venvstarter-0.8.tar.gz/venvstarter-0.8/setup.py
@@ -0,0 +1,21 @@
+from setuptools import setup
+
+setup(
+      name = "venvstarter"
+    , version = "0.8"
+    , py_modules = ['venvstarter']
+
+    , install_requires =
+      [ "pip"
+      , "virtualenv"
+      ]
+
+    # metadata for upload to PyPI
+    , url = "https://github.com/delfick/venvstarter"
+    , author = "Stephen Moore"
+    , author_email = "delfick755@gmail.com"
+    , description = "Tool to create virtualenvs, manage versions of packages in it and use it start a particular program"
+    , license = "MIT"
+    , keywords = "docker"
+    )
+
