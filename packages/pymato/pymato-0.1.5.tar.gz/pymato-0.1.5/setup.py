c2227700fb2433ccc8b97c572b520f05298347a9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:11:05 +0000

    Add package pymato/0.1.5/pymato-0.1.5.tar.gz

---
+++ packages/pymato/pymato-0.1.5.tar.gz/pymato-0.1.5/setup.py
@@ -0,0 +1,49 @@
+try:
+    from setuptools import setup
+except ImportError:
+    from distutils.core import setup
+
+with open('README.rst') as f:
+    readme = f.read()
+
+setup(
+    name='pymato',
+    version='0.1.5',
+    description='Simple command line pomodoro timer',
+    long_description=readme,
+    author='Wayne Werner',
+    author_email='waynejwerner@gmail.com',
+    license='MIT',
+    url='https://github.com/waynew/pymato',
+    keywords=[
+        'console',
+        'countdown',
+        'stopwatch',
+        'terminal',
+        'timer',
+        'pomodoro',
+    ],
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Environment :: Console :: Curses',
+        'License :: OSI Approved :: MIT License',
+        'Natural Language :: English',
+        'Operating System :: MacOS :: MacOS X',
+        'Operating System :: POSIX',
+        'Operating System :: Unix',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Topic :: Utilities',
+    ],
+    install_requires=[
+        'pytz',
+        'tzlocal',
+    ],
+    py_modules=['pymato'],
+    entry_points={
+        'console_scripts': [
+            'pymato=pymato:main',
+        ],
+    },
+)
