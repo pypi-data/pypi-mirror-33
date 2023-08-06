034e1745587d14988995e2241e24c57acc07ea3a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:35:28 +0000

    Add package grove.py/0.2/grove.py-0.2.tar.gz

---
+++ packages/grove.py/grove.py-0.2.tar.gz/grove.py-0.2/grove/grove_temperature_sensor.py
@@ -0,0 +1,46 @@
+
+
+import math
+import sys
+import time
+from grove.adc import ADC
+
+
+class GroveTemperatureSensor:
+    B = 4275.
+    R0 = 100000.
+
+    def __init__(self, channel):
+        self.channel = channel
+        self.adc = ADC()
+    
+    @property
+    def temperature(self):
+        value = self.adc.read(self.channel)
+        if value <= 0 or value >= 1000:
+            return float('nan')
+
+        r = 1000. / value - 1.
+        r = self.R0 * r
+
+        return 1. / (math.log10(r / self.R0) / self.B + 1 / 298.15) - 273.15
+
+
+Grove = GroveTemperatureSensor
+
+
+def main():
+    if len(sys.argv) < 2:
+        print('Usage: {} adc_channel'.format(sys.argv[0]))
+        sys.exit(1)
+
+    sensor = GroveTemperatureSensor(int(sys.argv[1]))
+
+    print('Detecting temperature...')
+    while True:
+        print('{} Celsius'.format(sensor.temperature))
+        time.sleep(1)
+
+
+if __name__ == '__main__':
+    main()
