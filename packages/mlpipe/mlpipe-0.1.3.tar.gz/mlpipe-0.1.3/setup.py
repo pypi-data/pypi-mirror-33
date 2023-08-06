5f9b168aef7b47fd254f8f9bc43cc4b8c5886b90
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:14:49 +0000

    Add package mlpipe/0.1.3/mlpipe-0.1.3.tar.gz

---
+++ packages/mlpipe/mlpipe-0.1.3.tar.gz/mlpipe-0.1.3/setup.py
@@ -0,0 +1,21 @@
+from setuptools import setup, find_packages
+print find_packages(exclude=['mlpipe.django.mlpipe.migrations', 'mlpipe.django.mlpipe.templatetags'])
+
+
+setup(
+    name='mlpipe',
+    version='0.1.3',
+    author='Longbin Chen',
+    author_email='lbchen@gmail.com',
+    packages=find_packages(exclude=['mlpipe.django.mlpipe.migrations', 'mlpipe.django.mlpipe.templatetags']),
+    package_data={'':['*.yaml',]},
+    scripts=['mlpipe/django/manage.py',],
+    license='LICENSE.txt',
+    url='https://github.com/LongbinChen/mlpipe',
+    description='A toolkit to manage machine learning libraries.',
+    long_description=open('README.md').read(),
+    include_package_data=True,
+    install_requires=[
+        "Django >= 1.8.1",
+    ],
+)
