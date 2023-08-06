fbcd1ae0f7ecf2b266eb43ea0c95264d67ffe4d3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 05:07:47 +0000

    Add package lore/0.5.27/lore-0.5.27-py2.py3-none-any.whl

---
+++ packages/lore/lore-0.5.27-py2.py3-none-any.whl/lore/estimators/holt_winters/__init__.py
@@ -0,0 +1,31 @@
+import logging
+
+from sklearn.base import BaseEstimator
+import holtwinters
+from lore.util import timed
+
+
+class HoltWinters(BaseEstimator):
+
+  def __init__(self, **kwargs):
+    super(HoltWinters, self).__init__()
+    self.periodicity = kwargs.get('periodicity')
+    self.forecasts = kwargs.get('days_to_forecast')
+    self.kwargs = kwargs
+    self.params = None
+
+  @timed(logging.INFO)
+  def fit(self, x, y=None):
+    results = holtwinters.additive(x, self.periodicity, self.forecasts, 
+      alpha=self.kwargs.get('alpha'),
+      beta=self.kwargs.get('beta'),
+      gamma=self.kwargs.get('gamma'))
+    self.params = {'alpha': results[1], 'beta': results[2], 'gamma': results[3]}
+    self.rmse = results[4]
+    return {'alpha': results[1], 'beta': results[2], 'gamma': results[3], 'RMSE': self.rmse}
+
+  @timed(logging.INFO)
+  def predict(self, X):
+    return holtwinters.additive(X, self.periodicity, self.forecasts, 
+      **self.params)[0]
+    
