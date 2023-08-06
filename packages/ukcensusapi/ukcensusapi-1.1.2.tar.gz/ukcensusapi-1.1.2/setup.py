acc255fec1e77d5ac0ea5c50ccbf337013a3ddae
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:34:47 +0000

    Add package ukcensusapi/1.1.2/ukcensusapi-1.1.2.tar.gz

---
+++ packages/ukcensusapi/ukcensusapi-1.1.2.tar.gz/ukcensusapi-1.1.2/setup.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python3
+
+from setuptools import setup
+
+def readme():
+  with open('README.md') as f:
+    return f.read()
+
+setup(name='ukcensusapi',
+  version='1.1.2',
+  description='UK census data query automation',
+  long_description=readme(),
+  url='https://github.com/virgesmith/UKCensusAPI',
+  author='Andrew P Smith',
+  author_email='a.p.smith@leeds.ac.uk',
+  license='MIT',
+  packages=['ukcensusapi'],
+  python_requires='>=3',
+  zip_safe=False,
+  test_suite='nose.collector',
+  tests_require=['nose'],
+  package_data = {
+    # paths relative to the package directory
+    'ukcensusapi': ['../README.md'],
+  },
+  include_package_data=True
+)
