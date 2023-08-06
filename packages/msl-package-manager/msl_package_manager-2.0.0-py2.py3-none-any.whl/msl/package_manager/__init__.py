90f05ba51a1e25fa145c2df6b5f351d56e1c7049
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:35:16 +0000

    Add package msl-package-manager/2.0.0/msl_package_manager-2.0.0-py2.py3-none-any.whl

---
+++ packages/msl-package-manager/msl_package_manager-2.0.0-py2.py3-none-any.whl/msl/package_manager/__init__.py
@@ -0,0 +1,25 @@
+"""
+Install, uninstall, update, list and create MSL packages.
+
+The following constants are available.
+"""
+from collections import namedtuple
+
+__author__ = 'Joseph Borbely'
+__copyright__ = '\xa9 2017 - 2018, ' + __author__
+__version__ = '2.0.0'
+
+version_info = namedtuple('version_info', 'major minor micro')(*map(int, __version__.split('.')[:3]))
+""":obj:`~collections.namedtuple`: Contains the version information as a (major, minor, micro) tuple."""
+
+_PKG_NAME = 'msl-package-manager'
+
+from .utils import pypi
+from .utils import github
+from .utils import installed
+from .utils import info
+from .utils import set_log_level
+from .update import update
+from .create import create
+from .install import install
+from .uninstall import uninstall
