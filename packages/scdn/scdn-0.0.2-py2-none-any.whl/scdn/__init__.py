c96e0c457a04c5695a6bcc730ef776b16f6b9b1b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:25:41 +0000

    Add package scdn/0.0.2/scdn-0.0.2-py2-none-any.whl

---
+++ packages/scdn/scdn-0.0.2-py2-none-any.whl/scdn/__init__.py
@@ -0,0 +1,8 @@
+"""
+This package implements sparse causal dynamic network analysis for convolution model
+
+Main function: scdn_analysis
+"""
+from scdn.scdn_analysis import *
+from scdn.evaluation import eva
+__all__ = ['scdn_multi_sub', 'eva']
