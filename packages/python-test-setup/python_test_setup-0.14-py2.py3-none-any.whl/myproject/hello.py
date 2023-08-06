cac61b7fbba8e914b218ee526699fbdc0aedcbeb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:24:10 +0000

    Add package python-test-setup/0.14/python_test_setup-0.14-py2.py3-none-any.whl

---
+++ packages/python-test-setup/python_test_setup-0.14-py2.py3-none-any.whl/myproject/hello.py
@@ -0,0 +1,11 @@
+"""Some very simple functions to test."""
+
+class Hello():
+    def say_hello(self):
+        return "hello"
+
+    def say_hi(self):
+        return "hi"
+
+    def oh_dear(self):
+        raise Exception("Oh dear!")
