e1a6ca94c6836502663b4fac4ba0589a1856086e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:49:34 +0000

    Add package tkprocess/0.1/tkprocess-0.1-py3.6.egg

---
+++ packages/tkprocess/tkprocess-0.1-py3.6.egg/EGG-INFO/scripts/run_tkprocess.py
@@ -0,0 +1,6 @@
+#!C:\Python36-32\python.exe
+# coding=utf-8
+# Stan 2018-06-26
+
+from tkprocess.tkprocess import main
+main()
---
+++ packages/tkprocess/tkprocess-0.1-py3.6.egg/tkprocess/__init__.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python
+# coding=utf-8
+
+
+__pkgname__ = __name__
+__description__ = "A subprocess application based on Tk."
+__version__ = "0.1"
