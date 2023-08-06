48382929252f8c7a19ba7505acb756b7a6e03a36
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:36:48 +0000

    Add package omxware/0.0.1.2.4/omxware-0.0.1.2.4-py2.py3-none-any.whl

---
+++ packages/omxware/omxware-0.0.1.2.4-py2.py3-none-any.whl/omxware/__init__.py
@@ -0,0 +1,9 @@
+# -*- coding: utf-8 -*-
+
+"""Top-level package for OMXWare."""
+
+__author__ = """Harsha Krishnareddy"""
+__email__ = 'hvkrishn@us.ibm.com'
+__version__ = '0.0.1.2'
+
+from omxware.OMXWare import OMXWare
