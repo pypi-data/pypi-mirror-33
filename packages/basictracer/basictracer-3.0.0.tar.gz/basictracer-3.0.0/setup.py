6f0aaa285a12cdccd896954e29b04754d63c5a04
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:52:55 +0000

    Add package basictracer/3.0.0/basictracer-3.0.0.tar.gz

---
+++ packages/basictracer/basictracer-3.0.0.tar.gz/basictracer-3.0.0/setup.py
@@ -0,0 +1,41 @@
+from setuptools import setup
+
+setup(
+    name='basictracer',
+    version='3.0.0',
+    author='The OpenTracing Authors',
+    author_email='info@opentracing.io',
+    license='MIT',
+    url='https://github.com/opentracing/basictracer-python',
+    keywords=['basictracer', 'opentracing'],
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: Implementation :: PyPy',
+        'Topic :: Software Development :: Libraries :: Python Modules',
+    ],
+    packages=['basictracer'],
+    include_package_data=True,
+    zip_safe=False,
+    platforms='any',
+    install_requires=[
+        'protobuf>=3.0.0b2.post2',
+        'opentracing==2.0.0',
+        'six>=1.10.0,<2.0',
+    ],
+    extras_require={
+        'tests': [
+            'doubles',
+            'flake8<3',  # see https://github.com/zheller/flake8-quotes/issues/29
+            'flake8-quotes',
+            'mock<1.1.0',
+            'pytest>=2.7,<3',
+            'pytest-cov',
+            'pytest-mock',
+            'Sphinx',
+            'sphinx_rtd_theme'
+        ]
+    },
+)
