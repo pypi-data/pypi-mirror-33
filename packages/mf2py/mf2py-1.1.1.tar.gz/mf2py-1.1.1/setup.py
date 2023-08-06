cdf84dbe35947cd5f607e15a8c8d19884b072688
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:14:54 +0000

    Add package mf2py/1.1.1/mf2py-1.1.1.tar.gz

---
+++ packages/mf2py/mf2py-1.1.1.tar.gz/mf2py-1.1.1/setup.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python
+
+from setuptools import setup
+import os.path
+
+ns = {}
+with open(os.path.join(os.path.dirname(__file__), 'mf2py/version.py'))\
+        as version_file:
+    exec(version_file.read(), ns)
+
+
+setup(name='mf2py',
+      version=ns['__version__'],
+      description='Python Microformats2 parser',
+      author='Tom Morris',
+      author_email='tom@tommorris.org',
+      url='http://microformats.org/wiki/mf2py',
+      install_requires=['html5lib', 'requests', 'BeautifulSoup4'],
+      tests_require=['nose', 'mock'],
+      packages=['mf2py'],
+      package_data={'mf2py': ['backcompat-rules/*.json']},
+      test_suite='nose.collector',
+      classifiers=[
+          'Development Status :: 4 - Beta',
+          'Intended Audience :: Developers',
+          'License :: OSI Approved :: MIT License',
+          'Natural Language :: English',
+          'Programming Language :: Python :: 2.7',
+          'Programming Language :: Python :: 3',
+          'Topic :: Text Processing :: Markup :: HTML'
+      ])
