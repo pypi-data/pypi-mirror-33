4b2e9e4bdedeb0ccaa650a6b4df8484708c125dd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:37:16 +0000

    Add package numpyx/0.3.0/numpyx-0.3.0.tar.gz

---
+++ packages/numpyx/numpyx-0.3.0.tar.gz/numpyx-0.3.0/setup.py
@@ -0,0 +1,52 @@
+"""
+numpyx. Some accelerated funcs for numpy
+
+"""
+import sys
+import os
+from setuptools import setup, Extension
+import numpy
+
+
+if sys.argv[-1] == 'publish':
+    os.system('python setup.py sdist upload -r pypi')
+    sys.exit()
+elif sys.argv[-1] == 'testpublish':
+    os.system('python setup.py sdif upload -r pypitest')
+    sys.exit()
+
+
+setup(
+    name = "numpyx",
+    setup_requires=[
+        'cython'
+    ],
+    ext_modules=[
+        Extension(
+            'numpyx',
+            sources=['numpyx.pyx'],
+            include_dirs=[numpy.get_include()]
+        ),
+    ],
+    # ext_modules = cythonize('numpyx.pyx'),  # accepts a glob pattern
+
+    # metadata
+    version          = "0.3.0",
+    url              = 'https://github.com/gesellkammer/numpyx',
+    download_url     = 'https://github.com/gesellkammer/numpyx',
+    author           = 'eduardo moguillansky',
+    author_email     = 'eduardo.moguillansky@gmail.com',
+    maintainer       = '',
+    maintainer_email = '',
+    install_requires = ["numpy", "cython"],
+    description = "Utility functions for numpy, written in cython",
+    license="GPL v3",
+    classifiers=[
+        'Development Status :: 2 - Pre-Alpha',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: BSD License',
+        'Natural Language :: English',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.6',
+    ]
+)
