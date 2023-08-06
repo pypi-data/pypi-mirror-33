7d329f829f337591359c72148cb0662b6fd55a63
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:15:54 +0000

    Add package broker_test/0.1/broker_test-0.1.tar.gz

---
+++ packages/broker_test/broker_test-0.1.tar.gz/broker_test-0.1/setup.py
@@ -0,0 +1,8 @@
+from setuptools import setup
+
+setup(
+    name='broker_test',    # This is the name of your PyPI-package.
+    version='0.1',                          # Update the version number for new releases
+    scripts=['broker_test.py']                  # The name of your scipt, and also the command you'll be using for calling it
+)
+
