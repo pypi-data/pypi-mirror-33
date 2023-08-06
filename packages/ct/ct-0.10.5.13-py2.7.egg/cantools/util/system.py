1ca9cf1ceb427da2dd1a80615da697fb37596e2b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:50:18 +0000

    Add package ct/0.10.5.13/ct-0.10.5.13-py2.7.egg

---
+++ packages/ct/ct-0.10.5.13-py2.7.egg/cantools/util/system.py
@@ -0,0 +1,48 @@
+import commands, os, subprocess
+from reporting import log
+from io import write
+
+def cp(content, fname): # more write than copy, buuuut...
+	log("writing %s"%(fname,), 2)
+	write(content, fname)
+
+def sym(src, dest, safe=False):
+	log("symlinking %s to %s"%(src, dest), 2)
+	try:
+		os.symlink(src, dest)
+	except Exception, e:
+		log("symlinking failed (%s) - file exists: %s"%(e, dest), 3)
+		if not safe:
+			try:
+				rm(dest)
+				sym(src, dest, True)
+			except:
+				log("ok, really failed - skipping", 3)
+
+def mkdir(pname, recursive=False):
+	log("new directory: %s"%(pname,), 2)
+	if recursive:
+		os.makedirs(pname)
+	else:
+		os.mkdir(pname)
+
+def rm(pname):
+	if os.path.islink(pname):
+		log("removing symlink: %s"%(pname,), 2)
+		os.remove(pname)
+	elif os.path.isdir(pname):
+		log("removing folder: %s"%(pname,), 2)
+		os.rmdir(pname)
+	elif os.path.exists(pname):
+		log("removing file: %s"%(pname,), 2)
+		os.remove(pname)
+	else:
+		log("can't remove file (doesn't exist): %s"%(pname,), 2)
+
+def cmd(cline):
+	log('issuing command: "%s"'%(cline,), 2)
+	subprocess.call(cline, shell=True)
+
+def output(cline):
+	log('getting output for: "%s"'%(cline,), 2)
+	return commands.getoutput(cline)
