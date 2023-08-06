3625784ae19410bd126cb183b26f7a59cc85b134
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:07:03 +0000

    Add package chatendpoint/0.3.0/chatendpoint-0.3.0.tar.gz

---
+++ packages/chatendpoint/chatendpoint-0.3.0.tar.gz/chatendpoint-0.3.0/tests/test_chatendpoint.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+"""Tests for `chatendpoint` package."""
+
+
+import unittest
+
+from chatendpoint import chatendpoint
+
+
+class TestChatendpoint(unittest.TestCase):
+    """Tests for `chatendpoint` package."""
+
+    def setUp(self):
+        """Set up test fixtures, if any."""
+
+    def tearDown(self):
+        """Tear down test fixtures, if any."""
+
+    def test_000_something(self):
+        """Test something."""
