3847932f339875a1164ae52a47fa43f500391971
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:35:30 +0000

    Add package nomunkhet_rpi/0.7.2.5/nomunkhet_rpi-0.7.2.5.tar.gz

---
+++ packages/nomunkhet_rpi/nomunkhet_rpi-0.7.2.5.tar.gz/nomunkhet_rpi-0.7.2.5/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='nomunkhet_rpi',
+      version='0.7.2.5',
+      description='Тус сан нь Номун хэт ХХК-н Электроникийн сургалтын "Raspberry Pi-д суурилсан IoT хөгжүүлэх сургалт"-д зориулагдсан болно.',
+      url='http://elecbook.net',
+      author='Garid',
+      author_email='garidzorigoo@gmail.com',
+      license='MIT',
+      packages=['nomunkhet_rpi'], 
+      zip_safe=False)
