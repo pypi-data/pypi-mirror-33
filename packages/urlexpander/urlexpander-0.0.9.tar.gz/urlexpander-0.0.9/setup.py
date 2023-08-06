cb4ae1b39aa00278ad0951f51dc0425d36b22aa4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:10:15 +0000

    Add package urlexpander/0.0.9/urlexpander-0.0.9.tar.gz

---
+++ packages/urlexpander/urlexpander-0.0.9.tar.gz/urlexpander-0.0.9/setup.py
@@ -0,0 +1,23 @@
+import sys
+from setuptools import setup
+
+setup(
+    name="urlexpander",
+    packages=['urlexpander', 'urlexpander.core'],
+    py_modules=['urlexpander'],
+    version='0.0.9',
+    description="urlExpander is a Python package for quickly and thoroughly expanding shortened URLs.",
+    long_description="This package makes working with link data from social media and webpages easier. It not only expands links, but catches errors, and makes parallel link expansion quick and efficient.",
+    author="leon yin",
+    author_email="",
+    url="https://github.com/SMAPPNYU/urlExpander",
+    keywords='smapp social media unshorten expand link url',
+    license="MIT",
+    install_requires=[
+        'tldextract',
+        'pandas',
+        'numpy',
+        'tqdm',
+        'unshortenit'
+    ]
+)
