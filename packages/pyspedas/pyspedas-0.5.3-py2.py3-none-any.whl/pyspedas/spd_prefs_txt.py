ebcdce3a5b0a486810b6f528ae95c4a172e15548
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:49:54 +0000

    Add package pyspedas/0.5.3/pyspedas-0.5.3-py2.py3-none-any.whl

---
+++ packages/pyspedas/pyspedas-0.5.3-py2.py3-none-any.whl/pyspedas/spd_prefs_txt.py
@@ -0,0 +1,5 @@
+# -*- coding: utf-8 -*-
+#Locations of remote files and local directory for cdf files
+#You can change data_dir to a directory of your choice
+data_dir='C:\Datapy\themis'
+themis_remote='http://themis.ssl.berkeley.edu/data/themis/'
