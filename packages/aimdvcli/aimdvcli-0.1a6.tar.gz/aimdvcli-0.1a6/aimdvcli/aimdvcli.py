9a1a15ff8757296de02854f1fa537ec847c65347
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 05:06:35 +0000

    Add package aimdvcli/0.1a6/aimdvcli-0.1a6.tar.gz

---
+++ packages/aimdvcli/aimdvcli-0.1a6.tar.gz/aimdvcli-0.1a6/aimdvcli/aimdvcli.py
@@ -0,0 +1,29 @@
+import sys
+import os
+
+from . import audio
+ 
+def help ():
+    print ("usage: aimdvcli <command> [<options>]")
+    print ("command:")
+    print ("  convert: convert to standard wave audio")        
+    sys.exit ()
+     
+def main ():    
+    try: 
+        cmd = sys.argv [1]
+    except IndexError:
+        help ()        
+    sys.argv.pop (1)    
+    if cmd == "audio":
+        cmd = "convert"
+        sys.argv.pop (1)
+        
+    if cmd in ("convert"):
+        audio.convert ()    
+    else:
+        print ("unknown conmand")
+
+
+if __name__ == "__main__":
+    main ()
