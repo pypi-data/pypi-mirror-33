cab809561d122298a64f6afa6a3561e4fb35f335
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:33:37 +0000

    Add package easyfunc/0.1/easyfunc-0.1.tar.gz

---
+++ packages/easyfunc/easyfunc-0.1.tar.gz/easyfunc-0.1/setup.py
@@ -0,0 +1,14 @@
+from distutils.core import setup
+
+setup(
+    name = 'easyfunc',
+    packages = ['easyfunc'],
+    version = '0.1',  # Ideally should be same as your GitHub release tag varsion
+    description = 'Easy and simple functional programming style helper for python.',
+    author = 'Key',
+    author_email = 'orangewar@hotmail.com',
+    url = 'https://github.com/DiamondGo/easyfunc',
+    download_url = 'https://github.com/DiamondGo/easyfunc/archive/0.1.tar.gz',
+    keywords = ['fp', 'python', 'functional'],
+    classifiers = [],
+)
