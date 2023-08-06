69032d9da435a6d74af32c565ae82d5d95dc1cb1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:17:58 +0000

    Add package cjs-fdc/0.0.1b1/cjs_fdc-0.0.1b1.tar.gz

---
+++ packages/cjs-fdc/cjs_fdc-0.0.1b1.tar.gz/cjs_fdc-0.0.1b1/setup.py
@@ -0,0 +1,31 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="cjs_fdc",
+    version="0.0.1b1",
+    author="CJS",
+    author_email="0empcjs@gmail.com",
+    description="count the number of files or dirs in a given dir",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/CCC-S/cjs_fdc",
+    packages=setuptools.find_packages(),
+    include_package_data=True,
+    install_requires=[
+        'Click',
+    ],
+    classifiers=(
+        "Development Status :: 4 - Beta",
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: POSIX :: Linux",
+        "Operating System :: MacOS",
+    ),
+    entry_points='''
+        [console_scripts]
+        cjs_fdc=cjs_fdc:fdc
+    ''',
+)
