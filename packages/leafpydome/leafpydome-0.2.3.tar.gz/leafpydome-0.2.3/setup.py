39043372e03bf8fef7f14023cb25cea92792363c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 07:12:49 +0000

    Add package leafpydome/0.2.3/leafpydome-0.2.3.tar.gz

---
+++ packages/leafpydome/leafpydome-0.2.3.tar.gz/leafpydome-0.2.3/setup.py
@@ -0,0 +1,21 @@
+
+import sys
+from setuptools import setup, find_packages
+
+open_kwds = {}
+if sys.version_info > (3,):
+    open_kwds['encoding'] = 'utf-8'
+
+setup(name='leafpydome',
+      version='0.2.3',
+      description='Lightweight python interface to the nissan leaf based on leafpy package.',
+      classifiers=[],
+      keywords='',
+      license='MIT',
+      packages=find_packages(exclude=['docs','tests','examples']),
+      include_package_data=True,
+      zip_safe=False,
+      install_requires=['requests','pycryptodome'],
+      setup_requires=['pytest-runner'],
+      tests_require=['pytest','vcrpy']
+)
