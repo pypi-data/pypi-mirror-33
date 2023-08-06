d645bb7c42f4bc7326a443bede32bc6ea44f7de8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:48:33 +0000

    Add package rakshita-jain/1.0.5/rakshita_jain-1.0.5.tar.gz

---
+++ packages/rakshita-jain/rakshita_jain-1.0.5.tar.gz/rakshita_jain-1.0.5/rakshita1.py
@@ -0,0 +1,2 @@
+def work():
+    return (' I need some rest, Deadline se pahle kaam finish kar deti hu. ')
---
+++ packages/rakshita-jain/rakshita_jain-1.0.5.tar.gz/rakshita_jain-1.0.5/setup.py
@@ -0,0 +1,10 @@
+from distutils.core import setup
+setup(
+    name='rakshita_jain',
+    version='1.0.5',
+    py_modules=['rakshita1'],
+    install_requires=['requests'],
+    author='lovetocode',
+    author_email='yogesh.chandra.iitbhu@gmail.com',
+    description='pretty_list() function print the nested list in beautiful pattern',
+)
