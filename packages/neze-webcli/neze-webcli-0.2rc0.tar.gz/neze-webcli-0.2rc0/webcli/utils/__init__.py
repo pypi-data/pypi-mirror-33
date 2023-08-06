462edf334f67893cea24e5cc58be963409db6626
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:36:07 +0000

    Add package neze-webcli/0.2rc0/neze-webcli-0.2rc0.tar.gz

---
+++ packages/neze-webcli/neze-webcli-0.2rc0.tar.gz/neze-webcli-0.2rc0/webcli/utils/__init__.py
@@ -0,0 +1,8 @@
+from . import rodict,docker,choices,pep,git
+
+def dict_update(d,s):
+    if hasattr(s,'keys'):
+        for k in s.keys():
+            d[k] = dict_update(d.get(k,{}),s[k])
+        return d
+    return s
