0cb7080d8efcf0417ef3d6d2b2e8adc27f23cde2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:35:04 +0000

    Add package BackupMega/0.0.1/BackupMega-0.0.1.tar.gz

---
+++ packages/BackupMega/BackupMega-0.0.1.tar.gz/BackupMega-0.0.1/setup.py
@@ -0,0 +1,24 @@
+from os.path import join, dirname
+
+from setuptools import find_packages
+
+try:
+    from setuptools.core import setup
+except ImportError:
+    from distutils.core import setup
+
+setup(
+    name='BackupMega',
+    version='0.0.1',
+    author='Nurlan Melis (NMelis)',
+    author_email='melis.zhoroev@gmail.com',
+    packages=find_packages(),
+    url='',
+    license='LICENSE.txt',
+    description='Backup system and push backup zip to storage mega.nz.',
+    long_description=open(join(dirname(__file__), 'README.txt')).read(),
+    entry_points={
+        'console_scripts':
+            ['backup = backup.main:backup']
+        },
+)
