a278459e287a38d9df6b68cd14b5fdd9080ec47e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:54:49 +0000

    Add package fena/0.0.4/fena-0.0.4.tar.gz

---
+++ packages/fena/fena-0.0.4.tar.gz/fena-0.0.4/setup.py
@@ -0,0 +1,35 @@
+import os
+from setuptools import setup
+
+"""
+Resourses:
+    https://pythonhosted.org/an_example_pypi_project/setuptools.html
+    https://packaging.python.org/tutorials/packaging-projects/
+    https://packaging.python.org/guides/distributing-packages-using-setuptools/
+
+Examples:
+    pylint: https://github.com/PyCQA/pylint/blob/master/setup.py
+    pyexpander: https://bitbucket.org/goetzpf/pyexpander/src/b466de6fd801545650edfa790a18f022dc7e151a/setup.py?at=default&fileviewer=file-view-default
+"""
+
+
+setup(
+    name="fena",
+    version="0.0.4",
+    author="Austin Siew",
+    author_email="glowing.locker@gmail.com",
+    description=("Fena Preprocessor Language for Minecaft"),
+    keywords="minecaft language fena preprocessor",
+    url="https://github.com/Aquafina-water-bottle/Fena",
+    # packages=['fena', 'fena_pyexpander'],
+    install_requires=["pyexpander"],
+    packages=["fenalib", "fena_pyexpander"],
+    scripts=["fena.py"],
+    classifiers=[
+        "Development Status :: 3 - Alpha",
+        'Natural Language :: English',
+        "Programming Language :: Python :: 3.6",
+        "License :: OSI Approved :: MIT License",
+    ],
+)
+
