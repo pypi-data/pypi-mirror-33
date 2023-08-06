85c21bd1bfcb54f803f0ebf405be2ad4897d18f9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:17:16 +0000

    Add package amatino/0.0.5/amatino-0.0.5-py3-none-any.whl

---
+++ packages/amatino/amatino-0.0.5-py3-none-any.whl/amatino/tests/test_sequence.py
@@ -0,0 +1,14 @@
+"""
+Amatino API Python Bindings
+Test Sequence Module
+Author: hugh@amatino.io
+
+Provides a constant manifest of tests to execute
+"""
+
+import amatino.tests.alpha as alpha
+
+SEQUENCE = [
+    alpha.AlphaCreateTest,
+    alpha.AlphaCreateEntityTest
+]
