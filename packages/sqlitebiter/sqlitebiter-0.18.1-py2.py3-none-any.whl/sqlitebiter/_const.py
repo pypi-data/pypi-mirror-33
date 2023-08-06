def9f1648ccc4ae2c10855ee8b7d48ae9e061c1c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:19:33 +0000

    Add package sqlitebiter/0.18.1/sqlitebiter-0.18.1-py2.py3-none-any.whl

---
+++ packages/sqlitebiter/sqlitebiter-0.18.1-py2.py3-none-any.whl/sqlitebiter/_const.py
@@ -0,0 +1,16 @@
+# encoding: utf-8
+
+"""
+.. codeauthor:: Tsuyoshi Hombashi <tsuyoshi.hombashi@gmail.com>
+"""
+
+from __future__ import absolute_import, unicode_literals
+
+
+PROGRAM_NAME = "sqlitebiter"
+MAX_VERBOSITY_LEVEL = 2
+
+SOURCE_INFO_TABLE = "_source_info_"
+
+IPYNB_FORMAT_NAME_LIST = ["ipynb"]
+TABLE_NOT_FOUND_MSG_FORMAT = "table not found in {}"
