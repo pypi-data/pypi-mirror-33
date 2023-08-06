7656cfa43391452c9cfe68690029462e570a394f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:52:48 +0000

    Add package pyvoro-mmalahe/1.3.3/pyvoro-mmalahe-1.3.3.tar.gz

---
+++ packages/pyvoro-mmalahe/pyvoro-mmalahe-1.3.3.tar.gz/pyvoro-mmalahe-1.3.3/setup.py
@@ -0,0 +1,48 @@
+# 
+# setup.py : pyvoro python interface to voro++
+# 
+# this extension to voro++ is released under the original modified BSD license
+# and constitutes an Extension to the original project.
+#
+# Copyright (c) Joe Jordan 2012
+# contact: <joe.jordan@imperial.ac.uk> or <tehwalrus@h2j9k.org>
+#
+
+import setuptools
+from setuptools import setup, Extension
+
+# fall back to provided cpp file if Cython is not found
+extensions = [
+    Extension("pyvoro.voroplusplus",
+              sources=["pyvoro/voroplusplus.cpp",
+                       "pyvoro/vpp.cpp",
+                       "src/voro++.cc"],
+              include_dirs=["src"],
+              language="c++",
+              )
+]
+
+setup(
+    name="pyvoro-mmalahe",
+    version="1.3.3",
+    description="2D and 3D Voronoi tessellations: a python entry point for the voro++ library.",
+    author="Joe Jordan",
+    author_email="joe.jordan@imperial.ac.uk",
+    url="https://github.com/mmalahe/pyvoro",
+    #download_url="https://github.com/joe-jordan/pyvoro/tarball/v1.3.3",
+    packages=["pyvoro",],
+    package_dir={"pyvoro": "pyvoro"},
+    ext_modules=extensions,
+    keywords=["geometry", "mathematics", "Voronoi"],
+    classifiers=[
+        "Development Status :: 5 - Production/Stable",
+        "Topic :: Scientific/Engineering :: Mathematics",
+        "Topic :: Scientific/Engineering :: Physics",
+        "Intended Audience :: Science/Research",
+        "Operating System :: POSIX :: Linux",
+        "Programming Language :: Python :: 2",
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: BSD License",
+    ],
+    test_suite="test",
+)
