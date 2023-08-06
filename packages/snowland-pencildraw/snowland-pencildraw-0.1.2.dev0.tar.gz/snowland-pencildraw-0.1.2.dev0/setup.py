0b34fd044deb694a735ead2bc03c364fa7a8a221
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:43:25 +0000

    Add package snowland-pencildraw/0.1.2.dev0/snowland-pencildraw-0.1.2.dev0.tar.gz

---
+++ packages/snowland-pencildraw/snowland-pencildraw-0.1.2.dev0.tar.gz/snowland-pencildraw-0.1.2.dev0/setup.py
@@ -0,0 +1,46 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# @Time    : 2018/3/4 0004 下午 10:37
+# @Author  : A.Star
+# @Site    :
+# @contact : astar@snowland.ltd
+# @File    : setup.py
+# @Software: PyCharm 
+
+from setuptools import setup, find_packages
+
+setup(
+    name='snowland-pencildraw',
+    version='0.1.2dev',
+    description=(
+        'pencil draw'
+    ),
+    long_description=open('README.rst').read(),
+    author='A.Star',
+    author_email='astar@snowland.ltd',
+    maintainer='A.Star',
+    maintainer_email='astar@snowland.ltd',
+    license='BSD License',
+    packages=find_packages(),
+    platforms=["all"],
+    url='https://gitee.com/hoops/PencilDrawing_python3',
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Operating System :: OS Independent',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: BSD License',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: Implementation',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+        'Topic :: Software Development :: Libraries'
+    ],
+    install_requires=[
+        'scikit-image==0.14.0',
+        'numpy>=1.0.0',
+        'scipy>=0.14.0',
+        'matplotlib>=2.0.0'
+    ]
+)
