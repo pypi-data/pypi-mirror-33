f0317346a4818d5d3ad4ff05603e8db4d024a71f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:59:54 +0000

    Add package pyswrve/0.2.2/pyswrve-0.2.2.tar.gz

---
+++ packages/pyswrve/pyswrve-0.2.2.tar.gz/pyswrve-0.2.2/pyswrve/items_api.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+
+from .api import SwrveApi
+
+
+class SwrveItemsApi(SwrveApi):
+    """
+    Class for viewing/creating/updating items with Swrve Items API
+
+    https://docs.swrve.com/swrves-apis/non-client-apis/swrve-items-api-guide
+    """
+
+    pass
