ca16c2966e7e73e45f79c4c0f81bbdfb334bf973
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:49:54 +0000

    Add package moelog/0.0.0.3/moelog-0.0.0.3-py3-none-any.whl

---
+++ packages/moelog/moelog-0.0.0.3-py3-none-any.whl/moelog/__main.py
@@ -0,0 +1,43 @@
+#coding=utf-8
+import logging
+import sys
+
+__logger_record = {}
+
+def get_logger(logger_name='moelog',logging_level=logging.INFO,both_to_file_path=None):
+    '''
+    example:
+    l = get_logger()
+    l.info('foo')
+    l.i('foo')
+    '''
+
+    if logger_name in __logger_record:
+        result = __logger_record[logger_name]
+        return result
+
+    result = logging.getLogger(logger_name)
+    __logger_record[logger_name] = result
+
+    assert isinstance(result,logging.Logger)
+
+    result.setLevel(logging_level)
+
+    foramter = logging.Formatter('%(asctime)s [%(levelname)s]\t:%(message)s')
+
+    t = logging.StreamHandler(sys.stdout)
+    t.setFormatter(foramter)
+    result.addHandler(t)
+
+    if both_to_file_path:
+        t = logging.FileHandler(both_to_file_path)
+        t.setFormatter(foramter)
+        result.addHandler(t)
+
+    setattr(result,'d',result.debug)
+    setattr(result,'i',result.info)
+    setattr(result,'w',result.warning)
+    setattr(result,'e',result.error)
+    setattr(result,'c',result.critical)
+
+    return result
