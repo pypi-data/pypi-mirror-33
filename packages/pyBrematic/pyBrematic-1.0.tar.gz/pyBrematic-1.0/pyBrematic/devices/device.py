95aa379a436210f96878d8f119b73460259c52e7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:19:02 +0000

    Add package pyBrematic/1.0/pyBrematic-1.0.tar.gz

---
+++ packages/pyBrematic/pyBrematic-1.0.tar.gz/pyBrematic-1.0/pyBrematic/devices/device.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+"""Device class representing a remote socket-outlet"""
+
+
+class Device(object):
+    ACTION_OFF = 0
+    ACTION_ON = 1
+
+    def __init__(self, system_code, unit_code):
+        self.system_code = system_code
+        self.unit_code = unit_code
+
+    def get_signal(self, gateway, action):
+        """Returns a signal which triggers a device to execute the intended action"""
+        raise NotImplementedError("Subclasses must implement this method!")
---
+++ packages/pyBrematic/pyBrematic-1.0.tar.gz/pyBrematic-1.0/setup.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup, find_packages
+
+packages = find_packages()
+
+setup(name='pyBrematic',
+      version='1.0',
+      keywords='python telegram bot api wrapper',
+      description='Python code for controlling Brematic remote power outlets and potentially other stuff',
+      url='https://github.com/d-Rickyy-b/pyBrematic',
+      author='d-Rickyy-b',
+      author_email='pyBrematic@gmail.com',
+      license='MIT',
+      packages=packages,
+      zip_safe=False)
