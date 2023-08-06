55cdf607e0032284c2fe29cefb678eff80ee6ae4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:11:24 +0000

    Add package ethpm/0.1.0a31/ethpm-0.1.0a31-py3-none-any.whl

---
+++ packages/ethpm/ethpm-0.1.0a31-py3-none-any.whl/ethpm/constants.py
@@ -0,0 +1,8 @@
+# TODO update once registry standard eip has been approved
+REGISTRY_URI_SCHEME = "ercxxx"
+
+PACKAGE_NAME_REGEX = "[a-zA-Z][-_a-zA-Z0-9]{0,255}"
+
+IPFS_GATEWAY_PREFIX = "https://ipfs.io/ipfs/"
+
+INFURA_GATEWAY_PREFIX = "https://ipfs.infura.io/ipfs/"
---
+++ packages/ethpm/ethpm-0.1.0a31-py3-none-any.whl/ethpm/typing/misc.py
@@ -0,0 +1,5 @@
+from typing import NewType
+
+ContractName = NewType("ContractName", str)
+
+URI = NewType("URI", str)
