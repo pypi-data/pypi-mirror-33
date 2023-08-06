9f9ea2f776703827bdf1702c8e25ada6261be485
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:13:27 +0000

    Add package academictorrents/2.0.9/academictorrents-2.0.9.tar.gz

---
+++ packages/academictorrents/academictorrents-2.0.9.tar.gz/academictorrents-2.0.9/academictorrents/academictorrents.py
@@ -0,0 +1,21 @@
+import time
+import logging
+import json
+import io
+import os
+import bencode
+import requests
+import utils
+import pkg_resources
+from .Client import Client
+from .Torrent import Torrent
+
+
+def get(hash, datastore=None, name=None):
+    torrent_dir = utils.get_torrent_dir(datastore=datastore, name=name)
+    torrent = Torrent(hash, torrent_dir)
+    timestamp = utils.read_timestamp(hash)
+    if utils.timestamp_is_recent(timestamp) and utils.filenames_present(torrent, datastore):
+        return torrent_dir + torrent.torrentFile['info']['name']
+    else:
+        return Client(hash, torrent_dir).start()
