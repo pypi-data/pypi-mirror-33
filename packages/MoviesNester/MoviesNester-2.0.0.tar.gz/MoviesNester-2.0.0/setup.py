dcd1bf8350979e690b85a20cd0df8ea71cee5650
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:36:54 +0000

    Add package MoviesNester/2.0.0/MoviesNester-2.0.0.tar.gz

---
+++ packages/MoviesNester/MoviesNester-2.0.0.tar.gz/MoviesNester-2.0.0/setup.py
@@ -0,0 +1,10 @@
+from distutils.core import setup
+setup(
+	name="MoviesNester",
+	version="2.0.0",
+	py_modules=["MoviesNester"],
+	author="Guo.yang",
+	author_email="gyang973256@gmail.com",
+	url="https://github.com/holyTornado",
+	description="A simple printer of nested lists.",
+)
