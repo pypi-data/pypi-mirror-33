ccaccc6c6cba6bf546520dd3be4b3ccbe7cacdfc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:36:27 +0000

    Add package PennSDK/1.7.1/PennSDK-1.7.1-py2-none-any.whl

---
+++ packages/PennSDK/PennSDK-1.7.1-py2-none-any.whl/penn/__init__.py
@@ -0,0 +1,13 @@
+__version__ = '1.7.1'
+
+from .registrar import Registrar
+from .directory import Directory
+from .dining import Dining
+from .dining import DiningV2
+from .transit import Transit
+from .news import News
+from .laundry import Laundry
+from .map import Map
+from .studyspaces import StudySpaces
+from .calendar3year import Calendar
+from .fitness import Fitness
