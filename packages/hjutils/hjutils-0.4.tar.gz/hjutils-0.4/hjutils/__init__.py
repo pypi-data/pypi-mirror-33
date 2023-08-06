1beee6e9a72390d0449b7edbc1f50d0b0b8969c4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:44:57 +0000

    Add package hjutils/0.4/hjutils-0.4.tar.gz

---
+++ packages/hjutils/hjutils-0.4.tar.gz/hjutils-0.4/hjutils/__init__.py
@@ -0,0 +1,16 @@
+"""
+    welcome to use
+    ~~~~~
+
+    A Lightweight regular expressions  It's extensively documented
+    and follows best practice patterns.
+
+    :copyright: (c) 2017 by Jing He.
+    :license: BSD, see LICENSE for more details.
+    Using the step:
+    1. from hjutils.hjutils from RegexUtils
+    2. RegexUtils.functions(as your content)
+
+"""
+
+__version__ = '0.4'
