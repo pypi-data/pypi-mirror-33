b64514c39b2ae9ce9ae1f5c0730bf1e00b735095
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:44:05 +0000

    Add package cal-ai/0.2.0.3/cal_ai-0.2.0.3.tar.gz

---
+++ packages/cal-ai/cal_ai-0.2.0.3.tar.gz/cal_ai-0.2.0.3/cal_ai/__init__.py
@@ -0,0 +1,7 @@
+name = "cal_ai"
+
+from .ai_monitor import AIMonitor
+from .cal_crawler import CalCrawler
+from .data_template import DataType
+from .healthy_checker import HealthyChecker
+from .cal_plot import CalPlot
