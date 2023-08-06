6d1d436eaccbaa2e948dad6d4644653d065354af
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 05:07:29 +0000

    Add package lib-justin-furuness/0.2/lib_justin_furuness-0.2.tar.gz

---
+++ packages/lib-justin-furuness/lib_justin_furuness-0.2.tar.gz/lib_justin_furuness-0.2/README.md
@@ -0,0 +1,32 @@
+# justin-furuness
+
+My first pypi package
+
+## Installation
+
+git clone https://github.com/jfuruness/justin_furuness 
+
+## Usage
+
+Test project
+
+## Contributing
+
+1. Fork it!
+2. Create your feature branch: `git checkout -b my-new-feature`
+3. Commit your changes: `git commit -am 'Add some feature'`
+4. Push to the branch: `git push origin my-new-feature`
+5. Submit a pull request :D
+
+## History
+
+* 0.1.0 - Initial setup
+
+## Credits
+
+git clone git@github.com:DEV3L/python-package-archetype.git
+
+## License
+
+MIT
+
---
+++ packages/lib-justin-furuness/lib_justin_furuness-0.2.tar.gz/lib_justin_furuness-0.2/setup.py
@@ -0,0 +1,29 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='lib_justin_furuness',
+    packages=find_packages(),
+    version='0.2',
+    description='Test that prints out Justin Furuness',
+    author='Justin Furuness',
+    author_email='jfuruness@gmail.com',
+    url='https://github.com/jfuruness/justin_furuness',
+    download_url='https://github.com/jfuruness/justin_furuness',
+    keywords=['Furuness', 'furuness', 'pypi', 'package'],  # arbitrary keywords
+    install_requires=[
+        'pytest==2.9.2'
+    ],
+    classifiers=[
+        'Environment :: Console',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 3',
+        'Topic :: Software Development :: Libraries :: Python Modules'],
+    entry_points={
+        'console_scripts': [
+            'justin_furuness = lib_justin_furuness.justin_furuness:print_justin_furuness'
+        ]},
+)
