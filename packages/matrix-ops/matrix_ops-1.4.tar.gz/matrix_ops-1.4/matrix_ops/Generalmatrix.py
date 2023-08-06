da384bf03839da959d2a576aae27b14e80aa1fb7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:18:01 +0000

    Add package matrix-ops/1.4/matrix_ops-1.4.tar.gz

---
+++ packages/matrix-ops/matrix_ops-1.4.tar.gz/matrix_ops-1.4/matrix_ops/Generalmatrix.py
@@ -0,0 +1,20 @@
+class matrix:
+    
+    def __init__(self,data = []):
+        """ General matrix class for instantiating matrices
+        and performing operations
+        
+        Attributes:
+            row_size(int) representing the number of rows in matrix
+            col_size(int) representing the number of cols in matrix
+            data_elements(list of floats) representing the elements in matrix
+            """
+        self.data_elements = data
+        if (type(self.data_elements[0]) == list):
+            self.row_size = len(self.data_elements)
+            self.col_size = len(self.data_elements[0])
+        else:
+            self.row_size = 1
+            self.col_size = len(self.data_elements)
+        
+    
