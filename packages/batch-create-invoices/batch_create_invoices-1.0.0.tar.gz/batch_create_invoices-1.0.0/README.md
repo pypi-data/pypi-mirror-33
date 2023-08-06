efc9a9a05c0de905b92d07946e58efb82888891d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:52:25 +0000

    Add package batch-create-invoices/1.0.0/batch_create_invoices-1.0.0.tar.gz

---
+++ packages/batch-create-invoices/batch_create_invoices-1.0.0.tar.gz/batch_create_invoices-1.0.0/README.md
@@ -0,0 +1,6 @@
+# Batch Create Invoice
+This util is used to batch create mock invoice for tradeshift platform for test only.
+
+# How two use
+pip install batch-create-invoices  
+python -m mock_invoices
---
+++ packages/batch-create-invoices/batch_create_invoices-1.0.0.tar.gz/batch_create_invoices-1.0.0/setup.py
@@ -0,0 +1,19 @@
+import setuptools
+
+setuptools.setup(
+    name='batch_create_invoices',
+    version='1.0.0',
+    author='Lewis Zhang',
+    author_email='244563813@qq.com',
+    description='A util for batch create mock invoice for tradeshift.',
+    long_description='A util for batch create mock invoice for tradeshift platform. This is used for test only!',
+    long_description_content_type='text/raw',
+    url='https://github.com/Lewis-ZhangHP/structures_algorithms',
+    packages=setuptools.find_packages(),
+    install_requires=['requests'],
+    classifiers=(
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent"
+    )
+)
