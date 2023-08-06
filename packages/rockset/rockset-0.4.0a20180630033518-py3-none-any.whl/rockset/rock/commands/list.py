a95179b8b061dafc50c5544f4cc6539f1ae68133
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 07:14:20 +0000

    Add package rockset/0.4.0a20180630033518/rockset-0.4.0a20180630033518-py3-none-any.whl

---
+++ packages/rockset/rockset-0.4.0a20180630033518-py3-none-any.whl/rockset/rock/commands/list.py
@@ -0,0 +1,27 @@
+from .command_rest import RESTCommand
+
+class List(RESTCommand):
+    @classmethod
+    def what(cls):
+        return('ls', 'List all collections')
+
+    def usage(self):
+        return """
+usage: rock ls [-h]
+
+List all collections.
+
+options:
+  -h, --help            show this help message and exit
+        """
+
+    def go(self):
+        if not self.format:
+            self.format = 'text'
+        path='/orgs/{}/ws/{}/collections'.format('self', 'commons')
+        items = self.get(path)['data']
+        sorted_items= sorted(items, key=lambda k: k['type'] + ':' + k['name'])
+        return (0, sorted_items)
+
+    def print_result(self, result):
+        self.print_list(0, result, ['type', 'name', 'status', 'description'])
