dc40c2a01ab46d0c1e74c3bdbee6ce56a9f74bec
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:26:09 +0000

    Add package bigread/0.0.3/bigread-0.0.3.tar.gz

---
+++ packages/bigread/bigread-0.0.3.tar.gz/bigread-0.0.3/setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup
+
+setup (
+  name='bigread',
+  version='0.0.3',
+  packages=['bigread'],
+  keywords = ['text-mining', 'large-files', 'streaming'],
+  description='Stream huge files with minimal RAM usage',
+  url='https://github.com/duhaime/bigread',
+  author='Douglas Duhaime',
+  author_email='douglas.duhaime@gmail.com',
+  license='MIT',
+  install_requires=[
+    'six==1.11.0',
+  ],
+)
