f17a338343be4b46d76854ec8a4a3d973578618c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:17:10 +0000

    Add package foowise/0.3/foowise-0.3.tar.gz

---
+++ packages/foowise/foowise-0.3.tar.gz/foowise-0.3/foowise/__init__.py
@@ -0,0 +1,11 @@
+from . import channels.Cla
+from . import channels.Infomorphism
+from . import channels.Invariant
+from . import channels.Channel
+from . import channels.Index
+from . import channels.DistSys
+
+from . import math.Dual
+from . import math.LinAlg
+from . import math.Relation
+from . import math.Set
