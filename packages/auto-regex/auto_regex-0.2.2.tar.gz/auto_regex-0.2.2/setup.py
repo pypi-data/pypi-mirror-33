6c8c742934097e23e079e71e3bd7cefdef2a2047
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 05:07:23 +0000

    Add package auto-regex/0.2.2/auto_regex-0.2.2.tar.gz

---
+++ packages/auto-regex/auto_regex-0.2.2.tar.gz/auto_regex-0.2.2/setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='auto_regex',
+    version='0.2.2',
+    packages=['auto_regex'],
+    url='https://github.com/JarbasAl/auto_regex',
+    license='MIT',
+    author='jarbasAI',
+    author_email='jarbasai@mailfence.com',
+    description='regex made easy'
+)
