ca80c30fa245cb4fc34e565715b2973b3c4f5bd2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:49:12 +0000

    Add package mrcnn/0.1/mrcnn-0.1.tar.gz

---
+++ packages/mrcnn/mrcnn-0.1.tar.gz/mrcnn-0.1/setup.py
@@ -0,0 +1,20 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="mrcnn",
+    version="0.1",
+    author="Exmaple",
+    author_email="author@example.com",
+    description="A small example package",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/pypa/example-project",
+    packages=setuptools.find_packages(),
+    classifiers=(
+        "Programming Language :: Python :: 3",
+        "Operating System :: OS Independent",
+    ),
+)
