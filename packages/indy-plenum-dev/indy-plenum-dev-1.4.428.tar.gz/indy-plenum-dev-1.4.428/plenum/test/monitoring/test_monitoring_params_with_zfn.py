121f1e72ab93481d86f84bdd159fa213b805e9fe
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:55:39 +0000

    Add package indy-plenum-dev/1.4.428/indy-plenum-dev-1.4.428.tar.gz

---
+++ packages/indy-plenum-dev/indy-plenum-dev-1.4.428.tar.gz/indy-plenum-dev-1.4.428/plenum/test/monitoring/test_monitoring_params_with_zfn.py
@@ -0,0 +1,32 @@
+import pytest
+
+from plenum.test.helper import get_key_from_req
+
+nodeCount = 7
+
+
+def testThroughputThreshold(looper, txnPoolNodeSet, requests):
+    for node in txnPoolNodeSet:
+        masterThroughput, avgBackupThroughput = node.monitor.getThroughputs(
+            node.instances.masterId)
+        for r in node.replicas:
+            print("{} stats: {}".format(r, repr(r.stats)))
+        assert masterThroughput / avgBackupThroughput >= node.monitor.Delta
+
+
+def testReqLatencyThreshold(looper, txnPoolNodeSet, requests):
+    for node in txnPoolNodeSet:
+        for rq in requests:
+            key = get_key_from_req(rq)
+            assert key in node.monitor.masterReqLatenciesTest
+            assert node.monitor.masterReqLatenciesTest[key] <= node.monitor.Lambda
+
+
+def testClientLatencyThreshold(looper, txnPoolNodeSet, requests):
+    rq = requests[0]
+    for node in txnPoolNodeSet:
+        latc = node.monitor.getAvgLatency(
+            node.instances.masterId)[rq['identifier']]
+        avglat = node.monitor.getAvgLatency(
+            *node.instances.backupIds)[rq['identifier']]
+        assert latc - avglat <= node.monitor.Omega
