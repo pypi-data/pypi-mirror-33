a34d527608250d202317653f8def378827242e6f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:48:03 +0000

    Add package thoth-common/0.0.8/thoth-common-0.0.8.tar.gz

---
+++ packages/thoth-common/thoth-common-0.0.8.tar.gz/thoth-common-0.0.8/thoth/common/__init__.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python3
+# thoth-common
+# Copyright(C) 2018 Fridolin Pokorny
+#
+# This program is free software: you can redistribute it and / or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program. If not, see <http://www.gnu.org/licenses/>.
+
+"""Shared code across Thoth analyzers."""
+
+from .helpers import cwd
+from .helpers import datetime2datetime_str
+from .helpers import datetime_str2timestamp
+from .helpers import parse_datetime
+from .helpers import datetime_str_from_timestamp
+from .json import SafeJSONEncoder
+from .logging import init_logging
+from .logging import logger_setup
+
+__name__ = 'thoth-common'
+__version__ = '0.0.8'
