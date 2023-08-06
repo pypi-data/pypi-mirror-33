cd70ae8b58be537525a53655f5394ad65f878425
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 07:14:03 +0000

    Add package sqliteschema/0.10.2/sqliteschema-0.10.2-py2.py3-none-any.whl

---
+++ packages/sqliteschema/sqliteschema-0.10.2-py2.py3-none-any.whl/sqliteschema/_const.py
@@ -0,0 +1,17 @@
+# encoding: utf-8
+
+'''
+.. codeauthor:: Tsuyoshi Hombashi <tsuyoshi.hombashi@gmail.com>
+'''
+
+
+MAX_VERBOSITY_LEVEL = 100
+
+
+class Header(object):
+    ATTR_NAME = "Attribute name"
+    DATA_TYPE = "Data type"
+    PRIMARY_KEY = "PRIMARY KEY"
+    NOT_NULL = "NOT NULL"
+    UNIQUE = "UNIQUE"
+    INDEX = "Index"
