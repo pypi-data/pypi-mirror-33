48f941baa9f04e280352f0c0da02ca8f6c27e9e9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:18:15 +0000

    Add package sanpy/0.0.6/sanpy-0.0.6.tar.gz

---
+++ packages/sanpy/sanpy-0.0.6.tar.gz/sanpy-0.0.6/san/get.py
@@ -0,0 +1,28 @@
+import san.sanbase_graphql
+from san.graphql import execute_gql
+from san.error import SanError
+from san.pandas_utils import convert_to_datetime_idx_df
+
+
+def get(dataset, **kwargs):
+    gql_query = get_gql_query(0, dataset, **kwargs)
+    gql_query = "{\n" + gql_query + "\n}"
+    res = execute_gql(gql_query)
+    return convert_to_datetime_idx_df(res["query_0"])
+
+
+def get_gql_query(idx, dataset, **kwargs):
+    query, slug = _parse_dataset(dataset)
+    return getattr(
+        san.sanbase_graphql,
+        query,
+        lambda *args, **kwargs: not_found(query)
+        )(idx, slug, **kwargs)
+
+
+def _parse_dataset(dataset):
+    return dataset.split("/")
+
+
+def not_found(query):
+    raise SanError(query + ' not found')
