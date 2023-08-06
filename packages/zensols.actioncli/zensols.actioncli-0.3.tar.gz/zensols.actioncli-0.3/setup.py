327ffd284fe2b9fe5eced3989d0641cbc230e28f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:23:00 +0000

    Add package zensols.actioncli/0.3/zensols.actioncli-0.3.tar.gz

---
+++ packages/zensols.actioncli/zensols.actioncli-0.3.tar.gz/zensols.actioncli-0.3/setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup, find_packages
+
+setup(
+    name = "zensols.actioncli",
+    packages = ['zensols.actioncli'],
+    version = '0.3',
+    description = 'This library intends to make command line execution and configuration easy.',
+    author = 'Paul Landes',
+    author_email = 'landes@mailc.net',
+    url = 'https://github.com/plandes/actioncli',
+    download_url = 'https://github.com/plandes/actioncli/releases/download/v0.0.3/zensols.actioncli-0.3-py3-none-any.whl',
+    keywords = ['tooling'],
+    classifiers = [],
+)
---
+++ packages/zensols.actioncli/zensols.actioncli-0.3.tar.gz/zensols.actioncli-0.3/zensols/actioncli/__init__.py
@@ -0,0 +1,3 @@
+from zensols.actioncli.config import *
+from zensols.actioncli.simple import *
+from zensols.actioncli.peraction import *
