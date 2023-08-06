55704d03641f4e0f975545f9f5aa32a204fcff46
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:23:34 +0000

    Add package modelforge/0.6.0/modelforge-0.6.0.tar.gz

---
+++ packages/modelforge/modelforge-0.6.0.tar.gz/modelforge-0.6.0/modelforge/__init__.py
@@ -0,0 +1,3 @@
+from modelforge.model import Model, merge_strings, split_strings, \
+    assemble_sparse_matrix, disassemble_sparse_matrix
+from modelforge.models import register_model, GenericModel
