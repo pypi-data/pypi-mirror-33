9cf41f304fdcc7dd622a608ad92cf308d82ec2d2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:52:19 +0000

    Add package eote-dice/1.1.0/eote-dice-1.1.0.tar.gz

---
+++ packages/eote-dice/eote-dice-1.1.0.tar.gz/eote-dice-1.1.0/setup.py
@@ -0,0 +1,44 @@
+import setuptools
+
+
+setuptools.setup(
+    name='eote-dice',
+    version='1.1.0',
+
+    description='Utility for analyzing EotE dice rolls.',
+    long_description=open('README.rst').read(),
+    keywords='star wars EotE dice role-playing',
+
+    author='John Hagen',
+    author_email='johnthagen@gmail.com',
+    url='https://github.com/johnthagen/eote-dice',
+    license='MIT',
+
+    install_requires=open('requirements.txt').readlines(),
+    zip_safe=False,
+
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Environment :: Console',
+        'Intended Audience :: End Users/Desktop',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3 :: Only',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Topic :: Games/Entertainment :: Role-Playing',
+    ],
+
+    py_modules=['eote_dice', 'dice', 'distribution'],
+
+    scripts=['eote_dice.py'],
+
+    entry_points={
+        'console_scripts': [
+            'eote_dice = eote_dice:main',
+        ],
+    }
+)
