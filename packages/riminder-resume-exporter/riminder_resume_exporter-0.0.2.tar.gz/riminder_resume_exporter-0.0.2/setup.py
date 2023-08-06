021ae574a7ed56fded21e571c0e9e4cd1015dc13
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:49:58 +0000

    Add package riminder-resume-exporter/0.0.2/riminder_resume_exporter-0.0.2.tar.gz

---
+++ packages/riminder-resume-exporter/riminder_resume_exporter-0.0.2.tar.gz/riminder_resume_exporter-0.0.2/setup.py
@@ -0,0 +1,20 @@
+from setuptools import setup
+
+setup(name='riminder_resume_exporter',
+      version='0.0.2',
+      description='Riminder resume exporter.',
+      url='https://github.com/Riminder/riminder-resumes-exporter',
+      author='riminder',
+      author_email='contact@rimider.net',
+      license='MIT',
+      install_requires=[
+          'riminder==1.0.4'
+      ],
+      packages=['resume_exporter'],
+      entry_points={
+        'console_scripts': [
+            'resumeExporter = resume_exporter.resume_exporter:main',
+        ]
+      },
+      python_requires='>=3.5',
+      zip_safe=False)
