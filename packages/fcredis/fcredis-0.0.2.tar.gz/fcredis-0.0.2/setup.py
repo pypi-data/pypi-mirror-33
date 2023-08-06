d633f54a9e3df287409491c0b39d5bd4ad6779ca
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:34:48 +0000

    Add package fcredis/0.0.2/fcredis-0.0.2.tar.gz

---
+++ packages/fcredis/fcredis-0.0.2.tar.gz/fcredis-0.0.2/setup.py
@@ -0,0 +1,36 @@
+from setuptools import find_packages
+from setuptools import setup
+
+
+def get_lines(filename):
+    with open(filename, "r") as f:
+        return [line.strip() for line in f.readlines()]
+
+
+install_requires = get_lines("requirements.txt")
+dev_requires = get_lines("requirements-dev.txt")
+
+setup(
+    name="fcredis",
+    version='0.0.2',
+    description='Redis API for users and allocation',
+    long_description="",
+    classifiers=[
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: GNU General Public License v3 (GPLv3)",
+        "Programming Language :: Python :: 3.5",
+        "Programming Language :: Python :: 3.6",
+        "Topic :: Software Development :: Libraries",
+        "Topic :: Software Development :: Libraries :: Python Modules"
+    ],
+    url='https://github.com/forever-am/fcredis',
+    author='Alice Wang',
+    author_email="alice.wang@forever-am.com",
+    keywords='database, redis',
+    license="GPLv3",
+    packages=find_packages(),
+    include_package_data=False,
+    zip_safe=False,
+    install_requires=install_requires,
+    extras_require={"test": dev_requires}
+)
