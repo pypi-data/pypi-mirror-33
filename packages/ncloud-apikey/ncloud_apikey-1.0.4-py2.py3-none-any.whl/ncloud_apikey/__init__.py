12ef47f393e22e6b832a37c5833c42a8bf2df0be
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 05:07:33 +0000

    Add package ncloud-apikey/1.0.4/ncloud_apikey-1.0.4-py2.py3-none-any.whl

---
+++ packages/ncloud-apikey/ncloud_apikey-1.0.4-py2.py3-none-any.whl/ncloud_apikey/__init__.py
@@ -0,0 +1,5 @@
+from __future__ import absolute_import
+
+# import apis into api package
+from ncloud_apikey.ncloud_key import NcloudKey
+from ncloud_apikey.properties_parser import PropertiesParser
