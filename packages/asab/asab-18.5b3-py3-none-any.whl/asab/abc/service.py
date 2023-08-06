089cc3a59a95584d165b0f4e4d9eda2e8d874591
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:49:24 +0000

    Add package asab/18.5b3/asab-18.5b3-py3-none-any.whl

---
+++ packages/asab/asab-18.5b3-py3-none-any.whl/asab/abc/service.py
@@ -0,0 +1,19 @@
+import abc
+
+class Service(abc.ABC):
+	"""
+	Abstract service class
+	"""
+
+	def __init__(self, app, service_name):
+		self.Name = service_name
+		app._register_service(self)
+
+
+	# Lifecycle
+
+	async def initialize(self, app):
+		pass
+
+	async def finalize(self, app):
+		pass
