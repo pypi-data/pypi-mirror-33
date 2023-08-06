bdee6363b398cff1a822980b44fa41829678e99f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:34:39 +0000

    Add package naughties/0.1.3/naughties-0.1.3.tar.gz

---
+++ packages/naughties/naughties-0.1.3.tar.gz/naughties-0.1.3/setup.py
@@ -0,0 +1,13 @@
+from distutils.core import setup
+
+setup(
+    name="naughties",
+    version="0.1.3",
+    author="Chad Estioco",
+    author_email="chadestioco@gmail.com",
+    packages=["naughties"],
+    data_files=[(".", ["blns.json"])],
+    install_requires=["Faker==0.8.16"],
+    license="MIT",
+    description="Fakers that return naughty strings."
+)
