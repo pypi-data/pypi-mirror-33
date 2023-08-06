984bd4e91d88555a495755aedd3b1756adb42c49
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:56:09 +0000

    Add package apitax/2.1.3/Apitax-2.1.3.tar.gz

---
+++ packages/apitax/Apitax-2.1.3.tar.gz/Apitax-2.1.3/apitax/config/Config.py
@@ -0,0 +1,47 @@
+import configparser
+from apitax.utilities.Numbers import isNumber
+from apitax.utilities.Booleans import isBoolean, str2bool
+from apitax.utilities.Files import getRootPath
+
+# Reads the config and allows us to retrieve values from it
+class Config:
+	
+    configs = {}
+    
+    def __init__(self, path, sectionName):
+        self.cp = configparser.RawConfigParser()
+        self.cFilePath = path
+        self.sectionName = sectionName
+        self.cp.read(self.cFilePath)
+        self.path = ''
+        
+	
+    def read(path=getRootPath('/config.txt'), sectionName='Apitax'):
+        print('> Config path: ' + path)
+        if(path in Config.configs):
+            return Config.configs[path]
+        config = Config(path, sectionName)
+        Config.configs[path] = config
+        return config
+
+    def get(self, param):
+        prop: str = self.cp.get(self.sectionName, param)
+        if(isBoolean(prop)):
+            return str2bool(prop)
+        elif(isNumber(prop)):
+            return float(prop)
+        return prop
+        
+    def getAsList(self, param):
+        items = list(str(self.cp.get(self.sectionName, param)).split(","))
+        return [x.strip(' ') for x in items]
+
+    def has(self, param):
+        return self.cp.has_option(self.sectionName, param)
+        
+    def serialize(self, serializable:list):
+        returner = dict()
+        for property in serializable:
+            if(self.has(property)):
+                returner.update({property: self.get(property)})
+        return returner
