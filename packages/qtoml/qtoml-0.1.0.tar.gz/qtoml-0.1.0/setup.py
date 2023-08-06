916ef3f92d88f0db386798104a940492fe5aec53
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:35:08 +0000

    Add package qtoml/0.1.0/qtoml-0.1.0.tar.gz

---
+++ packages/qtoml/qtoml-0.1.0.tar.gz/qtoml-0.1.0/setup.py
@@ -0,0 +1,27 @@
+#!python
+
+from setuptools import setup
+
+with open('README.rst') as rm:
+    long_description = rm.read()
+
+setup(
+    name='qtoml',
+    version='0.1.0',
+    author="alethiophile",
+    author_email="tomdicksonhunt@gmail.com",
+    description="New TOML encoder/decoder",
+    long_description=long_description,
+    long_description_content_type='text/x-rst',
+    license='MIT',
+    packages=['qtoml'],
+    url="https://github.com/alethiophile/qtoml",
+    install_requires=['click'],
+    python_requires='~=3.6',
+    classifiers=(
+        "Programming Language :: Python :: 3",
+        "Operating System :: OS Independent",
+        "License :: OSI Approved :: MIT License",
+        "Topic :: Software Development :: Libraries :: Python Modules"
+    )
+)
