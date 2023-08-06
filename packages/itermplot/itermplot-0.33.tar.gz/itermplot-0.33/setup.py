0ccdd61e23571ff38652149e2f9d396bb836c136
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:22:32 +0000

    Add package itermplot/0.33/itermplot-0.33.tar.gz

---
+++ packages/itermplot/itermplot-0.33.tar.gz/itermplot-0.33/setup.py
@@ -0,0 +1,28 @@
+from setuptools import setup, find_packages
+from os import path
+from io import open
+
+here = path.abspath(path.dirname(__file__))
+
+try:
+    with open(path.join(here, 'README'), encoding='utf-8') as f:
+        long_description = f.read()
+except(IOError):
+    with open(path.join(here, 'README.md'), encoding='utf-8') as f:
+        long_description = f.read()
+
+setup(name='itermplot',
+      packages=find_packages(),
+      install_requires=[
+        'matplotlib',
+        'six',
+        'numpy'
+      ],
+      version='0.33',
+      description='An awesome iTerm2 backend for Matplotlib, so you can plot directly in your terminal.',
+      long_description=long_description,
+      url='http://github.com/daleroberts/itermplot',
+      author='Dale Roberts',
+      author_email='dale.o.roberts@gmail.com',
+      license='MIT',
+      zip_safe=False)
