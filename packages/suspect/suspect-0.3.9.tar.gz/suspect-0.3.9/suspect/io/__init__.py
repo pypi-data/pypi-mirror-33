e12838681f456319dea6ba179131d78c7b094976
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:53:21 +0000

    Add package suspect/0.3.9/suspect-0.3.9.tar.gz

---
+++ packages/suspect/suspect-0.3.9.tar.gz/suspect-0.3.9/suspect/io/__init__.py
@@ -0,0 +1,7 @@
+from suspect.io.rda import load_rda
+from suspect.io.twix import load_twix
+from suspect.io.philips import load_sdat
+from suspect.io.siemens import load_siemens_dicom
+from . import tarquin, lcmodel, felix
+from .dicom import load_dicom
+from .bruker import load_svs_bruker
