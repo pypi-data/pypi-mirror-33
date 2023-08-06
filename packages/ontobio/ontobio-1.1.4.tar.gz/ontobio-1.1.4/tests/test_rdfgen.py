1fc0b5870f6fceeabb146d0e932829d383500f7c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 05:07:29 +0000

    Add package ontobio/1.1.4/ontobio-1.1.4.tar.gz

---
+++ packages/ontobio/ontobio-1.1.4.tar.gz/ontobio-1.1.4/tests/test_rdfgen.py
@@ -0,0 +1,26 @@
+"""
+Test Generate RDF from Assocs
+"""
+from ontobio.io.gafparser import GafParser
+from ontobio.rdfgen.assoc_rdfgen import SimpleAssocRdfTransform, CamRdfTransform
+from ontobio.assoc_factory import AssociationSetFactory
+from ontobio.ontol_factory import OntologyFactory
+
+POMBASE = "tests/resources/truncated-pombase.gaf"
+ONT = "tests/resources/go-truncated-pombase.json"
+
+def test_parse():
+    ont = OntologyFactory().create(ONT)
+    p = GafParser()
+    assocs = p.parse(open(POMBASE,"r"))
+    #gen(assocs,SimpleAssocRdfTransform(),'simple')
+    gen(assocs,CamRdfTransform(),'cam')
+
+def gen(assocs, tr, n):
+    fn = 'tests/resources/{}.rdf'.format(n)
+    tr.emit_header()
+    for a in assocs:
+        tr.translate(a)
+    tr.writer.serialize(destination=open(fn,'w'))
+    #tr.writer.serialize(fn, 'ntriples')
+
