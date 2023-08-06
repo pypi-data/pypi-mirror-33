14806b8f8c7f01dcb8d15751f96b7f098afeeaa9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:33:59 +0000

    Add package nbsite/0.4.4a13/nbsite-0.4.4a13.tar.gz

---
+++ packages/nbsite/nbsite-0.4.4a13.tar.gz/nbsite-0.4.4a13/nbsite/__main__.py
@@ -0,0 +1,49 @@
+import argparse
+import inspect
+
+from .cmd import init, generate_rst, build
+
+def main(args=None):
+
+    parser = argparse.ArgumentParser(description="nbsite commands")
+    subparsers = parser.add_subparsers(title='available commands')
+
+    init_parser = subparsers.add_parser("init", help=inspect.getdoc(init))
+    init_parser.add_argument('--path',type=str,help='where to init doc',default='doc')
+    init_parser.set_defaults(func=lambda args: init(path=args.path))
+
+    generaterst_parser = subparsers.add_parser("generate-rst", help=inspect.getdoc(generate_rst))
+    generaterst_parser.add_argument('--project',type=str,help='where to init doc',default='pyviz')
+    generaterst_parser.add_argument('--org',type=str,help='where to init doc',default='pyviz')
+    generaterst_parser.add_argument('--repo',type=str,help='where to init doc',default='pyviz')
+    generaterst_parser.add_argument('--examples-path',type=str,help='where to init doc',default='examples')
+    generaterst_parser.add_argument('--doc-path',type=str,help='where to init doc',default='doc')
+    generaterst_parser.add_argument('--offset',type=int,help='where to init doc',default=1)
+    generaterst_parser.add_argument('--overwrite',type=int,help='where to init doc',default=False)
+    
+    generaterst_parser.set_defaults(func=lambda args: generate_rst(args.project,args.examples_path,args.doc_path,args.org,args.repo,args.offset,args.overwrite))
+
+    build_parser = subparsers.add_parser("build", help=inspect.getdoc(build))
+    build_parser.add_argument('--what',type=str,help='where to init doc',default='html')
+    build_parser.add_argument('--examples-path',type=str,help='where to init doc',default='examples')
+    build_parser.add_argument('--doc-path',type=str,help='where to init doc',default='doc')
+    build_parser.add_argument('--output',type=str,help='where to init doc',default="builtdocs")
+    build_parser.add_argument('--assets',type=str,help='where to init doc',default="assets")    
+    build_parser.set_defaults(func=lambda args: build(args.what,args.examples_path,args.doc_path,args.output,args.assets))
+    
+    
+    # add commands from pyct, for examples
+    try:
+        import pyct.cmd
+    except ImportError:
+        import sys
+        from . import _missing_cmd
+        print(_missing_cmd())
+        sys.exit(1)
+    pyct.cmd.add_commands(subparsers,'nbsite',cmds=None,args=args)
+
+    args = parser.parse_args()
+    return args.func(args) if hasattr(args,'func') else parser.error("must supply command to run") 
+
+if __name__ == "__main__":
+    main()
