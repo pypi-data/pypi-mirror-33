86acdb981d6c9f82b5233cbff7797cba33ae9756
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:53:17 +0000

    Add package SimPEG/0.8.6/SimPEG-0.8.6-py2.py3-none-any.whl

---
+++ packages/SimPEG/SimPEG-0.8.6-py2.py3-none-any.whl/SimPEG/__init__.py
@@ -0,0 +1,27 @@
+from __future__ import print_function
+
+from SimPEG import Utils
+from SimPEG.Utils import mkvc
+from SimPEG.Utils.SolverUtils import (
+    _checkAccuracy, SolverWrapD, SolverWrapI,
+    Solver, SolverCG, SolverDiag, SolverLU, SolverBiCG,
+)
+
+import discretize as Mesh
+from SimPEG import Maps
+from SimPEG import Models
+from SimPEG import Problem
+from SimPEG import Survey
+from SimPEG import Regularization
+from SimPEG import DataMisfit
+from SimPEG import InvProblem
+from SimPEG import Optimization
+from SimPEG import Directives
+from SimPEG import Inversion
+from SimPEG import Tests
+from SimPEG.Utils import versions
+
+__version__   = '0.8.6'
+__author__    = 'SimPEG Team'
+__license__   = 'MIT'
+__copyright__ = '2013 - 2017, SimPEG Team, http://simpeg.xyz'
