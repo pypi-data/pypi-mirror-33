8524ff8b3b0e937629f4782f7246c7f110ba36eb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:57:03 +0000

    Add package domo-python/0.0.11/domo_python-0.0.11.tar.gz

---
+++ packages/domo-python/domo_python-0.0.11.tar.gz/domo_python-0.0.11/setup.py
@@ -0,0 +1,16 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="domo_python",
+    version="0.0.11",
+    author="Big Squid, Inc.",
+    author_email="bcooper@bigsquid.com",
+    description="Domo API functions to import and export data into usable Pandas dataframes and manage your Domo instancea",
+    url="https://github.com/brockcooper/domo_python",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    packages=setuptools.find_packages(),
+)
