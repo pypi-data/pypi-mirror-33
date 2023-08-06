284c158173eb0e75e219dd9344507004234b6b2e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:24:19 +0000

    Add package a2p2/0.0.2/a2p2-0.0.2.tar.gz

---
+++ packages/a2p2/a2p2-0.0.2.tar.gz/a2p2-0.0.2/a2p2/__init__.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python
+
+__all__ = ['utils', 'apis', 'gui', 'samp', 'client']
+
+from . import utils
+from . import apis
+from . import gui
+from . import samp
+from . import client
+from .client import A2p2Client
