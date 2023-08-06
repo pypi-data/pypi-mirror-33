010c20066fcf2af7fae2836aa63ec7565a25eae3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:13:40 +0000

    Add package gmqtt/0.0.17/gmqtt-0.0.17.tar.gz

---
+++ packages/gmqtt/gmqtt-0.0.17.tar.gz/gmqtt-0.0.17/gmqtt/__init__.py
@@ -0,0 +1,20 @@
+import datetime
+
+from .client import Client
+from .mqtt import constants
+from .mqtt.protocol import BaseMQTTProtocol
+
+__author__ = "Mikhail Turchunovich"
+__email__ = 'mitu@gurtam.com'
+__copyright__ = ("Copyright 2013-%d, Gurtam; " % datetime.datetime.now().year,)
+
+__credits__ = [
+    "Mikhail Turchunovich",
+    "Elena Nikolaichik"
+]
+__version__ = "0.0.17"
+
+
+__all__ = [
+    'Client', 'BaseMQTTProtocol', 'constants'
+]
