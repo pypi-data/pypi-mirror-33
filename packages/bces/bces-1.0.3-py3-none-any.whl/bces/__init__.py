300c3693092279f38018f14b0f6bd37de468cdd0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:18:37 +0000

    Add package bces/1.0.3/bces-1.0.3-py3-none-any.whl

---
+++ packages/bces/bces-1.0.3-py3-none-any.whl/bces/__init__.py
@@ -0,0 +1,15 @@
+"""
+
+Python module for performing robust linear regression on (X,Y) data points where 
+both X and Y have measurement errors.
+
+The fitting method is the bivariate correlated errors and intrinsic scatter 
+(BCES) and follows the description given in Akritas, M. G., & Bershady, M. A. 
+Astrophysical Journal, 1996, 470, 706. 
+
+Author: Rodrigo Nemmen, https://rodrigonemmen.com
+"""
+
+
+
+
