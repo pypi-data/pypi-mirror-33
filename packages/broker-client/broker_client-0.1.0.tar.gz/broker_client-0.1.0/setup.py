ac6d4063b38fa44daffa1ba5bc7ed1c1b27aaa12
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:15:48 +0000

    Add package broker-client/0.1.0/broker_client-0.1.0.tar.gz

---
+++ packages/broker-client/broker_client-0.1.0.tar.gz/broker_client-0.1.0/setup.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+
+# Learn more: https://github.com/kennethreitz/setup.py
+
+from setuptools import setup, find_packages
+
+setup(
+    name='broker_client',
+    version='0.1.0',
+    description='A client for the IoT Broker written in Python',
+    author='Geovane Fedrecheski',
+    packages=find_packages(exclude=('tests', 'docs'))
+)
+
