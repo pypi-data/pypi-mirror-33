c249a0e5e564e1a9f650e46d6032ae3ccd1b63ec
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:17:09 +0000

    Add package stardate/1.2.3/stardate-1.2.3.tar.gz

---
+++ packages/stardate/stardate-1.2.3.tar.gz/stardate-1.2.3/setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup
+
+setup(name='stardate',
+    version='1.2.3',
+    description='Represent points in time as fractional years in UTC',
+    license='MIT',
+    packages=['stardate'],
+    author='Chris Oei',
+    author_email='chris.oei@gmail.com',
+    install_requires=[
+        'python-dateutil',
+    ],
+    scripts=['bin/stardate'],
+    zip_safe=False)
+
