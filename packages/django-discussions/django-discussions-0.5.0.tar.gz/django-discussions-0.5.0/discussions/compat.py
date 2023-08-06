26e9fa126882a328cda9429478ab500da497ca13
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:34:52 +0000

    Add package django-discussions/0.5.0/django-discussions-0.5.0.tar.gz

---
+++ packages/django-discussions/django-discussions-0.5.0.tar.gz/django-discussions-0.5.0/discussions/compat.py
@@ -0,0 +1,20 @@
+import django
+
+from django.contrib.auth import get_user_model
+
+
+# Django 1.6+ compatibility
+if django.VERSION >= (1, 4):
+    from django.utils.text import Truncator
+
+    def truncate_words(s, num, end_text="..."):
+        truncate = end_text and " %s" % end_text or ""
+        return Truncator(s).words(num, truncate=truncate)
+
+
+else:
+    from django.utils.text import truncate_words
+
+User = get_user_model()
+
+__all__ = ["User", "truncate_words"]
