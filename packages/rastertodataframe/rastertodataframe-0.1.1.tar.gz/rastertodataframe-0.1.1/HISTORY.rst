868c691568f580470c5adee7d7f4be5a73b96075
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:13:04 +0000

    Add package rastertodataframe/0.1.1/rastertodataframe-0.1.1.tar.gz

---
+++ packages/rastertodataframe/rastertodataframe-0.1.1.tar.gz/rastertodataframe-0.1.1/HISTORY.rst
@@ -0,0 +1,13 @@
+=======
+History
+=======
+
+0.1.1 (2018-07-08)
+------------------
+
+* All logic implement with unit tests. Prepare for PyPI release.
+
+0.1.0 (2018-07-07)
+------------------
+
+* Project started.
---
+++ packages/rastertodataframe/rastertodataframe-0.1.1.tar.gz/rastertodataframe-0.1.1/rastertodataframe/__init__.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+
+"""Top-level package for Raster To DataFrame."""
+
+__author__ = """Martin Black"""
+__email__ = 'mblack@posteo.de'
+__version__ = '0.1.1'
+
+from .rastertodataframe import *
+from . import util
