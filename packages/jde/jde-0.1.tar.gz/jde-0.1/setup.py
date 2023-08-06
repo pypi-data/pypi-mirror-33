840129a4abf25b401fc2befe565dc40a083f1c73
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:35:56 +0000

    Add package jde/0.1/jde-0.1.tar.gz

---
+++ packages/jde/jde-0.1.tar.gz/jde-0.1/setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(
+    name='jde',
+    version='0.1',
+    description='Jupyter Integrated Development Environment',
+    url='http://github.com/ebanner/jde',
+    author='Edward Banner',
+    author_email='edward.banner@gmail.com',
+    license='MIT',
+    packages=['jde'],
+    zip_safe=False
+)
