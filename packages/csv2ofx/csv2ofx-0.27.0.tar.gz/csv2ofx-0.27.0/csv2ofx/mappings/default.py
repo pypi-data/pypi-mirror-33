0a7e5ae193dfb72457ee184e5d0ca3817c1601c3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:22:35 +0000

    Add package csv2ofx/0.27.0/csv2ofx-0.27.0.tar.gz

---
+++ packages/csv2ofx/csv2ofx-0.27.0.tar.gz/csv2ofx-0.27.0/csv2ofx/mappings/default.py
@@ -0,0 +1,20 @@
+from __future__ import (
+    absolute_import, division, print_function, unicode_literals)
+
+from operator import itemgetter
+
+mapping = {
+    'has_header': True,
+    'is_split': False,
+    'bank': 'Bank',
+    'currency': 'USD',
+    'delimiter': ',',
+    'account': itemgetter('Account'),
+    'date': itemgetter('Date'),
+    'amount': itemgetter('Amount'),
+    'desc': itemgetter('Reference'),
+    'payee': itemgetter('Description'),
+    'notes': itemgetter('Notes'),
+    'check_num': itemgetter('Num'),
+    'id': itemgetter('Row'),
+}
---
+++ packages/csv2ofx/csv2ofx-0.27.0.tar.gz/csv2ofx-0.27.0/csv2ofx/mappings/exim.py
@@ -0,0 +1,16 @@
+from __future__ import (
+    absolute_import, division, print_function, unicode_literals)
+
+from operator import itemgetter
+
+mapping = {
+    'bank': 'Exim',
+    'has_header': True,
+    'currency': 'USD',
+    'account': itemgetter('Account'),
+    'date': itemgetter('Date'),
+    'amount': itemgetter('Amount'),
+    'payee': itemgetter('Narration'),
+    'notes': itemgetter('Notes'),
+    'id': itemgetter('Reference Number'),
+}
