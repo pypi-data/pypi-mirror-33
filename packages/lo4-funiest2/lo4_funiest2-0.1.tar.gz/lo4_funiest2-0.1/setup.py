d7aecd5747f38fe9ce20959ff836a3b46b3bc49c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:19:11 +0000

    Add package lo4-funiest2/0.1/lo4_funiest2-0.1.tar.gz

---
+++ packages/lo4-funiest2/lo4_funiest2-0.1.tar.gz/lo4_funiest2-0.1/setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(
+    name='lo4_funiest2',
+    version='0.1',
+    descript='the funiest joke in the world',
+    author='Phuong Huynh',
+    author_email='lo4kings@gmail.com',
+    license='MIT',
+    packages=['lo4_funiest'],
+    zip_safe=False,
+    python_requires='>=3.5',
+)
