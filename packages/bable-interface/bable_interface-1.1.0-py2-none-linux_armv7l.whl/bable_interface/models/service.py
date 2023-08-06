91dd3462f6e1c0e4ff9f71cfa3b033388fb51630
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:18:13 +0000

    Add package bable-interface/1.1.0/bable_interface-1.1.0-py2-none-linux_armv7l.whl

---
+++ packages/bable-interface/bable_interface-1.1.0-py2-none-linux_armv7l.whl/bable_interface/models/service.py
@@ -0,0 +1,13 @@
+class Service(object):
+
+    def __init__(self, raw_service):
+        self.uuid = raw_service.Uuid().decode()
+        self.handle = raw_service.Handle()
+        self.group_end_handle = raw_service.GroupEndHandle()
+
+    def __str__(self):
+        return self.__repr__()
+
+    def __repr__(self):
+        return "<Service uuid={}, start_handle={}, end_handle={}>"\
+            .format(self.uuid, self.handle, self.group_end_handle)
