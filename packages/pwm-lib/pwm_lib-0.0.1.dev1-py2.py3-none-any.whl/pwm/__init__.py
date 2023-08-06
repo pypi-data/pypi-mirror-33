0c391df8d4eb7f9d383b6cc796c69aca1ac75205
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:09:31 +0000

    Add package pwm-lib/0.0.1.dev1/pwm_lib-0.0.1.dev1-py2.py3-none-any.whl

---
+++ packages/pwm-lib/pwm_lib-0.0.1.dev1-py2.py3-none-any.whl/pwm/__init__.py
@@ -0,0 +1,33 @@
+# -*- coding: utf-8 -*-
+"""PAiP Web Modules
+
+NAME - PAiP Web Modules
+AUTHOR - Patryk Adamczyk <patrykadamczyk@paipweb.com>
+LICENSE - MIT
+
+"""
+# Underscore Variables
+
+"""Title of Program"""
+__title__ = 'pwm_lib'
+"""Version of Program"""
+__version__ = '0.0.1-dev1'
+"""Author of the module"""
+__author__ = 'Patryk Adamczyk'
+"""Module License"""
+__license__ = 'MIT'
+"""Documentation format"""
+__docformat__ = 'restructuredtext en'
+
+# Imports
+
+
+# Running as pwbs command
+
+
+def main():
+    pass
+
+
+if __name__ == '__main__':
+    main()
---
+++ packages/pwm-lib/pwm_lib-0.0.1.dev1-py2.py3-none-any.whl/pwm_lib-0.0.1.dev1.dist-info/DESCRIPTION.rst
@@ -0,0 +1,31 @@
+PAiP Web Modules
+================
+
+Actual Version : v.0.0.1-dev1
+
+PWM is DRY library (If you need something more just make merge request).
+
+
+* Free software: MIT license
+* PyPi Package: https://pypi.python.org/pypi/pwbs/ or https://pypi.org/project/pwbs/
+
+Modules
+-------
+
+* TODO: Working on it
+
+Credits
+---------
+
+Created by Patryk Adamczyk
+
+
+History
+=======
+
+0.0.1-dev0 (10.07.2018)
+-----------------------
+
+* First packaging
+
+
