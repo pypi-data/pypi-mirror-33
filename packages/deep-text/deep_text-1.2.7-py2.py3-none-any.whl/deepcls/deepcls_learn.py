573ca47cf4e35e0339265b509078c0974073ae7e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:33:50 +0000

    Add package deep-text/1.2.7/deep_text-1.2.7-py2.py3-none-any.whl

---
+++ packages/deep-text/deep_text-1.2.7-py2.py3-none-any.whl/deepcls/deepcls_learn.py
@@ -0,0 +1,79 @@
+# -*- coding=utf-8 -*-
+
+from common import utils
+from .train import TextCNNTrainer
+from .train import TextRNNTrainer
+from .default_custom import DefaultCNNTransform
+from .default_custom import DefaultRNNTransform
+import sys
+import getopt
+
+
+def main():
+    model_type = sys.argv[1]
+    train_input_path = ""
+    test_input_path = ""
+    config_file_path = ""
+    model_save_path = ""
+    need_transform = False
+    need_build_word2vec = False
+    try:
+        opts, args = getopt.getopt(sys.argv[2:], "ht:e:c:m:fw",
+                                   ["train=", "test=", "config=", "model="])
+    except getopt.getopt.GetoptError:
+        print('deepcls_learn [cnn|rnn] -t <trainfile> -e <testfile> -c <configfile> -m <model save path> '
+              '[-f <do transform> -w <build word2vec>]')
+        sys.exit(2)
+        
+    if len(opts) == 0:
+        print('deepcls_learn [cnn|rnn] -t <trainfile> -e <testfile> -c <configfile> -m <model save path> '
+              '[-f <do transform> -w <build word2vec>]')
+        sys.exit()
+        
+    for opt, arg in opts:
+        if opt == '-h':
+            print('deepcls_learn [cnn|rnn] -t <trainfile> -e <testfile> -c <configfile> -m <model save path> '
+                  '[-f <do transform> -w <build word2vec>]')
+            sys.exit()
+        elif opt in ("-t", "--train"):
+            train_input_path = arg
+        elif opt in ("-e", "--test"):
+            test_input_path = arg
+        elif opt in ("-c", "--config"):
+            config_file_path = arg
+        elif opt in ("-m", "--model"):
+            model_save_path = arg
+        elif opt == "-f":
+            need_transform = True
+        elif opt == "-w":
+            need_build_word2vec = True
+
+    if train_input_path == "":
+        print("Please input -t or --train to set train_input_path.")
+        sys.exit()
+         
+    if test_input_path == "":
+        print("Please input -e or --test to set test_input_path.")
+        sys.exit()
+        
+    if config_file_path == "":
+        print("Please input -c or --config to set config_file_path.")
+        sys.exit()
+            
+    if model_save_path == "":
+        print("Please input -m or --model to set model_save_path.")
+        sys.exit()
+
+    if model_type == "cnn":
+        config = utils.load_deep_cls_cnn_config(config_file_path, train_input_path, test_input_path)
+        trainer = TextCNNTrainer(config, DefaultCNNTransform, need_transform, need_build_word2vec)
+        model = trainer.train()
+    elif model_type == "rnn":
+        config = utils.load_deep_cls_rnn_config(config_file_path, train_input_path, test_input_path)
+        trainer = TextRNNTrainer(config, DefaultRNNTransform, need_transform, need_build_word2vec)
+        model = trainer.train()
+    else:
+        print('The model type must be \'cnn\' or \'rnn\'')
+        sys.exit()
+
+    model.save(model_save_path)
