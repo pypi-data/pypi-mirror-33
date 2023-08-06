d74bc563c0204ae3fa726b8d54e2e4356387426a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:34:24 +0000

    Add package fake-gen/1.0.0b1/fake_gen-1.0.0b1-py3-none-any.whl

---
+++ packages/fake-gen/fake_gen-1.0.0b1-py3-none-any.whl/fake_gen/factories/fake.py
@@ -0,0 +1,14 @@
+from fake_gen.base import Callable
+from fake_gen.errors import NoSuchDatatype
+import faker
+
+class FakeDataFactory(Callable):
+
+    _FAKER_FACTORY = faker.Factory.create()
+
+    def __init__(self, data_type):
+        if not hasattr(self._FAKER_FACTORY, data_type):
+            raise NoSuchDatatype(data_type)
+        self._data_type = data_type
+        faker_func = getattr(self._FAKER_FACTORY, data_type)
+        super(FakeDataFactory, self).__init__(faker_func)
