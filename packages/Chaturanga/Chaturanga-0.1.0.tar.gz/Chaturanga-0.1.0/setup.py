ffef37b60e7ee70db11c76ca2ee680b5953c6078
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:09:15 +0000

    Add package Chaturanga/0.1.0/Chaturanga-0.1.0.tar.gz

---
+++ packages/Chaturanga/Chaturanga-0.1.0.tar.gz/Chaturanga-0.1.0/setup.py
@@ -0,0 +1,20 @@
+from setuptools import setup
+
+
+with open('README.md') as f:
+    long_description = f.read()
+
+
+setup(name='Chaturanga',
+      version='0.1.0',
+      description='Chess API for python',
+      long_description=long_description,
+      long_description_content_type='text/markdown',
+      url='https://github.com/Cheran-Senthil/Chaturanga',
+      author='Cheran',
+      license='MIT',
+      packages=['chaturanga'],
+      install_requires=[
+          'six',
+          'termcolor',
+      ])
