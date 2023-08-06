63d0bbfee598939c3a4db58c69880315cf1e8a46
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:09:04 +0000

    Add package starlette/0.1.7/starlette-0.1.7.tar.gz

---
+++ packages/starlette/starlette-0.1.7.tar.gz/starlette-0.1.7/starlette/__init__.py
@@ -0,0 +1,29 @@
+from starlette.decorators import asgi_application
+from starlette.response import (
+    FileResponse,
+    HTMLResponse,
+    JSONResponse,
+    Response,
+    PlainTextResponse,
+    StreamingResponse,
+)
+from starlette.request import Request
+from starlette.routing import Path, PathPrefix, Router
+from starlette.testclient import TestClient
+
+
+__all__ = (
+    "asgi_application",
+    "FileResponse",
+    "HTMLResponse",
+    "JSONResponse",
+    "Path",
+    "PathPrefix",
+    "PlainTextResponse",
+    "Response",
+    "Router",
+    "StreamingResponse",
+    "Request",
+    "TestClient",
+)
+__version__ = "0.1.7"
