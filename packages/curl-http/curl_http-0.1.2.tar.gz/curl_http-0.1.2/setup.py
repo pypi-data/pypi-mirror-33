1b0e066f7241f6b476e6c47da5830ec5fbf40779
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:13:24 +0000

    Add package curl-http/0.1.2/curl_http-0.1.2.tar.gz

---
+++ packages/curl-http/curl_http-0.1.2.tar.gz/curl_http-0.1.2/setup.py
@@ -0,0 +1,22 @@
+# !/usr/bin/env python
+# -*- coding:utf-8 -*-
+from setuptools import setup, find_packages
+
+setup(
+    name="curl_http",
+    version="0.1.2",
+    keywords=("curl", "http", "HTTP", "curl_http", "PyCUrl"),
+    description="curl_http is a wrapper for PyCUrl",
+    long_description="curl_http is a wrapper for PyCUrl,which we use PyCURL more convenient and easy",
+    license="MIT Licence",
+
+    url="https://github.com/LDouble/curl_http",
+    author="DoubleL",
+    author_email="2943200389@qq.com",
+
+    packages=find_packages(),
+    include_package_data=True,
+    platforms="any",
+    install_requires=["pycurl","chardet"]
+
+)
