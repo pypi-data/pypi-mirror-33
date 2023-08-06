de17754bdf6a739ef41243f2da0ce5eedd320f0d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:22:45 +0000

    Add package mitoo/1.9.8/mitoo-1.9.8.tar.gz

---
+++ packages/mitoo/mitoo-1.9.8.tar.gz/mitoo-1.9.8/mitoo/output/__init__.py
@@ -0,0 +1,9 @@
+from .output_adapter import OutputAdapter
+from .microsoft import Microsoft
+from .terminal import TerminalAdapter
+
+__all__ = (
+    'OutputAdapter',
+    'Microsoft',
+    'TerminalAdapter',
+)
