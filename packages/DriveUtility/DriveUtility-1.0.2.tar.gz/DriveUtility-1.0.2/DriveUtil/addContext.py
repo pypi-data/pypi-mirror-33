b3f0887f4fc5318aa6a9c8f1e9a27ed6560fe008
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 07:12:10 +0000

    Add package DriveUtility/1.0.2/DriveUtility-1.0.2.tar.gz

---
+++ packages/DriveUtility/DriveUtility-1.0.2.tar.gz/DriveUtility-1.0.2/DriveUtil/addContext.py
@@ -0,0 +1,24 @@
+# !/usr/bin/env python
+# -*- coding: utf-8 -*-
+#Pedro Gabaldon
+
+from _winreg import *
+import os
+
+dirBackPath = "Directory\\Background\\shell\\"
+dirPath = "Directory\\shell\\"
+anyExtPath = "*\\shell\\"
+
+removeAuthK = SetValue(CreateKey(HKEY_CLASSES_ROOT, dirBackPath + "Remove Drive access"), "command", REG_SZ, os.path.dirname(__file__) + "\\cmd\\removeOauth.bat")
+
+newFolderK = SetValue(CreateKey(HKEY_CLASSES_ROOT, dirBackPath + "New Drive folder"), "command", REG_SZ, os.path.dirname(__file__) + "\\cmd\\createFolder.bat")
+
+deleteK = SetValue(CreateKey(HKEY_CLASSES_ROOT, dirBackPath + "Delete Drive file/folder"), "command", REG_SZ, os.path.dirname(__file__) + "\\cmd\\delete.bat")
+
+updirK = SetValue(CreateKey(HKEY_CLASSES_ROOT, dirPath + "Upload Drive"), "command", REG_SZ, "\"" + os.path.dirname(__file__) + "\\cmd\\upload.bat" + "\"" + " \"%1\"")
+upAnyExtK = SetValue(CreateKey(HKEY_CLASSES_ROOT, anyExtPath + "Upload Drive"), "command", REG_SZ, "\"" + os.path.dirname(__file__) + "\\cmd\\upload.bat" + "\"" + " \"%1\"")
+
+updirSK = SetValue(CreateKey(HKEY_CLASSES_ROOT, dirPath + "Upload Drive(Specific)"), "command", REG_SZ, "\"" + os.path.dirname(__file__) + "\\cmd\\upload (specific).bat" + "\"" + " \"%1\"")
+upAnyExtSK = SetValue(CreateKey(HKEY_CLASSES_ROOT, anyExtPath + "Upload Drive(Specific)"), "command", REG_SZ, "\"" + os.path.dirname(__file__) + "\\cmd\\upload (specific).bat" + "\"" + " \"%1\"")
+
+print 'Added'
