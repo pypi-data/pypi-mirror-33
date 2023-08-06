e19720de772b55e9e101cd9f7da7ba8c31356b14
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:22:21 +0000

    Add package megacli/0.0.11/megacli-0.0.11.tar.gz

---
+++ packages/megacli/megacli-0.0.11.tar.gz/megacli-0.0.11/setup.py
@@ -0,0 +1,16 @@
+from distutils.core import setup
+
+version = '0.0.11'
+
+setup(
+  name = 'megacli',
+  packages = ['megacli'],
+  version = version,
+  description = 'Python library for MegaCli',
+  author = 'Matteo Cerutti',
+  author_email = 'matteo.cerutti@hotmail.co.uk',
+  url = 'https://github.com/m4ce/megacli-python',
+  download_url = 'https://github.com/m4ce/megacli-python/tarball/%s' % (version,),
+  keywords = ['megacli'],
+  classifiers = []
+)
