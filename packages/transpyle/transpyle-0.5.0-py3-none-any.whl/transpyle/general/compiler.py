73eb126019c3a74685410cae75d07702125fccce
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:24:17 +0000

    Add package transpyle/0.5.0/transpyle-0.5.0-py3-none-any.whl

---
+++ packages/transpyle/transpyle-0.5.0-py3-none-any.whl/transpyle/general/compiler.py
@@ -0,0 +1,19 @@
+"""Language-specific compiler interfaces."""
+
+import pathlib
+import typing as t
+
+from .registry import Registry
+
+
+class Compiler(Registry):
+
+    """Interface for language-specific compilers."""
+
+    def __init__(self, *args, **kwargs):
+        self.default_args = args
+        self.default_kwargs = kwargs
+
+    def compile(self, code: str, path: t.Optional[pathlib.Path] = None,
+                output_folder: t.Optional[pathlib.Path] = None, **kwargs) -> pathlib.Path:
+        raise NotImplementedError()
---
+++ packages/transpyle/transpyle-0.5.0-py3-none-any.whl/transpyle/general/transpiler.py
@@ -0,0 +1,47 @@
+"""Transpilation of source code."""
+
+import pathlib
+
+from .registry import Registry
+from .code_writer import CodeWriter
+from .language import Language
+from .compiler import Compiler
+from .binder import Binder
+from .translator import Translator, AutoTranslator
+
+
+class Transpiler(Registry):
+
+    """Translate a function to another language, compile and create binding for the result."""
+
+    def __init__(self, translator: Translator, compiler: Compiler, binder: Binder):
+        self.translator = translator
+        self.compiler = compiler
+        self.binder = binder
+
+    def transpile(self, code: str, path: pathlib.Path = None, translated_path: pathlib.Path = None,
+                  compile_folder: pathlib.Path = None):
+        """Transpile given code."""
+        assert isinstance(path, pathlib.Path), type(path)
+        assert path.is_file(), path
+        assert isinstance(translated_path, pathlib.Path), type(translated_path)
+        assert isinstance(compile_folder, pathlib.Path), type(compile_folder)
+        assert compile_folder.is_dir(), compile_folder
+        translated_code = self.translator.translate(code, path)
+        code_writer = CodeWriter(translated_path.suffix)
+        code_writer.write_file(translated_code, translated_path)
+        compiled_path = self.compiler.compile(translated_code, translated_path, compile_folder)
+        binding = self.binder.bind(compiled_path)
+        return binding
+
+
+class AutoTranspiler(Transpiler):
+
+    """Translate a function to another language, compile and create binding for the result."""
+
+    def __init__(self, from_language: Language, to_language: Language):
+        super().__init__(AutoTranslator(from_language, to_language),
+                         Compiler.find(to_language)(),
+                         Binder.find(to_language)())
+        self.from_language = from_language
+        self.to_language = to_language
