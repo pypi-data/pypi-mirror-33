ecf2615176af87ee4e37a304d3b9ab206d22db1f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:20:54 +0000

    Add package bbblong1/0.0.2/bbblong1-0.0.2-py3-none-any.whl

---
+++ packages/bbblong1/bbblong1-0.0.2-py3-none-any.whl/bbblong/__init__.py
@@ -0,0 +1,10 @@
+name="bbblong"
+
+print("from the whole class")
+
+class a1():
+    print("This is in class a")
+    def number(self):
+        return 3
+def anumber1():
+    return 3
---
+++ packages/bbblong1/bbblong1-0.0.2-py3-none-any.whl/bbblong/test.py
@@ -0,0 +1,8 @@
+class a():
+    print("This is in class a")
+    def number(self):
+        return 3
+def anumber():
+    return 3
+
+print("the whole file")
