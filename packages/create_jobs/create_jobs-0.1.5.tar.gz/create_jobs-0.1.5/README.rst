1b2bad858d76288ef9cae4fe92d45bf97def5214
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:50:34 +0000

    Add package create_jobs/0.1.5/create_jobs-0.1.5.tar.gz

---
+++ packages/create_jobs/create_jobs-0.1.5.tar.gz/create_jobs-0.1.5/README.rst
@@ -0,0 +1,6 @@
+create_jobs
+###########
+
+For usage and documentation, check out the `GitHub repo`_.
+
+.. _`GitHub repo`: https://github.com/benlindsay/create_jobs/
---
+++ packages/create_jobs/create_jobs-0.1.5.tar.gz/create_jobs-0.1.5/setup.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+#
+# Copyright (c) 2017 Ben Lindsay <benjlindsay@gmail.com>
+
+from distutils.core import setup
+
+desc = 'A module for automating comput job creation and submission'
+
+with open('README.rst', 'r') as f:
+    long_desc = f.read()
+
+setup(
+  name = 'create_jobs',
+  packages = ['create_jobs'],
+  version = '0.1.5',
+  description = desc,
+  long_description = long_desc,
+  requires = ['numpy', 'pandas'],
+  install_requires = ['numpy', 'pandas'],
+  scripts = ['bin/create_jobs'],
+  author = 'Ben Lindsay',
+  author_email = 'benjlindsay@gmail.com',
+  url = 'https://github.com/benlindsay/create_jobs',
+  keywords = ['workflow', 'simulations'],
+  classifiers = [],
+)
