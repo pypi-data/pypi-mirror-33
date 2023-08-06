59513cef0e7753e3cdf8a36e3270cf92f0081745
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:26:31 +0000

    Add package waiverdb/0.11.0/waiverdb-0.11.0.tar.gz

---
+++ packages/waiverdb/waiverdb-0.11.0.tar.gz/waiverdb-0.11.0/waiverdb/cli.py
@@ -0,0 +1,234 @@
+# SPDX-License-Identifier: GPL-2.0+
+"""
+CLI for creating new waivers against test results.
+"""
+import click
+import requests
+import json
+import configparser
+import re
+from xmlrpc import client
+
+requests_session = requests.Session()
+
+
+def validate_config(config):
+    """
+    Validates the configuration needed for WaiverDB
+    :return: None or ClickException
+    """
+    # Verify that all necessary config options are set
+    config_error = ('The config option "{0}" is required')
+    if not config.has_option('waiverdb', 'auth_method'):
+        raise click.ClickException(config_error.format('auth_method'))
+    auth_method = config.get('waiverdb', 'auth_method')
+    if auth_method not in ['OIDC', 'Kerberos', 'dummy']:
+        raise click.ClickException('The WaiverDB authentication mechanism of '
+                                   '"{0}" is not supported'.format(auth_method))
+    required_configs = ['api_url']
+    if auth_method == 'OIDC':
+        required_configs.append('oidc_id_provider')
+        required_configs.append('oidc_client_id')
+        required_configs.append('oidc_scopes')
+    for required_config in required_configs:
+        if not config.has_option('waiverdb', required_config):
+            raise click.ClickException(config_error.format(required_config))
+
+
+def check_response(resp, data, result_id=None):
+    if result_id:
+        msg = 'for result with id {0}'.format(result_id)
+    else:
+        msg = 'for result with subject {0} and testcase {1}'.format(json.dumps(data['subject']),
+                                                                    data['testcase'])
+    if not resp.ok:
+        try:
+            error_msg = resp.json()['message']
+        except (ValueError, KeyError):
+            error_msg = resp.text
+        raise click.ClickException(
+            'Failed to create waiver {0}:\n{1}'
+            .format(msg, error_msg))
+    click.echo('Created waiver {0} {1}'.format(
+        resp.json()['id'], msg))
+
+
+def guess_product_version(toparse, koji_build=False):
+    if toparse == 'rawhide' or toparse.startswith('Fedora-Rawhide'):
+        return 'fedora-rawhide'
+    else:
+        product_version = None
+        if (toparse.startswith('f') and koji_build):
+            product_version = 'fedora-'
+        elif toparse.startswith('epel'):
+            product_version = 'epel-'
+        elif toparse.startswith('el'):
+            product_version = 'rhel-'
+        elif toparse.startswith('fc') or toparse.startswith('Fedora'):
+            product_version = 'fedora-'
+        if product_version:
+            # seperate the prefix from the number
+            result = list(filter(None, '-'.join(re.split(r'(\d+)', toparse)).split('-')))
+            if len(result) >= 2:
+                try:
+                    int(result[1])
+                    product_version += result[1]
+                    return product_version
+                except ValueError:
+                    pass
+    return None
+
+
+@click.command(context_settings={'help_option_names': ['-h', '--help']})
+@click.option('--config-file', '-C', default='/etc/waiverdb/client.conf',
+              type=click.Path(exists=True),
+              help='Specify a config file to use')
+@click.option('--result-id', '-r', multiple=True, type=int,
+              help='Specify one or more results to be waived')
+@click.option('--subject', '-s',
+              help='Specify one subject for a result to waive')
+@click.option('--testcase', '-t',
+              help='Specify a testcase for the subject')
+@click.option('--product-version', '-p',
+              help='Specify one of PDC\'s product version identifiers.')
+@click.option('--waived/--no-waived', default=True,
+              help='Whether or not the result is waived')
+@click.option('--comment', '-c',
+              help='A comment explaining why the result is waived')
+def cli(comment, waived, product_version, testcase, subject, result_id, config_file):
+    """
+    Creates new waiver against test results.
+
+    Examples:
+
+    \b
+        waiverdb-cli -r 47 -r 48 -p "fedora-28" -c "This is fine"
+
+    \b
+        waiverdb-cli -t dist.rpmdeplint \\
+            -s '{"item": "qclib-1.3.1-3.fc28", "type": "koji_build"}' \\
+            -p "fedora-28" -c "This is expected for non-x86 packages"
+    """
+    config = configparser.SafeConfigParser()
+
+    config.read(config_file)
+    validate_config(config)
+
+    result_ids = result_id
+    if not comment:
+        raise click.ClickException('Please specify comment')
+    if result_ids and (subject or testcase):
+        raise click.ClickException('Please specify result_id or subject/testcase. Not both')
+    if not result_ids and not subject:
+        raise click.ClickException('Please specify one subject')
+    if not result_ids and not testcase:
+        raise click.ClickException('Please specify testcase')
+
+    if not product_version and not result_ids:
+        # trying to guess the product_version
+        if json.loads(subject).get('type', None) == 'koji_build':
+            try:
+                short_prod_version = json.loads(subject)['item'].split('.')[-1]
+                product_version = guess_product_version(short_prod_version, koji_build=True)
+            except KeyError:
+                pass
+
+        # try to call koji
+        if config.has_option('waiverdb', 'koji_base_url'):
+            koji_base_url = config.get('waiverdb', 'koji_base_url')
+            proxy = client.ServerProxy(koji_base_url)
+            try:
+                build = proxy.getBuild(json.loads(subject)['item'])
+                if build:
+                    target = proxy.getTaskRequest(build['task_id'])[1]
+                    product_version = guess_product_version(target, koji_build=True)
+            except KeyError:
+                pass
+            except client.Fault:
+                pass
+
+        if "productmd.compose.id" in json.loads(subject):
+            product_version = guess_product_version(
+                json.loads(subject)["productmd.compose.id"])
+
+    if not product_version:
+        raise click.ClickException('Please specify product version using --product-version')
+
+    auth_method = config.get('waiverdb', 'auth_method')
+    data_list = []
+    if not result_ids:
+        data_list.append({
+            'subject': json.loads(subject),
+            'testcase': testcase,
+            'waived': waived,
+            'product_version': product_version,
+            'comment': comment
+        })
+
+    # XXX - TODO - remove this in a future release.  (for backwards compat)
+    for result_id in result_ids:
+        data_list.append({
+            'result_id': result_id,
+            'waived': waived,
+            'product_version': product_version,
+            'comment': comment
+        })
+
+    api_url = config.get('waiverdb', 'api_url')
+    if auth_method == 'OIDC':
+        # Try to import this now so the user gets immediate feedback if
+        # it isn't installed
+        try:
+            import openidc_client  # noqa: F401
+        except ImportError:
+            raise click.ClickException('python-openidc-client needs to be installed')
+        # Get the auth token using the OpenID client.
+        oidc_client_secret = None
+        if config.has_option('waiverdb', 'oidc_client_secret'):
+            oidc_client_secret = config.get('waiverdb', 'oidc_client_secret')
+        oidc = openidc_client.OpenIDCClient(
+            'waiverdb',
+            config.get('waiverdb', 'oidc_id_provider'),
+            {'Token': 'Token', 'Authorization': 'Authorization'},
+            config.get('waiverdb', 'oidc_client_id'),
+            oidc_client_secret)
+        scopes = config.get('waiverdb', 'oidc_scopes').strip().splitlines()
+
+        for data in data_list:
+            resp = oidc.send_request(
+                scopes=scopes,
+                url='{0}/waivers/'.format(api_url.rstrip('/')),
+                data=json.dumps(data),
+                headers={'Content-Type': 'application/json'},
+                timeout=60)
+            check_response(resp, data, data.get('result_id', None))
+    elif auth_method == 'Kerberos':
+        # Try to import this now so the user gets immediate feedback if
+        # it isn't installed
+        try:
+            import requests_gssapi  # noqa: F401
+        except ImportError:
+            raise click.ClickException(
+                'python-requests-gssapi needs to be installed')
+        auth = requests_gssapi.HTTPKerberosAuth(
+            mutual_authentication=requests_gssapi.OPTIONAL)
+        for data in data_list:
+            resp = requests.request('POST', '{0}/waivers/'.format(api_url.rstrip('/')),
+                                    data=json.dumps(data), auth=auth,
+                                    headers={'Content-Type': 'application/json'},
+                                    timeout=60)
+            if resp.status_code == 401:
+                raise click.ClickException('WaiverDB authentication using GSSAPI failed. '
+                                           'Make sure you have a valid Kerberos ticket.')
+            check_response(resp, data, data.get('result_id', None))
+    elif auth_method == 'dummy':
+        for data in data_list:
+            resp = requests.request('POST', '{0}/waivers/'.format(api_url.rstrip('/')),
+                                    data=json.dumps(data), auth=('user', 'pass'),
+                                    headers={'Content-Type': 'application/json'},
+                                    timeout=60)
+            check_response(resp, data, data.get('result_id', None))
+
+
+if __name__ == '__main__':
+    cli()  # pylint: disable=E1120
