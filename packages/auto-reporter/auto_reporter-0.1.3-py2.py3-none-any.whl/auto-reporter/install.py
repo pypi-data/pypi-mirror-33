d7d33e0a3c1f0ef4a4beed9a16db1db12025390d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:18:22 +0000

    Add package auto-reporter/0.1.3/auto_reporter-0.1.3-py2.py3-none-any.whl

---
+++ packages/auto-reporter/auto_reporter-0.1.3-py2.py3-none-any.whl/auto-reporter/install.py
@@ -0,0 +1,17 @@
+import os
+import stat
+import subprocess
+import sys
+
+def main():
+    git_dir = subprocess.check_output(['git', 'rev-parse', '--git-dir'])
+    hook_file_path = os.path.join(
+        os.path.abspath(git_dir.strip().decode('utf-8')), 'hooks', 'pre-push'
+    )
+
+    with open(hook_file_path, 'w') as fd:
+        content = '%s -c "import sys; import os; import auto-reporter.hook; sys.exit(auto-reporter.main(%s, os.path.split(os.getcwd())[1]))" $@\n' % (sys.executable, sys.argv[1:])
+        fd.write(content)
+    os.chmod(hook_file_path, stat.S_IRWXU | stat.S_IRGRP | stat.S_IROTH)
+
+    sys.stdout.write('Auto report installed.\n')
