70db4d60e94df663b8a5ffd86cf65d0fc2ceeb57
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:22:06 +0000

    Add package pocoui/1.0.55/pocoui-1.0.55.tar.gz

---
+++ packages/pocoui/pocoui-1.0.55.tar.gz/pocoui-1.0.55/poco/utils/simplerpc/transport/tcp/main.py
@@ -0,0 +1,37 @@
+# coding=utf-8
+
+from ..interfaces import IClient
+from .asynctcp import Client, init_loop
+from .protocol import SimpleProtocolFilter
+
+
+DEFAULT_ADDR = ("0.0.0.0", 5001)
+
+
+class TcpClient(IClient):
+    """docstring for TcpClient"""
+    def __init__(self, addr=DEFAULT_ADDR):
+        super(TcpClient, self).__init__()
+        self.addr = addr
+        self.prot = SimpleProtocolFilter()
+        self.c = None
+
+    def connect(self):
+        if not self.c:
+            self.c = Client(self.addr,
+                            on_connect=self.on_connect,
+                            on_close=self.on_close)
+            init_loop()
+        self.c.connect_server()
+
+    def send(self, msg):
+        msg_bytes = self.prot.pack(msg)
+        self.c.say(msg_bytes)
+
+    def recv(self):
+        msg_bytes = self.c.read_message()
+        return self.prot.input(msg_bytes)
+
+    def close(self):
+        self.c.close_connection()
+        self.c = None
