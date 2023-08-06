b0d10f29cff866fb9c0b0d26c50b834d1e7eae82
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:49:53 +0000

    Add package icenumerics/0.1.9/icenumerics-0.1.9-py3-none-any.whl

---
+++ packages/icenumerics/icenumerics-0.1.9-py3-none-any.whl/icenumerics/__init__.py
@@ -0,0 +1,19 @@
+from pint import UnitRegistry
+
+import sys
+
+try: 
+    from .magcolloids import magcolloid as mc
+except ImportError as e:
+    try: 
+        import magcolloid as mc
+    except ImportError as e:
+        raise ImportError
+
+ureg = mc.ureg
+
+from icenumerics.spins import *
+from icenumerics.colloidalice import *
+from icenumerics.vertices import *
+
+__version__ = "0.1.9"
