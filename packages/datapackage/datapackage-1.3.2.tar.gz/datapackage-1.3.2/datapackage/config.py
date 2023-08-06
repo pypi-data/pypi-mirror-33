5c551368f471da77134512c23ceb377e1061dd4a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:43:09 +0000

    Add package datapackage/1.3.2/datapackage-1.3.2.tar.gz

---
+++ packages/datapackage/datapackage-1.3.2.tar.gz/datapackage-1.3.2/datapackage/config.py
@@ -0,0 +1,24 @@
+# -*- coding: utf-8 -*-
+from __future__ import absolute_import
+from __future__ import division
+from __future__ import print_function
+from __future__ import unicode_literals
+
+
+# Module API
+
+TABULAR_FORMATS = ['csv', 'tsv', 'xls', 'xlsx']
+DEFAULT_DATA_PACKAGE_PROFILE = 'data-package'
+DEFAULT_RESOURCE_PROFILE = 'data-resource'
+DEFAULT_FIELD_TYPE = 'string'
+DEFAULT_FIELD_FORMAT = 'default'
+DEFAULT_MISSING_VALUES = ['']
+DEFAULT_DIALECT = {
+    'delimiter': ',',
+    'doubleQuote': True,
+    'lineTerminator': '\r\n',
+    'quoteChar': '"',
+    'skipInitialSpace': True,
+    'header': True,
+    'caseSensitiveHeader': False,
+}
