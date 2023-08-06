ea94f7c9ff50bfb6f55d4e4180bde1c0abdf0e1f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:22:10 +0000

    Add package laminarflow/0.0.6/laminarflow-0.0.6.tar.gz

---
+++ packages/laminarflow/laminarflow-0.0.6.tar.gz/laminarflow-0.0.6/setup.py
@@ -0,0 +1,44 @@
+import os
+import setuptools
+
+with open('README.md') as f:
+  long_description = f.read()
+
+with open('requirements.txt') as f:
+  install_requirements = f.readlines()
+
+setuptools.setup(
+  name='laminarflow',
+  version='0.0.6',
+  description='Streamline your TensorFlow workflow.',
+  long_description=long_description,
+  long_description_content_type='text/markdown',
+  url='https://github.com/elliotwaite/laminarflow',
+  author='Elliot Waite',
+  author_email='elliot@elliotwaite.com',
+  packages=setuptools.find_packages(),
+  install_requires=install_requirements,
+  classifiers=(
+    'Development Status :: 1 - Planning',
+    'Intended Audience :: Developers',
+    'Intended Audience :: Education',
+    'Intended Audience :: Science/Research',
+    'License :: OSI Approved :: Apache Software License',
+    'Programming Language :: Python :: 2',
+    'Programming Language :: Python :: 2.7',
+    'Programming Language :: Python :: 3',
+    'Programming Language :: Python :: 3.4',
+    'Programming Language :: Python :: 3.5',
+    'Programming Language :: Python :: 3.6',
+    'Topic :: Scientific/Engineering',
+    'Topic :: Scientific/Engineering :: Artificial Intelligence',
+    'Topic :: Scientific/Engineering :: Mathematics',
+    'Topic :: Software Development',
+    'Topic :: Software Development :: Libraries',
+    'Topic :: Software Development :: Libraries :: Python Modules',
+  ),
+  license='Apache 2.0',
+  keywords='laminarflow laminar tensorflow tensor tfrecord tfrecords ml '
+           'machine learning ai artificial intelligence deep neural network '
+           'networks',
+)
