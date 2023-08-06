045af092161601881b07a611515130839ba2ba20
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:45:45 +0000

    Add package tap-news-utils/0.0.2/tap_news_utils-0.0.2.tar.gz

---
+++ packages/tap-news-utils/tap_news_utils-0.0.2.tar.gz/tap_news_utils-0.0.2/tap_news_utils/news_api_client.py
@@ -0,0 +1,41 @@
+import requests
+
+from json import loads
+
+NEWS_API_ENDPOINT = "https://newsapi.org/v1/"
+NEWS_API_KEY = "2ffd0fc65c15466b8bb037a5dc41d9c6"
+ARTICLES_API = "articles"
+
+#defineconstant
+CNN = 'cnn'
+DEFAULT_SOURCE = [CNN]
+SORT_BY_TOP = 'top'
+
+def _buildUrl(endPoint = NEWS_API_ENDPOINT, apiName = ARTICLES_API):
+    return endPoint + apiName
+
+def getNewsFromSource(sources = DEFAULT_SOURCE, sortBy = SORT_BY_TOP):
+    articles = []
+
+    # get news from each source
+    for source in sources:
+        payload = {
+            'apiKey': NEWS_API_KEY,
+            'source': source,
+            'sortBy': sortBy
+        }
+        response = requests.get(_buildUrl(), params = payload)
+        # response.content is a binary
+        res_json = loads(response.content.decode('utf-8'))
+        print ('news api received ', res_json)
+        # Extract info from response
+        if (res_json is not None and
+            res_json['status'] == 'ok' and
+            res_json['source'] is not None):
+            # populate news source in each articles
+            for news in res_json['articles']:
+                news['source'] = res_json['source']
+            # join two list into one list
+            articles.extend(res_json['articles'])
+
+    return articles
