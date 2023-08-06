e3d3e50a141aefeaf8f8fc70249a42c96e7ec252
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:07:38 +0000

    Add package avi-okta/0.0.1/avi_okta-0.0.1.tar.gz

---
+++ packages/avi-okta/avi_okta-0.0.1.tar.gz/avi_okta-0.0.1/setup.py
@@ -0,0 +1,24 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="avi_okta",
+    version="0.0.1",
+    author="Neel Parikh",
+    author_email="nparikh@avinetworks.com",
+    description="Avi Networks Okta sdk",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/avinetworks/avi-internal",
+    packages=setuptools.find_packages(),
+    classifiers=(
+	"Programming Language :: Python :: 2",
+    	"Programming Language :: Python :: 2.6",
+        "Programming Language :: Python :: 2.7",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ),
+)
+
