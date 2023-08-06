f4875f2ab2b39b9d6926383c6fda9ca625fda4b4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:45:45 +0000

    Add package ssh-websocket/0.0.6/ssh-websocket-0.0.6.tar.gz

---
+++ packages/ssh-websocket/ssh-websocket-0.0.6.tar.gz/ssh-websocket-0.0.6/ssh_websocket/ssh_client.py
@@ -0,0 +1,68 @@
+"""SSHClient for websocket connection."""
+import asyncio
+import websockets
+import hashlib
+import json
+import sys
+from termcolor import colored
+
+async def handshake():
+    """Handshake with the server."""
+    global slave
+    global password
+    async with websockets.connect(sys.argv[1]) as websocket:
+        await websocket.send(b"list")
+        slave_list = await websocket.recv()
+        print("The current slaves are availables :")
+        print(" ".join([
+            i["name"] if i["name"] is not None else ""
+            for i in json.loads(slave_list)
+        ]))
+        slave = input("Slave name $> ")
+        password = hashlib.sha512(input("Password $> ").encode()).hexdigest()
+        data = {"command": "echo \"The password is good\"", "slave": slave, "pswd": password}
+        await websocket.send(json.dumps(data).encode())
+        greeting = await websocket.recv()
+        try:
+            returned = json.loads(greeting)
+        except json.decoder.JSONDecodeError:
+            print("Unkown JSON")
+        sys.stdout.write(colored(returned["stdout"], "green"))
+        sys.stdout.write(colored(returned["stderr"], "red"))
+        if returned["stderr"] == "Bad password !":
+            return 0
+        return 1
+
+
+async def send_commands():
+    """Send a command."""
+    global slave
+    global password
+    async with websockets.connect(sys.argv[1]) as websocket:
+        while True:
+            command = input(" $> ")
+            data = {"command": command, "slave": slave, "pswd": password}
+            await websocket.send(json.dumps(data).encode())
+            greeting = await websocket.recv()
+            try:
+                returned = json.loads(greeting)
+            except json.decoder.JSONDecodeError:
+                print("Unkown JSON")
+            else:
+                print(returned["stdout"][:-1])
+                print(returned["stderr"][:-1])
+
+def main():
+    global slave
+    while True:
+        if asyncio.get_event_loop().run_until_complete(handshake()):
+            try:
+                asyncio.get_event_loop().run_until_complete(send_commands())
+            except EOFError:
+                print(f"Exiting {slave}")
+
+if __name__ == "__main__":
+    try:
+        main()
+    except EOFError:
+        print("Exiting...")
