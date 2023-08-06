d89ef2427123852edee5eeebfe142d74028de60b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:11:28 +0000

    Add package RegApi/0.0.1/RegApi-0.0.1.tar.gz

---
+++ packages/RegApi/RegApi-0.0.1.tar.gz/RegApi-0.0.1/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name="RegApi",
+    author="Vyacheslav Anzhiganov",
+    author_email="vanzhiganov@ya.ru",
+    version="0.0.1",
+    packages=[
+        "RegApi"
+    ]
+)
