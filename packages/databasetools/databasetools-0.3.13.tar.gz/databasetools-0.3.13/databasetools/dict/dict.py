99f53f80f907e7a8882566bbe6d6565aeda7bb73
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:53:03 +0000

    Add package databasetools/0.3.13/databasetools-0.3.13.tar.gz

---
+++ packages/databasetools/databasetools-0.3.13.tar.gz/databasetools-0.3.13/databasetools/dict/dict.py
@@ -0,0 +1,71 @@
+import os
+from pprint import pprint
+from databasetools.dict.json import JSON
+from databasetools.dict.npy import NPY
+from databasetools.dict.pickle import Pickle
+
+
+class DictTools:
+    def __init__(self, directory, name, protocol='json'):
+        """
+        Save or load data to or from .npy dictionary file
+        :param directory: Directory to save or load file from
+        :param name: File name
+        :param protocal: Method for saving and loading dictionaries
+        """
+        # Get dictionary of protocals
+        protocols_dict = self.protocol_options()
+        self.choice = protocol.strip('.')  # Remove leading '.' if included in protocal var
+        try:
+            self.protocol = protocols_dict[self.choice]
+        except KeyError:
+            # Use Numpy as default
+            self.protocol = protocols_dict['json']
+        # Join root directory and save name to create full path
+        self.save_name = os.path.join(directory, str(name + self.protocol['ext']))
+
+    def __iter__(self):
+        return iter(self.read_dictionary)
+
+    @property
+    def choices(self):
+        """
+        Print dictionary of protocal choices for reference
+        :return: protocals dictionary
+        """
+        protocols_dict = self.protocal_options()
+        pprint(protocols_dict)
+        return protocols_dict
+
+    @staticmethod
+    def protocol_options():
+        protocols_dict = {
+            'npy': {
+                'class': NPY,
+                'ext': '.npy'
+            },
+            'pickle': {
+                'class': Pickle,
+                'ext': '.p'
+            },
+            'json': {
+                'class': JSON,
+                'ext': '.json'
+            },
+        }
+        return protocols_dict
+
+    def save(self, data_dict):
+        dict_class = self.protocol['class']
+        dict_class(self.save_name).save(data_dict)
+        print("\n" + self.choice + " saved to " + self.save_name)
+
+    @property
+    def load(self):
+        try:
+            dict_class = self.protocol['class']
+            read_dictionary = dict_class(self.save_name).load()
+            return read_dictionary
+        except IOError:
+            print('\nError: Unable to load file ' + str(self.save_name))
+
---
+++ packages/databasetools/databasetools-0.3.13.tar.gz/databasetools-0.3.13/setup.py
@@ -0,0 +1,23 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='databasetools',
+    version='0.3.13',
+    packages=find_packages(),
+    install_requires=[
+        'looptools',
+        'mysql-connector==2.1.6',
+        'numpy',
+        'pandas',
+        'tqdm',
+        'pathlib',
+    ],
+    url='https://github.com/mrstephenneal/databasetools.git',
+    license='MIT License',
+    author='Stephen Neal',
+    author_email='stephen@stephenneal.net',
+    description='Additional functionality added to DictTools allowing for dictionaries to be saved in numpy, '
+                'json or pickle format.',
+    long_description='A collection of database tools written in Python for handling basic actions with CSV files, '
+                'numpy dictionaries, SQLite databases and MySQL databases.'
+)
