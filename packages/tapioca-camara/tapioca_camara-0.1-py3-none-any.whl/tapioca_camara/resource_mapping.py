f00e9a2a91f6afdbe572818e32b37f7b81e23d09
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:56:08 +0000

    Add package tapioca-camara/0.1/tapioca_camara-0.1-py3-none-any.whl

---
+++ packages/tapioca-camara/tapioca_camara-0.1-py3-none-any.whl/tapioca_camara/resource_mapping.py
@@ -0,0 +1,12 @@
+# coding: utf-8
+
+RESOURCE_MAPPING = {
+    'deputados': {
+        'resource': 'deputados',
+        'docs': 'https://dadosabertos.camara.leg.br/swagger/api.html#api'
+    },
+    'deputado': {
+        'resource': 'deputados/{id}',
+        'docs': 'https://dadosabertos.camara.leg.br/swagger/api.html#api'
+    },
+}
