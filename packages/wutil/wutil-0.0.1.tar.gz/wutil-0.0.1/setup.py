a3069d12da10439297eb9ce7fbf8cd2b27449fe8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:57:35 +0000

    Add package wutil/0.0.1/wutil-0.0.1.tar.gz

---
+++ packages/wutil/wutil-0.0.1.tar.gz/wutil-0.0.1/setup.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python
+import os
+import shutil
+import sys
+from setuptools import setup, find_packages
+
+
+readme = open('README.md').read()
+
+VERSION = '0.0.1'
+
+requirements = [
+    'numpy',
+    'shapely'
+]
+
+setup(
+    # Metadata
+    name='wutil',
+    version=VERSION,
+    author='Red Pandas Team',
+    author_email='param3456@gmail.com',
+    description='Various utils',
+    long_description=readme,
+    license='BSD',
+
+    # Package info
+    packages=find_packages(exclude=('test',)),
+
+    zip_safe=True,
+    install_requires=requirements,
+)
