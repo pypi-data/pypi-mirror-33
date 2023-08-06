cd666b8c4dff7aae2dd36c371b0361ee77e38058
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:25:50 +0000

    Add package smms.py/0.0.2/smms.py-0.0.2.tar.gz

---
+++ packages/smms.py/smms.py-0.0.2.tar.gz/smms.py-0.0.2/setup.py
@@ -0,0 +1,26 @@
+from setuptools import setup
+
+setup(
+    name='smms.py',
+    version='0.0.2',
+    author='Queensferry',
+    author_email='queensferry.me@gmail.com',
+    description='command line toolkit for managing images at https://sm.ms',
+    long_description=open('README.md', 'rt').read(),
+    long_description_content_type='text/markdown',
+    py_modules=['main'],
+    include_package_data=True,
+    install_requires=[
+        'Click',
+        'requests'
+    ],
+    entry_points='''
+        [console_scripts]
+        smms=main:cli
+    ''',
+    classifiers=(
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    )
+)
