f2cb8f24ab7a5a76c3116f6244bc7017d33cfd98
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:23:33 +0000

    Add package rusenttokenize/0.0.4/rusenttokenize-0.0.4.tar.gz

---
+++ packages/rusenttokenize/rusenttokenize-0.0.4.tar.gz/rusenttokenize-0.0.4/setup.py
@@ -0,0 +1,21 @@
+import setuptools
+
+with open("README.md", "r", encoding='utf8') as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="rusenttokenize",
+    version="0.0.4",
+    author="Marat Zaynutdinov",
+    author_email="tsundokum@gmail.com",
+    description="Rule-based sentence tokenizer for Russian language",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/deepmipt/ru_sentence_tokenizer",
+    packages=setuptools.find_packages(),
+    classifiers=(
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: Apache Software License",
+        "Operating System :: OS Independent",
+    ),
+)
