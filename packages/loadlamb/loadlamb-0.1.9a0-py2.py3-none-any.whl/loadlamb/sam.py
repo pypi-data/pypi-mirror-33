14168b4bedc6d2e22f51e86fb694c3c309d54bbc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:36:00 +0000

    Add package loadlamb/0.1.9a0/loadlamb-0.1.9a0-py2.py3-none-any.whl

---
+++ packages/loadlamb/loadlamb-0.1.9a0-py2.py3-none-any.whl/loadlamb/sam.py
@@ -0,0 +1,73 @@
+import sammy as sm
+
+
+
+sqs = sm.SQS(name='SQSQueue',QueueName='loadlamb')
+
+sqs_event = sm.SQSEvent(
+    name='loadlambsqs',
+    Queue=sm.Sub(Sub='arn:aws:sqs:${AWS::Region}:${AWS::AccountId}:loadlamb'),
+    BatchSize=1
+)
+
+db = sm.SimpleTable(
+    name='loadlambddb',
+    PrimaryKey={'Name':'_id','Type':'String'}
+)
+
+env = sm.Environment(Variables={
+    'DYNAMODB_TABLE':sm.Ref(Ref='loadlambddb')
+})
+
+role = sm.Role(
+    name='loadlambpolicy',
+    AssumeRolePolicyDocument={
+               "Version" : "2012-10-17",
+               "Statement": [ {
+                  "Effect": "Allow",
+                  "Principal": {
+                     "Service": [ "lambda.amazonaws.com" ]
+                  },
+                  "Action": [ "sts:AssumeRole" ]
+               } ]
+            },
+    ManagedPolicyArns=['arn:aws:iam::aws:policy/AmazonSQSFullAccess',
+                       'arn:aws:iam::aws:policy/AmazonDynamoDBFullAccess',
+                       'arn:aws:iam::aws:policy/CloudWatchLogsFullAccess'],
+    RoleName='loadlamb'
+)
+
+f = sm.Function(name='loadlambpush',
+                FunctionName='loadlamb-push',
+                CodeUri=sm.S3URI(
+                    Bucket=sm.Ref(Ref='CodeBucket'),
+                    Key=sm.Ref(Ref='CodeZipKey')),
+                Handler='loadlamb.lambdas.push_handler',
+                Runtime='python3.6',
+                Role=sm.Sub(Sub='arn:aws:iam::${AWS::AccountId}:role/loadlamb'),
+                Environment=env
+                )
+
+f2 = sm.Function(
+    name='loadlambpull',
+    FunctionName='loadlamb-pull',
+    CodeUri=sm.S3URI(
+        Bucket=sm.Ref(Ref='CodeBucket'),
+        Key=sm.Ref(Ref='CodeZipKey')
+    ),
+    Handler='loadlamb.lambdas.pull_handler',
+    Runtime='python3.6',
+    Events=[sqs_event],
+    Environment=env
+)
+
+
+
+s = sm.SAM(render_type='yaml')
+s.add_parameter(sm.Parameter(name='CodeBucket', Type='String'))
+s.add_parameter(sm.Parameter(name='CodeZipKey', Type='String'))
+s.add_resource(sqs)
+s.add_resource(f)
+s.add_resource(f2)
+s.add_resource(db)
+s.add_resource(role)
