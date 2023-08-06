ef3b6783fc6274ec1aa7703b1b185ba8a31f60da
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 05:07:20 +0000

    Add package packy-agent/0.3.7/packy-agent-0.3.7.tar.gz

---
+++ packages/packy-agent/packy-agent-0.3.7.tar.gz/packy-agent-0.3.7/packy_agent/utils/sentry.py
@@ -0,0 +1,28 @@
+from __future__ import absolute_import
+
+import raven
+
+import logging
+import packy_agent
+from packy_agent.configuration.agent.base import configuration
+
+logger = logging.getLogger(__name__)
+
+
+def get_raven_client(component_name):
+    packy_agent_sentry_dsn = configuration.get_packy_agent_sentry_dsn()
+
+    if packy_agent_sentry_dsn:
+        agent_id = configuration.get_agent_id()
+        name = '[{}] {}'.format(
+            component_name,
+            'Inactive agent' if agent_id is None else 'Agent ID: {}'.format(agent_id))
+
+        return raven.Client(
+            packy_agent_sentry_dsn,
+            name=name,
+            release=packy_agent.__version__,
+            environment=configuration.get_server_base_url() or ''
+        )
+    else:
+        logger.warning('Sentry DSN is not configured')
