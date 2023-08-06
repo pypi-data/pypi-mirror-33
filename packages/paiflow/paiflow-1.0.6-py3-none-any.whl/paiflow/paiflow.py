f4872ec9ee07c4fb4091bea195d0ab383d950539
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:43:26 +0000

    Add package paiflow/1.0.6/paiflow-1.0.6-py3-none-any.whl

---
+++ packages/paiflow/paiflow-1.0.6-py3-none-any.whl/paiflow/paiflow.py
@@ -0,0 +1,12 @@
+from .info_model import InfoModel
+
+
+class PaiFlow:
+    @staticmethod
+    def deploy(model: object, info: InfoModel):
+        '''发布模型
+        参数:
+            model: 训练好的模型
+            info: 信息
+        '''
+        pass
