753bce89eebb9f8cade1e64cd3ca8721c729c9d8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:43:15 +0000

    Add package fx-crash-sig/0.1.2/fx-crash-sig-0.1.2.tar.gz

---
+++ packages/fx-crash-sig/fx-crash-sig-0.1.2.tar.gz/fx-crash-sig-0.1.2/setup.py
@@ -0,0 +1,36 @@
+#!/usr/bin/env python
+from setuptools import setup, find_packages
+
+
+def read_file(name):
+    with open(name) as f:
+        return f.read().strip()
+
+
+install_requires = [
+    'requests',
+    'siggen',
+    'ujson',
+]
+
+setup(
+    name='fx-crash-sig',
+    version='0.1.2',
+    description=' Get crash signature from Firefox crash trace ',
+    long_description=read_file('README.md'),
+    long_description_content_type='text/markdown',
+    maintainer='Ben Wu',
+    maintainer_email='bwub124@gmail.com',
+    url='https://github.com/Ben-Wu/fx-crash-sig',
+    packages=find_packages(),
+    include_package_data=True,
+    install_requires=install_requires,
+    entry_points="""
+        [console_scripts]
+        fx-crash-sig=fx_crash_sig.cmd_get_crash_sig:cmdline
+    """,
+    classifiers=[
+        'License :: OSI Approved :: Mozilla Public License 2.0 (MPL 2.0)',
+        'Programming Language :: Python :: 2 :: Only',
+    ]
+)
