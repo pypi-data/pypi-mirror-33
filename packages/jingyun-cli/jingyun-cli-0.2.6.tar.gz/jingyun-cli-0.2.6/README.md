014d0689b16120f0d92cda961fbcc29781483a70
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 05:06:13 +0000

    Add package jingyun-cli/0.2.6/jingyun-cli-0.2.6.tar.gz

---
+++ packages/jingyun-cli/jingyun-cli-0.2.6.tar.gz/jingyun-cli-0.2.6/README.md
@@ -0,0 +1,42 @@
+# jingyun
+
+# 0.2.6
+jy-oss-download下载时如果文件存在将不进行下载
+
+# 0.2.5
+jy-supervisord
+jy-supervisorctl
+
+# 0.2.2
+add jy-jingd-usermod
+udpate jy-conf-read 添加参数 -i --ignore
+
+# 0.2.1
+add jy-conf-read
+
+## 0.1.10
+add jy-ssh-nonkey
+
+## 0.1.9
+jy-conf-format 支持遍历和筛选整个目录下的文件
+
+## 0.1.8
+jy-conf-format add mode params
+
+## 0.1.7
+add jy-conf-format
+
+## 0.1.6
+jy-key add --only-value 只打印密钥要打印的密钥中的属性值，不打印属性
+
+## 0.1.5
+fix not include jingyun_cli/key
+
+## 0.1.4
+key cli 上线 jy-key
+
+## 0.1.3
+oss cli 上线 jy-oss-download
+
+## 0.1
+json cli 上线 json-merge
---
+++ packages/jingyun-cli/jingyun-cli-0.2.6.tar.gz/jingyun-cli-0.2.6/jingyun_cli/jingd/jy_supervisor.py
@@ -0,0 +1,37 @@
+#! /usr/bin/env python
+# coding: utf-8
+
+import os
+import sys
+from supervisor.supervisorctl import main as supervisorctl_main
+from supervisor.supervisord import main as supervisord_main
+
+try:
+    from .help import g_help, error_and_exit
+except ValueError:
+    from help import g_help, error_and_exit
+
+__author__ = '鹛桑够'
+
+
+def set_conf():
+    conf_dir = os.environ.get("JINGD_CONF_DIR")
+    if conf_dir is None:
+        error_and_exit(g_help("defect_env", "JINGD_CONF_DIR"))
+    conf_path = os.path.join(conf_dir, "supervisord.conf")
+    if os.path.exists(conf_path) is False:
+        error_and_exit(g_help("file_lost"))
+    sys.argv.insert(1, "-c")
+    sys.argv.insert(2, conf_path)
+
+
+def jy_supervisorctl():
+    set_conf()
+    code = supervisorctl_main()
+    sys.exit(code)
+
+
+def jy_supervisord():
+    set_conf()
+    code = supervisord_main()
+    sys.exit(code)
