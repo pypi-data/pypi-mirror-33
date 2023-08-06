f78f9bb549b5c6c1fffc5e033e379d63d7cc0b69
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:49:09 +0000

    Add package dronedirector/0.0.1/dronedirector-0.0.1.tar.gz

---
+++ packages/dronedirector/dronedirector-0.0.1.tar.gz/dronedirector-0.0.1/dronedirector/_version.py
@@ -0,0 +1,5 @@
+# -*- coding: utf-8 -*-
+# Copyright 2018 Alex Marchenko
+# Distributed under the terms of the Apache License 2.0
+version_info = (0, 0, 1)
+__version__ = ".".join(map(str, version_info))
