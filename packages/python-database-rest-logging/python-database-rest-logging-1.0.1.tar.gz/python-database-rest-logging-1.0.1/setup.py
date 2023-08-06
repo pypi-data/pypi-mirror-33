3fa66b7e5abf5ddc9c0268168f606617807e3f97
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:34:46 +0000

    Add package python-database-rest-logging/1.0.1/python-database-rest-logging-1.0.1.tar.gz

---
+++ packages/python-database-rest-logging/python-database-rest-logging-1.0.1.tar.gz/python-database-rest-logging-1.0.1/setup.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+import setuptools
+from distutils.core import setup
+
+setup(name='python-database-rest-logging',
+      version='1.0.1',
+      description='Python database rest logging',
+      author='Marco Mendao',
+      author_email='mac.mendao@gmail.com',
+      url='http://marcomendao.betacode.tech',
+      packages=['python_database_rest_logging','python_database_rest_logging/api','python_database_rest_logging/model','python_database_rest_logging/services']
+)
