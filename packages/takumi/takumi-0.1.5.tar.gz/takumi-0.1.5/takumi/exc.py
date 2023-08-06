d556e5101b7871e849e77441c591b0fb2175e968
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:34:47 +0000

    Add package takumi/0.1.5/takumi-0.1.5.tar.gz

---
+++ packages/takumi/takumi-0.1.5.tar.gz/takumi-0.1.5/takumi/exc.py
@@ -0,0 +1,28 @@
+# -*- coding: utf-8 -*-
+
+"""
+takumi.exc
+~~~~~~~~~~
+
+Takumi related Exceptin definitions.
+"""
+
+
+class TakumiException(Exception):
+    """Base class for all Takumi exceptions
+    """
+
+
+class CloseConnectionError(TakumiException):
+    """Exception for closing client connection
+    """
+
+
+class TimeoutException(TakumiException):
+    """Raised when api call timeout
+    """
+    def __init__(self, timeout):
+        self.timeout = timeout
+
+    def __str__(self):
+        return 'Timeout after {} seconds'.format(self.timeout)
