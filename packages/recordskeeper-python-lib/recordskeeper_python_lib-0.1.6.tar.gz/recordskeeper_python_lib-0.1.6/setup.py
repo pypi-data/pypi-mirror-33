5fb89bf4b0b1ff82aff8f8615f3db9629da697e0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 05:06:39 +0000

    Add package recordskeeper-python-lib/0.1.6/recordskeeper_python_lib-0.1.6.tar.gz

---
+++ packages/recordskeeper-python-lib/recordskeeper_python_lib-0.1.6.tar.gz/recordskeeper_python_lib-0.1.6/setup.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python
+
+from setuptools import setup, find_packages
+import os
+import codecs
+
+here = os.path.abspath(os.path.dirname(__file__))
+
+with open(os.path.join(here, 'README.md')) as f:
+    README = f.read()
+
+requires = ["requests", "green", "pyyaml"]
+
+setup(
+      name='recordskeeper_python_lib',
+      version='0.1.6',
+      description='RecordsKeeper Python library',
+      long_description=README,
+      long_description_content_type='text/markdown',
+      classifiers=[
+          "Programming Language :: Python"
+      ],
+      url='https://github.com/RecordsKeeper/recordskeeper-python-sdk',
+      keywords='recordskeeper, python, library',
+      packages=find_packages(),
+      zip_safe=False,
+      install_requires=requires,
+      test_suite="test"
+     )
