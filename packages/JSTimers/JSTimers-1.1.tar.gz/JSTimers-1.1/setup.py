411e36737fc5829707861c89852cd463d5ee087b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:23:35 +0000

    Add package JSTimers/1.1/JSTimers-1.1.tar.gz

---
+++ packages/JSTimers/JSTimers-1.1.tar.gz/JSTimers-1.1/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup, find_packages
+setup(name='JSTimers',
+      version='1.1',
+      description='Javascript Timer functions for Python',
+      author='Shashwat Chandra',
+      author_email='chandras.2013@iitkalumni.org',
+      url='https://github.com/Shashwat986/jstimers',
+      packages=find_packages()
+      )
