73e827d12b4dc93e95a4a9a129b2048fc0e698c8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:50:15 +0000

    Add package nester20180626/1.0.0/nester20180626-1.0.0.tar.gz

---
+++ packages/nester20180626/nester20180626-1.0.0.tar.gz/nester20180626-1.0.0/setup.py
@@ -0,0 +1,16 @@
+from distutils.core import setup
+
+setup(
+    name = 'nester20180626',
+    version = '1.0.0',
+    py_modules = ['nester'],
+    author = 'typy',
+    author_email= 'enternaltin@163.com',
+    url='https://github.com/Yelena2015/nester',
+    #packages=['nester'],
+    #include_package_data=True,
+    #install_requires=['requests>=2.0','httplib2>=0.9'],
+    #license='MIT License',
+    #zip_safe=False
+) 
+
