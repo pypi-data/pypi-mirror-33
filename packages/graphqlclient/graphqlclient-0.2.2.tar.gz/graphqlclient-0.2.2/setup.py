fc3969c488cfd160c72570d3879cdc0906a0453a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:08:34 +0000

    Add package graphqlclient/0.2.2/graphqlclient-0.2.2.tar.gz

---
+++ packages/graphqlclient/graphqlclient-0.2.2.tar.gz/graphqlclient-0.2.2/setup.py
@@ -0,0 +1,19 @@
+from setuptools import setup
+
+with open('README.md', 'r') as fh:
+    long_description = fh.read()
+
+setup(name='graphqlclient',
+      version='0.2.2',
+      description='Simple GraphQL client for Python 2.7+',
+      long_description=long_description,
+      long_description_content_type='text/markdown',
+      url='https://github.com/prismagraphql/python-graphql-client',
+      author='prisma.io',
+      author_email='hello@prisma.io',
+      license='MIT',
+      packages=['graphqlclient'],
+      install_requires=[
+          'six',
+      ],
+      zip_safe=False)
