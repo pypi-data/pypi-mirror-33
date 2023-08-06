a5befc31750c02e243f967e161647fffb737b9a0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:07:41 +0000

    Add package filechex/0.1.0/filechex-0.1.0.tar.gz

---
+++ packages/filechex/filechex-0.1.0.tar.gz/filechex-0.1.0/setup.py
@@ -0,0 +1,25 @@
+import setuptools
+
+setuptools.setup(
+    name="filechex",
+    version="0.1.0",
+    url="https://github.com/cw-andrews/cookiecutter-pypackage-minimal",
+
+    author="cw-andrews",
+    author_email="cwandrews.oh@gmail.com",
+
+    description=("A utility package designed to compare files to determine "
+                 "whether source files should be processed."),
+    long_description=open('README.md').read(),
+
+    packages=setuptools.find_packages(),
+
+    install_requires=[],
+
+    classifiers=[
+        'Development Status :: 2 - Pre-Alpha',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7'
+    ],
+)
