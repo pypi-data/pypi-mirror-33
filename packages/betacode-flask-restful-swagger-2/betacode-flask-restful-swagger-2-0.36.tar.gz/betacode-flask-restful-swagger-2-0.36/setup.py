e42441cf4f854bd09c35e5049dd182c8cebf00e9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:45:49 +0000

    Add package betacode-flask-restful-swagger-2/0.36/betacode-flask-restful-swagger-2-0.36.tar.gz

---
+++ packages/betacode-flask-restful-swagger-2/betacode-flask-restful-swagger-2-0.36.tar.gz/betacode-flask-restful-swagger-2-0.36/setup.py
@@ -0,0 +1,25 @@
+try:
+    from setuptools import setup
+except ImportError:
+    from distutils.core import setup
+
+with open('README') as file:
+    long_description = file.read()
+
+setup(name='betacode-flask-restful-swagger-2',
+      version='0.36',
+      url='https://github.com/swege/flask-restful-swagger-2.0',
+      zip_safe=False,
+      packages=['betacode_flask_restful_swagger_2'],
+      package_data={
+        'betacode_flask_restful_swagger_2': [
+        ]
+      },
+      description='Extract swagger specs from your flask-restful project. Project based on flask-restful-swagger by Ran Tavory.',
+      author='Soeren Wegener',
+      license='MIT',
+      long_description=long_description,
+      install_requires=['Flask-RESTful>=0.2.12'],
+      tests_require=['nose'],
+      test_suite='nose.collector'
+      )
