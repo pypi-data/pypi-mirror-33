9fd90b8371b19948c3111fe801233cdf128e22b8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:48:14 +0000

    Add package a569/1.3.0/a569-1.3.0.tar.gz

---
+++ packages/a569/a569-1.3.0.tar.gz/a569-1.3.0/setup.py
@@ -0,0 +1,12 @@
+
+from distutils.core import setup
+
+setup(
+    name = 'a569',
+    version = '1.3.0',
+    py_modules = ['a569'],
+    author = 'hfpython',
+    author_email = 'notachance@abc.com',
+    description = 'A simple primer of nested lists'
+)
+    
