5395b9f3e9da7f7018dcf8745cf7f27602af3750
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:09:13 +0000

    Add package dlhub-client/0.0.1/dlhub_client-0.0.1-py3-none-any.whl

---
+++ packages/dlhub-client/dlhub_client-0.0.1-py3-none-any.whl/dlhub_client/client.py
@@ -0,0 +1,29 @@
+import requests
+import pandas as pd
+import ipywidgets
+
+class DLHub():
+    service = "http://dlhub.org:5000/api/v1"
+    
+    def __init__(self):
+        pass
+    
+    def get_servables(self):
+        r = requests.get("{service}/servables".format(service=self.service))
+        return pd.DataFrame(r.json())
+    
+    def get_id_by_name(self, name):
+        r = requests.get("{service}/servables".format(service=self.service))
+        df_tmp =  pd.DataFrame(r.json())
+        serv = df_tmp[df_tmp.name==name]
+        return serv.iloc[0]['uuid']
+    
+    def run(self, servable_id, data):
+        servable_path = '{service}/servables/{servable_id}/run'.format(service=self.service,
+                                                                       servable_id=servable_id)
+        payload = {"data":data}
+
+        r = requests.post(servable_path, json=data)
+        if r.status_code is not 200:
+            raise Exception(r)
+        return pd.DataFrame(r.json())
