9dc93cdb989b6f5f588b33d00ee28577ba9f43c8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:17:50 +0000

    Add package s3-deploy-website/0.2.2/s3-deploy-website-0.2.2.tar.gz

---
+++ packages/s3-deploy-website/s3-deploy-website-0.2.2.tar.gz/s3-deploy-website-0.2.2/setup.py
@@ -0,0 +1,45 @@
+#!/usr/bin/env python
+
+from setuptools import setup, find_packages
+
+
+# Read long description
+with open('README.rst') as f:
+    long_description = f.read()
+
+setup(
+    name='s3-deploy-website',
+    version='0.2.2',
+    license='MIT',
+    author='Jon Lund Steffensen',
+    author_email='jonlst@gmail.com',
+
+    description='S3 website deployment tool',
+    long_description=long_description,
+
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+        'Topic :: Internet :: WWW/HTTP :: Site Management',
+    ],
+
+    packages=find_packages(),
+    entry_points={
+        'console_scripts': [
+            's3-deploy-website = s3_deploy.deploy:main'
+        ]
+    },
+    install_requires=[
+        'boto3', 'PyYAML', 'six'
+    ],
+    test_suite='s3_deploy.tests',
+    tests_require=[
+        'mock',
+        'moto',
+    ],
+)
