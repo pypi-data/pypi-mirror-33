2e5edde6bb427864c30b5543221aa13e81bc3153
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:14:37 +0000

    Add package amshorelineone/1.1/amshorelineone-1.1.tar.gz

---
+++ packages/amshorelineone/amshorelineone-1.1.tar.gz/amshorelineone-1.1/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+from setuptools import find_packages
+
+setup(name='amshorelineone',
+      version='1.1',
+      url='https://github//',
+      description='hiahia',
+      author='amshoreline',
+      author_email='371350508@qq.com',
+      packages=find_packages(),
+    )
