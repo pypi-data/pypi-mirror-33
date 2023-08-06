dbc4b880eb54d2f2c20774ef13ec7448d323b05e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 05:08:35 +0000

    Add package q2-itsxpress/1.3/q2_itsxpress-1.3.tar.gz

---
+++ packages/q2-itsxpress/q2_itsxpress-1.3.tar.gz/q2_itsxpress-1.3/setup.py
@@ -0,0 +1,22 @@
+from setuptools import setup
+
+setup(
+    name='q2_itsxpress',
+    version='1.3',
+    packages=['q2_itsxpress'],
+    author='Kyle Weber',
+    author_email='kweber1@ufl.edu',
+    description="itsxpress qiime2 plugin",
+    url='https://github.com/kweber1/q2_itsxpress',
+    test_suite ='nose.collector',
+
+    install_requires=[
+        'itsxpress'
+
+    ],
+    entry_points={
+        'qiime2.plugins':['q2_itsxpress=q2_itsxpress.plugin_setup:plugin']
+    },
+    zip_safe=False
+)
+
