2af235e70d4200b135fd65d5a5e802f5331c8c4d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:13:31 +0000

    Add package rescape-graphene/0.2.6/rescape_graphene-0.2.6-py3-none-any.whl

---
+++ packages/rescape-graphene/rescape_graphene-0.2.6-py3-none-any.whl/rescape_graphene/helpers/test_ramda.py
@@ -0,0 +1,30 @@
+from snapshottest import TestCase
+from ..functional import ramda as R
+
+
+class TestRamda(TestCase):
+
+    def test_filter_dict(self):
+        dct = R.filter_dict(lambda keyvalue: keyvalue[0] == 'a', dict(a=1, b=2))
+        assert dct == dict(a=1)
+
+    def test_map_prop_value_as_index(self):
+        res = R.map_prop_value_as_index(
+            'province',
+            [
+                dict(province="Alberta"),
+                dict(province="Manitoba")
+            ]
+        )
+        assert res == dict(Alberta=dict(province="Alberta"), Manitoba=dict(province="Manitoba"))
+
+    def test_item_path_or(self):
+        assert R.item_path_or('racehorse', ['one', 'one', 'was'], dict(one=dict(one=dict(was='a')))) == 'a'
+        assert R.item_path_or('racehorse', ['one', 'one', 'is'], dict(one=dict(one=dict(was='a')))) == 'racehorse'
+        assert R.item_path_or('racehorse', 'one.one.was', dict(one=dict(one=dict(was='a')))) == 'a'
+
+        # Try with a mix of dict and obj
+        class Fellow(object):
+            def __init__(self, one):
+                self.one = one
+        assert R.item_path_or('racehorse', 'one.one.was', dict(one=Fellow(one=dict(was='a')))) == 'a'
