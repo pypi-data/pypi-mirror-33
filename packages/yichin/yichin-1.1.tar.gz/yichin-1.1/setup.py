4959c780de3c79420509b9413c865cb5b9c422d4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:18:34 +0000

    Add package yichin/1.1/yichin-1.1.tar.gz

---
+++ packages/yichin/yichin-1.1.tar.gz/yichin-1.1/setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+setup(
+  name='yichin',
+  packages=['yichin'],
+  version='1.1',
+  description='yichin encode/decode',
+  author='Jellyfish',
+  author_email='turtle20953809@gmail.com',
+  url='https://github.com/killua4564/yichin/',
+  keywords=['yichin', 'encode', 'decode', 'whitespace'],
+  classifiers=[]
+)
