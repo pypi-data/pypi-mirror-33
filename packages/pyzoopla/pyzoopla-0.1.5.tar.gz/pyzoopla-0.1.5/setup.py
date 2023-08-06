1ee349cf6406e8536f2c3bf9cda08a3c8c8651cf
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 05:07:09 +0000

    Add package pyzoopla/0.1.5/pyzoopla-0.1.5.tar.gz

---
+++ packages/pyzoopla/pyzoopla-0.1.5.tar.gz/pyzoopla-0.1.5/setup.py
@@ -0,0 +1,32 @@
+import setuptools
+
+
+with open('README.md', 'r') as f:
+    long_description = f.read()
+
+setuptools.setup(
+    name='pyzoopla',
+    version='0.1.5',
+    author='Imran Khan',
+    author_email='imrankhan17@hotmail.co.uk',
+    description='A Python package to access information about properties from Zoopla',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    url='https://github.com/imrankhan17/pyzoopla',
+    packages=setuptools.find_packages(),
+    license='MIT',
+    install_requires=[
+        'beautifulsoup4',
+        'bs4',
+        'lxml',
+        'numpy',
+        'pandas',
+        'pymysql'
+        'requests'
+    ],
+    classifiers=(
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+    ),
+)
