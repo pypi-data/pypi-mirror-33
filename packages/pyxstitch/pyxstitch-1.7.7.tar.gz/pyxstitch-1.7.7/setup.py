cbb389b9ca223f81c5615af71874d6e4b867ea0b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:20:52 +0000

    Add package pyxstitch/1.7.7/pyxstitch-1.7.7.tar.gz

---
+++ packages/pyxstitch/pyxstitch-1.7.7.tar.gz/pyxstitch-1.7.7/setup.py
@@ -0,0 +1,34 @@
+#!/usb/bin/python
+
+"""Setup for pyxstitch."""
+
+from setuptools import setup, find_packages
+import os
+
+__pkg_name__ = "pyxstitch"
+with open(os.path.join(__pkg_name__, "version.py")) as v_file:
+    exec(v_file.read())
+
+long_description = ""
+with open("README.rst", 'r') as f:
+    long_description = f.read()
+
+setup(
+    author="Sean Enck",
+    author_email="enckse@gmail.com",
+    name=__pkg_name__,
+    version=__version__,
+    description='Convert source code to cross stitch patterns',
+    long_description=long_description,
+    url='https://github.com/enckse/pyxstitch',
+    license='MIT',
+    python_requires='>=3.5',
+    packages=[__pkg_name__, __pkg_name__ + ".fonts"],
+    install_requires=['pygments', 'Pillow'],
+    keywords='crossstitch cross stitch',
+    entry_points={
+        'console_scripts': [
+            'pyxstitch = pyxstitch.pyxstitch:main',
+        ],
+    },
+)
