bbccfabcacdb2d37b0ec736448a0ec5598d87f9f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:35:06 +0000

    Add package merkyl/1.0.7/merkyl-1.0.7.tar.gz

---
+++ packages/merkyl/merkyl-1.0.7.tar.gz/merkyl-1.0.7/setup.py
@@ -0,0 +1,22 @@
+from setuptools import setup
+
+setup(
+    name="merkyl",
+    version='1.0.7',
+    description="Log file tailer with bottle",
+    author="Pete Savage",
+    keywords=["tail", "linux", "bottle"],
+    license="MIT",
+    classifiers=[
+        "Programming Language :: Python",
+        "Development Status :: 4 - Beta",
+        "Operating System :: POSIX :: Linux",
+        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python :: 2.7",],
+    packages=['merkyl'],
+    entry_points={'console_scripts':
+                  ['merkyl = merkyl:main']},
+    install_requires=['bottle'],
+    include_package_data=True,
+    url="https://github.com/psav/merkyl",
+)
