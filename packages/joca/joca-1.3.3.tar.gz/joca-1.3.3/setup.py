267b28efa114762530250b3ae3f2f5ae6fe2d134
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:45:12 +0000

    Add package joca/1.3.3/joca-1.3.3.tar.gz

---
+++ packages/joca/joca-1.3.3.tar.gz/joca-1.3.3/setup.py
@@ -0,0 +1,38 @@
+#!/usr/bin/env python
+"""Setup package"""
+
+import setuptools
+
+try:
+    import pypandoc
+    long_description = pypandoc.convert('README.md', 'rst')
+except(IOError, ImportError):
+    long_description = open('README.md').read()
+
+setuptools.setup(
+    name="joca",
+    version="1.3.3",
+    author="Bryce McNab",
+    author_email="brycemcnab@pm.me",
+    description="Sync project lead with ical (for on call assignees)",
+    long_description=long_description,
+    url="https://www,github.com/betsythefc/joca",
+    packages=setuptools.find_packages(),
+    include_package_data=True,
+    classifiers=[
+        "Programming Language :: Python :: 2.7",
+        "Operating System :: OS Independent",
+        "Development Status :: 5 - Production/Stable",
+        "License :: OSI Approved :: GNU General Public License v3 (GPLv3)",
+        "Topic :: Software Development :: Version Control :: Git",
+    ],
+    scripts=[
+        'bin/joca',
+    ],
+    install_requires=[
+        "requests",
+        "jira",
+        "icalendar",
+        "jsonschema",
+    ]
+) 
