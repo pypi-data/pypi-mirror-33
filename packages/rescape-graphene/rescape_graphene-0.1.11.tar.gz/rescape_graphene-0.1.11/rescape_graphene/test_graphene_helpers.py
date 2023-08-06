d08482452cb69d12e6011eb49301fba2fbc420db
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 07:12:41 +0000

    Add package rescape-graphene/0.1.11/rescape_graphene-0.1.11.tar.gz

---
+++ packages/rescape-graphene/rescape_graphene-0.1.11.tar.gz/rescape_graphene-0.1.11/rescape_graphene/test_graphene_helpers.py
@@ -0,0 +1,23 @@
+from graphene_helpers import quote
+from snapshottest import TestCase
+from ramda import *
+
+
+class TestGrapheneHelpers(TestCase):
+
+    def test_quote_unless_number(self):
+        str = quote(dict(
+            a=1,
+            b='a',
+            c=dict(
+                d=1,
+                e='a'
+            ),
+            f=[
+                'a',
+                'b',
+                3.1
+            ]
+        ))
+        print(str)
+        assert str == 'a: 1\nb: "a"\nc:\nd: 1\ne: "a"\nf: ["a"\n"b"\n3.1]'
