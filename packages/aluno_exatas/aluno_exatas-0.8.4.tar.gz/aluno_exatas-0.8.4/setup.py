541946523f40555f5113f560ce7862486dd060a7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 05:07:37 +0000

    Add package aluno_exatas/0.8.4/aluno_exatas-0.8.4.tar.gz

---
+++ packages/aluno_exatas/aluno_exatas-0.8.4.tar.gz/aluno_exatas-0.8.4/setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup
+
+setup(
+    name='aluno_exatas',
+    version='0.8.4',
+    packages=['aluno_exatas'],
+    install_requires = ['numpy', 'sympy'],
+    license='MIT License',
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
+   	url='https://github.com/ericonr/aluno_exatas',
+    author='EricoNR',
+    author_email='e170610@g.unicamp.br',
+)
