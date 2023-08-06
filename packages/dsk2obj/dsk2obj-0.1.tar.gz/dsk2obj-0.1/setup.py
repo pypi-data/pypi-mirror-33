6416f233f5da5d86d90738d0023ffc850e44b482
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:57:17 +0000

    Add package dsk2obj/0.1/dsk2obj-0.1.tar.gz

---
+++ packages/dsk2obj/dsk2obj-0.1.tar.gz/dsk2obj-0.1/setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(name='dsk2obj',
+    version='0.1',
+    description='generates an obj file from a bds file (dsk kernel) and dsiplays it',
+    author='Alfredo Escalante',
+    author_email='alfredoescalante95@gmail.com',
+    # license='GPL',
+    packages=[],
+    install_requires=['spiceypy','numpy','pygame','OpenGL','sys','os'],
+    python_requires='>=3',
+)
