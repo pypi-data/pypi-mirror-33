365d53b7c71bec5a5d36bc9190f5975e2ddb8d6d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 07:13:05 +0000

    Add package autoloader/0.0.1/autoloader-0.0.1.tar.gz

---
+++ packages/autoloader/autoloader-0.0.1.tar.gz/autoloader-0.0.1/autoloader/exceptions.py
@@ -0,0 +1,1 @@
+class InvalidAutoloadPath(Exception): pass
---
+++ packages/autoloader/autoloader-0.0.1.tar.gz/autoloader-0.0.1/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name="autoloader",
+    version='0.0.1',
+    packages=[
+        'autoloader',
+    ],
+    install_requires=[],
+    include_package_data=True,
+)
