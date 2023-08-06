f0757f8a8fcfcb26c8a6cd8a9e1edce9a0449c69
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:09:38 +0000

    Add package alas-ce0-client/0.105/alas-ce0-client-0.105.tar.gz

---
+++ packages/alas-ce0-client/alas-ce0-client-0.105.tar.gz/alas-ce0-client-0.105/alas_ce0/management/notification.py
@@ -0,0 +1,24 @@
+from enum import Enum
+
+from alas_ce0.common.client_base import ApiClientBase
+
+
+class NotificationMessageType(Enum):
+    Success = 1
+    Warning = 2
+    Failed = 3
+    System = 4
+    Processing = 5
+
+
+class NotificationClient(ApiClientBase):
+    entity_endpoint_base_url = '/management/notifications/'
+
+    def notify_user(self, resource_name, params=None):
+        if params is None:
+            params = dict()
+
+        return self.http_post_json(self.entity_endpoint_base_url + 'user/' + resource_name, params)
+
+    def notify_message(self, params):
+        return self.http_post_json(self.entity_endpoint_base_url + 'message', params)
