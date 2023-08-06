dd658fcd5198a69c750eebb01abd2f17649aaf23
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:52:09 +0000

    Add package sshas/1.0/sshas-1.0.tar.gz

---
+++ packages/sshas/sshas-1.0.tar.gz/sshas-1.0/README.md
@@ -0,0 +1,24 @@
+# sshas
+
+SSH config script
+
+## Install
+
+```
+export PATH=$PATH:$HOME/.local/bin
+python setup.py install --user
+```
+
+## Usage
+
+```
+# Help
+sshas -h
+
+sshas list
+sshas cb    # copy to clipboard before
+sshas add
+sshas mod
+sshas del
+```
+
---
+++ packages/sshas/sshas-1.0.tar.gz/sshas-1.0/setup.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+from distutils.core import setup
+
+
+setup(
+    name='sshas',
+    version='1.0',
+    description='sshas alias: ~/.ssh/config handling script',
+    author='jongwony',
+    author_email='lastone9182@gmail.com',
+    scripts=['sshas'],
+    url='https://github.com/jongwony/sshas',
+)
