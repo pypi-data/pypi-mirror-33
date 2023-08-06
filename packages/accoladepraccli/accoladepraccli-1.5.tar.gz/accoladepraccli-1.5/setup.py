e1dfccadad853c50712448233a909a31db5b8aaf
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:17:36 +0000

    Add package accoladepraccli/1.5/accoladepraccli-1.5.tar.gz

---
+++ packages/accoladepraccli/accoladepraccli-1.5.tar.gz/accoladepraccli-1.5/setup.py
@@ -0,0 +1,4 @@
+from setuptools import setup
+
+setup(name='accoladepraccli', version='1.5', packages = ['cliparser'], author= 'J.V.', description= 'Accolade CLI'
+      , scripts=['cliparser/sa.py'])
