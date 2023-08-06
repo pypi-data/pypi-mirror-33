72cca9a0891f29e1dafb5a042ff62a27d86a4117
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 05:06:30 +0000

    Add package imrt-qa-data-miner/0.1.6/imrt-qa-data-miner-0.1.6.tar.gz

---
+++ packages/imrt-qa-data-miner/imrt-qa-data-miner-0.1.6.tar.gz/imrt-qa-data-miner-0.1.6/README.md
@@ -0,0 +1,18 @@
+# IMRT-QA-Data-Miner
+Scans a directory for SNC Patient IMRT QA results.
+
+
+### Install
+~~~~
+$ pip install imrt-qa-data-miner
+~~~~
+
+
+### How to run
+~~~~
+$ imrt-qa-data-miner <initial-scan-dir> <optional-output-filename>
+~~~~
+
+### Notes
+This script was written specifically for SNC Patient, but I'd be happy to include support for other vendors 
+if someone could provide some anonymized example reports.
---
+++ packages/imrt-qa-data-miner/imrt-qa-data-miner-0.1.6.tar.gz/imrt-qa-data-miner-0.1.6/src/main.py
@@ -0,0 +1,74 @@
+#!/usr/bin/env python2
+# -*- coding: utf-8 -*-
+"""
+main program for IMRT QA PDF report parser
+Created on Wed Apr 18 2018
+@author: Dan Cutright, PhD
+"""
+
+from __future__ import print_function
+from qa_parser import pdf_to_qa_result
+import os
+import sys
+from datetime import datetime
+
+
+def process_data(init_directory, results_file):
+
+    with open(results_file, "w") as csv:
+        columns = ['Patient Name',
+                   'Patient ID',
+                   'Plan Date',
+                   'Dose Type',
+                   'Difference (%)',
+                   'Distance(mm)',
+                   'Threshold (%)',
+                   'Meas Uncertainty',
+                   'Analysis Type',
+                   'Total Points',
+                   'Passed',
+                   'Failed',
+                   '% Passed\n']
+        csv.write(','.join(columns))
+
+    for dirName, subdirList, fileList in os.walk(init_directory):
+        for fname in fileList:
+            if fname.find('.pdf') > -1:
+                try:
+                    row = pdf_to_qa_result(os.path.join(dirName, fname))
+                    if row:
+                        with open(results_file, "a") as csv:
+                            csv.write(row + '\n')
+                        print("Processed: %s" % os.path.join(dirName, fname))
+                    else:
+                        print("Non-compatible PDF detected: %s" % os.path.join(dirName, fname))
+                except:
+                    print("Non-compatible PDF detected: %s" % os.path.join(dirName, fname))
+
+
+def main():
+
+    if len(sys.argv) > 3:
+        print("Too many arguments provided.")
+        return
+
+    if len(sys.argv) < 2:
+        print("Please include an initial directory for scanning when calling.")
+        return
+
+    if not os.path.isdir(sys.argv[1]):
+        print("Invalid directory: %s" % sys.argv[1])
+        return
+
+    init_directory = sys.argv[1]
+
+    if len(sys.argv) == 3:
+        output_file = sys.argv[2]
+    else:
+        output_file = "results_%s.txt" % str(datetime.now()).replace(':', '-').replace('.', '-')
+
+    process_data(init_directory, output_file)
+
+
+if __name__ == '__main__':
+    main()
