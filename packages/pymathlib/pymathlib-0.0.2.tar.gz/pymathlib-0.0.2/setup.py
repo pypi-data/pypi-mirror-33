81dd618448408075bc40c03555f879d5c66aafa0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:36:01 +0000

    Add package pymathlib/0.0.2/pymathlib-0.0.2.tar.gz

---
+++ packages/pymathlib/pymathlib-0.0.2.tar.gz/pymathlib-0.0.2/setup.py
@@ -0,0 +1,13 @@
+from distutils.core import setup
+setup(
+  name = 'pymathlib',
+  packages = ['pymathlib'], # this must be the same as the name above
+  version = '0.0.2',
+  description = 'A simple python lib',
+  author = 'Kenny Zhou',
+  author_email = 'dev.heyi@gmail.com',
+  url = 'https://github.com/kenny-heyi/pymathlib',
+  download_url = 'https://github.com/kenny-heyi/pymathlib/tarball/0.0.2',
+  keywords = ['add', 'sub', 'tests'], 
+  classifiers = []
+)
