ac48c1de5cacc88c51b3d7df507864961d023794
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 07:12:45 +0000

    Add package aio-kong/0.2.5/aio-kong-0.2.5.tar.gz

---
+++ packages/aio-kong/aio-kong-0.2.5.tar.gz/aio-kong-0.2.5/tests/test_cli.py
@@ -0,0 +1,31 @@
+from click.testing import CliRunner
+
+from kong import __version__
+from kong.cli import kong
+
+
+def test_empty():
+    runner = CliRunner()
+    result = runner.invoke(kong, [])
+    assert result.exit_code == 0
+    assert result.output.startswith('Usage: kong [OPTIONS]')
+
+
+def test_version():
+    runner = CliRunner()
+    result = runner.invoke(kong, ['--version'])
+    assert result.exit_code == 0
+    assert result.output.rstrip() == __version__
+
+
+def test_plugins():
+    runner = CliRunner()
+    result = runner.invoke(kong, ['--yaml', 'tests/test4.yaml'])
+    assert result.exit_code == 0
+
+
+def test_bad_config():
+    runner = CliRunner()
+    result = runner.invoke(kong, ['--yaml', 'tests/test5.yaml'])
+    assert result.exit_code == 1
+    assert result.output.strip() == 'Error: Plugin name not specified'
