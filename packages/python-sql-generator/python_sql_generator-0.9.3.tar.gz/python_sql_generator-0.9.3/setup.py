3335cef9773f99de252dddeeb0ba07345a073db0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:53:08 +0000

    Add package python-sql-generator/0.9.3/python_sql_generator-0.9.3.tar.gz

---
+++ packages/python-sql-generator/python_sql_generator-0.9.3.tar.gz/python_sql_generator-0.9.3/setup.py
@@ -0,0 +1,23 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="python_sql_generator",
+    version="0.9.3",
+    scripts=['python_sql_generator.py'],
+    author="Tim Muzzin",
+    author_email="timmuzzin@gmail.com",
+    description="Simple Package for generating SQL from Dictionary Objects.",
+    long_description="Simple Package for generating SQL from Dictionary \
+        Objects. Helpful when dealing with large tables with lots of fields.",
+    long_description_content_type="text/markdown",
+    url="https://github.com/tmuzzin/PythonSQLGenerator",
+    packages=setuptools.find_packages(),
+    classifiers=(
+        "Programming Language :: Python :: 2.7",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ),
+)
