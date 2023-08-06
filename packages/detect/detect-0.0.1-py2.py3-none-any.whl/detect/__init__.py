293584ccf0c829df080a9514c2f4a5c108dcf0d3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:23:02 +0000

    Add package detect/0.0.1/detect-0.0.1-py2.py3-none-any.whl

---
+++ packages/detect/detect-0.0.1-py2.py3-none-any.whl/detect/__init__.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python
+import sys
+
+# os
+freebsd = "freebsd".lower() in sys.platform.lower()
+linux = "linux".lower() in sys.platform.lower()
+mac = "darwin".lower() in sys.platform.lower()
+windows = "win32".lower() in sys.platform.lower()
+cygwin = "cygwin".lower() in sys.platform.lower()
+unix = not windows and not cygwin
+
+# python versions
+py2 = sys.version_info[0] == 2
+py3 = sys.version_info[0] == 3
+CI = sys.version_info[0] == 3
