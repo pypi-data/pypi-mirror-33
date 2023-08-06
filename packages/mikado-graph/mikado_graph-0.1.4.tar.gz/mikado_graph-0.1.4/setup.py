4275ddf57c15e9bdf524535a8354a84f4fad811f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:19:28 +0000

    Add package mikado-graph/0.1.4/mikado_graph-0.1.4.tar.gz

---
+++ packages/mikado-graph/mikado_graph-0.1.4.tar.gz/mikado_graph-0.1.4/setup.py
@@ -0,0 +1,30 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="mikado_graph",
+    version="0.1.4",
+    author="Salah Missri",
+    author_email="syrianspock@gmail.com",
+    description="Python tool to draw graphs for Mikado refactoring",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/syrianspock/mikado-graph",
+    packages=setuptools.find_packages(),
+    install_requires=[
+        "graphviz",
+        "watchdog",
+    ],
+    entry_points={
+        'console_scripts': [
+            'mikado = mikado_graph.mikado_graph:main',
+        ],
+    },
+    classifiers=(
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ),
+)
