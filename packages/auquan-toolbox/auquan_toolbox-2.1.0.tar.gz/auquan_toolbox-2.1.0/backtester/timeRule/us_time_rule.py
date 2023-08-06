90b97123849b55816ff17c6b802b511e5d6e5630
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 07:13:28 +0000

    Add package auquan-toolbox/2.1.0/auquan_toolbox-2.1.0.tar.gz

---
+++ packages/auquan-toolbox/auquan_toolbox-2.1.0.tar.gz/auquan_toolbox-2.1.0/backtester/timeRule/us_time_rule.py
@@ -0,0 +1,12 @@
+from backtester.timeRule.custom_time_rule import CustomTimeRule
+from datetime import datetime, timedelta
+import pandas as pd
+from pandas.tseries.offsets import CustomBusinessHour
+from pandas.tseries.offsets import CustomBusinessDay
+from pandas.tseries.holiday import USFederalHolidayCalendar
+import os
+
+
+class USTimeRule(CustomTimeRule):
+    def __init__(self, startDate, endDate, frequency='H', sample='1'):
+        super(USTimeRule, self).__init__(startDate = startDate, endDate = endDate, calendar = USFederalHolidayCalendar(), frequency = frequency, sample = sample)
