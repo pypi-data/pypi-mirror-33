9dfea3d660ef2aa495f2af471c586c0ba2ef8a78
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:22:10 +0000

    Add package x-mroy-1047/0.0.9/x-mroy-1047-0.0.9.tar.gz

---
+++ packages/x-mroy-1047/x-mroy-1047-0.0.9.tar.gz/x-mroy-1047-0.0.9/setup.py
@@ -0,0 +1,18 @@
+from setuptools import setup, find_packages
+
+
+setup(name='x-mroy-1047',
+    version='0.0.9',
+    description='a anayzer package',
+    url='https://github.com/Qingluan/.git',
+    author='Qing luan',
+    license='MIT',
+    include_package_data=True,
+    zip_safe=False,
+    packages=find_packages(),
+    install_requires=[ 'mroylib-min>=1.5.1', 'geoip2','fabric3', 'qrcode', 'pillow','image', 'xlwt','xlrd'],
+    entry_points={
+        'console_scripts': ['x-web=web.main:main', 'x-web-cmd=web.vultr:main']
+    },
+
+)
