067281caf18e5ba9979b38764f5ff37782091000
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 07:13:33 +0000

    Add package peek-desktop/1.0.0/peek-desktop-1.0.0.tar.gz

---
+++ packages/peek-desktop/peek-desktop-1.0.0.tar.gz/peek-desktop-1.0.0/peek_desktop/src/app/app.component.ts
@@ -0,0 +1,23 @@
+import {Component} from "@angular/core";
+import {VortexService, VortexStatusService} from "@synerty/vortexjs";
+import {OnInit} from "@angular/core";
+
+@Component({
+    selector: "peek-main-app",
+    templateUrl: "app.component.dweb.html",
+    styleUrls: ["app.component.dweb.scss"],
+    moduleId: module.id
+})
+export class AppComponent implements OnInit {
+
+    constructor(private vortexService: VortexService,
+                private vortexStatusService: VortexStatusService) {
+
+    }
+
+    ngOnInit() {
+        this.vortexService.reconnect();
+    }
+
+}
+
