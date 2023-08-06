33f334cce88c55822d150e9fcd17071be44308d4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 07:13:19 +0000

    Add package Nikippe/0.1a14/Nikippe-0.1a14.tar.gz

---
+++ packages/Nikippe/Nikippe-0.1a14.tar.gz/Nikippe-0.1a14/nikippe/schema/elementfactory.py
@@ -0,0 +1,31 @@
+import nikippe.schema.mqttimage
+import nikippe.schema.statictext
+import nikippe.schema.mqtttext
+import nikippe.schema.digitalclock
+import nikippe.schema.bar
+import nikippe.schema.sequentialchart
+import nikippe.schema.staticimage
+import nikippe.schema.imagelist
+
+
+def get_schema():
+    schema = {
+                    "description": "List of elements for renderer.",
+                    "type": "array",
+                    "items": {
+                        "oneOf": [
+                        ]
+                    },
+                    "additionalItems": False
+            }
+
+    schema["items"]["oneOf"].append(nikippe.schema.statictext.get_schema())
+    schema["items"]["oneOf"].append(nikippe.schema.staticimage.get_schema())
+    schema["items"]["oneOf"].append(nikippe.schema.mqtttext.get_schema())
+    schema["items"]["oneOf"].append(nikippe.schema.digitalclock.get_schema())
+    schema["items"]["oneOf"].append(nikippe.schema.bar.get_schema())
+    schema["items"]["oneOf"].append(nikippe.schema.sequentialchart.get_schema())
+    schema["items"]["oneOf"].append(nikippe.schema.imagelist.get_schema())
+    schema["items"]["oneOf"].append(nikippe.schema.mqttimage.get_schema())
+
+    return schema
---
+++ packages/Nikippe/Nikippe-0.1a14.tar.gz/Nikippe-0.1a14/nikippe/schema/sequentialchart.py
@@ -0,0 +1,5 @@
+import nikippe.schema.achart
+
+
+def get_schema():
+    return nikippe.schema.achart.get_schema()
