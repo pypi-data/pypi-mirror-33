7416b370729fda11d7c1c4d1c7a7563803675140
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 07:13:04 +0000

    Add package viablimp/0.2/viablimp-0.2.tar.gz

---
+++ packages/viablimp/viablimp-0.2.tar.gz/viablimp-0.2/setup.py
@@ -0,0 +1,19 @@
+from setuptools import setup,find_packages
+
+def readme():
+    with open('viablimp/README.rst') as f:
+        return f.read()
+
+setup(name='viablimp',
+      version='0.2',
+      description='logging messages by facebook bot to your account',
+      long_description=readme(),
+      url='https://facebooklogger.herokuapp.com/',
+      author='Abhinav Rai',
+      author_email='rai.1@iitj.ac.in',
+      license='MIT',
+      packages=['viablimp'],
+      install_requires=[
+        'requests'
+		],
+      zip_safe=False)
