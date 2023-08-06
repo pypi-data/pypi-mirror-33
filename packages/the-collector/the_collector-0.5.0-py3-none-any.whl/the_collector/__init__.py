fe70fcee7dba4a7c43437b4a96ede1ca3381ea5e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:13:30 +0000

    Add package the-collector/0.5.0/the_collector-0.5.0-py3-none-any.whl

---
+++ packages/the-collector/the_collector-0.5.0-py3-none-any.whl/the_collector/__init__.py
@@ -0,0 +1,7 @@
+from the_collector.bagit import BagWriter, BagReader
+from the_collector.bagitjson import BagJsonWriter, BagJsonReader
+from the_collector.circular_buffer import CircularBuffer
+
+__license__ = "MIT"
+__author__ = "Kevin J. Walchko"
+__version__ = '0.5.0'
