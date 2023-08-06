864e8fac89aa1168d8e6448a2c2ce25b528e51db
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:21:55 +0000

    Add package ongeza/1.12.2/ongeza-1.12.2-py2.py3-none-any.whl

---
+++ packages/ongeza/ongeza-1.12.2-py2.py3-none-any.whl/ongeza/git_utils.py
@@ -0,0 +1,141 @@
+# -*- coding: utf-8 -*-
+# vim: sw=4:ts=4:expandtab
+
+"""
+ongeza.git_utils
+~~~~~~~~~~~~~~~~
+
+helpers for working with git.
+
+Examples:
+    basic usage::
+
+        >>> Git().tags[:2] == ['v0.8.0', 'v0.8.1']
+        True
+"""
+
+from __future__ import (
+    absolute_import, division, print_function, with_statement,
+    unicode_literals)
+
+from functools import partial, cmp_to_key
+
+import semver
+import pygogo as gogo
+
+from builtins import *  # noqa pylint: disable=unused-import
+from .shell_utils import sh
+
+logger = gogo.Gogo(__name__, monolog=True).logger
+
+
+class Git(object):
+    """
+    class representing Git commands.
+    """
+    def __init__(self, dir_=None, verbose=False):
+        """
+        Parameters
+        ----------
+        dir: directory containing the git project
+        """
+        self.dir = dir_
+        self.stash_count = 0
+        self.logger = logger
+        self.sh = partial(sh, path=self.dir)
+
+    @property
+    def current_tag(self):
+        """
+            :returns: string of the current git tag on the git index, not the
+            latest tag version created.
+        """
+        cmd = 'git describe --tags --abbrev=0'
+        return self.sh(cmd, True)
+
+    @property
+    def is_clean(self):
+        """
+        Returns
+        -------
+        boolean if there is a dirty index.
+        """
+        return self.sh("git diff --quiet")
+
+    @property
+    def is_dirty(self):
+        """
+        Returns
+        -------
+        boolean if there is a dirty index.
+        """
+        return not self.is_clean
+
+    @property
+    def dirty_files(self):
+        """
+        Returns
+        -------
+        list of string names of the dirty files.
+        """
+        files = self.sh("git diff --minimal --numstat", True)
+        return [x.split("\t")[-1] for x in files.splitlines()]
+
+    @property
+    def files(self):
+        """
+        Returns
+        -------
+        list of string names of all files.
+        """
+        cmd = "git ls-tree --full-tree --name-only -r HEAD"
+        return self.sh(cmd, True).splitlines()
+
+    @property
+    def tags(self):
+        """
+            :returns: list of git tags, sorted by the version number.
+        """
+        cmd = 'git tag'
+        tags = self.sh(cmd, True).split('\n')
+        compare = lambda x, y: semver.compare(x.lstrip('v'), y.lstrip('v'))
+        return sorted(tags, key=cmp_to_key(compare))
+
+    def add(self, files):
+        files = ' '.join(files)
+        self.logger.info('add files: "%s"', files)
+        return self.sh('git add %s' % files)
+
+    def commit(self, message):
+        self.logger.info('making git commit: "%s"', message)
+        return self.sh('git commit -m "%s"' % message)
+
+    def tag(self, message, tag_text, sign=False):
+        self.logger.info('making git tag: "%s"', message)
+        opts = 'sm' if sign else 'm'
+        cmd = 'git tag -%s "%s" %s' % (opts, message, tag_text)
+        return self.sh(cmd)
+
+    def push(self):
+        """
+        pushes current branch and tags to remote.
+        """
+        return self.sh("git push && git push --tags")
+
+    def stash(self):
+        """
+        stashes current changes in git.
+        """
+        if self.sh("git stash"):
+            self.stash_count += 1
+
+        return self.stash_count
+
+    def unstash(self):
+        """
+        pops previous stash from git.
+        """
+        if self.stash_count and self.sh("git stash pop"):
+            self.stash_count -= 1
+
+        return self.stash_count
---
+++ packages/ongeza/ongeza-1.12.2-py2.py3-none-any.whl/ongeza/shell_utils.py
@@ -0,0 +1,112 @@
+# -*- coding: utf-8 -*-
+# vim: sw=4:ts=4:expandtab
+
+"""
+ongeza.shell_utils
+~~~~~~~~~~~~~~~~~~
+
+helpers for working with the shell.
+
+Examples:
+    basic usage::
+
+        >>> sh('echo hello world')
+        True
+        >>> sh('echo hello world', True) == 'hello world'
+        True
+
+Attributes:
+    ENCODING (str): The module encoding
+"""
+
+from __future__ import (
+    absolute_import, division, print_function, with_statement,
+    unicode_literals)
+
+import os
+
+from subprocess import check_call, check_output, CalledProcessError
+from builtins import *  # noqa pylint: disable=unused-import
+
+try:
+    from subprocess import DEVNULL
+except ImportError:
+    DEVNULL = False
+
+
+def quiet_call(cmd, devnull):
+    """Calls an external command while suppressing stdout.
+
+    Args:
+        cmd (str): The command to run
+        devnull (object): File-like object
+
+    Returns:
+        bool: True if the commabd return code was zero, else otherwise
+
+    Examples:
+        >>> with open(os.devnull, 'wb') as devnull:
+        ...     quiet_call('ls', devnull)
+        True
+    """
+    try:
+        check_call(cmd, shell=True, stdout=devnull)
+    except CalledProcessError:
+        return False
+    else:
+        return True
+
+
+def sh(cmd, output=False, path=None):
+    """runs an external command.
+
+    Args:
+        cmd (str): The command to run
+        output (bool): return the command output (default: False)
+        path (str): The path to run the command from
+
+    Returns:
+        mixed: command output if `output` is True, else bool representing
+            successful completion of the command
+
+    Examples:
+        >>> len(sh('ls', True)) > 0
+        True
+    """
+    good = True
+
+    if path:
+        try:
+            os.chdir(os.path.abspath(path))
+        except OSError:
+            good = False
+
+    if output and good:
+        try:
+            result = check_output(cmd, shell=True).strip().decode('utf-8')
+        except CalledProcessError:
+            result = ''
+    elif good:
+        if DEVNULL:
+            result = quiet_call(cmd, DEVNULL)
+        else:
+            with open(os.devnull, 'wb') as devnull:
+                result = quiet_call(cmd, devnull)
+    elif output:
+        result = ''
+    else:
+        result = False
+
+    return result
+
+
+def choice(msg):
+    """Prompts for a True/False input from the user command line.
+
+    Args:
+        msg (str): The message to present to the user
+
+    Returns:
+        bool: the user input
+    """
+    return raw_input("{}  [Yn]: ".format(msg)).lower().startswith('y')
