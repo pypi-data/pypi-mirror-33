ddc46c8fc83e0d505c719a3fab16d609bbae7a4e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:24:18 +0000

    Add package ublox-lara-r2/0.0.1/ublox-lara-r2-0.0.1.tar.gz

---
+++ packages/ublox-lara-r2/ublox-lara-r2-0.0.1.tar.gz/ublox-lara-r2-0.0.1/setup.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python
+
+try:
+    from setuptools import setup, find_packages
+except ImportError:
+	from distutils.core import setup
+
+with open('package_description.rst', 'r') as f:
+	long_description = f.read()
+
+
+setup(
+      name='ublox-lara-r2',
+      version='0.0.1',
+      packages = find_packages(),
+      description='LTE Pi Hat driver using celluar module ublox lara r2 LTE Cat.1.',
+      long_description=long_description,
+      author='Lambor',
+      author_email='lanselambor@gmail.com',
+      url='https://www.github.com/Seeed-Studio/ublox_lara_r2_pi_hat',
+      license='MIT',
+      scripts=["bin/rpirtscts"],
+      classifiers=(
+          "Programming Language :: Python :: 2",
+          "Programming Language :: Python :: 2.7",
+          "License :: OSI Approved :: MIT License",
+          "Operating System :: POSIX :: Linux",
+      ),
+      keywords=['ublox lara r2', 'ublox', 'R211', 'R204', 'LTE', 'Cat.1'],
+     )
