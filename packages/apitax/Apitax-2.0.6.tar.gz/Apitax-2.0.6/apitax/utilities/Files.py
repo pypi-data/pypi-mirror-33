596255e4363238c0a5ca775f73c6fa064833a8b7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:49:27 +0000

    Add package apitax/2.0.6/Apitax-2.0.6.tar.gz

---
+++ packages/apitax/Apitax-2.0.6.tar.gz/Apitax-2.0.6/apitax/utilities/Files.py
@@ -0,0 +1,35 @@
+import glob
+import os
+from pathlib import Path
+import __main__
+
+
+def getAllFiles(path):
+    return glob.glob(getPath(path), recursive=True)
+    
+def readFile(path):
+    with open(getPath(path), 'r') as myfile:
+        return myfile.read()#.replace('\n', '').replace('\r', '')
+        
+def renameFile(originalFile, newFile):
+    if(getPath(path, asString=False).exists()):
+        return False
+    getPath(path, asString=False).rename(Path(newFile))
+    return True
+    
+def saveFile(path, contents):
+    with open(getPath(path), "w+") as text_file:
+        print(contents, file=text_file)
+    
+def deleteFile(path):
+    return getPath(path, asString=False).unlink()
+    
+def getPath(path, asString=True):
+    pathStr = str(Path(path).resolve()).replace(os.sep, '/')
+    if(asString):
+        return pathStr
+    else:
+        return Path(pathStr)
+    
+def getPathToTopLevel(path = ''):
+    return getPath(os.path.dirname(os.path.abspath(__main__.__file__)) + '/' + path)
