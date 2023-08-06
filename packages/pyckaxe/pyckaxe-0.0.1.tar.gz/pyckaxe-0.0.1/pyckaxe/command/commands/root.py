0e5db27891587c929922eabe5507707516e3f5e1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:48:00 +0000

    Add package pyckaxe/0.0.1/pyckaxe-0.0.1.tar.gz

---
+++ packages/pyckaxe/pyckaxe-0.0.1.tar.gz/pyckaxe-0.0.1/pyckaxe/command/commands/root.py
@@ -0,0 +1,53 @@
+import typing
+
+from pyckaxe.command.abc.command import CommandNode
+from pyckaxe.command.commands.clear import ClearCommand
+from pyckaxe.command.commands.execute import ExecuteCommand
+from pyckaxe.command.commands.say import SayCommand
+from pyckaxe.command.commands.setblock import SetblockCommand
+from pyckaxe.command.commands.tag import TagCommand
+from pyckaxe.command.commands.teleport import TeleportCommand
+from pyckaxe.command.commands.time import TimeCommand
+from pyckaxe.command.commands.tp import TpCommand
+
+
+class RootCommandMixin:
+    @property
+    def clear(self: CommandNode) -> ClearCommand:
+        return ClearCommand(self)
+
+    @property
+    def execute(self: CommandNode) -> ExecuteCommand:
+        return ExecuteCommand(self)
+
+    @property
+    def say(self: CommandNode) -> SayCommand:
+        return SayCommand(self)
+
+    @property
+    def setblock(self: CommandNode) -> SetblockCommand:
+        return SetblockCommand(self)
+
+    @property
+    def tag(self: CommandNode) -> TagCommand:
+        return TagCommand(self)
+
+    @property
+    def teleport(self: CommandNode) -> TeleportCommand:
+        return TeleportCommand(self)
+
+    @property
+    def time(self: CommandNode) -> TimeCommand:
+        return TimeCommand(self)
+
+    @property
+    def tp(self: CommandNode) -> TpCommand:
+        return TpCommand(self)
+
+
+class RootCommand(CommandNode, RootCommandMixin):
+    def __init__(self):
+        super().__init__(None, None)
+
+    def _tokens(self) -> typing.Iterable[typing.Any]:
+        yield from ()
