75968bb2fd8a135dfbee7bca18e03451fe91f6f4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:26:30 +0000

    Add package deep-text/1.2.2/deep_text-1.2.2-py2.py3-none-any.whl

---
+++ packages/deep-text/deep_text-1.2.2-py2.py3-none-any.whl/common/config_ops.py
@@ -0,0 +1,21 @@
+from deepcls.config import TextCNNConfig
+from deepcls.config import TextRNNConfig
+from deepcrf.config import DeepCRFConfig
+
+
+def load_deep_crf_config(path, train_input_path=None, test_input_path=None):
+    with open(path, "r") as fp:
+        json_str = fp.read()
+        return DeepCRFConfig(json_str, train_input_path, test_input_path)
+
+
+def load_deep_cls_cnn_config(path, train_input_path=None, test_input_path=None):
+    with open(path, "r") as fp:
+        json_str = fp.read()
+        return TextCNNConfig(json_str, train_input_path, test_input_path)
+
+
+def load_deep_cls_rnn_config(path, train_input_path=None, test_input_path=None):
+    with open(path, "r") as fp:
+        json_str = fp.read()
+        return TextRNNConfig(json_str, train_input_path, test_input_path)
