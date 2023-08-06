0b30e3ff9b841818262392d9c17f7500fa7cdf34
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:48:19 +0000

    Add package pupy/1.2.0/pupy-1.2.0.tar.gz

---
+++ packages/pupy/pupy-1.2.0.tar.gz/pupy-1.2.0/setup.py
@@ -0,0 +1,28 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup, find_packages
+
+
+setup(name='pupy',
+      version='1.2.0',
+      description='pretty useful python',
+      long_description='coming soon',
+      classifiers=[
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+      ],
+      keywords='pretty useful python',
+      # url='http://github.com/jessekrubin/pupy/',
+      url='https://upload.pypi.org/legacy/',
+      author='jesse r',
+      author_email='jessekrubin@gmail.com',
+      license='MIT',
+      packages=find_packages(),
+      install_requires=[
+          'markdown',
+          'tqdm>=4',
+      ],
+      include_package_data=True,
+      zip_safe=False)
+
+
