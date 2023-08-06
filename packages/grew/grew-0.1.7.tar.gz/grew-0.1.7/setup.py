2910d5fcdbcca8c9ea40736f9a1505cb984018ba
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:22:00 +0000

    Add package grew/0.1.7/grew-0.1.7.tar.gz

---
+++ packages/grew/grew-0.1.7.tar.gz/grew-0.1.7/setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+
+setup(
+    name='grew',
+    version='0.1.7',
+    packages=['grew',],
+    license='LICENSE/Licence_CeCILL_V2-en.txt',
+    long_description=open('README.md').read(),
+    url="http://grew.fr",
+    author="bguil",
+    author_email="Bruno.Guillaume@loria.fr"
+)
