ebe61a040af2f65c878eab14c17dd3efc0d7b3eb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:25:33 +0000

    Add package aws-rotate/0.0.2/aws-rotate-0.0.2.tar.gz

---
+++ packages/aws-rotate/aws-rotate-0.0.2.tar.gz/aws-rotate-0.0.2/setup.py
@@ -0,0 +1,95 @@
+from setuptools import setup, find_packages
+from codecs import open
+from os import path
+
+here = path.abspath(path.dirname(__file__))
+
+# Get the long description from the README file
+with open(path.join(here, 'README.md'), encoding='utf-8') as f:
+    long_description = f.read()
+
+# Arguments marked as "Required" below must be included for upload to PyPI.
+# Fields marked as "Optional" may be commented out.
+
+setup(
+    name='aws-rotate',  # Required
+
+    version='0.0.2',  # Required
+
+    description='A simple AWS Access ID/Secret rotation script',  # Required
+
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+
+    url='https://github.com/grahamhar/aws-rotate',  # Optional
+
+    author='grahamhar',  # Optional
+
+
+    classifiers=[  # Optional
+        # How mature is this project? Common values are
+        #   3 - Alpha
+        #   4 - Beta
+        #   5 - Production/Stable
+        'Development Status :: 3 - Alpha',
+
+        # Indicate who your project is intended for
+        'Intended Audience :: Developers',
+        'Topic :: Software Development :: Build Tools',
+
+        # Pick your license as you wish
+        'License :: OSI Approved :: Apache Software License',
+
+        # Specify the Python versions you support here. In particular, ensure
+        # that you indicate whether you support Python 2, Python 3 or both.
+        'Programming Language :: Python :: 3',
+    ],
+
+    keywords='aws access iam rotation',  # Optional
+
+    packages=find_packages(exclude=['contrib', 'docs', 'tests']),  # Required
+
+    install_requires=['boto3'],  # Optional
+
+    # List additional groups of dependencies here (e.g. development
+    # dependencies). Users will be able to install these using the "extras"
+    # syntax, for example:
+    #
+    #   $ pip install sampleproject[dev]
+    #
+    # Similar to `install_requires` above, these must be valid existing
+    # projects.
+    extras_require={  # Optional
+        'dev': ['pytest', 'pytest-cov'],
+    },
+
+
+
+
+    # To provide executable scripts, use entry points in preference to the
+    # "scripts" keyword. Entry points provide cross-platform support and allow
+    # `pip` to create the appropriate form of executable for the target
+    # platform.
+    #
+    # For example, the following would provide a command called `sample` which
+    # executes the function `main` from this package when invoked:
+    entry_points={  # Optional
+        'console_scripts': [
+            'aws-rotate-keys=aws_rotate:run',
+        ],
+    },
+
+    # List additional URLs that are relevant to your project as a dict.
+    #
+    # This field corresponds to the "Project-URL" metadata fields:
+    # https://packaging.python.org/specifications/core-metadata/#project-url-multiple-use
+    #
+    # Examples listed include a pattern for specifying where the package tracks
+    # issues, where the source is hosted, where to say thanks to the package
+    # maintainers, and where to support the project financially. The key is
+    # what's used to render the link text on PyPI.
+    project_urls={  # Optional
+        'Bug Reports': 'https://github.com/grahamhar/aws-rotate/issues',
+        'Source': 'https://github.com/grahamhar/aws-rotate/',
+    },
+)
