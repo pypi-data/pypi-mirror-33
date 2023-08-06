00322c617f05e565dc9f50fff6504e31e8b7e6cb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:10:41 +0000

    Add package iotile-transport-bled112/1.7.3/iotile-transport-bled112-1.7.3.tar.gz

---
+++ packages/iotile-transport-bled112/iotile-transport-bled112-1.7.3.tar.gz/iotile-transport-bled112-1.7.3/iotile_transport_bled112/config_variables.py
@@ -0,0 +1,11 @@
+"""Canonical list of config variables defined by iotile-transport-bled112."""
+
+from __future__ import unicode_literals, absolute_import, print_function
+
+def get_variables():
+    prefix = "bled112"
+
+    conf_vars = []
+    conf_vars.append(["active-scan", "bool", "Probe devices during scan for uptime, voltage and broadcast data", "false"])
+
+    return prefix, conf_vars
