d2f58e43010655cd021148c69cddf4b2365e01a5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 05:07:40 +0000

    Add package jsntlib/1.5.0/jsntlib-1.5.0-py2.py3-none-any.whl

---
+++ packages/jsntlib/jsntlib-1.5.0-py2.py3-none-any.whl/ntlib/__init__.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+
+'''Caution:
+
+For Python 2.7, `__init__.py` file in folders is nessary.
+'''
+
+
+from __future__ import print_function
+from .ntl       import *
+
+
+print('The `jsntlib` module is now deprecated and renamed as `pyntlib`.')
