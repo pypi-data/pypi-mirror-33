232b70f15b0c67e32941a4e41319831669c69cf9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:13:20 +0000

    Add package mountainlab-pytools/0.1.2a1/mountainlab_pytools-0.1.2a1.tar.gz

---
+++ packages/mountainlab-pytools/mountainlab_pytools-0.1.2a1.tar.gz/mountainlab_pytools-0.1.2a1/setup.py
@@ -0,0 +1,16 @@
+"""
+Setup Module to setup mountainlab_pytools package
+"""
+import setuptools
+
+setuptools.setup(
+    name='mountainlab_pytools',
+    version='0.1.2a-1',
+    description='Tools for integrating MountainLab with python',
+    packages=setuptools.find_packages(),
+    install_requires=[
+        'requests',
+        'numpy',
+        'numpydoc'
+    ]
+)
