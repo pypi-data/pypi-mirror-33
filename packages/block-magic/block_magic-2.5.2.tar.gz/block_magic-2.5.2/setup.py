97123e5519c43bde65678d84167a4088d4b72770
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 07:12:34 +0000

    Add package block-magic/2.5.2/block_magic-2.5.2.tar.gz

---
+++ packages/block-magic/block_magic-2.5.2.tar.gz/block_magic-2.5.2/setup.py
@@ -0,0 +1,14 @@
+from distutils.core import setup
+
+setup(
+    name = 'block_magic',
+    packages = ['block_magic'],
+    version = '2.5.2',
+    description = 'Simple Web data I/O on Secure Blockchain',
+    author = 'Monty Dimkpa',
+    author_email = 'cmdimkpa@gmail.com',
+    url = 'https://github.com/cmdimkpa/BlockMagic',
+    download_url = 'https://github.com/cmdimkpa/BlockMagic/archive/v2.5.2.tar.gz',
+    keywords = ['Web I/O', 'blockchain'],
+    classifiers = [],
+)
