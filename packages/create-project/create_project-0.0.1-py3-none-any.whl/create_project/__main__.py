12ee7261fd09da3106dd2426c0003e2e0aec3512
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:38:51 +0000

    Add package create-project/0.0.1/create_project-0.0.1-py3-none-any.whl

---
+++ packages/create-project/create_project-0.0.1-py3-none-any.whl/create_project/__main__.py
@@ -0,0 +1,51 @@
+# -----------------------------------------------------------------------------
+# Created: Sun 15 Jul 2018 19:12:54 IST
+# Last-Updated: Sun 15 Jul 2018 19:40:24 IST
+#
+# __main__.py is part of create-project
+# URL: https://gitlab.com/justinethomas/create-project
+# Description: Endpoint
+#
+# Copyright (c) 2018, Justine T Kizhakkinedath
+# All rights reserved
+#
+# Licensed under the terms of MIT License
+# See LICENSE file in the project root for full information.
+# -----------------------------------------------------------------------------
+#
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+#
+# The above copyright notice and this permission notice shall be included in
+# all copies or substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+# EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+# MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.
+# IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM,
+# DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR
+# OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE
+# OR OTHER DEALINGS IN THE SOFTWARE.
+#
+# -----------------------------------------------------------------------------
+
+# import sys
+
+import argparse
+
+
+def main(args=None):
+    """The main routine."""
+    parser = argparse.ArgumentParser()
+
+    parser.add_argument("project_name", help="Enter the name of project you want to create")
+    args = parser.parse_args()
+    print(args.project_name)
+
+
+if __name__ == "__main__":
+    main()
