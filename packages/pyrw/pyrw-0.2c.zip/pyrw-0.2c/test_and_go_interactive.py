74c6e4e6da1f1502d280eeda890b49bf6e10885c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:47:58 +0000

    Add package pyrw/0.2c/pyrw-0.2c.zip

---
+++ packages/pyrw/pyrw-0.2c.zip/pyrw-0.2c/test_and_go_interactive.py
@@ -0,0 +1,26 @@
+'''
+Brief:
+    Quick script to install and then play with pyrw
+
+Author(s):
+    Charles Machalow
+'''
+
+import os
+import sys
+
+THIS_FILE = os.path.abspath(__file__)
+THIS_FOLDER = os.path.abspath(os.path.dirname(THIS_FILE))
+
+if __name__ == '__main__':
+    if 'TEST_AND_GO' in os.environ:
+        from pyrw.rwe import ReadWriteEverything
+        rwe = ReadWriteEverything()
+        print ('| --------------------------------------------- |')
+        print ('| rwe object has been created for your usage... |')
+        print ('| --------------------------------------------- |')
+        # we should be interactive..
+    else:
+        os.system('%s setup.py --quiet clean --all install > nul 2>&1' % sys.executable)
+        os.environ['TEST_AND_GO'] = '1'
+        os.system('%s -i %s' % (sys.executable, THIS_FILE))
