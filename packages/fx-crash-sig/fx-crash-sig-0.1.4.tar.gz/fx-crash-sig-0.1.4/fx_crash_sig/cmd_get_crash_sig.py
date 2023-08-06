18eabb16850a1c0a32f800ccd26f74d079bed14f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:43:27 +0000

    Add package fx-crash-sig/0.1.4/fx-crash-sig-0.1.4.tar.gz

---
+++ packages/fx-crash-sig/fx-crash-sig-0.1.4.tar.gz/fx-crash-sig-0.1.4/fx_crash_sig/cmd_get_crash_sig.py
@@ -0,0 +1,20 @@
+from __future__ import print_function
+
+import sys
+
+import ujson as json
+
+from fx_crash_sig.crash_processor import CrashProcessor
+
+DESCRIPTION = """
+Takes raw crash trace and symbolicates it to return the crash signature
+"""
+
+
+def cmdline():
+    crash_processor = CrashProcessor()
+    crash_data = json.loads(sys.stdin.read())
+
+    signature = crash_processor.get_signature(crash_data)
+
+    print(json.dumps(signature))
