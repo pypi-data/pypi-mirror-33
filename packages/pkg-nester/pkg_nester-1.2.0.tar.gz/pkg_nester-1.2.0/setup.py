eb546f73f216cc5e10e03e8ff1f9a02bd48e8806
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:18:23 +0000

    Add package pkg-nester/1.2.0/pkg_nester-1.2.0.tar.gz

---
+++ packages/pkg-nester/pkg_nester-1.2.0.tar.gz/pkg_nester-1.2.0/setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+
+setup(
+        name = 'pkg_nester',
+        version = '1.2.0',
+        py_modules = ['nester'],
+        author = 'hfpython',
+        author_email = 'hrpyton@headfirstlabs.com',
+        url = 'http://www.headfirstlabs.com',
+        description = 'A simple printer of nested lists',
+    )
