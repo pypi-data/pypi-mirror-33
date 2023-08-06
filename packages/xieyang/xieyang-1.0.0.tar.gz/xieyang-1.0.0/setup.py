8c5a23c7c1da0934bb0c82cd35918174d1ebf93e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 05:06:43 +0000

    Add package xieyang/1.0.0/xieyang-1.0.0.tar.gz

---
+++ packages/xieyang/xieyang-1.0.0.tar.gz/xieyang-1.0.0/setup.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python-3
+# -*- coding:utf-8 -*-
+from distutils.core import setup
+setup(
+    name = 'xieyang',
+    version='1.0.0',
+    py_modules=['nester'],
+    author='xieyang',
+    author_email='1336370424@qq.com',
+    description='a simple printer of nested lists',
+)
