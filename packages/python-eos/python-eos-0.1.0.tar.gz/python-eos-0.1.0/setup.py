3d39fa19fb1e4d5ba73a6054f1b2087a24f7c2c4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:34:36 +0000

    Add package python-eos/0.1.0/python-eos-0.1.0.tar.gz

---
+++ packages/python-eos/python-eos-0.1.0.tar.gz/python-eos-0.1.0/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='python-eos',
+      version='0.1.0',
+      description='',
+      url='https://github.com/tokenika/python-eos',
+      author='Patryk Perduta',
+      author_email='patryk@perduta.net',
+      license='MIT',
+      packages=['python-eos'],
+      zip_safe=False)
