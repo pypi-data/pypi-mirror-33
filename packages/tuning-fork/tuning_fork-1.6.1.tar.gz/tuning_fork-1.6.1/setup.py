310151375d541174f084340bc840aecdf14849ce
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:08:20 +0000

    Add package tuning-fork/1.6.1/tuning_fork-1.6.1.tar.gz

---
+++ packages/tuning-fork/tuning_fork-1.6.1.tar.gz/tuning_fork-1.6.1/setup.py
@@ -0,0 +1,14 @@
+import json
+
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+with open("setup.json", "r") as js:
+    setupJSON = json.loads(js.read())
+setupJSON["long_description"] = long_description
+
+setuptools.setup(
+    **setupJSON
+)
