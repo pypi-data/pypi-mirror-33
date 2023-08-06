dacf5b1a5aa5d7ab676df795e3fe43bd6308f174
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:34:37 +0000

    Add package flake8-logging-format/0.4.0/flake8-logging-format-0.4.0.tar.gz

---
+++ packages/flake8-logging-format/flake8-logging-format-0.4.0.tar.gz/flake8-logging-format-0.4.0/logging_format/violations.py
@@ -0,0 +1,16 @@
+"""
+Defined violations
+
+"""
+
+
+STRING_FORMAT_VIOLATION = "G001 Logging statement uses string.format()"
+STRING_CONCAT_VIOLATION = "G003 Logging statement uses '+'"
+
+PERCENT_FORMAT_VIOLATION = "G002 Logging statement uses '%'"
+
+FSTRING_VIOLATION = "G004 Logging statement uses f-string"
+
+WARN_VIOLATION = "G010 Logging statement uses 'warn' instead of 'warning'"
+
+WHITELIST_VIOLATION = "G100 Logging statement uses non-whitelisted extra keyword argument: {}"
