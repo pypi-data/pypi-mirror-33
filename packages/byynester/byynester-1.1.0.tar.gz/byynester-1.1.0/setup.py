f817cce8a12a9f0b97fe156fd3942bc04cfbe21b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:36:54 +0000

    Add package byynester/1.1.0/byynester-1.1.0.tar.gz

---
+++ packages/byynester/byynester-1.1.0.tar.gz/byynester-1.1.0/setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+
+setup(
+    name = 'byynester',
+    version = '1.1.0',
+    py_modules = ['byynester'],
+    author = 'byypython',
+    author_email = 'relaxingbyy@163.com',
+    url = 'http://www.headfirstlabs.com',
+    description = 'A simple printer of nested lists'
+)
