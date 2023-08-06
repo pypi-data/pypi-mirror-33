5cd1b593ae6ea2da59a93dd11450adc1088b5b1e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:14:44 +0000

    Add package simpleCalci/0.4/simpleCalci-0.4.tar.gz

---
+++ packages/simpleCalci/simpleCalci-0.4.tar.gz/simpleCalci-0.4/setup.py
@@ -0,0 +1,7 @@
+from setuptools import setup
+
+setup(name='simpleCalci',
+      version='0.4',
+      description='Calculator to perform simple math operations',
+      packages=['simpleCalci'],
+      zip_safe=False)
