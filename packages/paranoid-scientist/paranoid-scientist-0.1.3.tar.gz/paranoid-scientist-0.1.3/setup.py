355c60f8369db548a43bc78696f02d556671101d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:14:43 +0000

    Add package paranoid-scientist/0.1.3/paranoid-scientist-0.1.3.tar.gz

---
+++ packages/paranoid-scientist/paranoid-scientist-0.1.3.tar.gz/paranoid-scientist-0.1.3/setup.py
@@ -0,0 +1,33 @@
+# Copyright 2018 Max Shinn <max@maxshinnpotential.com>
+# 
+# This file is part of Paranoid Scientist, and is available under the
+# MIT license.  Please see LICENSE.txt in the root directory for more
+# information.
+
+from setuptools import setup
+
+from paranoid._version import __version__
+
+setup(
+    name = 'paranoid-scientist',
+    version = __version__,
+    description = 'Runtime verification and automated testing for scientific code',
+    author = 'Max Shinn',
+    author_email = 'maxwell.shinn@yale.edu',
+    url = 'https://github.com/mwshinn/paranoidscientist',
+    maintainer = 'Max Shinn',
+    license = 'MIT',
+    python_requires='>=3.5',
+    maintainer_email = 'maxwell.shinn@yale.edu',
+    packages = ['paranoid', 'paranoid.types'],
+    classifiers = [
+        'Development Status :: 5 - Production/Stable',
+        'Intended Audience :: Developers',
+        'Intended Audience :: Science/Research',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 3',
+        'Topic :: Education :: Testing',
+        'Topic :: Scientific/Engineering',
+        'Topic :: Software Development :: Quality Assurance',
+        'Topic :: Software Development :: Testing']
+)
