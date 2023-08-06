cd702e50f89303868b841f1876f9612b6e071f64
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:33:41 +0000

    Add package bddrest/1.8.0a6/bddrest-1.8.0a6.tar.gz

---
+++ packages/bddrest/bddrest-1.8.0a6.tar.gz/bddrest-1.8.0a6/bddrest/__init__.py
@@ -0,0 +1,9 @@
+
+from .specification import Call, FirstCall, AlteredCall, HeaderSet, HTTPStatus
+from .authoring import Given, when, story, response, Story, Append, Update,\
+    Remove
+from .exceptions import InvalidUrlParametersError, CallVerifyError
+
+
+__version__ = '1.8.0a6'
+
