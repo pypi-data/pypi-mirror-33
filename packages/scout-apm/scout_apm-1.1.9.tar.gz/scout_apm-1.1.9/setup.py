e2042a7c8eb1c3163d68037b3f4c1190d4617ea7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:50:29 +0000

    Add package scout-apm/1.1.9/scout_apm-1.1.9.tar.gz

---
+++ packages/scout-apm/scout_apm-1.1.9.tar.gz/scout_apm-1.1.9/setup.py
@@ -0,0 +1,39 @@
+from glob import glob
+from os.path import basename, splitext
+
+from setuptools import find_packages, setup
+
+setup(name='scout_apm',
+      version='1.1.9',
+      description='Scout Application Performance Monitoring Agent',
+      long_description='Scout Application Performance Monitoring Agent',
+      url='https://github.com/scoutapp/scout_apm_python',
+      author='Scout',
+      author_email='support@scoutapp.com',
+      license='MIT',
+      zip_safe=False,
+      python_requires='>=3.4, <4',
+      packages=find_packages('src'),
+      package_dir={'': 'src'},
+      py_modules=[splitext(basename(path))[0] for path in glob('src/*.py')],
+      entry_points={
+          'console_scripts': [
+              'core-agent-manager = scout_apm.core.cli.core_agent_manager:main'
+          ]
+      },
+      install_requires=['psutil', 'PyYAML', 'requests'],
+      keywords='apm performance monitoring development',
+      classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: Developers',
+        'Topic :: System :: Monitoring',
+        'License :: Other/Proprietary License',
+        'Operating System :: MacOS',
+        'Operating System :: POSIX',
+        'Operating System :: POSIX :: Linux',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+      ])
