8b883be0364eec1664bc67798f03cc3161c16531
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 07:14:03 +0000

    Add package criticalpath/0.1.4/criticalpath-0.1.4.tar.gz

---
+++ packages/criticalpath/criticalpath-0.1.4.tar.gz/criticalpath-0.1.4/criticalpath/__init__.py
@@ -0,0 +1,4 @@
+from .criticalpath import Node
+
+VERSION = (0, 1, 4)
+__version__ = '.'.join(map(str, VERSION))
---
+++ packages/criticalpath/criticalpath-0.1.4.tar.gz/criticalpath-0.1.4/setup.py
@@ -0,0 +1,38 @@
+#!/usr/bin/env python
+
+from os import path
+from setuptools import setup, find_packages
+
+import criticalpath
+
+with open(path.join(path.abspath(path.dirname(__file__)), 'README.md'), encoding='utf-8') as f:
+    long_description = f.read()
+
+setup(
+    name='criticalpath',
+    version=criticalpath.__version__,
+    packages=find_packages(),
+    package_data={
+        'criticalpath': [
+            'fixtures/*',
+        ],
+    },
+    author='Chris Spencer',
+    author_email='chrisspen@gmail.com',
+    description='Calculates the critical path in a task network.',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    license='LGPL',
+    url='https://github.com/chrisspen/criticalpath',
+    #https://pypi.python.org/pypi?%3Aaction=list_classifiers
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: GNU Lesser General Public License v3 (LGPLv3)',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3.6'
+    ],
+    zip_safe=False,
+)
