a9adb614cd040a69016520226e5777017d29db66
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 05:07:40 +0000

    Add package django-loggable/0.2.1/django-loggable-0.2.1.tar.gz

---
+++ packages/django-loggable/django-loggable-0.2.1.tar.gz/django-loggable-0.2.1/setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup
+
+setup(name = "django-loggable",
+      version = "0.2.1",
+      description = "this library adds 'created_at', 'updated_at' and 'delete_at'  fields like a rail apps in django,"
+                    " also added soft delete method",
+      long_description=open('README.rst', 'r').read(),
+      author = "Carlos Ganoza Plasencia",
+      author_email = "cganozap@gmail.com",
+      url = "https://github.com/drneox/django_loggable",
+      license = "GPL",
+      packages=["django_loggable"],
+      keywords="django created_at updated_at deleted_at fields models django-admin",
+)
