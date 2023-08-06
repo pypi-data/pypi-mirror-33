6edac9bc7654b7a3b777619ca568a4e7915fa996
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:49:20 +0000

    Add package gatktool/0.0.1/gatktool-0.0.1.tar.gz

---
+++ packages/gatktool/gatktool-0.0.1.tar.gz/gatktool-0.0.1/setup.py
@@ -0,0 +1,10 @@
+from distutils.core import setup
+
+setup(name='gatktool',
+      version='0.0.1',
+      description='Functions and classes used to extend a GATK tool with Python.',
+      author='GATK Team',
+      author_email='sam@broadinstitute.org',
+      packages=['gatktool'],
+      url='https://broadinstitute.org/'
+      )
