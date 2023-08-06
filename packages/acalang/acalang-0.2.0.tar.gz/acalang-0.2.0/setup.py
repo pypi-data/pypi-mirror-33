7b59c0ce444b8e36136c8ef5e7174233edd5bd3e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:11:36 +0000

    Add package acalang/0.2.0/acalang-0.2.0.tar.gz

---
+++ packages/acalang/acalang-0.2.0.tar.gz/acalang-0.2.0/setup.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python3
+
+"""Aca setup script"""
+
+from setuptools import setup, find_packages
+from aca.aca import __VERSION__
+
+with open("README.md", "r") as f:
+    LONG_DESCRIPTION = f.read()
+
+setup(
+    name="acalang",
+    version=__VERSION__,
+    description="Aca, a functional programming language, and shitty toy",
+    long_description=LONG_DESCRIPTION,
+    long_description_content_type="text/markdown",
+    license="MIT",
+    author="AnqurVanillapy",
+    author_email="anqurvanillapy@gmail.com",
+    url="https://github.com/anqurvanillapy/acalang",
+    entry_points={"console_scripts": ["aca=aca.aca:main"]},
+    packages=["aca"],
+    classifiers=(
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ),
+)
