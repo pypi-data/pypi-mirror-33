1b9e435a9682429abc5f5bca61e21bcb59b1faed
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:18:03 +0000

    Add package Yuci-Dictionary/0.1.5/Yuci_Dictionary-0.1.5-py3-none-any.whl

---
+++ packages/Yuci-Dictionary/Yuci_Dictionary-0.1.5-py3-none-any.whl/dictionary/argument.py
@@ -0,0 +1,13 @@
+import argparse as ap
+
+
+def get_argument():
+    parser = ap.ArgumentParser(description='test')
+    parser.add_argument('word', action='store', type=str, help='The word that you want to search.')
+    parser.add_argument('-n', '--deepth', action='store', type=int, default=5, help='Number of examples, default 5.')
+    parser.add_argument('--history', action='store', type=bool, default=True, help='Search word from history. True for enable, False for disable.')
+    args = parser.parse_args()
+    return args
+
+if __name__ == '__main__':
+    args = get_argument()
