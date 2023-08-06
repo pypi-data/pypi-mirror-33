4ae1728a3b76671d3ef6bf7737bc2fa67855b886
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:22:04 +0000

    Add package shoppinglist/1.0.4/shoppinglist-1.0.4.tar.gz

---
+++ packages/shoppinglist/shoppinglist-1.0.4.tar.gz/shoppinglist-1.0.4/setup.py
@@ -0,0 +1,35 @@
+from setuptools import setup
+from os.path import expanduser
+
+def readme():
+    with open('README.md') as f:
+        return f.read()
+
+setup(
+    name='shoppinglist',
+    version='1.0.4',
+    description='Quickly different combinations of prices and spare money',
+    long_description=readme(),
+    keywords='shopping calculator cost saving money shopping_list cli awesome',
+    classifiers=[
+        'Environment :: Console',
+        'Intended Audience :: Developers',
+        'Intended Audience :: End Users/Desktop',
+        'License :: OSI Approved :: MIT License',
+        'Topic :: Utilities',
+        'Topic :: Office/Business :: Financial',
+        'Topic :: Home Automation'
+    ],
+    url='https://floriankempenich.github.io/Shopping-List',
+    author='Florian Kempenich',
+    author_email='Flori@nKempenich.com',
+    packages=['shoppinglist'],
+    license='MIT',
+    scripts=['bin/shoppinglist'],
+    install_requires=[
+        'click',
+        'pyyaml'
+    ],
+    include_package_data=True,
+    zip_safe=True
+)
