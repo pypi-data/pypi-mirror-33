c032654ff3f5ce994131ffe1bca1645c0b74e3c4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:24:50 +0000

    Add package futhark-ffi/0.6.1b1/futhark_ffi-0.6.1b1-py3.6.egg

---
+++ packages/futhark-ffi/futhark_ffi-0.6.1b1-py3.6.egg/futhark_ffi/build.py
@@ -0,0 +1,22 @@
+import sys
+from cffi import FFI
+
+def strip_includes(lines):
+    return '\n'.join(line for line in lines if not line.startswith('#'))
+
+def build(name):
+    ffibuilder = FFI()
+
+    with open(name+'.c') as source:
+        ffibuilder.set_source('_'+name, source.read(), libraries=['OpenCL'])
+
+    with open(name+'.h') as header:
+        cdef = strip_includes(header)
+        ffibuilder.cdef(cdef)
+
+    return ffibuilder
+
+def main():
+    name = sys.argv[1]
+    ffi = build(name)
+    ffi.compile()
