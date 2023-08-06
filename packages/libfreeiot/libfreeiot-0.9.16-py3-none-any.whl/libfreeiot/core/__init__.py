3e0aebbb0c1f82dfaf378e9cafa86e7a33f31357
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 07:14:30 +0000

    Add package libfreeiot/0.9.16/libfreeiot-0.9.16-py3-none-any.whl

---
+++ packages/libfreeiot/libfreeiot-0.9.16-py3-none-any.whl/libfreeiot/core/__init__.py
@@ -0,0 +1,30 @@
+"""
+    FreeIOT Core Package Initial Script
+
+    Author: Noah Gao
+    Updated at: 2018-02-23
+"""
+from flask import Flask
+from flask_pymongo import PyMongo
+from libfreeiot.config import CONFIG
+
+mongo = PyMongo()
+
+def create_app(config_name, scope = None):
+    """
+        Function for create Flask App instance
+    """
+    if scope is None:
+        scope = dict()
+    app = Flask(__name__) # Initialize app
+
+    # Import project config
+    app.config.from_object(CONFIG[config_name])
+    CONFIG[config_name].init_app(app)
+
+    # Init MongoDB Ext
+    mongo.init_app(app)
+
+    from libfreeiot.core.routes import create_routes
+    app, api = create_routes(app, scope) # Initialize api services with Routes definition
+    return app
