62f7ad0c2296f5a3012918fcec7884501abf3ffa
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:52:19 +0000

    Add package radis/0.2.2/radis-0.2.2-py2.py3-none-any.whl

---
+++ packages/radis/radis-0.2.2-py2.py3-none-any.whl/radis/spectrum/__init__.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+"""
+Spectrum-class module 
+"""
+
+from .spectrum import (Spectrum, calculated_spectrum, experimental_spectrum,
+                       transmittance_spectrum, is_spectrum)
+from .compare import (plot_diff, get_diff, get_distance, get_ratio, get_residual,
+                      get_residual_integral)
+from .operations import (substract, multiply)
