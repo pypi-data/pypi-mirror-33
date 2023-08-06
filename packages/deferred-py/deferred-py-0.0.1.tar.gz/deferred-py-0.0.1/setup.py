1cc9c3785677abe39214f7ba10f58faa11025f1f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:13:28 +0000

    Add package deferred-py/0.0.1/deferred-py-0.0.1.tar.gz

---
+++ packages/deferred-py/deferred-py-0.0.1.tar.gz/deferred-py-0.0.1/setup.py
@@ -0,0 +1,16 @@
+
+from setuptools import setup, find_packages
+
+setup(
+	name='deferred-py',
+	version='0.0.1',
+	url='https://github.com/everclear72216/deferred-py',
+	description='A Javascript-style defer() implementation for promises in multi-threaded Python applications.',
+	author='Martin Bertsche',
+	author_email='martin72216@googlemail.com',
+	license='MIT',
+	classifiers=[
+		'Development Status :: 4 - Beta'
+	],
+	packages=find_packages()
+)
