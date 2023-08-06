e3d534560f03c19c5dfded1bf08b905b0307e8e5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:18:03 +0000

    Add package rich-text-diff/0.0.1/rich_text_diff-0.0.1.tar.gz

---
+++ packages/rich-text-diff/rich_text_diff-0.0.1.tar.gz/rich_text_diff-0.0.1/setup.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+setup(
+    name='rich_text_diff',
+    version='0.0.1',
+    author='liukai',
+    author_email='liukai@zhihu.com',
+    packages=['diff'],
+    test_suite='nose.collector',
+    url='https://github.com/c1ay/diff',
+    tests_require=['nose'],
+    install_requires=[
+        'lxml',
+        'diff_match_patch',
+        'bidict',
+    ]
+)
