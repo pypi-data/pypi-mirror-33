dd23203062c57301feebee9f5f4aff7eaebd0a89
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 05:06:50 +0000

    Add package wip_hello_world/0.1/wip_hello_world-0.1.tar.gz

---
+++ packages/wip_hello_world/wip_hello_world-0.1.tar.gz/wip_hello_world-0.1/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='wip_hello_world',
+      version='0.1',
+      description='Simple hello world',
+      url='',
+      author='Hugo',
+      author_email='hugo@example.com',
+      license='MIT',
+      packages=['wip_hello_world'],
+      zip_safe=False)
