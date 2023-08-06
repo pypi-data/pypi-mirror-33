d21e1b946b374abbfeed0d1e7567378b4a2f3e7c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:43:10 +0000

    Add package nosees/0.0.1/nosees-0.0.1-py2.7.egg

---
+++ packages/nosees/nosees-0.0.1-py2.7.egg/nosees/plugin.py
@@ -0,0 +1,29 @@
+# -*- coding: utf8 -*-
+
+import logging
+import os
+
+
+from nose.plugins import Plugin
+from mock import patch
+
+from nosees.mock import mock_es
+
+
+log = logging.getLogger('nose.plugins.nosees')
+
+
+class FakeElasticsearchPlugin(Plugin):
+    name = 'noes-es'
+
+    def options(self, parser, env=os.environ):
+        super(FakeElasticsearchPlugin, self).options(parser, env=env)
+
+    def configure(self, options, conf):
+        super(FakeElasticsearchPlugin, self).configure(options, conf)
+        if not self.enabled:
+            return
+
+    def begin(self):
+        if self.enabled:
+            mock_es()
