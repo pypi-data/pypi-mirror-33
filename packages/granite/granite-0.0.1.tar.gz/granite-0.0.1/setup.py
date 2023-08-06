e2c705b9623dde0c5f1a57ed5df729701269ddb5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:54:54 +0000

    Add package granite/0.0.1/granite-0.0.1.tar.gz

---
+++ packages/granite/granite-0.0.1.tar.gz/granite-0.0.1/setup.py
@@ -0,0 +1,25 @@
+import sys
+
+from setuptools import setup
+
+dependencies = [
+    'jinja2>=2.10',
+]
+
+if sys.version_info <= (3, 0):
+    dependencies.append('mock>=2.0')
+
+setup(
+    name='granite',
+    description='Augments the unittest TestCase API by providing additional utilities useful for Python tools.',
+    author_email='aaron@aaronboman.com',
+    author='Aaron Boman',
+    license='MIT',
+    url='https://github.com/frenchtoast747/granite',
+    version='0.0.1',
+    long_description=__doc__,
+    packages=['granite'],
+    include_package_data=True,
+    zip_safe=True,
+    install_requires=dependencies
+)
