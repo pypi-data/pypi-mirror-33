ce3661bc8191b2220aeaa49ede0973b34b7ed5d9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:14:04 +0000

    Add package Dero/0.12.0/Dero-0.12.0.tar.gz

---
+++ packages/Dero/Dero-0.12.0.tar.gz/Dero-0.12.0/dero/manager/pipelines/models/interfaces.py
@@ -0,0 +1,12 @@
+from typing import Callable, Union, List, Dict
+
+from dero.manager.basemodels.pipeline import Pipeline
+
+PipelineOrFunction = Union[Pipeline, Callable]
+PipelineOrFunctionOrCollection = Union[PipelineOrFunction, 'PipelineCollection']
+PipelinesOrFunctions = List[PipelineOrFunction]
+PipelinesOrFunctionsOrCollections = List[PipelineOrFunctionOrCollection]
+PipelineDict = Dict[str, Union[PipelinesOrFunctions, 'PipelineDict']]
+PipelineDictOrPipelineOrFunction = Union[PipelineDict, PipelineOrFunction]
+PipelineDictsOrPipelinesOrFunctions = List[PipelineDictOrPipelineOrFunction]
+StrList = List[str]
