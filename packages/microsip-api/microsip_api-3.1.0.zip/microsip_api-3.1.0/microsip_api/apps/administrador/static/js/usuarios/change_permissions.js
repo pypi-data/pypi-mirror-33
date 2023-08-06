7c2c3e59adfe4c8777a08091b3ffa97ebf4184f1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:17:24 +0000

    Add package microsip-api/3.1.0/microsip_api-3.1.0.zip

---
+++ packages/microsip-api/microsip_api-3.1.0.zip/microsip_api-3.1.0/microsip_api/apps/administrador/static/js/usuarios/change_permissions.js
@@ -0,0 +1,54 @@
+function cambia_permiso(permiso_id, checked)
+{
+  var django_user_id = $("#django_user_id").val();
+  $.ajax({
+    url:'/administrador/change_user_permission/',
+    type : 'get',
+    data:{
+      'permiso_id' : permiso_id,
+      'django_user_id': django_user_id,
+      'checked':checked
+    },
+    error: function() {
+      alert('algo fallo');
+    },
+  });
+}
+
+$.ajax({
+  url:'/administrador/get_apppsermissions/',
+  type : 'get',
+  data:{'username': $("#django_username").val()},
+  success: function (data) {
+    $("#permissions").jstree({
+      "core" : {
+          "themes" : {
+            "variant" : "small"
+          },
+          'data' : data,
+        },
+       "types" : {
+          "default" : {
+            "icon" : "glyphicon glyphicon-co"
+          },
+          "demo" : {
+            "icon" : "glyphicon glyphicon-co"
+          }
+        },
+       "plugins" : ["checkbox", "types",]
+     }).on('select_node.jstree', function (e, data) {
+        if ($.isNumeric(data.node.id)){
+          cambia_permiso(data.node.id, true);
+        }
+    }).on('deselect_node.jstree', function (e, data) {
+        if ($.isNumeric(data.node.id)){
+          cambia_permiso(data.node.id, false);
+        }
+    });
+  },
+  error: function() {
+    alert('algo fallo');
+  },
+});
+
+$(".permiso_chbox").on('change', cambia_permiso);
