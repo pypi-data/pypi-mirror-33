a3a2fc34c807990b8ae582f56be065640097738f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:36:41 +0000

    Add package goldenhinges/0.1.1/goldenhinges-0.1.1.tar.gz

---
+++ packages/goldenhinges/goldenhinges-0.1.1.tar.gz/goldenhinges-0.1.1/setup.py
@@ -0,0 +1,19 @@
+import ez_setup
+ez_setup.use_setuptools()
+
+from setuptools import setup, find_packages
+
+exec(open('goldenhinges/version.py').read()) # loads __version__
+
+setup(
+	name='goldenhinges',
+    version=__version__,
+    author='Zulko',
+    description='',
+    long_description=open('pypi-readme.rst').read(),
+    license='see LICENSE.txt',
+    keywords="golden gate assembly overhangs constraint programming",
+    packages= find_packages(exclude='docs'),
+    install_requires=['numpy', 'Numberjack', 'Biopython', 'networkx',
+	                  'tqdm', 'dnachisel']
+)
