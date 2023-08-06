84bbb3d9da4895ee9290780a14b13c0306bd3f64
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:24:42 +0000

    Add package deployml/0.0.1/deployml-0.0.1-py3-none-any.whl

---
+++ packages/deployml/deployml-0.0.1-py3-none-any.whl/deployml/sklearn/__init__.py
@@ -0,0 +1,7 @@
+"""Sk-learn module."""
+
+from deployml.sklearn.models.decision_tree import DecisionTree
+
+from deployml.sklearn.models.logistic_regression import LogisticRegressionBase
+
+from deployml.sklearn.models.neural_network import NeuralNetworkBase
