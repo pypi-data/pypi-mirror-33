81075ff990a7de1d16103cc8fd835f87bd48be30
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:51:06 +0000

    Add package tuning-fork/1.3/tuning_fork-1.3.tar.gz

---
+++ packages/tuning-fork/tuning_fork-1.3.tar.gz/tuning_fork-1.3/setup.py
@@ -0,0 +1,23 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="tuning_fork",
+    version="1.3",
+    author="Garrett Credi",
+    author_email="gcc@ameritech.net",
+    description="A clip/sample auto tuner",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/ddxtanx/TuningFork",
+    packages=["tuning_fork", "tuning_fork.tools"],
+    py_modules=["tuning_fork.main"],
+    classifiers=(
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ),
+    install_requires=["librosa", "scipy", "numpy"]
+)
