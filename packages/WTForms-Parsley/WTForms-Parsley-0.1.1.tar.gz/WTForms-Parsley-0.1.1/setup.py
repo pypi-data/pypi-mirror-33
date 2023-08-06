3a6d2dfd2d6dcdc0437dd28f1f1759af87d5b99a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 07:14:07 +0000

    Add package WTForms-Parsley/0.1.1/WTForms-Parsley-0.1.1.tar.gz

---
+++ packages/WTForms-Parsley/WTForms-Parsley-0.1.1.tar.gz/WTForms-Parsley-0.1.1/setup.py
@@ -0,0 +1,23 @@
+#
+# WTForms-Parsley
+#
+# Copyright (C) 2017 Boris Raicheff
+# All rights reserved
+#
+
+
+from setuptools import find_packages, setup
+
+
+setup(
+    name='WTForms-Parsley',
+    version='0.1.1',
+    description='WTForms-Parsley',
+    author='Boris Raicheff',
+    author_email='b@raicheff.com',
+    url='https://github.com/raicheff/wtforms-parsley',
+    py_modules=('wtforms_parsley',),
+)
+
+
+# EOF
