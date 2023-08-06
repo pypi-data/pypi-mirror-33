897523013fa52badb7e55b151aa14442f6bfa005
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:48:24 +0000

    Add package pyspedas/0.6.16/pyspedas-0.6.16-py2.py3-none-any.whl

---
+++ packages/pyspedas/pyspedas-0.6.16-py2.py3-none-any.whl/pyspedas/examples/basic/ex_omni.py
@@ -0,0 +1,28 @@
+# -*- coding: utf-8 -*-
+"""
+File: 
+    ex_omni.py
+
+Desrciption:  
+    Basic example of plotting OMNI data.
+    
+"""
+
+import pyspedas
+import pytplot
+
+def ex_omni():
+    # Print the installed version of pyspedas
+    pyspedas.get_spedas_prefs() 
+    # Delete any existing pytplot variables
+    pytplot.del_data()     
+    # Download OMNI data for 2015-12-31
+    pyspedas.load_data('omni', ['2015-12-31', '2016-01-01'], '', '', '1min')
+    # OMNI loader downloads a full month of data, apply time_clip for a single day
+    pyspedas.time_clip('flow_speed', '2015-12-31 00:00:00', '2016-01-01 23:59:59')
+    # Plot
+    pytplot.tplot_options('title', 'OMNI flow_speed 2015-12-31') 
+    pytplot.tplot(['flow_speed-tclip'])
+
+#Run the example code
+#ex_omni()
