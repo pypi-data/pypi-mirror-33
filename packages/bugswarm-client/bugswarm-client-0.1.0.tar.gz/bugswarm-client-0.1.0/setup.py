f725d7ebe1c96ab174fc482cd0e6f13763f48e1e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:49:24 +0000

    Add package bugswarm-client/0.1.0/bugswarm-client-0.1.0.tar.gz

---
+++ packages/bugswarm-client/bugswarm-client-0.1.0.tar.gz/bugswarm-client-0.1.0/setup.py
@@ -0,0 +1,33 @@
+from setuptools import setup
+from setuptools import find_packages
+
+setup(
+    name='bugswarm-client',
+    version='0.1.0',
+    url='https://github.com/BugSwarm/client',
+    author='BugSwarm',
+    author_email='dev.bugswarm@gmail.com',
+
+    description='The official command line client for the BugSwarm artifact dataset',
+    long_description='The official command line client for the BugSwarm artifact dataset',
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: BSD License',
+    ],
+    zip_safe=False,
+    packages=find_packages(),
+    namespace_packages=[
+        'bugswarm',
+    ],
+    install_requires=[
+        'Click==6.7',
+        'requests==2.18.4',
+        'bugswarm-common==0.0.3',
+    ],
+
+    entry_points={
+        'console_scripts': [
+            'bugswarm = bugswarm.client.bugswarm:cli',
+        ],
+    },
+)
