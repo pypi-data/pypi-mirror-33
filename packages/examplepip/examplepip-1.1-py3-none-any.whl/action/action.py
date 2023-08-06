6f2dc92e7b4f7e3b612d9e39db951b2efd2d77de
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:56:50 +0000

    Add package examplepip/1.1/examplepip-1.1-py3-none-any.whl

---
+++ packages/examplepip/examplepip-1.1-py3-none-any.whl/action/action.py
@@ -0,0 +1,8 @@
+def win():
+    print("This is how you win!")
+def loser():
+    print("Now you really lost!")
+def good():
+    print("Wow this is a good guy!")
+name = 10
+lol = 'winner,winner,chicken,dinner'
