cac196ac1653d0809d2a9a0d229f81c447ea691d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:26:30 +0000

    Add package tapioca-camara/0.2.0/tapioca-camara-0.2.0.tar.gz

---
+++ packages/tapioca-camara/tapioca-camara-0.2.0.tar.gz/tapioca-camara-0.2.0/tapioca_camara/resource_mapping.py
@@ -0,0 +1,38 @@
+# coding: utf-8
+
+RESOURCE_MAPPING = {
+    # Deputados
+    'deputados': {
+        'resource': 'deputados',
+        'docs': 'https://dadosabertos.camara.leg.br/swagger/api.html#api'
+    },
+    'deputado': {
+        'resource': 'deputados/{id}',
+        'docs': 'https://dadosabertos.camara.leg.br/swagger/api.html#api'
+    },
+    # Órgãos
+    'orgaos': {
+        'resource': 'orgaos',
+        'docs': 'https://dadosabertos.camara.leg.br/swagger/api.html#api'
+    },
+    'orgao': {
+        'resource': 'orgaos/{id}',
+        'docs': 'https://dadosabertos.camara.leg.br/swagger/api.html#api'
+    },
+    'orgao_eventos': {
+        'resource': 'orgaos/{id}/eventos',
+        'docs': 'https://dadosabertos.camara.leg.br/swagger/api.html#api'
+    },
+    'orgao_membros': {
+        'resource': 'orgaos/{id}/membros',
+        'docs': 'https://dadosabertos.camara.leg.br/swagger/api.html#api'
+    },
+    'orgaos_tipos': {
+        'resource': 'referencias/tiposOrgao',
+        'docs': 'https://dadosabertos.camara.leg.br/swagger/api.html#api'
+    },
+    'orgaos_situacoes': {
+        'resource': 'referencias/situacoesOrgao',
+        'docs': 'https://dadosabertos.camara.leg.br/swagger/api.html#api'
+    },
+}
