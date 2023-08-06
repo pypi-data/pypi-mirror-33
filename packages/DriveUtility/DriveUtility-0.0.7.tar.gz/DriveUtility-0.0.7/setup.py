90052eb8f62e1ba3662cdf38e1c37e26254bff9c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 07:12:50 +0000

    Add package DriveUtility/0.0.7/DriveUtility-0.0.7.tar.gz

---
+++ packages/DriveUtility/DriveUtility-0.0.7.tar.gz/DriveUtility-0.0.7/setup.py
@@ -0,0 +1,42 @@
+# !/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+#
+#
+# Copyright (c) 2018 Pedro Gabaldon
+#
+#
+# Licensed under MIT License. See LICENSE
+#
+#
+
+import setuptools
+
+requires=["oauth2client", "httplib2", "google-api-python-client"]
+scripts = "DriveUtil"
+
+with open("README.md", "r") as f:
+    long_description = f.read()
+
+setuptools.setup(
+	name="DriveUtility",
+	version="0.0.7",
+	author="Pedro Gabaldon Julia",
+	author_email="petergj@protonmail.com",
+	description="Google Drive tool",
+	long_description=long_description,
+    long_description_content_type="text/markdown",
+	url="https://github.com/PeterGabaldon/DriveUtility",
+	scripts=["bin/DriveUtil.py"],
+	license="MIT",
+	include_package_data=True,
+	install_requires=requires,
+	packages=setuptools.find_packages(),
+	package_data = {'' : ['*.json', '*.bat']},
+	classifiers=(
+		"Programming Language :: Python :: 2.7",
+		"License :: OSI Approved :: MIT License",
+		"Development Status :: 4 - Beta",
+		)
+
+	)
