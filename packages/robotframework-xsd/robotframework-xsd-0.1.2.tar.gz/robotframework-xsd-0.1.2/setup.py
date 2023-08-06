9612d8f840020ba617a8287f275b838609c9bcf1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:26:30 +0000

    Add package robotframework-xsd/0.1.2/robotframework-xsd-0.1.2.tar.gz

---
+++ packages/robotframework-xsd/robotframework-xsd-0.1.2.tar.gz/robotframework-xsd-0.1.2/setup.py
@@ -0,0 +1,31 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+setup(
+    name='robotframework-xsd',
+    version='0.1.2',
+    description=(
+        'xsd for robotframework'
+    ),
+    author='mangobowl',
+    author_email='mangobowl@163.com',
+    license='BSD License',
+    packages=['XsdLibrary'],
+    install_requires=[
+        'robotframework<3.0',
+        'xmlschema<1.0',
+        'validators'
+    ],
+    platforms=["all"],
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Operating System :: OS Independent',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: BSD License',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: Implementation',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.7',
+        'Topic :: Software Development :: Libraries'
+    ],
+)
