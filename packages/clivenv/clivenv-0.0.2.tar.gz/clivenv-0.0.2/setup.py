471f4504be9c908f0737dab5ad0365aeddf6e77e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:24:07 +0000

    Add package clivenv/0.0.2/clivenv-0.0.2.tar.gz

---
+++ packages/clivenv/clivenv-0.0.2.tar.gz/clivenv-0.0.2/setup.py
@@ -0,0 +1,25 @@
+import setuptools
+from os.path import join
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="clivenv",
+    version="0.0.2",
+    author="duckingod",
+    author_email="kingoduck@gmail.com",
+    description="Fast access/manage to virtualenv between different python versions.",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/duckingod/clivenv",
+    packages=setuptools.find_packages(),
+    install_requires=['virtualenv'],
+    classifiers=(
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Environment :: Console"
+    ),
+    scripts=[join('venv', 'venv'), join('venv', 'venv.py'), join('venv', 'venv_pip_recording')],
+)
+
