1f50eccd55f7d05ff2cb3201752476f7e3ac36e2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:18:10 +0000

    Add package fletcher/0.1.2/fletcher-0.1.2-py2.py3-none-any.whl

---
+++ packages/fletcher/fletcher-0.1.2-py2.py3-none-any.whl/fletcher/_algorithms.py
@@ -0,0 +1,159 @@
+# -*- coding: utf-8 -*-
+
+from __future__ import absolute_import, division, print_function
+
+import numba
+import numpy as np
+
+from ._numba_compat import NumbaStringArray
+
+
+@numba.jit(nogil=True, nopython=True)
+def _extract_isnull_bytemap(bitmap, bitmap_length, bitmap_offset, dst_offset, dst):
+    """
+    (internal) write the values of a valid bitmap as bytes to a pre-allocatored
+    isnull bytemap.
+
+    Parameters
+    ----------
+    bitmap: pyarrow.Buffer
+        bitmap where a set bit indicates that a value is valid
+    bitmap_length: int
+        Number of bits to read from the bitmap
+    bitmap_offset: int
+        Number of bits to skip from the beginning of the bitmap.
+    dst_offset: int
+        Number of bytes to skip from the beginning of the output
+    dst: numpy.array(dtype=bool)
+        Pre-allocated numpy array where a byte is set when a value is null
+    """
+    for i in range(bitmap_length):
+        idx = bitmap_offset + i
+        byte_idx = idx // 8
+        bit_mask = 1 << (idx % 8)
+        dst[dst_offset + i] = (bitmap[byte_idx] & bit_mask) == 0
+
+
+def extract_isnull_bytemap(chunked_array):
+    """
+    Extract the valid bitmaps of a chunked array into numpy isnull bytemaps.
+
+    Parameters
+    ----------
+    chunked_array: pyarrow.ChunkedArray
+
+    Returns
+    -------
+    valid_bytemap: numpy.array
+    """
+    # TODO: Can we use np.empty here to improve performance?
+    result = np.zeros(len(chunked_array), dtype=bool)
+
+    offset = 0
+    for chunk in chunked_array.chunks:
+        valid_bitmap = chunk.buffers()[0]
+        if valid_bitmap:
+            # TODO(ARROW-2664): We only need to following line to support
+            #   executing the code in disabled-JIT mode.
+            buf = memoryview(valid_bitmap)
+            _extract_isnull_bytemap(buf, len(chunk), chunk.offset, offset, result)
+        else:
+            return np.full(len(chunked_array), False)
+        offset += len(chunk)
+
+    return result
+
+
+@numba.jit(nogil=True, nopython=True)
+def isnull(sa):
+    result = np.empty(sa.size, np.uint8)
+    _isnull(sa, 0, result)
+    return result
+
+
+@numba.jit(nogil=True, nopython=True)
+def _isnull(sa, offset, out):
+    for i in range(sa.size):
+        out[offset + i] = sa.isnull(i)
+
+
+@numba.jit(nogil=True, nopython=True)
+def _startswith(sa, needle, na, offset, out):
+    for i in range(sa.size):
+        if sa.isnull(i):
+            out[offset + i] = na
+            continue
+
+        if sa.byte_length(i) < needle.length:
+            out[offset + i] = 0
+            continue
+
+        for j in range(needle.length):
+            if sa.get_byte(i, j) != needle.get_byte(j):
+                out[offset + i] = 0
+                break
+
+        else:
+            out[offset + i] = 1
+
+
+@numba.jit(nogil=True, nopython=True)
+def _endswith(sa, needle, na, offset, out):
+    for i in range(sa.size):
+        if sa.isnull(i):
+            out[offset + i] = na
+            continue
+
+        string_length = sa.byte_length(i)
+        needle_length = needle.length
+        if string_length < needle.length:
+            out[offset + i] = 0
+            continue
+
+        for j in range(needle_length):
+            if sa.get_byte(i, string_length - needle_length + j) != needle.get_byte(j):
+                out[offset + i] = 0
+                break
+
+        else:
+            out[offset + i] = 1
+
+
+@numba.jit(nogil=True, nopython=True)
+def str_length(sa):
+    result = np.empty(sa.size, np.uint32)
+
+    for i in range(sa.size):
+        result[i] = sa.length(i)
+
+    return result
+
+
+@numba.jit(nogil=True, nopython=True)
+def str_concat(sa1, sa2):
+    # TODO: check overflow of size
+    assert sa1.size == sa2.size
+
+    result_missing = sa1.missing | sa2.missing
+    result_offsets = np.zeros(sa1.size + 1, np.uint32)
+    result_data = np.zeros(sa1.byte_size + sa2.byte_size, np.uint8)
+
+    offset = 0
+    for i in range(sa1.size):
+        if sa1.isnull(i) or sa2.isnull(i):
+            result_offsets[i + 1] = offset
+            continue
+
+        for j in range(sa1.byte_length(i)):
+            result_data[offset] = sa1.get_byte(i, j)
+            offset += 1
+
+        for j in range(sa2.byte_length(i)):
+            result_data[offset] = sa2.get_byte(i, j)
+            offset += 1
+
+        result_offsets[i + 1] = offset
+
+    result_data = result_data[:offset]
+
+    return NumbaStringArray(result_missing, result_offsets, result_data, 0)
