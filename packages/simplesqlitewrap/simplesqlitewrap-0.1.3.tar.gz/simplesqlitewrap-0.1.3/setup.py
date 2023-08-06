1994f9982fe549043df5133f3666ed30542795e2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:34:55 +0000

    Add package simplesqlitewrap/0.1.3/simplesqlitewrap-0.1.3.tar.gz

---
+++ packages/simplesqlitewrap/simplesqlitewrap-0.1.3.tar.gz/simplesqlitewrap-0.1.3/setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(name='simplesqlitewrap',
+      version='0.1.3',
+      description='Simple util from which I inherit my sqlite classes',
+      url='http://github.com/storborg/funniest',
+      author='zeroone2numeral2',
+      license='MIT',
+      packages=['simplesqlitewrap'],
+      zip_safe=False)
