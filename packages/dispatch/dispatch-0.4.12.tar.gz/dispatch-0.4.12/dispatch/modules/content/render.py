91fdde4d3cab5b31f9b8b07e916fa9c998c7a674
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:49:39 +0000

    Add package dispatch/0.4.12/dispatch-0.4.12.tar.gz

---
+++ packages/dispatch/dispatch-0.4.12.tar.gz/dispatch-0.4.12/dispatch/modules/content/render.py
@@ -0,0 +1,39 @@
+from django.utils.safestring import mark_safe
+
+from dispatch.modules.content.embeds import embeds, EmbedException
+
+def content_to_html(content):
+    """Returns artilce/page content as HTML"""
+
+    def render_node(html, node):
+        """Renders node as HTML"""
+
+        if node['type'] == 'paragraph':
+            return html + '<p>%s</p>' % node['data']
+        else:
+            try:
+                if node['type'] == 'poll':
+                    node['type'] = 'widget'
+                    node['data']['data'] = node['data']
+
+                return html + embeds.render(node['type'], node['data'])
+            except EmbedException:
+                return html
+
+    return mark_safe(reduce(render_node, content, ''))
+
+def content_to_json(content):
+    """Returns artilce/page content as JSON"""
+
+    def render_node(node):
+        """Renders node as JSON"""
+
+        if node['type'] == 'paragraph':
+            return node
+        else:
+            return {
+                'type': node['type'],
+                'data': embeds.to_json(node['type'], node['data'])
+            }
+
+    return map(render_node, content)
