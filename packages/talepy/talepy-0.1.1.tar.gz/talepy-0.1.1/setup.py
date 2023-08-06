d17f23fed18e7af7f4fd7c667ca50dbeae0e978f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:25:24 +0000

    Add package talepy/0.1.1/talepy-0.1.1.tar.gz

---
+++ packages/talepy/talepy-0.1.1.tar.gz/talepy-0.1.1/setup.py
@@ -0,0 +1,23 @@
+import os
+from distutils.core import setup
+
+def read(fname):
+    return open(os.path.join(os.path.dirname(__file__), fname)).read()
+
+setup(
+    name = "talepy",
+    packages = ["talepy"],
+    version = "0.1.1",
+    description = "Distributed Transactions Helper",
+    author = "Steve Brazier",
+    author_email = "meadsteve@gmail.com",
+    url = "https://github.com/meadsteve/talepy",
+    keywords = ["saga", "distributed transaction"],
+    classifiers = [
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 3",
+        "Development Status :: 4 - Beta",
+        ],
+    long_description = read("README.md"),
+    long_description_content_type='text/markdown'
+)
