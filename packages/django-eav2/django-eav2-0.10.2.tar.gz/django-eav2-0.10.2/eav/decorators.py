09946a96e894196f3be60ea7375cc4d6e98b6a69
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:34:48 +0000

    Add package django-eav2/0.10.2/django-eav2-0.10.2.tar.gz

---
+++ packages/django-eav2/django-eav2-0.10.2.tar.gz/django-eav2-0.10.2/eav/decorators.py
@@ -0,0 +1,26 @@
+'''
+Decorators.
+
+This module contains pure wrapper functions used as decorators.
+Functions in this module should be simple and not involve complex logic.
+'''
+
+def register_eav(**kwargs):
+    '''
+    Registers the given model(s) classes and wrapped Model class with
+    django-eav::
+
+        @register_eav
+        class Author(models.Model):
+            pass
+    '''
+    from . import register
+    from django.db.models import Model
+
+    def _model_eav_wrapper(model_class):
+        if not issubclass(model_class, Model):
+            raise ValueError('Wrapped class must subclass Model.')
+        register(model_class, **kwargs)
+        return model_class
+
+    return _model_eav_wrapper
