8b72fd6b903a21460f99b9502e87e2a6f9ceae3c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:44:53 +0000

    Add package acdh-spacyal/0.2.18/acdh-spacyal-0.2.18.tar.gz

---
+++ packages/acdh-spacyal/acdh-spacyal-0.2.18.tar.gz/acdh-spacyal-0.2.18/spacyal/api_urls.py
@@ -0,0 +1,13 @@
+from django.urls import path
+from . import api_views
+
+app_name = 'spacyal_api'
+
+urlpatterns = [
+    path('retrievecases/', api_views.RetrieveCasesView.as_view(),
+         name='retrievecases'),
+    path('progress_model/', api_views.GetProgressModelView.as_view(),
+         name='progress_model'),
+    path('download_model/', api_views.DownloadModelView.as_view(),
+         name='download_model'),
+]
