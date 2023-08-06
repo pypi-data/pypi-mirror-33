f40eb861f7b035bd119e9bb62d2253ac294c2696
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:18:09 +0000

    Add package joca/1.0.0/joca-1.0.0.tar.gz

---
+++ packages/joca/joca-1.0.0.tar.gz/joca-1.0.0/setup.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python
+"""Setup package"""
+
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read
+
+setuptools.setup(
+    name="joca",
+    version="1.0.0",
+    author="Bryce McNab",
+    author_email="brycemcnab@pm.me",
+    description="Sync project lead with ical (for on call assignees)",
+    long_description=long_description,
+    url="https://www,github.com/betsythefc/joca",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 2.7",
+        "Operating System :: OS Independent",
+        "Development Status :: 5 - Production/Stable",
+        "License :: OSI Approved :: GNU General Public License v3 (GPLv3)",
+        "Topic :: Software Development :: Version Control :: Git",
+    ],
+) 
