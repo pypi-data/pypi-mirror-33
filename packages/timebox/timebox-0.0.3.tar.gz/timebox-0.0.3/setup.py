d46d3ca7bef41c9ccb47d00b13fbbcb867a70f84
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:18:04 +0000

    Add package timebox/0.0.3/timebox-0.0.3.tar.gz

---
+++ packages/timebox/timebox-0.0.3.tar.gz/timebox-0.0.3/setup.py
@@ -0,0 +1,29 @@
+import setuptools
+
+with open('README.md', 'r') as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name='timebox',
+    version='0.0.3',
+    author='Brian Kopp',
+    author_email='briankopp.usa@gmail.com',
+    description='An IO library for pandas',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    url='https://github.com/briankopp/timebox',
+    packages=setuptools.find_packages(),
+    install_requires=[
+        'numexpr',
+        'numpy',
+        'pandas',
+        'python-dateutil',
+        'pytz',
+        'six'
+    ],
+    classifier=(
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: Ubuntu'
+    ),
+)
