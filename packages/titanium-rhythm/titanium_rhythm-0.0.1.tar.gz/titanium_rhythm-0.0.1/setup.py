26a0f1d94cd78f8ac291caccf5b5063e35f21d6c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:22:17 +0000

    Add package titanium-rhythm/0.0.1/titanium_rhythm-0.0.1.tar.gz

---
+++ packages/titanium-rhythm/titanium_rhythm-0.0.1.tar.gz/titanium_rhythm-0.0.1/setup.py
@@ -0,0 +1,23 @@
+import setuptools
+
+# with open("README.md", "r") as fh:
+#    long_description = fh.read()
+
+setuptools.setup(
+    name="titanium_rhythm",
+    version="0.0.1",
+    author="Ganesh Kathiresan",
+    author_email="ganesh3597@gmail.com",
+    description="Automatic id3 modifier .mp3 files",
+#   long_description=long_description,
+#   long_description_content_type="text/markdown",
+    url="https://github.com/DarkFate13/titanium-rhythm",
+	packages=['titanium_rhythm'],
+	package_dir = {'titanium_rhythm': 'titanium_rhythm/'},
+	package_data={'titanium_rhythm': ['info/*.*', 'song_info/*.xml']},
+    classifiers=(
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ),
+)
