e96aa58cd4180338a8d60dc94230f8f813d7f81d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:14:48 +0000

    Add package tf-env/0.1.0/tf-env-0.1.0.tar.gz

---
+++ packages/tf-env/tf-env-0.1.0.tar.gz/tf-env-0.1.0/tf_env/__init__.py
@@ -0,0 +1,8 @@
+"""
+Reinforcement Learning environments in pure TensorFlow.
+"""
+
+from .anyrl_env import TFBatchedEnv
+from .base import TFEnv
+from .point_seeker import PointSeeker
+from .pong import Pong
