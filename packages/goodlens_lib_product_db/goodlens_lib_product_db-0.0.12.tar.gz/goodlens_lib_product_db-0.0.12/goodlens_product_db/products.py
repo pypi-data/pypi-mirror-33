a5d358fcf113995565bab277153eb287216ce468
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:36:49 +0000

    Add package goodlens_lib_product_db/0.0.12/goodlens_lib_product_db-0.0.12.tar.gz

---
+++ packages/goodlens_lib_product_db/goodlens_lib_product_db-0.0.12.tar.gz/goodlens_lib_product_db-0.0.12/goodlens_product_db/products.py
@@ -0,0 +1,34 @@
+from bson.objectid import ObjectId
+from goodlens_product_db.database import DataBase
+
+class Products(DataBase):
+  def __init__(self):
+    super().__init__()
+    self.products = self.db.products
+
+  def add_product_common(self, product_common):
+    product_id = None
+    try:
+      query = {"gtin": product_common.gtin}
+      r = self.products.update_one(query,
+                                  {"$set": product_common.to_dict()},
+                                  upsert=True)
+    except Exception as e:
+      print(e)
+
+    if 'upserted' in r.raw_result:
+      product_id = str(r.raw_result['upserted'])
+
+    return product_id
+
+  def update_product_common(self, product_id, product_common):
+    try:
+      query = {"_id": ObjectId(product_id)}
+      r = self.products.update_one(query,
+                                   {"$set": product_common.to_dict()},
+                                   upsert=False)
+    except Exception as e:
+      print(e)
+      return None
+
+    return r.raw_result
