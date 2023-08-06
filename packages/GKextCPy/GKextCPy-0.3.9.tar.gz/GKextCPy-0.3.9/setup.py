80c323eb95a1a5b8e4b480448c70de245760ac51
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 05:07:09 +0000

    Add package GKextCPy/0.3.9/GKextCPy-0.3.9.tar.gz

---
+++ packages/GKextCPy/GKextCPy-0.3.9.tar.gz/GKextCPy-0.3.9/setup.py
@@ -0,0 +1,51 @@
+#!/usr/bin/env python
+
+"""
+setup.py file for SWIG
+"""
+
+from setuptools import setup, Extension
+
+import subprocess
+import sys
+
+# Solve the chicken-and-egg problem of requiring packages *before* the
+# script has been parsed.
+for package in ['numpy', 'pkgconfig']:
+    if '--user' in sys.argv:
+        subprocess.check_call([sys.executable, '-m', 'pip', 'install', '--user', package])
+    else:
+        subprocess.check_call([sys.executable, '-m', 'pip', 'install', package])
+
+def get_include_dirs():
+    import pkgconfig
+    import numpy
+
+    if not pkgconfig.exists('eigen3'):
+        raise Exception('Missing `eigen3` library. Please install it using the package manager of your operating system')
+
+    np_include_dir = numpy.get_include()
+
+    # We have to throw away the `-I` part of the `pkgconfig` output
+    # because it is not part of the include directory.
+    eigen3_include_dir = pkgconfig.cflags('eigen3')[2:]
+
+    return [np_include_dir, eigen3_include_dir]
+
+GKextCPy_module = Extension('_GKextCPy',
+    sources = ['GKextCPy_wrap.cxx', 'GKextCPy.cpp'],
+    swig_opts = ['-c++'],
+    extra_compile_args = ['-std=c++11', '-O3'],
+    include_dirs = get_include_dirs()
+)
+
+setup(name = 'GKextCPy',
+    version = '0.3.9',
+    author = 'Elisabetta Ghisu',
+    description = """Graph Kernels: building the extension Python module. This is a wrapper package from C++ to Python.""",
+    ext_modules = [GKextCPy_module],
+    py_modules = ['GKextCPy'],
+    setup_requires = ['pkgconfig', 'numpy'],
+    install_requires = ['pkgconfig', 'numpy'],
+    license = 'ETH Zurich',
+)
