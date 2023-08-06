b9dbdff5d660a0ea96f1eed1ffef7d4337b1f472
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:50:04 +0000

    Add package mcfit/0.0.10/mcfit-0.0.10.tar.gz

---
+++ packages/mcfit/mcfit-0.0.10.tar.gz/mcfit-0.0.10/setup.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python
+from setuptools import setup
+
+setup(
+    name = 'mcfit',
+    version = '0.0.10',
+    description = 'Multiplicatively convolutional fast integral transforms',
+    url = 'https://github.com/eelregit/mcfit',
+    author = 'Yin Li',
+    author_email = 'eelregit@gmail.com',
+    license = 'GPLv3',
+    keywords = 'numerical integral transform FFTLog cosmology',
+    packages = ['mcfit', 'mcfit.tests'],
+    install_requires = ['numpy', 'scipy', 'mpmath'],
+)
