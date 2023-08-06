1554b77544c7629650834e6f4e1583ec0b325266
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 07:13:07 +0000

    Add package tester-gae/1.1.0/tester_gae-1.1.0.tar.gz

---
+++ packages/tester-gae/tester_gae-1.1.0.tar.gz/tester_gae-1.1.0/setup.py
@@ -0,0 +1,32 @@
+import setuptools
+
+
+long_description = """
+# test.helper.gae
+
+Class for autotests GoogleAppEngine platform python apps
+
+GAE Cloud SDK must be installed: https://cloud.google.com/sdk/
+
+`gcloud components install app-engine-python`
+
+"""
+
+setuptools.setup(
+    name = 'tester_gae',
+    version = '1.1.0',
+    author = 'Vitaly Bogomolov',
+    author_email = 'mail@vitaly-bogomolov.ru',
+    description = 'Class for autotests GoogleAppEngine platform python apps',
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url = 'https://github.com/vb64/test.helper.gae',
+    packages = ['tester_gae'],
+    download_url = 'https://github.com/vb64/test.helper.gae/archive/v1.1.tar.gz',
+    keywords = ['python', 'Python27', 'GAE', 'GoogleAppEngine', 'unittest'],
+    classifiers=[
+        "Programming Language :: Python :: 2.7",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+)
