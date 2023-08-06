389d2e003f0299daeec7ba1e983488c5aed7ed9b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:00:05 +0000

    Add package py3o.renderserver/0.5.2/py3o.renderserver-0.5.2.tar.gz

---
+++ packages/py3o.renderserver/py3o.renderserver-0.5.2.tar.gz/py3o.renderserver-0.5.2/setup.py
@@ -0,0 +1,43 @@
+# -*- encoding: utf-8 -*-
+from setuptools import setup, find_packages
+
+version = '0.5.2'
+
+setup(
+    name='py3o.renderserver',
+    version=version,
+    description=("An easy solution to transform "
+                 "libreoffice/openoffice documents to supported formats"),
+    long_description=open("README.rst").read(),
+    classifiers=[
+        "Programming Language :: Python",
+        "Topic :: Software Development :: Libraries :: Python Modules",
+    ],
+    keywords='LibreOffice OpenOffice PDF',
+    author='Florent Aide',
+    author_email='florent.aide@gmail.com',
+    url='http://bitbucket.org/faide/py3o.renderserver',
+    license='BSD License',
+    packages=find_packages(exclude=['ez_setup', 'examples', 'tests']),
+    namespace_packages=['py3o'],
+    include_package_data=True,
+    zip_safe=True,
+    install_requires=[
+        'setuptools',
+        'pyf.station >= 2.0.1',
+        'pyjon.utils >= 0.6',
+        'py3o.formats',
+        'py3o.renderers.juno >= 0.8.1',
+    ],
+    entry_points=dict(
+        # -*- Entry points: -*-
+        console_scripts=[
+            'start-py3o-renderserver = py3o.renderserver.server:cli_server',
+            'config-ooservice = ooservice.ooservice:config',
+            'setup-ooservice = ooservice.ooservice:setup',
+            'config-py3oservice = py3o.renderserver.service:config',
+            'setup-py3oservice = py3o.renderserver.service:setup',
+        ],
+    ),
+    test_suite = 'nose.collector',
+)
---
+++ packages/py3o.renderserver/py3o.renderserver-0.5.2.tar.gz/py3o.renderserver-0.5.2/start.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+start-py3o-renderserver --java=/usr/lib/jvm/java-8-openjdk-amd64/jre/lib/amd64/server/libjvm.so --ure=/usr/lib/libreoffice --office=/usr/share --driver=juno --sofficeport=8997
