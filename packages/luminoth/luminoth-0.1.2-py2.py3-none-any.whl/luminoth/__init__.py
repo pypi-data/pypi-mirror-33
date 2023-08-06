fa82db36280c06c996607465e0b7e922167447e0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:49:10 +0000

    Add package luminoth/0.1.2/luminoth-0.1.2-py2.py3-none-any.whl

---
+++ packages/luminoth/luminoth-0.1.2-py2.py3-none-any.whl/luminoth/__init__.py
@@ -0,0 +1,16 @@
+__version__ = '0.1.2'
+
+__title__ = 'Luminoth'
+__description__ = 'Computer vision toolkit based on TensorFlow'
+__uri__ = 'https://luminoth.ai'
+__doc__ = __description__ + ' <' + __uri__ + '>'
+
+__author__ = 'Tryolabs'
+__email__ = 'luminoth@tryolabs.com'
+
+__license__ = 'BSD 3-Clause License'
+__copyright__ = 'Copyright (c) 2018 Tryolabs S.A.'
+
+# Import functions that are part of Luminoth's public interface.
+from luminoth.cli import cli  # noqa
+from luminoth.utils.predicting import PredictorNetwork  # noqa
