9dde377f868f68681933e54db4422be36a06f21b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 07:13:24 +0000

    Add package abel/0.0.3/abel-0.0.3.tar.gz

---
+++ packages/abel/abel-0.0.3.tar.gz/abel-0.0.3/setup.py
@@ -0,0 +1,21 @@
+import setuptools
+
+with open('README.md', 'r') as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name='abel',
+    version='0.0.3',
+    author='Hunan Rostomyan',
+    author_email='hunan131@gmail.com',
+    description='Machine learning components',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    url='https://github.com/hunan-rostomyan/abel',
+    packages=setuptools.find_packages(),
+    classifiers=(
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+    ),
+)
