03d3368d74ccadce44cd273bf22d8d79c2070d24
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:36:39 +0000

    Add package inlineplz/0.37.13/inlineplz-0.37.13-py2.py3-none-any.whl

---
+++ packages/inlineplz/inlineplz-0.37.13-py2.py3-none-any.whl/inlineplz/interfaces/__init__.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+
+from __future__ import absolute_import
+from __future__ import unicode_literals
+
+from inlineplz.interfaces.github import GitHubInterface
+
+INTERFACES = {
+    'github': GitHubInterface
+}
