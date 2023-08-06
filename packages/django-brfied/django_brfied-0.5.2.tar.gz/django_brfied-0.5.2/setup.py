302bb6635b44e1e90a154d771cfbc82a7bcd7465
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:17:17 +0000

    Add package django-brfied/0.5.2/django_brfied-0.5.2.tar.gz

---
+++ packages/django-brfied/django_brfied-0.5.2.tar.gz/django_brfied-0.5.2/setup.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+from distutils.core import setup
+setup(
+    name='django_brfied',
+    packages=['django_brfied', 'django_brfied/migrations', 'django_brfied/management/commands', ],
+    package_dir={'django_brfied': 'django_brfied'},
+    # package_data={'django_brfied': ['django_brfied/static/js/*', 'django_brfied/*', 'django_brfied/migrations/*', 'django_brfied/management/commands/*'],},
+    version='0.5.2',
+    download_url='https://github.com/kelsoncm/django_brfied/releases/tag/0.5.2',
+    description='Django Application specific brazilian fields types',
+    author='Kelson da Costa Medeiros',
+    author_email='kelsoncm@gmail.com',
+    url='https://github.com/kelsoncm/django_brfied',
+    keywords=['django', 'BR', 'Brazil', 'Brasil', 'model', 'form', 'locale', ],
+    classifiers=[],
+    extras_require={':python_version == "2.7"': ['django_brfied==0.5.0'], ':python_version >= "3.3"': ['django_brfied==0.5.0'], },
+)
