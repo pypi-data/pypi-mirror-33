0327492b2863dff346be83f2b719ed6aa7a9b140
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:52:41 +0000

    Add package databasetools/0.3.12/databasetools-0.3.12.tar.gz

---
+++ packages/databasetools/databasetools-0.3.12.tar.gz/databasetools-0.3.12/databasetools/__init__.py
@@ -0,0 +1,8 @@
+from databasetools.csv_tools import CSVExport, CSVImport
+from databasetools.sqlite import SQLiteSyntax, SQLiteQuery, SQLiteTools
+from databasetools.dict import DictTools
+from databasetools.my_sql import MySQLTools
+from databasetools.php import PHPArray
+
+__name__ = "Database Tools"
+__all__ = ["CSVExport", "CSVImport", "SQLiteSyntax", "SQLiteQuery", "SQLiteTools", "DictTools", "MySQLTools", "PHPArray"]
