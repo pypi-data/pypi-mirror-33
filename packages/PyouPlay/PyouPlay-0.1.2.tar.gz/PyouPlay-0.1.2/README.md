13fa9b13fe6693ac9781fb01498348f00253e6fe
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 07:12:00 +0000

    Add package PyouPlay/0.1.2/PyouPlay-0.1.2.tar.gz

---
+++ packages/PyouPlay/PyouPlay-0.1.2.tar.gz/PyouPlay-0.1.2/README.md
@@ -0,0 +1,4 @@
+[![Build Status](https://travis-ci.com/omi10859/PyouPlay.svg?branch=master)](https://travis-ci.com/omi10859/PyouPlay)
+# PYouPlay
+python Package to get links to videos from youtube when you pass search argument
+
---
+++ packages/PyouPlay/PyouPlay-0.1.2.tar.gz/PyouPlay-0.1.2/setup.py
@@ -0,0 +1,23 @@
+from setuptools import setup, find_packages
+import PyouPlay.get
+
+setup(
+    name='PyouPlay',
+    version='0.1.2',
+    author="Omkar Yadav",
+    author_email="omkar10859@gmail.com",
+    packages=['PyouPlay'],
+    scripts=['PyouPlay/get.py'],
+    url="https://github.com/omi10859/PyouPlay",
+    description="This is a simple python package when passed with a search argument returns youtube video link.",
+    long_description="A simple package to get youtube video links just by passing a search argument it can give a top one link and top 20 links.",
+    install_requires=[
+        "bs4>=0.0.1",
+        "beautifulsoup4>=4.6.0",
+        "requests>=2.19.1",
+        "urllib3>=1.23",
+        "certifi>=2018.4.16",
+        "chardet>=3.0.4",
+        "lxml>=4.2.3",
+    ],
+)
