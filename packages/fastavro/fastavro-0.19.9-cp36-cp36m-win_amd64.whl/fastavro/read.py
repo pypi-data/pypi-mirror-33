b31da50172d19112e708ec717c615755a02445f4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 07:12:19 +0000

    Add package fastavro/0.19.9/fastavro-0.19.9-cp36-cp36m-win_amd64.whl

---
+++ packages/fastavro/fastavro-0.19.9-cp36-cp36m-win_amd64.whl/fastavro/read.py
@@ -0,0 +1,25 @@
+try:
+    from . import _read
+except ImportError as e:
+    from . import _read_py as _read
+
+from ._read_common import (
+    HEADER_SCHEMA, SYNC_SIZE, MAGIC, SchemaResolutionError
+)
+
+acquaint_schema = _read.acquaint_schema
+reader = iter_avro = _read.reader
+block_reader = _read.block_reader
+schemaless_reader = _read.schemaless_reader
+read_data = _read.read_data
+is_avro = _read.is_avro
+
+_READERS = _read.READERS
+LOGICAL_READERS = _read.LOGICAL_READERS
+BLOCK_READERS = _read.BLOCK_READERS
+
+__all__ = [
+    'acquaint_schema', 'reader', 'schemaless_reader', 'read_data', 'is_avro',
+    'HEADER_SCHEMA', 'SYNC_SIZE', 'MAGIC', 'SchemaResolutionError',
+    'LOGICAL_READERS', 'BLOCK_READERS',
+]
