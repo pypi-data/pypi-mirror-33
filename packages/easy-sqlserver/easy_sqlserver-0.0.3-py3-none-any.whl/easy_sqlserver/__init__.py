d04f6b9505b9e1b4ce01dffe074e5178d36d9921
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:47:53 +0000

    Add package easy-sqlserver/0.0.3/easy_sqlserver-0.0.3-py3-none-any.whl

---
+++ packages/easy-sqlserver/easy_sqlserver-0.0.3-py3-none-any.whl/easy_sqlserver/__init__.py
@@ -0,0 +1,10 @@
+__author__ = "Marcelo Moraes"
+__copyright__ = "Copyright 2018"
+__credits__ = ["Marcelo Moraes"]
+__license__ = "MIT"
+__version__ = "1.0.0"
+__maintainer__ = "Marcelo Moraes"
+__email__ = "marcelomoraesjr28@gmail.com"
+__status__ = "Testing"
+
+from easy_sqlserver import EasySQLServer
