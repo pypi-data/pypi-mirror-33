b9bc49a353dc8603a82b43bee629c6594823d11f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:59:47 +0000

    Add package pybleno/0.8/pybleno-0.8.tar.gz

---
+++ packages/pybleno/pybleno-0.8.tar.gz/pybleno-0.8/pybleno/Descriptor.py
@@ -0,0 +1,15 @@
+from . import UuidUtil
+import array
+
+# function Descriptor(options) {
+#   this.uuid = UuidUtil.removeDashes(options.uuid);
+#   this.value = options.value || new Buffer(0);
+# }
+
+class Descriptor:
+    def __init__(self, uuid=None, value=None):
+        self.uuid = uuid
+        if isinstance(value, str):
+            self.value = array.array('B', [ord(c) for c in value])
+        else:
+            self.value = value
