f9772bd481c5a4ea5515c354eacabd9f7829f62f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:59:35 +0000

    Add package andes/0.3.1/andes-0.3.1.tar.gz

---
+++ packages/andes/andes-0.3.1.tar.gz/andes-0.3.1/andes/__init__.py
@@ -0,0 +1,11 @@
+__author__ = 'Hantao Cui'
+__version__ = '2018.07.03-dev'
+__all__ = ['main',
+           'consts',
+           'plot',
+           'system',
+           'settings']
+
+
+from andes.main import main
+from andes.plot import main as plot
