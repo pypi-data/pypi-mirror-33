21ce55ae085f2974c696c4fb04e96071c0a062a5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:26:55 +0000

    Add package pyleri/1.2.1/pyleri-1.2.1.tar.gz

---
+++ packages/pyleri/pyleri-1.2.1.tar.gz/pyleri-1.2.1/setup.py
@@ -0,0 +1,53 @@
+"""setup.py
+
+Upload to PyPI, Thx to: http://peterdowns.com/posts/first-time-with-pypi.html
+
+python3 setup.py sdist
+twine upload --repository pypitest dist/pyleri-1.2.1c.tar.gz
+
+python3 setup.py register -r pypitest
+python3 setup.py sdist upload -r pypitest
+
+python3 setup.py register -r pypi
+python3 setup.py sdist upload -r pypi
+"""
+from setuptools import setup
+from pyleri import __version__ as version
+
+try:
+    with open('README.md', 'r') as f:
+        long_description = f.read()
+except IOError:
+    long_description = ''
+
+setup(
+    name='pyleri',
+    packages=['pyleri'],
+    version=version,
+    description='Python Left-Right Parser',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    author='Jeroen van der Heijden',
+    author_email='jeroen@transceptor.technology',
+    url='https://github.com/transceptor-technology/pyleri',
+    download_url=(
+        'https://github.com/transceptor-technology/'
+        'pyleri/tarball/{}'.format(version)),
+    keywords=['parser', 'grammar', 'autocompletion'],
+    classifiers=[
+        'Development Status :: 5 - Production/Stable',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.0',
+        'Programming Language :: Python :: 3.1',
+        'Programming Language :: Python :: 3.2',
+        'Programming Language :: Python :: 3.3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3 :: Only',
+        'Topic :: Software Development :: Libraries :: Python Modules',
+        'Topic :: Text Processing :: Linguistic'
+    ],
+)
