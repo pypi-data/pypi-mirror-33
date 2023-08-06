ce046d4c547eef749c8072e04312924263c02dce
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:22:20 +0000

    Add package Flask-Locale/1.0.4/Flask-Locale-1.0.4.tar.gz

---
+++ packages/Flask-Locale/Flask-Locale-1.0.4.tar.gz/Flask-Locale-1.0.4/setup.py
@@ -0,0 +1,35 @@
+"""
+Flask-Locale
+-----------
+
+Adds i18n/l10n support to Flask applications.
+"""
+from setuptools import setup
+
+setup(
+    name='Flask-Locale',
+    version='1.0.4',
+    url='http://github.com/derkan/flask-locale',
+    license='BSD',
+    author='Erkan Durmus',
+    author_email='derkan@gmail.com',
+    description='Adds i18n/l10n support to Flask applications easily. Uses CSV files(or database) to load translations.',
+    long_description=open('README.rst').read(),
+    packages=['flask_locale'],
+    zip_safe=False,
+    platforms='any',
+    install_requires=[
+        'Flask',
+        'speaklater'
+    ],
+    classifiers=[
+        'Environment :: Web Environment',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: BSD License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 3',
+        'Topic :: Internet :: WWW/HTTP :: Dynamic Content',
+        'Topic :: Software Development :: Libraries :: Python Modules'
+    ]
+)
