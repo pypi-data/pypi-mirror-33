427e87fc09935042c096247280cac737a77a7975
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 07:12:47 +0000

    Add package tap-exacttarget/1.1.2/tap-exacttarget-1.1.2.tar.gz

---
+++ packages/tap-exacttarget/tap-exacttarget-1.1.2.tar.gz/tap-exacttarget-1.1.2/setup.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python
+
+from setuptools import setup, find_packages
+
+setup(
+    name='tap-exacttarget',
+    version='1.1.2',
+    description='Singer.io tap for extracting data from the ExactTarget API',
+    author='Fishtown Analytics',
+    url='http://fishtownanalytics.com',
+    classifiers=['Programming Language :: Python :: 3 :: Only'],
+    py_modules=['tap_exacttarget'],
+    install_requires=[
+        'funcy==1.9.1',
+        'singer-python>=5.0.6',
+        'python-dateutil==2.6.0',
+        'voluptuous==0.10.5',
+        'pyjwt>=0.1.9',
+        'requests>=2.2.1',
+        'suds-jurko>=0.6'
+    ],
+    entry_points='''
+    [console_scripts]
+    tap-exacttarget=tap_exacttarget:main
+    ''',
+    packages=find_packages()
+)
