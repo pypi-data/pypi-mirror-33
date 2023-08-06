11294d7eadaa757ec2d9a3f56b3e24318b9fcf15
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:14:39 +0000

    Add package trafaret-config/2.0.0/trafaret-config-2.0.0.tar.gz

---
+++ packages/trafaret-config/trafaret-config-2.0.0.tar.gz/trafaret-config-2.0.0/setup.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python3
+
+from setuptools import setup
+
+setup(name='trafaret-config',
+      version='2.0.0',
+      description='A configuration library for python using trafaret and yaml',
+      author='Paul Colomiets',
+      author_email='paul@colomiets.name',
+      url='http://github.com/tailhook/trafaret-config',
+      packages=['trafaret_config'],
+      install_requires=[
+        'PyYaml',
+        'trafaret',
+      ],
+      classifiers=[
+          'Development Status :: 5 - Production/Stable',
+          'Programming Language :: Python :: 2',
+          'Programming Language :: Python :: 2.7',
+          'Programming Language :: Python :: 3',
+          'Programming Language :: Python :: 3.3',
+          'Programming Language :: Python :: 3.4',
+          'Programming Language :: Python :: 3.5',
+          'License :: OSI Approved :: MIT License',
+          'License :: OSI Approved :: Apache Software License',
+      ],
+)
