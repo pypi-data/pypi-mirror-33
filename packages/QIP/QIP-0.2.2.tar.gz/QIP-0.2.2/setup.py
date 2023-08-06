92bb0d7bed8526b4b6ebec052c66d9660a7a22a0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:13:38 +0000

    Add package QIP/0.2.2/QIP-0.2.2.tar.gz

---
+++ packages/QIP/QIP-0.2.2.tar.gz/QIP-0.2.2/setup.py
@@ -0,0 +1,25 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup, find_packages
+from distutils.extension import Extension
+from Cython.Build import cythonize
+import numpy
+
+readme = "QIP: A quantum computing simulation library."
+
+with open('LICENSE') as f:
+    license = f.read()
+
+setup(
+    name='QIP',
+    version='0.2.2',
+    description='Quantum Computing Library',
+    long_description=readme,
+    author='Sumner Hearth',
+    author_email='sumnernh@gmail.com',
+    url='https://github.com/Renmusxd/QIP',
+    license=license,
+    packages=find_packages(exclude=('tests','benchmark')),
+    ext_modules=cythonize([
+        Extension("qip.ext.*", ["qip/ext/*.pyx"], extra_compile_args=["-O3"], include_dirs=[numpy.get_include()])
+    ])
+)
