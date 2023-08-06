b859074ca903386ed5210cbc21efc63e10249a25
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:18:12 +0000

    Add package blocksmith/1.1.2/blocksmith-1.1.2.tar.gz

---
+++ packages/blocksmith/blocksmith-1.1.2.tar.gz/blocksmith-1.1.2/setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup
+setup(
+    name = 'blocksmith',
+    packages = ['blocksmith'],
+    install_requires = ['ecdsa'],
+    version = '1.1.2',
+    description = 'Bitcoin key manipulation',
+    author = 'Timur Badretdinov',
+    author_email = 'destinerx@gmail.com',
+    url = 'https://github.com/Destiner/blocksmith',
+    download_url = 'https://github.com/Destiner/blocksmith/tarball/1.1.2',
+    keywords = ['security', 'cryptography', 'cryptocurrency', 'bitcoin'],
+    classifiers = [],
+)
