8d3cfe872766c068d5bf11bdfb89ef03b396750f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:14:51 +0000

    Add package funniest_jl/0.1/funniest_jl-0.1.tar.gz

---
+++ packages/funniest_jl/funniest_jl-0.1.tar.gz/funniest_jl-0.1/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='funniest_jl',
+            version='0.1',
+            description='The funniest joke in the world',
+            url='http://github.com/HoustonJ2013/funniest',
+            author='Jingbo Liu',
+            author_email='jingbo.liu2013@gmail.com',
+            license='tem',
+            packages=['funniest'],
+            zip_safe=False)
