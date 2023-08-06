1b0653a638fa451932b9a9c2d97cf56b61174028
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:36:59 +0000

    Add package xdoctest/0.5.4/xdoctest-0.5.4-py3.6.egg

---
+++ packages/xdoctest/xdoctest-0.5.4-py3.6.egg/xdoctest/utils/util_misc.py
@@ -0,0 +1,29 @@
+# -*- coding: utf-8 -*-
+from __future__ import print_function, division, absolute_import, unicode_literals
+from os.path import join
+import random
+from .util_path import TempDir
+
+
+class TempDoctest(object):
+    """
+    Creates a temporary file containing a module-level doctest for testing
+
+    Example:
+        >>> from xdoctest import core
+        >>> self = TempDoctest('>>> a = 1')
+        >>> doctests = list(core.parse_doctestables(self.modpath))
+        >>> assert len(doctests) == 1
+    """
+    def __init__(self, docstr, modname=None):
+        if modname is None:
+            # make a random temporary module name
+            alphabet = list(map(chr, range(97, 97 + 26)))
+            modname = ''.join([random.choice(alphabet) for _ in range(8)])
+        self.modname = modname
+        self.docstr = docstr
+        self.temp = TempDir()
+        self.dpath = self.temp.ensure()
+        self.modpath = join(self.dpath, self.modname + '.py')
+        with open(self.modpath, 'w') as file:
+            file.write("'''\n%s'''" % self.docstr)
