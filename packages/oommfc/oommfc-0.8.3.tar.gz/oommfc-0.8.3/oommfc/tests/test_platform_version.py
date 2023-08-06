53b59fb256909ca0ffd134fdd2b721e6355a38e0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:23:10 +0000

    Add package oommfc/0.8.3/oommfc-0.8.3.tar.gz

---
+++ packages/oommfc/oommfc-0.8.3.tar.gz/oommfc-0.8.3/oommfc/tests/test_platform_version.py
@@ -0,0 +1,12 @@
+import pytest
+import oommfc
+
+
+def test_version_returns():
+    runner = oommfc.get_oommf_runner()
+    runner.version()
+
+
+def test_platform_returns():
+    runner = oommfc.get_oommf_runner()
+    runner.version()
