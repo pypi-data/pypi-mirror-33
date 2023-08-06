80d1bf4603c58b128ee14a81bd17e8ca5639b002
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:22:25 +0000

    Add package dq-client/0.3.0/dq-client-0.3.0.tar.gz

---
+++ packages/dq-client/dq-client-0.3.0.tar.gz/dq-client-0.3.0/dq/__init__.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+
+from .client import DQClient
+from .job import JobConfig
+
+__author__ = """Mikołaj Olszewski"""
+__email__ = 'mikolaj.olszewski@algolytics.pl'
+__version__ = '0.1.0'
+
+__all__ = ['DQClient', 'JobConfig']
