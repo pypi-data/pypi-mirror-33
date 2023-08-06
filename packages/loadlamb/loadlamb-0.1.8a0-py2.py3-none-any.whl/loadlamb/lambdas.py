ba6b088d83cb705c0374ac4ce12cff3332bbfc86
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:36:00 +0000

    Add package loadlamb/0.1.8a0/loadlamb-0.1.8a0-py2.py3-none-any.whl

---
+++ packages/loadlamb/loadlamb-0.1.8a0-py2.py3-none-any.whl/loadlamb/lambdas.py
@@ -0,0 +1,32 @@
+import datetime
+import json
+
+import boto3
+
+from loadlamb.contrib.db.models import Run
+from loadlamb.load import LoadLamb
+from loadlamb.utils import grouper
+
+sqs = boto3.resource('sqs')
+
+
+def push_handler(event,context):
+    print('User Number',event['user_num'])
+    run_slug = '{}-{}'.format(event['name'],datetime.datetime.now())
+    event['run_slug'] = run_slug
+    r = Run(project_slug=event['name'],run_slug=run_slug)
+    r.save()
+    q = sqs.get_queue_by_name(QueueName='loadlamb')
+    g = grouper(event['user_batch_size'],event['user_num'])
+    for s in g:
+        b = [{'Id':str(i),'MessageBody':json.dumps(event)} for i in range(s)]
+        r = q.send_messages(Entries=b)
+
+
+
+def pull_handler(event,context):
+    msg = json.loads(event['Records'][0]['body'])
+    print(msg)
+    responses = LoadLamb(msg).run()
+    for i in responses:
+        i.save()
