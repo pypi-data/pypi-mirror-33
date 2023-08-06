e0f1f688a94851c8bf250d630b978af5d0c031e1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:36:33 +0000

    Add package porfavor/0.3.0/porfavor-0.3.0-py3-none-any.whl

---
+++ packages/porfavor/porfavor-0.3.0-py3-none-any.whl/porfavor/__init__.py
@@ -0,0 +1,18 @@
+"""Manage documentation in a client-server architecture."""
+import click
+
+from .server import server_cli
+from .client import publish, publish_cli  # noqa
+
+
+@click.group(context_settings=dict(help_option_names=['-h', '--help']))
+def main():
+    """Manage documentation the easiest way possible."""
+
+
+main.add_command(server_cli)
+main.add_command(publish_cli)
+
+
+if __name__ == "__main__":
+    main()
