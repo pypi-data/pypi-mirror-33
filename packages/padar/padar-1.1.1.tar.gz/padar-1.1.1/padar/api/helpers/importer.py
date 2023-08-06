a46a3d828b112ddfe29885bb2e6777b4b6484ba2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 05:06:12 +0000

    Add package padar/1.1.1/padar-1.1.1.tar.gz

---
+++ packages/padar/padar-1.1.1.tar.gz/padar-1.1.1/padar/api/helpers/importer.py
@@ -0,0 +1,28 @@
+import pandas as pd
+
+def import_sensor_file_mhealth(filepath, verbose=False):
+	df = pd.read_csv(filepath, 
+		dtype=str,
+		error_bad_lines=False, 
+		warn_bad_lines=False, 
+		skip_blank_lines=True, 
+		low_memory=True,
+		comment='#')
+	df.iloc[:,0] = pd.to_datetime(df.iloc[:,0], infer_datetime_format=True, errors='coerce', format='%Y-%m-%d %H:%M:%S.%f', exact=True).values.astype('datetime64[ms]')
+	df.iloc[:,1:4] = df.iloc[:,1:4].apply(pd.to_numeric, errors='coerce')
+	if verbose:
+		print('na rows:' + str(df.shape[0] - df.dropna().shape[0]))
+	df = df.dropna()
+	return df
+
+def import_annotation_file_mhealth(filepath, verbose=False):
+	df = pd.read_csv(filepath,
+		error_bad_lines=False, 
+		warn_bad_lines=False, 
+		skip_blank_lines=True,
+		low_memory=True,
+		parse_dates=[0,1,2],
+		infer_datetime_format=True,
+		comment='#')
+	return df
+	
