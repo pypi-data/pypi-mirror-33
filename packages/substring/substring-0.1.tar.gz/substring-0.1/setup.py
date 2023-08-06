e097e7eab5df6d18b8c67d58e3d451ee0eebf32e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:49:40 +0000

    Add package substring/0.1/substring-0.1.tar.gz

---
+++ packages/substring/substring-0.1.tar.gz/substring-0.1/setup.py
@@ -0,0 +1,24 @@
+from setuptools import setup
+
+setup(name='substring',
+      version='0.1',
+      description='Multi-Operational and Flexible Substring Package',
+      url='https://github.com/VETURISRIRAM/cpython',
+      author='Sriram Veturi',
+      author_email='sriram.tutu@gmail.com',
+      license='MIT',
+      packages=['substring'],
+      classifiers=(
+          "License :: OSI Approved :: MIT License",
+          "Operating System :: OS Independent",
+          "Programming Language :: Python",
+          "Programming Language :: Python :: 3",
+          "Programming Language :: Python :: 3.3",
+          "Programming Language :: Python :: 3.4",
+          "Programming Language :: Python :: 3.5",
+          "Programming Language :: Python :: 3.6",
+          "Programming Language :: Python :: Implementation :: CPython",
+          "Programming Language :: Python :: Implementation :: PyPy",
+          "Topic :: Software Development :: Libraries :: Python Modules"
+      ),
+)
---
+++ packages/substring/substring-0.1.tar.gz/substring-0.1/substring/__init__.py
@@ -0,0 +1,4 @@
+from .substring import *
+
+name = "substring"
+__version__ = "1.0"
