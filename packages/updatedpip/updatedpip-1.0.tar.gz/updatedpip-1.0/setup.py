bfaecf523ce062c4a4c753e1a0d40b61e5cad0bb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:56:20 +0000

    Add package updatedpip/1.0/updatedpip-1.0.tar.gz

---
+++ packages/updatedpip/updatedpip-1.0.tar.gz/updatedpip-1.0/setup.py
@@ -0,0 +1,3 @@
+import setuptools
+setuptools.setup(name = 'updatedpip', version='1.0', author="VikdaBeast", description="Example pip package",
+                 long_description = "This is necessary for winning", packages=setuptools.find_packages())
