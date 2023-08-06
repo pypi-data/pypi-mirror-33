6eba44a9b1de979f19510de833940540a24c5133
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 05:07:33 +0000

    Add package drytools/0.1.3/drytools-0.1.3-py2.py3-none-any.whl

---
+++ packages/drytools/drytools-0.1.3-py2.py3-none-any.whl/drytools/__init__.py
@@ -0,0 +1,6 @@
+from .annotation.composition import compose_annotations
+from .annotation.functions import check, iterify
+from .decorator import args2attrs, ordered_by
+#from .mixins import repr_from_init
+
+__version__ = '0.1.3'
