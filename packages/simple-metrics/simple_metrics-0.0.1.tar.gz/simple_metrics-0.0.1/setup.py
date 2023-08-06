8d40628119fefb11607f2c1ecfb48062950d0a11
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:35:18 +0000

    Add package simple-metrics/0.0.1/simple_metrics-0.0.1.tar.gz

---
+++ packages/simple-metrics/simple_metrics-0.0.1.tar.gz/simple_metrics-0.0.1/setup.py
@@ -0,0 +1,21 @@
+from distutils.core import setup
+
+setup(name='simple_metrics',
+    version='0.0.1',
+    description='Simple portfolio metrics for Robinhood',
+    author='Weston Platter',
+    author_email='westonplatter@gmail.com',
+    url='https://www.python.org/sigs/simple_metrics/',
+    packages=['simple_metrics'],
+    install_requires=[
+        'click',
+        'Robinhood==1.0.1'
+    ],
+    dependency_links=[
+        'https://github.com/Jamonek/Robinhood/archive/1.0.1.zip#egg=Robinhood==1.0.1',
+    ],
+    entry_points='''
+        [console_scripts]
+        sm=simple_metrics.cli:cli
+    ''',
+)
---
+++ packages/simple-metrics/simple_metrics-0.0.1.tar.gz/simple_metrics-0.0.1/simple_metrics/fetch.py
@@ -0,0 +1,12 @@
+import Robinhood
+
+def stock_trades(account):
+    print('fetching stock_trades')
+    pass
+
+def option_trades(account):
+    print('fetching option_trades')
+    pass
+
+def positions():
+    pass
