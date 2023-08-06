fcc559ea901b747daf421cedc89ffc8498fccd68
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:34:57 +0000

    Add package cutil/2.6.7/cutil-2.6.7.tar.gz

---
+++ packages/cutil/cutil-2.6.7.tar.gz/cutil-2.6.7/setup.py
@@ -0,0 +1,32 @@
+from distutils.core import setup
+
+try:
+    import pypandoc
+    long_description = pypandoc.convert('README.md', 'rst', format='md')
+except (IOError, ImportError) as e:
+    print(str(e))
+    long_description = ''
+
+setup(
+    name='cutil',
+    packages=['cutil'],
+    version='2.6.7',
+    description='A collection of useful functions',
+    long_description=long_description,
+    author='Eddy Hintze',
+    author_email="eddy@hintze.co",
+    url="https://github.com/xtream1101/cutil",
+    license='MIT',
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "Development Status :: 4 - Beta",
+        "Environment :: Other Environment",
+        "Operating System :: OS Independent",
+        "Topic :: Software Development :: Libraries :: Python Modules",
+        "Topic :: Utilities",
+    ],
+    install_requires=['hashids',
+                      'psycopg2',
+                      'pytz',
+                      ]
+)
