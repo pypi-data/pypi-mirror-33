10863b9578d44060eaa8cc80a48b4a76ca727789
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:52:40 +0000

    Add package ie_mbdbl2017_C_yahoo_ks_datareader/1.0.1/ie_mbdbl2017_C_yahoo_ks_datareader-1.0.1.tar.gz

---
+++ packages/ie_mbdbl2017_C_yahoo_ks_datareader/ie_mbdbl2017_C_yahoo_ks_datareader-1.0.1.tar.gz/ie_mbdbl2017_C_yahoo_ks_datareader-1.0.1/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='ie_mbdbl2017_C_yahoo_ks_datareader',
+      version='1.0.1',  # Development release
+      description='Python Packaging for Risk and Fraud Course, GMBD2017, GroupC',
+      url='https://github.com/dancual/pythonpackagesample',
+      author='Daniel Serrano',
+      author_email='dserrano1983@gmail.com',
+      license='MIT',
+      packages=['ie_mbdbl2017_C_yahoo_ks_datareader'],
+      zip_safe=False)
