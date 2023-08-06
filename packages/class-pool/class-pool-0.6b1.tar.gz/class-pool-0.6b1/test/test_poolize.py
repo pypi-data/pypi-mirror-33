19e5c64bdfdd8c0e103708e237743379574e74c0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:18:44 +0000

    Add package class-pool/0.6b1/class-pool-0.6b1.tar.gz

---
+++ packages/class-pool/class-pool-0.6b1.tar.gz/class-pool-0.6b1/test/test_poolize.py
@@ -0,0 +1,34 @@
+from class_pool.pool import poolize, Pool
+from test.base import PoolTestCase
+
+
+class PoolizeTestCase(PoolTestCase):
+
+    def test_poolize_no_args(self):
+
+        @poolize
+        class TestClass(object):
+            pass
+
+        self.assertTrue(isinstance(Pool.of(TestClass), Pool))
+
+    def test_poolize_args(self):
+
+        @poolize()
+        class TestClass(object):
+            pass
+
+        self.assertTrue(isinstance(Pool.of(TestClass), Pool))
+
+    def test_poolize_subclasses(self):
+
+        @poolize
+        class TestClass(object):
+            pass
+
+        class SubClass(TestClass):
+            pass
+
+        pool = Pool.of(TestClass)
+
+        self.assertIn(SubClass.__name__, pool)
