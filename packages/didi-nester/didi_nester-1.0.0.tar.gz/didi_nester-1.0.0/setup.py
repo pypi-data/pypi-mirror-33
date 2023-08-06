22aab8b533b9f6824cae2619d8443533f2a6548e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:45:03 +0000

    Add package didi-nester/1.0.0/didi_nester-1.0.0.tar.gz

---
+++ packages/didi-nester/didi_nester-1.0.0.tar.gz/didi_nester-1.0.0/setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+
+setup(
+        name         = 'didi_nester',
+        version      = '1.0.0',
+        py_modules   = ['didi_nester'],
+        author       = 'hfpython',
+        author_email = 'didistonecold@live.com',
+        url          = '',
+        description  = 'A simple printer of nested lists',
+)
