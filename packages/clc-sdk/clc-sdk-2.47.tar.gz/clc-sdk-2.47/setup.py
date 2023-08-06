dbd881ac3b59aaa198a2932cfc3df87386e258c4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:22:36 +0000

    Add package clc-sdk/2.47/clc-sdk-2.47.tar.gz

---
+++ packages/clc-sdk/clc-sdk-2.47.tar.gz/clc-sdk-2.47/setup.py
@@ -0,0 +1,36 @@
+
+#
+# python setup.py sdist
+# python setup.py bdist_dumb
+# python setup.py bdist_rpm
+#
+
+# follow notes from https://packaging.python.org/distributing/#id69
+# and release with `python setup.py sdist upload`
+
+from setuptools import setup, find_packages
+
+setup(
+	name = "clc-sdk",
+	version = "2.47",
+	packages = find_packages("."),
+
+	install_requires = ['prettytable','clint','argparse','requests'],
+
+	entry_points = {
+		'console_scripts': [
+			'clc  = clc.APIv1.cli:main',
+		],
+	},
+
+
+	# metadata for upload to PyPI
+	author = "CenturyLink Cloud",
+	author_email = "ecosystem@centurylink.com",
+	description = "CenturyLink Cloud SDK and CLI",
+	keywords = "CenturyLink Cloud SDK CLI",
+	url = "https://github.com/CenturyLinkCloud/clc-python-sdk",
+
+	# could also include long_description, download_url, classifiers, etc.
+)
+
