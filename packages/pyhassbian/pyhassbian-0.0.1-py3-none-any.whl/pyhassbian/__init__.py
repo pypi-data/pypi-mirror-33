9b129135d352bbdba15340630de60bb5ccb9f019
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:53:21 +0000

    Add package pyhassbian/0.0.1/pyhassbian-0.0.1-py3-none-any.whl

---
+++ packages/pyhassbian/pyhassbian-0.0.1-py3-none-any.whl/pyhassbian/__init__.py
@@ -0,0 +1,14 @@
+"""
+A python module to interact with hassbian.
+This code is released under the terms of the MIT license. See the LICENSE
+file for more details.
+"""
+import requests
+
+__version__ = '0.0.1'
+
+class Hassbian:
+    """This class is used to interact with hassbian."""
+
+    def __init__(self):
+        """Initialize"""
