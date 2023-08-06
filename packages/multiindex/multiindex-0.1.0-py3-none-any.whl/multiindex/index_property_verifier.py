6cf57867e4e6f5ab0a91549d6e870ca726e74760
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:48:51 +0000

    Add package multiindex/0.1.0/multiindex-0.1.0-py3-none-any.whl

---
+++ packages/multiindex/multiindex-0.1.0-py3-none-any.whl/multiindex/index_property_verifier.py
@@ -0,0 +1,41 @@
+from abc import abstractmethod, ABCMeta
+
+from .indexed_by import (HashedUnique, OrderedUnique, HashedNonUnique)
+
+
+class MultiIndexVisitor(object):
+    __metaclass__ = ABCMeta
+    @abstractmethod
+    def visit(self):
+        pass
+
+
+class MultiIndexInserter(object):
+    def visit(self, index, obj, overwrite):
+        index_val = getattr(obj, index.index_name)
+        if isinstance(index, (OrderedUnique, HashedUnique)):
+            # don't check if overwrite
+            if overwrite:
+                return True
+            # not unique, don't insert
+            if index.get(index_val) is not None:
+                return False
+        return True
+
+
+class MultiIndexModifier(object):
+    def visit(self, index, obj):
+        if isinstance(index, (OrderedUnique, HashedUnique)):
+            # not unique, don't insert
+            new_index_val = getattr(obj, index.index_name)
+            if index.get(new_index_val):
+                return False
+        return True
+
+
+class MultiIndexDeleter(object):
+    def visit(self, index, obj):
+        index_val = getattr(obj, index.index_name)
+        if index.get(index_val):
+            return index_val
+        return None
---
+++ packages/multiindex/multiindex-0.1.0-py3-none-any.whl/multiindex/indexed_by.py
@@ -0,0 +1,59 @@
+from abc import ABCMeta
+from . import view
+
+
+class IndexIterator(object):
+    def __init__(self, index_to_iterate):
+        self.iterator = iter(index_to_iterate)
+
+    def __iter__(self):
+        return self
+
+    def __next__(self):
+        return next(self.iterator)
+
+
+class IndexedBy(object):
+    __metaclass__ = ABCMeta
+    def insert(self, obj):
+        index_val = getattr(obj, self.index_name)
+        self.view.insert(index_val, obj)
+
+    def get(self, value):
+        return self.view.get(value)
+
+    def modify(self, value, obj):
+        return self.view.modify(value, getattr(obj, self.index_name), obj)
+
+    def remove(self, value):
+        return self.view.remove(value)
+
+    def can_be_inserted(self, visitor, obj, overwrite):
+        return visitor.visit(self, obj, overwrite)
+
+    def can_be_modified(self, visitor, obj):
+        return visitor.visit(self, obj)
+
+    def can_be_removed(self, visitor, obj):
+        return visitor.visit(self, obj)
+
+    def __iter__(self):
+        return IndexIterator(self.view)
+
+
+class HashedUnique(IndexedBy):
+    def __init__(self, index_name):
+        self.index_name = index_name
+        self.view = view.HashedUnique()
+
+
+class HashedNonUnique(IndexedBy):
+    def __init__(self, index_name):
+        self.index_name = index_name
+        self.view = view.HashedNonUnique()
+
+
+class OrderedUnique(IndexedBy):
+    def __init__(self, index_name):
+        self.index_name = index_name
+        self.view = view.OrderedUnique()
