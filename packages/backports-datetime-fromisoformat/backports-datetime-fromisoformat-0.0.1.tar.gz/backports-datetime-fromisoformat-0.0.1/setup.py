881187eac2cd186447f53c4cac1deedec187fb75
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:25:46 +0000

    Add package backports-datetime-fromisoformat/0.0.1/backports-datetime-fromisoformat-0.0.1.tar.gz

---
+++ packages/backports-datetime-fromisoformat/backports-datetime-fromisoformat-0.0.1.tar.gz/backports-datetime-fromisoformat-0.0.1/setup.py
@@ -0,0 +1,40 @@
+import os
+
+from setuptools import setup, Extension
+# workaround for open() with encoding='' python2/3 compability
+from io import open
+
+with open('README.rst', encoding='utf-8') as file:
+    long_description = file.read()
+
+setup(
+    name="backports-datetime-fromisoformat",
+    version="0.0.1",
+    description="Backport of Python 3.7's datetime.fromisoformat",
+    long_description=long_description,
+    license="MIT",
+    author="Michael Overmeyer",
+    author_email="backports@movermeyer.com",
+    url="https://github.com/movermeyer/backports.datetime_fromisoformat",
+    ext_package="backports",
+    ext_modules=[Extension("_datetime_fromisoformat", [
+        os.path.join("backports", "_datetime_fromisoformat", "module.c"), 
+        os.path.join("backports", "_datetime_fromisoformat", "_datetimemodule.c"),
+        os.path.join("backports", "_datetime_fromisoformat", "timezone.c")
+    ])],
+    test_suite='tests',
+    tests_require=[
+        'pytz'
+    ],
+    classifiers=[
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+        'Topic :: Software Development :: Libraries :: Python Modules'
+    ]
+)
