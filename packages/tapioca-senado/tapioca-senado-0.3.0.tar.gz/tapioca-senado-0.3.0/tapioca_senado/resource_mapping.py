0d62b05b8fa660a55822aca12f829db516694c1a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:59:47 +0000

    Add package tapioca-senado/0.3.0/tapioca-senado-0.3.0.tar.gz

---
+++ packages/tapioca-senado/tapioca-senado-0.3.0.tar.gz/tapioca-senado-0.3.0/tapioca_senado/resource_mapping.py
@@ -0,0 +1,16 @@
+# coding: utf-8
+
+RESOURCE_MAPPING = {
+
+    # Senadores
+    'senadores': {
+        'resource': 'senador/lista/atual',
+        'docs': 'http://legis.senado.gov.br/dadosabertos/docs/ui/index.html#!/ListaSenadorService/resource_ListaSenadorService_listaSenadoresXml_GET',
+    },
+
+    # Comissões
+    'comissoes': {
+        'resource': 'comissao/lista/colegiados',
+        'docs': 'http://legis.senado.gov.br/dadosabertos/docs/ui/index.html#!/ListaComissaoService/resource_ListaComissaoService_listaColegiadosXml_GET',
+    },
+}
