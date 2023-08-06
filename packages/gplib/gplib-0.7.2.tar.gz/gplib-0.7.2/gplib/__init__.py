a91023673baa02c535d25ba3a757f67c960814f2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:43:58 +0000

    Add package gplib/0.7.2/gplib-0.7.2.tar.gz

---
+++ packages/gplib/gplib-0.7.2.tar.gz/gplib-0.7.2/gplib/__init__.py
@@ -0,0 +1,8 @@
+from .gaussian_process import GaussianProcess as GP
+from . import covariance_functions as cov
+from . import mean_functions as mea
+from . import likelihood_functions as lik
+from . import inference_methods as inf
+from . import fitting_methods as fit
+from . import measurements as me
+from .plot import Plot as plot
