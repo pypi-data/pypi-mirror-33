f6003b72910d8c348d5c21b1aa8880794ea1b627
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:37:21 +0000

    Add package jjkkqq/1.0.0/jjkkqq-1.0.0.tar.gz

---
+++ packages/jjkkqq/jjkkqq-1.0.0.tar.gz/jjkkqq-1.0.0/setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+
+setup(
+	name 		= 'jjkkqq',
+	version		= '1.0.0',
+	py_modules 	= ['nester'],
+	author 		= 'hfpython',
+	author_email 	= 'hfpython@headfirstlabs.com',
+	url 		= 'http://www.headfistalabs.com',
+	description	= 'A simple printer of nested lists',
+     )
