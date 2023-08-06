dbfe373dcf5b8eaf817d20286e6d863511918763
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:54:33 +0000

    Add package examplepip/0.0.1/examplepip-0.0.1.tar.gz

---
+++ packages/examplepip/examplepip-0.0.1.tar.gz/examplepip-0.0.1/setup.py
@@ -0,0 +1,3 @@
+import setuptools
+setuptools.setup(name = 'examplepip', version='0.0.1', author="VikdaBeast", description="Example pip package",
+                 long_description = "This is necessary for winning", packages =setuptools.find_packages())
