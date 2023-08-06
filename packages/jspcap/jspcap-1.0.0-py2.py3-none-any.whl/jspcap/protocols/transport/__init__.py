f76b8f5e94d8c72248b4aeb8b1e81ac342589d6d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 07:13:55 +0000

    Add package jspcap/1.0.0/jspcap-1.0.0-py2.py3-none-any.whl

---
+++ packages/jspcap/jspcap-1.0.0-py2.py3-none-any.whl/jspcap/protocols/transport/__init__.py
@@ -0,0 +1,25 @@
+# -*- coding: utf-8 -*-
+"""transport layer protocols
+
+`jspcap.protocols.transport` is collection of all protocols
+in transport layer, with detailed implementation and
+methods.
+
+"""
+# TODO: Implements DCCP, RSVP, STCP.
+
+# Base Class for Transport Layer
+from jspcap.protocols.transport.transport import Transport
+
+# Utility Classes for Protocols
+from jspcap.protocols.transport.tcp import TCP
+from jspcap.protocols.transport.udp import UDP
+
+# Transport Layer Protocol Numbers
+from jspcap.protocols.transport.transport import TP_PROTO
+
+
+__all__ = [
+    'TP_PROTO',     # Protocol Numbers
+    'TCP', 'UDP',   # Transport Layer Protocols
+]
