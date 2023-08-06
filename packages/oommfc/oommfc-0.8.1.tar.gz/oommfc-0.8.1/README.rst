e52b9f452ce135c0849b3158d1f17351a935b6cd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:48:05 +0000

    Add package oommfc/0.8.1/oommfc-0.8.1.tar.gz

---
+++ packages/oommfc/oommfc-0.8.1.tar.gz/oommfc-0.8.1/README.rst
@@ -0,0 +1,18 @@
+.. image:: https://travis-ci.org/joommf/oommfc.svg?branch=master
+   :target: https://travis-ci.org/joommf/oommfc
+   :align: right
+
+.. image:: https://codecov.io/gh/joommf/oommfc/branch/master/graph/badge.svg
+   :target: https://codecov.io/gh/joommf/oommfc
+   :align: right
+
+.. image:: https://readthedocs.org/projects/oommfc/badge/?version=latest
+   :target: http://oommfc.readthedocs.io/en/latest/?badge=latest
+   :alt: Documentation Status
+
+.. image:: https://badge.fury.io/py/oommfc.svg
+   :target: https://badge.fury.io/py/oommfc
+
+A Python-based OOMMF calculator.
+
+Authors: Marijan Beg, Ryan Pepper, Hans Fangohr
---
+++ packages/oommfc/oommfc-0.8.1.tar.gz/oommfc-0.8.1/setup.py
@@ -0,0 +1,24 @@
+import setuptools
+
+with open('README.rst') as f:
+    readme = f.read()
+
+setuptools.setup(
+    name='oommfc',
+    version="0.8.1",
+    description='A Python-based OOMMF calculator.',
+    long_description=readme,
+    url='https://joommf.github.io',
+    author='Marijan Beg, Ryan A. Pepper, Thomas Kluyver, and Hans Fangohr',
+    author_email='jupyteroommf@gmail.com',
+    packages=setuptools.find_packages(),
+    install_requires=['scipy',
+                      'discretisedfield',
+                      'micromagneticmodel',
+                      'oommfodt'],
+    classifiers=['Development Status :: 3 - Alpha',
+                 'License :: OSI Approved :: BSD License',
+                 'Topic :: Scientific/Engineering :: Physics',
+                 'Intended Audience :: Science/Research',
+                 'Programming Language :: Python :: 3 :: Only']
+)
