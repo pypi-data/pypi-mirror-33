7c23b8d6137cf0cf596e387427a57c8e02507258
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:18:48 +0000

    Add package extend-me/1.1.4/extend_me-1.1.4.tar.gz

---
+++ packages/extend-me/extend_me-1.1.4.tar.gz/extend_me-1.1.4/setup.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python
+try:
+    from setuptools import setup
+except ImportError:
+    from distutils.core import setup
+
+_doc = open('README.rst', 'rt').read()
+
+setup(
+    name='extend_me',
+    version='1.1.4',
+    description='Class based extension/plugin library',
+    author='Dmytro Katyukha',
+    author_email='dmytro.katyukha@gmail.com',
+    url='https://github.com/katyukha/extend-me',
+    long_description=_doc,
+    install_requires=[
+        'six>=1.9',
+    ],
+    license="MPL 2.0",
+    py_modules=['extend_me'],
+    classifiers=[
+        'Development Status :: 5 - Production/Stable',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: Mozilla Public License 2.0 (MPL 2.0)',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 3',
+        'Topic :: Software Development :: Libraries',
+        'Topic :: Software Development :: Libraries :: Python Modules',
+    ],
+    keywords=['extension', 'plugin'],
+)
