ff388055440eeb25cdec27de983aff0344921736
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:22:17 +0000

    Add package holistics/0.0a11/holistics-0.0a11.tar.gz

---
+++ packages/holistics/holistics-0.0a11.tar.gz/holistics-0.0a11/setup.py
@@ -0,0 +1,27 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="holistics",
+    version="0.0a11",
+    author="Phat Vo",
+    author_email="phat.vo@holistics.io",
+    description="Package to export report's data from holistics.io",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/holistics/holistics-python",
+    packages=['holistics','tests'],
+    classifiers=(
+		'Programming Language :: Python :: 3',		
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ),
+	install_requires=[
+        "requests", "pandas",
+    ],
+	test_suite='nose.collector',
+    tests_require=['nose'],
+    python_requires='>= 3, < 4',
+)
