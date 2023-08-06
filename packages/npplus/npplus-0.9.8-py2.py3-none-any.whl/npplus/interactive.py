48992ffe8ed0619309e84e76a9de1c91c44906bc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 07:12:48 +0000

    Add package npplus/0.9.8/npplus-0.9.8-py2.py3-none-any.whl

---
+++ packages/npplus/npplus-0.9.8-py2.py3-none-any.whl/npplus/interactive.py
@@ -0,0 +1,95 @@
+# Copyright (c) 2016, David H. Munro
+# All rights reserved.
+# This is Open Source software, released under the BSD 2-clause license,
+# see http://opensource.org/licenses/BSD-2-Clause for details.
+"""Import most names in npplus for interactive use. ::
+
+    from npplus.interactive import *
+
+This module should be imported only from PYTHONSTARTUP, except for
+similar interactive.py modules in other packages.
+
+--------
+"""
+
+# sys and os too common to have to import them
+import sys
+import os  # noqa
+
+# duplicate the pylab numpy imports for interactive use
+import numpy as np
+import numpy.ma as ma  # noqa
+from numpy import *  # noqa
+from numpy.fft import *  # noqa
+from numpy.random import *  # noqa
+from numpy.linalg import *  # noqa
+# fix clobbered datetime and bytes (np.random.bytes)
+# note: modern numpy does not clobber datetime, but this is harmless
+import datetime  # noqa
+bytes = __builtins__['bytes']
+
+# force sane SIGFPE error handling
+np.seterr(divide='raise', over='raise', invalid='raise')
+
+# give interpreted access to npplus modules
+from . import *  # noqa
+
+# implement deprecated execfile for python3
+if sys.version_info >= (3,):
+    if sys.version_info >= (3, 4):
+        from importlib import reload
+    else:
+        from imp import reload
+
+    def execfile(*args):
+        """python2 execfile -- avoid this, write modules and use import."""
+        name, args = args[0], args[1:] if len(args) > 1 else (globals(),)
+        with open(name) as f:
+            code = compile(f.read(), name, 'exec')  # noqa
+            eval("exec(code, *args)")  # evade syntax error in python2
+
+
+# implement extended reload for development reloadx-pdb-edit cycle
+def reloadx(module):
+    """Shorthand for ``reload(module); from module import *``.
+
+    Also injects symbol `my` into the module namespace, with value
+    equal to the `__main__` module.  This is useful for interactive
+    debugging with pdb.
+
+    Notes
+    -----
+    To interactively develop a module, do this::
+
+        # You may want to softlink 'ln -s /path/to/module_or_package .'
+        # in your user site-packages directory (python -m site --user-site).
+        # See site module in python standard library documentation.
+        import module    # begin by importing your module or package
+        reloadx(module)  # same as from module import *
+        # test your module
+        import pdb       # use pdb.pm(), pdb.run("test...") to debug
+        # edit module.py (or package/module.py)
+        reloadx(module)  # reload module and re-import its symbols
+        # be sure to recreate any objects constructed from modified classes
+        # loop debug, edit, reloadx
+
+    The `my` debugging feature will only work if `module` is really a
+    module, not a package; usually it should be the specific module you are
+    debugging.  After ``reloadx(module)`` you can do things like this from
+    the pdb prompt::
+
+        (Pdb) my.plot(x, y)
+        (Pdb) my.savexy = (x, y)
+
+    In the latter case, after you quit pdb, the variable `savexy` will be
+    present in your top level interactive namespace.  Anything that is
+    available in your interactive namespace will be available through the
+    `my` variable in your pdb debugging session.
+    """
+    reload(module)
+    import __main__
+    for nm in vars(module):
+        if not (nm.startswith('__') and nm.endswith('__')):
+            setattr(__main__, nm, getattr(module, nm))  # emulate import *
+    if not hasattr(module, 'my'):
+        module.__dict__['my'] = __main__
---
+++ packages/npplus/npplus-0.9.8-py2.py3-none-any.whl/npplus/pyplotx/pltwraps.py
@@ -0,0 +1,98 @@
+# Copyright (c) 2016, David H. Munro
+# All rights reserved.
+# This is Open Source software, released under the BSD 2-clause license,
+# see http://opensource.org/licenses/BSD-2-Clause for details.
+"""Interactive wrappers for interactive use of various pyplot APIs.
+
+Functions like ``matplotlib.pyplot.plot(x,y)`` are usually invoked as
+subroutine calls interactively, but inconveniently return an object
+which prints at the terminal.  This module wraps such functions to
+return None, so that an interactive terminal isn't half filled with
+unwanted object outputs.  In the rare cases in which you need the
+return value, you can easily use, e.g.- ``result = plt.plot(x,y)``.
+
+Also provides the convenience functions `xylim` and `logxy`.
+
+--------
+"""
+
+# require alternatives: axis
+__all__ = ['annotate', 'axhline', 'axhspan', 'axvline', 'axvspan',
+           'bar', 'barbs', 'barh', 'boxplot', 'broken_barh',
+           'colorbar', 'contour', 'contourf', 'errorbar', 'eventplot',
+           'figimage', 'figlegend', 'figtext', 'figure', 'fill',
+           'fill_between', 'fill_betweenx', 'hexbin', 'hist',
+           'hist2d', 'hlines', 'imshow', 'legend', 'pcolor',
+           'pcolormesh', 'pie', 'plot', 'plot_date', 'quiver',
+           'scatter', 'stem', 'step', 'streamplot', 'subplot',
+           'suptitle', 'text', 'title', 'tricontour', 'tricontourf',
+           'tripcolor', 'triplot', 'violinplot', 'vlines', 'xlabel',
+           'xlim', 'ylabel', 'ylim']
+
+from functools import wraps as _wraps
+import matplotlib.pyplot as plt  # name plt cannot appear in __all__ list
+
+
+def _iwrap(name):
+    f = getattr(plt, name)
+
+    @_wraps(f)
+    def iwrapped(*args, **kwargs):
+        f(*args, **kwargs)
+
+    globals()[name] = iwrapped
+
+
+for _ in __all__:
+    try:
+        _iwrap(_)
+    except AttributeError:
+        # Happens in two important cases:
+        # 1. Old versions of matplotlib do not have violinplot.
+        # 2. ReadTheDocs uses mock matplotlib.pyplot with no names.
+        exec(_+'=NotImplemented')  # otherwise "from pltwraps import *" fails
+
+
+def xylim(xmin=None, xmax=None, ymin=None, ymax=None):
+    """Set xmin, xmax, ymin, ymax limits for the current axes, return None.
+
+    Parameters
+    ----------
+    xmin,xmax,ymin,ymax : float, optional
+        Axis limit or None to leave unchanged.
+
+    See Also
+    --------
+    matplotlib.pyplot.axis : get or set limits for current axes
+
+    Notes
+    -----
+    The `xylim` function avoids the need for passing a tuple to `axis`,
+    and has no return value, to avoid printing a value.  It is strictly
+    a convenience function for interactive use; use `axis` in scripts.
+    """
+    plt.axis(xmin=xmin, xmax=xmax, ymin=ymin, ymax=ymax)
+
+
+def logxy(islog=None, islogy=Ellipsis):
+    """Alternative to xscale, yscale for setting log or linear axes.
+
+    Parameters
+    ----------
+    islog : bool, optional
+        True means to use log scale for axes, False means linear scale.
+        None or omitted means leave axis scaling unchanged.
+    islogy : bool, optional
+        If islogy omitted, islog applies to both x and y axes.
+        If islogy provided, islog applies only to the x axis and islogy
+        applies to the y axis; values have the same meaning as for islog.
+    """
+    if islog is not None:
+        plt.xscale('log' if islog else 'linear')
+    if islogy is Ellipsis:
+        islogy = islog
+    if islogy is not None:
+        plt.yscale('log' if islogy else 'linear')
+
+
+__all__ += ['xylim', 'logxy']  # defined here
